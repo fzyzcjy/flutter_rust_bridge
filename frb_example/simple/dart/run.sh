23e879ad64acce60da78ab946373e23fb12a5820
fzyzcjy <ch271828n@outlook.com>
2021-10-04 22:38:12 +0800

    try to add dart.supp, but seems not good

--- frb_example/simple/dart/run.sh
+++ frb_example/simple/dart/run.sh
@@ -18,4 +18,5 @@ dart compile exe lib/main.dart -o main.exe
 # --log-file=valgrind.log # you can add this
 valgrind --leak-check=full --trace-children=yes --ignore-ranges=0x000-0xFFF \
   --error-exitcode=1 --errors-for-leak-kinds=definite \
+  --suppressions=dart.supp \
   ./main.exe "${CARGO_TARGET_DIR}/debug/libflutter_rust_bridge_example.so"
