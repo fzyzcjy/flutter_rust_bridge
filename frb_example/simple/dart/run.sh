3a9f07a0632dbb3980dd301296f9c213e6fa0527
fzyzcjy <ch271828n@outlook.com>
2021-10-04 22:54:47 +0800

    add valgrind_util

--- frb_example/simple/dart/run.sh
+++ frb_example/simple/dart/run.sh
@@ -14,8 +14,4 @@ fi
 # need to be AOT, since prod environment is AOT, and JIT+valgrind will have strange problems
 dart compile exe lib/main.dart -o main.exe
 
-# some configurations ref: https://github.com/dart-lang/sdk/blob/master/runtime/tools/valgrind.py
-# --log-file=valgrind.log # you can add this
-valgrind --leak-check=full --trace-children=yes --ignore-ranges=0x000-0xFFF \
-  --error-exitcode=1 --errors-for-leak-kinds=definite \
-  ./main.exe "${CARGO_TARGET_DIR}/debug/libflutter_rust_bridge_example.so"
+./valgrind_util.py ./main.exe "${CARGO_TARGET_DIR}/debug/libflutter_rust_bridge_example.so"
