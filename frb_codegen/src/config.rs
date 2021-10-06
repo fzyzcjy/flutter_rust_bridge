069f4157f4d1798328f77a85fc63bf17c049f683
fzyzcjy <ch271828n@outlook.com>
2021-10-06 16:47:23 +0800

    more default arguments

--- frb_codegen/src/config.rs
+++ frb_codegen/src/config.rs
@@ -6,6 +6,7 @@ use anyhow::Result;
 use convert_case::{Case, Casing};
 use serde::Deserialize;
 use structopt::StructOpt;
+use tempfile::NamedTempFile;
 use toml::Value;
 
 #[derive(StructOpt, Debug, PartialEq, Deserialize)]
@@ -49,25 +50,36 @@ pub fn parse(raw: RawOpts) -> Opts {
     let rust_input_path = canon_path(&raw.rust_input);
 
     let rust_crate_dir = canon_path(&raw.rust_crate_dir.unwrap_or_else(|| {
-        fallback_rust_crate_dir(&rust_input_path).expect("fail to guess rust_crate_dir")
+        fallback_rust_crate_dir(&rust_input_path)
+            .expect(&format_fail_to_guess_error("rust_crate_dir"))
     }));
     let rust_output_path = canon_path(&raw.rust_output.unwrap_or_else(|| {
-        fallback_rust_output_path(&rust_input_path).expect("fail to guess rust_output")
+        fallback_rust_output_path(&rust_input_path)
+            .expect(&format_fail_to_guess_error("rust_output"))
     }));
-
     let class_name = raw.class_name.unwrap_or_else(|| {
-        fallback_class_name(&*rust_crate_dir).expect("fail to guess class_name")
+        fallback_class_name(&*rust_crate_dir).expect(&format_fail_to_guess_error("class_name"))
     });
+    let c_output_path = canon_path(&raw.c_output.unwrap_or_else(|| {
+        fallback_c_output_path().expect(&format_fail_to_guess_error("c_output"))
+    }));
 
     Opts {
         rust_input_path,
         dart_output_path: canon_path(&raw.dart_output),
-        c_output_path: canon_path(&raw.c_output.unwrap_or_else(|| TODO)),
+        c_output_path,
         rust_crate_dir,
         rust_output_path,
         class_name,
         dart_format_line_length: raw.dart_format_line_length.unwrap_or(80),
     }
+}
+
+fn format_fail_to_guess_error(name: &str) -> String {
+    format!(
+        "fail to guess {}, please specify it manually in command line arguments",
+        name
+    )
 }
 
 fn fallback_rust_crate_dir(rust_input_path: &str) -> Result<String> {
@@ -88,6 +100,11 @@ fn fallback_rust_crate_dir(rust_input_path: &str) -> Result<String> {
     Err(anyhow!(
         "look at parent directories but none contains Cargo.toml"
     ))
+}
+
+fn fallback_c_output_path() -> Result<String> {
+    let named_temp_file = Box::leak(Box::new(NamedTempFile::new()?));
+    Ok(named_temp_file.path().to_str()?.to_string())
 }
 
 fn fallback_rust_output_path(rust_input_path: &str) -> Result<String> {
@@ -114,39 +131,6 @@ fn canon_path(sub_path: &str) -> String {
     path.into_os_string()
         .into_string()
         .expect(&format!("fail to parse path: {}", sub_path))
-}
-
-impl RawOpts {
-    // pub fn read(config_path: &str) -> Self {
-    //     let raw_config: Config =
-    //         serde_yaml::from_str(&fs::read_to_string(config_path).unwrap()).unwrap();
-    //     raw_config.canonicalize(PathBuf::from(config_path).parent().unwrap())
-    // }
-
-    // TODO
-    // pub fn canonicalize(self, base_dir: &Path) -> Config {
-    //     let canon_dir = |sub_path: &str| {
-    //         let mut path = PathBuf::from(base_dir);
-    //         path.push(sub_path);
-    //         path.into_os_string().into_string().unwrap()
-    //     };
-    //
-    //     Config {
-    //         rust: ConfigRust {
-    //             crate_dir: canon_dir(&self.rust.crate_dir),
-    //             input_path: canon_dir(&self.rust.input_path),
-    //             output_path: canon_dir(&self.rust.output_path),
-    //         },
-    //         dart: ConfigDart {
-    //             output_path: canon_dir(&self.dart.output_path),
-    //             output_class_name: self.dart.output_class_name,
-    //             format_line_length: self.dart.format_line_length,
-    //         },
-    //         c: ConfigC {
-    //             output_path: canon_dir(&self.c.output_path),
-    //         },
-    //     }
-    // }
 }
 
 impl RawOpts {
