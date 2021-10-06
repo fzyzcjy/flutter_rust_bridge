58f355122017535fcf1f48842575527071e70009
fzyzcjy <ch271828n@outlook.com>
2021-10-06 08:14:51 +0800

    try to add some flutter tests for the example app

---
+++ frb_example/with_flutter/integration_test/main.dart
@@ -0,0 +1,63 @@
+// ignore_for_file: avoid_print
+
+import 'dart:developer';
+import 'dart:io';
+import 'dart:isolate';
+
+import 'package:flutter_rust_bridge_example/main.dart' as app;
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:vm_service/vm_service_io.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('end-to-end test', () {
+    testWidgets('tap on the floating action button, verify counter', (WidgetTester tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+
+      // run many times to see memory leaks or other problems
+      for (var i = 0; i < 10; ++i) {
+        // wait a bit - should not crash
+        await Future.delayed(const Duration(seconds: 3));
+        await tester.pumpAndSettle();
+
+        expect(find.textContaining('Hi this string is from Rust'), findsOneWidget);
+
+        _maybeGC();
+      }
+    });
+  });
+}
+
+Future<void> _maybeGC() async {
+  final serverUri = (await Service.getInfo()).serverUri;
+
+  if (serverUri == null) {
+    print('Please run the application with the --observe parameter!');
+    exit(1);
+  }
+
+  final isolateId = Service.getIsolateID(Isolate.current)!;
+  final vmService = await vmServiceConnectUri(_toWebSocket(serverUri));
+  final profile = await vmService.getAllocationProfile(isolateId, gc: true);
+
+  print('Allocation profile: $profile');
+}
+
+List<String> _cleanupPathSegments(Uri uri) {
+  final pathSegments = <String>[];
+  if (uri.pathSegments.isNotEmpty) {
+    pathSegments.addAll(uri.pathSegments.where(
+      (s) => s.isNotEmpty,
+    ));
+  }
+  return pathSegments;
+}
+
+String _toWebSocket(Uri uri) {
+  final pathSegments = _cleanupPathSegments(uri);
+  pathSegments.add('ws');
+  return uri.replace(scheme: 'ws', pathSegments: pathSegments).toString();
+}
