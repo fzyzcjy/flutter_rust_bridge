22f77bd8187c2f3724fe7cd2b80c45c70b5b6c7a
fzyzcjy <ch271828n@outlook.com>
2021-10-16 11:42:59 +0800

    refactor Handler and add Executor

--- frb_rust/src/executor.rs
+++ frb_rust/src/executor.rs
@@ -18,53 +18,82 @@ pub trait Handler {
         TaskRet: IntoDart;
 }
 
-/// The simple handler uses a simple thread pool to execute tasks.
-pub struct SimpleHandler;
+pub trait Executor {
+    fn execute<TaskFn, TaskRet>(&self, task: TaskFn)
+    where
+        TaskFn: FnOnce() -> Result<TaskRet> + Send + UnwindSafe + 'static,
+        TaskRet: IntoDart;
+}
 
-impl SimpleHandler {
-    pub fn new() -> Self {
-        SimpleHandler {}
+/// The simple handler uses a simple thread pool to execute tasks.
+pub struct SimpleHandler<E: Executor> {
+    pub executor: E,
+}
+
+impl<E: Executor> SimpleHandler<E> {
+    pub fn new(executor: E) -> Self {
+        SimpleHandler { executor }
     }
 }
 
 impl Default for SimpleHandler {
     fn default() -> Self {
-        Self::new()
+        Self::new(ThreadPoolExecutor::new(4))
     }
 }
 
-impl Executor for SimpleHandler {
-    fn execute(&self, _debug_name: &str, port: i64, f: ExecutorTask) {
-        const DEFAULT_WORKER_THREAD_POOL_NUM_WORKERS: usize = 4;
-
-        lazy_static! {
-            static ref DEFAULT_WORKER_THREAD_POOL: Mutex<ThreadPool> =
-                Mutex::new(ThreadPool::new(DEFAULT_WORKER_THREAD_POOL_NUM_WORKERS));
-        }
-
+impl<E: Executor> Handler for SimpleHandler<E> {
+    fn wrap<PrepareFn, TaskFn, TaskRet>(&self, debug_name: &str, port: i64, prepare: PrepareFn)
+    where
+        PrepareFn: FnOnce() -> TaskFn,
+        TaskFn: FnOnce() -> Result<TaskRet> + Send + UnwindSafe + 'static,
+        TaskRet: IntoDart,
+    {
         let result = panic::catch_unwind(move || {
-            DEFAULT_WORKER_THREAD_POOL.lock().execute(move || {
-                let thread_result = panic::catch_unwind(move || {
-                    let rust2dart = Rust2Dart::new(port);
-
-                    let ret = f();
-
-                    match ret {
-                        Ok(result) => rust2dart.success(result),
-                        Err(error) => {
-                            rust2dart.error("GENERAL_ERROR".to_string(), format!("{:?}", error))
-                        }
-                    };
-                });
-
-                if let Err(err) = thread_result {
-                    Rust2Dart::new(port).error("PANIC_ERROR".to_string(), format!("{:?}", err));
-                }
-            });
+            self.executor.execute(f);
         });
 
         if let Err(err) = result {
             Rust2Dart::new(port).error("PANIC_ERROR".to_string(), format!("{:?}", err));
         }
+    }
+}
+
+pub struct ThreadPoolExecutor {
+    thread_pool: Mutex<ThreadPool>,
+}
+
+impl ThreadPoolExecutor {
+    pub fn new(num_threads: usize) -> Self {
+        ThreadPoolExecutor {
+            thread_pool: Mutex::new(ThreadPool::new(num_threads)),
+        }
+    }
+}
+
+impl Executor for ThreadPoolExecutor {
+    fn execute<TaskFn, TaskRet>(&self, task: TaskFn)
+    where
+        TaskFn: FnOnce() -> Result<TaskRet> + Send + UnwindSafe + 'static,
+        TaskRet: IntoDart,
+    {
+        self.thread_pool.lock().execute(move || {
+            let thread_result = panic::catch_unwind(move || {
+                let rust2dart = Rust2Dart::new(port);
+
+                let ret = task().map(|ret| ret.into_dart());
+
+                match ret {
+                    Ok(result) => rust2dart.success(result),
+                    Err(error) => {
+                        rust2dart.error("GENERAL_ERROR".to_string(), format!("{:?}", error))
+                    }
+                };
+            });
+
+            if let Err(err) = thread_result {
+                Rust2Dart::new(port).error("PANIC_ERROR".to_string(), format!("{:?}", err));
+            }
+        });
     }
 }
