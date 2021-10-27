b2548195c8f354e8881a839e852e0efd758e8108
Viet Dinh <54ckb0y789@gmail.com>
2021-10-27 00:08:10 -0400

    with-flutter on Windows

--- frb_example/with_flutter/lib/main.dart
+++ frb_example/with_flutter/lib/main.dart
@@ -10,13 +10,13 @@ import 'package:flutter_rust_bridge_example/off_topic_code.dart';
 // Simple Flutter code. If you are not familiar with Flutter, this may sounds a bit long. But indeed
 // it is quite trivial and Flutter is just like that. Please refer to Flutter's tutorial to learn Flutter.
 
-final ext = Platform.isWindows
-    ? 'dll'
+const base = 'flutter_rust_bridge_example';
+final path = Platform.isWindows
+    ? '$base.dll'
     : Platform.isMacOS
-        ? 'dylib'
-        : 'so';
-late final dylib =
-    Platform.isIOS ? DynamicLibrary.process() : DynamicLibrary.open('libflutter_rust_bridge_example.$ext');
+        ? 'lib$base.dylib'
+        : 'lib$base.so';
+late final dylib = Platform.isIOS ? DynamicLibrary.process() : DynamicLibrary.open(path);
 late final api = FlutterRustBridgeExample(dylib);
 
 void main() => runApp(const MyApp());
