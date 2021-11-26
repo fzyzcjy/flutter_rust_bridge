a4e9333a3d31f7f90e61b8b79719e52f98db5f13
fzyzcjy <ch271828n@outlook.com>
2021-11-26 20:31:24 +0800

    add more linter ignores

--- frb_codegen/src/generator_dart.rs
+++ frb_codegen/src/generator_dart.rs
@@ -55,7 +55,7 @@ pub fn generate(
     let header = format!(
         "{}
 
-        // ignore_for_file: non_constant_identifier_names, unused_element, duplicate_ignore, directives_ordering, curly_braces_in_flow_control_structures, unnecessary_lambdas, slash_for_doc_comments, prefer_const_literals_to_create_immutables
+        // ignore_for_file: non_constant_identifier_names, unused_element, duplicate_ignore, directives_ordering, curly_braces_in_flow_control_structures, unnecessary_lambdas, slash_for_doc_comments, prefer_const_literals_to_create_immutables, implicit_dynamic_list_literal
         import 'dart:convert';
         import 'dart:typed_data';
 
