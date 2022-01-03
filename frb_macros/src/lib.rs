17c71a841703ec8ff6d6bee5a37dba85301a1590
Viet Dinh <54ckb0y789@gmail.com>
2022-01-03 13:17:12 -0800

    Remove all dependencies from macros

--- frb_macros/src/lib.rs
+++ frb_macros/src/lib.rs
@@ -1,73 +1,39 @@
-use proc_macro::TokenStream;
-use quote::quote;
-use syn::{self, *};
+use proc_macro::*;
 
-fn is_marker_attr(attr: &Attribute) -> bool {
-    matches!(
-        attr.parse_meta(),
-        Ok(Meta::Path(path)) if path.is_ident("frb")
-    )
+fn remove_marker_attr(input: TokenStream, ident: &str) -> TokenStream {
+    #[derive(Default)]
+    struct Attribute(Option<TokenTree>);
+    use TokenTree as tt;
+    input
+        .into_iter()
+        .scan(Attribute::default(), |state, x| match (state, x) {
+            (Attribute(pound @ None), tt::Punct(p)) if p.as_char() == '#' => {
+                *pound = Some(TokenTree::Punct(p));
+                Some(vec![])
+            }
+            (Attribute(p @ Some(_)), tt::Group(g)) => match g.delimiter() {
+                Delimiter::Bracket => match g.stream().into_iter().next() {
+                    Some(tt::Ident(i)) if i.to_string() == ident => {
+                        let _ = p.take();
+                        Some(vec![])
+                    }
+                    Some(_) => Some(vec![p.take().unwrap(), tt::Group(g)]),
+                    _ => Some(vec![tt::Group(g)]),
+                },
+                _ => Some(vec![p.take().unwrap(), tt::Group(g)]),
+            },
+            (Attribute(None), tt::Group(g)) => Some(vec![tt::Group(Group::new(
+                g.delimiter(),
+                remove_marker_attr(g.stream(), ident),
+            ))]),
+            (_, x) => Some(vec![x]),
+        })
+        .flatten()
+        .collect()
 }
 
 /// Attribute to guide code generation.
 #[proc_macro_attribute]
-pub fn frb(_: TokenStream, item: TokenStream) -> TokenStream {
-    if let Ok(mut input) = syn::parse::<DeriveInput>(item.clone()) {
-        input.data = match input.data {
-            Data::Struct(mut st) => {
-                st.fields = remove_marker_attr_from_fields(st.fields);
-                Data::Struct(st)
-            }
-            Data::Enum(mut enu) => {
-                enu.variants = enu
-                    .variants
-                    .into_iter()
-                    .map(remove_marker_attr_from_variant)
-                    .collect();
-                Data::Enum(enu)
-            }
-            Data::Union(mut uni) => {
-                uni.fields = remove_marker_attr_from_named_fields(uni.fields);
-                Data::Union(uni)
-            }
-        };
-        quote! { #input }.into()
-    } else {
-        item
-    }
-}
-
-fn remove_marker_attr_from_variant(mut variant: Variant) -> Variant {
-    variant.attrs = variant.attrs.into_iter().filter(is_marker_attr).collect();
-    variant.fields = remove_marker_attr_from_fields(variant.fields);
-    variant
-}
-
-fn remove_marker_attr_from_field(mut field: Field) -> Field {
-    field.attrs = field.attrs.into_iter().filter(is_marker_attr).collect();
-    field
-}
-
-fn remove_marker_attr_from_named_fields(mut fields: FieldsNamed) -> FieldsNamed {
-    fields.named = fields
-        .named
-        .into_iter()
-        .map(remove_marker_attr_from_field)
-        .collect();
-    fields
-}
-
-fn remove_marker_attr_from_fields(fields: Fields) -> Fields {
-    match fields {
-        Fields::Named(fields) => Fields::Named(remove_marker_attr_from_named_fields(fields)),
-        Fields::Unnamed(mut fields) => {
-            fields.unnamed = fields
-                .unnamed
-                .into_iter()
-                .map(remove_marker_attr_from_field)
-                .collect();
-            Fields::Unnamed(fields)
-        }
-        _ => fields,
-    }
+pub fn frb(_: proc_macro::TokenStream, item: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    remove_marker_attr(item, "frb")
 }
