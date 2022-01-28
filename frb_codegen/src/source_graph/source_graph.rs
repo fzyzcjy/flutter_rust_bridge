9da5fe553c74f81c133ca769717ca46ac860a6ea
joshuawade <SecondFlight@users.noreply.github.com>
2022-01-27 23:48:34 -0500

    Capture module visibility

--- frb_codegen/src/source_graph/source_graph.rs
+++ frb_codegen/src/source_graph/source_graph.rs
@@ -1,10 +1,12 @@
 use std::{fmt::Debug, fs, path::PathBuf};
 
 use cargo_metadata::MetadataCommand;
+use syn::Ident;
 
 /// Represents a crate, including a map of its imports.
 #[derive(Debug)]
 pub struct Crate {
+    pub name: String,
     pub manifest_path: PathBuf,
     pub root_src_file: PathBuf,
     pub root_module: Module,
@@ -43,13 +45,15 @@ impl Crate {
         let file_ast = syn::parse_file(&source_rust_content).unwrap();
 
         let mut result = Crate {
+            name: root_package.name.clone(),
             manifest_path: fs::canonicalize(manifest_path).unwrap(),
             root_src_file: root_src_file.clone().into(),
             root_module: Module {
+                visibility: Visibility::Public,
                 file_path: root_src_file.clone(),
                 module_path: vec!["crate".to_string()],
                 root: Some(ModuleSource::File(file_ast)),
-                children: None,
+                scope: None,
             },
         };
 
@@ -64,37 +68,77 @@ impl Crate {
     }
 }
 
+// Mirrors syn::Visibility, but can be created without a token
+#[derive(Debug)]
+pub enum Visibility {
+    Public,
+    Crate,
+    Restricted, // Not currently supported
+    Inherited, // Usually means private
+}
+
+fn syn_vis_to_visibility(vis: &syn::Visibility) -> Visibility {
+    match vis {
+        syn::Visibility::Public(_) => Visibility::Public,
+        syn::Visibility::Crate(_) => Visibility::Crate,
+        syn::Visibility::Restricted(_) => Visibility::Restricted,
+        syn::Visibility::Inherited => Visibility::Inherited,
+    }
+}
+
+#[derive(Debug)]
+pub struct Import;
+
 #[derive(Debug)]
 pub enum ModuleSource {
     File(syn::File),
     ModuleInFile(Vec<syn::Item>),
 }
 
+#[derive(Debug)]
+pub struct ModuleScope {
+    pub modules: Vec<Module>,
+    pub enums: Vec<Ident>,
+    pub structs: Vec<Ident>,
+    pub imports: Vec<Import>,
+}
+
+// TODO: Capture struct and enum visibility
 pub struct Module {
+    pub visibility: Visibility,
     pub file_path: PathBuf,
     pub module_path: Vec<String>,
     pub root: Option<ModuleSource>,
-    pub children: Option<Vec<Module>>,
+    pub scope: Option<ModuleScope>,
 }
 
 impl Debug for Module {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("Module")
-            .field("file_path", &self.file_path)
+            .field("visibility", &self.visibility)
             .field("module_path", &self.module_path)
+            .field("file_path", &self.file_path)
             .field("root", &"omitted")
-            .field("children", &self.children)
+            .field("scope", &self.scope)
             .finish()
     }
 }
 
 impl Module {
     pub fn is_resolved(&self) -> bool {
-        self.children.is_some() && self.root.is_some()
+        self.scope.is_some() && self.root.is_some()
     }
 
     pub fn resolve(&mut self) {
-        let mut children = Vec::new();
+        self.resolve_modules();
+        self.resolve_imports();
+    }
+
+    /// Maps out modules, structs and enums within the scope of this module
+    fn resolve_modules(&mut self) {
+        let mut scope_modules = Vec::new();
+        let mut scope_structs = Vec::new();
+        let mut scope_enums = Vec::new();
 
         let items = match self.root.as_ref().unwrap() {
             ModuleSource::File(file) => &file.items,
@@ -103,19 +147,32 @@ impl Module {
 
         for item in items.iter() {
             match item {
+                syn::Item::Struct(item_struct) => {
+                    scope_structs.push(item_struct.ident.clone());
+                }
+                syn::Item::Enum(item_enum) => {
+                    scope_enums.push(item_enum.ident.clone());
+                }
                 syn::Item::Mod(item_mod) => {
                     let ident = item_mod.ident.clone();
 
                     let mut module_path = self.module_path.clone();
                     module_path.push(ident.to_string());
 
-                    children.push(match &item_mod.content {
-                        Some(content) => Module {
-                            file_path: self.file_path.clone(),
-                            module_path,
-                            root: Some(ModuleSource::ModuleInFile(content.1.clone())),
-                            children: Some(vec![]),
-                        },
+                    scope_modules.push(match &item_mod.content {
+                        Some(content) => {
+                            let mut child_module = Module {
+                                visibility: syn_vis_to_visibility(&item_mod.vis),
+                                file_path: self.file_path.clone(),
+                                module_path,
+                                root: Some(ModuleSource::ModuleInFile(content.1.clone())),
+                                scope: None,
+                            };
+
+                            child_module.resolve();
+
+                            child_module
+                        }
                         None => {
                             let folder_path =
                                 self.file_path.parent().unwrap().join(ident.to_string());
@@ -142,10 +199,11 @@ impl Module {
                             };
 
                             let mut child_module = Module {
+                                visibility: syn_vis_to_visibility(&item_mod.vis),
                                 file_path,
                                 module_path,
                                 root,
-                                children: None,
+                                scope: None,
                             };
 
                             if file_exists {
@@ -160,7 +218,16 @@ impl Module {
             }
         }
 
-        self.children = Some(children);
+        self.scope = Some(ModuleScope {
+            modules: scope_modules,
+            enums: scope_enums,
+            structs: scope_structs,
+            imports: vec![], // Will be filled in by resolve_imports()
+        });
+    }
+
+    fn resolve_imports(&mut self) {
+        // let mut imports = &self.scope.as_ref().unwrap().imports;
     }
 }
 
