5bf69e7c223f95646cd3a1afb4a3ed3d15ca2ead
fzyzcjy <5236035+fzyzcjy@users.noreply.github.com>
2022-02-12 14:05:54 +0800

    fix formatter

--- frb_codegen/src/parser.rs
+++ frb_codegen/src/parser.rs
@@ -132,7 +132,10 @@ impl<'a> Parser<'a> {
                 if let Some(inner) = CAPTURE_RESULT.captures(&type_string) {
                     self.parse_type(&inner)
                 } else {
-                    panic!("Functions must return `Result<_>`, but current type_string is: {}", type_string);
+                    panic!(
+                        "Functions must return `Result<_>`, but current type_string is: {}",
+                        type_string
+                    );
                 }
             } else {
                 panic!("unsupported output: {:?}", sig.output);
