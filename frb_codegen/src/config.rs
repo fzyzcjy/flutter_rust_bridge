1360d5dc899061dfb7fac544fce1cc19b72a2f30
Viet Dinh <54ckb0y789@gmail.com>
2022-03-08 11:36:11 -0500

    Disable --wasm

--- frb_codegen/src/config.rs
+++ frb_codegen/src/config.rs
@@ -59,9 +59,10 @@ pub struct RawOpts {
     /// Show debug messages.
     #[structopt(short, long)]
     pub verbose: bool,
-    /// Emit glue code to interface with wasm-bindgen. Overrides --dart-decl-output.
-    #[structopt(long)]
-    pub wasm: bool,
+    // TODO: Enable --wasm config once done
+    // /// Emit glue code to interface with wasm-bindgen. Overrides --dart-decl-output.
+    // #[structopt(long)]
+    // pub wasm: bool,
 }
 
 #[derive(Debug)]
@@ -152,7 +153,7 @@ pub fn parse(raw: RawOpts) -> Opts {
             ]
         }),
         llvm_compiler_opts: raw.llvm_compiler_opts.unwrap_or_else(|| "".to_string()),
-        wasm: raw.wasm,
+        wasm: false,
         manifest_path,
         dart_root,
         build_runner: !raw.no_build_runner,
