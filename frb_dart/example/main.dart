3e23d140dfb353e7b459ef252de329245f959b17
fzyzcjy <ch271828n@outlook.com>
2022-04-24 10:45:41 +0800

    change example

--- frb_dart/example/main.dart
+++ frb_dart/example/main.dart
@@ -1,4 +1,13 @@
 void main() {
-  // Well, indeed you should not directly use code exported by flutter_rust_bridge.
-  // Instead, use the generated API class.
+  // ============================================================================================
+  // Well, indeed you should not directly use code exported by `flutter_rust_bridge`.
+  // Instead, run the code-generator and use the automatically generated code.
+  //
+  // Please see https://fzyzcjy.github.io/flutter_rust_bridge/ - a *whole* mini-book
+  // demonstrating the package, with plenty of examples!
+  //
+  // Or, you may be interested in:
+  // * A quick tutorial: http://fzyzcjy.github.io/flutter_rust_bridge/tutorial_with_flutter.html
+  // * The feature list: http://fzyzcjy.github.io/flutter_rust_bridge/feature.html
+  // ============================================================================================
 }
