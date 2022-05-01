3bbf1596b1bf1fc45ab9c02f9154c0f7c3833e28
sagudev <16504129+sagudev@users.noreply.github.com>
2022-05-01 07:42:18 +0200

    use build.rs in pure_dart example

---
+++ frb_example/pure_dart/rust/build.rs
@@ -0,0 +1,19 @@
+use lib_lutter_rust_bridge_codegen::{frb_codegen, Opts};
+
+const BRIDGE_FILE: &'static str = "src/api.rs";
+
+fn main() {
+    // Tell Cargo that if the given file changes, to rerun this build script.
+    println!("cargo:rerun-if-changed={BRIDGE_FILE}");
+    // settings for fbr_codegen
+    let opts = Opts {
+        // Path of input Rust code
+        rust_input: BRIDGE_FILE.to_string(),
+        // Path of output generated Dart code
+        dart_output: "../dart/lib/bridge_generated.dart".to_string(),
+        // for other options lets use default
+        ..Default::default()
+    };
+    // run fbr_codegen
+    frb_codegen(opts);
+}
