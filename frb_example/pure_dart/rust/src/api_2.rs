36014bb4012e7e223ecffd641cc5b8199f932c6e
dbsx <602721790@qq.com>
2022-06-17 12:09:55 +0800

    add test for multi blocks of rust api

---
+++ frb_example/pure_dart/rust/src/api_2.rs
@@ -0,0 +1,4 @@
+/// Documentation on a simple adder function.
+pub fn simple_adder2(a: i32, b: i32) -> i32 {
+    a + b
+}
--- frb_example/pure_dart/rust/src/lib.rs
+++ frb_example/pure_dart/rust/src/lib.rs
@@ -1,3 +1,5 @@
-mod api;
-mod bridge_generated;
+mod api_1;
+mod api_2;
+mod generated_api_1;
+mod generated_api_2;
 mod data;
