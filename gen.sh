29b7d6c06e101a42dac2dab29bf6bee56cd2368d
Lattice 0 <lattice0@protonmail.com>
2022-07-01 11:47:20 -0300

    gen

---
+++ gen.sh
@@ -0,0 +1,3 @@
+#cargo run --manifest-path=frb_codegen/Cargo.toml -- --rust-input frb_example/with_flutter/rust/src/api.rs --dart-output frb_example/with_flutter/lib/bridge_generated.dart
+#cargo run --manifest-path=frb_codegen/Cargo.toml -- --rust-input something/src/api.rs --dart-output something/bridge_generated.dart
+cargo run --manifest-path=frb_codegen/Cargo.toml -- --rust-input frb_example/pure_dart/rust/src/api.rs --dart-output frb_example/pure_dart/dart/lib/bridge_generated.dart
