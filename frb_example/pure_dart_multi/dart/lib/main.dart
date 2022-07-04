d778e39271e4a598d5759ed9bfa245f7e2c936b6
dbsx <602721790@qq.com>
2022-07-04 11:54:52 +0800

    add `run.sh` back to test example for multi blocks

--- frb_example/pure_dart_multi/dart/lib/main.dart
+++ frb_example/pure_dart_multi/dart/lib/main.dart
@@ -5,18 +5,18 @@ import 'bridge_generated_api_2.dart';
 import 'package:test/test.dart';
 
 void main(List<String> args) {
-  String dylibPath1 = args[0];
-  String dylibPath2 = args[1];
-  print('flutter_rust_bridge example program start (dylibPath=$dylibPath1,$dylibPath2)');
+  print(args.length);
+  print(args);
+  String dylibPath = args[0];
+  print('flutter_rust_bridge example program start (dylibPath=$dylibPath)');
   print('construct api');
-  final dylib1 = DynamicLibrary.open(dylibPath1);
-  final api1 = ApiClass1Impl(dylib1);
+  final dylib = DynamicLibrary.open(dylibPath);
+  final api1 = ApiClass1Impl(dylib);
   test('dart call simpleAdder', () async {
     expect(await api1.simpleAdder1(a: 42, b: 100), 142);
   });
 
-  final dylib2 = DynamicLibrary.open(dylibPath2);
-  final api2 = ApiClass2Impl(dylib2);
+  final api2 = ApiClass2Impl(dylib);
   test('dart call simpleAdder2', () async {
     expect(await api2.simpleAdder2(a: 42, b: 100), 142);
   });
