2a13ad27ca66a6b34378abe7a8f55ff4490274c6
dbsx <602721790@qq.com>
2022-07-04 15:34:37 +0800

    change `api_1.rs` name to match CI config file

--- frb_example/pure_dart_multi/rust/src/lib.rs
+++ frb_example/pure_dart_multi/rust/src/lib.rs
@@ -1,4 +1,4 @@
-mod api_1;
+mod api;
 mod api_2;
 mod generated_api_1; /* AUTO INJECTED BY flutter_rust_bridge. This line may not be accurate, and you can change it according to your needs. */
 mod generated_api_2; /* AUTO INJECTED BY flutter_rust_bridge. This line may not be accurate, and you can change it according to your needs. */
