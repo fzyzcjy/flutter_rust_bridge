cb3a68de81b755221dab767d075c0145e6590d6e
Lattice 0 <lattice0@protonmail.com>
2022-07-05 22:34:09 -0300

    removes trash

--- something/dart/bin/dart.dart
+++
@@ -1,5 +0,0 @@
-import 'package:dart/dart.dart' as dart;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dart.calculate()}!');
-}
--- something/dart/test/dart_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dart/dart.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
