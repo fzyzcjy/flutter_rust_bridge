17c785df7e7ce722eae11444a86a57da295df583
Lattice 0 <lattice0@protonmail.com>
2022-07-18 16:36:06 -0300

    works

---
+++ something/something/bin/something.dart
@@ -0,0 +1,5 @@
+import 'package:something/something.dart' as something;
+
+void main(List<String> arguments) {
+  print('Hello world: ${something.calculate()}!');
+}
---
+++ something/something/test/something_test.dart
@@ -0,0 +1,8 @@
+import 'package:something/something.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
