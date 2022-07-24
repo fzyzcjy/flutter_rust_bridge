db40975844ed70080fe501f7916705b2862ff48e
Lattice 0 <lattice0@protonmail.com>
2022-07-24 13:50:37 -0300

    removes cargo

--- gen.sh
+++
@@ -1,8 +0,0 @@
-set -ex
-cargo run --manifest-path=frb_codegen/Cargo.toml -- --rust-input frb_example/pure_dart/rust/src/api.rs --dart-output frb_example/pure_dart/dart/lib/bridge_generated.dart --dart-format-line-length 120
-dart format --line-length 120 frb_example/pure_dart/dart/
-cargo fmt
-dart format --line-length 80 frb_dart
-cargo build
-cd ~/flutter_rust_bridge/frb_example/pure_dart/dart && dart run lib/main.dart ~/flutter_rust_bridge/target/debug/libflutter_rust_bridge_example.so
-#cargo run --manifest-path=frb_codegen/Cargo.toml -- --rust-input something/src/api.rs --dart-output something/something/lib/bridge_generated.dart --dart-format-line-length 120
