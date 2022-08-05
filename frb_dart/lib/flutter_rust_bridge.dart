12e794e79bbcc46c83a5b25baca0295abf3818ba
Lattice 0 <lattice0@protonmail.com>
2022-08-05 19:30:41 -0300

    removes unused import

--- frb_dart/lib/flutter_rust_bridge.dart
+++ frb_dart/lib/flutter_rust_bridge.dart
@@ -1,5 +1,3 @@
-import 'package:flutter_rust_bridge/src/basic.dart';
-
 export 'src/flutter_rust_bridge_io.dart'
     if (dart.library.html) 'flutter_rust_bridge_web.dart';
 export 'src/platform_independent.dart';
