8bd9d7deb41e138427c5249999daca9c71e9684d
Roms1383 <romain.kelifa@gmail.com>
2022-09-16 23:03:04 +0700

    :construction: update from review
    - delegate to dart api2wire
    
    Co-authored-by: Viet Dinh <54ckb0y789@gmail.com>

--- frb_codegen/src/generator/dart/ty_delegate.rs
+++ frb_codegen/src/generator/dart/ty_delegate.rs
@@ -42,22 +42,22 @@ impl TypeDartGeneratorTrait for TypeDelegateGenerator<'_> {
             IrTypeDelegate::Time(ref ir) => match ir {
                 IrTypeTime::Utc => Acc {
                     io: Some("return raw.microsecondsSinceEpoch;".into()),
-                    wasm: Some("return BigInt.from(raw.millisecondsSinceEpoch);".into()),
+                    wasm: Some("return api2wire_i64(raw.millisecondsSinceEpoch);".into()),
                     ..Default::default()
                 },
                 IrTypeTime::Local => Acc {
                     io: Some("return raw.microsecondsSinceEpoch;".into()),
-                    wasm: Some("return BigInt.from(raw.millisecondsSinceEpoch);".into()),
+                    wasm: Some("return api2wire_i64(raw.millisecondsSinceEpoch);".into()),
                     ..Default::default()
                 },
                 IrTypeTime::Naive => Acc {
                     io: Some("return raw.microsecondsSinceEpoch;".into()),
-                    wasm: Some("return BigInt.from(raw.millisecondsSinceEpoch);".into()),
+                    wasm: Some("return api2wire_i64(raw.millisecondsSinceEpoch);".into()),
                     ..Default::default()
                 },
                 IrTypeTime::Duration => Acc {
                     io: Some("return raw.inMicroseconds;".into()),
-                    wasm: Some("return BigInt.from(raw.inMilliseconds);".into()),
+                    wasm: Some("return api2wire_i64(raw.inMilliseconds);".into()),
                     ..Default::default()
                 },
             },
