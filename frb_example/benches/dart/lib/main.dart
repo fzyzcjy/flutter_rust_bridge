e8beecc7f6c7b544b7d0f3eb7e0ee86e1ec2be23
Roms1383 <romain.kelifa@gmail.com>
2022-10-02 00:02:04 +0700

    :coffin: remove unused method

--- frb_example/benches/dart/lib/main.dart
+++ frb_example/benches/dart/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter_rust_bridge/flutter_rust_bridge.dart';
 import 'ffi.io.dart' if (dart.library.html) 'ffi.web.dart';
 import 'package:flutter_rust_bridge_benchmark/utils.dart';
-import 'package:test/test.dart';
 import 'package:uuid/uuid.dart';
 
 void main(List<String> args) async {
@@ -38,19 +37,6 @@ void main(List<String> args) async {
   );
 
   await api.whenBenchmarkComplete();
-}
-
-class MatchBigInt extends CustomMatcher {
-  MatchBigInt(matcher)
-      : super("is a numeric", "value", _featureValueOf(matcher));
-  @override
-  Object? featureValueOf(actual) => _featureValueOf(actual);
-
-  static Object? _featureValueOf(actual) {
-    if (actual is Iterable) return actual.map(_featureValueOf);
-    if (actual is int) return BigInt.from(actual);
-    return actual;
-  }
 }
 
 // vim:expandtab:ts=2:sw=2
