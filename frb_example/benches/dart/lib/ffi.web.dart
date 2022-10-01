47ad80c56c7aa1c1b0f14b64996a86ed10a39552
Roms1383 <romain.kelifa@gmail.com>
2022-10-01 20:36:11 +0700

    :pencil2: fix incorrect benches example wasm binary

--- frb_example/benches/dart/lib/ffi.web.dart
+++ frb_example/benches/dart/lib/ffi.web.dart
@@ -3,7 +3,7 @@ import 'interceptor.dart';
 export 'bridge_generated.web.dart';
 import 'package:flutter_rust_bridge/flutter_rust_bridge.dart';
 
-const root = 'pkg/flutter_rust_bridge_example';
+const root = 'pkg/flutter_rust_bridge_example_benchmark_suite';
 
 FlutterRustBridgeExampleBenchmarkSuiteImplBench initializeExternalLibrary(
         void _) =>
