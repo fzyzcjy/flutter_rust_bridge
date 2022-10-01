b7d9c3fc412bae82c791174bb10b85d186e3b726
Roms1383 <romain.kelifa@gmail.com>
2022-10-01 14:44:53 +0700

    :camera: codegen

---
+++ frb_example/benches/rust/src/bridge_generated.io.rs
@@ -0,0 +1,162 @@
+use super::*;
+// Section: wire functions
+
+#[no_mangle]
+pub extern "C" fn wire_handle_uuid(port_: i64, id: *mut wire_uint_8_list) {
+    wire_handle_uuid_impl(port_, id)
+}
+
+#[no_mangle]
+pub extern "C" fn wire_handle_uuids(port_: i64, ids: *mut wire_uint_8_list) {
+    wire_handle_uuids_impl(port_, ids)
+}
+
+#[no_mangle]
+pub extern "C" fn wire_handle_uuids_convert_to_strings(port_: i64, ids: *mut wire_uint_8_list) {
+    wire_handle_uuids_convert_to_strings_impl(port_, ids)
+}
+
+#[no_mangle]
+pub extern "C" fn wire_handle_nested_uuids(port_: i64, ids: *mut wire_FeatureUuid) {
+    wire_handle_nested_uuids_impl(port_, ids)
+}
+
+#[no_mangle]
+pub extern "C" fn wire_handle_strings(port_: i64, strings: *mut wire_StringList) {
+    wire_handle_strings_impl(port_, strings)
+}
+
+#[no_mangle]
+pub extern "C" fn wire_send_i64(port_: i64, value: i64) {
+    wire_send_i64_impl(port_, value)
+}
+
+// Section: allocate functions
+
+#[no_mangle]
+pub extern "C" fn new_StringList_0(len: i32) -> *mut wire_StringList {
+    let wrap = wire_StringList {
+        ptr: support::new_leak_vec_ptr(<*mut wire_uint_8_list>::new_with_null_ptr(), len),
+        len,
+    };
+    support::new_leak_box_ptr(wrap)
+}
+
+#[no_mangle]
+pub extern "C" fn new_box_autoadd_feature_uuid_0() -> *mut wire_FeatureUuid {
+    support::new_leak_box_ptr(wire_FeatureUuid::new_with_null_ptr())
+}
+
+#[no_mangle]
+pub extern "C" fn new_uint_8_list_0(len: i32) -> *mut wire_uint_8_list {
+    let ans = wire_uint_8_list {
+        ptr: support::new_leak_vec_ptr(Default::default(), len),
+        len,
+    };
+    support::new_leak_box_ptr(ans)
+}
+
+// Section: impl Wire2Api
+
+impl Wire2Api<String> for *mut wire_uint_8_list {
+    fn wire2api(self) -> String {
+        let vec: Vec<u8> = self.wire2api();
+        String::from_utf8_lossy(&vec).into_owned()
+    }
+}
+impl Wire2Api<Vec<String>> for *mut wire_StringList {
+    fn wire2api(self) -> Vec<String> {
+        let vec = unsafe {
+            let wrap = support::box_from_leak_ptr(self);
+            support::vec_from_leak_ptr(wrap.ptr, wrap.len)
+        };
+        vec.into_iter().map(Wire2Api::wire2api).collect()
+    }
+}
+impl Wire2Api<uuid::Uuid> for *mut wire_uint_8_list {
+    fn wire2api(self) -> uuid::Uuid {
+        let single: Vec<u8> = self.wire2api();
+        wire2api_uuid_ref(single.as_slice())
+    }
+}
+impl Wire2Api<Vec<uuid::Uuid>> for *mut wire_uint_8_list {
+    fn wire2api(self) -> Vec<uuid::Uuid> {
+        let multiple: Vec<u8> = self.wire2api();
+        wire2api_uuids(multiple)
+    }
+}
+impl Wire2Api<FeatureUuid> for *mut wire_FeatureUuid {
+    fn wire2api(self) -> FeatureUuid {
+        let wrap = unsafe { support::box_from_leak_ptr(self) };
+        Wire2Api::<FeatureUuid>::wire2api(*wrap).into()
+    }
+}
+impl Wire2Api<FeatureUuid> for wire_FeatureUuid {
+    fn wire2api(self) -> FeatureUuid {
+        FeatureUuid {
+            one: self.one.wire2api(),
+            many: self.many.wire2api(),
+        }
+    }
+}
+
+impl Wire2Api<Vec<u8>> for *mut wire_uint_8_list {
+    fn wire2api(self) -> Vec<u8> {
+        unsafe {
+            let wrap = support::box_from_leak_ptr(self);
+            support::vec_from_leak_ptr(wrap.ptr, wrap.len)
+        }
+    }
+}
+// Section: wire structs
+
+#[repr(C)]
+#[derive(Clone)]
+pub struct wire_StringList {
+    ptr: *mut *mut wire_uint_8_list,
+    len: i32,
+}
+
+#[repr(C)]
+#[derive(Clone)]
+pub struct wire_FeatureUuid {
+    one: *mut wire_uint_8_list,
+    many: *mut wire_uint_8_list,
+}
+
+#[repr(C)]
+#[derive(Clone)]
+pub struct wire_uint_8_list {
+    ptr: *mut u8,
+    len: i32,
+}
+
+// Section: impl NewWithNullPtr
+
+pub trait NewWithNullPtr {
+    fn new_with_null_ptr() -> Self;
+}
+
+impl<T> NewWithNullPtr for *mut T {
+    fn new_with_null_ptr() -> Self {
+        std::ptr::null_mut()
+    }
+}
+
+impl NewWithNullPtr for wire_FeatureUuid {
+    fn new_with_null_ptr() -> Self {
+        Self {
+            one: core::ptr::null_mut(),
+            many: core::ptr::null_mut(),
+        }
+    }
+}
+
+// Section: sync execution mode utility
+
+#[no_mangle]
+pub extern "C" fn free_WireSyncReturnStruct(val: support::WireSyncReturnStruct) {
+    unsafe {
+        let _ = support::vec_from_leak_ptr(val.ptr, val.len);
+    }
+}
---
+++ frb_example/benches/rust/src/bridge_generated.web.rs
@@ -0,0 +1,123 @@
+use super::*;
+// Section: wire functions
+
+#[wasm_bindgen]
+pub fn wire_handle_uuid(port_: MessagePort, id: Box<[u8]>) {
+    wire_handle_uuid_impl(port_, id)
+}
+
+#[wasm_bindgen]
+pub fn wire_handle_uuids(port_: MessagePort, ids: Box<[u8]>) {
+    wire_handle_uuids_impl(port_, ids)
+}
+
+#[wasm_bindgen]
+pub fn wire_handle_uuids_convert_to_strings(port_: MessagePort, ids: Box<[u8]>) {
+    wire_handle_uuids_convert_to_strings_impl(port_, ids)
+}
+
+#[wasm_bindgen]
+pub fn wire_handle_nested_uuids(port_: MessagePort, ids: JsValue) {
+    wire_handle_nested_uuids_impl(port_, ids)
+}
+
+#[wasm_bindgen]
+pub fn wire_handle_strings(port_: MessagePort, strings: JsValue) {
+    wire_handle_strings_impl(port_, strings)
+}
+
+#[wasm_bindgen]
+pub fn wire_send_i64(port_: MessagePort, value: i64) {
+    wire_send_i64_impl(port_, value)
+}
+
+// Section: allocate functions
+
+// Section: impl Wire2Api
+
+impl Wire2Api<String> for String {
+    fn wire2api(self) -> String {
+        self
+    }
+}
+impl Wire2Api<Vec<String>> for JsValue {
+    fn wire2api(self) -> Vec<String> {
+        self.dyn_into::<JsArray>()
+            .unwrap()
+            .iter()
+            .map(Wire2Api::wire2api)
+            .collect()
+    }
+}
+impl Wire2Api<uuid::Uuid> for Box<[u8]> {
+    fn wire2api(self) -> uuid::Uuid {
+        let single: Vec<u8> = self.wire2api();
+        wire2api_uuid_ref(single.as_slice())
+    }
+}
+impl Wire2Api<Vec<uuid::Uuid>> for Box<[u8]> {
+    fn wire2api(self) -> Vec<uuid::Uuid> {
+        let multiple: Vec<u8> = self.wire2api();
+        wire2api_uuids(multiple)
+    }
+}
+
+impl Wire2Api<FeatureUuid> for JsValue {
+    fn wire2api(self) -> FeatureUuid {
+        let self_ = self.dyn_into::<JsArray>().unwrap();
+        assert_eq!(
+            self_.length(),
+            2,
+            "Expected 2 elements, got {}",
+            self_.length()
+        );
+        FeatureUuid {
+            one: self_.get(0).wire2api(),
+            many: self_.get(1).wire2api(),
+        }
+    }
+}
+
+impl Wire2Api<Vec<u8>> for Box<[u8]> {
+    fn wire2api(self) -> Vec<u8> {
+        self.into_vec()
+    }
+}
+// Section: impl Wire2Api for JsValue
+
+impl Wire2Api<String> for JsValue {
+    fn wire2api(self) -> String {
+        self.as_string().expect("non-UTF-8 string, or not a string")
+    }
+}
+impl Wire2Api<uuid::Uuid> for JsValue {
+    fn wire2api(self) -> uuid::Uuid {
+        self.unchecked_into::<js_sys::Uint8Array>()
+            .to_vec()
+            .into_boxed_slice()
+            .wire2api()
+    }
+}
+impl Wire2Api<Vec<uuid::Uuid>> for JsValue {
+    fn wire2api(self) -> Vec<uuid::Uuid> {
+        self.unchecked_into::<js_sys::Uint8Array>()
+            .to_vec()
+            .into_boxed_slice()
+            .wire2api()
+    }
+}
+impl Wire2Api<i64> for JsValue {
+    fn wire2api(self) -> i64 {
+        ::std::convert::TryInto::try_into(self.dyn_into::<js_sys::BigInt>().unwrap()).unwrap()
+    }
+}
+impl Wire2Api<u8> for JsValue {
+    fn wire2api(self) -> u8 {
+        self.unchecked_into_f64() as _
+    }
+}
+impl Wire2Api<Vec<u8>> for JsValue {
+    fn wire2api(self) -> Vec<u8> {
+        self.unchecked_into::<js_sys::Uint8Array>().to_vec().into()
+    }
+}
