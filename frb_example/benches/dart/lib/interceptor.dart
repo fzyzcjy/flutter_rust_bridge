4e0b6aec58c4c604eddb912680f58ff475fd593f
Roms1383 <romain.kelifa@gmail.com>
2022-10-01 20:35:50 +0700

    :recycle: use solely stopwatch
    and refactor incorrect implementation with mixin (on platform implementation instead)

--- frb_example/benches/dart/lib/interceptor.dart
+++ frb_example/benches/dart/lib/interceptor.dart
@@ -1,21 +1,18 @@
 import 'dart:async';
+import 'dart:developer';
 
 import 'package:flutter_rust_bridge/flutter_rust_bridge.dart';
-import 'interceptor.io.dart' if (dart.library.html) 'interceptor.web.dart';
 import 'bridge_generated.io.dart'
     if (dart.library.html) 'bridge_generated.web.dart';
+import 'dart:ffi' as ffi;
 
 class FlutterRustBridgeExampleBenchmarkSuiteImplBench
     extends FlutterRustBridgeExampleBenchmarkSuiteImpl {
-  // ignore: unused_field
-  final FlutterRustBridgeInterceptor _interceptor =
-      FlutterRustBridgeInterceptor();
   factory FlutterRustBridgeExampleBenchmarkSuiteImplBench(
           ExternalLibrary dylib) =>
       FlutterRustBridgeExampleBenchmarkSuiteImplBench.raw(
-          FlutterRustBridgeExampleBenchmarkSuitePlatform(
-        dylib,
-      ));
+          FlutterRustBridgeExampleBenchmarkSuitePlatformBench(
+              dylib, FlutterRustBridgeInterceptor()));
 
   /// Only valid on web/WASM platforms.
   factory FlutterRustBridgeExampleBenchmarkSuiteImplBench.wasm(
@@ -24,10 +21,79 @@ class FlutterRustBridgeExampleBenchmarkSuiteImplBench
           module as ExternalLibrary);
   @override
   FlutterRustBridgeExampleBenchmarkSuiteImplBench.raw(
-      FlutterRustBridgeExampleBenchmarkSuitePlatform platform)
+      FlutterRustBridgeExampleBenchmarkSuitePlatformBench platform)
       : super.raw(platform);
+}
 
-  FlutterRustBridgeInterceptor get interceptor {
-    return _interceptor;
+class AsyncStopWatch extends Stopwatch {
+  int? starts;
+  int? ends;
+  @override
+  void start() {
+    starts = elapsedMicroseconds;
+    super.start();
+  }
+
+  @override
+  void stop() {
+    ends = elapsedMicroseconds;
+    super.stop();
+  }
+}
+
+class FlutterRustBridgeInterceptor {
+  Future<AsyncStopWatch> beforeExecuteNormal(String debugName) async {
+    log('@beforeExecuteNormal $debugName');
+    return Future.sync(() {
+      final AsyncStopWatch stopwatch = AsyncStopWatch();
+      stopwatch.start();
+      return stopwatch;
+    });
+  }
+
+  Future<void> afterExecuteNormal<S>(
+      String debugName, AsyncStopWatch stopwatch) async {
+    return Future.sync(() {
+      stopwatch.stop();
+      log('[$debugName] executed in ${stopwatch.ends! - stopwatch.starts!}μs');
+    });
+  }
+
+  AsyncStopWatch beforeExecuteSync<S>(String _) {
+    final AsyncStopWatch stopwatch = AsyncStopWatch();
+    stopwatch.start();
+    return stopwatch;
+  }
+
+  void afterExecuteSync<S>(String debugName, AsyncStopWatch stopwatch) {
+    stopwatch.stop();
+    log('[$debugName] executed in ${stopwatch.ends! - stopwatch.starts!}μs');
+  }
+}
+
+class FlutterRustBridgeExampleBenchmarkSuitePlatformBench
+    extends FlutterRustBridgeExampleBenchmarkSuitePlatform {
+  final FlutterRustBridgeInterceptor _interceptor;
+  FlutterRustBridgeExampleBenchmarkSuitePlatformBench(
+      ffi.DynamicLibrary dylib, this._interceptor)
+      : super(dylib);
+  FlutterRustBridgeInterceptor get interceptor => _interceptor;
+  @override
+  Future<S> executeNormal<S>(FlutterRustBridgeTask<S> task) async {
+    final String debugName = task.constMeta.debugName;
+    final AsyncStopWatch stopwatch =
+        await interceptor.beforeExecuteNormal(debugName);
+    final result = await super.executeNormal(task);
+    await interceptor.afterExecuteNormal(debugName, stopwatch);
+    return result;
+  }
+
+  @override
+  S executeSync<S>(FlutterRustBridgeSyncTask task) {
+    final String debugName = task.constMeta.debugName;
+    final AsyncStopWatch stopwatch = interceptor.beforeExecuteSync(debugName);
+    final result = super.executeSync(task);
+    interceptor.afterExecuteSync(debugName, stopwatch);
+    return result;
   }
 }
