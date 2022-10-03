5ede4ba8e50437ef204e28917fa884446869291c
Roms1383 <romain.kelifa@gmail.com>
2022-10-04 00:36:49 +0700

    :coffin: remove unused methods from api in benches example

--- frb_example/benches/rust/src/api.rs
+++ frb_example/benches/rust/src/api.rs
@@ -8,12 +8,6 @@ lazy_static::lazy_static! {
   pub static ref FLUTTER_RUST_BRIDGE_HANDLER: BenchHandler = BenchHandler::new(BenchExecutor::new(*FLUTTER_RUST_BRIDGE_ERROR_HANDLER), *FLUTTER_RUST_BRIDGE_ERROR_HANDLER);
 }
 
-#[derive(Debug, Clone)]
-pub struct FeatureUuid {
-    pub one: uuid::Uuid,
-    pub many: Vec<uuid::Uuid>,
-}
-
 #[allow(dead_code)]
 #[derive(Debug, Clone)]
 pub enum Unit {
@@ -40,10 +34,6 @@ pub fn rust_metrics() -> anyhow::Result<Vec<Metric>> {
         .collect::<Vec<_>>())
 }
 
-pub fn handle_uuid(id: uuid::Uuid) -> anyhow::Result<uuid::Uuid> {
-    Ok(id)
-}
-
 pub fn handle_uuids(ids: Vec<uuid::Uuid>) -> anyhow::Result<Vec<uuid::Uuid>> {
     Ok(ids)
 }
@@ -55,14 +45,6 @@ pub fn handle_uuids_convert_to_strings(ids: Vec<uuid::Uuid>) -> anyhow::Result<V
         .collect())
 }
 
-pub fn handle_nested_uuids(ids: FeatureUuid) -> anyhow::Result<FeatureUuid> {
-    Ok(ids)
-}
-
 pub fn handle_strings(strings: Vec<String>) -> anyhow::Result<Vec<String>> {
     Ok(strings)
-}
-
-pub fn send_i64(value: i64) -> i64 {
-    value
 }
