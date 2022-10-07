6f1c695f97feca89a7c9ba3f4c97c686e4de829d
Roms1383 <romain.kelifa@gmail.com>
2022-10-07 09:48:17 +0700

    :bug: fix incorrect env retrieval method
    turns out it's even more inconsistent based on the way the env var was defined ^^

--- frb_dart/lib/src/env/io.dart
+++ frb_dart/lib/src/env/io.dart
@@ -2,13 +2,34 @@
 
 import 'dart:io';
 
-bool readEnv(String envVarName, {bool defaultValue = false}) =>
-    (Platform.environment[envVarName] ?? defaultValue.toString()) == 'true';
+const truthyValues = ['true', '1', 'yes'];
 
-final isWeb = readEnv('dart.library.html');
+/// ✅ correctly read e.g. export JSON=true && ...
+/// ❌ fail at reading e.g. dart --define=SILICON=true ...
+/// ❌ fail at reading e.g. dart -DSILICON=true ...
+bool? readEnvFromPlatform(String envVarName) =>
+(Platform.environment[envVarName] != null && Platform.environment[envVarName]!.isNotEmpty) ? 
+    truthyValues.contains(Platform.environment[envVarName]!.toLowerCase()) : null;
+
+/// ✅ correctly read e.g. dart --define=SILICON=true ...
+/// ✅ correctly read e.g. dart -DSILICON=true ...
+/// ❌ fail at reading e.g. export JSON=true && ...
+bool? readEnvFromPrimitive(String envVarName) =>
+    bool.hasEnvironment(envVarName) ? bool.fromEnvironment(envVarName) : null;
+
+bool readBoolEnv(String envVarName, {bool defaultValue = false}) =>
+    readEnvFromPlatform(envVarName) ??
+    readEnvFromPrimitive(envVarName) ??
+    defaultValue;
+
+final isWeb = readBoolEnv('dart.library.html');
 final int? portEnv = Platform.environment['PORT'] != null
     ? int.parse(Platform.environment['PORT']!)
     : null;
-final isSilicon = readEnv('SILICON');
-final useJSON = readEnv('JSON');
+final isSilicon = readBoolEnv('SILICON');
+final useJSON = readBoolEnv('JSON');
 final String? dylibPath = Platform.environment['DYLIB_PATH'];
+
+bool? readPlatform(String envVarName) =>
+    Platform.environment[envVarName] == 'true';
+bool? readViaType(String envVarName) => bool.fromEnvironment(envVarName);
