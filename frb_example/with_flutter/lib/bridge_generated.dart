6e62f3561ee8eb2644a85b14a010e560e7f007ab
rogurotus <rogurotus@gmail.com>
2022-10-18 18:24:07 +0300

    fix

--- frb_example/with_flutter/lib/bridge_generated.dart
+++ frb_example/with_flutter/lib/bridge_generated.dart
@@ -6,7 +6,6 @@ import "bridge_definitions.dart";
 import 'dart:convert';
 import 'dart:async';
 import 'package:flutter_rust_bridge/flutter_rust_bridge.dart';
-import 'package:uuid/uuid.dart';
 import 'bridge_generated.io.dart' if (dart.library.html) 'bridge_generated.web.dart';
 import 'package:meta/meta.dart';
 
