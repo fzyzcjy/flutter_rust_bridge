ad5c870c09c2ac8fad4ae5c409260fc73e516268
Roms1383 <romain.kelifa@gmail.com>
2022-10-27 09:01:29 +0700

    :recycle: rename env var for dylib open
    since now many users are already accustomed to the term SILICON (see other PRs), rename USE_OPEN_DYLIB

--- frb_dart/lib/src/env/web.dart
+++ frb_dart/lib/src/env/web.dart
@@ -3,4 +3,4 @@
 const isWeb = bool.fromEnvironment('dart.library.html');
 final int? portEnv =
     const int.fromEnvironment('PORT') != 0 ? int.fromEnvironment('PORT') : null;
-const useOpenDylib = bool.fromEnvironment('USE_OPEN_DYLIB');
+const useOpenDylib = bool.fromEnvironment('SILICON');
