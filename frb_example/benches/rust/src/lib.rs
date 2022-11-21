5c1d079f6408d5db29339b71d6323d9800e5ac0b
Roms1383 <romain.kelifa@gmail.com>
2022-11-21 15:24:08 +0700

    :coffin: remove unused method

--- frb_example/benches/rust/src/lib.rs
+++ frb_example/benches/rust/src/lib.rs
@@ -1,12 +1,2 @@
 mod api;
 mod bridge_generated;
-
-impl api::Unit {
-    pub fn acronym(&self) -> &str {
-        match self {
-            api::Unit::Milliseconds => "ms",
-            api::Unit::Microseconds => "μs",
-            api::Unit::Nanoseconds => "ns",
-        }
-    }
-}
