fe56cac8cf908406e528a1ad25fd58f72d8739a4
temeddix <temeddix@gmail.com>
2023-02-05 19:51:33 +0900

    Add features for max and single number of threads

--- frb_rust/src/thread.rs
+++ frb_rust/src/thread.rs
@@ -1,4 +1,32 @@
-pub const WORKERS_COUNT: usize = 4;
+#[cfg(not(wasm))]
+fn get_thread_count() -> usize {
+    #[allow(unused_mut, unused_assignments)]
+    let mut thread_count: usize = 4;
+    #[cfg(feature = "thread-count-single")]
+    {
+        worker_count = 1;
+    }
+    #[cfg(feature = "thread-count-max")]
+    {
+        thread_count = std::thread::available_parallelism().unwrap().get();
+    }
+    thread_count
+}
+
+#[cfg(wasm)]
+fn get_worker_count() -> usize {
+    #[allow(unused_mut, unused_assignments)]
+    let mut worker_count: usize = 4;
+    #[cfg(feature = "worker-count-single")]
+    {
+        worker_count = 1;
+    }
+    #[cfg(feature = "worker-count-max")]
+    {
+        worker_count = 16;
+    }
+    worker_count
+}
 
 #[cfg(not(wasm))]
 mod io {
@@ -10,7 +38,7 @@ mod io {
     lazy_static! {
         pub static ref THREAD_POOL: Mutex<ThreadPool> = Mutex::new(ThreadPool::with_name(
             "frb_workerpool".into(),
-            WORKERS_COUNT
+            get_thread_count()
         ));
     }
 }
@@ -25,7 +53,8 @@ mod web {
     use super::*;
 
     thread_local! {
-        pub static WORKER_POOL: Option<WorkerPool> = WorkerPool::new(WORKERS_COUNT, script_path().unwrap())
+        pub static WORKER_POOL: Option<WorkerPool> = WorkerPool::new(
+            get_worker_count(), script_path().unwrap())
                 .map_err(|err| crate::console_error!("Failed to spawn worker: {:?}", err)).ok()
     }
 }
