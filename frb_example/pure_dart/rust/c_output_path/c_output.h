a85b4e1b26ccb51d3b193ff19185cb2487c2abcb
dbsx <602721790@qq.com>
2023-02-13 21:37:27 +0800

    change back to "static"

--- frb_example/pure_dart/rust/c_output_path/c_output.h
+++ frb_example/pure_dart/rust/c_output_path/c_output.h
@@ -900,7 +900,7 @@ union SpeedKind *inflate_Speed_GPS(void);
 
 void free_WireSyncReturn(WireSyncReturn ptr);
 
-inline int64_t dummy_method_to_enforce_bundling(void) {
+static int64_t dummy_method_to_enforce_bundling(void) {
     int64_t dummy_var = 0;
     dummy_var ^= ((int64_t) (void*) wire_simple_adder);
     dummy_var ^= ((int64_t) (void*) wire_simple_adder_sync);
