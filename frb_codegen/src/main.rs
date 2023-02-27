578f4022db4fc3af4188adb99764ebcdca363a7d
老董 <17372655+dbsxdbsx@users.noreply.github.com>
2023-02-27 10:55:17 +0800

    Update frb_codegen/src/main.rs
    Co-authored-by: fzyzcjy <5236035+fzyzcjy@users.noreply.github.com>

--- frb_codegen/src/main.rs
+++ frb_codegen/src/main.rs
@@ -16,7 +16,7 @@ fn main() -> anyhow::Result<()> {
 
     // generation of rust api for ffi
     let all_symbols = get_symbols_if_no_duplicates(&configs)?;
-    for (config_index, _) in configs.iter().enumerate() {
+    for config_index in 0..configs.len() {
         if let Err(err) = frb_codegen_multi(&configs, config_index, &all_symbols) {
             error!("fatal: {}", err);
             exit(1);
