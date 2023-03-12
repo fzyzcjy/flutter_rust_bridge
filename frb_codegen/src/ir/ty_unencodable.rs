918d414723df1232edb8162905c1f92a63ad65b7
Brent Lewis <coder0xff@gmail.com>
2023-03-12 07:52:34 -0700

    Cleanup src/ir/ty_unencodable.rs

--- frb_codegen/src/ir/ty_unencodable.rs
+++ frb_codegen/src/ir/ty_unencodable.rs
@@ -1,11 +1,6 @@
 use crate::{ir::*, target::Target};
 use quote::ToTokens;
 
-// use convert_case::{Case, Casing};
-// use lazy_static::lazy_static;
-// use quote::ToTokens;
-// use regex::Regex;
-
 #[derive(Debug, Clone, Hash, Eq, PartialEq)]
 pub enum ArgsRefs<'a> {
     Generic(&'a [IrType]),
@@ -45,33 +40,17 @@ impl ArgsUnpackable for Vec<(String, Option<Args>)> {
     }
 }
 
-// fn char_not_alphanumeric(c: char) -> bool {
-//     !c.is_alphanumeric()
-// }
-
-// fn rust_type_to_dart_type(rust: &str) -> String {
-//     lazy_static! {
-//         static ref OPAQUE_FILTER: Regex =
-//             Regex::new(r"(\bdyn|'static|\bDartSafe|\+ (Send|Sync|UnwindSafe|RefUnwindSafe))\b")
-//                 .unwrap();
-//     }
-//     OPAQUE_FILTER
-//         .replace_all(rust, "")
-//         .replace(char_not_alphanumeric, "_")
-//         .to_case(Case::Pascal)
-// }
-
 impl IrTypeTrait for IrTypeUnencodable {
     fn visit_children_types<F: FnMut(&IrType) -> bool>(&self, _f: &mut F, _ir_file: &IrFile) {
         // Do nothing.
     }
 
     fn safe_ident(&self) -> String {
-        "UNENCODABLE_SAFE_IDENT".to_string()
+        todo!("generate code for type \"{}\"", self.underlying_type.to_token_stream());
     }
 
     fn dart_api_type(&self) -> String {
-        "UNENCODABLE_DART_API_TYPE".to_string()
+        todo!("generate code for type \"{}\"", self.underlying_type.to_token_stream());
     }
 
     fn dart_wire_type(&self, target: crate::target::Target) -> String {
