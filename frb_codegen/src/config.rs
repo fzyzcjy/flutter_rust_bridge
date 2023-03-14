53fcec692af1ec07a5d7be2582bc4ad495608b57
Viet Dinh <54ckb0y789@gmail.com>
2023-03-14 10:28:42 -0400

    fix: compile on Rust 1.64

--- frb_codegen/src/config.rs
+++ frb_codegen/src/config.rs
@@ -161,30 +161,54 @@ fn bail(err: clap::error::ErrorKind, message: Cow<str>) -> ! {
 impl RawOpts {
     /// Parses options from arguments, or from a config file if no arguments are given.
     /// Terminates the program if argument validation fails.
+    #[cfg(feature = "serde")]
     pub fn try_parse_args_or_yaml() -> anyhow::Result<Self> {
         const CONFIG_LOCATIONS: [&str; 3] = [
             ".flutter_rust_bridge.yml",
             ".flutter_rust_bridge.yaml",
             ".flutter_rust_bridge.json",
         ];
+        const PUBSPEC_LOCATIONS: [&str; 2] = ["pubspec.yaml", "pubspec.yml"];
+
         let has_args = std::env::args_os().len() > 1;
-        Ok(match Self::try_parse() {
-            Ok(opts) => opts,
+        match Self::try_parse() {
+            Ok(opts) => Ok(opts),
             Err(err) if has_args => err.exit(),
             // Try to parse a command file, if exists
-            Err(err) => 'from_file: {
+            Err(err) => {
                 for location in CONFIG_LOCATIONS {
-                    let Ok(file) = fs::File::open(location) else {continue};
-                    log::info!("Found config file {location}");
-                    break 'from_file serde_yaml::from_reader(file)
-                        .map_err(|err| anyhow::anyhow!("Could not parse {location}: {err}"))?;
+                    if let Ok(file) = fs::File::open(location) {
+                        eprintln!("Found config file {location}");
+                        return serde_yaml::from_reader(file)
+                            .with_context(|| format!("Could not parse {location}"));
+                    }
                 }
-                _ = err.print();
 
-                eprintln!("Hint: To call flutter_rust_bridge_codegen with no arguments, fill in .flutter_rust_bridge.yml with your config.");
+                let mut hint = "fill in .flutter_rust_bridge.yml with your config.".to_owned();
+                for location in PUBSPEC_LOCATIONS {
+                    if let Ok(pubspec) = fs::File::open(location) {
+                        #[derive(serde::Deserialize)]
+                        struct Needle {
+                            #[serde(rename = "flutter_rust_bridge")]
+                            data: Option<RawOpts>,
+                        }
+                        match serde_yaml::from_reader(pubspec) {
+                            Ok(Needle { data: Some(data) }) => return Ok(data),
+                            Ok(_) => {
+                                hint = format!("create an entry called 'flutter_rust_bridge' in {location} with your config.");
+                            }
+                            Err(err) => {
+                                eprintln!("The 'flutter_rust_bridge' entry in {location} could not be parsed:\n  {err}");
+                            }
+                        }
+                    }
+                }
+
+                _ = err.print();
+                eprintln!("Hint: To call flutter_rust_bridge_codegen with no arguments, {hint}");
                 std::process::exit(1)
             }
-        })
+        }
     }
 }
 
