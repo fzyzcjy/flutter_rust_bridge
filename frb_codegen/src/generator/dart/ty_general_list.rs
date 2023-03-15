7b50988f8d5e862ff3d9931c206231b67093ebd1
erikas-taroza <erikastaroza@gmail.com>
2023-03-15 15:07:27 -0700

    Remove new line

--- frb_codegen/src/generator/dart/ty_general_list.rs
+++ frb_codegen/src/generator/dart/ty_general_list.rs
@@ -3,7 +3,6 @@ use crate::ir::*;
 use crate::target::Acc;
 use crate::type_dart_generator_struct;
 
-
 type_dart_generator_struct!(TypeGeneralListGenerator, IrTypeGeneralList);
 
 impl TypeDartGeneratorTrait for TypeGeneralListGenerator<'_> {
