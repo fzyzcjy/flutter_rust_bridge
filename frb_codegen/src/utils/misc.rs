2481db8506cb324c3e6fd1e35ba57bada170845e
dbsxdbsx <602721790@qq.com>
2023-05-11 01:10:26 +0800

    fix a doc test error

--- frb_codegen/src/utils/misc.rs
+++ frb_codegen/src/utils/misc.rs
@@ -228,7 +228,7 @@ pub trait ExtraTraitForVec<T: Clone + Eq + std::hash::Hash> {
     /// # Examples
     ///
     /// ```
-    /// use lib_flutter_rust_bridge_codegen::ExtraTraitForVec;
+    /// use crate::utils::misc::ExtraTraitForVec;
     ///
     /// let vec = vec![1, 2, 3, 2, 4, 5, 4, 6];
     /// let (uniques, duplicates) = vec.find_uniques_and_duplicates(true, true);
