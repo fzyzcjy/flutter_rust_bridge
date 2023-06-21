3579cc70f0660e993ed012ec3060a4dec3402493
dbsxdbsx <602721790@qq.com>
2023-06-21 17:14:31 +0800

    add fake module tests

--- frb_example/pure_dart_multi/rust/src/lib.rs
+++ frb_example/pure_dart_multi/rust/src/lib.rs
@@ -4,6 +4,7 @@ mod generated_api_block_1; /* AUTO INJECTED BY flutter_rust_bridge. This line ma
 mod generated_api_block_2; /* AUTO INJECTED BY flutter_rust_bridge. This line may not be accurate, and you can change it according to your needs. */
 mod generated_api_block_3; /* AUTO INJECTED BY flutter_rust_bridge. This line may not be accurate, and you can change it according to your needs. */
 mod shared_type_module;
+mod fake_module;
 
 mod api_block_1;
 mod api_block_2;
