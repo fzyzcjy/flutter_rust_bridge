e3352d93edc38c12f345c84f47e9f0432efd5cab
Marc Gutenberger <gutenfries@gmail.com>
2023-07-31 12:01:23 -0500

    Discard changes to frb_example/pure_dart_multi/rust/src/generated_api_1.io.rs

--- frb_example/pure_dart_multi/rust/src/generated_api_1.io.rs
+++ frb_example/pure_dart_multi/rust/src/generated_api_1.io.rs
@@ -3,7 +3,7 @@ use super::*;
 
 #[no_mangle]
 pub extern "C" fn wire_simple_adder_1(port_: i64, a: i32, b: i32) {
-	wire_simple_adder_1_impl(port_, a, b)
+    wire_simple_adder_1_impl(port_, a, b)
 }
 
 // Section: allocate functions
@@ -17,20 +17,20 @@ pub extern "C" fn wire_simple_adder_1(port_: i64, a: i32, b: i32) {
 // Section: impl NewWithNullPtr
 
 pub trait NewWithNullPtr {
-	fn new_with_null_ptr() -> Self;
+    fn new_with_null_ptr() -> Self;
 }
 
 impl<T> NewWithNullPtr for *mut T {
-	fn new_with_null_ptr() -> Self {
-		std::ptr::null_mut()
-	}
+    fn new_with_null_ptr() -> Self {
+        std::ptr::null_mut()
+    }
 }
 
 // Section: sync execution mode utility
 
 #[no_mangle]
 pub extern "C" fn free_WireSyncReturn(ptr: support::WireSyncReturn) {
-	unsafe {
-		let _ = support::box_from_leak_ptr(ptr);
-	};
+    unsafe {
+        let _ = support::box_from_leak_ptr(ptr);
+    };
 }
