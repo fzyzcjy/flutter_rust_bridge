173b325d1218d565f47e9d00dfaf977ebeadc942
Marc Gutenberger <gutenfries@gmail.com>
2023-07-31 11:41:17 -0500

    (chore): manually bump `proc-macro2` -> `1.0.66`

--- frb_example/pure_dart_multi/rust/src/generated_api_1.web.rs
+++ frb_example/pure_dart_multi/rust/src/generated_api_1.web.rs
@@ -3,7 +3,7 @@ use super::*;
 
 #[wasm_bindgen]
 pub fn wire_simple_adder_1(port_: MessagePort, a: i32, b: i32) {
-    wire_simple_adder_1_impl(port_, a, b)
+	wire_simple_adder_1_impl(port_, a, b)
 }
 
 // Section: allocate functions
@@ -15,7 +15,7 @@ pub fn wire_simple_adder_1(port_: MessagePort, a: i32, b: i32) {
 // Section: impl Wire2Api for JsValue
 
 impl Wire2Api<i32> for JsValue {
-    fn wire2api(self) -> i32 {
-        self.unchecked_into_f64() as _
-    }
+	fn wire2api(self) -> i32 {
+		self.unchecked_into_f64() as _
+	}
 }
