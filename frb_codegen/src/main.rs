140cdc8b93d0bc7136d5f8dc2a31d7208c7f4603
dbsxdbsx <djw0214@gmail.com>
2023-08-02 17:32:48 +0800

    fix  "program not found" issue on windows

--- frb_codegen/src/main.rs
+++ frb_codegen/src/main.rs
@@ -61,9 +61,9 @@ mod tests {
         static ref LOGGER: () = init_logger(".", true).unwrap();
     }
 
-    #[cfg(target_os = "windows")]
+    #[cfg(target_env = "msvc")] // usually, it is for Windows
     const DART: &str = "dart.bat";
-    #[cfg(not(target_os = "windows"))]
+    #[cfg(not(target_env = "msvc"))]
     const DART: &str = "dart";
 
     // VS Code runs in frb_codegen with "Run test" and flutter_rust_bridge with "Debug test" >_>
