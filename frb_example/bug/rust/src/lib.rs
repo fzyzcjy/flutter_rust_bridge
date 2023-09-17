8b2402dc228cd2d8edc969f2f7d67fc43c5ba528
fzyzcjy <ch271828n@outlook.com>
2023-09-17 14:51:39 +0800

    add panic

--- frb_example/bug/rust/src/lib.rs
+++ frb_example/bug/rust/src/lib.rs
@@ -1,4 +1,8 @@
 #[no_mangle]
 pub extern "C" fn hello_rust_function() {
-    println!("hello_rust_function is called!");
+    println!("hello_rust_function start");
+    let result_of_catch_unwind = std::panic::catch_unwind(move || {
+        println!("call panic");
+        panic!("hi this is panic");
+    });
 }
