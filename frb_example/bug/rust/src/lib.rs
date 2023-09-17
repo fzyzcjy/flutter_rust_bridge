5be1ffeb97a93cb273fadf498de4602a6a83cc2e
fzyzcjy <ch271828n@outlook.com>
2023-09-17 15:37:24 +0800

    temp

--- frb_example/bug/rust/src/lib.rs
+++ frb_example/bug/rust/src/lib.rs
@@ -1,9 +1,10 @@
 #[no_mangle]
 pub extern "C" fn hello_rust_function() {
-    println!("hello_rust_function start");
-    let result = std::panic::catch_unwind(move || {
-        println!("call panic");
-        panic!("hi this is panic");
-    });
-    println!("hello_rust_function end {:?}", result);
+    println!("hi backtrace {:?}", std::backtrace::Backtrace::force_capture());
+    // println!("hello_rust_function start");
+    // let result = std::panic::catch_unwind(move || {
+    //     println!("call panic");
+    //     panic!("hi this is panic");
+    // });
+    // println!("hello_rust_function end {:?}", result);
 }
