0ef3356616df89c055ed264345cbaff103908e65
Viet Dinh <54ckb0y789@gmail.com>
2023-10-08 23:41:11 -0400

    doc: downgrade corrosion to v0.4.4
    The default template does not yet make use of any advanced features
    that require later versions of CMake, so it is more sensible to choose
    a version that is more compatible with available CMake versions.

--- frb_example/with_flutter/windows/rust.cmake
+++ frb_example/with_flutter/windows/rust.cmake
@@ -9,7 +9,7 @@ include(FetchContent)
 FetchContent_Declare(
     Corrosion
     GIT_REPOSITORY https://github.com/AndrewGaspar/corrosion.git
-    GIT_TAG origin/master # Optionally specify a version tag or branch here
+    GIT_TAG v0.4.4 # Optionally specify a version tag or branch here
 )
 
 FetchContent_MakeAvailable(Corrosion)
