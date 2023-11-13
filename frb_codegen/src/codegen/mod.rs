82b86b0992f1146177831a554a01201d1cd16ce6
fzyzcjy <ch271828n@outlook.com>
2023-11-13 21:21:19 +0800

    try clap

--- frb_codegen/src/codegen/mod.rs
+++ frb_codegen/src/codegen/mod.rs
@@ -1,11 +1,11 @@
 //! Code generator for `flutter_rust_bridge`
 
-/// Parsed configuration for code generation
-pub struct Config {
-    // TODO
-}
-
 /// Execute code generation
 pub fn generate(config: &Config) -> anyhow::Result<()> {
     todo!()
+}
+
+/// Parsed configuration for code generation
+pub struct Config {
+    // TODO
 }
--- frb_codegen/src/main.rs
+++ frb_codegen/src/main.rs
@@ -1,3 +1,35 @@
+use clap::{Parser, Subcommand};
+
 fn main() -> anyhow::Result<()> {
+    let cli = Cli::parse();
     todo!()
+}
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// hello world
+    Generate {
+        /// hello world
+        #[arg(short, long)]
+        list: bool,
+    },
+    /// hello world
+    Create {
+        /// hello world
+        #[arg(short, long)]
+        list: bool,
+    },
+    /// hello world
+    Integrate {
+        /// hello world
+        #[arg(short, long)]
+        list: bool,
+    },
 }
