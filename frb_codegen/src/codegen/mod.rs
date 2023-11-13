429ce1d391c525b746a9d6db74786ee6ea9f6627
fzyzcjy <ch271828n@outlook.com>
2023-11-13 21:30:44 +0800

    more arg

--- frb_codegen/src/codegen/mod.rs
+++ frb_codegen/src/codegen/mod.rs
@@ -1,6 +1,6 @@
 //! Code generator for `flutter_rust_bridge`
 
-/// Execute main code generator
+/// Execute the main code generator
 pub fn generate(config: &Config) -> anyhow::Result<()> {
     todo!()
 }
--- frb_codegen/src/main.rs
+++ frb_codegen/src/main.rs
@@ -1,10 +1,11 @@
-use clap::{Parser, Subcommand};
+use clap::{Args, Parser, Subcommand};
 
 fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
     todo!()
 }
 
+// The name `Cli`, `Commands` come from https://docs.rs/clap/latest/clap/_derive/_tutorial/chapter_0/index.html
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
@@ -14,24 +15,30 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
-    /// Execute main code generator
-    Generate {
-        /// hello world
-        #[arg(short, long)]
-        list: bool,
-    },
+    /// Execute the main code generator
+    Generate(GenerateCommandArgs),
 
     /// Create a new Flutter + Rust project
-    Create {
-        /// hello world
-        #[arg(short, long)]
-        list: bool,
-    },
+    Create(CreateCommandArgs),
 
     /// Integrate Rust into existing Flutter project
-    Integrate {
-        /// hello world
-        #[arg(short, long)]
-        list: bool,
-    },
+    Integrate(IntegrateCommandArgs),
+}
+
+#[derive(Debug, Args)]
+struct GenerateCommandArgs {
+    /// hello world
+    #[arg(short, long)]
+    list: bool,
+}
+
+#[derive(Debug, Args)]
+struct CreateCommandArgs {
+    /// Name of the new project
+    name: String,
+}
+
+#[derive(Debug, Args)]
+struct IntegrateCommandArgs {
+    // nothing yet
 }
