4ca46c69d15534f788688acb2720b463cf2c9987
fzyzcjy <ch271828n@outlook.com>
2023-11-13 21:57:54 +0800

    command args

--- frb_codegen/src/codegen/mod.rs
+++ frb_codegen/src/codegen/mod.rs
@@ -1,5 +1,8 @@
 //! Code generator for `flutter_rust_bridge`
 
+use clap::ValueEnum;
+use serde::Deserialize;
+
 /// Execute the main code generator
 pub fn generate(config: &Config) -> anyhow::Result<()> {
     todo!("generate")
@@ -9,3 +12,11 @@ pub fn generate(config: &Config) -> anyhow::Result<()> {
 pub struct Config {
     // TODO
 }
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Deserialize, ValueEnum, enum_iterator::Sequence)]
+#[serde(rename_all = "snake_case")]
+pub enum ConfigDump {
+    Config,
+    Ir,
+}
+
