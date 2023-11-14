922bdb580eb4c866097ce6d015f64bd802c9ab86
fzyzcjy <ch271828n@outlook.com>
2023-11-14 11:45:45 +0800

    refactor: use Path instead of String

--- frb_codegen/src/library/codegen/internal_config.rs
+++ frb_codegen/src/library/codegen/internal_config.rs
@@ -1,18 +1,20 @@
+use std::path::Path;
+
 #[derive(Debug, Clone, serde::Serialize)]
 pub(crate) struct InternalConfig {
-    pub rust_input_path: String,
-    pub dart_output_path: String,
-    pub dart_decl_output_path: Option<String>,
-    pub c_output_path: Vec<String>,
+    pub rust_input_path: Path,
+    pub dart_output_path: Path,
+    pub dart_decl_output_path: Option<Path>,
+    pub c_output_path: Vec<Path>,
     pub rust_crate_dir: String,
-    pub rust_output_path: String,
+    pub rust_output_path: Path,
     pub class_name: String,
     pub dart_format_line_length: u32,
     pub dart_enums_style: bool,
     pub skip_add_mod_to_lib: bool,
-    pub llvm_path: Vec<String>,
+    pub llvm_path: Vec<Path>,
     pub llvm_compiler_opts: String,
-    pub manifest_path: String,
+    pub manifest_path: Path,
     pub dart_root: Option<String>,
     pub build_runner: bool,
     pub block_index: BlockIndex,
