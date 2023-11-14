633ef971f7cfc5bd9185f643700282fd821e7814
fzyzcjy <ch271828n@outlook.com>
2023-11-14 12:04:14 +0800

    refactor: decompose InternalConfig

--- frb_codegen/src/library/codegen/internal_config.rs
+++ frb_codegen/src/library/codegen/internal_config.rs
@@ -2,27 +2,52 @@ use std::path::PathBuf;
 
 #[derive(Debug, Clone, serde::Serialize)]
 pub(crate) struct InternalConfig {
+    pub parser: ParserInternalConfig,
+    pub generator: GeneratorInternalConfig,
+    pub polisher: PolisherInternalConfig,
+    pub keep_going: bool,
+}
+
+pub(crate) struct ParserInternalConfig {
     pub rust_input_path: Vec<PathBuf>,
+    pub manifest_path: PathBuf,
+}
+
+pub(crate) struct GeneratorInternalConfig {
+    pub dart: GeneratorDartInternalConfig,
+    pub rust: GeneratorRustInternalConfig,
+    pub c: GeneratorCInternalConfig,
+}
+
+pub(crate) struct GeneratorDartInternalConfig {
     pub dart_output_path: PathBuf,
     pub dart_decl_output_path: Option<PathBuf>,
-    pub c_output_path: Vec<PathBuf>,
-    pub extra_c_output_path: Vec<PathBuf>,
+    pub dart_enums_style: bool,
+    pub class_name: String,
+    pub dart_root: Option<String>,
+    pub use_bridge_in_method: bool,
+    pub wasm_enabled: bool,
+    pub dart3: bool,
+}
+
+pub(crate) struct GeneratorRustInternalConfig {
     pub rust_crate_dir: String,
     pub rust_output_path: PathBuf,
-    pub class_name: String,
-    pub dart_format_line_length: u32,
-    pub dart_enums_style: bool,
-    pub add_mod_to_lib: bool,
+    pub inline_rust: bool,
+}
+
+pub(crate) struct GeneratorCInternalConfig {
+    pub c_output_path: Vec<PathBuf>,
     pub llvm_path: Vec<PathBuf>,
     pub llvm_compiler_opts: String,
-    pub dart_root: Option<String>,
-    pub build_runner: bool,
-    pub use_bridge_in_method: bool,
     pub extra_headers: String,
-    pub wasm_enabled: bool,
-    pub inline_rust: bool,
+}
+
+// TODO naming
+pub(crate) struct PolisherInternalConfig {
+    pub extra_c_output_path: Vec<PathBuf>,
+    pub dart_format_line_length: u32,
+    pub add_mod_to_lib: bool,
+    pub build_runner: bool,
     pub deps_check: bool,
-    pub dart3: bool,
-    pub keep_going: bool,
-    pub manifest_path: PathBuf,
 }
