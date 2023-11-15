1564a0eed998c07d65c452a96a1ec2b65fe4160d
fzyzcjy <ch271828n@outlook.com>
2023-11-15 14:30:07 +0800

    refactor: more

--- frb_codegen/src/library/codegen/parser/source_graph/crates.rs
+++ frb_codegen/src/library/codegen/parser/source_graph/crates.rs
@@ -1,7 +1,7 @@
 use std::fs;
 use std::path::{Path, PathBuf};
 use anyhow::Context;
-use cargo_metadata::MetadataCommand;
+use cargo_metadata::{Metadata, MetadataCommand, Package};
 use crate::codegen::parser::ParserResult;
 use crate::codegen::parser::source_graph::modules::{Module, ModuleSource, Visibility};
 
@@ -16,36 +16,24 @@ pub struct Crate {
 
 impl Crate {
     pub fn parse(manifest_path: &Path) -> ParserResult<Self> {
-        let metadata = {
-            let mut cmd = MetadataCommand::new();
-            cmd.manifest_path(manifest_path);
-            cmd.exec()?
-        };
+        let metadata = execute_cargo_metadata(manifest_path)?;
 
         // TODO is this duplicated with logic before?
         let root_package = metadata.root_package().context("no root package")?;
-        let root_src_file = (|| {
-            for attempt_relative_path in ["src/lib.rs", "src/main.rs"] {
-                let file = root_package.manifest_path.parent().unwrap().join(attempt_relative_path);
-                if file.exists() {
-                    return Ok(fs::canonicalize(file).unwrap());
-                }
-            }
-            Err(super::Error::NoEntryPoint)
-        })()?;
+        let root_src_file = get_root_src_file(root_package)?;
 
-        let source_rust_content = fs::read_to_string(&root_src_file).unwrap();
-        let file_ast = syn::parse_file(&source_rust_content).unwrap();
+        let root_src_content = fs::read_to_string(&root_src_file)?;
+        let root_src_ast = syn::parse_file(&root_src_content)?;
 
         let mut result = Crate {
             name: root_package.name.clone(),
-            manifest_path: fs::canonicalize(manifest_path).unwrap(),
+            manifest_path: fs::canonicalize(manifest_path)?,
             root_src_file: root_src_file.clone(),
             root_module: Module {
                 visibility: Visibility::Public,
                 file_path: root_src_file,
                 module_path: vec!["crate".to_string()],
-                source: ModuleSource::File(file_ast),
+                source: ModuleSource::File(root_src_ast),
                 scope: None,
             },
         };
@@ -54,4 +42,20 @@ impl Crate {
     }
 
     pub fn root_module(&self) -> &Module { &self.root_module }
+}
+
+fn execute_cargo_metadata(manifest_path: &Path) -> anyhow::Result<Metadata> {
+    let mut cmd = MetadataCommand::new();
+    cmd.manifest_path(manifest_path);
+    Ok(cmd.exec()?)
+}
+
+fn get_root_src_file(root_package: &Package) -> ParserResult<PathBuf> {
+    for attempt_relative_path in ["src/lib.rs", "src/main.rs"] {
+        let file = root_package.manifest_path.parent().unwrap().join(attempt_relative_path);
+        if file.exists() {
+            return Ok(fs::canonicalize(file).unwrap());
+        }
+    }
+    Err(super::Error::NoEntryPoint)
 }
