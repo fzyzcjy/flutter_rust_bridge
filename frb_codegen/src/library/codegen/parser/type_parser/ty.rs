8f8334ef25baa0925abc82fd32930c0521262280
fzyzcjy <ch271828n@outlook.com>
2023-11-15 22:35:32 +0800

    refactor: parse_type_tuple

--- frb_codegen/src/library/codegen/parser/type_parser/ty.rs
+++ frb_codegen/src/library/codegen/parser/type_parser/ty.rs
@@ -1,11 +1,16 @@
+use crate::codegen::ir::field::IrField;
+use crate::codegen::ir::ident::IrIdent;
 use crate::codegen::ir::ty::delegate::{IrTypeDelegate, IrTypeDelegateArray};
 use crate::codegen::ir::ty::primitive::IrTypePrimitive;
+use crate::codegen::ir::ty::record::IrTypeRecord;
+use crate::codegen::ir::ty::structure::{IrStruct, IrStructIdent, IrTypeStructRef};
 use crate::codegen::ir::ty::unencodable::IrTypeUnencodable;
 use crate::codegen::ir::ty::IrType;
 use crate::codegen::ir::ty::IrType::{Delegate, Primitive};
 use crate::codegen::parser::type_parser::TypeParser;
 use quote::ToTokens;
-use syn::{Expr, Type};
+use syn::punctuated::Punctuated;
+use syn::{Expr, Token, Type};
 
 impl<'a> TypeParser<'a> {
     pub(crate) fn parse_type(&mut self, ty: &Type) -> IrType {
@@ -48,5 +53,51 @@ impl<'a> TypeParser<'a> {
                 general: Box::new(others),
             })),
         }
+    }
+
+    fn parse_type_tuple(&mut self, elems: Punctuated<Type, Token![,]>) -> IrType {
+        let values = elems
+            .iter()
+            .map(|elem| self.parse_type(elem))
+            .collect::<Vec<_>>();
+        let safe_ident = values
+            .iter()
+            .map(IrType::safe_ident)
+            .collect::<Vec<_>>()
+            .join("_");
+        let safe_ident = format!("__record__{safe_ident}");
+        self.struct_pool.insert(
+            safe_ident.clone(),
+            IrStruct {
+                name: safe_ident.clone(),
+                wrapper_name: None,
+                path: None,
+                is_fields_named: true,
+                dart_metadata: vec![],
+                comments: vec![],
+                fields: values
+                    .iter()
+                    .enumerate()
+                    .map(|(idx, ty)| IrField {
+                        ty: ty.clone(),
+                        name: IrIdent::new(format!("field{idx}")),
+                        is_final: true,
+                        comments: vec![],
+                        default: None,
+                        settings: Default::default(),
+                    })
+                    .collect(),
+            },
+        );
+        IrType::Record(IrTypeRecord {
+            inner: IrTypeStructRef {
+                // name: safe_ident,
+                // freezed: false,
+                // empty: false,
+                ident: IrStructIdent(safe_ident),
+                is_exception: false,
+            },
+            values: values.into_boxed_slice(),
+        })
     }
 }
--- frb_codegen/src/old/parser/ty.rs
+++ frb_codegen/src/old/parser/ty.rs
@@ -412,49 +412,4 @@ impl<'a> TypeParser<'a> {
             )),
         }
     }
-
-    fn parse_type_tuple(&mut self, elems: Punctuated<Type, Token![,]>) -> IrType {
-        let values = elems
-            .iter()
-            .map(|elem| self.parse_type(elem))
-            .collect::<Vec<_>>();
-        let safe_ident = values
-            .iter()
-            .map(IrType::safe_ident)
-            .collect::<Vec<_>>()
-            .join("_");
-        let safe_ident = format!("__record__{safe_ident}");
-        self.struct_pool.insert(
-            safe_ident.clone(),
-            IrStruct {
-                name: safe_ident.clone(),
-                wrapper_name: None,
-                path: None,
-                is_fields_named: true,
-                dart_metadata: vec![],
-                comments: vec![],
-                fields: values
-                    .iter()
-                    .enumerate()
-                    .map(|(idx, ty)| IrField {
-                        ty: ty.clone(),
-                        name: IrIdent::new(format!("field{idx}")),
-                        is_final: true,
-                        comments: vec![],
-                        default: None,
-                        settings: Default::default(),
-                    })
-                    .collect(),
-            },
-        );
-        IrType::Record(IrTypeRecord {
-            inner: IrTypeStructRef {
-                name: safe_ident,
-                freezed: false,
-                empty: false,
-                is_exception: false,
-            },
-            values: values.into_boxed_slice(),
-        })
-    }
 }
