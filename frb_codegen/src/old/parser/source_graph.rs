0bc11fa4002354b20f7f41413b336712ee7e9adb
fzyzcjy <ch271828n@outlook.com>
2023-11-15 20:09:35 +0800

    refactor: comment out resolve_imports, since it is unfinished in old version

--- frb_codegen/src/old/parser/source_graph.rs
+++ frb_codegen/src/old/parser/source_graph.rs
@@ -239,200 +239,200 @@ impl Module {
         });
     }
 
-    fn resolve_imports(&mut self) {
-        let imports = &mut self.scope.as_mut().unwrap().imports;
-
-        let items = match self.source.as_ref().unwrap() {
-            ModuleSource::File(file) => &file.items,
-            ModuleSource::ModuleInFile(items) => items,
-        };
-
-        for item in items.iter() {
-            if let syn::Item::Use(item_use) = item {
-                let flattened_imports = flatten_use_tree(&item_use.tree).unwrap();
-
-                for import in flattened_imports {
-                    imports.push(Import {
-                        path: import,
-                        visibility: syn_vis_to_visibility(&item_use.vis),
-                    });
-                }
-            }
-        }
-    }
+    // fn resolve_imports(&mut self) {
+    //     let imports = &mut self.scope.as_mut().unwrap().imports;
+    //
+    //     let items = match self.source.as_ref().unwrap() {
+    //         ModuleSource::File(file) => &file.items,
+    //         ModuleSource::ModuleInFile(items) => items,
+    //     };
+    //
+    //     for item in items.iter() {
+    //         if let syn::Item::Use(item_use) = item {
+    //             let flattened_imports = flatten_use_tree(&item_use.tree).unwrap();
+    //
+    //             for import in flattened_imports {
+    //                 imports.push(Import {
+    //                     path: import,
+    //                     visibility: syn_vis_to_visibility(&item_use.vis),
+    //                 });
+    //             }
+    //         }
+    //     }
+    // }
 }
 
-fn flatten_use_tree_rename_abort_warning(use_tree: &UseTree) {
-    debug!("WARNING: flatten_use_tree() found an import rename (use a::b as c). flatten_use_tree() will now abort.");
-    debug!("WARNING: This happened while parsing {:?}", use_tree);
-    debug!("WARNING: This use statement will be ignored.");
-}
-
-/// Takes a use tree and returns a flat list of use paths (list of string tokens)
-///
-/// Example:
-///     use a::{b::c, d::e};
-/// becomes
-///     [
-///         ["a", "b", "c"],
-///         ["a", "d", "e"]
-///     ]
-///
-/// Warning: As of writing, import renames (import a::b as c) are silently
-/// ignored.
-fn flatten_use_tree(use_tree: &UseTree) -> ParserResult<Vec<Vec<String>>> {
-    // Vec<(path, is_complete)>
-    let mut result = vec![(vec![], false)];
-
-    let mut counter: usize = 0;
-
-    loop {
-        counter += 1;
-
-        if counter > 10000 {
-            panic!("flatten_use_tree: Use statement complexity limit exceeded. This is probably a bug.");
-        }
-
-        // If all paths are complete, break from the loop
-        if result.iter().all(|result_item| result_item.1) {
-            break;
-        }
-
-        let mut items_to_push = Vec::new();
-
-        for path_tuple in &mut result {
-            let path = &mut path_tuple.0;
-            let is_complete = &mut path_tuple.1;
-
-            if *is_complete {
-                continue;
-            }
-
-            let mut tree_cursor = use_tree;
-
-            for path_item in path.iter() {
-                match tree_cursor {
-                    UseTree::Path(use_path) => {
-                        let ident = use_path.ident.to_string();
-                        if *path_item != ident {
-                            panic!("This ident did not match the one we already collected. This is a bug.");
-                        }
-                        tree_cursor = use_path.tree.as_ref();
-                    }
-                    UseTree::Group(use_group) => {
-                        let mut moved_tree_cursor = false;
-
-                        for tree in use_group.items.iter() {
-                            match tree {
-                                UseTree::Path(use_path) => {
-                                    if path_item == &use_path.ident.to_string() {
-                                        tree_cursor = use_path.tree.as_ref();
-                                        moved_tree_cursor = true;
-                                        break;
-                                    }
-                                }
-                                // Since we're not matching UseTree::Group here, a::b::{{c}, {d}} might
-                                // break. But also why would anybody do that
-                                _ => unreachable!(),
-                            }
-                        }
-
-                        if !moved_tree_cursor {
-                            unreachable!();
-                        }
-                    }
-                    _ => unreachable!(),
-                }
-            }
-
-            match tree_cursor {
-                UseTree::Name(use_name) => {
-                    path.push(use_name.ident.to_string());
-                    *is_complete = true;
-                }
-                UseTree::Path(use_path) => {
-                    path.push(use_path.ident.to_string());
-                }
-                UseTree::Glob(_) => {
-                    path.push("*".to_string());
-                    *is_complete = true;
-                }
-                UseTree::Group(use_group) => {
-                    // We'll modify the first one in-place, and make clones for
-                    // all subsequent ones
-                    let mut first: bool = true;
-                    // Capture the path in this state, since we're about to
-                    // modify it
-                    let path_copy = path.clone();
-                    for tree in use_group.items.iter() {
-                        let mut new_path_tuple = if first {
-                            None
-                        } else {
-                            let new_path = path_copy.clone();
-                            items_to_push.push((new_path, false));
-                            Some(items_to_push.iter_mut().last().unwrap())
-                        };
-
-                        match tree {
-                            UseTree::Path(use_path) => {
-                                let ident = use_path.ident.to_string();
-
-                                if first {
-                                    path.push(ident);
-                                } else {
-                                    new_path_tuple.unwrap().0.push(ident);
-                                }
-                            }
-                            UseTree::Name(use_name) => {
-                                let ident = use_name.ident.to_string();
-
-                                if first {
-                                    path.push(ident);
-                                    *is_complete = true;
-                                } else {
-                                    let path_tuple = new_path_tuple.as_mut().unwrap();
-                                    path_tuple.0.push(ident);
-                                    path_tuple.1 = true;
-                                }
-                            }
-                            UseTree::Glob(_) => {
-                                if first {
-                                    path.push("*".to_string());
-                                    *is_complete = true;
-                                } else {
-                                    let path_tuple = new_path_tuple.as_mut().unwrap();
-                                    path_tuple.0.push("*".to_string());
-                                    path_tuple.1 = true;
-                                }
-                            }
-                            UseTree::Group(_) => {
-                                Err(anyhow::anyhow!(
-                                    "Directly-nested use groups ({}) are not supported by flutter_rust_bridge. Use {} instead.",
-                                    "use a::{{b}, c}",
-                                    "a::{b, c}"
-                                ))?;
-                            }
-                            // UseTree::Group(_) => panic!(),
-                            UseTree::Rename(_) => {
-                                flatten_use_tree_rename_abort_warning(use_tree);
-                                return Ok(vec![]);
-                            }
-                        }
-
-                        first = false;
-                    }
-                }
-                UseTree::Rename(_) => {
-                    flatten_use_tree_rename_abort_warning(use_tree);
-                    return Ok(vec![]);
-                }
-            }
-        }
-
-        for item in items_to_push {
-            result.push(item);
-        }
-    }
-
-    Ok(result.into_iter().map(|val| val.0).collect())
-}
+// fn flatten_use_tree_rename_abort_warning(use_tree: &UseTree) {
+//     debug!("WARNING: flatten_use_tree() found an import rename (use a::b as c). flatten_use_tree() will now abort.");
+//     debug!("WARNING: This happened while parsing {:?}", use_tree);
+//     debug!("WARNING: This use statement will be ignored.");
+// }
+//
+// /// Takes a use tree and returns a flat list of use paths (list of string tokens)
+// ///
+// /// Example:
+// ///     use a::{b::c, d::e};
+// /// becomes
+// ///     [
+// ///         ["a", "b", "c"],
+// ///         ["a", "d", "e"]
+// ///     ]
+// ///
+// /// Warning: As of writing, import renames (import a::b as c) are silently
+// /// ignored.
+// fn flatten_use_tree(use_tree: &UseTree) -> ParserResult<Vec<Vec<String>>> {
+//     // Vec<(path, is_complete)>
+//     let mut result = vec![(vec![], false)];
+//
+//     let mut counter: usize = 0;
+//
+//     loop {
+//         counter += 1;
+//
+//         if counter > 10000 {
+//             panic!("flatten_use_tree: Use statement complexity limit exceeded. This is probably a bug.");
+//         }
+//
+//         // If all paths are complete, break from the loop
+//         if result.iter().all(|result_item| result_item.1) {
+//             break;
+//         }
+//
+//         let mut items_to_push = Vec::new();
+//
+//         for path_tuple in &mut result {
+//             let path = &mut path_tuple.0;
+//             let is_complete = &mut path_tuple.1;
+//
+//             if *is_complete {
+//                 continue;
+//             }
+//
+//             let mut tree_cursor = use_tree;
+//
+//             for path_item in path.iter() {
+//                 match tree_cursor {
+//                     UseTree::Path(use_path) => {
+//                         let ident = use_path.ident.to_string();
+//                         if *path_item != ident {
+//                             panic!("This ident did not match the one we already collected. This is a bug.");
+//                         }
+//                         tree_cursor = use_path.tree.as_ref();
+//                     }
+//                     UseTree::Group(use_group) => {
+//                         let mut moved_tree_cursor = false;
+//
+//                         for tree in use_group.items.iter() {
+//                             match tree {
+//                                 UseTree::Path(use_path) => {
+//                                     if path_item == &use_path.ident.to_string() {
+//                                         tree_cursor = use_path.tree.as_ref();
+//                                         moved_tree_cursor = true;
+//                                         break;
+//                                     }
+//                                 }
+//                                 // Since we're not matching UseTree::Group here, a::b::{{c}, {d}} might
+//                                 // break. But also why would anybody do that
+//                                 _ => unreachable!(),
+//                             }
+//                         }
+//
+//                         if !moved_tree_cursor {
+//                             unreachable!();
+//                         }
+//                     }
+//                     _ => unreachable!(),
+//                 }
+//             }
+//
+//             match tree_cursor {
+//                 UseTree::Name(use_name) => {
+//                     path.push(use_name.ident.to_string());
+//                     *is_complete = true;
+//                 }
+//                 UseTree::Path(use_path) => {
+//                     path.push(use_path.ident.to_string());
+//                 }
+//                 UseTree::Glob(_) => {
+//                     path.push("*".to_string());
+//                     *is_complete = true;
+//                 }
+//                 UseTree::Group(use_group) => {
+//                     // We'll modify the first one in-place, and make clones for
+//                     // all subsequent ones
+//                     let mut first: bool = true;
+//                     // Capture the path in this state, since we're about to
+//                     // modify it
+//                     let path_copy = path.clone();
+//                     for tree in use_group.items.iter() {
+//                         let mut new_path_tuple = if first {
+//                             None
+//                         } else {
+//                             let new_path = path_copy.clone();
+//                             items_to_push.push((new_path, false));
+//                             Some(items_to_push.iter_mut().last().unwrap())
+//                         };
+//
+//                         match tree {
+//                             UseTree::Path(use_path) => {
+//                                 let ident = use_path.ident.to_string();
+//
+//                                 if first {
+//                                     path.push(ident);
+//                                 } else {
+//                                     new_path_tuple.unwrap().0.push(ident);
+//                                 }
+//                             }
+//                             UseTree::Name(use_name) => {
+//                                 let ident = use_name.ident.to_string();
+//
+//                                 if first {
+//                                     path.push(ident);
+//                                     *is_complete = true;
+//                                 } else {
+//                                     let path_tuple = new_path_tuple.as_mut().unwrap();
+//                                     path_tuple.0.push(ident);
+//                                     path_tuple.1 = true;
+//                                 }
+//                             }
+//                             UseTree::Glob(_) => {
+//                                 if first {
+//                                     path.push("*".to_string());
+//                                     *is_complete = true;
+//                                 } else {
+//                                     let path_tuple = new_path_tuple.as_mut().unwrap();
+//                                     path_tuple.0.push("*".to_string());
+//                                     path_tuple.1 = true;
+//                                 }
+//                             }
+//                             UseTree::Group(_) => {
+//                                 Err(anyhow::anyhow!(
+//                                     "Directly-nested use groups ({}) are not supported by flutter_rust_bridge. Use {} instead.",
+//                                     "use a::{{b}, c}",
+//                                     "a::{b, c}"
+//                                 ))?;
+//                             }
+//                             // UseTree::Group(_) => panic!(),
+//                             UseTree::Rename(_) => {
+//                                 flatten_use_tree_rename_abort_warning(use_tree);
+//                                 return Ok(vec![]);
+//                             }
+//                         }
+//
+//                         first = false;
+//                     }
+//                 }
+//                 UseTree::Rename(_) => {
+//                     flatten_use_tree_rename_abort_warning(use_tree);
+//                     return Ok(vec![]);
+//                 }
+//             }
+//         }
+//
+//         for item in items_to_push {
+//             result.push(item);
+//         }
+//     }
+//
+//     Ok(result.into_iter().map(|val| val.0).collect())
+// }
