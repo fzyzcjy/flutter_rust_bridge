777153156e1fbe6d366af3ae42b77b04dda00669
fzyzcjy <ch271828n@outlook.com>
2023-11-15 10:21:04 +0800

    chore: comments

--- frb_codegen/src/library/codegen/parser/mod.rs
+++ frb_codegen/src/library/codegen/parser/mod.rs
@@ -13,6 +13,7 @@ pub(crate) type ParserResult<T = (), E = error::Error> = Result<T, E>;
 
 pub(crate) fn parse(config: &ParserInternalConfig) -> ParserResult<IrPack> {
     let raw_packs = config.rust_input_path_pack.rust_input_path.iter()
+        // TODO handle namespace
         .map(|(namespace, rust_input_path)| parse_one(rust_input_path, &config.rust_crate_dir))
         .collect::<Result<Vec<_>, _>>()?;
     let merged_pack = raw_packs.into_iter().reduce(|acc, e| acc.merge(e)).unwrap();
@@ -20,11 +21,8 @@ pub(crate) fn parse(config: &ParserInternalConfig) -> ParserResult<IrPack> {
 }
 
 fn parse_one(rust_input_path: &Path, rust_crate_dir: &Path) -> ParserResult<IrPack> {
-    debug!("Phase: Parse source code to AST");
     let source_rust_content = read_rust_file(rust_input_path, rust_crate_dir)?;
     let file_ast = syn::parse_file(&source_rust_content)?;
-
-    debug!("Phase: Parse AST to IR");
     parse_one_ast(&source_rust_content, file_ast, &rust_crate_dir)
 }
 
