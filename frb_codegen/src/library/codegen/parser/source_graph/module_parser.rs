223f404a99a875e0139ea92ad2021ae3b7c25470
fzyzcjy <ch271828n@outlook.com>
2023-11-15 20:35:19 +0800

    chore: mv

--- frb_codegen/src/library/codegen/parser/source_graph/module_parser.rs
+++ frb_codegen/src/library/codegen/parser/source_graph/module_parser.rs
@@ -6,9 +6,17 @@ use crate::utils::path_utils::path_to_string;
 use itertools::Itertools;
 use log::{debug, warn};
 use std::path::{Path, PathBuf};
+use syn::{Attribute, Ident, PathArguments};
 
 impl Module {
     /// Maps out modules, structs and enums within the scope of this module
+    //
+    // Things this doesn't currently support that it might need to later:
+    //
+    // - Import parsing is unfinished and so is currently disabled
+    // - When import parsing is enabled:
+    //     - Import renames (use a::b as c) - these are silently ignored
+    //     - Imports that start with two colons (use ::a::b) - these are also silently ignored
     pub fn parse(info: ModuleInfo) -> anyhow::Result<Self> {
         let mut scope_modules = Vec::new();
         let mut scope_structs = Vec::new();
@@ -150,6 +158,31 @@ fn get_module_file_path_candidates(
 
 fn first_existing_path(path_candidates: &[PathBuf]) -> Option<&PathBuf> {
     path_candidates.iter().find(|path| path.exists())
+}
+
+/// Get a struct or enum ident, possibly remapped by a mirror marker
+fn get_ident(ident: &Ident, attrs: &[Attribute]) -> (Vec<Ident>, bool) {
+    // TODO use `metadata.mirror()`
+    let res = markers::extract_mirror_marker(attrs)
+        .into_iter()
+        .filter_map(|path| {
+            // eq: path.get_ident().map(Clone::clone)
+            if path.leading_colon.is_none()
+                && path.segments.len() == 1
+                && path.segments[0].arguments == PathArguments::None
+            {
+                Some(path.segments.into_iter().next().unwrap().ident)
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<_>>();
+    let mirror = !res.is_empty();
+    if mirror {
+        (res, mirror)
+    } else {
+        (vec![ident.clone()], mirror)
+    }
 }
 
 #[cfg(test)]
--- frb_codegen/src/old/parser/source_graph.rs
+++ frb_codegen/src/old/parser/source_graph.rs
@@ -1,12 +1,3 @@
-/*
-    Things this doesn't currently support that it might need to later:
-
-    - Import parsing is unfinished and so is currently disabled
-    - When import parsing is enabled:
-        - Import renames (use a::b as c) - these are silently ignored
-        - Imports that start with two colons (use ::a::b) - these are also silently ignored
-*/
-
 use std::{
     collections::HashMap,
     fmt::Debug,
@@ -20,28 +11,3 @@ use syn::{Attribute, Ident, ItemEnum, ItemStruct, PathArguments, Type, UseTree};
 
 use super::ParserResult;
 use crate::{parser::markers, utils::misc::read_rust_file};
-
-/// Get a struct or enum ident, possibly remapped by a mirror marker
-fn get_ident(ident: &Ident, attrs: &[Attribute]) -> (Vec<Ident>, bool) {
-    // TODO use `metadata.mirror()`
-    let res = markers::extract_mirror_marker(attrs)
-        .into_iter()
-        .filter_map(|path| {
-            // eq: path.get_ident().map(Clone::clone)
-            if path.leading_colon.is_none()
-                && path.segments.len() == 1
-                && path.segments[0].arguments == PathArguments::None
-            {
-                Some(path.segments.into_iter().next().unwrap().ident)
-            } else {
-                None
-            }
-        })
-        .collect::<Vec<_>>();
-    let mirror = !res.is_empty();
-    if mirror {
-        (res, mirror)
-    } else {
-        (vec![ident.clone()], mirror)
-    }
-}
