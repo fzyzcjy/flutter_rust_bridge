1716265d5dd2d19a2c0d5b46548f8a21686b1688
fzyzcjy <ch271828n@outlook.com>
2023-11-15 22:33:52 +0800

    refactor: extract

--- frb_codegen/src/library/codegen/parser/type_parser/ty.rs
+++ frb_codegen/src/library/codegen/parser/type_parser/ty.rs
@@ -5,35 +5,16 @@ use crate::codegen::ir::ty::IrType;
 use crate::codegen::ir::ty::IrType::{Delegate, Primitive};
 use crate::codegen::parser::type_parser::TypeParser;
 use quote::ToTokens;
+use syn::{Expr, Type};
 
 impl<'a> TypeParser<'a> {
-    pub(crate) fn parse_type(&mut self, ty: &syn::Type) -> IrType {
+    pub(crate) fn parse_type(&mut self, ty: &Type) -> IrType {
         let resolve_ty = self.resolve_alias(ty).clone();
 
         match resolve_ty.clone() {
-            syn::Type::Path(path) => self.convert_path_to_ir_type(&path).unwrap(),
-            syn::Type::Array(syn::TypeArray { elem, len, .. }) => {
-                let length: usize = match len {
-                    syn::Expr::Lit(lit) => match &lit.lit {
-                        syn::Lit::Int(x) => x.base10_parse().unwrap(),
-                        _ => panic!("Cannot parse array length"),
-                    },
-                    _ => panic!("Cannot parse array length"),
-                };
-                match self.parse_type(&elem) {
-                    Primitive(primitive) => {
-                        Delegate(IrTypeDelegate::Array(IrTypeDelegateArray::PrimitiveArray {
-                            length,
-                            primitive,
-                        }))
-                    }
-                    others => Delegate(IrTypeDelegate::Array(IrTypeDelegateArray::GeneralArray {
-                        length,
-                        general: Box::new(others),
-                    })),
-                }
-            }
-            syn::Type::Tuple(syn::TypeTuple { elems, .. }) => {
+            Type::Path(path) => self.convert_path_to_ir_type(&path).unwrap(),
+            Type::Array(syn::TypeArray { elem, len, .. }) => self.parse_type_array(&elem, len),
+            Type::Tuple(syn::TypeTuple { elems, .. }) => {
                 if elems.is_empty() {
                     Primitive(IrTypePrimitive::Unit)
                 } else {
@@ -44,6 +25,28 @@ impl<'a> TypeParser<'a> {
                 string: resolve_ty.to_token_stream().to_string(),
                 segments: vec![],
             }),
+        }
+    }
+
+    fn parse_type_array(&mut self, elem: &Box<Type>, len: Expr) -> IrType {
+        let length: usize = match len {
+            Expr::Lit(lit) => match &lit.lit {
+                syn::Lit::Int(x) => x.base10_parse().unwrap(),
+                _ => panic!("Cannot parse array length"),
+            },
+            _ => panic!("Cannot parse array length"),
+        };
+        match self.parse_type(&elem) {
+            Primitive(primitive) => {
+                Delegate(IrTypeDelegate::Array(IrTypeDelegateArray::PrimitiveArray {
+                    length,
+                    primitive,
+                }))
+            }
+            others => Delegate(IrTypeDelegate::Array(IrTypeDelegateArray::GeneralArray {
+                length,
+                general: Box::new(others),
+            })),
         }
     }
 }
