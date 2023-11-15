d2033136a90567af3ed2b3ad5f42d7aa04715ab0
fzyzcjy <ch271828n@outlook.com>
2023-11-15 22:00:17 +0800

    fix: compile

--- frb_codegen/src/library/codegen/parser/type_parser/enumeration.rs
+++ frb_codegen/src/library/codegen/parser/type_parser/enumeration.rs
@@ -3,71 +3,79 @@ use crate::codegen::ir::field::{IrField, IrFieldSettings};
 use crate::codegen::ir::ident::IrIdent;
 use crate::codegen::ir::ty::enumeration::{IrEnum, IrVariant, IrVariantKind};
 use crate::codegen::ir::ty::structure::IrStruct;
+use crate::codegen::parser::attribute_parser::FrbAttributes;
 use crate::codegen::parser::type_parser::misc::parse_comments;
 use crate::codegen::parser::type_parser::TypeParser;
+use anyhow::anyhow;
 use syn::Field;
 
 impl<'a> TypeParser<'a> {
-    fn parse_enum(&mut self, ident_string: &str) -> IrEnum {
+    fn parse_enum(&mut self, ident_string: &str) -> anyhow::Result<IrEnum> {
         let src_enum = self.src_enums[ident_string];
-        let name = src_enum.ident.to_string();
-        let wrapper_name = if src_enum.mirror {
+        let name = src_enum.0.ident.to_string();
+        let wrapper_name = if src_enum.0.mirror {
             Some(format!("mirror_{name}"))
         } else {
             None
         };
 
-        let path = src_enum.path.clone();
-        let comments = parse_comments(&src_enum.src.attrs);
+        let path = src_enum.0.path.clone();
+        let comments = parse_comments(&src_enum.0.src.attrs);
         let variants = src_enum
+            .0
             .src
             .variants
             .iter()
-            .map(|variant| IrVariant {
-                name: IrIdent::new(variant.ident.to_string()),
-                wrapper_name: IrIdent::new(format!("{}_{}", src_enum.ident, variant.ident)),
-                comments: parse_comments(&variant.attrs),
-                kind: match variant.fields.iter().next() {
-                    None => IrVariantKind::Value,
-                    Some(Field {
-                        attrs,
-                        ident: field_ident,
-                        ..
-                    }) => {
-                        let variant_ident = variant.ident.to_string();
-                        IrVariantKind::Struct(IrStruct {
-                            name: variant_ident,
-                            wrapper_name: None,
-                            path: None,
-                            is_fields_named: field_ident.is_some(),
-                            dart_metadata: parse_metadata(attrs),
-                            comments: parse_comments(attrs),
-                            fields: variant
-                                .fields
-                                .iter()
-                                .enumerate()
-                                .map(|(idx, field)| IrField {
-                                    name: IrIdent::new(
-                                        field
-                                            .ident
-                                            .as_ref()
-                                            .map(ToString::to_string)
-                                            .unwrap_or_else(|| format!("field{idx}")),
-                                    ),
-                                    ty: self.parse_type(&field.ty),
-                                    is_final: true,
-                                    comments: parse_comments(&field.attrs),
-                                    default: IrDefaultValue::extract(&field.attrs),
-                                    settings: IrFieldSettings {
-                                        is_in_mirrored_enum: src_enum.mirror,
-                                    },
-                                })
-                                .collect(),
-                        })
-                    }
-                },
+            .map(|variant| {
+                Ok(IrVariant {
+                    name: IrIdent::new(variant.ident.to_string()),
+                    wrapper_name: IrIdent::new(format!("{}_{}", src_enum.0.ident, variant.ident)),
+                    comments: parse_comments(&variant.attrs),
+                    kind: match variant.fields.iter().next() {
+                        None => IrVariantKind::Value,
+                        Some(Field {
+                            attrs,
+                            ident: field_ident,
+                            ..
+                        }) => {
+                            let variant_ident = variant.ident.to_string();
+                            IrVariantKind::Struct(IrStruct {
+                                name: variant_ident,
+                                wrapper_name: None,
+                                path: None,
+                                is_fields_named: field_ident.is_some(),
+                                dart_metadata: FrbAttributes::parse(attrs)?.dart_metadata(),
+                                comments: parse_comments(attrs),
+                                fields: variant
+                                    .fields
+                                    .iter()
+                                    .enumerate()
+                                    .map(|(idx, field)| {
+                                        Ok(IrField {
+                                            name: IrIdent::new(
+                                                field
+                                                    .ident
+                                                    .as_ref()
+                                                    .map(ToString::to_string)
+                                                    .unwrap_or_else(|| format!("field{idx}")),
+                                            ),
+                                            ty: self.parse_type(&field.ty),
+                                            is_final: true,
+                                            comments: parse_comments(&field.attrs),
+                                            default: FrbAttributes::parse(&field.attrs)?
+                                                .default_value(),
+                                            settings: IrFieldSettings {
+                                                is_in_mirrored_enum: src_enum.0.mirror,
+                                            },
+                                        })
+                                    })
+                                    .collect::<anyhow::Result<Vec<_>>>()?,
+                            })
+                        }
+                    },
+                })
             })
-            .collect();
+            .collect::<anyhow::Result<Vec<_>>>()?;
         IrEnum::new(name, wrapper_name, path, comments, variants, false)
     }
 }
