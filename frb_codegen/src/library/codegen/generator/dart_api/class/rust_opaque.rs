7286880aa17808a8334b3dd6d6b3a0ac255ab7f3
fzyzcjy <ch271828n@outlook.com>
2023-11-16 21:49:43 +0800

    feat: dart_api_instance_name

--- frb_codegen/src/library/codegen/generator/dart_api/class/rust_opaque.rs
+++ frb_codegen/src/library/codegen/generator/dart_api/class/rust_opaque.rs
@@ -3,28 +3,33 @@ use crate::codegen::generator::dart_api::class::DartApiGeneratorClassTrait;
 
 impl<'a> DartApiGeneratorClassTrait for RustOpaqueDartApiGenerator<'a> {
     fn generate_class(&self) -> Option<String> {
-        let bridge = self.context.config.get_dart_api_bridge_name();
-        let bridge_class = &self.context.config.dart_api_class_name;
+        let dart_api_instance_name = &self.context.config.dart_api_instance_name;
+        let dart_api_class_name = &self.context.config.dart_api_class_name;
+        let dart_api_type = self.ir.dart_api_type();
+
         let (field, param) = if self.context.config.use_bridge_in_method {
-            (format!("final {bridge_class} bridge;"), "this.bridge")
+            (
+                format!("final {dart_api_class_name} bridge;"),
+                ", this.bridge",
+            )
         } else {
             (String::new(), "")
         };
-        let dart_api_type = self.ir.dart_api_type();
+
         Some(format!(
             "@sealed class {dart_api_type} extends FrbOpaque {{
                 {field}
 
-                {dart_api_type}.fromRaw(int ptr, int size, {param}): super.unsafe(ptr, size);
+                {dart_api_type}.fromRaw(int ptr, int size {param}): super.unsafe(ptr, size);
 
                 @override
-                DropFnType get dropFn => {bridge}.dropOpaque{dart_api_type};
+                DropFnType get dropFn => {dart_api_instance_name}.dropOpaque{dart_api_type};
         
                 @override
-                ShareFnType get shareFn => {bridge}.shareOpaque{dart_api_type};
+                ShareFnType get shareFn => {dart_api_instance_name}.shareOpaque{dart_api_type};
         
                 @override
-                OpaqueTypeFinalizer get staticFinalizer => {bridge}.{dart_api_type}Finalizer;
+                OpaqueTypeFinalizer get staticFinalizer => {dart_api_instance_name}.{dart_api_type}Finalizer;
             }}"
         ))
     }
--- frb_codegen/src/library/codegen/generator/dart_api/internal_config.rs
+++ frb_codegen/src/library/codegen/generator/dart_api/internal_config.rs
@@ -6,5 +6,7 @@ use std::path::PathBuf;
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
 pub(crate) struct GeneratorDartApiInternalConfig {
     pub dart_api_class_name: String,
+    // TODO originally it is `fn get_dart_api_bridge_name()`
+    pub dart_api_instance_name: String,
     pub use_bridge_in_method: bool,
 }
