642ba87bd7efb467aa0087d67bbaf545ff9b8eaf
fzyzcjy <ch271828n@outlook.com>
2023-11-16 10:34:06 +0800

    feat: enhance log setup

--- frb_codegen/src/library/utils/logs.rs
+++ frb_codegen/src/library/utils/logs.rs
@@ -38,23 +38,18 @@ pub fn configure_opinionated_logging(path: &str, verbose: bool) -> Result<(), fe
     });
 
     std::fs::create_dir_all(path).unwrap();
-    match std::env::var("RUST_LOG")
-        .unwrap_or_else(|_| if verbose { "debug" } else { "info" }.to_owned())
-        .as_str()
-    {
-        // #[cfg(debug_assertions)]
-        "debug" => d
+    match log_level_from_env_var().unwrap_or_else(|| verbose_to_level_filter(verbose)) {
+        LevelFilter::Debug => d
             .level(LevelFilter::Debug)
             .chain(fern::DateBased::new(path, "%Y-%m-%d.log"))
             .chain(std::io::stdout())
             .apply()?,
-        // #[cfg(not(debug_assertions))]
-        "info" => d
+        LevelFilter::Info => d
             .level(LevelFilter::Info)
             .level_for("cbindgen", LevelFilter::Error)
             .chain(std::io::stdout())
             .apply()?,
-        _ => panic!("only allow \"debug\" and \"info\""),
+        _ => panic!("only allow \"debug\" or \"info\""),
     }
 
     let prev = std::panic::take_hook();
@@ -66,12 +61,34 @@ pub fn configure_opinionated_logging(path: &str, verbose: bool) -> Result<(), fe
     Ok(())
 }
 
+fn log_level_from_env_var() -> Option<LevelFilter> {
+    std::env::var("RUST_LOG")
+        .ok()
+        .map(|value| match &value[..] {
+            "trace" => LevelFilter::Trace,
+            "debug" => LevelFilter::Debug,
+            "info" => LevelFilter::Info,
+            "warn" => LevelFilter::Warn,
+            "error" => LevelFilter::Error,
+            "off" => LevelFilter::Off,
+            _ => panic!("unknown RUST_LOG level: {value}"),
+        })
+}
+
+fn verbose_to_level_filter(verbose: bool) -> LevelFilter {
+    if verbose {
+        LevelFilter::Debug
+    } else {
+        LevelFilter::Info
+    }
+}
+
 /// Configure an opinionated way of logging, useful in tests.
 pub fn configure_opinionated_test_logging() {
     // https://github.com/daboross/fern/issues/54
     // This will fail if called twice; don't worry.
     let _ = fern::Dispatch::new()
-        .level(log::LevelFilter::Debug)
+        .level(log_level_from_env_var().unwrap_or(LevelFilter::Debug))
         .chain(fern::Output::call(|record| println!("{}", record.args())))
         .apply();
 }
