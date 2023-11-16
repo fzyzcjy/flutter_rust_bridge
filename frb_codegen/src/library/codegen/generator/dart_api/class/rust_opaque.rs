1fdc4cdcfaf5fd3daef3e5d29ea9c69cd6173464
fzyzcjy <ch271828n@outlook.com>
2023-11-16 20:31:30 +0800

    chore: comment out partially

--- frb_codegen/src/library/codegen/generator/dart_api/class/rust_opaque.rs
+++ frb_codegen/src/library/codegen/generator/dart_api/class/rust_opaque.rs
@@ -4,27 +4,28 @@ use crate::codegen::ir::ty::rust_opaque::IrTypeRustOpaque;
 
 impl<'a> DartApiClassGeneratorTrait for RustOpaqueDartApiGenerator<'a> {
     fn generate_class(&self) -> Option<String> {
-        let bridge = self.context.config.get_dart_api_bridge_name();
-        let bridge_class = self.context.config.dart_api_class_name();
-        let (field, param) = if self.context.config.bridge_in_method {
-            (format!("final {bridge_class} bridge;"), "this.bridge")
-        } else {
-            (String::new(), "")
-        };
-        Some(format!(
-            "@sealed class {0} extends FrbOpaque {{
-                {field}
-                {0}.fromRaw(int ptr, int size, {param}) : super.unsafe(ptr, size);
-                @override
-                DropFnType get dropFn => {bridge}.dropOpaque{0};
-
-                @override
-                ShareFnType get shareFn => {bridge}.shareOpaque{0};
-
-                @override
-                OpaqueTypeFinalizer get staticFinalizer => {bridge}.{0}Finalizer;
-            }}",
-            self.ir.dart_api_type()
-        ))
+        todo!()
+        // let bridge = self.context.config.get_dart_api_bridge_name();
+        // let bridge_class = self.context.config.dart_api_class_name();
+        // let (field, param) = if self.context.config.bridge_in_method {
+        //     (format!("final {bridge_class} bridge;"), "this.bridge")
+        // } else {
+        //     (String::new(), "")
+        // };
+        // Some(format!(
+        //     "@sealed class {0} extends FrbOpaque {{
+        //         {field}
+        //         {0}.fromRaw(int ptr, int size, {param}) : super.unsafe(ptr, size);
+        //         @override
+        //         DropFnType get dropFn => {bridge}.dropOpaque{0};
+        //
+        //         @override
+        //         ShareFnType get shareFn => {bridge}.shareOpaque{0};
+        //
+        //         @override
+        //         OpaqueTypeFinalizer get staticFinalizer => {bridge}.{0}Finalizer;
+        //     }}",
+        //     self.ir.dart_api_type()
+        // ))
     }
 }
--- frb_codegen/src/library/codegen/generator/dart_api/class/structure.rs
+++ frb_codegen/src/library/codegen/generator/dart_api/class/structure.rs
@@ -4,164 +4,165 @@ use crate::codegen::ir::ty::structure::IrTypeStructRef;
 
 impl<'a> DartApiClassGeneratorTrait for StructRefDartApiGenerator<'a> {
     fn generate_class(&self) -> Option<String> {
-        let src = self.ir.get(self.context.ir_pack);
-        let comments = dart_comments(&src.comments);
-        let metadata = dart_metadata(&src.dart_metadata);
-
-        let ir_pack = self.context.ir_pack;
-        let methods = ir_pack
-            .funcs
-            .iter()
-            .filter(|f| {
-                let f = FunctionName::deserialize(&f.name);
-                f.is_method_for_struct(&src.name) || f.is_static_method_for_struct(&src.name)
-            })
-            .collect::<Vec<_>>();
-
-        let has_methods = !methods.is_empty();
-        let methods = methods
-            .iter()
-            .map(|func| {
-                generate_api_method(
-                    func,
-                    src,
-                    self.context.config.dart_api_class_name().to_string(),
-                    self.context.config,
-                )
-            })
-            .collect::<Vec<_>>();
-
-        let methods_string = methods
-            .iter()
-            .map(|g| {
-                format!(
-                    "{}{}=>{};\n\n",
-                    g.comments,
-                    g.signature.clone(),
-                    g.implementation.clone()
-                )
-            })
-            .collect::<Vec<_>>()
-            .concat();
-        let extra_argument = if self.context.config.bridge_in_method {
-            "required this.bridge,".to_string()
-        } else {
-            "".to_string()
-        };
-        let field_bridge = if self.context.config.bridge_in_method {
-            format!(
-                "final {} bridge;",
-                self.context.config.dart_api_class_name(),
-            )
-        } else {
-            String::new()
-        };
-        let private_constructor = if has_methods {
-            format!("const {}._();", self.ir.name)
-        } else {
-            "".to_owned()
-        };
-        Some(if src.using_freezed() {
-            let mut constructor_params = src
-                .fields
-                .iter()
-                .map(|field| {
-                    let r#default = field.field_default(true, Some(self.context.config));
-                    format!(
-                        "{default} {} {} {},",
-                        field.required_modifier(),
-                        field.ty.dart_api_type(),
-                        field.name.dart_style()
-                    )
-                })
-                .collect::<Vec<_>>();
-            if has_methods && self.context.config.bridge_in_method {
-                constructor_params.insert(
-                    0,
-                    format!(
-                        "required {} bridge,",
-                        self.context.config.dart_api_class_name()
-                    ),
-                );
-            }
-            let constructor_params = constructor_params.join("");
-
-            format!(
-                "{comments}{meta}class {Name} with _${Name} {implements_exception} {{
-                    {private_constructor}
-                    const factory {Name}({{{}}}) = _{Name};
-                    {}
-                }}",
-                constructor_params,
-                methods_string,
-                comments = comments,
-                private_constructor = private_constructor,
-                meta = metadata,
-                Name = self.ir.name,
-                implements_exception = dart_maybe_implements_exception(self.ir.is_exception),
-            )
-        } else {
-            let mut field_declarations = src
-                .fields
-                .iter()
-                .map(|f| {
-                    let comments = dart_comments(&f.comments);
-                    format!(
-                        "{}{} {} {};",
-                        comments,
-                        if f.is_final { "final" } else { "" },
-                        f.ty.dart_api_type(),
-                        f.name.dart_style()
-                    )
-                })
-                .collect::<Vec<_>>();
-            if has_methods {
-                field_declarations.insert(0, field_bridge);
-            }
-            let field_declarations = field_declarations.join("\n");
-            let mut constructor_params = src
-                .fields
-                .iter()
-                .map(|f| {
-                    format!(
-                        "{required}this.{} {default},",
-                        f.name.dart_style(),
-                        required = f.required_modifier(),
-                        default = f.field_default(false, Some(self.context.config))
-                    )
-                })
-                .collect::<Vec<_>>();
-            if has_methods {
-                constructor_params.insert(0, extra_argument);
-            }
-
-            let (left, right) = if constructor_params.is_empty() {
-                ("", "")
-            } else {
-                ("{", "}")
-            };
-            let constructor_params = constructor_params.join("");
-            let const_capable = if src.const_capable() { "const " } else { "" };
-
-            format!(
-                "{comments}{meta}class {Name} {implements_exception} {{
-                    {}
-
-                    {const}{Name}({}{}{});
-
-                {}
-            }}",
-                field_declarations,
-                left,
-                constructor_params,
-                right,
-                methods_string,
-                comments = comments,
-                meta = metadata,
-                Name = self.ir.name,
-                const = const_capable,
-                implements_exception = dart_maybe_implements_exception(self.ir.is_exception),
-            )
-        })
+        todo!()
+        // let src = self.ir.get(self.context.ir_pack);
+        // let comments = dart_comments(&src.comments);
+        // let metadata = dart_metadata(&src.dart_metadata);
+        //
+        // let ir_pack = self.context.ir_pack;
+        // let methods = ir_pack
+        //     .funcs
+        //     .iter()
+        //     .filter(|f| {
+        //         let f = FunctionName::deserialize(&f.name);
+        //         f.is_method_for_struct(&src.name) || f.is_static_method_for_struct(&src.name)
+        //     })
+        //     .collect::<Vec<_>>();
+        //
+        // let has_methods = !methods.is_empty();
+        // let methods = methods
+        //     .iter()
+        //     .map(|func| {
+        //         generate_api_method(
+        //             func,
+        //             src,
+        //             self.context.config.dart_api_class_name().to_string(),
+        //             self.context.config,
+        //         )
+        //     })
+        //     .collect::<Vec<_>>();
+        //
+        // let methods_string = methods
+        //     .iter()
+        //     .map(|g| {
+        //         format!(
+        //             "{}{}=>{};\n\n",
+        //             g.comments,
+        //             g.signature.clone(),
+        //             g.implementation.clone()
+        //         )
+        //     })
+        //     .collect::<Vec<_>>()
+        //     .concat();
+        // let extra_argument = if self.context.config.bridge_in_method {
+        //     "required this.bridge,".to_string()
+        // } else {
+        //     "".to_string()
+        // };
+        // let field_bridge = if self.context.config.bridge_in_method {
+        //     format!(
+        //         "final {} bridge;",
+        //         self.context.config.dart_api_class_name(),
+        //     )
+        // } else {
+        //     String::new()
+        // };
+        // let private_constructor = if has_methods {
+        //     format!("const {}._();", self.ir.name)
+        // } else {
+        //     "".to_owned()
+        // };
+        // Some(if src.using_freezed() {
+        //     let mut constructor_params = src
+        //         .fields
+        //         .iter()
+        //         .map(|field| {
+        //             let r#default = field.field_default(true, Some(self.context.config));
+        //             format!(
+        //                 "{default} {} {} {},",
+        //                 field.required_modifier(),
+        //                 field.ty.dart_api_type(),
+        //                 field.name.dart_style()
+        //             )
+        //         })
+        //         .collect::<Vec<_>>();
+        //     if has_methods && self.context.config.bridge_in_method {
+        //         constructor_params.insert(
+        //             0,
+        //             format!(
+        //                 "required {} bridge,",
+        //                 self.context.config.dart_api_class_name()
+        //             ),
+        //         );
+        //     }
+        //     let constructor_params = constructor_params.join("");
+        //
+        //     format!(
+        //         "{comments}{meta}class {Name} with _${Name} {implements_exception} {{
+        //             {private_constructor}
+        //             const factory {Name}({{{}}}) = _{Name};
+        //             {}
+        //         }}",
+        //         constructor_params,
+        //         methods_string,
+        //         comments = comments,
+        //         private_constructor = private_constructor,
+        //         meta = metadata,
+        //         Name = self.ir.name,
+        //         implements_exception = dart_maybe_implements_exception(self.ir.is_exception),
+        //     )
+        // } else {
+        //     let mut field_declarations = src
+        //         .fields
+        //         .iter()
+        //         .map(|f| {
+        //             let comments = dart_comments(&f.comments);
+        //             format!(
+        //                 "{}{} {} {};",
+        //                 comments,
+        //                 if f.is_final { "final" } else { "" },
+        //                 f.ty.dart_api_type(),
+        //                 f.name.dart_style()
+        //             )
+        //         })
+        //         .collect::<Vec<_>>();
+        //     if has_methods {
+        //         field_declarations.insert(0, field_bridge);
+        //     }
+        //     let field_declarations = field_declarations.join("\n");
+        //     let mut constructor_params = src
+        //         .fields
+        //         .iter()
+        //         .map(|f| {
+        //             format!(
+        //                 "{required}this.{} {default},",
+        //                 f.name.dart_style(),
+        //                 required = f.required_modifier(),
+        //                 default = f.field_default(false, Some(self.context.config))
+        //             )
+        //         })
+        //         .collect::<Vec<_>>();
+        //     if has_methods {
+        //         constructor_params.insert(0, extra_argument);
+        //     }
+        //
+        //     let (left, right) = if constructor_params.is_empty() {
+        //         ("", "")
+        //     } else {
+        //         ("{", "}")
+        //     };
+        //     let constructor_params = constructor_params.join("");
+        //     let const_capable = if src.const_capable() { "const " } else { "" };
+        //
+        //     format!(
+        //         "{comments}{meta}class {Name} {implements_exception} {{
+        //             {}
+        //
+        //             {const}{Name}({}{}{});
+        //
+        //         {}
+        //     }}",
+        //         field_declarations,
+        //         left,
+        //         constructor_params,
+        //         right,
+        //         methods_string,
+        //         comments = comments,
+        //         meta = metadata,
+        //         Name = self.ir.name,
+        //         const = const_capable,
+        //         implements_exception = dart_maybe_implements_exception(self.ir.is_exception),
+        //     )
+        // })
     }
 }
