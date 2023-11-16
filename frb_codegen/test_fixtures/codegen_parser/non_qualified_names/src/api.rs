557e6306f43e04b812aa3051fc607cb271230ae4
fzyzcjy <ch271828n@outlook.com>
2023-11-16 16:45:48 +0800

    fix: wrong test

--- frb_codegen/test_fixtures/codegen_parser/non_qualified_names/src/api.rs
+++ frb_codegen/test_fixtures/codegen_parser/non_qualified_names/src/api.rs
@@ -2,4 +2,8 @@ use anyhow::Result;
 use chrono::*;
 use uuid::Uuid;
 
-pub fn f(anyhow_result: Result<i32, String>, duration: Duration, uuid: Uuid) {}
+pub fn func_1(duration: Duration, uuid: Uuid) {}
+
+pub fn func_anyhow_result() -> Result<i32, String> {
+    panic!()
+}
--- frb_codegen/test_fixtures/codegen_parser/qualified_names/src/api.rs
+++ frb_codegen/test_fixtures/codegen_parser/qualified_names/src/api.rs
@@ -1,8 +1,13 @@
-pub fn f(
-    result: Result<i32, String>,
-    anyhow_result: anyhow::Result<i32, String>,
-    std_result_result: std::result::Result<i32, String>,
-    chrono_duration: chrono::Duration,
-    uuid_uuid: uuid::Uuid,
-) {
+pub fn func_1(chrono_duration: chrono::Duration, uuid_uuid: uuid::Uuid) {}
+
+pub fn func_result() -> Result<i32, String> {
+    panic!()
+}
+
+pub fn func_anyhow_result() -> anyhow::Result<i32, String> {
+    panic!()
+}
+
+pub fn func_std_result_result() -> std::result::Result<i32, String> {
+    panic!()
 }
