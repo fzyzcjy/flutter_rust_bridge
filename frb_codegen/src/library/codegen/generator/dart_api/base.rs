4fbe0998a23d20b29f0400469197560e94d698df
fzyzcjy <ch271828n@outlook.com>
2023-11-16 20:11:08 +0800

    fix: more compile

--- frb_codegen/src/library/codegen/generator/dart_api/base.rs
+++ frb_codegen/src/library/codegen/generator/dart_api/base.rs
@@ -1,10 +1,30 @@
+#![allow(unused_imports)]
+
+use crate::codegen::ir::pack::IrPack;
+use crate::codegen::ir::ty::boxed::IrTypeBoxed;
+use crate::codegen::ir::ty::dart_opaque::IrTypeDartOpaque;
+use crate::codegen::ir::ty::delegate::IrTypeDelegate;
+use crate::codegen::ir::ty::dynamic::IrTypeDynamic;
+use crate::codegen::ir::ty::enumeration::IrTypeEnumRef;
+use crate::codegen::ir::ty::general_list::IrTypeGeneralList;
+use crate::codegen::ir::ty::optional::IrTypeOptional;
+use crate::codegen::ir::ty::optional_list::IrTypeOptionalList;
+use crate::codegen::ir::ty::primitive::IrTypePrimitive;
+use crate::codegen::ir::ty::primitive_list::IrTypePrimitiveList;
+use crate::codegen::ir::ty::record::IrTypeRecord;
+use crate::codegen::ir::ty::rust_opaque::IrTypeRustOpaque;
+use crate::codegen::ir::ty::structure::IrTypeStructRef;
+use crate::codegen::ir::ty::unencodable::IrTypeUnencodable;
+use crate::codegen::ir::ty::IrType;
 use crate::codegen::ir::ty::IrType::*;
+use enum_dispatch::enum_dispatch;
+use paste::paste;
 
 macro_rules! generate_code {
     ($($name:ident),*,) => (
         paste! {
             #[enum_dispatch(DartApiClassGeneratorTrait)]
-            pub enum DartApiGenerator<'a> {
+            pub(crate) enum DartApiGenerator<'a> {
                 $(
                     $name([<$name DartApiGenerator>]<'a>),
                 )*
@@ -15,7 +35,7 @@ macro_rules! generate_code {
                     let context = DartApiGeneratorContext { ir_pack };
                     Some(match ty {
                         $(
-                            $name(ir) => [<$name DartApiGenerator>]::new(ir, context).into(),
+                            $name(ir) => Self::$name([<$name DartApiGenerator>]::new(ir, context)),
                         )*
                     })
                 }
@@ -23,14 +43,14 @@ macro_rules! generate_code {
 
             $(
                 #[derive(Debug, Clone)]
-                pub(super) struct [<$name DartApiGenerator>]<'a> {
+                pub(crate) struct [<$name DartApiGenerator>]<'a> {
                     ir: [<IrType $name>],
-                    ir_pack: &'a crate::codegen::ir::pack::IrPack,
+                    context: DartApiGeneratorContext<'a>,
                 }
 
                 impl<'a> [<$name DartApiGenerator>]<'a> {
-                    pub(super) fn new(ir: [<IrType $name>], context: DartApiGeneratorContext<'a>) -> Self {
-                        Self { ir, ir_pack }
+                    pub(crate) fn new(ir: [<IrType $name>], context: DartApiGeneratorContext<'a>) -> Self {
+                        Self { ir, context }
                     }
                 }
             )*
@@ -54,3 +74,8 @@ generate_code!(
     StructRef,
     Unencodable,
 );
+
+#[derive(Debug, Clone)]
+pub(crate) struct DartApiGeneratorContext<'a> {
+    ir_pack: &'a IrPack,
+}
