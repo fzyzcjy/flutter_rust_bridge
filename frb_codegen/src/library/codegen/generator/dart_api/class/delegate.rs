84b2d1265da99e2f87378096b145b39cdbbe2008
fzyzcjy <ch271828n@outlook.com>
2023-11-16 20:22:52 +0800

    chore: uncomment

--- frb_codegen/src/library/codegen/generator/dart_api/class/delegate.rs
+++ frb_codegen/src/library/codegen/generator/dart_api/class/delegate.rs
@@ -1,38 +1,34 @@
-// TODO
-// use crate::codegen::generator::dart_api::class::enumeration::EnumRefDartApiClassGenerator;
-// use crate::codegen::generator::dart_api::class::DartApiClassGeneratorTrait;
-// use crate::codegen::ir::ty::delegate::{IrTypeDelegate, IrTypeDelegatePrimitiveEnum};
-// use crate::dart_api_class_generator_struct;
-//
-// dart_api_class_generator_struct!(DelegateDartApiClassGenerator, IrTypeDelegate);
-//
-// impl<'a> DartApiClassGeneratorTrait for DelegateDartApiClassGenerator<'a> {
-//     fn generate_class(&self) -> Option<String> {
-//         match &self.ir {
-//             IrTypeDelegate::PrimitiveEnum(IrTypeDelegatePrimitiveEnum { ir, .. }) => {
-//                 EnumRefDartApiClassGenerator::new(ir.clone(), self.ir_pack).generate_class()
-//             }
-//             IrTypeDelegate::Array(array) => {
-//                 format!(
-//                     "
-//                 class {0} extends NonGrowableListView<{1}> {{
-//                     static const arraySize = {2};
-//                     {0}({3} inner)
-//                         : assert(inner.length == arraySize),
-//                           super(inner);
-//                     {0}.unchecked({3} inner)
-//                         : super(inner);
-//                     {4}
-//                   }}
-//                 ",
-//                     array.dart_api_type(),
-//                     array.inner_dart_api_type(),
-//                     array.length(),
-//                     array.get_delegate().dart_api_type(),
-//                     array.dart_init_method(),
-//                 )
-//             }
-//             _ => "".into(),
-//         }
-//     }
-// }
+use crate::codegen::generator::dart_api::class::enumeration::EnumRefDartApiClassGenerator;
+use crate::codegen::generator::dart_api::class::DartApiClassGeneratorTrait;
+use crate::codegen::ir::ty::delegate::{IrTypeDelegate, IrTypeDelegatePrimitiveEnum};
+
+impl<'a> DartApiClassGeneratorTrait for DelegateDartApiClassGenerator<'a> {
+    fn generate_class(&self) -> Option<String> {
+        match &self.ir {
+            IrTypeDelegate::PrimitiveEnum(IrTypeDelegatePrimitiveEnum { ir, .. }) => {
+                EnumRefDartApiClassGenerator::new(ir.clone(), self.ir_pack).generate_class()
+            }
+            IrTypeDelegate::Array(array) => {
+                format!(
+                    "
+                class {0} extends NonGrowableListView<{1}> {{
+                    static const arraySize = {2};
+                    {0}({3} inner)
+                        : assert(inner.length == arraySize),
+                          super(inner);
+                    {0}.unchecked({3} inner)
+                        : super(inner);
+                    {4}
+                  }}
+                ",
+                    array.dart_api_type(),
+                    array.inner_dart_api_type(),
+                    array.length(),
+                    array.get_delegate().dart_api_type(),
+                    array.dart_init_method(),
+                )
+            }
+            _ => "".into(),
+        }
+    }
+}
