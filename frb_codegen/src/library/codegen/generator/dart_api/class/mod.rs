15a7377c5c07085f37b0321a4757f840af15a800
fzyzcjy <ch271828n@outlook.com>
2023-11-16 20:21:13 +0800

    chore: empty impl

--- frb_codegen/src/library/codegen/generator/dart_api/class/mod.rs
+++ frb_codegen/src/library/codegen/generator/dart_api/class/mod.rs
@@ -12,3 +12,18 @@ pub(super) trait DartApiClassGeneratorTrait {
         None
     }
 }
+
+impl<'a> DartApiClassGeneratorTrait for BoxedDartApiGenerator<'a> {}
+impl<'a> DartApiClassGeneratorTrait for DartOpaqueDartApiGenerator<'a> {}
+impl<'a> DartApiClassGeneratorTrait for DelegateDartApiGenerator<'a> {}
+impl<'a> DartApiClassGeneratorTrait for DynamicDartApiGenerator<'a> {}
+impl<'a> DartApiClassGeneratorTrait for EnumRefDartApiGenerator<'a> {}
+impl<'a> DartApiClassGeneratorTrait for GeneralListDartApiGenerator<'a> {}
+impl<'a> DartApiClassGeneratorTrait for OptionalDartApiGenerator<'a> {}
+impl<'a> DartApiClassGeneratorTrait for OptionalListDartApiGenerator<'a> {}
+impl<'a> DartApiClassGeneratorTrait for PrimitiveDartApiGenerator<'a> {}
+impl<'a> DartApiClassGeneratorTrait for PrimitiveListDartApiGenerator<'a> {}
+impl<'a> DartApiClassGeneratorTrait for RecordDartApiGenerator<'a> {}
+impl<'a> DartApiClassGeneratorTrait for RustOpaqueDartApiGenerator<'a> {}
+impl<'a> DartApiClassGeneratorTrait for StructRefDartApiGenerator<'a> {}
+impl<'a> DartApiClassGeneratorTrait for UnencodableDartApiGenerator<'a> {}
