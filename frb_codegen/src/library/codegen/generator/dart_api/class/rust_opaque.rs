fbf836811ac28b67eb1801763f8bd623c26c39c2
fzyzcjy <ch271828n@outlook.com>
2023-11-16 21:43:37 +0800

    chore: uncomment

--- frb_codegen/src/library/codegen/generator/dart_api/class/rust_opaque.rs
+++ frb_codegen/src/library/codegen/generator/dart_api/class/rust_opaque.rs
@@ -3,28 +3,27 @@ use crate::codegen::generator::dart_api::class::DartApiGeneratorClassTrait;
 
 impl<'a> DartApiGeneratorClassTrait for RustOpaqueDartApiGenerator<'a> {
     fn generate_class(&self) -> Option<String> {
-        todo!()
-        // let bridge = self.context.config.get_dart_api_bridge_name();
-        // let bridge_class = self.context.config.dart_api_class_name();
-        // let (field, param) = if self.context.config.bridge_in_method {
-        //     (format!("final {bridge_class} bridge;"), "this.bridge")
-        // } else {
-        //     (String::new(), "")
-        // };
-        // Some(format!(
-        //     "@sealed class {0} extends FrbOpaque {{
-        //         {field}
-        //         {0}.fromRaw(int ptr, int size, {param}) : super.unsafe(ptr, size);
-        //         @override
-        //         DropFnType get dropFn => {bridge}.dropOpaque{0};
-        //
-        //         @override
-        //         ShareFnType get shareFn => {bridge}.shareOpaque{0};
-        //
-        //         @override
-        //         OpaqueTypeFinalizer get staticFinalizer => {bridge}.{0}Finalizer;
-        //     }}",
-        //     self.ir.dart_api_type()
-        // ))
+        let bridge = self.context.config.get_dart_api_bridge_name();
+        let bridge_class = self.context.config.dart_api_class_name();
+        let (field, param) = if self.context.config.bridge_in_method {
+            (format!("final {bridge_class} bridge;"), "this.bridge")
+        } else {
+            (String::new(), "")
+        };
+        let dart_api_type = self.ir.dart_api_type();
+        Some(format!(
+            "@sealed class {dart_api_type} extends FrbOpaque {{
+                {field}
+                {dart_api_type}.fromRaw(int ptr, int size, {param}) : super.unsafe(ptr, size);
+                @override
+                DropFnType get dropFn => {bridge}.dropOpaque{dart_api_type};
+        
+                @override
+                ShareFnType get shareFn => {bridge}.shareOpaque{dart_api_type};
+        
+                @override
+                OpaqueTypeFinalizer get staticFinalizer => {bridge}.{dart_api_type}Finalizer;
+            }}"
+        ))
     }
 }
