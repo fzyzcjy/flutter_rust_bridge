c8ae0c40da2ea17928e65397951950aba0dd131c
fzyzcjy <ch271828n@outlook.com>
2023-11-16 14:22:53 +0800

    refactor: set_is_exception_flag

--- frb_codegen/src/library/codegen/parser/function_parser/output.rs
+++ frb_codegen/src/library/codegen/parser/function_parser/output.rs
@@ -56,25 +56,31 @@ fn parse_fn_output_type_result(args: &[IrType]) -> ParserResult<FunctionPartialI
             IrType::Unencodable(IrTypeUnencodable { string, .. }) => string == "anyhow :: Error",
             _ => false,
         });
-    let error = if is_anyhow {
+
+    let error_output = if is_anyhow {
         Some(IrType::Delegate(IrTypeDelegate::Anyhow))
     } else {
         args.last().cloned()
     };
 
-    let error_output = if let Some(StructRef(mut struct_ref)) = error {
-        struct_ref.is_exception = true;
-        Some(StructRef(struct_ref))
-    } else if let Some(EnumRef(mut enum_ref)) = error {
-        enum_ref.is_exception = true;
-        Some(EnumRef(enum_ref))
-    } else {
-        error
-    };
+    let error_output = error_output.map(set_is_exception_flag);
 
     Ok(FunctionPartialInfo {
         ok_output: Some(ok_output.clone()),
         error_output,
         ..Default::default()
     })
+}
+
+fn set_is_exception_flag(mut ty: IrType) -> IrType {
+    match &mut ty {
+        StructRef(mut inner) => {
+            inner.is_exception = true;
+        }
+        EnumRef(mut inner) => {
+            inner.is_exception = true;
+        }
+        _ => {}
+    }
+    ty
 }
