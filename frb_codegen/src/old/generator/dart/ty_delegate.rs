b9a636c46f5c4701e99955392b95bb221eb77eec
fzyzcjy <ch271828n@outlook.com>
2023-11-16 18:21:49 +0800

    chore: mv code

--- frb_codegen/src/old/generator/dart/ty_delegate.rs
+++ frb_codegen/src/old/generator/dart/ty_delegate.rs
@@ -148,35 +148,4 @@ impl TypeDartGeneratorTrait for TypeDelegateGenerator<'_> {
             IrTypeDelegate::Anyhow => "return FrbAnyhowException(raw as String);".to_owned(),
         }
     }
-
-    fn structs(&self) -> String {
-        match &self.ir {
-            IrTypeDelegate::PrimitiveEnum { ir, .. } => super::TypeEnumRefGenerator {
-                ir: ir.clone(),
-                context: self.context.clone(),
-            }
-            .structs(),
-            IrTypeDelegate::Array(array) => {
-                format!(
-                    "
-                class {0} extends NonGrowableListView<{1}> {{
-                    static const arraySize = {2};
-                    {0}({3} inner)
-                        : assert(inner.length == arraySize),
-                          super(inner);
-                    {0}.unchecked({3} inner)
-                        : super(inner);
-                    {4}
-                  }}
-                ",
-                    array.dart_api_type(),
-                    array.inner_dart_api_type(),
-                    array.length(),
-                    array.get_delegate().dart_api_type(),
-                    array.dart_init_method(),
-                )
-            }
-            _ => "".into(),
-        }
-    }
 }
