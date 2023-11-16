2b35fc653a75bf6a3a5897c9fb8be0dcceb97f46
fzyzcjy <ch271828n@outlook.com>
2023-11-16 19:55:41 +0800

    chore: try macros

--- frb_codegen/src/library/codegen/generator/dart_api/base.rs
+++ frb_codegen/src/library/codegen/generator/dart_api/base.rs
@@ -4,6 +4,47 @@ use crate::codegen::ir::ty::IrType;
 use crate::codegen::ir::ty::IrType::*;
 use enum_dispatch::enum_dispatch;
 
+macro_rules! primary {
+    ($($x:expr),+) => (
+        fn f() {
+            g($($x),+);
+        }
+    )
+}
+
+primary!(
+    Boxed,
+    DartOpaque,
+    Delegate,
+    Dynamic,
+    EnumRef,
+    GeneralList,
+    Optional,
+    OptionalList,
+    Primitive,
+    PrimitiveList,
+    Record,
+    RustOpaque,
+    StructRef,
+    Unencodable,
+);
+
+macro_rules! find_min {
+    // Base case:
+    ($x:expr) => ($x);
+    // `$x` followed by at least one `$y,`
+    ($x:expr, $($y:expr),+) => (
+        // Call `find_min!` on the tail `$y`
+        std::cmp::min($x, find_min!($($y),+))
+    )
+}
+
+fn main() {
+    println!("{}", find_min!(1));
+    println!("{}", find_min!(1 + 2, 2));
+    println!("{}", find_min!(5, 2 * 3, 4));
+}
+
 // #[enum_dispatch(IrTypeTrait)]
 // pub enum DartApiGenerator<'a> {
 //     // alphabetical order
