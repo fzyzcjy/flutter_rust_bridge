889851417555a171064f1b59730240d7b1fe79a8
fzyzcjy <ch271828n@outlook.com>
2023-11-17 13:21:01 +0800

    chore: comments

--- frb_codegen/src/library/codegen/generator/dart_api/internal_config.rs
+++ frb_codegen/src/library/codegen/generator/dart_api/internal_config.rs
@@ -2,6 +2,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
 pub(crate) struct GeneratorDartApiInternalConfig {
+    // TODO multi file support
     pub dart_api_class_name: String,
     // TODO originally it is `fn get_dart_api_bridge_name()`
     pub dart_api_instance_name: String,
