4997d8f57e1f13e5106305fc09fcd8d4f056cc43
fzyzcjy <ch271828n@outlook.com>
2023-11-17 22:41:37 +0800

    fix: compile

--- frb_codegen/src/library/codegen/generator/wire/rust/wire2api/mod.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/wire2api/mod.rs
@@ -31,8 +31,8 @@ fn generate_impl_wire2api_misc() -> &'static str {
     }"#
 }
 
-fn generate_wire2api_for_type(ty: IrType, context: WireRustGeneratorContext) -> Acc<String> {
-    let generator = WireRustGenerator::new(ty, context);
+fn generate_wire2api_for_type(ty: &IrType, context: WireRustGeneratorContext) -> Acc<String> {
+    let generator = WireRustGenerator::new(ty.clone(), context);
     let raw: Acc<Option<String>> = generator.wire2api_body();
     raw.map(|body, target| {
         body.map(|body| {
