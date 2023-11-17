573aaf1bed629b8380f090c9b704e2195da324ad
fzyzcjy <ch271828n@outlook.com>
2023-11-17 21:37:41 +0800

    Revert "chore: temp rm everything in `common`"
    This reverts commit c3afcb905506450881f4a6885e38e406e16bcb16.

--- frb_codegen/src/library/codegen/generator/wire/rust/common/ty/structure.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/common/ty/structure.rs
@@ -1,4 +1,9 @@
 use crate::codegen::generator::wire::rust::base::*;
 use crate::codegen::generator::wire::rust::common::ty::WireRustGeneratorCommonTrait;
 
-impl<'a> WireRustGeneratorCommonTrait for StructRefWireRustGenerator<'a> {}
+impl<'a> WireRustGeneratorCommonTrait for StructRefWireRustGenerator<'a> {
+    fn wrapper_struct_name(&self) -> Option<String> {
+        let src = self.ir.get(self.context.ir_pack);
+        src.wrapper_name.as_ref().cloned()
+    }
+}
