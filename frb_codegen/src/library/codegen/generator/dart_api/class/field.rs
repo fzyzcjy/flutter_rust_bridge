90b3063940a6602084515b11451df9bcd89e28ad
fzyzcjy <ch271828n@outlook.com>
2023-11-17 08:54:56 +0800

    fix: compile

--- frb_codegen/src/library/codegen/generator/dart_api/class/field.rs
+++ frb_codegen/src/library/codegen/generator/dart_api/class/field.rs
@@ -2,6 +2,8 @@ use crate::codegen::ir::default::IrDefaultValue;
 use crate::codegen::ir::field::IrField;
 use crate::codegen::ir::ty::delegate::IrTypeDelegate;
 use crate::codegen::ir::ty::IrType;
+use crate::utils::dart_keywords::make_string_keyword_safe;
+use convert_case::Case;
 
 pub(crate) fn generate_field_required_modifier(field: &IrField) -> &str {
     if field.is_optional() {
@@ -48,8 +50,7 @@ fn default_value_to_dart_style(lit: &LitStr) -> String {
     let enum_name = split.next().unwrap();
 
     let variant_name = split.next().unwrap().to_string();
-    let variant_name =
-        crate::utils::misc::make_string_keyword_safe(variant_name.to_case(Case::Camel));
+    let variant_name = make_string_keyword_safe(variant_name.to_case(Case::Camel));
 
     format!("{enum_name}.{variant_name}")
 }
