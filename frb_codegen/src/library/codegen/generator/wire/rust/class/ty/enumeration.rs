66c0740f023cffc67a28ce2e6258274b2cc82a40
fzyzcjy <ch271828n@outlook.com>
2023-11-17 18:39:51 +0800

    refactor: extract and beautify

--- frb_codegen/src/library/codegen/generator/wire/rust/class/ty/enumeration.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/class/ty/enumeration.rs
@@ -1,48 +1,24 @@
 use crate::codegen::generator::misc::Target;
 use crate::codegen::generator::wire::rust::base::*;
 use crate::codegen::generator::wire::rust::class::ty::WireRustClassGeneratorClassTrait;
-use crate::codegen::ir::ty::enumeration::IrVariantKind;
+use crate::codegen::ir::ty::enumeration::{IrEnumMode, IrVariant, IrVariantKind};
 use itertools::Itertools;
 
 impl<'a> WireRustClassGeneratorClassTrait for EnumRefWireRustGenerator<'a> {
     fn generate_class(&self) -> Option<String> {
         let src = self.ir.get(self.context.ir_pack);
-        if !src.is_struct() {
+        if src.mode == IrEnumMode::Simple {
             return None;
         }
 
-        let variant_structs = src
-            .variants()
+        let variants = src.variants();
+
+        let variant_structs = variants
             .iter()
-            .map(|variant| {
-                let fields = match &variant.kind {
-                    IrVariantKind::Value => vec![],
-                    IrVariantKind::Struct(s) => s
-                        .fields
-                        .iter()
-                        .map(|field| {
-                            format!(
-                                "{}: {}{},",
-                                field.name.rust_style(),
-                                field.ty.rust_wire_modifier(Target::Io),
-                                field.ty.rust_wire_type(Target::Io)
-                            )
-                        })
-                        .collect(),
-                };
-                format!(
-                    "#[repr(C)]
-                    #[derive(Clone)]
-                    pub struct wire_{}_{} {{ {} }}",
-                    self.ir.ident.0,
-                    variant.name.raw,
-                    fields.join("\n")
-                )
-            })
+            .map(|variant| self.generate_variant(&variant))
             .join("\n\n");
 
-        let union_fields = src
-            .variants()
+        let union_fields = variants
             .iter()
             .map(|variant| format!("{0}: *mut wire_{1}_{0},", variant.name.raw, self.ir.ident.0))
             .join("\n");
@@ -61,5 +37,33 @@ impl<'a> WireRustClassGeneratorClassTrait for EnumRefWireRustGenerator<'a> {
             rust_wire_type = self.ir.rust_wire_type(Target::Io),
             name = self.ir.ident.0,
         ))
+    }
+}
+
+impl<'a> EnumRefWireRustGenerator<'a> {
+    fn generate_variant(&self, variant: &IrVariant) -> String {
+        let fields = match &variant.kind {
+            IrVariantKind::Value => vec![],
+            IrVariantKind::Struct(s) => s
+                .fields
+                .iter()
+                .map(|field| {
+                    format!(
+                        "{}: {}{},",
+                        field.name.rust_style(),
+                        field.ty.rust_wire_modifier(Target::Io),
+                        field.ty.rust_wire_type(Target::Io)
+                    )
+                })
+                .collect(),
+        };
+        format!(
+            "#[repr(C)]
+            #[derive(Clone)]
+            pub struct wire_{}_{} {{ {} }}",
+            self.ir.ident.0,
+            variant.name.raw,
+            fields.join("\n")
+        )
     }
 }
