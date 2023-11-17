42f61c264ebcefe382a219ce202834e13bfd43ac
fzyzcjy <ch271828n@outlook.com>
2023-11-17 21:37:41 +0800

    Revert "chore: temp add `apple`"
    This reverts commit 14400b21875d0b78ec1649780ee0648d6614be1b.

--- frb_codegen/src/library/codegen/generator/wire/rust/apple/ty/delegate.rs
+++
@@ -1,5 +0,0 @@
-use crate::codegen::generator::wire::rust::apple::ty::WireRustGeneratorAppleTrait;
-use crate::codegen::generator::wire::rust::base::*;
-use crate::codegen::ir::ty::delegate::IrTypeDelegate;
-
-impl<'a> WireRustGeneratorAppleTrait for DelegateWireRustGenerator<'a> {}
