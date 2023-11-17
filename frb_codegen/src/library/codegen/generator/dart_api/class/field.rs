cd4186b5b9fb1e0adb75a68175a23cbee50f57ef
fzyzcjy <ch271828n@outlook.com>
2023-11-17 09:10:25 +0800

    feat: reimpl generate_field_default

--- frb_codegen/src/library/codegen/generator/dart_api/class/field.rs
+++ frb_codegen/src/library/codegen/generator/dart_api/class/field.rs
@@ -1,9 +1,10 @@
-use crate::codegen::ir::default::{IrDefaultValue, IrDefaultValueMode};
+use crate::codegen::ir::default::IrDefaultValue;
 use crate::codegen::ir::field::IrField;
 use crate::codegen::ir::ty::delegate::IrTypeDelegate;
 use crate::codegen::ir::ty::IrType;
 use crate::utils::dart_keywords::make_string_keyword_safe;
 use convert_case::{Case, Casing};
+use std::borrow::Cow;
 
 pub(crate) fn generate_field_required_modifier(field: &IrField) -> &str {
     if field.is_optional() {
@@ -19,18 +20,13 @@ pub(crate) fn generate_field_default(
     dart_enums_style: bool,
 ) -> String {
     if let Some(default_value) = field.default.as_ref() {
-        let default_value = match default_value.mode {
-            IrDefaultValueMode::String
+        let default_value = match default_value {
+            IrDefaultValue::String { content }
                 if !matches!(&field.ty, IrType::Delegate(IrTypeDelegate::String)) =>
             {
-                // Convert the default value to Dart style.
-                if dart_enums_style {
-                    default_value_to_dart_style(&default_value.literal).into()
-                } else {
-                    todo
-                }
+                default_value_maybe_to_dart_style(content, dart_enums_style)
             }
-            _ => &default_value.literal,
+            _ => default_value.to_dart_literal(),
         };
 
         if freezed {
@@ -40,6 +36,14 @@ pub(crate) fn generate_field_default(
         }
     } else {
         "".to_string()
+    }
+}
+
+fn default_value_maybe_to_dart_style(value: &str, enable: bool) -> Cow<str> {
+    if enable {
+        default_value_to_dart_style(value).into()
+    } else {
+        value.into()
     }
 }
 
