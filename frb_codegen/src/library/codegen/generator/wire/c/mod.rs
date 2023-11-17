0b6fd8c29df9bd3182c6e1f33369713a0c32483a
fzyzcjy <ch271828n@outlook.com>
2023-11-17 14:11:53 +0800

    feat: generate_dummy_function

--- frb_codegen/src/library/codegen/generator/wire/c/mod.rs
+++ frb_codegen/src/library/codegen/generator/wire/c/mod.rs
@@ -1,33 +1,20 @@
-use std::path::Path;
+use itertools::Itertools;
 
 fn generate_dummy_function(func_names: &[String]) -> String {
-    get_dummy_func("", func_names)
-}
-
-fn get_dummy_func(api_block_name: &str, func_names: &[String]) -> String {
+    let dummy_var_operations = compute_dummy_var_operations(func_names);
     format!(
-        r#"static int64_t {signature}(void) {{
+        r#"static int64_t dummy_method_to_enforce_bundling(void) {{
     int64_t dummy_var = 0;
-{content}
+{dummy_var_operations}
     return dummy_var;
 }}
-"#,
-        signature = get_dummy_signature(api_block_name),
-        content = get_dummy_var(func_names),
+"#
     )
 }
 
-fn get_dummy_signature(api_block_name: &str) -> String {
-    match api_block_name.is_empty() {
-        true => "dummy_method_to_enforce_bundling".into(),
-        false => format!(r#"dummy_method_to_enforce_bundling_{api_block_name}"#),
-    }
-}
-
-fn get_dummy_var(func_names: &[String]) -> String {
+fn compute_dummy_var_operations(func_names: &[String]) -> String {
     func_names
         .iter()
         .map(|func_name| format!("    dummy_var ^= ((int64_t) (void*) {func_name});"))
-        .collect_vec()
         .join("\n")
 }
