094900e9690a0c5124c1e5eab788d2d28f3e6bd8
fzyzcjy <ch271828n@outlook.com>
2023-11-17 14:58:00 +0800

    fix: more compile

--- frb_codegen/src/library/codegen/generator/wire/rust/class/misc.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/class/misc.rs
@@ -1,6 +1,7 @@
 use crate::codegen::generator::misc::Target;
 use crate::codegen::generator::wire::rust::base::{WireRustGenerator, WireRustGeneratorContext};
 use crate::codegen::ir::ty::IrType;
+use crate::library::codegen::generator::wire::rust::rust_wire_type::WireRustGeneratorRustWireTypeTrait;
 
 pub(super) fn generate_class_from_fields(
     ty: &IrType,
--- frb_codegen/src/library/codegen/generator/wire/rust/rust_wire_type.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/rust_wire_type.rs
@@ -1,91 +1,92 @@
+use crate::codegen::generator::misc::Target;
 use crate::codegen::generator::wire::rust::base::*;
 use enum_dispatch::enum_dispatch;
 
 #[enum_dispatch]
 pub(crate) trait WireRustGeneratorRustWireTypeTrait {
-    fn rust_wire_type(&self) -> String;
+    fn rust_wire_type(&self, target: Target) -> String;
 }
 
 impl<'a> WireRustGeneratorRustWireTypeTrait for BoxedWireRustGenerator<'a> {
-    fn rust_wire_type(&self) -> String {
+    fn rust_wire_type(&self, target: Target) -> String {
         todo!()
     }
 }
 
 impl<'a> WireRustGeneratorRustWireTypeTrait for DartOpaqueWireRustGenerator<'a> {
-    fn rust_wire_type(&self) -> String {
+    fn rust_wire_type(&self, target: Target) -> String {
         todo!()
     }
 }
 
 impl<'a> WireRustGeneratorRustWireTypeTrait for DelegateWireRustGenerator<'a> {
-    fn rust_wire_type(&self) -> String {
+    fn rust_wire_type(&self, target: Target) -> String {
         todo!()
     }
 }
 
 impl<'a> WireRustGeneratorRustWireTypeTrait for DynamicWireRustGenerator<'a> {
-    fn rust_wire_type(&self) -> String {
+    fn rust_wire_type(&self, target: Target) -> String {
         todo!()
     }
 }
 
 impl<'a> WireRustGeneratorRustWireTypeTrait for EnumRefWireRustGenerator<'a> {
-    fn rust_wire_type(&self) -> String {
+    fn rust_wire_type(&self, target: Target) -> String {
         todo!()
     }
 }
 
 impl<'a> WireRustGeneratorRustWireTypeTrait for GeneralListWireRustGenerator<'a> {
-    fn rust_wire_type(&self) -> String {
+    fn rust_wire_type(&self, target: Target) -> String {
         todo!()
     }
 }
 
 impl<'a> WireRustGeneratorRustWireTypeTrait for OptionalWireRustGenerator<'a> {
-    fn rust_wire_type(&self) -> String {
+    fn rust_wire_type(&self, target: Target) -> String {
         todo!()
     }
 }
 
 impl<'a> WireRustGeneratorRustWireTypeTrait for OptionalListWireRustGenerator<'a> {
-    fn rust_wire_type(&self) -> String {
+    fn rust_wire_type(&self, target: Target) -> String {
         todo!()
     }
 }
 
 impl<'a> WireRustGeneratorRustWireTypeTrait for PrimitiveWireRustGenerator<'a> {
-    fn rust_wire_type(&self) -> String {
+    fn rust_wire_type(&self, target: Target) -> String {
         todo!()
     }
 }
 
 impl<'a> WireRustGeneratorRustWireTypeTrait for PrimitiveListWireRustGenerator<'a> {
-    fn rust_wire_type(&self) -> String {
+    fn rust_wire_type(&self, target: Target) -> String {
         todo!()
     }
 }
 
 impl<'a> WireRustGeneratorRustWireTypeTrait for RecordWireRustGenerator<'a> {
-    fn rust_wire_type(&self) -> String {
+    fn rust_wire_type(&self, target: Target) -> String {
         todo!()
     }
 }
 
 impl<'a> WireRustGeneratorRustWireTypeTrait for RustOpaqueWireRustGenerator<'a> {
-    fn rust_wire_type(&self) -> String {
+    fn rust_wire_type(&self, target: Target) -> String {
         todo!()
     }
 }
 
 impl<'a> WireRustGeneratorRustWireTypeTrait for StructRefWireRustGenerator<'a> {
-    fn rust_wire_type(&self) -> String {
+    fn rust_wire_type(&self, target: Target) -> String {
         todo!()
     }
 }
 
 impl<'a> WireRustGeneratorRustWireTypeTrait for UnencodableWireRustGenerator<'a> {
-    fn rust_wire_type(&self) -> String {
+    fn rust_wire_type(&self, target: Target) -> String {
         todo!()
     }
 }
