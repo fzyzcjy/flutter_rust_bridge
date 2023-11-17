7d519c106e3c66a38a09b6bfc3fbacc801307605
fzyzcjy <ch271828n@outlook.com>
2023-11-17 14:10:14 +0800

    refactor: temporarily rm logic handling multi .h file (will have single .h but multi .dart/.rs)

--- frb_codegen/src/library/codegen/generator/wire/c/mod.rs
+++ frb_codegen/src/library/codegen/generator/wire/c/mod.rs
@@ -1,57 +1,7 @@
 use std::path::Path;
 
-use crate::config::opts::Opts;
-use crate::utils::misc::{BlockIndex, PathExt};
-
-pub fn generate_dummy(
-    config: &Opts,
-    all_configs: &[Opts],
-    func_names: &[String],
-    c_path_index: usize,
-) -> String {
-    if all_configs.len() > 1 {
-        let basic_dummy_func = get_dummy_func(&config.class_name, func_names);
-        if config.block_index == BlockIndex(0) {
-            let func_names = all_configs
-                .iter()
-                .map(|e| get_dummy_signature(&e.class_name))
-                .collect_vec();
-
-            let other_headers = all_configs
-                .iter()
-                .skip(1)
-                .map(|e| {
-                    // get directory only from paths
-                    let src_p = Path::new(&config.c_output_path[c_path_index]);
-                    let dst_p = Path::new(&e.c_output_path[c_path_index]);
-                    // get reletive path and header file name
-                    let relative_p =
-                        src_p.get_relative_path_to(dst_p.directory_name_str().unwrap(), true);
-                    let header_file_name = dst_p.file_name_str().unwrap();
-                    // final string for importing
-                    format!(
-                        r#"#include "{}""#,
-                        Path::join(relative_p.as_ref(), header_file_name)
-                            .into_os_string()
-                            .into_string()
-                            .unwrap()
-                    )
-                })
-                .collect_vec()
-                .join("\n");
-
-            format!(
-                "{}\n{}\n{}",
-                basic_dummy_func,
-                other_headers,
-                get_dummy_func("", &func_names)
-            )
-        } else {
-            basic_dummy_func
-        }
-    } else {
-        get_dummy_func("", func_names)
-    }
+fn generate_dummy_function(func_names: &[String]) -> String {
+    get_dummy_func("", func_names)
 }
 
 fn get_dummy_func(api_block_name: &str, func_names: &[String]) -> String {
