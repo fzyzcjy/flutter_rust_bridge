43b29e063cfd1babe6291f13c919829447bfa001
fzyzcjy <ch271828n@outlook.com>
2023-11-17 21:47:45 +0800

    chore: mv

--- frb_codegen/src/library/codegen/generator/wire/rust/common/mod.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/common/mod.rs
@@ -1,1 +1,23 @@
+use crate::codegen::generator::wire::rust::base::{WireRustGenerator, WireRustGeneratorContext};
+use crate::codegen::ir::ty::IrType;
+
 pub(crate) mod ty;
+
+pub(crate) fn generate_wrapper_struct(
+    ty: &IrType,
+    context: WireRustGeneratorContext,
+) -> Option<String> {
+    // the generated wrapper structs need to be public for the StreamSinkTrait impl to work
+    WireRustGenerator::new(ty.clone(), context)
+        .wrapper_struct_name()
+        .map(|wrapper_struct_name| {
+            format!(
+                r###"
+                #[derive(Clone)]
+                pub struct {}({});
+                "###,
+                wrapper_struct_name,
+                ty.rust_api_type(),
+            )
+        })
+}
--- frb_codegen/src/library/codegen/generator/wire/rust/common/ty/mod.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/common/ty/mod.rs
@@ -26,22 +26,3 @@ pub(crate) trait WireRustGeneratorCommonTrait {
         None
     }
 }
-
-pub(crate) fn generate_wrapper_struct(
-    ty: &IrType,
-    context: WireRustGeneratorContext,
-) -> Option<String> {
-    // the generated wrapper structs need to be public for the StreamSinkTrait impl to work
-    WireRustGenerator::new(ty.clone(), context)
-        .wrapper_struct_name()
-        .map(|wrapper_struct_name| {
-            format!(
-                r###"
-                #[derive(Clone)]
-                pub struct {}({});
-                "###,
-                wrapper_struct_name,
-                ty.rust_api_type(),
-            )
-        })
-}
