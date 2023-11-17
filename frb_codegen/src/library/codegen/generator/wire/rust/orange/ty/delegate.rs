ab7c9267212b10e569b9566f82db56697bda6c1b
fzyzcjy <ch271828n@outlook.com>
2023-11-17 21:13:14 +0800

    chore: temp further rename

--- frb_codegen/src/library/codegen/generator/wire/rust/orange/ty/delegate.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/orange/ty/delegate.rs
@@ -1,5 +1,5 @@
 use crate::codegen::generator::wire::rust::base::*;
-use crate::codegen::generator::wire::rust::common::ty::WireRustGeneratorOrangeTrait;
+use crate::codegen::generator::wire::rust::orange::ty::WireRustGeneratorOrangeTrait;
 use crate::codegen::ir::ty::delegate::IrTypeDelegate;
 
 impl<'a> WireRustGeneratorOrangeTrait for DelegateWireRustGenerator<'a> {}
