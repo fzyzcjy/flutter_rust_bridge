23b63a7a29fc02fb264d7abb2b6946d559e5ad4c
fzyzcjy <ch271828n@outlook.com>
2023-11-17 21:37:41 +0800

    Revert "chore: rm api2wire"
    This reverts commit 0e6b3b409f5c42f9e0e12010813801bd22f5b473.

---
+++ frb_codegen/src/library/codegen/generator/wire/rust/api2wire/ty/general_list.rs
@@ -0,0 +1,4 @@
+use crate::codegen::generator::wire::rust::api2wire::ty::WireRustGeneratorApi2wireTrait;
+use crate::codegen::generator::wire::rust::base::*;
+
+impl<'a> WireRustGeneratorApi2wireTrait for GeneralListWireRustGenerator<'a> {}
