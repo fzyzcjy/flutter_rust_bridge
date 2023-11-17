7b9d5beca2f9af5f1c0cf000b03c0dca3e307283
fzyzcjy <ch271828n@outlook.com>
2023-11-17 20:56:05 +0800

    chore: temp rm `api2wire`

--- frb_codegen/src/library/codegen/generator/wire/rust/api2wire/ty/boxed.rs
+++
@@ -1,8 +0,0 @@
-use crate::codegen::generator::wire::rust::api2wire::ty::WireRustGeneratorApi2wireTrait;
-use crate::codegen::generator::wire::rust::base::*;
-
-impl<'a> WireRustGeneratorApi2wireTrait for BoxedWireRustGenerator<'a> {
-    fn hello(&self) -> Option<String> {
-        todo!()
-    }
-}
