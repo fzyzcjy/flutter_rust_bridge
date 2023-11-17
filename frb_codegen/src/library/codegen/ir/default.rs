1107a4b0b342e1d0439b5195491f1ef82b079cb9
fzyzcjy <ch271828n@outlook.com>
2023-11-17 09:11:22 +0800

    fix: compile

--- frb_codegen/src/library/codegen/ir/default.rs
+++ frb_codegen/src/library/codegen/ir/default.rs
@@ -1,11 +1,11 @@
 use std::borrow::Cow;
 
-// crate::ir! {
+crate::ir! {
 pub enum IrDefaultValue {
     String { content: String },
     Others { dart_literal: String },
 }
-// }
+}
 
 impl IrDefaultValue {
     pub(crate) fn to_dart_literal(&self) -> Cow<str> {
