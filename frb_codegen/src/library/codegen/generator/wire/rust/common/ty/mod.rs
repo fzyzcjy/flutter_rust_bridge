0aac3c6acda85799d5a46d49f15e0ef069d82f87
fzyzcjy <ch271828n@outlook.com>
2023-11-17 20:29:30 +0800

    feat: generate_wrapper_struct

--- frb_codegen/src/library/codegen/generator/wire/rust/common/ty/mod.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/common/ty/mod.rs
@@ -15,6 +15,8 @@ mod rust_opaque;
 mod structure;
 mod unencodable;
 
+use crate::codegen::ir::pack::IrPack;
+use crate::codegen::ir::ty::IrType;
 use enum_dispatch::enum_dispatch;
 
 #[enum_dispatch]
@@ -22,4 +24,23 @@ pub(crate) trait WireRustGeneratorCommonTrait {
     fn wrapper_struct_name(&self) -> Option<String> {
         None
     }
+}
+
+pub(crate) fn generate_wrapper_struct(
+    ty: &IrType,
+    context: WireRustGeneratorContext,
+) -> Option<String> {
+    // the generated wrapper structs need to be public for the StreamSinkTrait impl to work
+    WireRustGenerator::new(ty.clone(), context)
+        .wrapper_struct_name()
+        .map(|wrapper_struct_name| {
+            format!(
+                r###"
+                #[derive(Clone)]
+                pub struct {}({});
+                "###,
+                wrapper_struct_name,
+                ty.rust_api_type(),
+            )
+        })
 }
--- frb_codegen/src/old/generator/rust/mod.rs
+++ frb_codegen/src/old/generator/rust/mod.rs
@@ -481,20 +481,6 @@ impl<'a> Generator<'a> {
         TypeRustGenerator::new(ty.clone(), ir_pack, self.config).static_checks()
     }
 
-    fn generate_wrapper_struct_name(&mut self, ty: &IrType, ir_pack: &IrPack) -> Option<String> {
-        // the generated wrapper structs need to be public for the StreamSinkTrait impl to work
-        match ty {
-            IrType::StructRef(_)
-            | IrType::EnumRef(_)
-            | IrType::Delegate(IrTypeDelegate::PrimitiveEnum { .. }) => {
-                TypeRustGenerator::new(ty.clone(), ir_pack, self.config)
-                    .wrapper_struct_name()
-                    .map(|wrapper| generate_wrapper_struct_from_name())
-            }
-            _ => None,
-        }
-    }
-
     fn generate_new_with_nullptr_misc(&self) -> &'static str {
         "pub trait NewWithNullPtr {
             fn new_with_null_ptr() -> Self;
