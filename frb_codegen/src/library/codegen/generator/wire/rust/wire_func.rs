ca501e36cb2b1f27c11f8773513d3a9f6a30837a
fzyzcjy <ch271828n@outlook.com>
2023-11-18 13:47:47 +0800

    fix: compile

--- frb_codegen/src/library/codegen/generator/wire/rust/wire_func.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/wire_func.rs
@@ -10,6 +10,7 @@ use crate::codegen::ir::func::{
     IrFunc, IrFuncMode, IrFuncOwnerInfo, IrFuncOwnerInfoMethod, IrFuncOwnerInfoMethodMode,
 };
 use crate::codegen::ir::pack::IrPack;
+use crate::library::codegen::generator::wire::rust::api2wire::ty::WireRustGeneratorApi2wireTrait;
 use crate::library::codegen::generator::wire::rust::info::WireRustGeneratorInfoTrait;
 use crate::library::codegen::ir::ty::IrTypeTrait;
 use crate::misc::consts::HANDLER_NAME;
