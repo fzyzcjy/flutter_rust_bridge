c273317ee8775824bd07b83abcf031ba61755bd3
fzyzcjy <ch271828n@outlook.com>
2023-11-18 10:18:32 +0800

    chore: more mv

--- frb_codegen/src/library/codegen/generator/wire/rust/wire2api/ty/rust_opaque.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/wire2api/ty/rust_opaque.rs
@@ -72,4 +72,37 @@ impl<'a> WireRustGeneratorWire2apiTrait for RustOpaqueWireRustGenerator<'a> {
             ..Default::default()
         }
     }
+
+    fn generate_related_funcs(&self) -> Acc<Option<CodeWithExternFunc>> {
+        let mut generate_impl = |target| {
+            [
+                collector.generate(
+                    &format!("drop_opaque_{}", self.ir.safe_ident()),
+                    [("ptr: *const c_void", "")],
+                    None,
+                    &format!(
+                        "unsafe {{Arc::<{}>::decrement_strong_count(ptr as _);}}",
+                        self.ir.inner_rust
+                    ),
+                    target,
+                ),
+                collector.generate(
+                    &format!("share_opaque_{}", self.ir.safe_ident()),
+                    [("ptr: *const c_void", "")],
+                    Some("*const c_void"),
+                    &format!(
+                        "unsafe {{Arc::<{}>::increment_strong_count(ptr as _); ptr}}",
+                        self.ir.inner_rust
+                    ),
+                    target,
+                ),
+            ]
+            .join("\n")
+        };
+        Acc {
+            io: Some(generate_impl(Target::Io)),
+            wasm: Some(generate_impl(Target::Wasm)),
+            ..Default::default()
+        }
+    }
 }
--- frb_codegen/src/old/generator/rust/ty_rust_opaque.rs
+++ frb_codegen/src/old/generator/rust/ty_rust_opaque.rs
@@ -10,37 +10,4 @@ use super::{ExternFuncCollector, NO_PARAMS};
 
 type_rust_generator_struct!(TypeRustOpaqueGenerator, IrTypeRustOpaque);
 
-impl TypeRustGeneratorTrait for TypeRustOpaqueGenerator<'_> {
-    fn generate_related_funcs(&self) -> Acc<Option<CodeWithExternFunc>> {
-        let mut generate_impl = |target| {
-            [
-                collector.generate(
-                    &format!("drop_opaque_{}", self.ir.safe_ident()),
-                    [("ptr: *const c_void", "")],
-                    None,
-                    &format!(
-                        "unsafe {{Arc::<{}>::decrement_strong_count(ptr as _);}}",
-                        self.ir.inner_rust
-                    ),
-                    target,
-                ),
-                collector.generate(
-                    &format!("share_opaque_{}", self.ir.safe_ident()),
-                    [("ptr: *const c_void", "")],
-                    Some("*const c_void"),
-                    &format!(
-                        "unsafe {{Arc::<{}>::increment_strong_count(ptr as _); ptr}}",
-                        self.ir.inner_rust
-                    ),
-                    target,
-                ),
-            ]
-            .join("\n")
-        };
-        Acc {
-            io: Some(generate_impl(Target::Io)),
-            wasm: Some(generate_impl(Target::Wasm)),
-            ..Default::default()
-        }
-    }
-}
+impl TypeRustGeneratorTrait for TypeRustOpaqueGenerator<'_> {}
