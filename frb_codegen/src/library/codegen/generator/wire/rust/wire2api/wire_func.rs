8f58fece821b499dbe4c27f9e82704a583d82d63
fzyzcjy <ch271828n@outlook.com>
2023-11-18 12:52:50 +0800

    refactor: more

--- frb_codegen/src/library/codegen/generator/wire/rust/wire2api/wire_func.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/wire2api/wire_func.rs
@@ -1,92 +1,30 @@
 use crate::codegen::generator::acc::Acc;
-use crate::codegen::generator::misc::Target;
-use crate::codegen::generator::wire::rust::wire2api::extern_func::ExternFunc;
+use crate::codegen::generator::misc::{Target, TargetOrCommon};
+use crate::codegen::generator::wire::rust::wire2api::extern_func::{ExternFunc, ExternFuncParam};
 use crate::codegen::ir::func::{IrFunc, IrFuncMode};
 use crate::codegen::ir::pack::IrPack;
 use itertools::Itertools;
+use std::convert::TryInto;
 
 pub(crate) fn generate_wire_func(func: &IrFunc, ir_pack: &IrPack) -> Acc<String> {
-    let mut params = if func.mode.has_port_argument() {
-        Acc {
-            io: vec!["port_: i64".to_owned()],
-            wasm: vec!["port_: MessagePort".to_owned()],
-            common: vec!["port_: MessagePort".to_owned()],
-        }
-    } else {
-        Acc::default()
-    };
-
-    params += func
-        .inputs
-        .iter()
-        .map(|field| {
-            let name = field.name.rust_style();
-            Acc::new(|target| match target {
-                Common => format!(
-                    "{}: impl Wire2Api<{}> + UnwindSafe",
-                    name,
-                    field.ty.rust_api_type()
-                ),
-                Io | Wasm => format!(
-                    "{}: {}{}",
-                    name,
-                    field.ty.rust_wire_modifier(target),
-                    field.ty.rust_wire_type(target)
-                ),
-            })
-        })
-        .collect();
-
-    let mut inner_func_params = [
-        vec![],
-        func.inputs
-            .iter()
-            .map(|field| format!("api_{}", field.name.rust_style()))
-            .collect_vec(),
-    ]
-    .concat();
-    if let IrFuncMode::Stream { argument_index } = func.mode {
-        inner_func_params.insert(
-            argument_index,
-            format!(
-                "task_callback.stream_sink::<_,{}>()",
-                func.output.intodart_type(ir_pack)
-            ),
-        );
-    }
-    let wrap_info_obj = format!(
-        "WrapInfo{{ debug_name: \"{}\", port: {}, mode: FfiCallMode::{} }}",
-        func.name,
-        if func.mode.has_port_argument() {
-            "Some(port_)"
-        } else {
-            "None"
-        },
-        func.mode.ffi_call_mode(),
-    );
-
-    let code_wire2api = func
-        .inputs
-        .iter()
-        .map(|field| format!("let api_{0} = {0}.wire2api();", field.name.rust_style()))
-        .collect_vec()
-        .join("");
+    let params = generate_params(func);
+    let inner_func_params = generate_inner_func_params(func, ir_pack);
+    let wrap_info_obj = generate_wrap_info_obj(func);
+    let code_wire2api = generate_code_wire2api(func);
 
     let code_call_inner_func = if func.owner.is_non_static_method() || func.owner.is_static_method()
     {
         let method_name = if func.owner.is_non_static_method() {
             inner_func_params[0] = format!("&{}", inner_func_params[0]);
-            FunctionName::deserialize(&func.name).method_name()
+            func.owner.method_name()
         } else if func.owner.is_static_method() {
-            FunctionName::deserialize(&func.name)
-                .static_method_name()
-                .unwrap()
+            func.owner.static_method_name().unwrap()
         } else {
             panic!("{} is not a method, nor a static method.", func.name)
         };
         format!(
             r"{}::{}({})",
-            struct_name.unwrap(),
+            func.owner.struct_name.unwrap(),
             method_name,
             inner_func_params.join(", ")
         )
@@ -136,21 +74,23 @@ pub(crate) fn generate_wire_func(func: &IrFunc, ir_pack: &IrPack) -> Acc<String>
     );
 
     Acc::new(|target| match target {
-        Io | Wasm => ExternFunc {
-            func_name: func.wire_func_name(),
-            params: if target == Target::Wasm {
-                &params.wasm[..]
-            } else {
-                &params.io[..]
+        TargetOrCommon::Io | TargetOrCommon::Wasm => {
+            let target = target.try_into().unwrap();
+            ExternFunc {
+                func_name: func.wire_func_name(),
+                params: match target {
+                    Target::Wasm => params.wasm,
+                    Target::Io => params.io,
+                }
+                .iter()
+                .cloned()
+                .collect_vec(),
+                return_type,
+                body: redirect_body,
+                target,
             }
-            .iter()
-            .map(|item| (item, ""))
-            .collect_vec(),
-            return_type,
-            body: &redirect_body,
-            target,
-        },
-        Common => format!(
+        }
+        TargetOrCommon::Common => format!(
             "fn {}_impl({}) {} {{ {} }}",
             func.wire_func_name(),
             params.common.join(","),
@@ -158,4 +98,87 @@ pub(crate) fn generate_wire_func(func: &IrFunc, ir_pack: &IrPack) -> Acc<String>
             body,
         ),
     })
+}
+
+fn generate_inner_func_params(func: &IrFunc, ir_pack: &IrPack) -> Vec<String> {
+    let mut ans = func
+        .inputs
+        .iter()
+        .map(|field| format!("api_{}", field.name.rust_style()))
+        .collect_vec();
+
+    if let IrFuncMode::Stream { argument_index } = func.mode {
+        ans.insert(
+            argument_index,
+            format!(
+                "task_callback.stream_sink::<_,{}>()",
+                func.output.intodart_type(ir_pack)
+            ),
+        );
+    }
+
+    ans
+}
+
+fn generate_params(func: &IrFunc) -> Acc<Vec<ExternFuncParam>> {
+    let mut params = if func.mode.has_port_argument() {
+        Acc::new(|target| {
+            let rust_type = match target {
+                TargetOrCommon::Io => "i64",
+                TargetOrCommon::Common | TargetOrCommon::Wasm => "MessagePort",
+            }
+            .to_owned();
+            vec![ExternFuncParam {
+                name: "port_".to_owned(),
+                rust_type,
+                dart_type: None,
+            }]
+        })
+    } else {
+        Acc::default()
+    };
+
+    params += func
+        .inputs
+        .iter()
+        .map(|field| {
+            let name = field.name.rust_style();
+            Acc::new(|target| match target {
+                TargetOrCommon::Common => format!(
+                    "{}: impl Wire2Api<{}> + UnwindSafe",
+                    name,
+                    field.ty.rust_api_type()
+                ),
+                TargetOrCommon::Io | TargetOrCommon::Wasm => format!(
+                    "{}: {}{}",
+                    name,
+                    field.ty.rust_wire_modifier(target),
+                    field.ty.rust_wire_type(target)
+                ),
+            })
+        })
+        .collect();
+
+    params
+}
+
+fn generate_wrap_info_obj(func: &IrFunc) -> String {
+    format!(
+        "WrapInfo{{ debug_name: \"{}\", port: {}, mode: FfiCallMode::{} }}",
+        func.name,
+        if func.mode.has_port_argument() {
+            "Some(port_)"
+        } else {
+            "None"
+        },
+        func.mode.ffi_call_mode(),
+    )
+}
+
+fn generate_code_wire2api(func: &IrFunc) -> String {
+    func.inputs
+        .iter()
+        .map(|field| format!("let api_{0} = {0}.wire2api();", field.name.rust_style()))
+        .collect_vec()
+        .join("")
 }
