57db3844c40ca83febc1e5d488e7e75ca48d0b96
fzyzcjy <ch271828n@outlook.com>
2023-11-18 17:00:14 +0800

    feat: wire_dart_misc

---
+++ frb_codegen/src/library/codegen/generator/wire/dart/misc/ty/rust_opaque.rs
@@ -0,0 +1,4 @@
+use crate::codegen::generator::wire::dart::base::*;
+use crate::codegen::generator::wire::dart::misc::ty::WireDartGeneratorMiscTrait;
+
+impl<'a> WireDartGeneratorMiscTrait for RustOpaqueWireDartGenerator<'a> {}
--- frb_codegen/src/library/codegen/generator/wire/dart/mod.rs
+++ frb_codegen/src/library/codegen/generator/wire/dart/mod.rs
@@ -1,2 +1,3 @@
 pub(crate) mod base;
 pub(crate) mod info;
+pub(crate) mod misc;
