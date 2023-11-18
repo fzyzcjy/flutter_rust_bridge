eeab4897941aa5f467a15fa315e656a691e424d4
fzyzcjy <ch271828n@outlook.com>
2023-11-18 22:07:26 +0800

    fix: typo

--- frb_codegen/src/library/commands/format_rust.rs
+++ frb_codegen/src/library/commands/format_rust.rs
@@ -8,7 +8,7 @@ pub fn format_rust(path: &[PathBuf]) -> anyhow::Result<()> {
     let res = execute_command("rustfmt", path, None)?;
     if !res.status.success() {
         bail!(
-            "Dart formatting failed: {}",
+            "Rust formatting failed: {}",
             String::from_utf8_lossy(&res.stderr),
         )
     }
