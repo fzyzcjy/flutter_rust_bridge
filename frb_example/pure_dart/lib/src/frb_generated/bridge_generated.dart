c0ba719d8ed14895600420f9553a3ea6e39694f6
fzyzcjy <ch271828n@outlook.com>
2023-11-19 22:04:52 +0800

    chore: codegen

--- frb_example/pure_dart/lib/src/frb_generated/bridge_generated.dart
+++ frb_example/pure_dart/lib/src/frb_generated/bridge_generated.dart
@@ -1,8 +1,12 @@
-abstract class BridgeGenerated {
-            Future<int> simpleAdder({ required int a , required int b , dynamic hint });
 
-FlutterRustBridgeTaskConstMeta get kSimpleAdderConstMeta;
+
+// Section: c_binding
+// Section: impl_wire2api
+int _wire2api_i_32(dynamic raw) {
+            return raw as int;
         }
-
-        
-        
+        // Section: api2wire_funcs
+@protected
+                int api2wire_i_32(int raw) {
+                    return raw;
+                }// Section: api_fill_to_wire_funcs
--- frb_example/pure_dart/rust/src/bridge_generated.io.rs
+++ frb_example/pure_dart/rust/src/bridge_generated.io.rs
@@ -1,16 +1,4 @@
 use super::*;
-// Section: file_attributes
-// Section: code_header
-// Section: imports
-// Section: wire_funcs
-// Section: wrapper_structs
-// Section: static_checks
-// Section: executor
-// Section: allocate_funcs
-// Section: related_funcs
-// Section: impl_wire2api
-// Section: wire2api_class
-// Section: impl_new_with_nullptr
 pub trait NewWithNullPtr {
         fn new_with_null_ptr() -> Self;
     }
@@ -20,8 +8,7 @@ pub trait NewWithNullPtr {
             std::ptr::null_mut()
         }
     }
-    // Section: impl_into_dart
-
+    
 
                 #[no_mangle]
                 pub extern "C" fn wire_simple_adder(port_: i64, a: i32, b: i32)  {
