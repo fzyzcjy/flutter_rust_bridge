068fee0f9304226f54f34b7627550c197f0bcbc3
fzyzcjy <ch271828n@outlook.com>
2023-11-19 20:58:07 +0800

    chore: rm comments

--- frb_codegen/src/library/codegen/generator/wire/rust/text_generator.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/text_generator.rs
@@ -11,7 +11,6 @@ use strum::IntoEnumIterator;
 // and we want to emphasize we are generating final output text here.
 pub(super) struct WireRustOutputText {
     pub(super) text: Acc<Option<String>>,
-    // TODO originally from: `generated_rust.extern_func_names`
     pub(super) extern_func_names: Vec<String>,
 }
 
