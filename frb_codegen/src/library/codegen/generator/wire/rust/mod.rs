e599cc80eaa30ce60631662667893e8da5c7bcbf
fzyzcjy <ch271828n@outlook.com>
2023-11-19 09:41:45 +0800

    refactor: mv entrypoint/rust

--- frb_codegen/src/library/codegen/generator/wire/rust/mod.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/mod.rs
@@ -14,7 +14,26 @@ pub(crate) mod spec_generator;
 mod text_generator;
 
 pub(crate) fn generate(ir_pack: &IrPack, context: WireRustGeneratorContext) -> anyhow::Result<()> {
+    let rust_output_dir = Path::new(&rust_output_paths.base_path).parent().unwrap();
+    fs::create_dir_all(rust_output_dir)?;
+
     let spec = spec_generator::generate(ir_pack, context);
     let text = text_generator::generate(spec, context.config)?;
-    emitter::emit(text, context.config)
+    emitter::emit(text, context.config)?;
+
+    if !config.skip_add_mod_to_lib {
+        others::try_add_mod_to_lib(&config.rust_crate_dir, &config.rust_output_path);
+    }
+
+    command_run!(
+        commands::format_rust,
+        &config.rust_output_path,
+        (
+            config.wasm_enabled,
+            config.rust_io_output_path(),
+            config.rust_wasm_output_path(),
+        )
+    )?;
+
+    Ok(())
 }
