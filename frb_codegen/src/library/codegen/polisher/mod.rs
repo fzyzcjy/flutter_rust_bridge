d0f515c20ba51bc3e99645c3f96fc40fe25de6a5
fzyzcjy <ch271828n@outlook.com>
2023-11-19 22:12:45 +0800

    chore: uncomment

--- frb_codegen/src/library/codegen/polisher/mod.rs
+++ frb_codegen/src/library/codegen/polisher/mod.rs
@@ -4,6 +4,7 @@ use crate::codegen::polisher::internal_config::PolisherInternalConfig;
 use crate::command_run;
 use crate::commands::format_rust::format_rust;
 use crate::library::commands::dart_build_runner::dart_build_runner;
+use crate::library::commands::format_dart::format_dart;
 use std::fs;
 
 pub(crate) mod add_mod_to_lib;
@@ -29,22 +30,21 @@ fn execute_build_runner(
     dart_build_runner(&config.dart_root)
 }
 
-fn execute_dart_format(_config: &PolisherInternalConfig) -> anyhow::Result<()> {
-    todo!()
-    // command_run!(
-    //     format_dart[config.dart_format_line_length],
-    //     &dart_output_paths.base_path,
-    //     ?config.dart_decl_output_path,
-    //     (
-    //         config.wasm_enabled,
-    //         dart_output_paths.wasm_path,
-    //         dart_output_paths.io_path,
-    //     ),
-    //     (
-    //         needs_freezed && config.build_runner,
-    //         config.dart_freezed_path(),
-    //     )
-    // )
+fn execute_dart_format(config: &PolisherInternalConfig) -> anyhow::Result<()> {
+    command_run!(
+        format_dart[config.dart_format_line_length],
+        &dart_output_paths.base_path,
+        ?config.dart_decl_output_path,
+        (
+            config.wasm_enabled,
+            dart_output_paths.wasm_path,
+            dart_output_paths.io_path,
+        ),
+        (
+            needs_freezed && config.build_runner,
+            config.dart_freezed_path(),
+        )
+    )
 }
 
 fn execute_rust_format(config: &PolisherInternalConfig) -> anyhow::Result<()> {
