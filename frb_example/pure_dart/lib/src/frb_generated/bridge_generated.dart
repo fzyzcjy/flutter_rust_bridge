598214409254233262f00142eb77f19b31357519
fzyzcjy <ch271828n@outlook.com>
2023-11-19 20:37:34 +0800

    chore: more codegen

---
+++ frb_example/pure_dart/lib/src/frb_generated/bridge_generated.dart
@@ -0,0 +1,8 @@
+abstract class BridgeGenerated {
+            Future<int> simpleAdder({ required int a , required int b , dynamic hint });
+
+FlutterRustBridgeTaskConstMeta get kSimpleAdderConstMeta;
+        }
+
+        
+        
