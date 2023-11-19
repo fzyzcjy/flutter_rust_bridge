7962c1eb6e482c05eea077a5c85b903d627cbca1
fzyzcjy <ch271828n@outlook.com>
2023-11-19 12:15:37 +0800

    fix: compile

--- frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/api2wire/misc.rs
+++ frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/api2wire/misc.rs
@@ -1,9 +1,10 @@
 use crate::codegen::generator::misc::Target;
 use crate::codegen::generator::wire::dart::spec_generator::base::WireDartGeneratorImplTrait;
 use crate::codegen::generator::wire::rust::spec_generator::base::WireRustGenerator;
+use crate::library::codegen::generator::wire::rust::spec_generator::wire2api::ty::WireRustGeneratorWire2apiTrait;
 
 pub(super) fn dart_wire_type_from_rust_wire_type_or_wasm(
-    that: impl WireDartGeneratorImplTrait,
+    that: &impl WireDartGeneratorImplTrait,
     target: Target,
     wasm_type: String,
 ) -> String {
