d1aa3a521f81f3bb4dc91a45878bc6137cf55172
fzyzcjy <ch271828n@outlook.com>
2023-11-19 09:51:15 +0800

    chore: result

--- frb_codegen/src/library/codegen/generator/wire/c/mod.rs
+++ frb_codegen/src/library/codegen/generator/wire/c/mod.rs
@@ -2,6 +2,6 @@ mod dummy_function;
 
 use crate::codegen::ir::pack::IrPack;
 
-pub(crate) fn generate(ir_pack: &IrPack) {
+pub(crate) fn generate(ir_pack: &IrPack) -> anyhow::Result<()> {
     todo!()
 }
