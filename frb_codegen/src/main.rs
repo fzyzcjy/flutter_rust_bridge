c75af7d16076d68d42137d24c342ed78988aa484
fzyzcjy <ch271828n@outlook.com>
2023-11-19 16:29:21 +0800

    fix: logging

--- frb_codegen/src/main.rs
+++ frb_codegen/src/main.rs
@@ -11,12 +11,13 @@ use lib_flutter_rust_bridge_codegen::*;
 use log::debug;
 
 fn main() -> anyhow::Result<()> {
-    main_given_cli(Cli::parse())
+    let cli = Cli::parse();
+    configure_opinionated_logging("./logs/", cli.verbose)?;
+    main_given_cli(cli)
 }
 
+// Only use as entrypoint of tests
 fn main_given_cli(cli: Cli) -> anyhow::Result<()> {
-    configure_opinionated_logging("./logs/", cli.verbose)?;
-
     debug!("cli={cli:?}");
     match cli.command {
         Commands::Generate(args) => codegen::generate(compute_codegen_config(args)?)?,
@@ -32,11 +33,13 @@ mod tests {
     use crate::binary::test_utils::set_cwd_test_fixture;
     use crate::main_given_cli;
     use clap::Parser;
+    use lib_flutter_rust_bridge_codegen::utils::logs::configure_opinionated_test_logging;
     use serial_test::serial;
 
     #[test]
     #[serial]
     fn test_execute_generate_on_frb_example_pure_dart() -> anyhow::Result<()> {
+        configure_opinionated_test_logging();
         set_cwd_test_fixture("../../frb_example/pure_dart")?;
         main_given_cli(Cli::parse_from(vec!["", "generate"]))
     }
