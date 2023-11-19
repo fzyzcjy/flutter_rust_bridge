ae5a8480176478686dbaedd14f72560b0e8f70c4
fzyzcjy <ch271828n@outlook.com>
2023-11-19 17:24:42 +0800

    refactor: extract

--- frb_codegen/src/library/codegen/generator/wire/c/mod.rs
+++ frb_codegen/src/library/codegen/generator/wire/c/mod.rs
@@ -1,21 +1,12 @@
 mod cbindgen_executor;
 mod dummy_function;
+pub(crate) mod internal_config;
 
+use crate::codegen::generator::wire::c::internal_config::GeneratorWireCInternalConfig;
 use crate::codegen::ir::pack::IrPack;
 use crate::library::commands::cbindgen::{cbindgen, CbindgenArgs};
 use crate::utils::file_utils::{create_dir_all_and_write, temp_change_file};
 use std::path::{Path, PathBuf};
-
-// TODO unify with `GeneratorCInternalConfig` config
-pub(crate) struct Config {
-    pub(crate) rust_crate_dir: PathBuf,
-    pub(crate) rust_output_path: PathBuf,
-    pub(crate) c_output_path: PathBuf,
-    // TODO originally from: `generated_rust.extern_func_names`
-    pub(crate) extern_func_names: Vec<String>,
-    // TODO originally created via `get_c_struct_names`, should calc it from wire-rust layer, in analogy to `extern_func_names`
-    pub(crate) extern_struct_names: Vec<String>,
-}
 
 // TODO
 // fn get_c_struct_names(ir_pack: &IrPack) -> Vec<String> {
@@ -32,7 +23,10 @@ pub(crate) struct Config {
 //         .collect()
 // }
 
-pub(crate) fn generate(ir_pack: &IrPack, config: &Config) -> anyhow::Result<()> {
+pub(crate) fn generate(
+    ir_pack: &IrPack,
+    config: &GeneratorWireCInternalConfig,
+) -> anyhow::Result<()> {
     let code_cbindgen = cbindgen_executor::execute(ir_pack, config)?;
     let code_dummy = dummy_function::generate(config);
     emit(code_cbindgen, &code_dummy, &config.c_output_path)?;
