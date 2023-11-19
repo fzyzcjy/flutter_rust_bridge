c6ca4723d802dafe945299c9eb9bda0d3591fd7b
fzyzcjy <ch271828n@outlook.com>
2023-11-19 18:35:13 +0800

    feat: more

--- frb_codegen/src/library/codegen/generator/wire/rust/misc.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/misc.rs
@@ -3,26 +3,25 @@ use crate::codegen::generator::wire::rust::spec_generator::WireRustOutputSpec;
 use crate::codegen::generator::wire::rust::GeneratorWireRustOutput;
 use crate::codegen::ir::pack::IrPack;
 use crate::codegen::ir::ty::IrType;
+use crate::if_then_some;
 
-pub(super) fn compute_output(spec: &WireRustOutputSpec) -> GeneratorWireRustOutput {
+pub(super) fn compute_output(
+    spec: &WireRustOutputSpec,
+    ir_pack: &IrPack,
+) -> GeneratorWireRustOutput {
     GeneratorWireRustOutput {
         // TODO originally from: `generated_rust.extern_func_names`
         extern_func_names: TODO,
-        // TODO originally created via `get_c_struct_names`, should calc it from wire-rust layer, in analogy to `extern_func_names`
-        extern_struct_names: TODO,
+        extern_struct_names: get_c_struct_names(ir_pack),
     }
 }
 
 fn get_c_struct_names(ir_pack: &IrPack) -> Vec<String> {
-    ir_pack
-        .distinct_types(true, true)
+    let distinct_types = ir_pack.distinct_types(true, true);
+
+    distinct_types
         .iter()
-        .filter_map(|ty| {
-            if let IrType::StructRef(_) = ty {
-                Some(ty.rust_wire_type(Target::Io))
-            } else {
-                None
-            }
-        })
+        .filter(|ty| matches!(&ty, IrType::StructRef(_)))
+        .map(|ty| ty.rust_wire_type(Target::Io))
         .collect()
 }
