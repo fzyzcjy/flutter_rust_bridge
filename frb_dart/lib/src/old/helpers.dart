50ebd530d68a352a410b7a524fa28f0f0ef9ff48
fzyzcjy <ch271828n@outlook.com>
2023-11-20 20:40:19 +0800

    chore: mapNonNull

--- frb_dart/lib/src/old/helpers.dart
+++ frb_dart/lib/src/old/helpers.dart
@@ -50,12 +50,3 @@ class FlutterRustBridgeTimeoutException {
   String toString() =>
       'FlutterRustBridgeTimeoutException(debugName=$debugName, duration=$duration, stackTrace=$stackTrace)';
 }
-
-List<T?> mapNonNull<T, I>(List<I?> items, T Function(I) mapper) {
-  final out = List<T?>.filled(items.length, null);
-  for (var i = 0; i < items.length; ++i) {
-    final item = items[i];
-    if (item != null) out[i] = mapper(item);
-  }
-  return out;
-}
---
+++ frb_dart/lib/src/utils/misc_utils.dart
@@ -0,0 +1,8 @@
+List<T?> mapNonNull<T, I>(List<I?> items, T Function(I) mapper) {
+  final out = List<T?>.filled(items.length, null);
+  for (var i = 0; i < items.length; ++i) {
+    final item = items[i];
+    if (item != null) out[i] = mapper(item);
+  }
+  return out;
+}
