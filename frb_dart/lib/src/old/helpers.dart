228887e6799b92b0c773e15b2d09ca5638dcbc46
fzyzcjy <ch271828n@outlook.com>
2023-11-20 20:39:09 +0800

    refactor: rm FlutterRustBridgeSetupMixin, since can directly setup when call `Rust.init`

--- frb_dart/lib/src/old/helpers.dart
+++ frb_dart/lib/src/old/helpers.dart
@@ -8,65 +8,6 @@ import 'package:uuid/uuid.dart';
 
 export '../ffi.dart';
 
-/// Allow custom setup hooks before ffi can be executed.
-/// All other ffi calls will wait (async) until the setup ffi finishes.
-///
-/// Usage:
-///
-/// 1. Please call [setupMixinConstructor] inside the constructor of your class.
-/// 2. Inside your [setup], please call ffi functions with hint=[kHintSetup].
-mixin FlutterRustBridgeSetupMixin<T extends FlutterRustBridgeWireBase> on FlutterRustBridgeBase<T> {
-  /// Inside your [setup], please call ffi functions with hint=[kHintSetup].
-  static const kHintSetup = _FlutterRustBridgeSetupMixinSkipWaitHint._();
-
-  final _setupCompleter = Completer<void>();
-
-  /// Please call it inside the constructor of your class.
-  void setupMixinConstructor() {
-    () async {
-      try {
-        log('FlutterRustBridgeSetupMixin.setupMixinConstructor start setup');
-        await setup();
-      } finally {
-        log('FlutterRustBridgeSetupMixin.setupMixinConstructor complete setup');
-        _setupCompleter.complete();
-      }
-    }();
-  }
-
-  @override
-  Future<S> executeNormal<S, E extends Object>(NormalTask<S, E> task) async {
-    await _beforeExecute(task);
-    return await super.executeNormal(task);
-  }
-
-  @override
-  Stream<S> executeStream<S, E extends Object>(NormalTask<S, E> task) async* {
-    await _beforeExecute(task);
-    yield* super.executeStream(task);
-  }
-
-  Future<void> _beforeExecute<S, E extends Object>(NormalTask<S, E> task) async {
-    if (!_setupCompleter.isCompleted && task.hint is! _FlutterRustBridgeSetupMixinSkipWaitHint) {
-      log('FlutterRustBridgeSetupMixin.beforeExecute start waiting setup to complete (task=${task.debugName})');
-      await _setupCompleter.future;
-      log('FlutterRustBridgeSetupMixin.beforeExecute end waiting setup to complete (task=${task.debugName})');
-    }
-  }
-
-  /// Do your setup logic inside this function.
-  @protected
-  Future<void> setup();
-
-  /// Configure a logger for error handling.
-  @protected
-  void log(String message) {}
-}
-
-class _FlutterRustBridgeSetupMixinSkipWaitHint {
-  const _FlutterRustBridgeSetupMixinSkipWaitHint._();
-}
-
 /// Add a timeout to [executeNormal]
 mixin FlutterRustBridgeTimeoutMixin<T extends FlutterRustBridgeWireBase> on FlutterRustBridgeBase<T> {
   @override
