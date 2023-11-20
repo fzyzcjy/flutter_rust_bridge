a6b37b8f5964e1a203aeb402bf602d21464496fb
fzyzcjy <ch271828n@outlook.com>
2023-11-20 13:26:38 +0800

    feat: .initialized getter

--- frb_dart/lib/src/entrypoint.dart
+++ frb_dart/lib/src/entrypoint.dart
@@ -2,6 +2,9 @@ import 'package:flutter_rust_bridge/src/dispatcher.dart';
 import 'package:meta/meta.dart';
 
 abstract class BaseEntrypoint<D extends BaseDispatcher> {
+  /// Whether the system has been initialized.
+  bool get initialized => _dispatcher != null;
+
   @internal
   D get dispatcher => _dispatcher ?? (throw StateError('flutter_rust_bridge has not been initialized'));
   D? _dispatcher;
