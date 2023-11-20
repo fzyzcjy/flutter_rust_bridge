d3e988bf110fb6765c25f3d15655f0ce4567327f
fzyzcjy <ch271828n@outlook.com>
2023-11-20 20:22:41 +0800

    refactor: extract state

--- frb_dart/lib/src/entrypoint.dart
+++ frb_dart/lib/src/entrypoint.dart
@@ -3,18 +3,26 @@ import 'package:meta/meta.dart';
 
 abstract class BaseEntrypoint<D extends BaseDispatcher> {
   /// Whether the system has been initialized.
-  bool get initialized => _dispatcher != null;
+  bool get initialized => __state != null;
 
   @internal
-  D get dispatcher => _dispatcher ?? (throw StateError('flutter_rust_bridge has not been initialized'));
-  D? _dispatcher;
+  D get dispatcher => _state.dispatcher;
+
+  _EntrypointState<D> get _state => __state ?? (throw StateError('flutter_rust_bridge has not been initialized'));
+  _EntrypointState<D>? __state;
 
   @protected
   Future<void> initImpl({
     required D dispatcher,
   }) async {
-    if (_dispatcher != null) throw StateError('Should not initialize flutter_rust_bridge twice');
-    _dispatcher = dispatcher;
+    if (initialized) throw StateError('Should not initialize flutter_rust_bridge twice');
+    __state = _EntrypointState(dispatcher: dispatcher);
     // TODO more init work
   }
+}
+
+class _EntrypointState<D extends BaseDispatcher> {
+  final D dispatcher;
+
+  const _EntrypointState({required this.dispatcher});
 }
