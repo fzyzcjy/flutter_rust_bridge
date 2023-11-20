0cf45de10b28bf03a13954604a1ab5bd791ec751
fzyzcjy <ch271828n@outlook.com>
2023-11-20 20:30:28 +0800

    feat: _DropPortManager

--- frb_dart/lib/src/entrypoint.dart
+++ frb_dart/lib/src/entrypoint.dart
@@ -1,5 +1,9 @@
 import 'package:flutter_rust_bridge/src/dispatcher.dart';
+import 'package:flutter_rust_bridge/src/platform_types/platform_types.dart';
+import 'package:flutter_rust_bridge/src/utils/port_generator.dart';
 import 'package:meta/meta.dart';
+
+import 'generalized_isolate/generalized_isolate.dart';
 
 abstract class BaseEntrypoint<D extends BaseDispatcher> {
   /// Whether the system has been initialized.
@@ -7,6 +11,9 @@ abstract class BaseEntrypoint<D extends BaseDispatcher> {
 
   @internal
   D get dispatcher => _state.dispatcher;
+
+  @internal
+  NativePortType get dropPort => _state.dropPortManager.dropPort;
 
   _EntrypointState<D> get _state => __state ?? (throw StateError('flutter_rust_bridge has not been initialized'));
   _EntrypointState<D>? __state;
@@ -22,7 +29,8 @@ abstract class BaseEntrypoint<D extends BaseDispatcher> {
 
   @protected
   void disposeImpl() {
-    TODO;
+    __state!.dispose();
+    __state = null;
   }
 
   @protected
@@ -31,6 +39,30 @@ abstract class BaseEntrypoint<D extends BaseDispatcher> {
 
 class _EntrypointState<D extends BaseDispatcher> {
   final D dispatcher;
+  final dropPortManager = _DropPortManager();
 
-  const _EntrypointState({required this.dispatcher});
+  _EntrypointState({required this.dispatcher}) {
+    TODO;
+  }
+
+  void dispose() {
+    dropPortManager.dispose();
+  }
+}
+
+class _DropPortManager {
+  NativePortType get dropPort => _dropPort.sendPort.nativePort;
+  late final _dropPort = _initDropPort();
+
+  ReceivePort _initDropPort() {
+    final port = broadcastPort(DropIdPortGenerator.create());
+    port.listen((message) {
+      inner.drop_dart_object(message);
+    });
+    return port;
+  }
+
+  void dispose() {
+    _dropPort.close();
+  }
 }
--- frb_dart/lib/src/old/basic.dart
+++ frb_dart/lib/src/old/basic.dart
@@ -1,12 +1,8 @@
-import 'dart:async';
-
 import 'package:flutter_rust_bridge/src/old/platform_independent.dart';
 import 'package:flutter_rust_bridge/src/old/utils.dart';
-import 'package:flutter_rust_bridge/src/utils/port_generator.dart';
 import 'package:flutter_rust_bridge/src/wire_base/wire_base.dart';
 import 'package:meta/meta.dart';
 
-import 'ffi.dart';
 import 'isolate.dart';
 
 export 'ffi.dart';
@@ -25,21 +21,6 @@ abstract class FlutterRustBridgeBase<T extends FlutterRustBridgeWireBase> {
 
   @protected
   final T inner;
-
-  NativePortType get dropPort => _dropPort.sendPort.nativePort;
-  late final _dropPort = _initDropPort();
-
-  ReceivePort _initDropPort() {
-    var port = broadcastPort(DropIdPortGenerator.create());
-    port.listen((message) {
-      inner.drop_dart_object(message);
-    });
-    return port;
-  }
-
-  void dispose() {
-    _dropPort.close();
-  }
 
   // void _sanityCheckSingleton() {
   //   if (_instances.contains(runtimeType)) {
