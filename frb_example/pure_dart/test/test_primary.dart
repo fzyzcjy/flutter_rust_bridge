16a034339ebedb805976ff3a2125722cd6b11467
fzyzcjy <ch271828n@outlook.com>
2023-11-20 08:53:52 +0800

    feat: test_primary.dart

---
+++ frb_example/pure_dart/test/test_primary.dart
@@ -0,0 +1,5 @@
+import 'package:frb_example_pure_dart/main.dart' as entrypoint;
+
+// NOTE: The (same) tests will also be executed from `bin/pure_dart.dart`,
+// because we want to use Valgrind to test as well.
+void main() => entrypoint.main();
