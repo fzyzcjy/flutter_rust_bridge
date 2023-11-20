3f55278a186aeefb8fe109e61e3569c22d7b0e8e
fzyzcjy <ch271828n@outlook.com>
2023-11-20 21:10:49 +0800

    chore: mv

--- frb_dart/lib/flutter_rust_bridge.dart
+++
@@ -1,5 +0,0 @@
-export 'src/dispatcher.dart';
-export 'src/entrypoint.dart';
-export 'src/exceptions.dart';
-export 'src/handler.dart';
-export 'src/task.dart';
--- frb_dart/lib/flutter_rust_bridge_for_generated.dart
+++ frb_dart/lib/flutter_rust_bridge_for_generated.dart
@@ -1,6 +1,13 @@
 /// This library should only be used by auto-generated code, instead of the developers.
+library flutter_rust_bridge_for_generated;
 
 export 'package:meta/meta.dart' show internal;
+
 export 'flutter_rust_bridge.dart';
+export 'src/dispatcher.dart';
+export 'src/entrypoint.dart';
+export 'src/exceptions.dart';
+export 'src/handler.dart';
+export 'src/task.dart';
 
 // TODO export more
