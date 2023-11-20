3a7ea5a213af437e09c3a5387fe2f0780322f225
fzyzcjy <ch271828n@outlook.com>
2023-11-20 20:38:29 +0800

    refactor: rm syncReturnPointerLength which does not seem to be used in whole repo

--- frb_dart/lib/src/old/ffi.dart
+++ frb_dart/lib/src/old/ffi.dart
@@ -1,4 +1,1 @@
 export 'ffi/stub.dart' if (dart.library.io) 'ffi/io.dart' if (dart.library.html) 'ffi/web.dart';
-
-/// Rust SyncReturn<usize> type is forced cast to u64.
-const syncReturnPointerLength = 8;
