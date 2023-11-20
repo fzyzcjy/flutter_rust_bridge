acd2799db2c971a25a579e40ac11907a8b81eb99
fzyzcjy <ch271828n@outlook.com>
2023-11-20 14:36:57 +0800

    fix: compile

--- frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/misc/dispatcher.rs
+++ frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/misc/dispatcher.rs
@@ -10,6 +10,7 @@ use crate::codegen::ir::ty::primitive::IrTypePrimitive;
 use crate::codegen::ir::ty::structure::IrTypeStructRef;
 use crate::codegen::ir::ty::IrType;
 use crate::codegen::ir::ty::IrType::{Primitive, StructRef};
+use crate::library::codegen::generator::api_dart::spec_generator::info::ApiDartGeneratorInfoTrait;
 use crate::library::codegen::ir::ty::IrTypeTrait;
 use convert_case::{Case, Casing};
 use itertools::Itertools;
