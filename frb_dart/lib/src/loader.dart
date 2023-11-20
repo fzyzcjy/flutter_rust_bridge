df993a1d19d0bab3485b389d28484cf2ca054520
fzyzcjy <ch271828n@outlook.com>
2023-11-20 20:43:58 +0800

    chore: mv loader

--- frb_dart/lib/src/loader.dart
+++ frb_dart/lib/src/loader.dart
@@ -1,6 +1,8 @@
 import 'dart:ffi';
 import 'dart:io';
 
+// TODO merge with my internal loading logic
+// TODO enhance the logic
 /// agnostic dynamic library loading on native platforms
 ///
 /// see [#898](https://github.com/fzyzcjy/flutter_rust_bridge/pull/898)
@@ -9,5 +11,5 @@ DynamicLibrary loadLibForFlutter(String path) => Platform.isIOS
     : Platform.isMacOS && Abi.current() == Abi.macosX64
         ? DynamicLibrary.executable()
         : DynamicLibrary.open(path);
-DynamicLibrary loadLibForDart(String path) =>
-    Platform.isIOS ? DynamicLibrary.process() : DynamicLibrary.open(path);
+
+DynamicLibrary loadLibForDart(String path) => Platform.isIOS ? DynamicLibrary.process() : DynamicLibrary.open(path);
