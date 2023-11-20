577e2e0210b4933a8aeb8b69fc175b593dc15039
fzyzcjy <ch271828n@outlook.com>
2023-11-20 18:13:58 +0800

    refactor: mv

--- frb_dart/lib/src/exceptions.dart
+++ frb_dart/lib/src/exceptions.dart
@@ -1,6 +1,6 @@
 class FrbException implements Exception {}
 
-class PanicException extends FrbException {
+class PanicException implements FrbException {
   final String error;
 
   PanicException(this.error);
@@ -17,4 +17,40 @@ class FrbAnyhowException implements FrbException {
 
 abstract class FrbBacktracedException extends FrbException {
   String get backtrace;
+}
+
+/// Thrown when the browser is not run in a [cross-origin isolated] environment.
+///
+/// [cross-origin isolated]: https://developer.mozilla.org/en-US/docs/Web/API/crossOriginIsolated
+class MissingHeaderException implements FrbException {
+  const MissingHeaderException();
+
+  static const _message = '''
+Buffers cannot be shared due to missing cross-origin headers.
+Make sure your web server responds with the following headers:
+- Cross-Origin-Opener-Policy: same-origin
+- Cross-Origin-Embedder-Policy: credentialless OR require-corp
+
+If running from Flutter, consider `flutter build web` and running a custom static-file server.''';
+
+  @override
+  String toString() => _message;
+}
+
+class PlatformMismatchException implements FrbException {
+  const PlatformMismatchException();
+
+  static const _wasm = 'Not implemented on non-WASM platforms';
+
+  @override
+  String toString() => _wasm;
+}
+
+class UnmodifiableTypedListException implements FrbException {
+  const UnmodifiableTypedListException();
+
+  static const _message = 'Cannot modify the length of typed lists.';
+
+  @override
+  String toString() => _message;
 }
--- frb_dart/lib/src/old/helpers.dart
+++ frb_dart/lib/src/old/helpers.dart
@@ -97,42 +97,6 @@ mixin FlutterRustBridgeTimeoutMixin<T extends FlutterRustBridgeWireBase> on Flut
   Duration? get timeLimitForExecuteNormal;
 }
 
-/// Thrown when the browser is not run in a [cross-origin isolated] environment.
-///
-/// [cross-origin isolated]: https://developer.mozilla.org/en-US/docs/Web/API/crossOriginIsolated
-class MissingHeaderException implements Exception {
-  const MissingHeaderException();
-
-  static const _message = '''
-Buffers cannot be shared due to missing cross-origin headers.
-Make sure your web server responds with the following headers:
-- Cross-Origin-Opener-Policy: same-origin
-- Cross-Origin-Embedder-Policy: credentialless OR require-corp
-
-If running from Flutter, consider `flutter build web` and running a custom static-file server.''';
-
-  @override
-  String toString() => _message;
-}
-
-class PlatformMismatchException implements Exception {
-  const PlatformMismatchException();
-
-  static const _wasm = 'Not implemented on non-WASM platforms';
-
-  @override
-  String toString() => _wasm;
-}
-
-class UnmodifiableTypedListException implements Exception {
-  const UnmodifiableTypedListException();
-
-  static const _message = 'Cannot modify the length of typed lists.';
-
-  @override
-  String toString() => _message;
-}
-
 DateTime wire2apiTimestamp({required int ts, required bool isUtc}) {
   if (kIsWeb) {
     return DateTime.fromMillisecondsSinceEpoch(ts, isUtc: isUtc);
