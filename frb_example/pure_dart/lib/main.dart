4a06fa9fe9965637dade02b14218d8e8fa0a1824
fzyzcjy <ch271828n@outlook.com>
2023-11-20 09:41:01 +0800

    feat: add init

--- frb_example/pure_dart/lib/main.dart
+++ frb_example/pure_dart/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:frb_example_pure_dart/src/rust/api/simple.dart';
 import 'package:test/test.dart';
 
-void main() {
+Future<void> main() async {
+  await WhatName.init();
+
   test('dart call simpleAdder', () async {
     expect(await simpleAdder(a: 42, b: 100), 142);
   });
