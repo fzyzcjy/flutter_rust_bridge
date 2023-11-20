603e0e681e1a730d4492ece90a08fb6b7b19436c
fzyzcjy <ch271828n@outlook.com>
2023-11-20 21:32:09 +0800

    feat: comments

--- frb_dart/lib/flutter_rust_bridge.dart
+++ frb_dart/lib/flutter_rust_bridge.dart
@@ -1,3 +1,6 @@
+/// The library for developers to customize the behavior of flutter_rust_bridge.
+library;
+
+export 'src/exceptions.dart';
 export 'src/handler.dart' show BaseHandler;
 export 'src/task.dart' show NormalTask, SyncTask, StreamTask;
-export 'src/exceptions.dart';
