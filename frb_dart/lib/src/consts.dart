895a195cce70460d48d0b638a676e70b4b97fb01
fzyzcjy <ch271828n@outlook.com>
2023-11-20 21:27:20 +0800

    feat: more

--- frb_dart/lib/src/consts.dart
+++ frb_dart/lib/src/consts.dart
@@ -1,5 +1,8 @@
+import 'package:meta/meta.dart';
+
 /// borrowed from flutter foundation [kIsWeb](https://api.flutter.dev/flutter/foundation/kIsWeb-constant.html),
 /// but allows for using it in a Dart context alike
 const bool kIsWeb = identical(0, 0.0);
 
+@internal
 const kEnableFrbFfiTestTool = bool.fromEnvironment("ENABLE_FRB_FFI_TEST_TOOL");
--- frb_dart/lib/src/wire_base/_io.dart
+++ frb_dart/lib/src/wire_base/_io.dart
@@ -3,36 +3,42 @@ import 'dart:ffi' as ffi;
 import 'package:flutter_rust_bridge/src/platform_types/_io.dart';
 
 // TODO rename the class (should not have `FlutterRustBridge` prefix when possible)
-/// This class, together with its subclasses, are only for internal usage.
-/// Usually it should not be used by normal users.
+/// {@macro flutter_rust_bridge.only_for_generated_code}
 abstract class FlutterRustBridgeWireBase {
+  /// {@macro flutter_rust_bridge.only_for_generated_code}
   // ignore: non_constant_identifier_names
   void store_dart_post_cobject(DartPostCObject ptr) {
     throw UnimplementedError();
   }
 
+  /// {@macro flutter_rust_bridge.only_for_generated_code}
   // ignore: non_constant_identifier_names
   Object get_dart_object(int ptr) {
     throw UnimplementedError();
   }
 
+  /// {@macro flutter_rust_bridge.only_for_generated_code}
   // ignore: non_constant_identifier_names
   void drop_dart_object(int ptr) {
     throw UnimplementedError();
   }
 
+  /// {@macro flutter_rust_bridge.only_for_generated_code}
   // ignore: non_constant_identifier_names
   int new_dart_opaque(Object obj) {
     throw UnimplementedError();
   }
 
+  /// {@macro flutter_rust_bridge.only_for_generated_code}
   // ignore: non_constant_identifier_names
   void free_WireSyncReturn(WireSyncReturn val) {
     throw UnimplementedError();
   }
 }
 
+/// {@macro flutter_rust_bridge.only_for_generated_code}
 extension StoreDartPostCObjectExt on FlutterRustBridgeWireBase {
+  /// {@macro flutter_rust_bridge.only_for_generated_code}
   void storeDartPostCObject() {
     store_dart_post_cobject(ffi.NativeApi.postCObject.cast());
   }
