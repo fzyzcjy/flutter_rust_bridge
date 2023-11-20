04a24e2fed3b703c2c3b961a480aa90181593abc
fzyzcjy <ch271828n@outlook.com>
2023-11-20 08:42:38 +0800

    chore: rm

--- frb_example/pure_dart/lib/src/frb_generated/bridge_generated..io.dart
+++
@@ -1,8 +0,0 @@
-abstract class BridgeGenerated..io {
-            Future<int> simpleAdder({ required int a , required int b , dynamic hint });
-
-FlutterRustBridgeTaskConstMeta get kSimpleAdderConstMeta;
-        }
-
-        
-        
