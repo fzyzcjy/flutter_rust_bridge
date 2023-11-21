de663562c3a4c034eec43792df82bbfd472d7496
fzyzcjy <ch271828n@outlook.com>
2023-11-21 11:45:14 +0800

    refactor: minor

--- frb_dart/lib/src/loader/_io.dart
+++ frb_dart/lib/src/loader/_io.dart
@@ -1,20 +1,23 @@
+import 'dart:ffi';
+import 'dart:io';
+
 import 'package:flutter_rust_bridge/src/platform_types/_io.dart';
 
 /// {@macro flutter_rust_bridge.only_for_generated_code}
 ExternalLibrary loadExternalLibrary({
   required String name,
 }) {
-  throw Exception('TODO');
-}
+  // also see https://github.com/fzyzcjy/flutter_rust_bridge/pull/898
 
-// TODO merge with my internal loading logic
-// TODO enhance the logic
-// /// agnostic dynamic library loading on native platforms
-// ///
-// /// see [#898](https://github.com/fzyzcjy/flutter_rust_bridge/pull/898)
-// DynamicLibrary loadLibForFlutter(String path) => Platform.isIOS
-//     ? DynamicLibrary.process()
-//     : Platform.isMacOS && Abi.current() == Abi.macosX64
-//     ? DynamicLibrary.executable()
-//     : DynamicLibrary.open(path);
-// DynamicLibrary loadLibForDart(String path) => Platform.isIOS ? DynamicLibrary.process() : DynamicLibrary.open(path);
+  if (Platform.isIOS) {
+    return DynamicLibrary.process();
+  }
+
+  if (Platform.isMacOS) {
+    if (Abi.current() == Abi.macosX64) {
+      return DynamicLibrary.executable();
+    }
+  }
+
+  return DynamicLibrary.open(path);
+}
