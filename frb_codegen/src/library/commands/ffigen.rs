6d64c4e3fd69d6efd842a4a27d80159b7cc30228
fzyzcjy <ch271828n@outlook.com>
2023-11-21 08:56:45 +0800

    feat: empty generate_dart_native_api

--- frb_codegen/src/library/commands/ffigen.rs
+++ frb_codegen/src/library/commands/ffigen.rs
@@ -60,11 +60,15 @@ fn ffigen_to_file(args: FfigenToFileArgs) -> anyhow::Result<()> {
     let config = parse_config(&args)?;
     debug!("ffigen config: {}", config);
 
+    ffigen_raw(&config, args.dart_root)
+}
+
+pub(crate) fn ffigen_raw(config: &str, dart_root: &Path) -> anyhow::Result<()> {
     let mut config_file = tempfile::NamedTempFile::new()?;
     config_file.write_all(config.as_bytes())?;
     debug!("ffigen config_file: {:?}", config_file);
 
-    let repo = DartRepository::from_str(&path_to_string(args.dart_root)?).unwrap();
+    let repo = DartRepository::from_str(&path_to_string(dart_root)?).unwrap();
     let res = command_run!(
         call_shell[Some(args.dart_root)],
         *repo.toolchain.as_run_command(),
