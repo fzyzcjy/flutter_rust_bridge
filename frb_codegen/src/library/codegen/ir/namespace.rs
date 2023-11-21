133bcd4c10f62026d957d8be43c82c446665a38a
fzyzcjy <ch271828n@outlook.com>
2023-11-21 22:32:08 +0800

    feat: serialization

--- frb_codegen/src/library/codegen/ir/namespace.rs
+++ frb_codegen/src/library/codegen/ir/namespace.rs
@@ -1,5 +1,6 @@
 use itertools::Itertools;
-use serde::{Deserialize, Serialize};
+use serde::__private::ser::serialize_tagged_newtype;
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use std::fmt::{Display, Formatter};
 
 /// The Rust files/modules/namespaces.
@@ -10,25 +11,25 @@ pub struct Namespace {
     joined_path: String,
 }
 
-const NAMESPACE_PATH_SEPARATOR: &str = "::";
+const SEP: &str = "::";
 
 impl Namespace {
     pub fn new(path: Vec<String>) -> Self {
-        assert!((path.iter()).all(|item| !item.contains(NAMESPACE_PATH_SEPARATOR)));
-        Self {
-            joined_path: path.join(NAMESPACE_PATH_SEPARATOR),
-        }
+        assert!((path.iter()).all(|item| !item.contains(SEP)));
+        Self::new_raw(path.join(SEP))
+    }
+
+    pub fn new_raw(joined_path: String) -> Self {
+        Self { joined_path }
     }
 
     pub fn new_self_crate(joined_path: String) -> Self {
         assert!(!joined_path.starts_with("crate::"));
-        Self {
-            joined_path: format!("crate::{joined_path}"),
-        }
+        Self::new_raw(format!("crate::{joined_path}"))
     }
 
     pub fn path(&self) -> Vec<&str> {
-        self.joined_path.split(NAMESPACE_PATH_SEPARATOR).collect()
+        self.joined_path.split(SEP).collect()
     }
 }
 
@@ -42,7 +43,8 @@ impl Display for Namespace {
 ///
 /// Usually, a name itself (say "Apple") is vague, since it can be `mod_a::Apple`
 /// or `mod_b::Apple`. Instead, a namespace + name unambiguously determines the object.
-#[derive(Debug, Clone, Hash, Eq, PartialEq, Serialize, Deserialize, Ord, PartialOrd)]
+// Note: Do NOT implement `display`, otherwise it is easy to misuse it.
+#[derive(Debug, Clone, Hash, Eq, PartialEq, Ord, PartialOrd)]
 pub struct NamespacedName {
     pub namespace: Namespace,
     pub name: String,
@@ -51,6 +53,29 @@ pub struct NamespacedName {
 impl NamespacedName {
     pub fn new(namespace: Namespace, name: String) -> Self {
         Self { namespace, name }
+    }
+}
+
+impl Serialize for NamespacedName {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        format!("{}{SEP}{}", self.namespace, self.name).serialize(serializer)
+    }
+}
+
+impl Deserialize for NamespacedName {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'_>,
+    {
+        let s = String::deserialize(deserializer)?;
+        let index = s.rfind(SEP).unwrap();
+        Ok(Self::new(
+            Namespace::new_raw(s[..index].to_owned()),
+            s[index + SEP.len()..].to_owned(),
+        ))
     }
 }
 
@@ -64,5 +89,17 @@ mod tests {
             Namespace::new(vec!["crate".into(), "hello".into(), "world".into()]).to_string(),
             "crate::hello::world"
         );
+    }
+
+    #[test]
+    pub fn test_namespaced_name_serialization() -> anyhow::Result<()> {
+        let original = NamespacedName::new(Namespace::new_raw("a::b".into()), "c".into());
+        let serialized = serde_json::to_string(&original)?;
+        let recovered: NamespacedName = serde_json::from_str(&serialized)?;
+
+        assert_eq!(serialized, "a::b::c");
+        assert_eq!(original, recovered);
+
+        Ok(())
     }
 }
