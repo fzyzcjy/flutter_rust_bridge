2e8b8d612f09c16140ec8b7f21cbde533930e16f
fzyzcjy <ch271828n@outlook.com>
2023-11-21 11:23:45 +0800

    feat: rm unneeded

--- frb_dart/lib/src/utils/io_utils.dart
+++ frb_dart/lib/src/utils/io_utils.dart
@@ -1,19 +1,17 @@
-import 'dart:ffi' as ffi;
-
-// TODO rename?
-/// {@macro flutter_rust_bridge.only_for_generated_code}
-class DartApiDl {
-  static int? _initCode;
-  final int Function(ffi.Pointer<ffi.Void>) _initFn;
-
-  /// {@macro flutter_rust_bridge.only_for_generated_code}
-  DartApiDl(this._initFn);
-
-  /// {@macro flutter_rust_bridge.only_for_generated_code}
-  void initApi() {
-    _initCode ??= _initFn(ffi.NativeApi.initializeApiDLData);
-    if (_initCode != 0) {
-      throw Exception('Failed to initialize Dart API. Code: $_initCode');
-    }
-  }
-}
+// TODO do not put in Wire, since now we have GeneralizedFrbRustBinding and can directly handle it
+// /// {@macro flutter_rust_bridge.only_for_generated_code}
+// class DartApiDl {
+//   static int? _initCode;
+//   final int Function(ffi.Pointer<ffi.Void>) _initFn;
+//
+//   /// {@macro flutter_rust_bridge.only_for_generated_code}
+//   DartApiDl(this._initFn);
+//
+//   /// {@macro flutter_rust_bridge.only_for_generated_code}
+//   void initApi() {
+//     _initCode ??= _initFn(ffi.NativeApi.initializeApiDLData);
+//     if (_initCode != 0) {
+//       throw Exception('Failed to initialize Dart API. Code: $_initCode');
+//     }
+//   }
+// }
