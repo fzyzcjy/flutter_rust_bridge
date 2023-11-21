1b855d17d279195c9345ccce70a60267c526de87
fzyzcjy <ch271828n@outlook.com>
2023-11-21 20:05:43 +0800

    feat: NamespacedName

--- frb_codegen/src/library/codegen/ir/namespace.rs
+++ frb_codegen/src/library/codegen/ir/namespace.rs
@@ -1,4 +1,13 @@
+use serde::{Deserialize, Serialize};
+
 /// The Rust files/modules/namespaces.
+#[derive(Debug, Clone, Hash, Eq, PartialEq, Serialize, Deserialize)]
 pub(crate) struct Namespace {
     pub(crate) path: Vec<String>,
+}
+
+#[derive(Debug, Clone, Hash, Eq, PartialEq, Serialize, Deserialize)]
+pub(crate) struct NamespacedName {
+    pub(crate) namespace: Namespace,
+    pub(crate) name: String,
 }
