7ad526ec8b1d234ee7baf170454a8cde50263a70
fzyzcjy <ch271828n@outlook.com>
2023-11-21 20:13:02 +0800

    feat: display

--- frb_codegen/src/library/codegen/ir/namespace.rs
+++ frb_codegen/src/library/codegen/ir/namespace.rs
@@ -1,9 +1,22 @@
 use serde::{Deserialize, Serialize};
+use std::fmt::{Display, Formatter};
 
 /// The Rust files/modules/namespaces.
 #[derive(Debug, Clone, Hash, Eq, PartialEq, Serialize, Deserialize)]
 pub(crate) struct Namespace {
     pub(crate) path: Vec<String>,
+}
+
+impl Namespace {
+    pub fn new(path: Vec<String>) -> Self {
+        Self { path }
+    }
+}
+
+impl Display for Namespace {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&self.path.join("::"))
+    }
 }
 
 /// A name and the namespace it is in.
@@ -14,4 +27,26 @@ pub(crate) struct Namespace {
 pub(crate) struct NamespacedName {
     pub(crate) namespace: Namespace,
     pub(crate) name: String,
+}
+
+impl NamespacedName {
+    pub fn new(namespace: Vec<String>, name: String) -> Self {
+        Self {
+            namespace: Namespace::new(namespace),
+            name,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    pub fn test_namespace_display() {
+        assert_eq!(
+            Namespace::new(vec!["crate".into(), "hello".into(), "world".into()]),
+            "crate::hello::world"
+        );
+    }
 }
