08bae59ec802f8db6a1d0f87fcdf00e635442739
fzyzcjy <ch271828n@outlook.com>
2023-11-21 11:33:31 +0800

    fix: export

--- frb_dart/lib/src/main_components/wire/wire.dart
+++ frb_dart/lib/src/main_components/wire/wire.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_rust_bridge/src/platform_types/platform_types.dart';
 
+export '_io.dart' if (dart.library.html) '_web.dart';
+
 /// {@macro flutter_rust_bridge.only_for_generated_code}
 typedef WireConstructor<W> = W Function({required ExternalLibrary externalLibrary});
