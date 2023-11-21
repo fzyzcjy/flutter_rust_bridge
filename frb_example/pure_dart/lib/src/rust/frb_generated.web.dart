25ea79b80db8eb95daf055886498d016b993a13e
fzyzcjy <ch271828n@outlook.com>
2023-11-21 16:44:58 +0800

    chore: partial codegen

--- frb_example/pure_dart/lib/src/rust/frb_generated.web.dart
+++ frb_example/pure_dart/lib/src/rust/frb_generated.web.dart
@@ -1,1 +1,13 @@
+import 'package:flutter_rust_bridge/flutter_rust_bridge_for_generated.dart';
 
+abstract class RustLibApiImplPlatform extends BaseApiImpl<RustLibWire> {
+  RustLibApiImplPlatform({
+    super.handler,
+    required super.wire,
+    required super.generalizedFrbRustBinding,
+  });
+}
+
+class RustLibWire extends BaseWire {
+  // TODO
+}
