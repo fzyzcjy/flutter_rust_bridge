25bb909ff8be54cb60ac0929e8c09df579fff51c
fzyzcjy <ch271828n@outlook.com>
2023-11-21 13:41:26 +0800

    feat: add assets and dependencies

--- frb_example/pure_dart/build.dart
+++ frb_example/pure_dart/build.dart
@@ -1,11 +1,23 @@
 import 'package:native_assets_cli/native_assets_cli.dart';
 
+// ref: https://github.com/dart-lang/native/blob/main/pkgs/native_assets_cli/example/native_add_library/build.dart
 void main(List<String> args) async {
   print('hi $args');
 
   final buildConfig = await BuildConfig.fromArgs(args);
   final buildOutput = BuildOutput();
 
-  print('hi $buildConfig $buildOutput');
-  throw Exception('TODO');
+  buildOutput.assets.add(Asset(
+    id: assetId,
+    linkMode: LinkMode.dynamic,
+    target: buildConfig.target,
+    path: AssetAbsolutePath(libUri),
+  ));
+
+  buildOutput.dependencies.dependencies.addAll({
+    // Note: We use a Set here to deduplicate the dependencies.
+    ...sources,
+    ...includeFiles,
+    ...dartBuildFiles,
+  });
 }
