4599158bbbf63b6fb7d68f5e73c36f69cb5ee578
fzyzcjy <ch271828n@outlook.com>
2023-11-21 08:51:49 +0800

    feat: forward internal

---
+++ frb_codegen/src/library/internal/mod.rs
@@ -0,0 +1,3 @@
+pub fn generate() -> anyhow::Result<()> {
+    todo!()
+}
--- frb_codegen/src/library/mod.rs
+++ frb_codegen/src/library/mod.rs
@@ -3,5 +3,6 @@
 pub mod codegen;
 pub(crate) mod commands;
 pub mod integration;
+pub mod internal;
 pub(crate) mod misc;
 pub mod utils;
