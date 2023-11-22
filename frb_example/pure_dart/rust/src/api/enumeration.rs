78e3149adf8d73005b20fa26f256a04467b1dc8b
fzyzcjy <ch271828n@outlook.com>
2023-11-23 07:53:55 +0800

    chore: comment

--- frb_example/pure_dart/rust/src/api/enumeration.rs
+++ frb_example/pure_dart/rust/src/api/enumeration.rs
@@ -1,36 +1,37 @@
-pub enum EnumSimple {
-    A,
-    B,
-}
-
-pub fn func_enum_simple_twin_normal(arg: EnumSimple) -> EnumSimple {
-    arg
-}
-
-pub enum EnumWithItemMixed {
-    A,
-    B(Vec<u8>),
-    C { c_field: String },
-}
-
-pub fn func_enum_with_item_mixed_twin_normal(arg: EnumWithItemMixed) -> EnumWithItemMixed {
-    arg
-}
-
-pub enum EnumWithItemTuple {
-    A(Vec<u8>),
-    B(Vec<i32>),
-}
-
-pub fn func_enum_with_item_tuple_twin_normal(arg: EnumWithItemTuple) -> EnumWithItemTuple {
-    arg
-}
-
-pub enum EnumWithItemStruct {
-    A { a_field: Vec<u8> },
-    B { b_field: Vec<i32> },
-}
-
-pub fn func_enum_with_item_struct_twin_normal(arg: EnumWithItemStruct) -> EnumWithItemStruct {
-    arg
-}
+// TODO
+// pub enum EnumSimple {
+//     A,
+//     B,
+// }
+//
+// pub fn func_enum_simple_twin_normal(arg: EnumSimple) -> EnumSimple {
+//     arg
+// }
+//
+// pub enum EnumWithItemMixed {
+//     A,
+//     B(Vec<u8>),
+//     C { c_field: String },
+// }
+//
+// pub fn func_enum_with_item_mixed_twin_normal(arg: EnumWithItemMixed) -> EnumWithItemMixed {
+//     arg
+// }
+//
+// pub enum EnumWithItemTuple {
+//     A(Vec<u8>),
+//     B(Vec<i32>),
+// }
+//
+// pub fn func_enum_with_item_tuple_twin_normal(arg: EnumWithItemTuple) -> EnumWithItemTuple {
+//     arg
+// }
+//
+// pub enum EnumWithItemStruct {
+//     A { a_field: Vec<u8> },
+//     B { b_field: Vec<i32> },
+// }
+//
+// pub fn func_enum_with_item_struct_twin_normal(arg: EnumWithItemStruct) -> EnumWithItemStruct {
+//     arg
+// }
--- frb_example/pure_dart/rust/src/api/structure.rs
+++ frb_example/pure_dart/rust/src/api/structure.rs
@@ -4,35 +4,36 @@ pub fn func_struct_with_zero_field_twin_normal(arg: StructWithZeroField) -> Stru
     arg
 }
 
-pub struct StructWithOneField {
-    pub a: i32,
-}
-
-pub fn func_struct_with_one_field_twin_normal(arg: StructWithOneField) -> StructWithOneField {
-    arg
-}
-
-pub struct StructWithTwoField {
-    pub a: i32,
-    pub b: i32,
-}
-
-pub fn func_struct_with_two_field_twin_normal(arg: StructWithTwoField) -> StructWithTwoField {
-    arg
-}
-
-pub struct TupleStructWithOneField(pub i32);
-
-pub fn func_tuple_struct_with_one_field_twin_normal(
-    arg: TupleStructWithOneField,
-) -> TupleStructWithOneField {
-    arg
-}
-
-pub struct TupleStructWithTwoField(pub i32, pub i32);
-
-pub fn func_tuple_struct_with_two_field_twin_normal(
-    arg: TupleStructWithTwoField,
-) -> TupleStructWithTwoField {
-    arg
-}
+// TODO
+// pub struct StructWithOneField {
+//     pub a: i32,
+// }
+//
+// pub fn func_struct_with_one_field_twin_normal(arg: StructWithOneField) -> StructWithOneField {
+//     arg
+// }
+//
+// pub struct StructWithTwoField {
+//     pub a: i32,
+//     pub b: i32,
+// }
+//
+// pub fn func_struct_with_two_field_twin_normal(arg: StructWithTwoField) -> StructWithTwoField {
+//     arg
+// }
+//
+// pub struct TupleStructWithOneField(pub i32);
+//
+// pub fn func_tuple_struct_with_one_field_twin_normal(
+//     arg: TupleStructWithOneField,
+// ) -> TupleStructWithOneField {
+//     arg
+// }
+//
+// pub struct TupleStructWithTwoField(pub i32, pub i32);
+//
+// pub fn func_tuple_struct_with_two_field_twin_normal(
+//     arg: TupleStructWithTwoField,
+// ) -> TupleStructWithTwoField {
+//     arg
+// }
