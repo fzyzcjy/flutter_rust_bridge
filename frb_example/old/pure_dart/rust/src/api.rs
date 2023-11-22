4a69624ea0a85aa8e951a49247686bfec04bb49e
fzyzcjy <ch271828n@outlook.com>
2023-11-22 22:13:22 +0800

    chore: comment

--- frb_example/old/pure_dart/rust/src/api.rs
+++ frb_example/old/pure_dart/rust/src/api.rs
@@ -45,33 +45,34 @@ pub fn handle_return_unit() -> () {
     info!("handle_return_unit()");
 }
 
-pub struct ZeroCopyVecOfPrimitivePack {
-    pub int8list: ZeroCopyBuffer<Vec<i8>>,
-    pub uint8list: ZeroCopyBuffer<Vec<u8>>,
-    pub int16list: ZeroCopyBuffer<Vec<i16>>,
-    pub uint16list: ZeroCopyBuffer<Vec<u16>>,
-    pub uint32list: ZeroCopyBuffer<Vec<u32>>,
-    pub int32list: ZeroCopyBuffer<Vec<i32>>,
-    pub uint64list: ZeroCopyBuffer<Vec<u64>>,
-    pub int64list: ZeroCopyBuffer<Vec<i64>>,
-    pub float32list: ZeroCopyBuffer<Vec<f32>>,
-    pub float64list: ZeroCopyBuffer<Vec<f64>>,
-}
-
-pub fn handle_zero_copy_vec_of_primitive(n: i32) -> ZeroCopyVecOfPrimitivePack {
-    ZeroCopyVecOfPrimitivePack {
-        int8list: ZeroCopyBuffer(vec![42i8; n as usize]),
-        uint8list: ZeroCopyBuffer(vec![42u8; n as usize]),
-        int16list: ZeroCopyBuffer(vec![42i16; n as usize]),
-        uint16list: ZeroCopyBuffer(vec![42u16; n as usize]),
-        int32list: ZeroCopyBuffer(vec![42i32; n as usize]),
-        uint32list: ZeroCopyBuffer(vec![42u32; n as usize]),
-        int64list: ZeroCopyBuffer(vec![42i64; n as usize]),
-        uint64list: ZeroCopyBuffer(vec![42u64; n as usize]),
-        float32list: ZeroCopyBuffer(vec![42.0f32; n as usize]),
-        float64list: ZeroCopyBuffer(vec![42.0f64; n as usize]),
-    }
-}
+// TODO after enabling zero-copy by default, this is not needed anymore
+// pub struct ZeroCopyVecOfPrimitivePack {
+//     pub int8list: ZeroCopyBuffer<Vec<i8>>,
+//     pub uint8list: ZeroCopyBuffer<Vec<u8>>,
+//     pub int16list: ZeroCopyBuffer<Vec<i16>>,
+//     pub uint16list: ZeroCopyBuffer<Vec<u16>>,
+//     pub uint32list: ZeroCopyBuffer<Vec<u32>>,
+//     pub int32list: ZeroCopyBuffer<Vec<i32>>,
+//     pub uint64list: ZeroCopyBuffer<Vec<u64>>,
+//     pub int64list: ZeroCopyBuffer<Vec<i64>>,
+//     pub float32list: ZeroCopyBuffer<Vec<f32>>,
+//     pub float64list: ZeroCopyBuffer<Vec<f64>>,
+// }
+//
+// pub fn handle_zero_copy_vec_of_primitive(n: i32) -> ZeroCopyVecOfPrimitivePack {
+//     ZeroCopyVecOfPrimitivePack {
+//         int8list: ZeroCopyBuffer(vec![42i8; n as usize]),
+//         uint8list: ZeroCopyBuffer(vec![42u8; n as usize]),
+//         int16list: ZeroCopyBuffer(vec![42i16; n as usize]),
+//         uint16list: ZeroCopyBuffer(vec![42u16; n as usize]),
+//         int32list: ZeroCopyBuffer(vec![42i32; n as usize]),
+//         uint32list: ZeroCopyBuffer(vec![42u32; n as usize]),
+//         int64list: ZeroCopyBuffer(vec![42i64; n as usize]),
+//         uint64list: ZeroCopyBuffer(vec![42u64; n as usize]),
+//         float32list: ZeroCopyBuffer(vec![42.0f32; n as usize]),
+//         float64list: ZeroCopyBuffer(vec![42.0f64; n as usize]),
+//     }
+// }
 
 pub fn handle_struct_sync_freezed(
     arg: MySizeFreezed,
