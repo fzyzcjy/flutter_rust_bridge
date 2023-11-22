fb52ce670584788c5363e76f04f3341fc052f55b
fzyzcjy <ch271828n@outlook.com>
2023-11-22 19:37:36 +0800

    refactor: mv

--- tools/frb_internal/lib/src/frb_example_pure_dart_generator/dart/entrypoint.dart
+++ tools/frb_internal/lib/src/frb_example_pure_dart_generator/dart/entrypoint.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_rust_bridge_internal/src/frb_example_pure_dart_generator/dart/direct_sources.dart';
 import 'package:flutter_rust_bridge_internal/src/frb_example_pure_dart_generator/dart/duplicator.dart';
+import 'package:flutter_rust_bridge_internal/src/frb_example_pure_dart_generator/utils/generator_utils.dart';
 import 'package:flutter_rust_bridge_internal/src/utils/execute_process.dart';
-import 'package:flutter_rust_bridge_internal/src/utils/generator_utils.dart';
 
 Future<void> generateDart({required Uri dartRoot}) async {
   writeCodeFiles(dartRoot, generateDartDirectSources());
--- tools/frb_internal/lib/src/frb_example_pure_dart_generator/rust/entrypoint.dart
+++ tools/frb_internal/lib/src/frb_example_pure_dart_generator/rust/entrypoint.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_rust_bridge_internal/src/frb_example_pure_dart_generator/rust/direct_sources.dart';
 import 'package:flutter_rust_bridge_internal/src/frb_example_pure_dart_generator/rust/duplicator.dart';
+import 'package:flutter_rust_bridge_internal/src/frb_example_pure_dart_generator/utils/generator_utils.dart';
 import 'package:flutter_rust_bridge_internal/src/utils/execute_process.dart';
-import 'package:flutter_rust_bridge_internal/src/utils/generator_utils.dart';
 
 Future<void> generateRust({required Uri rustRoot}) async {
   writeCodeFiles(rustRoot, generateRustDirectSources());
