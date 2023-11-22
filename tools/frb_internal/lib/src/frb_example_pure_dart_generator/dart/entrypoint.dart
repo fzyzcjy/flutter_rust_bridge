d458d2a2c3c2d2f2e6f40f3abd612428653b9628
fzyzcjy <ch271828n@outlook.com>
2023-11-22 20:15:26 +0800

    feat: more gen

--- tools/frb_internal/lib/src/frb_example_pure_dart_generator/dart/entrypoint.dart
+++ tools/frb_internal/lib/src/frb_example_pure_dart_generator/dart/entrypoint.dart
@@ -23,7 +23,7 @@ class DartGenerator extends BaseGenerator {
     return inputText
         // imports
         .replaceAllMapped(r'src/rust/api/(\w+).dart', (m) => 'src/rust/api/${m.group(1)}${mode.filePostfix}.dart')
-        // function calls
+        // function call, struct name, etc
         .replaceAll('TwinNormal', ReCase(mode.filePostfix).pascalCase);
   }
 }
--- tools/frb_internal/lib/src/frb_example_pure_dart_generator/rust/entrypoint.dart
+++ tools/frb_internal/lib/src/frb_example_pure_dart_generator/rust/entrypoint.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_rust_bridge_internal/src/frb_example_pure_dart_generator/rust/direct_sources.dart';
 import 'package:flutter_rust_bridge_internal/src/frb_example_pure_dart_generator/utils/generator_utils.dart';
 import 'package:flutter_rust_bridge_internal/src/utils/execute_process.dart';
+import 'package:recase/recase.dart';
 
 class RustGenerator extends BaseGenerator {
   RustGenerator({required super.packageRootDir, required super.interestDir});
@@ -23,9 +24,12 @@ class RustGenerator extends BaseGenerator {
       DuplicatorMode.sync => '#[frb(sync)] ',
     };
 
-    return inputText.replaceAllMapped(
-      RegExp(r'pub fn ([a-zA-Z0-9_-]+)\('),
-      (m) => '${prefix}pub fn ${m.group(1)}${mode.filePostfix}(',
-    );
+    return inputText
+        .replaceAllMapped(
+          RegExp(r'pub fn ([a-zA-Z0-9_-]+)\('),
+          (m) => '${prefix}pub fn ${m.group(1)}${mode.filePostfix}(',
+        )
+        // struct name, etc
+        .replaceAll('TwinNormal', ReCase(mode.filePostfix).pascalCase);
   }
 }
