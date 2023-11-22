7844d3dc35e0d9afeeb37598d69b3498b7d3203c
fzyzcjy <ch271828n@outlook.com>
2023-11-22 20:15:42 +0800

    chore: frb_internal codegen

---
+++ frb_example/pure_dart/test/api/comment_test_twin_sync.dart
@@ -0,0 +1,20 @@
+// NOTE: This file is auto-generated from `comment_test.dart` by frb_internal
+// Please do not modify manually, but modify the `comment_test.dart` and re-run frb_internal generator
+
+import 'package:frb_example_pure_dart/src/rust/api/comment.dart';
+import 'package:frb_example_pure_dart/src/rust/frb_generated.dart';
+import 'package:test/test.dart';
+
+Future<void> main() async {
+  await RustLib.init();
+
+  // do not care too much about the function results, since we are
+  // considering the `code comments` feature here, instead of actual function execution logic.
+  test('can call the functions', () async {
+    functionWithCommentsSlashStarStarTwinSync();
+    functionWithCommentsTripleSlashMultiLineTwinSync();
+    functionWithCommentsTripleSlashSingleLineTwinSync();
+    StructWithCommentsTwinSync(fieldWithComments: 42).instanceMethod();
+    StructWithCommentsTwinSync.staticMethod();
+  });
+}
