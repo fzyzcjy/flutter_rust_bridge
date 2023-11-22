ded64124b1a601349495464e4b14f46459166e81
fzyzcjy <ch271828n@outlook.com>
2023-11-22 20:49:05 +0800

    chore: temp rm

--- frb_example/pure_dart/lib/src/rust/api/simple.dart
+++
@@ -1,7 +0,0 @@
-// ignore_for_file: invalid_use_of_internal_member
-
-import '../frb_generated.dart';
-
-Future<int> simpleAdderTwinNormal(
-        {required int a, required int b, dynamic hint}) =>
-    RustLib.instance.api.simpleAdderTwinNormal(a: a, b: b, hint: hint);
--- frb_example/pure_dart/lib/src/rust/api/simple_twin_sync.dart
+++
@@ -1,6 +0,0 @@
-// ignore_for_file: invalid_use_of_internal_member
-
-import '../frb_generated.dart';
-
-int simpleAdderTwinSync({required int a, required int b, dynamic hint}) =>
-    RustLib.instance.api.simpleAdderTwinSync(a: a, b: b, hint: hint);
