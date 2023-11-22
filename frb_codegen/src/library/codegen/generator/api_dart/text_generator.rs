6c63c0ea416eda0ad3ff14bdd1a9dac0b86ca875
fzyzcjy <ch271828n@outlook.com>
2023-11-22 08:40:37 +0800

    feat: impl text gen

--- frb_codegen/src/library/codegen/generator/api_dart/text_generator.rs
+++ frb_codegen/src/library/codegen/generator/api_dart/text_generator.rs
@@ -3,6 +3,7 @@ use crate::codegen::generator::api_dart::spec_generator::function::ApiDartGenera
 use crate::codegen::generator::api_dart::spec_generator::ApiDartOutputSpec;
 use crate::codegen::ir::namespace::Namespace;
 use itertools::Itertools;
+use std::collections::HashSet;
 
 pub(super) struct ApiDartOutputText {
     pub(super) namespaced_texts: Vec<(Namespace, String)>,
@@ -10,14 +11,31 @@ pub(super) struct ApiDartOutputText {
 
 pub(super) fn generate(spec: ApiDartOutputSpec) -> anyhow::Result<ApiDartOutputText> {
     let ApiDartOutputSpec { funcs, classes } = spec;
-    Ok(ApiDartOutputText {
-        namespaced_texts: generate_end_api_text(&classes, &funcs),
-    })
+
+    let grouped_funcs = funcs.iter().into_group_map_by(|x| x.namespace.clone());
+    let grouped_classes = classes.iter().into_group_map_by(|x| x.namespace.clone());
+
+    let namespaces = grouped_funcs
+        .keys()
+        .chain(grouped_classes.keys())
+        .collect::<HashSet<_>>();
+
+    let namespaced_texts = namespaces
+        .iter()
+        .map(|&namespace| {
+            (
+                namespace.to_owned(),
+                generate_end_api_text(&grouped_classes[namespace], &grouped_funcs[namespace]),
+            )
+        })
+        .collect_vec();
+
+    Ok(ApiDartOutputText { namespaced_texts })
 }
 
 fn generate_end_api_text(
-    classes: &[ApiDartGeneratedClass],
-    funcs: &[ApiDartGeneratedFunction],
+    classes: &[&ApiDartGeneratedClass],
+    funcs: &[&ApiDartGeneratedFunction],
 ) -> String {
     format!(
         "// ignore_for_file: invalid_use_of_internal_member
