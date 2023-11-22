029491ca6ffe256b4336b38aba8a57bb6cc1ef96
fzyzcjy <ch271828n@outlook.com>
2023-11-22 16:53:21 +0800

    feat: more

--- tools/frb_internal/lib/src/frb_example_pure_dart_generator/rust/generator.dart
+++ tools/frb_internal/lib/src/frb_example_pure_dart_generator/rust/generator.dart
@@ -1,3 +1,12 @@
+import 'dart:io';
+
 void generateRust({required Uri rustRoot}) {
-  // TODO
+  void writeFile(String relativePath, String code) =>
+      File(rustRoot.resolve(relativePath).toFilePath()).writeAsStringSync(code);
+
+  writeFile('src/api/primitive.rs', _generateSrcApiPrimitive());
+}
+
+String _generateSrcApiPrimitive() {
+  return TODO;
 }
