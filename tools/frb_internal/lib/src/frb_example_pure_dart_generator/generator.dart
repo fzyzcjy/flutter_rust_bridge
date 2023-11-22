b36a796dc44a7e6b6c99db976e144e72bc9fa13f
fzyzcjy <ch271828n@outlook.com>
2023-11-22 16:49:58 +0800

    fix: dir

--- tools/frb_internal/lib/src/frb_example_pure_dart_generator/generator.dart
+++ tools/frb_internal/lib/src/frb_example_pure_dart_generator/generator.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 void generate() {
   final dartRoot = Directory.current.uri.resolve('../../frb_example/pure_dart');
-  if (!File(dartRoot.toFilePath()).existsSync()) throw StateError('dartRoot=$dartRoot does not exist');
+  if (!Directory(dartRoot.toFilePath()).existsSync()) throw StateError('dartRoot=$dartRoot does not exist');
 
   // TODO
 }
