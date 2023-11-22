2118da5897b714ef848090352c6bfbb37a0faae8
fzyzcjy <ch271828n@outlook.com>
2023-11-22 10:07:56 +0800

    feat: test enum method

--- frb_codegen/test_fixtures/library/codegen/parser/mod/methods/src/api.rs
+++ frb_codegen/test_fixtures/library/codegen/parser/mod/methods/src/api.rs
@@ -5,3 +5,14 @@ impl MyStruct {
 
     pub fn example_instance_method(&self) {}
 }
+
+pub enum MyEnum {
+    A,
+    B,
+}
+
+impl MyEnum {
+    pub fn example_static_method() {}
+
+    pub fn example_instance_method(&self) {}
+}
