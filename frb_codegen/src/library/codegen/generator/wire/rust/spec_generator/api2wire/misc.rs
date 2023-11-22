4445162d7b5d31d934ae51bfa1b3d536e1325cdb
fzyzcjy <ch271828n@outlook.com>
2023-11-23 07:57:59 +0800

    fix: into_into_dart

--- frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/api2wire/misc.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/api2wire/misc.rs
@@ -4,15 +4,19 @@ pub(crate) fn generate_impl_into_into_dart(
     name: &NamespacedName,
     wrapper_name: &Option<NamespacedName>,
 ) -> String {
-    let wrapper_name = wrapper_name
-        .as_ref()
-        .map(|x| x.name.clone())
-        .unwrap_or(name.name.clone());
+    let body = if let Some(wrapper_name) = wrapper_name {
+        wrapper_name.name.clone()
+    } else {
+        "self".to_owned()
+    };
+
+    let wrapper_name = (wrapper_name.as_ref().map(|x| x.name.clone())).unwrap_or(name.name.clone());
     let name = &name.name;
+
     format!(
         "impl rust2dart::IntoIntoDart<{wrapper_name}> for {name} {{
             fn into_into_dart(self) -> {wrapper_name} {{
-                {wrapper_name}(self)
+                {body}
             }}
         }}"
     )
