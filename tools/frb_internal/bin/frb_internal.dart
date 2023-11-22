f1303ccd28f66eb31fcfad247ca017bb4213d69e
fzyzcjy <ch271828n@outlook.com>
2023-11-22 16:36:23 +0800

    feat: empty package

---
+++ tools/frb_internal/bin/frb_internal.dart
@@ -0,0 +1,5 @@
+import 'package:frb_internal/frb_internal.dart' as frb_internal;
+
+void main(List<String> arguments) {
+  print('Hello world: ${frb_internal.calculate()}!');
+}
---
+++ tools/frb_internal/test/frb_internal_test.dart
@@ -0,0 +1,8 @@
+import 'package:frb_internal/frb_internal.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
