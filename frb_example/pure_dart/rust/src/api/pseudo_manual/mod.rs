c871de29d29acf514857f01dfb369621bc668e7f
fzyzcjy <ch271828n@outlook.com>
2023-11-22 21:15:31 +0800

    chore: mod

--- frb_example/pure_dart/rust/src/api/pseudo_manual/mod.rs
+++ frb_example/pure_dart/rust/src/api/pseudo_manual/mod.rs
@@ -2,5 +2,7 @@ pub mod comment_twin_sync;
 pub mod optional_primitive;
 pub mod optional_primitive_twin_sync;
 pub mod primitive;
+pub mod primitive_list;
+pub mod primitive_list_twin_sync;
 pub mod primitive_twin_sync;
 pub mod simple_twin_sync;
