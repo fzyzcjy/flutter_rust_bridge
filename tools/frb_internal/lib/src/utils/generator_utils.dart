13c3cf234a1af80bef05046deff2a8f30d519f6c
fzyzcjy <ch271828n@outlook.com>
2023-11-22 18:34:01 +0800

    fix: compile

--- tools/frb_internal/lib/src/utils/generator_utils.dart
+++ tools/frb_internal/lib/src/utils/generator_utils.dart
@@ -2,6 +2,6 @@ import 'dart:io';
 
 void writeCodeFiles(Uri baseDir, Map<String, String> textOfPathMap) {
   for (final entry in textOfPathMap.entries) {
-    File(baseDir.resolve(entry.key).toFilePath()).writeAsStringSync(_kPrelude + entry.value);
+    File(baseDir.resolve(entry.key).toFilePath()).writeAsStringSync(entry.value);
   }
 }
