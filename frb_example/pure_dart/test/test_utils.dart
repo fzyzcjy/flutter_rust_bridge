b5c1a42a22b3432c20c626fd98727888359e2879
fzyzcjy <ch271828n@outlook.com>
2023-11-22 19:59:37 +0800

    feat: partial dart dup

---
+++ frb_example/pure_dart/test/test_utils.dart
@@ -0,0 +1,3 @@
+void executeFunction() {
+  TODO;
+}
--- tools/frb_internal/lib/src/frb_example_pure_dart_generator/dart/entrypoint.dart
+++ tools/frb_internal/lib/src/frb_example_pure_dart_generator/dart/entrypoint.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_rust_bridge_internal/src/frb_example_pure_dart_generator/dart/direct_sources.dart';
 import 'package:flutter_rust_bridge_internal/src/frb_example_pure_dart_generator/utils/generator_utils.dart';
 import 'package:flutter_rust_bridge_internal/src/utils/execute_process.dart';
+import 'package:recase/recase.dart';
 
 class DartGenerator extends BaseGenerator {
   DartGenerator({required super.baseDir});
@@ -16,4 +17,13 @@ class DartGenerator extends BaseGenerator {
 
   @override
   String get extension => 'dart';
+
+  @override
+  String generateDuplicate(String inputText, DuplicatorMode mode) {
+    return inputText
+        // imports
+        .replaceAllMapped(r'src/rust/api/(\w+).dart', (m) => 'src/rust/api/${m.group(1)}${mode.filePostfix}.dart')
+        // function calls
+        .replaceAll('TwinNormal', ReCase(mode.filePostfix).pascalCase);
+  }
 }
