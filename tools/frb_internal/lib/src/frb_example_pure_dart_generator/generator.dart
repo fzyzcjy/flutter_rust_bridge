51f127a2f5995ef62cee4e26e0d45f082c414e40
fzyzcjy <ch271828n@outlook.com>
2023-11-22 17:03:06 +0800

    fix: folder

--- tools/frb_internal/lib/src/frb_example_pure_dart_generator/generator.dart
+++ tools/frb_internal/lib/src/frb_example_pure_dart_generator/generator.dart
@@ -4,9 +4,9 @@ import 'package:flutter_rust_bridge_internal/src/frb_example_pure_dart_generator
 import 'package:flutter_rust_bridge_internal/src/frb_example_pure_dart_generator/rust/generator.dart';
 
 Future<void> generate() async {
-  final dartRoot = Directory.current.uri.resolve('../../frb_example/pure_dart');
+  final dartRoot = Directory.current.uri.resolve('../../frb_example/pure_dart/');
   if (!Directory(dartRoot.toFilePath()).existsSync()) throw StateError('dartRoot=$dartRoot does not exist');
 
-  await generateRust(rustRoot: dartRoot.resolve('rust'));
+  await generateRust(rustRoot: dartRoot.resolve('rust/'));
   await generateDart(dartRoot: dartRoot);
 }
