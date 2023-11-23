152c958eeb1b631b3b35516d7b1d7d6798f6b769
fzyzcjy <ch271828n@outlook.com>
2023-11-23 21:20:58 +0800

    chore: codegen

--- frb_example/pure_dart/lib/src/rust/api/array.dart
+++ frb_example/pure_dart/lib/src/rust/api/array.dart
@@ -54,6 +54,15 @@ class Blob {
           field0 == other.field0;
 }
 
+class F64Array16 extends NonGrowableListView<double> {
+  static const arraySize = 16;
+  F64Array16(Float64List inner)
+      : assert(inner.length == arraySize),
+        super(inner);
+  F64Array16.unchecked(Float64List inner) : super(inner);
+  F64Array16.init() : super(Float64List(arraySize));
+}
+
 class FeedId {
   final U8Array8 field0;
 
@@ -70,6 +79,15 @@ class FeedId {
       other is FeedId &&
           runtimeType == other.runtimeType &&
           field0 == other.field0;
+}
+
+class I32Array2 extends NonGrowableListView<int> {
+  static const arraySize = 2;
+  I32Array2(Int32List inner)
+      : assert(inner.length == arraySize),
+        super(inner);
+  I32Array2.unchecked(Int32List inner) : super(inner);
+  I32Array2.init() : super(Int32List(arraySize));
 }
 
 class MessageId {
@@ -111,6 +129,15 @@ class Point {
           y == other.y;
 }
 
+class PointArray2 extends NonGrowableListView<Point> {
+  static const arraySize = 2;
+  PointArray2(List<Point> inner)
+      : assert(inner.length == arraySize),
+        super(inner);
+  PointArray2.unchecked(List<Point> inner) : super(inner);
+  PointArray2.init(Point fill) : super(List<Point>.filled(arraySize, fill));
+}
+
 class TestId {
   final I32Array2 field0;
 
@@ -127,4 +154,58 @@ class TestId {
       other is TestId &&
           runtimeType == other.runtimeType &&
           field0 == other.field0;
+}
+
+class TestIdArray2 extends NonGrowableListView<TestId> {
+  static const arraySize = 2;
+  TestIdArray2(List<TestId> inner)
+      : assert(inner.length == arraySize),
+        super(inner);
+  TestIdArray2.unchecked(List<TestId> inner) : super(inner);
+  TestIdArray2.init(TestId fill) : super(List<TestId>.filled(arraySize, fill));
+}
+
+class TestIdArray4 extends NonGrowableListView<TestId> {
+  static const arraySize = 4;
+  TestIdArray4(List<TestId> inner)
+      : assert(inner.length == arraySize),
+        super(inner);
+  TestIdArray4.unchecked(List<TestId> inner) : super(inner);
+  TestIdArray4.init(TestId fill) : super(List<TestId>.filled(arraySize, fill));
+}
+
+class U8Array1600 extends NonGrowableListView<int> {
+  static const arraySize = 1600;
+  U8Array1600(Uint8List inner)
+      : assert(inner.length == arraySize),
+        super(inner);
+  U8Array1600.unchecked(Uint8List inner) : super(inner);
+  U8Array1600.init() : super(Uint8List(arraySize));
+}
+
+class U8Array32 extends NonGrowableListView<int> {
+  static const arraySize = 32;
+  U8Array32(Uint8List inner)
+      : assert(inner.length == arraySize),
+        super(inner);
+  U8Array32.unchecked(Uint8List inner) : super(inner);
+  U8Array32.init() : super(Uint8List(arraySize));
+}
+
+class U8Array5 extends NonGrowableListView<int> {
+  static const arraySize = 5;
+  U8Array5(Uint8List inner)
+      : assert(inner.length == arraySize),
+        super(inner);
+  U8Array5.unchecked(Uint8List inner) : super(inner);
+  U8Array5.init() : super(Uint8List(arraySize));
+}
+
+class U8Array8 extends NonGrowableListView<int> {
+  static const arraySize = 8;
+  U8Array8(Uint8List inner)
+      : assert(inner.length == arraySize),
+        super(inner);
+  U8Array8.unchecked(Uint8List inner) : super(inner);
+  U8Array8.init() : super(Uint8List(arraySize));
 }
