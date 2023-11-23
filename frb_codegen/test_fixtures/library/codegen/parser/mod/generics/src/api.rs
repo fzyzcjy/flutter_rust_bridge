1ec111c1725ef9aa6ee342c3aeed25505b6d8bba
fzyzcjy <ch271828n@outlook.com>
2023-11-23 12:34:30 +0800

    feat: enhance tests

--- frb_codegen/test_fixtures/library/codegen/parser/mod/generics/src/api.rs
+++ frb_codegen/test_fixtures/library/codegen/parser/mod/generics/src/api.rs
@@ -4,8 +4,17 @@ pub struct MyGenericStruct<T> {
     normal_field: i32,
 }
 
-pub fn generic_func_string(arg: MyGenericStruct<String>) {}
+pub fn func_struct_string(arg: MyGenericStruct<String>) {}
 
-pub fn generic_func_string_repeated(arg: MyGenericStruct<String>) {}
+pub fn func_struct_string_repeated(arg: MyGenericStruct<String>) {}
 
-pub fn generic_func_bool(arg: MyGenericStruct<bool>) {}
+pub fn func_struct_bool(arg: MyGenericStruct<bool>) {}
+
+pub enum MyGenericEnum<A, B> {
+    One(A),
+    Two(B),
+}
+
+pub fn func_enum_string(arg: MyGenericEnum<String>) {}
+
+pub fn func_enum_bool(arg: MyGenericEnum<bool>) {}
