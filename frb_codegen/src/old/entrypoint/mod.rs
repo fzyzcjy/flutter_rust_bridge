699db0a2c4711904dc5c7663872e41855c362e88
fzyzcjy <ch271828n@outlook.com>
2023-11-23 08:23:14 +0800

    chore: rm empty file

--- frb_codegen/src/old/entrypoint/mod.rs
+++
@@ -1,2 +0,0 @@
-pub(crate) mod dart;
-pub(crate) mod rust;
--- frb_codegen/src/old/utils/mod.rs
+++
@@ -1,5 +0,0 @@
-pub(crate) mod command_runner;
-pub(crate) mod consts;
-pub(crate) mod dart_repository;
-pub(crate) mod method;
-pub(crate) mod misc;
