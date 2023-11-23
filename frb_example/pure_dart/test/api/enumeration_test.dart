6e8e3380082cc6c0d24de7e8d01222f36dde08c4
fzyzcjy <ch271828n@outlook.com>
2023-11-23 09:50:07 +0800

    refactor: addTestsIdentityFunctionCall

--- frb_example/pure_dart/test/api/enumeration_test.dart
+++ frb_example/pure_dart/test/api/enumeration_test.dart
@@ -2,23 +2,22 @@ import 'dart:typed_data';
 
 import 'package:frb_example_pure_dart/src/rust/api/enumeration.dart';
 import 'package:frb_example_pure_dart/src/rust/frb_generated.dart';
-import 'package:test/test.dart';
+
+import '../test_utils.dart';
 
 Future<void> main() async {
   await RustLib.init();
 
-  test('call funcStructWithZeroFieldTwinNormal', () async {
-    expect(await funcEnumSimpleTwinNormal(arg: EnumSimple.A), EnumSimple.A);
-    expect(await funcEnumSimpleTwinNormal(arg: EnumSimple.B), EnumSimple.B);
-  });
+  addTestsIdentityFunctionCall(funcEnumSimpleTwinNormal, [
+    EnumSimple.A,
+    EnumSimple.B,
+  ]);
 
-  test('call funcStructWithZeroFieldTwinNormal', () async {
-    expect(await funcEnumWithItemMixedTwinNormal(arg: const EnumWithItemMixed.a()), const EnumWithItemMixed.a());
-    expect(await funcEnumWithItemMixedTwinNormal(arg: EnumWithItemMixed.b(Uint8List.fromList([42]))),
-        EnumWithItemMixed.b(Uint8List.fromList([42])));
-    expect(await funcEnumWithItemMixedTwinNormal(arg: EnumWithItemMixed.c(cField: 'hi')),
-        EnumWithItemMixed.c(cField: 'hi'));
-  });
+  addTestsIdentityFunctionCall(funcEnumWithItemMixedTwinNormal, [
+    EnumWithItemMixed.a(),
+    EnumWithItemMixed.b(Uint8List.fromList([42])),
+    EnumWithItemMixed.c(cField: 'hi'),
+  ]);
 
   // TODO
 }
---
+++ frb_example/pure_dart/test/test_utils.dart
@@ -0,0 +1,18 @@
+import 'dart:async';
+
+import 'package:meta/meta.dart';
+import 'package:test/test.dart';
+
+@isTestGroup
+void addTestsIdentityFunctionCall<T>(
+  FutureOr<T> Function({required T arg, dynamic hint}) func,
+  List<T> values,
+) {
+  group('call $func', () {
+    for (final value in values) {
+      test('with value $value', () async {
+        expect(await func(arg: value), value);
+      });
+    }
+  });
+}
