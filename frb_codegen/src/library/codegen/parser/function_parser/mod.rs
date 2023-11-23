ea2befc9799b9462e1160963582bc882aa05b475
fzyzcjy <ch271828n@outlook.com>
2023-11-23 16:14:36 +0800

    fix: type

--- frb_codegen/src/library/codegen/parser/function_parser/mod.rs
+++ frb_codegen/src/library/codegen/parser/function_parser/mod.rs
@@ -54,7 +54,7 @@ impl<'a, 'b> FunctionParser<'a, 'b> {
         let sig = func.sig();
         let namespace =
             Namespace::new_self_crate(compute_mod_from_rust_path(file_path, rust_crate_dir)?);
-        let src_lineno = func.span().start();
+        let src_lineno = func.span().start().line;
 
         let owner = if let Some(owner) = parse_owner(func) {
             owner
