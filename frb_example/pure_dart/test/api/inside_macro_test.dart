bd44d5607a398cdfd34f0ea1427430a0bef6451b
fzyzcjy <ch271828n@outlook.com>
2023-11-23 14:33:27 +0800

    fix: minor

--- frb_example/pure_dart/test/api/inside_macro_test.dart
+++ frb_example/pure_dart/test/api/inside_macro_test.dart
@@ -2,12 +2,11 @@
 
 import 'package:frb_example_pure_dart/src/rust/api/inside_macro.dart';
 import 'package:frb_example_pure_dart/src/rust/frb_generated.dart';
-import 'package:test/test.dart';
 
 import '../test_utils.dart';
 
 Future<void> main() async {
   await RustLib.init();
 
-  addTestsIdentityFunctionCall(funcMacroStruct, [const MacroStruct(value: 42)]);
+  addTestsIdentityFunctionCall(funcMacroStruct, [const MacroStruct(data: 42)]);
 }
