b471844f9c6c410cfe537ee88313fe11851e1299
fzyzcjy <ch271828n@outlook.com>
2023-11-23 09:43:58 +0800

    feat: struct test

--- frb_example/pure_dart/test/api/structure_test.dart
+++ frb_example/pure_dart/test/api/structure_test.dart
@@ -8,4 +8,24 @@ Future<void> main() async {
   test('call funcStructWithZeroFieldTwinNormal', () async {
     expect(await funcStructWithZeroFieldTwinNormal(arg: const StructWithZeroField()), isA<StructWithZeroField>());
   });
+
+  test('call funcStructWithOneFieldTwinNormal', () async {
+    expect(await funcStructWithOneFieldTwinNormal(arg: const StructWithOneField(a: 42)),
+        isA<StructWithOneField>().having((x) => x.a, 'a', 42));
+  });
+
+  test('call funcStructWithTwoFieldTwinNormal', () async {
+    expect(await funcStructWithTwoFieldTwinNormal(arg: const StructWithTwoField(a: 10, b: 20)),
+        isA<StructWithTwoField>().having((x) => x.a, 'a', 10).having((x) => x.b, 'b', 20));
+  });
+
+  test('call funcTupleStructWithOneFieldTwinNormal', () async {
+    expect(await funcTupleStructWithOneFieldTwinNormal(arg: const TupleStructWithOneField(field0: 10)),
+        isA<TupleStructWithOneField>().having((x) => x.field0, 'field0', 10));
+  });
+
+  test('call funcTupleStructWithTwoFieldTwinNormal', () async {
+    expect(await funcTupleStructWithTwoFieldTwinNormal(arg: const TupleStructWithTwoField(field0: 10, field1: 20)),
+        isA<TupleStructWithTwoField>().having((x) => x.field0, 'field0', 10).having((x) => x.field1, 'field1', 20));
+  });
 }
