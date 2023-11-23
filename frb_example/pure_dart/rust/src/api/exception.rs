4894c2a10b21c5e6b8755beb4ea2f1ae7dd7670e
fzyzcjy <ch271828n@outlook.com>
2023-11-23 14:25:37 +0800

    feat: more backtrace

--- frb_example/pure_dart/rust/src/api/exception.rs
+++ frb_example/pure_dart/rust/src/api/exception.rs
@@ -1,5 +1,5 @@
 use anyhow::{anyhow, Result};
-use std::backtrace::Backtrace;
+use backtrace::Backtrace;
 
 // ------------------------------ built-in errors ----------------------------------
 
@@ -25,7 +25,7 @@ pub enum CustomEnumErrorTwinNormal {
 pub fn custom_enum_error_return_error_twin_normal() -> Result<u32, CustomEnumErrorTwinNormal> {
     Err(CustomEnumErrorTwinNormal::Error0 {
         e: "deliberate error".into(),
-        backtrace: Backtrace::capture(),
+        backtrace: Backtrace::new(),
     })
 }
 
