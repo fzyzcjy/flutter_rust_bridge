0c3680e3afbe3bb08f75a68cc858c0df93eb2bf8
fzyzcjy <ch271828n@outlook.com>
2023-11-23 09:45:57 +0800

    feat: partial enum test

--- frb_example/pure_dart/test/api/enumeration_test.dart
+++ frb_example/pure_dart/test/api/enumeration_test.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:frb_example_pure_dart/src/rust/api/enumeration.dart';
 import 'package:frb_example_pure_dart/src/rust/frb_generated.dart';
 import 'package:test/test.dart';
@@ -9,4 +11,14 @@ Future<void> main() async {
     expect(await funcEnumSimpleTwinNormal(arg: EnumSimple.A), EnumSimple.A);
     expect(await funcEnumSimpleTwinNormal(arg: EnumSimple.B), EnumSimple.B);
   });
+
+  test('call funcStructWithZeroFieldTwinNormal', () async {
+    expect(await funcEnumWithItemMixedTwinNormal(arg: const EnumWithItemMixed.a()), const EnumWithItemMixed.a());
+    expect(await funcEnumWithItemMixedTwinNormal(arg: EnumWithItemMixed.b(Uint8List.fromList([42]))),
+        EnumWithItemMixed.b(Uint8List.fromList([42])));
+    expect(await funcEnumWithItemMixedTwinNormal(arg: EnumWithItemMixed.c(cField: 'hi')),
+        EnumWithItemMixed.c(cField: 'hi'));
+  });
+ 
+  TODO;
 }
