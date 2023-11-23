1294397ac0d23d5c710b12b9c10b4150e2f8229a
fzyzcjy <ch271828n@outlook.com>
2023-11-23 09:58:36 +0800

    fix: import

--- tools/frb_internal/lib/src/frb_example_pure_dart_generator/dart/entrypoint.dart
+++ tools/frb_internal/lib/src/frb_example_pure_dart_generator/dart/entrypoint.dart
@@ -24,6 +24,7 @@ class DartGenerator extends BaseGenerator {
         // imports
         .replaceAllMapped(RegExp(r'src/rust/api/(pseudo_manual/)?(\w+)\.dart'),
             (m) => 'src/rust/api/pseudo_manual/${m.group(2)}${mode.postfix}.dart')
+        .replaceAll("'../test_utils.dart'", "'../../test_utils.dart'")
         // function call, struct name, etc
         .replaceAll('TwinNormal', ReCase(mode.postfix).pascalCase);
   }
