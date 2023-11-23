4838dcb769e7a1d2c1f318ee3ab1d74eb84b58de
fzyzcjy <ch271828n@outlook.com>
2023-11-23 18:31:05 +0800

    fix: Unencodable correct safe_ident

--- frb_codegen/src/library/codegen/ir/ty/unencodable.rs
+++ frb_codegen/src/library/codegen/ir/ty/unencodable.rs
@@ -1,5 +1,7 @@
 use crate::codegen::ir::pack::IrPack;
 use crate::codegen::ir::ty::{IrType, IrTypeTrait};
+use lazy_static::lazy_static;
+use regex::Regex;
 
 crate::ir! {
 pub struct IrTypeUnencodable {
@@ -23,7 +25,10 @@ impl IrTypeTrait for IrTypeUnencodable {
     fn visit_children_types<F: FnMut(&IrType) -> bool>(&self, _f: &mut F, _ir_pack: &IrPack) {}
 
     fn safe_ident(&self) -> String {
-        self.string.clone()
+        lazy_static! {
+            static ref NEG_FILTER: Regex = Regex::new(r"[^a-zA-Z0-9_]").unwrap();
+        }
+        NEG_FILTER.replace_all(&self.string, "").into_string()
     }
 
     fn rust_api_type(&self) -> String {
