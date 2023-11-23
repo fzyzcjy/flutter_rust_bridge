8250dead00c6661de8711cb0b00900dc0a916302
fzyzcjy <ch271828n@outlook.com>
2023-11-23 21:07:39 +0800

    feat: generate_imports

--- frb_codegen/src/library/codegen/generator/api_dart/spec_generator/mod.rs
+++ frb_codegen/src/library/codegen/generator/api_dart/spec_generator/mod.rs
@@ -11,8 +11,10 @@ use crate::codegen::ir::ty::IrType;
 use crate::library::codegen::generator::api_dart::spec_generator::class::ty::ApiDartGeneratorClassTrait;
 use crate::library::codegen::ir::ty::IrTypeTrait;
 use crate::utils::basic_code::DartBasicHeaderCode;
+use crate::utils::path_utils::path_to_string;
 use anyhow::Result;
 use itertools::Itertools;
+use pathdiff::diff_paths;
 use serde::Serialize;
 use std::collections::{HashMap, HashSet};
 
@@ -55,26 +57,28 @@ pub(crate) fn generate(
     let namespaced_items = namespaces
         .iter()
         .map(|&namespace| {
-            (
+            Ok((
                 namespace.to_owned(),
                 generate_item(
+                    namespace,
                     &grouped_classes.get(namespace),
                     &grouped_funcs.get(namespace),
                     context,
-                ),
-            )
+                )?,
+            ))
         })
-        .collect();
+        .collect::<Result<HashMap<_, _>>>()?;
 
     Ok(ApiDartOutputSpec { namespaced_items })
 }
 
 fn generate_item(
+    namespace: &Namespace,
     classes: &Option<&Vec<&IrType>>,
     funcs: &Option<&Vec<&IrFunc>>,
     context: ApiDartGeneratorContext,
-) -> ApiDartOutputSpecItem {
-    let imports = generate_imports(classes, funcs, context.ir_pack);
+) -> Result<ApiDartOutputSpecItem> {
+    let imports = generate_imports(namespace, classes, funcs, context.ir_pack)?;
 
     let funcs = funcs
         .map(|funcs| {
@@ -97,19 +101,20 @@ fn generate_item(
 
     let needs_freezed = classes.iter().any(|class| class.needs_freezed);
 
-    ApiDartOutputSpecItem {
+    Ok(ApiDartOutputSpecItem {
         funcs,
         classes,
         imports,
         needs_freezed,
-    }
+    })
 }
 
 fn generate_imports(
+    current_file_namespace: &Namespace,
     classes: &Option<&Vec<&IrType>>,
     funcs: &Option<&Vec<&IrFunc>>,
     ir_pack: &IrPack,
-) -> DartBasicHeaderCode {
+) -> Result<DartBasicHeaderCode> {
     let mut gatherer = DistinctTypeGatherer::new();
     if let Some(classes) = classes {
         (classes.iter()).for_each(|x| x.visit_types(&mut |ty| gatherer.add(ty), ir_pack));
@@ -122,11 +127,18 @@ fn generate_imports(
     let import = interest_types
         .iter()
         .filter_map(|ty| ty.self_namespace())
-        .map(|namespace| format!("import '{}';\n", TODO))
+        .map(|import_ty_namespace: &Namespace| {
+            let path_diff = diff_paths(
+                import_ty_namespace.to_pseudo_io_path("dart"),
+                (current_file_namespace.to_pseudo_io_path("dart").parent()).unwrap(),
+            )
+            .unwrap();
+            format!("import '{}';\n", path_to_string(&path_diff).unwrap())
+        })
         .join("");
 
-    DartBasicHeaderCode {
+    Ok(DartBasicHeaderCode {
         import,
         ..Default::default()
-    }
+    })
 }
