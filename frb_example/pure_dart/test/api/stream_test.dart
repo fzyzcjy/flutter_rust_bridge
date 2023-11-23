1937bdfd0469fd0b957858240210437ffe6bde38
fzyzcjy <ch271828n@outlook.com>
2023-11-23 10:45:05 +0800

    chore: update

--- frb_example/pure_dart/test/api/stream_test.dart
+++ frb_example/pure_dart/test/api/stream_test.dart
@@ -1,4 +1,4 @@
-import 'package:frb_example_pure_dart/src/rust/api/simple.dart';
+import 'package:frb_example_pure_dart/src/rust/api/stream.dart';
 import 'package:frb_example_pure_dart/src/rust/frb_generated.dart';
 import 'package:test/test.dart';
 
