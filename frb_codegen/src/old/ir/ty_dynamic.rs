c815c7732e6eaaa57c065bcc42aa4d4193645be8
fzyzcjy <ch271828n@outlook.com>
2023-11-23 08:25:14 +0800

    chore: rm already migrated file

--- frb_codegen/src/old/ir/ty_dynamic.rs
+++
@@ -1,9 +0,0 @@
-use super::*;
-
-impl IrTypeTrait for IrTypeDynamic {
-    fn visit_children_types<F: FnMut(&IrType) -> bool>(&self, _f: &mut F, _ir_pack: &IrPack) {}
-
-    fn safe_ident(&self) -> String {
-        "dartabi".to_owned()
-    }
-}
