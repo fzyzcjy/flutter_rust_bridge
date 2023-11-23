dadcd37369dc1fd1d6f700b5fdd0ee71bc97b3c0
fzyzcjy <ch271828n@outlook.com>
2023-11-23 08:03:37 +0800

    fix: wrong file

--- frb_example/pure_dart/test/api/structure_test.dart
+++ frb_example/pure_dart/test/api/structure_test.dart
@@ -1,4 +1,4 @@
-import 'package:frb_example_pure_dart/src/rust/api/enumeration.dart';
+import 'package:frb_example_pure_dart/src/rust/api/structure.dart';
 import 'package:frb_example_pure_dart/src/rust/frb_generated.dart';
 import 'package:test/test.dart';
 
@@ -6,7 +6,6 @@ Future<void> main() async {
   await RustLib.init();
 
   test('call funcStructWithZeroFieldTwinNormal', () async {
-    expect(await funcEnumSimpleTwinNormal(arg: EnumSimple.A), EnumSimple.A);
-    expect(await funcEnumSimpleTwinNormal(arg: EnumSimple.B), EnumSimple.B);
+    expect(await funcStructWithZeroFieldTwinNormal(arg: const StructWithZeroField()), isA<StructWithZeroField>());
   });
 }
