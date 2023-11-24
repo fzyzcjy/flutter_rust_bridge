4cab1388853d64e6acd2e2e38f1feebbc9ba0f42
fzyzcjy <ch271828n@outlook.com>
2023-11-24 17:59:22 +0800

    refactor: split

--- frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/wire_class/wasm.rs
+++ frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/wire_class/wasm.rs
@@ -2,6 +2,10 @@ use crate::codegen::generator::wire::dart::internal_config::GeneratorWireDartInt
 use crate::codegen::generator::wire::dart::spec_generator::output_code::WireDartOutputCode;
 
 pub(super) fn generate(config: &GeneratorWireDartInternalConfig) -> WireDartOutputCode {
+    generate_wire_class(config) + generate_wasm_module(config)
+}
+
+fn generate_wire_class(config: &GeneratorWireDartInternalConfig) -> WireDartOutputCode {
     let wire_class_name = &config.dart_output_class_name_pack.wire_class_name;
 
     let body = (funcs.into_iter().map(generate_wasm_wire_func_method)).join("\n\n");
@@ -14,4 +18,8 @@ pub(super) fn generate(config: &GeneratorWireDartInternalConfig) -> WireDartOutp
         ",
     )
     .into()
+}
+
+fn generate_wasm_module(config: &GeneratorWireDartInternalConfig) -> WireDartOutputCode {
+    todo!()
 }
