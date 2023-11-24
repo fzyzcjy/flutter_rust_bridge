063c476dd679b119c252413adfaf05c1de6bee42
fzyzcjy <ch271828n@outlook.com>
2023-11-24 22:28:12 +0800

    chore: mv

--- frb_example/old/pure_dart/dart/lib/main_with_vm_service.dart
+++ frb_example/old/pure_dart/dart/lib/main_with_vm_service.dart
@@ -1,11 +1,12 @@
 import 'package:flutter_rust_bridge/flutter_rust_bridge.dart';
+
 // ignore: implementation_imports
 import 'package:flutter_rust_bridge/src/ffi/dart_cobject.dart' as dart_cobject;
 import 'package:flutter_rust_bridge_example/bridge_definitions.dart';
 import 'package:test/test.dart';
 import 'ffi.io.dart' if (dart.library.html) 'ffi.web.dart';
 import 'main.dart';
-import 'test_flutter_memory_leak_utility.dart';
+import '../../../../pure_dart/test/utils/test_flutter_memory_leak_utility.dart';
 
 const isWeb = bool.fromEnvironment('dart.library.html');
 
