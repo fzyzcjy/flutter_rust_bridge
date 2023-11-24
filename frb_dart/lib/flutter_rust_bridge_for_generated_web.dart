2ab5e890df86817984ec90ac296147b679f10cd7
fzyzcjy <ch271828n@outlook.com>
2023-11-24 18:33:18 +0800

    feat: import

--- frb_dart/lib/flutter_rust_bridge_for_generated_web.dart
+++ frb_dart/lib/flutter_rust_bridge_for_generated_web.dart
@@ -1,6 +1,8 @@
 /// {@macro flutter_rust_bridge.only_for_generated_code}
 library;
 
+export 'package:js/js.dart';
+
 export 'flutter_rust_bridge_for_generated_common.dart';
 export 'src/main_components/wire/_web.dart';
 export 'src/manual_impl/_web.dart';
