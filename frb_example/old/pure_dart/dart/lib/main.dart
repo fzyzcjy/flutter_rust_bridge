54819dcd5f0a0a33893ebffc557d7bd01d3ed56e
fzyzcjy <ch271828n@outlook.com>
2023-11-24 22:13:58 +0800

    feat: prim list

--- frb_example/old/pure_dart/dart/lib/main.dart
+++ frb_example/old/pure_dart/dart/lib/main.dart
@@ -61,66 +61,6 @@ void main(List<String> args) async {
         handleVecU8Sync(v: Uint8List.fromList(List.filled(len, 127))), Uint8List.fromList(List.filled(len * 2, 127)));
   });
 
-  test('dart call handleVecOfPrimitive', () async {
-    final n = 10000;
-    final resp = await handleVecOfPrimitive(n: n);
-    expect(resp.uint8List, Uint8List.fromList(List.filled(n, 42)));
-    expect(resp.int8List, Int8List.fromList(List.filled(n, 42)));
-    expect(resp.uint16List, Uint16List.fromList(List.filled(n, 42)));
-    expect(resp.int16List, Int16List.fromList(List.filled(n, 42)));
-    expect(resp.uint32List, Uint32List.fromList(List.filled(n, 42)));
-    expect(resp.int32List, Int32List.fromList(List.filled(n, 42)));
-    expect(resp.float32List, Float32List.fromList(List.filled(n, 42)));
-    expect(resp.float64List, Float64List.fromList(List.filled(n, 42)));
-    expect(resp.uint64List, Uint64List.fromList(List.filled(n, 42)));
-    expect(resp.int64List, Int64List.fromList(List.filled(n, 42)));
-    expect(resp.boolList, List.filled(n, true));
-  });
-  test('dart call handleVecOfPrimitiveSync', () {
-    final n = 10000;
-    final resp = handleVecOfPrimitiveSync(n: n);
-    expect(resp.uint8List, Uint8List.fromList(List.filled(n, 42)));
-    expect(resp.int8List, Int8List.fromList(List.filled(n, 42)));
-    expect(resp.uint16List, Uint16List.fromList(List.filled(n, 42)));
-    expect(resp.int16List, Int16List.fromList(List.filled(n, 42)));
-    expect(resp.uint32List, Uint32List.fromList(List.filled(n, 42)));
-    expect(resp.int32List, Int32List.fromList(List.filled(n, 42)));
-    expect(resp.float32List, Float32List.fromList(List.filled(n, 42)));
-    expect(resp.float64List, Float64List.fromList(List.filled(n, 42)));
-    expect(resp.uint64List, Uint64List.fromList(List.filled(n, 42)));
-    expect(resp.int64List, Int64List.fromList(List.filled(n, 42)));
-    expect(resp.boolList, List.filled(n, true));
-  });
-
-  test('dart call handleZeroCopyVecOfPrimitive', () async {
-    final n = 10000;
-    final resp = await handleZeroCopyVecOfPrimitive(n: n);
-    expect(resp.uint8List, Uint8List.fromList(List.filled(n, 42)));
-    expect(resp.int8List, Int8List.fromList(List.filled(n, 42)));
-    expect(resp.uint16List, Uint16List.fromList(List.filled(n, 42)));
-    expect(resp.int16List, Int16List.fromList(List.filled(n, 42)));
-    expect(resp.uint32List, Uint32List.fromList(List.filled(n, 42)));
-    expect(resp.int32List, Int32List.fromList(List.filled(n, 42)));
-    expect(resp.float32List, Float32List.fromList(List.filled(n, 42)));
-    expect(resp.float64List, Float64List.fromList(List.filled(n, 42)));
-    expect(resp.uint64List, Uint64List.fromList(List.filled(n, 42)));
-    expect(resp.int64List, Int64List.fromList(List.filled(n, 42)));
-  });
-  test('dart call handleZeroCopyVecOfPrimitiveSync', () {
-    final n = 10000;
-    final resp = handleZeroCopyVecOfPrimitiveSync(n: n);
-    expect(resp.uint8List, Uint8List.fromList(List.filled(n, 42)));
-    expect(resp.int8List, Int8List.fromList(List.filled(n, 42)));
-    expect(resp.uint16List, Uint16List.fromList(List.filled(n, 42)));
-    expect(resp.int16List, Int16List.fromList(List.filled(n, 42)));
-    expect(resp.uint32List, Uint32List.fromList(List.filled(n, 42)));
-    expect(resp.int32List, Int32List.fromList(List.filled(n, 42)));
-    expect(resp.float32List, Float32List.fromList(List.filled(n, 42)));
-    expect(resp.float64List, Float64List.fromList(List.filled(n, 42)));
-    expect(resp.uint64List, Uint64List.fromList(List.filled(n, 42)));
-    expect(resp.int64List, Int64List.fromList(List.filled(n, 42)));
-  });
-
   test('dart call handleStruct', () async {
     final structResp =
         await handleStruct(arg: MySize(width: 42, height: 100), boxed: MySize(width: 1000, height: 10000));
---
+++ frb_example/pure_dart/test/api/primitive_list_misc_test.dart
@@ -0,0 +1,72 @@
+import 'dart:typed_data';
+
+import 'package:frb_example_pure_dart/src/rust/frb_generated.dart';
+import 'package:test/test.dart';
+
+Future<void> main() async {
+  await RustLib.init();
+
+  test('dart call handleVecOfPrimitive', () async {
+    final n = 10000;
+    final resp = await handleVecOfPrimitive(n: n);
+    expect(resp.uint8List, Uint8List.fromList(List.filled(n, 42)));
+    expect(resp.int8List, Int8List.fromList(List.filled(n, 42)));
+    expect(resp.uint16List, Uint16List.fromList(List.filled(n, 42)));
+    expect(resp.int16List, Int16List.fromList(List.filled(n, 42)));
+    expect(resp.uint32List, Uint32List.fromList(List.filled(n, 42)));
+    expect(resp.int32List, Int32List.fromList(List.filled(n, 42)));
+    expect(resp.float32List, Float32List.fromList(List.filled(n, 42)));
+    expect(resp.float64List, Float64List.fromList(List.filled(n, 42)));
+    expect(resp.uint64List, Uint64List.fromList(List.filled(n, 42)));
+    expect(resp.int64List, Int64List.fromList(List.filled(n, 42)));
+    expect(resp.boolList, List.filled(n, true));
+  });
+
+  // TODO rm?
+  // test('dart call handleVecOfPrimitiveSync', () {
+  //   final n = 10000;
+  //   final resp = handleVecOfPrimitiveSync(n: n);
+  //   expect(resp.uint8List, Uint8List.fromList(List.filled(n, 42)));
+  //   expect(resp.int8List, Int8List.fromList(List.filled(n, 42)));
+  //   expect(resp.uint16List, Uint16List.fromList(List.filled(n, 42)));
+  //   expect(resp.int16List, Int16List.fromList(List.filled(n, 42)));
+  //   expect(resp.uint32List, Uint32List.fromList(List.filled(n, 42)));
+  //   expect(resp.int32List, Int32List.fromList(List.filled(n, 42)));
+  //   expect(resp.float32List, Float32List.fromList(List.filled(n, 42)));
+  //   expect(resp.float64List, Float64List.fromList(List.filled(n, 42)));
+  //   expect(resp.uint64List, Uint64List.fromList(List.filled(n, 42)));
+  //   expect(resp.int64List, Int64List.fromList(List.filled(n, 42)));
+  //   expect(resp.boolList, List.filled(n, true));
+  // });
+
+  test('dart call handleZeroCopyVecOfPrimitive', () async {
+    final n = 10000;
+    final resp = await handleZeroCopyVecOfPrimitive(n: n);
+    expect(resp.uint8List, Uint8List.fromList(List.filled(n, 42)));
+    expect(resp.int8List, Int8List.fromList(List.filled(n, 42)));
+    expect(resp.uint16List, Uint16List.fromList(List.filled(n, 42)));
+    expect(resp.int16List, Int16List.fromList(List.filled(n, 42)));
+    expect(resp.uint32List, Uint32List.fromList(List.filled(n, 42)));
+    expect(resp.int32List, Int32List.fromList(List.filled(n, 42)));
+    expect(resp.float32List, Float32List.fromList(List.filled(n, 42)));
+    expect(resp.float64List, Float64List.fromList(List.filled(n, 42)));
+    expect(resp.uint64List, Uint64List.fromList(List.filled(n, 42)));
+    expect(resp.int64List, Int64List.fromList(List.filled(n, 42)));
+  });
+
+  // TODO rm?
+  // test('dart call handleZeroCopyVecOfPrimitiveSync', () {
+  //   final n = 10000;
+  //   final resp = handleZeroCopyVecOfPrimitiveSync(n: n);
+  //   expect(resp.uint8List, Uint8List.fromList(List.filled(n, 42)));
+  //   expect(resp.int8List, Int8List.fromList(List.filled(n, 42)));
+  //   expect(resp.uint16List, Uint16List.fromList(List.filled(n, 42)));
+  //   expect(resp.int16List, Int16List.fromList(List.filled(n, 42)));
+  //   expect(resp.uint32List, Uint32List.fromList(List.filled(n, 42)));
+  //   expect(resp.int32List, Int32List.fromList(List.filled(n, 42)));
+  //   expect(resp.float32List, Float32List.fromList(List.filled(n, 42)));
+  //   expect(resp.float64List, Float64List.fromList(List.filled(n, 42)));
+  //   expect(resp.uint64List, Uint64List.fromList(List.filled(n, 42)));
+  //   expect(resp.int64List, Int64List.fromList(List.filled(n, 42)));
+  // });
+}
