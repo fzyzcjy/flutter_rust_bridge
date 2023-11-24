d7507cf64e43a85a97f6504ef5788e3c610238d3
fzyzcjy <ch271828n@outlook.com>
2023-11-24 09:21:48 +0800

    feat: rust opaque import (not done)

--- frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/misc/ty/rust_opaque.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/misc/ty/rust_opaque.rs
@@ -1,4 +1,10 @@
 use crate::codegen::generator::wire::rust::spec_generator::base::*;
 use crate::codegen::generator::wire::rust::spec_generator::misc::ty::WireRustGeneratorMiscTrait;
 
-impl<'a> WireRustGeneratorMiscTrait for RustOpaqueWireRustGenerator<'a> {}
+impl<'a> WireRustGeneratorMiscTrait for RustOpaqueWireRustGenerator<'a> {
+    fn generate_imports(&self) -> Option<Vec<String>> {
+        // TODO
+        // Some(vec![format!("use {};", path.join("::"))])
+        None
+    }
+}
