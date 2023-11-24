8ac9694597f5af2b225fcc24a859f6c9873f8db2
fzyzcjy <ch271828n@outlook.com>
2023-11-24 22:29:59 +0800

    feat: vm service test

--- frb_example/old/pure_dart/dart/lib/main_with_vm_service.dart
+++ frb_example/old/pure_dart/dart/lib/main_with_vm_service.dart
@@ -4,9 +4,10 @@ import 'package:flutter_rust_bridge/flutter_rust_bridge.dart';
 import 'package:flutter_rust_bridge/src/ffi/dart_cobject.dart' as dart_cobject;
 import 'package:flutter_rust_bridge_example/bridge_definitions.dart';
 import 'package:test/test.dart';
+
+import '../../../../pure_dart/test/utils/test_flutter_memory_leak_utility.dart';
 import 'ffi.io.dart' if (dart.library.html) 'ffi.web.dart';
 import 'main.dart';
-import '../../../../pure_dart/test/utils/test_flutter_memory_leak_utility.dart';
 
 const isWeb = bool.fromEnvironment('dart.library.html');
 
@@ -17,65 +18,4 @@ void main(List<String> args) async {
   print('flutter_rust_bridge example program start (dylibPath=$dylibPath)');
   print('construct api');
   final api = initializeExternalLibrary(dylibPath);
-  final vmService = await VmServiceUtil.create();
-  tearDownAll(() {
-    api.dispose();
-    vmService.dispose();
-  });
-
-  group('sync return', () {
-    test('allocate a lot of zero copy data to check that it is properly freed', () async {
-      const n = 10000;
-      int calls = 0;
-      dart_cobject.testTool!.onExternalTypedDataFinalizer.add(expectAsync1(
-        (length) {
-          expect(length, n);
-          calls++;
-        },
-        count: 10,
-        reason: "Finalizer must be called once for each returned packed primitive list",
-      ));
-
-      ZeroCopyVecOfPrimitivePack? primitivePack = api.handleZeroCopyVecOfPrimitiveSync(n: n);
-      await vmService.gc();
-      await Future<void>.delayed(const Duration(milliseconds: 10));
-      expect(primitivePack, isNotNull);
-      expect(calls, 0);
-
-      primitivePack = null;
-      await vmService.gc();
-      await Future<void>.delayed(const Duration(milliseconds: 10));
-    });
-  });
-
-  group('dart opaque type', () {
-    group('GC', () {
-      test('drop', () async {
-        Uint8List? strongRef = createLargeList(mb: 300);
-        final weakRef = WeakReference(strongRef);
-        await api.setStaticDartOpaque(opaque: strongRef);
-        strongRef = null;
-
-        await vmService.gc();
-        await Future<void>.delayed(const Duration(milliseconds: 10));
-        expect(weakRef.target, isNotNull);
-
-        await api.dropStaticDartOpaque();
-        await vmService.gc();
-        await Future<void>.delayed(const Duration(milliseconds: 10));
-        expect(weakRef.target, isNull);
-      });
-
-      test('unwrap', () async {
-        Uint8List? strongRef = createLargeList(mb: 300);
-        final weakRef = WeakReference(strongRef);
-        expect(api.unwrapDartOpaque(opaque: strongRef), 'Test');
-        strongRef = null;
-
-        await vmService.gc();
-        await Future<void>.delayed(const Duration(milliseconds: 10));
-        expect(weakRef.target, isNull);
-      });
-    });
-  });
 }
---
+++ frb_example/pure_dart/test/with_vm_service_test.dart
@@ -0,0 +1,73 @@
+import 'dart:typed_data';
+
+import 'package:frb_example_pure_dart/src/rust/api/dart_opaque.dart';
+import 'package:frb_example_pure_dart/src/rust/api/dart_opaque_sync.dart';
+import 'package:frb_example_pure_dart/src/rust/frb_generated.dart';
+import 'package:test/test.dart';
+
+import 'test_utils.dart';
+import 'utils/test_flutter_memory_leak_utility.dart';
+
+// TODO handle infra that creates vm service
+Future<void> main() async {
+  await RustLib.init();
+
+  final vmService = await VmServiceUtil.create();
+  tearDownAll(() => vmService.dispose());
+
+  group('sync return', () {
+    test('allocate a lot of zero copy data to check that it is properly freed', () async {
+      const n = 10000;
+      int calls = 0;
+      dart_cobject.testTool!.onExternalTypedDataFinalizer.add(expectAsync1(
+        (length) {
+          expect(length, n);
+          calls++;
+        },
+        count: 10,
+        reason: "Finalizer must be called once for each returned packed primitive list",
+      ));
+
+      ZeroCopyVecOfPrimitivePack? primitivePack = handleZeroCopyVecOfPrimitiveSync(n: n);
+      await vmService.gc();
+      await Future<void>.delayed(const Duration(milliseconds: 10));
+      expect(primitivePack, isNotNull);
+      expect(calls, 0);
+
+      primitivePack = null;
+      await vmService.gc();
+      await Future<void>.delayed(const Duration(milliseconds: 10));
+    });
+  });
+
+  group('dart opaque type', () {
+    group('GC', () {
+      test('drop', () async {
+        Uint8List? strongRef = createLargeList(mb: 300);
+        final weakRef = WeakReference(strongRef);
+        await setStaticDartOpaque(opaque: strongRef);
+        strongRef = null;
+
+        await vmService.gc();
+        await Future<void>.delayed(const Duration(milliseconds: 10));
+        expect(weakRef.target, isNotNull);
+
+        await dropStaticDartOpaque();
+        await vmService.gc();
+        await Future<void>.delayed(const Duration(milliseconds: 10));
+        expect(weakRef.target, isNull);
+      });
+
+      test('unwrap', () async {
+        Uint8List? strongRef = createLargeList(mb: 300);
+        final weakRef = WeakReference(strongRef);
+        expect(unwrapDartOpaque(opaque: strongRef), 'Test');
+        strongRef = null;
+
+        await vmService.gc();
+        await Future<void>.delayed(const Duration(milliseconds: 10));
+        expect(weakRef.target, isNull);
+      });
+    });
+  });
+}
