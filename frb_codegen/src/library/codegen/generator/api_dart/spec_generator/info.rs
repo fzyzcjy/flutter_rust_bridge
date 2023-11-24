301c20b305993f8f6fc138ef36de8607ed7b330b
fzyzcjy <ch271828n@outlook.com>
2023-11-24 09:01:18 +0800

    fix: avoid prefix

--- frb_codegen/src/library/codegen/generator/api_dart/spec_generator/info.rs
+++ frb_codegen/src/library/codegen/generator/api_dart/spec_generator/info.rs
@@ -183,7 +183,7 @@ impl<'a> ApiDartGeneratorInfoTrait for RecordApiDartGenerator<'a> {
 
 impl<'a> ApiDartGeneratorInfoTrait for RustOpaqueApiDartGenerator<'a> {
     fn dart_api_type(&self) -> String {
-        rust_type_to_dart_type(&self.ir.rust_api_type())
+        rust_type_to_dart_type(&self.ir.inner.rust_api_type())
     }
 }
 
