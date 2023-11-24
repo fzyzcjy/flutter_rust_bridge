ee0ef0c8654362e9004c263fa82e1e4d805a2d1e
fzyzcjy <ch271828n@outlook.com>
2023-11-24 08:37:53 +0800

    feat: dump content

--- frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/dump.rs
+++ frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/dump.rs
@@ -1,10 +1,40 @@
+use crate::codegen::generator::misc::target::Target;
+use crate::codegen::generator::wire::dart::spec_generator::base::{
+    WireDartGenerator, WireDartGeneratorContext,
+};
+use crate::codegen::ir::pack::IrPackComputedCache;
 use serde::Serialize;
+use std::collections::HashMap;
+use strum::IntoEnumIterator;
 
 #[derive(Serialize)]
 pub(crate) struct WireDartDumpInfo {
-    // TODO
+    types: Vec<WireDartDumpInfoType>,
 }
 
-pub(super) fn generate_dump_info() -> WireDartDumpInfo {
-    todo!()
+#[derive(Serialize)]
+pub(crate) struct WireDartDumpInfoType {
+    safe_ident: String,
+    dart_wire_type: HashMap<Target, String>,
+}
+
+pub(super) fn generate_dump_info(
+    cache: IrPackComputedCache,
+    context: WireDartGeneratorContext,
+) -> WireDartDumpInfo {
+    WireDartDumpInfo {
+        types: cache
+            .distinct_types
+            .iter()
+            .map(|ty| {
+                let gen = WireDartGenerator::new(ty, context);
+                WireDartDumpInfoType {
+                    safe_ident: ty.safe_ident(),
+                    dart_wire_type: Target::iter()
+                        .map(|target| (target, gen.dart_wire_type(target)))
+                        .collect(),
+                }
+            })
+            .collect(),
+    }
 }
--- frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/dump.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/dump.rs
@@ -1,10 +1,47 @@
+use crate::codegen::generator::misc::target::Target;
+use crate::codegen::generator::wire::dart::spec_generator::base::WireRustGeneratorContext;
+use crate::codegen::generator::wire::rust::spec_generator::base::WireRustGenerator;
+use crate::codegen::ir::pack::IrPackComputedCache;
 use serde::Serialize;
+use std::collections::HashMap;
+use strum::IntoEnumIterator;
 
 #[derive(Serialize)]
-pub(crate) struct WireRustDartDumpInfo {
-    // TODO
+pub(crate) struct WireRustDumpInfo {
+    types: Vec<WireRustDumpInfoType>,
 }
 
-pub(super) fn generate_dump_info() -> WireRustDartDumpInfo {
-    todo!()
+#[derive(Serialize)]
+pub(crate) struct WireRustDumpInfoType {
+    safe_ident: String,
+    rust_wire_type: HashMap<Target, String>,
+    rust_wire_modifier: HashMap<Target, String>,
+    intodart_type: String,
+    wrapper_struct_name: Option<String>,
+}
+
+pub(super) fn generate_dump_info(
+    cache: IrPackComputedCache,
+    context: WireRustGeneratorContext,
+) -> WireRustDumpInfo {
+    WireRustDumpInfo {
+        types: cache
+            .distinct_types
+            .iter()
+            .map(|ty| {
+                let gen = WireRustGenerator::new(ty, context);
+                WireRustDumpInfoType {
+                    safe_ident: ty.safe_ident(),
+                    rust_wire_type: Target::iter()
+                        .map(|target| (target, gen.rust_wire_type(target)))
+                        .collect(),
+                    rust_wire_modifier: Target::iter()
+                        .map(|target| (target, gen.rust_wire_modifier(target)))
+                        .collect(),
+                    intodart_type: gen.intodart_type(context.ir_pack),
+                    wrapper_struct_name: gen.wrapper_struct_name(),
+                }
+            })
+            .collect(),
+    }
 }
