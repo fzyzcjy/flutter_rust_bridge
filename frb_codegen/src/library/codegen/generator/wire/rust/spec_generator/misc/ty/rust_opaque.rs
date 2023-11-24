8d9f9583bff42bf29ef26f8bea4eaf2638dec84f
fzyzcjy <ch271828n@outlook.com>
2023-11-24 13:11:04 +0800

    fix: err

--- frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/misc/ty/rust_opaque.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/misc/ty/rust_opaque.rs
@@ -3,6 +3,6 @@ use crate::codegen::generator::wire::rust::spec_generator::misc::ty::WireRustGen
 
 impl<'a> WireRustGeneratorMiscTrait for RustOpaqueWireRustGenerator<'a> {
     fn generate_imports(&self) -> Option<Vec<String>> {
-        Some(vec![self.ir.namespace.joined_path.clone()])
+        Some(vec![format!("use {};", self.ir.namespace.joined_path)])
     }
 }
