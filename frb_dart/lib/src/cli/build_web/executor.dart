4e355b403a9915267b9229d3260d90155580a109
fzyzcjy <ch271828n@outlook.com>
2023-11-25 20:58:20 +0800

    feat: dart compile js

--- frb_dart/lib/src/cli/build_web/executor.dart
+++ frb_dart/lib/src/cli/build_web/executor.dart
@@ -13,6 +13,9 @@ class BuildWebArgs {
   final bool release;
 
   /// {@macro flutter_rust_bridge.cli}
+  final bool verbose;
+
+  /// {@macro flutter_rust_bridge.cli}
   final String rustCrateDir;
 
   /// {@macro flutter_rust_bridge.cli}
@@ -28,6 +31,7 @@ class BuildWebArgs {
   const BuildWebArgs({
     required this.wasmOutput,
     required this.release,
+    required this.verbose,
     required this.rustCrateDir,
     required this.wasmPackArgs,
     required this.enableWasmBindgen,
@@ -47,9 +51,9 @@ Future<void> executeBuildWeb(BuildWebArgs args) async {
     await _executeWasmBindgen(args, rustCrateName: rustCrateName);
   }
 
-  // TODO
   // if (config.cliOpts.dartInput != null) {
-  //   await _executeDartCompile(config);
+  await _executeDartCompile(args);
+  // TODO
   // } else {
   //   await _executeFlutterBuildWeb(config);
   // }
@@ -133,24 +137,24 @@ Future<void> _executeWasmBindgen(BuildWebArgs args, {required String rustCrateNa
   ]);
 }
 
-// TODO
-// Future<void> _executeDartCompile(Config config) async {
-//   final output = p.basename(config.cliOpts.dartInput!);
-//   await runCommand('dart', [
-//     'compile',
-//     'js',
-//     '-o',
-//     '${config.root}/$output.js',
-//     if (config.cliOpts.release) '-O2',
-//     if (stdout.supportsAnsiEscapes) '--enable-diagnostic-colors',
-//     if (config.cliOpts.verbose) '--verbose',
-//     config.cliOpts.dartInput!,
-//   ]);
-// }
-//
-// Future<void> _executeFlutterBuildWeb(Config config) async {
+Future<void> _executeDartCompile(BuildWebArgs args) async {
+  final output = p.basename(args.dartInput);
+  await runCommand('dart', [
+    'compile',
+    'js',
+    '-o',
+    '${args.root}/$output.js',
+    if (args.release) '-O2',
+    if (stdout.supportsAnsiEscapes) '--enable-diagnostic-colors',
+    if (args.verbose) '--verbose',
+    args.dartInput!,
+  ]);
+}
+
+// TODO we may not need this, since we want to use `flutter run (web)` itself instead
+// Future<void> _executeFlutterBuildWeb(BuildWebArgs config) async {
 //   await runCommand(
 //     'flutter',
-//     ['build', 'web', if (!config.cliOpts.release) '--profile'] + config.restArgs,
+//     ['build', 'web', if (!config.release) '--profile'] + config.restArgs,
 //   );
 // }
