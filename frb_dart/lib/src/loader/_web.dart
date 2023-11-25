b9a0e7879f436dbf1e129764e7ad66fa4ef7789a
fzyzcjy <ch271828n@outlook.com>
2023-11-25 09:40:35 +0800

    feat: dummy impl

--- frb_dart/lib/src/loader/_web.dart
+++ frb_dart/lib/src/loader/_web.dart
@@ -1,9 +1,11 @@
 import 'package:flutter_rust_bridge/src/platform_types/_web.dart';
+import 'package:flutter_rust_bridge/src/wasm_module/_web.dart';
 
 /// {@macro flutter_rust_bridge.only_for_generated_code}
 ExternalLibrary loadExternalLibrary({
   String? defaultExternalLibraryRelativeDirectory,
   String? stem,
 }) {
-  throw Exception('TODO');
+  print('loadExternalLibrary return dummy thing');
+  return WasmModule.initialize(kind: const Modules.noModules(root: 'this-is-dummy-root'));
 }
