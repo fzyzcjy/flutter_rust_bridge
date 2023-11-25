e3ae7e6c0a1bf4f28a0f76b867b292c0198b2f63
fzyzcjy <ch271828n@outlook.com>
2023-11-26 07:53:01 +0800

    chore: logs

--- frb_example/dart_minimal/test/minimal_test.dart
+++ frb_example/dart_minimal/test/minimal_test.dart
@@ -3,9 +3,12 @@ import 'package:frb_example_dart_minimal/src/rust/frb_generated.dart';
 import 'package:test/test.dart';
 
 Future<void> main() async {
+  print('Action: Init rust');
   await RustLib.init();
 
   test('dart call minimalAdder', () async {
+    print('Action: Call rust (before)');
     expect(await minimalAdder(a: 100, b: 200), 300);
+    print('Action: Call rust (after)');
   });
 }
