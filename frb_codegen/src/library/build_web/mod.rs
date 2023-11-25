368eab16bb995b30cff2be4bb62ed7ccdc578e3e
fzyzcjy <ch271828n@outlook.com>
2023-11-25 13:20:50 +0800

    refactor: minor

--- frb_codegen/src/library/build_web/mod.rs
+++ frb_codegen/src/library/build_web/mod.rs
@@ -5,17 +5,17 @@ use std::path::PathBuf;
 use std::{env, fs};
 
 pub fn build(dart_root: Option<PathBuf>, args: Vec<String>) -> anyhow::Result<()> {
-    let dart_root = dart_root
-        .map(|x| Ok(fs::canonicalize(x)?))
-        .unwrap_or_else(|| {
-            fallback_dart_root()
-                .context("Please provide --dart-root, or run command inside a Flutter/Dart package")
-        })?;
+    let dart_root = parse_dart_root(dart_root)?;
     debug!("build dart_root={dart_root:?} args={args:?}");
 
     todo!()
 }
 
-fn fallback_dart_root() -> anyhow::Result<PathBuf> {
-    find_dart_package_dir(&env::current_dir()?)
+fn parse_dart_root(dart_root: Option<PathBuf>) -> anyhow::Result<PathBuf> {
+    dart_root
+        .map(|x| Ok(fs::canonicalize(x)?))
+        .unwrap_or_else(|| {
+            find_dart_package_dir(&env::current_dir()?)
+                .context("Please provide --dart-root, or run command inside a Flutter/Dart package")
+        })
 }
