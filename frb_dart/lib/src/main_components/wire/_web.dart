bf3a22c8bba22a35bb3c2ac33b02c131e65a4971
fzyzcjy <ch271828n@outlook.com>
2023-11-25 20:20:39 +0800

    chore: comments

--- frb_dart/lib/src/main_components/wire/_web.dart
+++ frb_dart/lib/src/main_components/wire/_web.dart
@@ -5,6 +5,7 @@ import 'package:flutter_rust_bridge/src/main_components/wire/wire.dart';
 import 'package:flutter_rust_bridge/src/platform_types/_web.dart';
 import 'package:flutter_rust_bridge/src/wasm_module/_web.dart';
 
+// TODO rm?
 /// {@macro flutter_rust_bridge.only_for_generated_code}
 class BaseWasmWire<T extends WasmModule> extends BaseWire {
   /// {@macro flutter_rust_bridge.only_for_generated_code}
