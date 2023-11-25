b22138a6f52fd10931915221c0f4dfe0a55ba768
fzyzcjy <ch271828n@outlook.com>
2023-11-25 14:05:54 +0800

    refactor: some config

--- frb_dart/lib/src/cli/build_web/config.dart
+++ frb_dart/lib/src/cli/build_web/config.dart
@@ -6,17 +6,71 @@ import 'package:flutter_rust_bridge/src/cli/build_web/executor.dart';
 /// {@endtemplate}
 @CliOptions()
 class Config {
-  // TODO
+  /// {@macro flutter_rust_bridge.cli}
+  @CliOption(
+    abbr: 'c',
+    help: 'Directory of the crate',
+    valueHelp: 'CRATE',
+    defaultsTo: 'native',
+  )
+  late String rustCrateDir;
+
+  /// {@macro flutter_rust_bridge.cli}
+  @CliOption(abbr: 'w', help: 'WASM output path', valueHelp: 'PKG')
+  late String? wasmOutput;
+
+  /// {@macro flutter_rust_bridge.cli}
+  @CliOption(help: 'Compile in release mode', negatable: false)
+  late bool release;
+
+  /// {@macro flutter_rust_bridge.cli}
+  @CliOption(help: 'Arguments passed to wasm-pack')
+  late List<String> wasmPackArgs;
+
+  /// {@macro flutter_rust_bridge.cli}
+  @CliOption(help: 'Arguments passed to wasm-bindgen')
+  late List<String> wasmBindgenArgs;
+
+// migrate to `wasmPackArgs`
+// /// {@macro flutter_rust_bridge.cli}
+// @CliOption(
+//   help: 'A comma-separated list of features to pass to `cargo build`.',
+// )
+// late String? features;
+//
+// /// {@macro flutter_rust_bridge.cli}
+// @CliOption(
+//   help: 'Whether to disable all features, useful with --features',
+//   negatable: false,
+// )
+// late bool noDefaultFeatures;
+
+// migrate to `wasmPackArgs`
+// /// {@macro flutter_rust_bridge.cli}
+// @CliOption(
+//   help: 'Enable the weak references proposal\n'
+//       'Requires wasm-bindgen in path',
+// )
+// late bool weakRefs;
+//
+// /// {@macro flutter_rust_bridge.cli}
+// @CliOption(
+//   help: 'Enable the reference types proposal\n'
+//       'Requires wasm-bindgen in path',
+// )
+// late bool referenceTypes;
 }
 
 /// {@macro flutter_rust_bridge.cli}
 BuildWebArgs parseConfig(List<String> args) {
+  final Config config = TODO;
+
   return BuildWebArgs(
-    wasmOutput: wasmOutput,
-    release: release,
-    rustCrateDir: rustCrateDir,
-    wasmPackArgs: wasmPackArgs,
-    enableWasmBindgen: enableWasmBindgen,
-    wasmBindgenArgs: wasmBindgenArgs,
+    wasmOutput: config.wasmOutput,
+    release: config.release,
+    rustCrateDir: config.rustCrateDir,
+    wasmPackArgs: config.wasmPackArgs,
+    enableWasmBindgen: config.wasmBindgenArgs.isNotEmpty,
+    wasmBindgenArgs: config.wasmBindgenArgs,
   );
 }
--- frb_dart/lib/src/cli/serve/config.dart
+++ frb_dart/lib/src/cli/serve/config.dart
@@ -30,24 +30,11 @@ class Opts {
 
   /// {@macro flutter_rust_bridge.cli}
   @CliOption(
-    abbr: 'c',
-    help: 'Directory of the crate',
-    valueHelp: 'CRATE',
-    defaultsTo: 'native',
-  )
-  late String crate;
-
-  /// {@macro flutter_rust_bridge.cli}
-  @CliOption(
     abbr: 'd',
     help: 'Run "dart compile" with the specified input instead of "flutter build"',
     valueHelp: 'ENTRY',
   )
   late String? dartInput;
-
-  /// {@macro flutter_rust_bridge.cli}
-  @CliOption(abbr: 'w', help: 'WASM output path', valueHelp: 'PKG')
-  late String? wasmOutput;
 
   /// {@macro flutter_rust_bridge.cli}
   @CliOption(abbr: 'v', help: 'Display more verbose information')
@@ -72,43 +59,12 @@ class Opts {
   late bool runTests;
 
   /// {@macro flutter_rust_bridge.cli}
-  @CliOption(help: 'Compile in release mode', negatable: false)
-  late bool release;
-
-  /// {@macro flutter_rust_bridge.cli}
-  @CliOption(
-    help: 'Enable the weak references proposal\n'
-        'Requires wasm-bindgen in path',
-  )
-  late bool weakRefs;
-
-  /// {@macro flutter_rust_bridge.cli}
-  @CliOption(
-    help: 'Enable the reference types proposal\n'
-        'Requires wasm-bindgen in path',
-  )
-  late bool referenceTypes;
-
-  /// {@macro flutter_rust_bridge.cli}
   @CliOption(abbr: 'h', help: 'Print this help message', negatable: false)
   late bool help;
 
   /// {@macro flutter_rust_bridge.cli}
   @CliOption(help: 'Whether to build the library.', defaultsTo: true)
   late bool build;
-
-  /// {@macro flutter_rust_bridge.cli}
-  @CliOption(
-    help: 'A comma-separated list of features to pass to `cargo build`.',
-  )
-  late String? features;
-
-  /// {@macro flutter_rust_bridge.cli}
-  @CliOption(
-    help: 'Whether to disable all features, useful with --features',
-    negatable: false,
-  )
-  late bool noDefaultFeatures;
 }
 
 /// {@macro flutter_rust_bridge.internal}
