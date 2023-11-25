6de58254fc714d8fe1dac73b73156cdf20fc8770
fzyzcjy <ch271828n@outlook.com>
2023-11-25 20:02:26 +0800

    fix: compile

--- frb_example/dart_minimal/test/minimal_test.dart
+++ frb_example/dart_minimal/test/minimal_test.dart
@@ -1,4 +1,5 @@
 import 'package:frb_example_dart_minimal/src/rust/api/minimal.dart';
+import 'package:frb_example_dart_minimal/src/rust/frb_generated.dart';
 import 'package:test/test.dart';
 
 Future<void> main() async {
