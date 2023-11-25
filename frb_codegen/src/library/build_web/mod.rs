c2e0c9a21b137413f0f4c63cbe2c841c0ac2e22b
fzyzcjy <ch271828n@outlook.com>
2023-11-25 13:19:47 +0800

    feat: more msg

--- frb_codegen/src/library/build_web/mod.rs
+++ frb_codegen/src/library/build_web/mod.rs
@@ -1,15 +1,18 @@
 use crate::utils::path_utils::find_dart_package_dir;
 use anyhow::Context;
 use log::debug;
-use std::env;
 use std::path::PathBuf;
+use std::{env, fs};
 
 pub fn build(dart_root: Option<PathBuf>, args: Vec<String>) -> anyhow::Result<()> {
-    let dart_root = dart_root.map(Ok).unwrap_or_else(|| {
-        fallback_dart_root()
-            .context("Please provide --dart-root, or run command inside a Flutter/Dart package")
-    })?;
+    let dart_root = dart_root
+        .map(|x| Ok(fs::canonicalize(x)?))
+        .unwrap_or_else(|| {
+            fallback_dart_root()
+                .context("Please provide --dart-root, or run command inside a Flutter/Dart package")
+        })?;
     debug!("build dart_root={dart_root:?} args={args:?}");
+
     todo!()
 }
 
