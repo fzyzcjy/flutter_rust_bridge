7d3c769d8396dc48e0d3da71f09d9c6473d0671b
fzyzcjy <ch271828n@outlook.com>
2023-11-26 09:32:43 +0800

    refactor: rm flutter things in dart_minimal

--- frb_example/dart_minimal/test/minimal_test.dart
+++ frb_example/dart_minimal/test/minimal_test.dart
@@ -1,11 +1,8 @@
 import 'package:frb_example_dart_minimal/src/rust/api/minimal.dart';
 import 'package:frb_example_dart_minimal/src/rust/frb_generated.dart';
 import 'package:test/test.dart';
-import 'package:integration_test/integration_test.dart';
 
 Future<void> main() async {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-
   print('Action: Init rust (before)');
   await RustLib.init();
   print('Action: Init rust (after)');
