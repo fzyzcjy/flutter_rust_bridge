98f751c48beed8a742e1ad63cfec27986467f810
fzyzcjy <ch271828n@outlook.com>
2023-11-26 16:36:04 +0800

    refactor: extract

--- frb_utils/lib/src/dart_web_test_utils/run_test.dart
+++ frb_utils/lib/src/dart_web_test_utils/run_test.dart
@@ -5,6 +5,7 @@ import 'package:flutter_rust_bridge_utils/src/commands/serve_web_command.dart';
 import 'package:flutter_rust_bridge_utils/src/commands/test_web_command.dart';
 import 'package:flutter_rust_bridge_utils/src/serve_web/run_server.dart';
 import 'package:puppeteer/puppeteer.dart';
+import 'package:shelf/src/handler.dart';
 import 'package:shelf_web_socket/shelf_web_socket.dart';
 
 const kTestResultKey = '__result__';
@@ -14,29 +15,15 @@ Future<void> executeTestWeb(TestWebConfig options) async {
 
   Browser? browser;
 
-  final socketHandler = webSocketHandler((channel) async {
-    await for (final mes in channel.stream) {
-      try {
-        final data = jsonDecode(mes);
-        if (data is Map && data.containsKey(kTestResultKey)) {
-          await browser?.close();
-          exit(data[kTestResultKey] ? 0 : 1);
-        } else {
-          print(data);
-        }
-      } catch (err, st) {
-        print('$err\nStacktrace:\n$st');
-      }
-    }
-  });
-
   await runServer(
     ServeWebConfig(
       webRoot: webRoot,
       port: ServeWebConfig.kDefaultPort,
       open: false,
     ),
-    extraHandler: socketHandler,
+    extraHandler: _createWebSocketHandler(
+      closeBrowser: () async => browser?.close(),
+    ),
   );
 
   print('runTests: puppeteer.launch');
@@ -47,4 +34,22 @@ Future<void> executeTestWeb(TestWebConfig options) async {
 
   print('runTests: page.goto($addr)');
   await page.goto(addr);
+}
+
+Handler _createWebSocketHandler({required Future<void> Function() closeBrowser}) {
+  return webSocketHandler((channel) async {
+    await for (final mes in channel.stream) {
+      try {
+        final data = jsonDecode(mes);
+        if (data is Map && data.containsKey(kTestResultKey)) {
+          await closeBrowser();
+          exit(data[kTestResultKey] ? 0 : 1);
+        } else {
+          print(data);
+        }
+      } catch (err, st) {
+        print('$err\nStacktrace:\n$st');
+      }
+    }
+  });
 }
