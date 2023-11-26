818f186000b4b411c2f3651f012e1b3a6fe5dfb7
fzyzcjy <ch271828n@outlook.com>
2023-11-26 08:31:34 +0800

    feat: temporarily, add flutter etc dependency to *dart* minimal

---
+++ frb_example/dart_minimal/test_driver/integration_test.dart
@@ -0,0 +1,4 @@
+import 'package:integration_test/integration_test_driver.dart';
+
+// https://docs.flutter.dev/cookbook/testing/integration/introduction#5b-web
+Future<void> main() => integrationDriver();
