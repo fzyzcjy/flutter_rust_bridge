c24f0ede80b0fadd735f39c413203d1273835692
fzyzcjy <ch271828n@outlook.com>
2023-11-27 17:24:49 +0800

    feat: more skeleton

--- tools/frb_internal/lib/src/makefile_dart/test.dart
+++ tools/frb_internal/lib/src/makefile_dart/test.dart
@@ -10,6 +10,9 @@ part 'test.g.dart';
 List<Command<void>> createCommands() {
   return [
     SimpleConfigCommand('test', test, _$populateTestConfigParser, _$parseTestConfigResult),
+    SimpleConfigCommand('test-rust', testRust, _$populateTestConfigParser, _$parseTestConfigResult),
+    SimpleConfigCommand('test-dart-native', testDartNative, _$populateTestConfigParser, _$parseTestConfigResult),
+    SimpleConfigCommand('test-dart-web', testDartWeb, _$populateTestConfigParser, _$parseTestConfigResult),
   ];
 }
 
@@ -19,5 +22,19 @@ class TestConfig {
 }
 
 Future<void> test(TestConfig config) async {
+  await testRust(config);
+  await testDartNative(config);
+  await testDartWeb(config);
+}
+
+Future<void> testRust(TestConfig config) async {
+  TODO;
+}
+
+Future<void> testDartNative(TestConfig config) async {
+  TODO;
+}
+
+Future<void> testDartWeb(TestConfig config) async {
   TODO;
 }
