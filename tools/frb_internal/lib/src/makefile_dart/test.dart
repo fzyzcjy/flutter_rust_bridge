d6c6ff1ac5cf1b53184e8aa06907ff769f724bce
fzyzcjy <ch271828n@outlook.com>
2023-11-27 18:13:40 +0800

    chore: rm some

--- tools/frb_internal/lib/src/makefile_dart/test.dart
+++ tools/frb_internal/lib/src/makefile_dart/test.dart
@@ -47,15 +47,12 @@ Future<void> testDartWeb(TestDartConfig config) async {
   TODO;
   '''
   dart_test_web_unit:
-    cd frb_dart && dart pub get
-    cd frb_dart && dart test test/*.dart
     cd frb_dart && dart test -p chrome test/*.dart
   ''';
 
   TODO;
   '''
   dart_test_web_integration features="":
-    just dart_pub_get dart_only
     cd {{dir_example_pure_dart}}/dart && dart run \
       ../../../frb_dart/bin/serve.dart \
       -c ../rust --dart-input lib/main.web.dart --root web/ --run-tests \
