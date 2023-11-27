9e43d63c5a779d32b1ce0898d007c1910a831e74
fzyzcjy <ch271828n@outlook.com>
2023-11-27 13:44:02 +0800

    refactor: GenerateTestCommand

--- tools/frb_internal/bin/flutter_rust_bridge_internal.dart
+++ tools/frb_internal/bin/flutter_rust_bridge_internal.dart
@@ -1,8 +1,9 @@
 import 'package:args/command_runner.dart';
-import 'package:flutter_rust_bridge_internal/src/commands/generate_test_command.dart';
+import 'package:flutter_rust_bridge_internal/src/frb_example_pure_dart_generator/generator.dart' as generator;
+import 'package:flutter_rust_bridge_internal/src/utils/makefile_dart_infra.dart';
 
 Future<void> main(List<String> args) async {
   final runner = CommandRunner<void>('flutter_rust_bridge_internal', '') //
-    ..addCommand(GenerateTestCommand());
+    ..addCommand(SimpleCommand(name: 'generate-test', executor: () => generator.generate()));
   await runner.run(args);
 }
--- tools/frb_internal/lib/src/utils/makefile_dart_infra.dart
+++ tools/frb_internal/lib/src/utils/makefile_dart_infra.dart
@@ -5,14 +5,27 @@ Future<void> execute(String command) async {
   TODO;
 }
 
-List<Command<void>> createCommands() {
-  return TODO;
-}
-
 extension ExtCommandRunner<T> on CommandRunner<T> {
   void addCommands(List<Command<T>> commands) {
     for (final command in commands) {
       addCommand(command);
     }
   }
+}
+
+class SimpleCommand extends Command<void> {
+  @override
+  final String name;
+  @override
+  final String description;
+  final Future<void> Function() executor;
+
+  SimpleCommand({
+    required this.name,
+    this.description = '',
+    required this.executor,
+  });
+
+  @override
+  Future<void> run() async => await executor();
 }
