5551d9044f3ff70c0e7aed63eae790f9de875fc9
fzyzcjy <ch271828n@outlook.com>
2023-11-27 13:41:41 +0800

    chore: infra

--- tools/frb_internal/lib/src/utils/makefile_dart_infra.dart
+++ tools/frb_internal/lib/src/utils/makefile_dart_infra.dart
@@ -1,4 +1,18 @@
 // TODO merge with things in frb_util
+import 'package:args/command_runner.dart';
+
 Future<void> execute(String command) async {
   TODO;
+}
+
+List<Command<void>> createCommands() {
+  return TODO;
+}
+
+extension ExtCommandRunner<T> on CommandRunner<T> {
+  void addCommands(List<Command<T>> commands) {
+    for (final command in commands) {
+      addCommand(command);
+    }
+  }
 }
