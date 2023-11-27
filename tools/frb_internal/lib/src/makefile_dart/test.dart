30528fb6ba42420cd71908f48210fda15067d62a
fzyzcjy <ch271828n@outlook.com>
2023-11-27 17:54:03 +0800

    chore: mv

--- tools/frb_internal/lib/src/makefile_dart/test.dart
+++ tools/frb_internal/lib/src/makefile_dart/test.dart
@@ -40,9 +40,39 @@ Future<void> testRustPackage(String package) async {
 }
 
 Future<void> testDartNative(TestDartConfig config) async {
+  '''
+    just install_ffigen_dependency
+    just dart_pub_get dart_only
+    just dart_test_simple pure_dart
+    just dart_test_simple pure_dart_multi
+    
+    dart_test_simple name:
+    just _dart_test_raw {{name}} ""
+
+  _dart_test_raw name script_prefix:
+    cd frb_example/{{name}}/rust && cargo build --verbose
+    # need to be AOT, since prod environment is AOT, and JIT+valgrind will have strange problems
+    cd frb_example/{{name}}/dart && dart compile exe bin/{{name}}.dart -o main.exe
+    cd frb_example/{{name}}/dart && \
+        {{script_prefix}} ./main.exe \
+        "../../../target/debug/libflutter_rust_bridge_example_{{name}}.{{library_file_ext}}" --chain-stack-traces
+  ''';
   TODO;
 }
 
 Future<void> testDartWeb(TestDartConfig config) async {
+  '''
+  dart_test_web_unit:
+    cd frb_dart && dart pub get
+    cd frb_dart && dart test test/*.dart
+    cd frb_dart && dart test -p chrome test/*.dart
+
+  dart_test_web_integration features="":
+    just dart_pub_get dart_only
+    cd {{dir_example_pure_dart}}/dart && dart run \
+      ../../../frb_dart/bin/serve.dart \
+      -c ../rust --dart-input lib/main.web.dart --root web/ --run-tests \
+      --features={{features}}
+  ''';
   TODO;
 }
