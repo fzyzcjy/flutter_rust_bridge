96ee8172398cf9344efe891409bda5e8f96b45ab
fzyzcjy <ch271828n@outlook.com>
2023-11-27 16:49:27 +0800

    chore: extract

--- tools/frb_internal/lib/src/makefile_dart/codegen.dart
+++ tools/frb_internal/lib/src/makefile_dart/codegen.dart
@@ -7,20 +7,3 @@ List<Command<void>> createCommands() {
     SimpleCommand('codegen-internal', generator.generate),
   ];
 }
-
-const kRustPackages = <String>[
-  'frb_rust',
-  'frb_codegen',
-  'frb_example/dart_minimal/rust',
-  'frb_example/pure_dart/rust',
-  // TODO `with_flutter` example
-];
-
-const kDartPackages = <String>[
-  'frb_dart',
-  'frb_example/dart_minimal',
-  'frb_example/pure_dart',
-  // TODO `with_flutter` example
-  'frb_utils',
-  'tools/frb_internal',
-];
