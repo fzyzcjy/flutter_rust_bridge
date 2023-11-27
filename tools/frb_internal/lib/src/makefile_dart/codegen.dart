65f9e1a3d66636b2291ca2ca668031649fef8f91
fzyzcjy <ch271828n@outlook.com>
2023-11-27 16:51:36 +0800

    refactor: minor

--- tools/frb_internal/lib/src/makefile_dart/codegen.dart
+++ tools/frb_internal/lib/src/makefile_dart/codegen.dart
@@ -1,9 +1,31 @@
 import 'package:args/command_runner.dart';
+import 'package:build_cli_annotations/build_cli_annotations.dart';
 import 'package:flutter_rust_bridge_internal/src/frb_example_pure_dart_generator/generator.dart' as generator;
 import 'package:flutter_rust_bridge_internal/src/utils/makefile_dart_infra.dart';
 
+part 'codegen.g.dart';
+
 List<Command<void>> createCommands() {
   return [
-    SimpleCommand('codegen-internal', generator.generate),
+    SimpleConfigCommand('codegen-internal', codegenInternal, _$populateCodegenConfigParser, _$parseCodegenConfigResult),
   ];
+}
+
+@CliOptions()
+class CodegenConfig {
+  @CliOption(defaultsTo: false)
+  final bool setExitIfChanged;
+
+  const CodegenConfig({
+    required this.setExitIfChanged,
+  });
+}
+
+Future<void> codegenInternal(CodegenConfig config) async {
+  await generator.generate();
+  _maybeSetExitIfChanged(config);
+}
+
+void _maybeSetExitIfChanged(CodegenConfig config) {
+  TODO;
 }
