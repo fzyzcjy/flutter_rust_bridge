51b958053d118b5f46eb6956e4ca3d747acf5946
fzyzcjy <ch271828n@outlook.com>
2023-11-27 13:34:29 +0800

    feat: for loop

--- tools/frb_internal/lib/src/makefile_dart/entrypoint.dart
+++ tools/frb_internal/lib/src/makefile_dart/entrypoint.dart
@@ -5,8 +5,22 @@ import 'package:flutter_rust_bridge_internal/src/utils/execute_process.dart';
 /// Similar to `makefile`/`justfile`/..., but based on Dart
 /// (Why not directly use justfile: Because want more flexible grammar, such as for loops)
 
-const _kRustPackages = <String>[TODO];
-const _kDartPackages = <String>[TODO];
+const _kRustPackages = <String>[
+  'frb_rust',
+  'frb_codegen',
+  'frb_example/dart_minimal/rust',
+  'frb_example/pure_dart/rust',
+  // TODO `with_flutter` example
+];
+
+const _kDartPackages = <String>[
+  'frb_dart',
+  'frb_example/dart_minimal',
+  'frb_example/pure_dart',
+  // TODO `with_flutter` example
+  'frb_utils',
+  'tools/frb_internal',
+];
 
 class LintConfig {
   final bool fix;
