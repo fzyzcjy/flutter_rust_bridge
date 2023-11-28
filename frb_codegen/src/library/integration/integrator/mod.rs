8eb55950a28a1b08170ebc7d26bf32c55881da0b
fzyzcjy <ch271828n@outlook.com>
2023-11-28 19:37:35 +0800

    chore: include dir

--- frb_codegen/src/library/integration/integrator/mod.rs
+++ frb_codegen/src/library/integration/integrator/mod.rs
@@ -1,6 +1,10 @@
 use crate::utils::path_utils::find_dart_package_dir;
+use include_dir::{include_dir, Dir};
 use log::debug;
 use std::env;
+
+static INTEGRATION_TEMPLATE_DIR: Dir<'_> =
+    include_dir!("$CARGO_MANIFEST_DIR/assets/integration_template");
 
 /// Integrate Rust into existing Flutter project.
 pub fn integrate() -> anyhow::Result<()> {
