9b43b16d6aa5d7fab58e855eeb78f18b1009d2af
fzyzcjy <ch271828n@outlook.com>
2023-11-28 11:58:37 +0800

    fix: some dart compile

--- frb_example/pure_dart/test/api/array_test.dart
+++ frb_example/pure_dart/test/api/array_test.dart
@@ -6,31 +6,32 @@ Future<void> main({bool skipRustLibInit = false}) async {
   if (!skipRustLibInit) await RustLib.init();
 
   test('dart call getArray()', () async {
-    var array = await getArray();
+    var array = await getArrayTwinNormal();
     expect(array, [1, 2, 3, 4, 5]);
   });
 
   test('dart call getComplexArray()', () async {
-    final points = await getComplexArray();
+    final points = await getComplexArrayTwinNormal();
 
     expect(points[0].x, 1.0);
     expect(points[1].x, 2.0);
   });
 
   test('MessageId', () async {
-    final MessageId msgid = await newMsgid(id: U8Array32.init());
+    final MessageIdTwinNormal msgid =
+        await newMsgidTwinNormal(id: U8Array32.init());
     msgid.field0[2] = 14;
-    final inner = await useMsgid(id: msgid);
+    final inner = await useMsgidTwinNormal(id: msgid);
     expect(inner[2], 14);
   });
 
   test('BlobId', () async {
     final inner = U8Array1600.init();
     inner[14] = 99;
-    final Blob blob = await boxedBlob(blob: inner);
+    final BlobTwinNormal blob = await boxedBlobTwinNormal(blob: inner);
     expect(blob.field0[14], 99);
     blob.field0[10] = 100;
-    final unboxed = await useBoxedBlob(blob: blob);
+    final unboxed = await useBoxedBlobTwinNormal(blob: blob);
     expect(unboxed[10], 100);
     expect(unboxed[14], 99);
   });
@@ -38,10 +39,11 @@ Future<void> main({bool skipRustLibInit = false}) async {
   test('FeedId', () async {
     final inner = U8Array8.init();
     inner[3] = 3;
-    final FeedId feedId = await returnBoxedFeedId(id: inner);
+    final FeedIdTwinNormal feedId =
+        await returnBoxedFeedIdTwinNormal(id: inner);
     expect(feedId.field0[3], 3);
     feedId.field0[5] = 5;
-    final raw = await returnBoxedRawFeedId(id: feedId);
+    final raw = await returnBoxedRawFeedIdTwinNormal(id: feedId);
     expect(raw[5], 5);
     expect(raw[3], 3);
   });
@@ -50,7 +52,8 @@ Future<void> main({bool skipRustLibInit = false}) async {
     final inner = I32Array2.init();
     inner[0] = 1;
     inner[1] = 2;
-    final testId = await funcTestId(id: TestId(field0: inner));
+    final testId =
+        await funcTestIdTwinNormal(id: TestIdTwinNormal(field0: inner));
     expect(testId.field0[0], 1);
     expect(testId.field0[1], 2);
   });
@@ -58,20 +61,21 @@ Future<void> main({bool skipRustLibInit = false}) async {
   test('LastNumber', () async {
     final array = F64Array16.init();
     array[15] = 88.888;
-    final result = await lastNumber(array: array);
+    final result = await lastNumberTwinNormal(array: array);
     expect(result, 88.888);
   });
 
   test('NestedId', () async {
-    final id0 = TestId(field0: I32Array2.init());
+    final id0 = TestIdTwinNormal(field0: I32Array2.init());
     id0.field0[1] = 10;
-    final id1 = TestId(field0: I32Array2.init());
+    final id1 = TestIdTwinNormal(field0: I32Array2.init());
     id1.field0[1] = 20;
-    final id2 = TestId(field0: I32Array2.init());
+    final id2 = TestIdTwinNormal(field0: I32Array2.init());
     id2.field0[1] = 30;
-    final id3 = TestId(field0: I32Array2.init());
+    final id3 = TestIdTwinNormal(field0: I32Array2.init());
     id3.field0[1] = 40;
-    final x = await nestedId(id: TestIdArray4([id0, id1, id2, id3]));
+    final x = await nestedIdTwinNormal(
+        id: TestIdTwinNormalArray4([id0, id1, id2, id3]));
     expect(x[0].field0[1], 10);
     expect(x[1].field0[1], 40);
   });
