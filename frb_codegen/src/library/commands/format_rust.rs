04164028680e5c1ab785edc9ed045f0a4d09c044
fzyzcjy <ch271828n@outlook.com>
2023-11-28 11:26:55 +0800

    fix: fmt cannot use unc

--- frb_codegen/src/library/commands/format_rust.rs
+++ frb_codegen/src/library/commands/format_rust.rs
@@ -1,11 +1,14 @@
 use crate::library::commands::command_runner::execute_command;
+use crate::library::commands::format_dart::normalize_windows_unc_paths;
+use crate::utils::path_utils::{normalize_windows_unc_path, path_to_string};
 use anyhow::bail;
 use log::debug;
 use std::path::PathBuf;
 
 pub fn format_rust(path: &[PathBuf]) -> anyhow::Result<()> {
+    let path = normalize_windows_unc_paths(path)?;
     debug!("execute format_rust path={path:?}");
-    let res = execute_command("rustfmt", path, None)?;
+    let res = execute_command("rustfmt", &path, None)?;
     if !res.status.success() {
         bail!(
             "Rust formatting failed: {}",
