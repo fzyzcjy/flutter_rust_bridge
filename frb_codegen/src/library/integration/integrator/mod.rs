d0ecd6f4a022482ba2113a58c1d2bd0ad1f16cc1
fzyzcjy <ch271828n@outlook.com>
2023-11-28 19:45:45 +0800

    feat: rough steps

--- frb_codegen/src/library/integration/integrator/mod.rs
+++ frb_codegen/src/library/integration/integrator/mod.rs
@@ -1,4 +1,5 @@
 use crate::utils::path_utils::find_dart_package_dir;
+use anyhow::Result;
 use include_dir::{include_dir, Dir};
 use log::debug;
 use std::env;
@@ -7,9 +8,48 @@ static INTEGRATION_TEMPLATE_DIR: Dir<'_> =
     include_dir!("$CARGO_MANIFEST_DIR/assets/integration_template");
 
 /// Integrate Rust into existing Flutter project.
-pub fn integrate() -> anyhow::Result<()> {
+pub fn integrate() -> Result<()> {
     let dart_root = find_dart_package_dir(&env::current_dir()?)?;
     debug!("integrate dart_root={dart_root:?}");
 
-    todo!("integrate")
+    handle_cargokit_dir()?;
+    handle_rust_dir()?;
+
+    handle_ios_or_macos("ios")?;
+    handle_ios_or_macos("macos")?;
+    handle_windows_or_linux("windows")?;
+    handle_windows_or_linux("linux")?;
+    handle_android()?;
+
+    Ok(())
+}
+
+fn handle_cargokit_dir() -> Result<()> {
+    copy_cargokit_dir()?;
+    cargokit_add_prelude()
+}
+
+fn copy_cargokit_dir() -> Result<()> {
+    todo!()
+}
+
+fn cargokit_add_prelude() -> Result<()> {
+    todo!()
+}
+
+fn handle_rust_dir() -> Result<()> {
+    // TODO the "cdylib + staticlib"
+    todo!()
+}
+
+fn handle_ios_or_macos(dir_name: &str) -> Result<()> {
+    todo!()
+}
+
+fn handle_windows_or_linux(dir_name: &str) -> Result<()> {
+    todo!()
+}
+
+fn handle_android() -> Result<()> {
+    todo!()
 }
