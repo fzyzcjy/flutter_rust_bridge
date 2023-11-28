34fe04da3241fcc4717a74e7c21b669d5bd4b6bf
fzyzcjy <ch271828n@outlook.com>
2023-11-28 16:51:20 +0800

    feat: minimal

---
+++ frb_example/dart_minimal/test/dart_valgrind_test_entrypoint.dart
@@ -0,0 +1,5 @@
+import 'minimal_test.dart' as minimal_test;
+
+Future<void> main() async {
+  await minimal_test.main();
+}
--- frb_example/dart_minimal/test/dart_web_test_entrypoint.dart
+++ frb_example/dart_minimal/test/dart_web_test_entrypoint.dart
@@ -2,8 +2,8 @@ import 'package:flutter_rust_bridge_utils/flutter_rust_bridge_utils_web.dart';
 
 import 'minimal_test.dart' as minimal_test;
 
-void main() {
-  dartWebTestEntrypoint(() async {
+Future<void> main() async {
+  await dartWebTestEntrypoint(() async {
     await minimal_test.main();
   });
 }
