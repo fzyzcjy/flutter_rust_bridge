cd12430ff49107d8e1c5cc2da5126edb1592e821
fzyzcjy <ch271828n@outlook.com>
2023-11-28 18:36:14 +0800

    feat: impl create

--- frb_codegen/src/library/integration/creator.rs
+++ frb_codegen/src/library/integration/creator.rs
@@ -1,11 +1,12 @@
+use crate::integration::integrator;
 use crate::library::commands::flutter_create::flutter_create;
 use log::debug;
+use std::env;
 
 /// Create a new Flutter + Rust project.
 pub fn create(name: &str) -> anyhow::Result<()> {
     debug!("create name={name}");
-
     flutter_create(name)?;
-
-    todo!("create")
+    env::set_current_dir(env::current_dir()?.join(name))?;
+    integrator::integrate()
 }
