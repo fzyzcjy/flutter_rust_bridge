6ca90bf5be2ee22df34f872da2ddaaaccf2da04c
fzyzcjy <ch271828n@outlook.com>
2023-11-28 11:58:51 +0800

    chore: temp rm pseudo manual

--- frb_example/pure_dart/lib/src/rust/api/pseudo_manual/simple_twin_sync.dart
+++
@@ -1,7 +0,0 @@
-// ignore_for_file: invalid_use_of_internal_member, unused_import
-
-import '../../frb_generated.dart';
-import 'package:flutter_rust_bridge/flutter_rust_bridge_for_generated.dart';
-
-int simpleAdderTwinSync({required int a, required int b, dynamic hint}) =>
-    RustLib.instance.api.simpleAdderTwinSync(a: a, b: b, hint: hint);
