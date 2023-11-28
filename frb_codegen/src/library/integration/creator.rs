661328a9d3f9c6baaa35479bb5a356af1c480360
fzyzcjy <ch271828n@outlook.com>
2023-11-28 21:32:37 +0800

    chore: comments

--- frb_codegen/src/library/integration/creator.rs
+++ frb_codegen/src/library/integration/creator.rs
@@ -1,12 +1,15 @@
 use crate::integration::integrator;
 use crate::library::commands::flutter_create::flutter_create;
-use log::debug;
+use log::{debug, info};
 use std::env;
 
 /// Create a new Flutter + Rust project.
 pub fn create(name: &str) -> anyhow::Result<()> {
     debug!("create name={name}");
+
     flutter_create(name)?;
     env::set_current_dir(env::current_dir()?.join(name))?;
+
+    info!("Step: Inject flutter_rust_bridge related code");
     integrator::integrate()
 }
