22174165297d470228541cbfd8854602081f3dbf
fzyzcjy <ch271828n@outlook.com>
2023-11-28 18:37:11 +0800

    feat: integrate get pkg path

--- frb_codegen/src/library/integration/integrator.rs
+++ frb_codegen/src/library/integration/integrator.rs
@@ -1,4 +1,11 @@
+use crate::utils::path_utils::find_dart_package_dir;
+use log::debug;
+use std::env;
+
 /// Integrate Rust into existing Flutter project.
 pub fn integrate() -> anyhow::Result<()> {
+    let dart_root = find_dart_package_dir(&env::current_dir()?)?;
+    debug!("integrate dart_root={dart_root:?}");
+
     todo!("integrate")
 }
