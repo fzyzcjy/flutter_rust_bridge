68e653f97a6272b1f7441e61327510599cac5e0f
fzyzcjy <ch271828n@outlook.com>
2023-11-28 19:48:05 +0800

    feat: copy

--- frb_codegen/src/library/integration/integrator/mod.rs
+++ frb_codegen/src/library/integration/integrator/mod.rs
@@ -3,6 +3,7 @@ use anyhow::Result;
 use include_dir::{include_dir, Dir};
 use log::debug;
 use std::env;
+use std::path::{Path, PathBuf};
 
 static INTEGRATION_TEMPLATE_DIR: Dir<'_> =
     include_dir!("$CARGO_MANIFEST_DIR/assets/integration_template");
@@ -12,44 +13,46 @@ pub fn integrate() -> Result<()> {
     let dart_root = find_dart_package_dir(&env::current_dir()?)?;
     debug!("integrate dart_root={dart_root:?}");
 
-    handle_cargokit_dir()?;
-    handle_rust_dir()?;
+    handle_cargokit_dir(&dart_root)?;
+    handle_rust_dir(&dart_root)?;
 
-    handle_ios_or_macos("ios")?;
-    handle_ios_or_macos("macos")?;
-    handle_windows_or_linux("windows")?;
-    handle_windows_or_linux("linux")?;
-    handle_android()?;
+    handle_ios_or_macos(&dart_root, "ios")?;
+    handle_ios_or_macos(&dart_root, "macos")?;
+    handle_windows_or_linux(&dart_root, "windows")?;
+    handle_windows_or_linux(&dart_root, "linux")?;
+    handle_android(dart_root)?;
 
     Ok(())
 }
 
-fn handle_cargokit_dir() -> Result<()> {
-    copy_cargokit_dir()?;
-    cargokit_add_prelude()
+fn handle_cargokit_dir(dart_root: &Path) -> Result<()> {
+    copy_cargokit_dir(dart_root)?;
+    cargokit_add_prelude(dart_root)
 }
 
-fn copy_cargokit_dir() -> Result<()> {
+fn copy_cargokit_dir(dart_root: &Path) -> Result<()> {
+    let dir_src_cargokit = INTEGRATION_TEMPLATE_DIR.get_dir("cargokit").unwrap();
+    dir_src_cargokit.extract(dart_root.join("cargokit"))?;
+    Ok(())
+}
+
+fn cargokit_add_prelude(dart_root: &Path) -> Result<()> {
     todo!()
 }
 
-fn cargokit_add_prelude() -> Result<()> {
-    todo!()
-}
-
-fn handle_rust_dir() -> Result<()> {
+fn handle_rust_dir(dart_root: &Path) -> Result<()> {
     // TODO the "cdylib + staticlib"
     todo!()
 }
 
-fn handle_ios_or_macos(dir_name: &str) -> Result<()> {
+fn handle_ios_or_macos(dart_root: &Path, dir_name: &str) -> Result<()> {
     todo!()
 }
 
-fn handle_windows_or_linux(dir_name: &str) -> Result<()> {
+fn handle_windows_or_linux(dart_root: &Path, dir_name: &str) -> Result<()> {
     todo!()
 }
 
-fn handle_android() -> Result<()> {
+fn handle_android(dart_root: &Path) -> Result<()> {
     todo!()
 }
