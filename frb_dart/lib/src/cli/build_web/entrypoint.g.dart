380c204df37aacd417485d327bc288cf029558e6
fzyzcjy <ch271828n@outlook.com>
2023-11-28 08:05:45 +0800

    feat: more ignore

--- frb_dart/lib/src/cli/build_web/entrypoint.g.dart
+++ frb_dart/lib/src/cli/build_web/entrypoint.g.dart
@@ -1,5 +1,7 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
+// ignore_for_file: public_member_api_docs
+
 part of 'entrypoint.dart';
 
 // **************************************************************************
