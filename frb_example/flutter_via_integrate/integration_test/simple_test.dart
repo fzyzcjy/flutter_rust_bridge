f52a04c4d69005680e7ef5a5c1a1efb7006fbcb9
fzyzcjy <ch271828n@outlook.com>
2023-11-29 13:43:02 +0800

    feat: exclude semantics

--- frb_example/flutter_via_integrate/integration_test/simple_test.dart
+++ frb_example/flutter_via_integrate/integration_test/simple_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_via_integrate/main.dart';
 import 'package:flutter_via_integrate/src/rust/frb_generated.dart';
@@ -5,7 +6,9 @@ import 'package:flutter_via_integrate/src/rust/frb_generated.dart';
 Future<void> main() async {
   await RustLib.init();
   testWidgets('Can call rust function', (WidgetTester tester) async {
-    await tester.pumpWidget(MyApp());
+    // Temporarily ExcludeSemantics before
+    // https://github.com/flutter/flutter/issues/138135 is fixed
+    await tester.pumpWidget(ExcludeSemantics(child: MyApp()));
     expect(find.textContaining('Result: `Hello, Tom!`'), findsOneWidget);
   });
 }
