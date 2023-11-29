2ef4c7a42c93422006d529cf02ebcbae94b50354
fzyzcjy <ch271828n@outlook.com>
2023-11-29 08:41:32 +0800

    refactor: type

--- frb_dart/lib/src/loader/_io.dart
+++ frb_dart/lib/src/loader/_io.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:ffi' as ffi;
 import 'dart:ffi';
 import 'dart:io';
 
@@ -13,18 +12,15 @@ import 'package:flutter_rust_bridge/src/platform_types/_io.dart';
 FutureOr<ExternalLibrary> loadExternalLibrary(
     ExternalLibraryLoaderConfig config) async {
   final ioDirectory = config.ioDirectory;
-  return ExternalLibrary(
-    ffiDynamicLibrary: loadExternalLibraryRaw(
-      nativeLibDirWhenNonPackaged: ioDirectory == null
-          ? null
-          : Directory.current.uri.resolve(ioDirectory),
-      stem: config.stem,
-    ),
+  return loadExternalLibraryRaw(
+    nativeLibDirWhenNonPackaged:
+        ioDirectory == null ? null : Directory.current.uri.resolve(ioDirectory),
+    stem: config.stem,
   );
 }
 
 /// Please see `loadExternalLibrary` for details
-ffi.DynamicLibrary loadExternalLibraryRaw({
+ExternalLibrary loadExternalLibraryRaw({
   Uri? nativeLibDirWhenNonPackaged,
   required String stem,
 }) {
@@ -32,34 +28,38 @@ ffi.DynamicLibrary loadExternalLibraryRaw({
   // * https://flutter.dev/docs/development/platform-integration/c-interop
   // * https://github.com/fzyzcjy/flutter_rust_bridge/pull/898
 
-  ffi.DynamicLibrary? tryAssumingNonPackaged(String name) {
+  ExternalLibrary? tryAssumingNonPackaged(String name) {
     if (nativeLibDirWhenNonPackaged == null) return null;
     final filePath = nativeLibDirWhenNonPackaged.resolve(name).toFilePath();
     if (!File(filePath).existsSync()) return null;
-    return DynamicLibrary.open(filePath);
+    return ExternalLibrary(ffiDynamicLibrary: DynamicLibrary.open(filePath));
   }
 
   if (Platform.isAndroid) {
-    return DynamicLibrary.open('lib$stem.so');
+    return ExternalLibrary(
+        ffiDynamicLibrary: DynamicLibrary.open('lib$stem.so'));
   }
 
   if (Platform.isIOS) {
-    return DynamicLibrary.process();
+    return ExternalLibrary(ffiDynamicLibrary: DynamicLibrary.process());
   }
 
   if (Platform.isWindows) {
     final name = '$stem.dll';
-    return tryAssumingNonPackaged(name) ?? DynamicLibrary.open(name);
+    return tryAssumingNonPackaged(name) ??
+        ExternalLibrary(ffiDynamicLibrary: DynamicLibrary.open(name));
   }
 
   if (Platform.isMacOS) {
     final name = 'lib$stem.dylib';
-    return tryAssumingNonPackaged(name) ?? DynamicLibrary.process();
+    return tryAssumingNonPackaged(name) ??
+        ExternalLibrary(ffiDynamicLibrary: DynamicLibrary.process());
   }
 
   if (Platform.isLinux) {
     final name = 'lib$stem.so';
-    return tryAssumingNonPackaged(name) ?? DynamicLibrary.open(name);
+    return tryAssumingNonPackaged(name) ??
+        ExternalLibrary(ffiDynamicLibrary: DynamicLibrary.open(name));
   }
 
   // Feel free to PR to add support for more platforms! (e.g. I do not have a Fuchsia device, so cannot test that)
