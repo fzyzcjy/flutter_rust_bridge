3a2f238543219fc8a955b272361632641772ee82
fzyzcjy <ch271828n@outlook.com>
2023-11-29 08:37:14 +0800

    chore: function call

--- frb_example/flutter_via_integrate/lib/main.dart
+++ frb_example/flutter_via_integrate/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_via_integrate/src/rust/api/simple.dart';
 
 Future<void> main() async {
   await RustLib.init();
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
           title: const Text('Flutter Demo'),
         ),
         body: Center(
-          child: Text('1 + 2 = ${add(1, 2)}'),
+          child: Text('1 + 2 = ${add(left: 1, right: 2)}'),
         ),
       ),
     );
