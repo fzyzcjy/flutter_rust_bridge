9cc53ff41c7f6a899dbb9357f52b6d733b8bbda2
fzyzcjy <ch271828n@outlook.com>
2023-11-29 11:36:28 +0800

    fix: init

--- frb_example/flutter_via_integrate/test/simple_test.dart
+++ frb_example/flutter_via_integrate/test/simple_test.dart
@@ -1,7 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_via_integrate/main.dart';
+import 'package:flutter_via_integrate/src/rust/frb_generated.dart';
 
-void main() {
+Future<void> main() async {
+  await RustLib.init();
   testWidgets('Can call rust function', (WidgetTester tester) async {
     await tester.pumpWidget(MyApp());
     expect(find.textContaining('Result: `Hello, Tom!`'), findsOneWidget);
