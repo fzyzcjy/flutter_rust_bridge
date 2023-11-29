b0c3143f900f5e3995efc7c30c38eff9988bfcdd
github-action-benchmark <github@users.noreply.github.com>
2023-11-29 14:40:25 +0000

    add Flutter Rust Bridge Benchmark (customSmallerIsBetter) benchmark result for 5d7815b9999320fedab3e0feea03caa949b7157f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1701268775531,
+  "lastUpdate": 1701268824880,
   "repoUrl": "https://github.com/fzyzcjy/flutter_rust_bridge",
   "entries": {
     "Flutter Rust Bridge Benchmark": [
@@ -38,6 +38,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "PureDart_ComputePrime_Number900000000013_Ubuntu",
             "value": 2056.0307346326836,
+            "unit": "Microseconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ch271828n@outlook.com",
+            "name": "fzyzcjy",
+            "username": "fzyzcjy"
+          },
+          "committer": {
+            "email": "ch271828n@outlook.com",
+            "name": "fzyzcjy",
+            "username": "fzyzcjy"
+          },
+          "distinct": true,
+          "id": "5d7815b9999320fedab3e0feea03caa949b7157f",
+          "message": "refactor: mv name",
+          "timestamp": "2023-11-29T22:37:39+08:00",
+          "tree_id": "b2a1d184dc5ac1df6bb7af2a1a486636a5d87a21",
+          "url": "https://github.com/fzyzcjy/flutter_rust_bridge/commit/5d7815b9999320fedab3e0feea03caa949b7157f"
+        },
+        "date": 1701268821253,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "PureDart_ComputePrime_Number90000049_Windows",
+            "value": 18.706092924733372,
+            "unit": "Microseconds"
+          },
+          {
+            "name": "PureDart_ComputePrime_Number9000000001_Windows",
+            "value": 205.58001238901508,
+            "unit": "Microseconds"
+          },
+          {
+            "name": "PureDart_ComputePrime_Number900000000013_Windows",
+            "value": 2054.5,
             "unit": "Microseconds"
           }
         ]
