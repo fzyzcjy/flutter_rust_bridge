ccaede5cc28fdfa3bab16530b69a94d411ad98d2
fzyzcjy <ch271828n@outlook.com>
2023-11-29 13:38:59 +0800

    Revert "chore: logs"
    This reverts commit 0ba1a70d28df22585f048a441a29e52194ee85fa.

--- frb_example/flutter_via_integrate/integration_test/simple_test.dart
+++ frb_example/flutter_via_integrate/integration_test/simple_test.dart
@@ -3,14 +3,9 @@ import 'package:flutter_via_integrate/main.dart';
 import 'package:flutter_via_integrate/src/rust/frb_generated.dart';
 
 Future<void> main() async {
-  print('hi 1');
   await RustLib.init();
-  print('hi 2');
   testWidgets('Can call rust function', (WidgetTester tester) async {
-    print('hi 3');
     await tester.pumpWidget(MyApp());
-    print('hi 4');
     expect(find.textContaining('Result: `Hello, Tom!`'), findsOneWidget);
-    print('hi 5');
   });
 }
