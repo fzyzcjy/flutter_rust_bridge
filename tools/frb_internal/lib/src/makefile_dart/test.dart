4aefff32d23867a1faf8d8c6d13d7a8fdfb8fe6f
fzyzcjy <ch271828n@outlook.com>
2023-11-29 15:10:20 +0800

    fix: try headless

--- tools/frb_internal/lib/src/makefile_dart/test.dart
+++ tools/frb_internal/lib/src/makefile_dart/test.dart
@@ -163,7 +163,7 @@ Future<void> testFlutterWeb(TestDartConfig config) async {
       'flutter drive '
       '--driver=test_driver/integration_test.dart '
       '--target=integration_test/simple_test.dart '
-      '-d chrome '
+      '-d chrome --headless '
       '--verbose',
       relativePwd: config.package);
 }
