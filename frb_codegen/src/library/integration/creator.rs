a5a498429679537ec95a2553d0a4d262dfccd1ed
fzyzcjy <ch271828n@outlook.com>
2023-11-29 16:44:51 +0800

    feat: remove_unnecessary_files

--- frb_codegen/src/library/integration/creator.rs
+++ frb_codegen/src/library/integration/creator.rs
@@ -1,15 +1,26 @@
 use crate::integration::integrator;
 use crate::library::commands::flutter_create::flutter_create;
 use log::{debug, info};
-use std::env;
+use std::path::{Path, PathBuf};
+use std::{env, fs};
 
 /// Create a new Flutter + Rust project.
 pub fn create(name: &str) -> anyhow::Result<()> {
     debug!("create name={name}");
 
     flutter_create(name)?;
-    env::set_current_dir(env::current_dir()?.join(name))?;
+
+    let dart_root = env::current_dir()?.join(name);
+    env::set_current_dir(&dart_root)?;
+
+    remove_unnecessary_files(&dart_root)?;
 
     info!("Step: Inject flutter_rust_bridge related code");
     integrator::integrate()
+}
+
+fn remove_unnecessary_files(dart_root: &Path) -> anyhow::Result<()> {
+    fs::remove_file(dart_root.join("widget_test.dart"))?;
+    fs::remove_file(dart_root.join("lib").join("main.dart"))?;
+    Ok(())
 }
