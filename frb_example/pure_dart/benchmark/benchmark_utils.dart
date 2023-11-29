974a837b4b0e5bfad2fb83ecc65489d28d551d3d
fzyzcjy <ch271828n@outlook.com>
2023-11-29 18:38:04 +0800

    feat: emitter

--- frb_example/pure_dart/benchmark/benchmark_utils.dart
+++ frb_example/pure_dart/benchmark/benchmark_utils.dart
@@ -1,9 +1,17 @@
 import 'package:benchmark_harness/benchmark_harness.dart';
 
 abstract class EnhancedBenchmarkBase extends BenchmarkBase {
-  const EnhancedBenchmarkBase(super.name);
+  const EnhancedBenchmarkBase(super.name) : super(emitter: const JsonEmitter());
 
   // To opt into the reporting the time per run() instead of per 10 run() calls.
   @override
   void exercise() => run();
+}
+
+class JsonEmitter extends ScoreEmitter {
+  @override
+  void emit(String testName, double value) {
+    // TODO
+    print('$testName(RunTime): $value us.');
+  }
 }
