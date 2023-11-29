7d09e47bcbc05b7e48ac3a8cab6b7f052b1a7e99
fzyzcjy <ch271828n@outlook.com>
2023-11-29 08:38:42 +0800

    chore: import

--- frb_example/flutter_via_integrate/lib/main.dart
+++ frb_example/flutter_via_integrate/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_via_integrate/src/rust/api/simple.dart';
+import 'package:flutter_via_integrate/src/rust/frb_generated.dart';
 
 Future<void> main() async {
   await RustLib.init();
