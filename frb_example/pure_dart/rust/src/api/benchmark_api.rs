3d57a7032bd5545af44da1f0afbca1ab831b0a78
fzyzcjy <ch271828n@outlook.com>
2023-11-30 08:42:08 +0800

    refactor: sum

--- frb_example/pure_dart/rust/src/api/benchmark_api.rs
+++ frb_example/pure_dart/rust/src/api/benchmark_api.rs
@@ -3,7 +3,7 @@
 pub fn benchmark_void_twin_normal() {}
 
 pub fn benchmark_input_bytes_twin_normal(bytes: Vec<u8>) -> i32 {
-    bytes.len() as i32
+    bytes.into_iter().map(|x| x as i32).sum()
 }
 
 pub fn benchmark_output_bytes_twin_normal(size: i32) -> Vec<u8> {
