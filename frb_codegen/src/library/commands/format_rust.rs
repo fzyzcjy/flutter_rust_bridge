2354cf7985cc741e029276db2acbcac8022a0f42
fzyzcjy <ch271828n@outlook.com>
2023-11-30 13:58:54 +0800

    refactor: rustfmt

--- frb_codegen/src/library/commands/format_rust.rs
+++ frb_codegen/src/library/commands/format_rust.rs
@@ -1,4 +1,5 @@
-use crate::library::commands::command_runner::{check_exit_code, execute_command};
+use crate::command_run;
+use crate::library::commands::command_runner::{call_shell, check_exit_code, execute_command};
 use crate::library::commands::format_dart::normalize_windows_unc_paths;
 use log::debug;
 use std::path::PathBuf;
@@ -6,5 +7,5 @@ use std::path::PathBuf;
 pub fn format_rust(path: &[PathBuf]) -> anyhow::Result<()> {
     let path = normalize_windows_unc_paths(path)?;
     debug!("execute format_rust path={path:?}");
-    check_exit_code(&execute_command("rustfmt", &path, None)?)
+    check_exit_code(&command_run!(call_shell[None], "rustfmt", *path)?)
 }
