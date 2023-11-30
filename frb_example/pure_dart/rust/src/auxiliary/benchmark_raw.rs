f40381ed6611484dcd65faec944ed96979543864
fzyzcjy <ch271828n@outlook.com>
2023-11-30 08:53:14 +0800

    feat: output

--- frb_example/pure_dart/rust/src/auxiliary/benchmark_raw.rs
+++ frb_example/pure_dart/rust/src/auxiliary/benchmark_raw.rs
@@ -4,6 +4,7 @@ use crate::api::benchmark_api::{
     benchmark_input_bytes_twin_normal, benchmark_output_bytes_twin_normal,
 };
 use flutter_rust_bridge::support::{new_leak_vec_ptr, vec_from_leak_ptr};
+use flutter_rust_bridge::{Channel, IntoDart, MessagePort, ZeroCopyBuffer};
 
 #[no_mangle]
 pub extern "C" fn benchmark_raw_void_sync() {}
@@ -28,13 +29,13 @@ pub extern "C" fn benchmark_raw_new_list_prim_u_8(len: i32) -> benchmark_raw_lis
 // doing so is only safe if the array was initially allocated by a Vec or String"
 // Thus, the input vec *should* be allocated by our `new_leak_vec_ptr` only.
 #[no_mangle]
-pub unsafe extern "C" fn benchmark_raw_input_bytes_sync(bytes: benchmark_raw_list_prim_u_8) -> i32 {
+pub unsafe extern "C" fn benchmark_raw_input_bytes(bytes: benchmark_raw_list_prim_u_8) -> i32 {
     let vec = vec_from_leak_ptr(bytes.ptr, bytes.len);
     benchmark_input_bytes_twin_normal(vec)
 }
 
 #[no_mangle]
-pub extern "C" fn benchmark_raw_output_bytes_sync(size: i32) -> benchmark_raw_list_prim_u_8 {
+pub extern "C" fn benchmark_raw_output_bytes(port: MessagePort, size: i32) {
     let vec = benchmark_output_bytes_twin_normal(size);
-    todo!()
+    Channel::new(port).post(ZeroCopyBuffer(vec).into_dart());
 }
