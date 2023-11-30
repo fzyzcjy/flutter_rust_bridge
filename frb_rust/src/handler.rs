676a235a8a87684698c6b409b16d1104698430cc
fzyzcjy <ch271828n@outlook.com>
2023-11-30 14:42:50 +0800

    feat: try to copy temporarily

--- frb_rust/src/handler.rs
+++ frb_rust/src/handler.rs
@@ -315,9 +315,49 @@ impl<EH: ErrorHandler> Executor for ThreadPoolExecutor<EH> {
         D: IntoDart,
         Er: IntoDart + 'static,
     {
+        // TODO merge with `execute` case later
         // TODO avoid lock later
+
+        let eh = self.error_handler;
+        let eh2 = self.error_handler;
+
+        let WrapInfo { port, mode, .. } = wrap_info;
+
         let runtime = crate::rust_async::ASYNC_RUNTIME.lock();
-        runtime.spawn(async { todo!() })
+        runtime.spawn(async || {
+            let port2 = port.as_ref().cloned();
+            let thread_result = panic::catch_unwind(move || {
+                let port2 = port2.expect("(worker) thread");
+                #[allow(clippy::clone_on_copy)]
+                let rust2dart = Rust2Dart::new(port2.clone());
+
+                let ret = task(TaskCallback::new(rust2dart.clone()))
+                    .map(|e| e.into_into_dart().into_dart());
+
+                match ret {
+                    Ok(result) => {
+                        match mode {
+                            FfiCallMode::Normal => {
+                                rust2dart.success(result);
+                            }
+                            FfiCallMode::Stream => {
+                                // nothing - ignore the return value of a Stream-typed function
+                            }
+                            FfiCallMode::Sync => {
+                                panic!("FfiCallMode::Sync should not call execute, please call execute_sync instead")
+                            }
+                        }
+                    }
+                    Err(error) => {
+                        eh2.handle_error(port2, Error::CustomError(Box::new(error)));
+                    }
+                };
+            });
+
+            if let Err(error) = thread_result {
+                eh.handle_error(port.expect("(worker) eh"), Error::Panic(error));
+            }
+        });
     }
 }
 
