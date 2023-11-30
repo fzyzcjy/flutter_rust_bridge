bf13541f3d8450d1c5cdc7445716a8fd42652b99
fzyzcjy <ch271828n@outlook.com>
2023-12-01 07:27:36 +0800

    fix: compile

--- frb_rust/src/rust_async/web.rs
+++ frb_rust/src/rust_async/web.rs
@@ -1,9 +1,11 @@
-use tokio::task::spawn_local;
+use std::future::Future;
+use std::thread::JoinHandle;
+use wasm_bindgen_futures::spawn_local;
 
 pub(crate) fn spawn<F>(future: F) -> JoinHandle<F::Output>
 where
     F: Future + Send + 'static,
     F::Output: Send + 'static,
 {
-    spawn_local(future);
+    spawn_local(future)
 }
