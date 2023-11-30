ba8ca063d12549fb36aa4a33d70c65007713596f
fzyzcjy <ch271828n@outlook.com>
2023-12-01 07:25:13 +0800

    chore: comments

--- frb_rust/src/rust_async/io.rs
+++ frb_rust/src/rust_async/io.rs
@@ -13,6 +13,7 @@ where
     F: Future + Send + 'static,
     F::Output: Send + 'static,
 {
+    // TODO for non-root spawn, directly use tokio's spawn and no need to explicitly call this runtime
     let runtime = ASYNC_RUNTIME.lock();
     runtime.spawn(future)
 }
