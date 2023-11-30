d980b16558421602e361551537465074206e2438
fzyzcjy <ch271828n@outlook.com>
2023-11-30 08:42:55 +0800

    feat: part of raw

--- frb_example/pure_dart/rust/src/auxiliary/benchmark_raw.rs
+++ frb_example/pure_dart/rust/src/auxiliary/benchmark_raw.rs
@@ -1,12 +1,19 @@
+use crate::api::benchmark_api::{
+    benchmark_input_bytes_twin_normal, benchmark_output_bytes_twin_normal,
+};
+use crate::frb_generated::wire_list_prim_u_8;
+
 #[no_mangle]
 pub extern "C" fn benchmark_raw_void_sync() {}
 
 #[no_mangle]
-pub extern "C" fn benchmark_raw_input_bytes_sync(ptr: *mut u8, len: i32) {
-    todo!()
+pub extern "C" fn benchmark_raw_input_bytes_sync(ptr: *mut u8, len: i32) -> i32 {
+    let vec = TODO;
+    benchmark_input_bytes_twin_normal(vec)
 }
 
 #[no_mangle]
-pub extern "C" fn benchmark_raw_output_bytes_sync() -> TODO {
+pub extern "C" fn benchmark_raw_output_bytes_sync(size: i32) -> wire_list_prim_u_8 {
+    let vec = benchmark_output_bytes_twin_normal(size);
     todo!()
 }
