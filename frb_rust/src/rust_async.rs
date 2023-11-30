08c0697689a407621d5c4f813d473ccea16d3ca1
fzyzcjy <ch271828n@outlook.com>
2023-11-30 20:56:38 +0800

    refactor: extract

--- frb_rust/src/rust_async.rs
+++ frb_rust/src/rust_async.rs
@@ -1,4 +1,5 @@
 // TODO move this file etc
+// TODO split to io/web
 
 use lazy_static::lazy_static;
 use parking_lot::Mutex;
@@ -6,9 +7,13 @@ use tokio::runtime::Runtime;
 
 lazy_static! {
     // TODO do not be public (but encapsulate)
-    pub static ref ASYNC_RUNTIME: Mutex<Runtime> = Mutex::new(Runtime::new().unwrap());
+    pub static ref ASYNC_RUNTIME: Mutex<Runtime> = Mutex::new(create_runtime());
 }
 
 pub(crate) fn init() {
     // TODO more customizations
+}
+
+fn create_runtime() -> Runtime {
+    Runtime::new().unwrap()
 }
