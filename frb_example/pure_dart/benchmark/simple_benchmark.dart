19c9410be370e2fc672616d5ae5e6557bec4eba4
fzyzcjy <ch271828n@outlook.com>
2023-11-30 09:08:29 +0800

    feat: input bytes

--- frb_example/pure_dart/benchmark/simple_benchmark.dart
+++ frb_example/pure_dart/benchmark/simple_benchmark.dart
@@ -3,6 +3,7 @@
 import 'dart:convert';
 import 'dart:io';
 import 'dart:math';
+import 'dart:typed_data';
 
 import 'package:benchmark_harness/benchmark_harness.dart';
 import 'package:frb_example_pure_dart/src/rust/api/benchmark_api.dart';
@@ -23,6 +24,12 @@ void main(List<String> args) {
   VoidAsyncBenchmark(emitter: emitter).report();
   VoidSyncBenchmark(emitter: emitter).report();
   VoidSyncRawBenchmark(emitter: emitter).report();
+
+  for (final size in [0, 10000, 1000000]) {
+    InputBytesAsyncBenchmark(size, emitter: emitter).report();
+    InputBytesSyncBenchmark(size, emitter: emitter).report();
+    InputBytesSyncRawBenchmark(size, emitter: emitter).report();
+  }
 
   final output = jsonEncode(emitter.items);
   print('Write reports to $pathOutput with output=$output');
@@ -72,4 +79,37 @@ class VoidSyncRawBenchmark extends EnhancedBenchmarkBase {
 
   @override
   void run() => _wire.benchmark_raw_void_sync();
+}
+
+class InputBytesAsyncBenchmark extends AsyncBenchmarkBase {
+  final Uint8List bytes;
+
+  InputBytesAsyncBenchmark(int size, {super.emitter})
+      : bytes = Uint8List(size),
+        super('InputBytesAsync_Size$size');
+
+  @override
+  Future<void> run() async => benchmarkInputBytesTwinNormal(bytes: bytes);
+}
+
+class InputBytesSyncBenchmark extends EnhancedBenchmarkBase {
+  final Uint8List bytes;
+
+  InputBytesSyncBenchmark(int size, {super.emitter})
+      : bytes = Uint8List(size),
+        super('InputBytesSync_Size$size');
+
+  @override
+  void run() => benchmarkInputBytesTwinSync(bytes: bytes);
+}
+
+class InputBytesSyncRawBenchmark extends EnhancedBenchmarkBase {
+  final Uint8List bytes;
+
+  InputBytesSyncRawBenchmark(int size, {super.emitter})
+      : bytes = Uint8List(size),
+        super('InputBytesSyncRaw_Size$size');
+
+  @override
+  void run() => TODO;
 }
