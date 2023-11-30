dee84b56c21c1ae0db74b7cf77be748cfb4319b6
fzyzcjy <ch271828n@outlook.com>
2023-11-30 14:03:58 +0800

    feat: dart test

---
+++ frb_example/pure_dart/test/api/async_misc_test.dart
@@ -0,0 +1,15 @@
+import 'package:frb_example_pure_dart/src/rust/api/async_misc.dart';
+import 'package:frb_example_pure_dart/src/rust/frb_generated.dart';
+import 'package:test/test.dart';
+
+Future<void> main({bool skipRustLibInit = false}) async {
+  if (!skipRustLibInit) await RustLib.init();
+
+  test('dart call funcAsyncVoid', () async {
+    await funcAsyncVoid();
+  });
+
+  test('dart call funcAsyncSimpleAdd', () async {
+    expect(await funcAsyncSimpleAdd(a: 10, b: 20), 30);
+  });
+}
