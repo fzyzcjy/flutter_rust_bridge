02fe535e0d0dcf57a0089c486792b8407ec0ff9f
fzyzcjy <ch271828n@outlook.com>
2023-12-01 07:28:02 +0800

    fix: more compile

--- frb_rust/src/rust_async/web.rs
+++ frb_rust/src/rust_async/web.rs
@@ -1,11 +1,9 @@
 use std::future::Future;
-use std::thread::JoinHandle;
 use wasm_bindgen_futures::spawn_local;
 
-pub(crate) fn spawn<F>(future: F) -> JoinHandle<F::Output>
+pub(crate) fn spawn<F>(future: F)
 where
-    F: Future + Send + 'static,
-    F::Output: Send + 'static,
+    F: Future<Output = ()> + Send + 'static,
 {
     spawn_local(future)
 }
