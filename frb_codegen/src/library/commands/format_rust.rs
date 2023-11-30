a0bf8d93e24c33b50d46fc00bd70e750d7442d28
fzyzcjy <ch271828n@outlook.com>
2023-11-30 13:59:12 +0800

    fix: err

--- frb_codegen/src/library/commands/format_rust.rs
+++ frb_codegen/src/library/commands/format_rust.rs
@@ -7,5 +7,12 @@ use std::path::PathBuf;
 pub fn format_rust(path: &[PathBuf]) -> anyhow::Result<()> {
     let path = normalize_windows_unc_paths(path)?;
     debug!("execute format_rust path={path:?}");
-    check_exit_code(&command_run!(call_shell[None], "rustfmt", *path)?)
+    check_exit_code(&command_run!(
+        call_shell[None],
+        "rustfmt",
+        // otherwise cannot understand `async move`
+        "--edition",
+        "2018",
+        *path
+    )?)
 }
