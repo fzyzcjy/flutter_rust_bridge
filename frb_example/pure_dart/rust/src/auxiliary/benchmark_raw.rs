4790aa97e95567b9e61ae22a298b2ce814497ae0
fzyzcjy <ch271828n@outlook.com>
2023-11-30 08:50:59 +0800

    feat: more

--- frb_example/pure_dart/rust/src/auxiliary/benchmark_raw.rs
+++ frb_example/pure_dart/rust/src/auxiliary/benchmark_raw.rs
@@ -1,22 +1,40 @@
+#![allow(non_camel_case_types)]
+
 use crate::api::benchmark_api::{
     benchmark_input_bytes_twin_normal, benchmark_output_bytes_twin_normal,
 };
-use crate::frb_generated::wire_list_prim_u_8;
+use flutter_rust_bridge::support::{new_leak_vec_ptr, vec_from_leak_ptr};
 
 #[no_mangle]
 pub extern "C" fn benchmark_raw_void_sync() {}
 
+#[repr(C)]
+#[derive(Clone)]
+pub struct benchmark_raw_list_prim_u_8 {
+    ptr: *mut u8,
+    len: i32,
+}
+
 #[no_mangle]
-pub unsafe extern "C" fn benchmark_raw_input_bytes_sync(ptr: *mut u8, len: i32) -> i32 {
-    // TODO wrong
-    // "it is normally not safe to build a Vec<u8> from a pointer to a C char array with length size_t,
-    // doing so is only safe if the array was initially allocated by a Vec or String"
-    let vec = Vec::from_raw_parts(ptr, len as usize, len as usize);
+pub extern "C" fn benchmark_raw_new_list_prim_u_8(len: i32) -> benchmark_raw_list_prim_u_8 {
+    benchmark_raw_list_prim_u_8 {
+        ptr: new_leak_vec_ptr(Default::default(), len),
+        len,
+    }
+}
+
+// NOTE: `Vec::from_raw_parts` says:
+// "it is normally not safe to build a Vec<u8> from a pointer to a C char array with length size_t,
+// doing so is only safe if the array was initially allocated by a Vec or String"
+// Thus, the input vec *should* be allocated by our `new_leak_vec_ptr` only.
+#[no_mangle]
+pub unsafe extern "C" fn benchmark_raw_input_bytes_sync(bytes: benchmark_raw_list_prim_u_8) -> i32 {
+    let vec = vec_from_leak_ptr(bytes.ptr, bytes.len);
     benchmark_input_bytes_twin_normal(vec)
 }
 
 #[no_mangle]
-pub extern "C" fn benchmark_raw_output_bytes_sync(size: i32) -> wire_list_prim_u_8 {
+pub extern "C" fn benchmark_raw_output_bytes_sync(size: i32) -> benchmark_raw_list_prim_u_8 {
     let vec = benchmark_output_bytes_twin_normal(size);
     todo!()
 }
