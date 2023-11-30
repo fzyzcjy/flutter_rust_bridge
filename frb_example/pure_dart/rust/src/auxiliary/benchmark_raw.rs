84687d3ffb550867f1214724ba3d5682b3bb7ce9
fzyzcjy <ch271828n@outlook.com>
2023-11-30 08:46:50 +0800

    feat: try

--- frb_example/pure_dart/rust/src/auxiliary/benchmark_raw.rs
+++ frb_example/pure_dart/rust/src/auxiliary/benchmark_raw.rs
@@ -7,8 +7,11 @@ use crate::frb_generated::wire_list_prim_u_8;
 pub extern "C" fn benchmark_raw_void_sync() {}
 
 #[no_mangle]
-pub extern "C" fn benchmark_raw_input_bytes_sync(ptr: *mut u8, len: i32) -> i32 {
-    let vec = TODO;
+pub unsafe extern "C" fn benchmark_raw_input_bytes_sync(ptr: *mut u8, len: i32) -> i32 {
+    // TODO wrong
+    // "it is normally not safe to build a Vec<u8> from a pointer to a C char array with length size_t,
+    // doing so is only safe if the array was initially allocated by a Vec or String"
+    let vec = Vec::from_raw_parts(ptr, len as usize, len as usize);
     benchmark_input_bytes_twin_normal(vec)
 }
 
