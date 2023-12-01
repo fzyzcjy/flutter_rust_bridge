69a90805503fa0a84280cb62f9349657b6ff02bd
fzyzcjy <ch271828n@outlook.com>
2023-12-01 12:19:04 +0800

    chore: empty package deliberate_bad

---
+++ frb_example/deliberate_bad/rust/src/frb_generated.io.rs
@@ -0,0 +1,22 @@
+// Section: imports
+
+use super::*;
+use flutter_rust_bridge::rust2dart::IntoIntoDart;
+use flutter_rust_bridge::Handler;
+
+// Section: impl_new_with_nullptr
+
+pub trait NewWithNullPtr {
+    fn new_with_null_ptr() -> Self;
+}
+
+impl<T> NewWithNullPtr for *mut T {
+    fn new_with_null_ptr() -> Self {
+        std::ptr::null_mut()
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn wire_minimal_adder(port_: i64, a: i32, b: i32) {
+    wire_minimal_adder_impl(port_, a, b)
+}
