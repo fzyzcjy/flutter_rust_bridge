02afe97e63e8211c89866086af7c9edf7b581ae2
fzyzcjy <ch271828n@outlook.com>
2023-12-01 14:49:09 +0800

    fix: compile

--- frb_example/deliberate_bad/rust/src/main.rs
+++ frb_example/deliberate_bad/rust/src/main.rs
@@ -1,11 +1,23 @@
-use frb_example_deliberate_bad::api::simple::*;
 use std::env;
 
 fn main() {
     match &env::args().skip(1).next().unwrap()[..] {
         "RustOnly_Good" => println!("This is good code"),
-        "RustOnly_StackBufferOverflow" => make_stack_buffer_overflow(),
-        "RustOnly_HeapUseAfterFree" => make_heap_use_after_free(),
+        "RustOnly_StackBufferOverflow" => {
+            // example from https://doc.rust-lang.org/beta/unstable-book/compiler-flags/sanitizer.html
+            // and https://github.com/japaric/rust-san
+            let xs = [0, 1, 2, 3];
+            let y = unsafe { *xs.as_ptr().offset(4) };
+            println!("xs={xs:?} y={y}");
+        }
+        "RustOnly_HeapUseAfterFree" => {
+            // https://github.com/japaric/rust-san?tab=readme-ov-file#use-after-free
+            let xs = vec![0, 1, 2, 3];
+            let y = xs.as_ptr();
+            drop(xs);
+            let z = unsafe { *y };
+            println!("z={z:?}");
+        }
         s => panic!("Unknown mode: {}", s),
     }
 }
