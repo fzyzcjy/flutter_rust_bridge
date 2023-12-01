79fb57db9662474aec6754cf39b9c3b5304e68ba
fzyzcjy <ch271828n@outlook.com>
2023-12-01 16:30:55 +0800

    refactor: mv more

--- frb_example/deliberate_bad/test/rust_only_test.dart
+++ frb_example/deliberate_bad/test/rust_only_test.dart
@@ -1,30 +1,5 @@
 import 'package:frb_example_deliberate_bad/src/misc.dart';
-import 'package:test/scaffolding.dart';
 
 import 'test_utils.dart';
 
-void main() {
-  for (final (name, expectSucceed, expectStderrContains) in [
-    ('RustOnly_Good', true, ''),
-    (
-      'RustOnly_StackBufferOverflow',
-      false,
-      'ERROR: AddressSanitizer: stack-buffer-overflow',
-    ),
-    (
-      'RustOnly_HeapUseAfterFree',
-      false,
-      'ERROR: AddressSanitizer: heap-use-after-free',
-    ),
-  ]) {
-    test('name=$name', () async {
-      await execAndCheck(
-        'cargo +nightly run ${CargoBuildAsanInfo.kExtraArgs.join(" ")} $name',
-        extraEnv: CargoBuildAsanInfo.kExtraEnv,
-        relativePwd: 'rust',
-        expectSucceed: expectSucceed,
-        expectStderrContains: expectStderrContains,
-      );
-    });
-  }
-}
+void main() {}
--- tools/frb_internal/lib/src/misc/dart_sanitizer_tester.dart
+++ tools/frb_internal/lib/src/misc/dart_sanitizer_tester.dart
@@ -16,11 +16,43 @@ Future<void> _runPackageDeliberateBad({required String package}) async {
   throw Exception('TODO');
 }
 
-class _ExpectInfo {
+Future<void> _runPackageDeliberateBadRustOnly({required String package}) async {
+  const kInfos = [
+    _Info(
+      name: 'RustOnly_Good',
+      expectSucceed: true,
+      expectOutputContains: '',
+    ),
+    _Info(
+      name: 'RustOnly_StackBufferOverflow',
+      expectSucceed: false,
+      expectOutputContains: 'ERROR: AddressSanitizer: stack-buffer-overflow',
+    ),
+    _Info(
+      name: 'RustOnly_HeapUseAfterFree',
+      expectSucceed: false,
+      expectOutputContains: 'ERROR: AddressSanitizer: heap-use-after-free',
+    ),
+  ];
+
+  for (final info in kInfos) {
+    await _execAndCheck(
+      'cargo +nightly run ${_CargoBuildAsanInfo.kExtraArgs.join(" ")} $name',
+      extraEnv: _CargoBuildAsanInfo.kExtraEnv,
+      relativePwd: 'rust',
+      expectSucceed: expectSucceed,
+      expectStderrContains: expectStderrContains,
+    );
+  }
+}
+
+class _Info {
+  final String name;
   final bool expectSucceed;
   final String expectOutputContains;
 
-  const _ExpectInfo({
+  const _Info({
+    required this.name,
     required this.expectSucceed,
     required this.expectOutputContains,
   });
@@ -28,7 +60,7 @@ class _ExpectInfo {
 
 Future<void> _execAndCheck(
   String cmd,
-  _ExpectInfo info, {
+  _Info info, {
   required String relativePwd,
   Map<String, String>? extraEnv,
 }) async {
@@ -53,3 +85,15 @@ Future<void> _execAndCheck(
 // TODO do not hardcode
 const sanitizedDart =
     '/home/cs144/dart-sdk/sdk/out/ReleaseASANX64/dart-sdk/bin/dart';
+
+class _CargoBuildAsanInfo {
+  static const kExtraArgs = [
+    '-Zbuild-std',
+    '--target',
+    'x86_64-unknown-linux-gnu'
+  ];
+
+  static const kExtraEnv = {
+    'RUSTFLAGS': '-Zsanitizer=address',
+  };
+}
