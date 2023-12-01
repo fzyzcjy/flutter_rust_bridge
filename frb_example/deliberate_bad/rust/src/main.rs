b175cc3e7f73d0a6e993ac8ab9c5d422d3a373e0
fzyzcjy <ch271828n@outlook.com>
2023-12-01 13:48:56 +0800

    feat: simple demo

--- frb_example/deliberate_bad/rust/src/main.rs
+++ frb_example/deliberate_bad/rust/src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+
 fn main() {
-    println!("hi");
+    match &env::args().skip(1).next().unwrap()[..] {
+        "good" => println!("This is a good code"),
+        "stack_buffer_overflow" => {
+            // example from https://doc.rust-lang.org/beta/unstable-book/compiler-flags/sanitizer.html
+            let xs = [0, 1, 2, 3];
+            let y = unsafe { *xs.as_ptr().offset(4) };
+            println!("xs={xs:?} y={y}");
+        }
+        s => panic!("Unknown mode: {}", s),
+    }
 }
