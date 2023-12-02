3e85384efaf4defd1fa823ddf9b01e0ce07ddf14
fzyzcjy <ch271828n@outlook.com>
2023-12-02 20:39:21 +0800

    chore: codegen

--- frb_example/dart_minimal/rust/src/frb_generated.rs
+++ frb_example/dart_minimal/rust/src/frb_generated.rs
@@ -39,8 +39,23 @@ fn wire_minimal_adder_impl(
 
 // Section: executor
 
+#[cfg(not(target_family = "wasm"))]
 flutter_rust_bridge::for_generated::lazy_static! {
-    pub static ref FLUTTER_RUST_BRIDGE_HANDLER: flutter_rust_bridge::DefaultHandler = Default::default();
+    pub static ref FLUTTER_RUST_BRIDGE_HANDLER:
+    flutter_rust_bridge::DefaultHandler<flutter_rust_bridge::for_generated::ThreadPool>
+    = flutter_rust_bridge::DefaultHandler::new_simple(Default::default());
+}
+
+#[cfg(target_family = "wasm")]
+thread_local! {
+    pub static THREAD_POOL: flutter_rust_bridge::for_generated::ThreadPool = Default::default();
+}
+
+#[cfg(target_family = "wasm")]
+flutter_rust_bridge::for_generated::lazy_static! {
+    pub static ref FLUTTER_RUST_BRIDGE_HANDLER:
+    flutter_rust_bridge::DefaultHandler<&'static std::thread::LocalKey<flutter_rust_bridge::for_generated::ThreadPool>>
+    = flutter_rust_bridge::DefaultHandler::new_simple(&THREAD_POOL);
 }
 
 // Section: impl_wire2api
