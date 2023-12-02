706d75ccd2407ef829ea7c448832ad6bdfa345be
fzyzcjy <ch271828n@outlook.com>
2023-12-02 20:17:14 +0800

    fix: non wasm

--- frb_example/pure_dart/rust/src/frb_generated.rs
+++ frb_example/pure_dart/rust/src/frb_generated.rs
@@ -8234,19 +8234,18 @@ const _: fn() = || {
 
 // Section: executor
 
-#[cfg(not(target_family = "wasm"))]
-flutter_rust_bridge::for_generated::lazy_static! {
-    pub static ref THREAD_POOL: Mutex<flutter_rust_bridge::for_generated::ThreadPool> = Mutex::new(Default::default());
-}
-
 #[cfg(target_family = "wasm")]
 thread_local! {
     pub static THREAD_POOL: flutter_rust_bridge::for_generated::ThreadPool = Default::default();
 }
 
 flutter_rust_bridge::for_generated::lazy_static! {
-    pub static ref FLUTTER_RUST_BRIDGE_HANDLER: flutter_rust_bridge::DefaultHandler =
-        flutter_rust_bridge::DefaultHandler::new_simple(&THREAD_POOL);
+    pub static ref FLUTTER_RUST_BRIDGE_HANDLER: flutter_rust_bridge::DefaultHandler = {
+        #[cfg(not(target_family = "wasm"))]
+        return flutter_rust_bridge::DefaultHandler::new_simple(Default::default());
+        #[cfg(target_family = "wasm")]
+        return flutter_rust_bridge::DefaultHandler::new_simple(&THREAD_POOL);
+    };
 }
 
 // Section: impl_wire2api
