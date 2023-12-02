1113ecb0a55a8987e9fffe0437f48e3acfbd5fa9
fzyzcjy <ch271828n@outlook.com>
2023-12-02 14:43:08 +0800

    fix: type

--- frb_rust/src/rust2dart/api2wire.rs
+++ frb_rust/src/rust2dart/api2wire.rs
@@ -1,28 +1,30 @@
+use allo_isolate::ffi::DartCObject;
 use allo_isolate::IntoDart;
+use crate::rust2dart::action::Rust2DartAction;
 
 pub struct Api2wire;
 
 impl Api2wire {
     /// A success
-    pub fn success(&self, result: impl IntoDart) -> bool {
+    pub fn success(&self, result: impl IntoDart) -> DartCObject {
         vec![
             Rust2DartAction::Success.into_dart(),
             result.into_dart(),
-        ]
+        ].into_dart()
     }
 
     /// A panic error
-    pub fn panic(&self, e: impl IntoDart) -> bool {
-        vec![Rust2DartAction::Panic.into_dart(), e.into_dart()]
+    pub fn panic(&self, e: impl IntoDart) -> DartCObject {
+        vec![Rust2DartAction::Panic.into_dart(), e.into_dart()].into_dart()
     }
 
     /// A detailed error
-    pub fn error(&self, e: impl IntoDart) -> bool {
-        vec![Rust2DartAction::Error.into_dart(), e.into_dart()]
+    pub fn error(&self, e: impl IntoDart) -> DartCObject {
+        vec![Rust2DartAction::Error.into_dart(), e.into_dart()].into_dart()
     }
 
     /// Close the stream
-    pub fn close_stream(&self) -> bool {
-        vec![Rust2DartAction::CloseStream.into_dart()]
+    pub fn close_stream(&self) -> DartCObject {
+        vec![Rust2DartAction::CloseStream.into_dart()].into_dart()
     }
 }
