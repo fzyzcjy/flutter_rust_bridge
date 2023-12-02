72cf37007d97cd56f71976f7858ccc5c6bcfa1b8
fzyzcjy <ch271828n@outlook.com>
2023-12-02 11:41:05 +0800

    refactor: web intodart

--- frb_rust/src/generalized_isolate/mod.rs
+++ frb_rust/src/generalized_isolate/mod.rs
@@ -3,7 +3,13 @@ mod web;
 #[cfg(wasm)]
 pub use web::*;
 
+#[cfg(wasm)]
+mod web_into_dart;
+#[cfg(wasm)]
+pub use web_into_dart::*;
+
 #[cfg(not(wasm))]
 mod io;
+
 #[cfg(not(wasm))]
 pub use io::*;
