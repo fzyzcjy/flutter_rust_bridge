586959538b85f054572592218190e16c81c1c8c3
fzyzcjy <ch271828n@outlook.com>
2023-12-02 17:20:36 +0800

    feat: exports

--- frb_rust/src/lib.rs
+++ frb_rust/src/lib.rs
@@ -1,13 +1,17 @@
+mod generalized_isolate;
 pub(crate) mod handler;
 mod misc;
 mod platform_types;
 mod rust2dart;
-mod generalized_isolate;
 pub(crate) mod third_party;
 pub(crate) mod thread_pool;
 
-pub(crate) mod rust_async;
 pub(crate) mod dart_opaque;
 pub(crate) mod ffi_binding;
-pub(crate) mod web_transfer;
 pub(crate) mod for_generated;
+pub(crate) mod rust_async;
+pub(crate) mod web_transfer;
+
+pub use crate::misc::into_into_dart::IntoIntoDart;
+pub use crate::platform_types::DartAbi;
+pub use flutter_rust_bridge_macros::frb;
