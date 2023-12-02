bb8950e6491e3aea15ca8640252559877ac00de3
fzyzcjy <ch271828n@outlook.com>
2023-12-02 21:26:13 +0800

    fix: has executor

--- frb_codegen/src/library/codegen/parser/misc.rs
+++ frb_codegen/src/library/codegen/parser/misc.rs
@@ -1,5 +1,5 @@
 use crate::library::misc::consts::HANDLER_NAME;
 
 pub(crate) fn parse_has_executor(source_rust_content: &str) -> bool {
-    source_rust_content.contains(HANDLER_NAME)
+    source_rust_content.contains(&format!("static ref FLUTTER_RUST_BRIDGE_HANDLER"))
 }
