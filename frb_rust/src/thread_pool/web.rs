4b01ecf71bfe50e142cb6df0b1a7fe3ea841f87e
fzyzcjy <ch271828n@outlook.com>
2023-12-02 18:29:36 +0800

    Revert "feat: try thread local"
    This reverts commit eac2764f602b456d974483031f086598389f89ed.

--- frb_rust/src/thread_pool/web.rs
+++ frb_rust/src/thread_pool/web.rs
@@ -1,5 +1,1 @@
-use thread_local::ThreadLocal;
-
 pub use crate::third_party::wasm_bindgen::worker_pool::WorkerPool as ThreadPool;
-
-pub(crate) struct SingleThreadAccessorThreadPool(ThreadLocal<ThreadPool>);
