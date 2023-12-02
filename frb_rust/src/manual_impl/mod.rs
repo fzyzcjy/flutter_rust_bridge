9f3b576a51460566614036a33b0b39e1bf149f70
fzyzcjy <ch271828n@outlook.com>
2023-12-02 14:11:40 +0800

    refactor: timestamp

--- frb_rust/src/manual_impl/mod.rs
+++ frb_rust/src/manual_impl/mod.rs
@@ -7,3 +7,51 @@ pub use io::*;
 mod web;
 #[cfg(wasm)]
 pub use web::*;
+
+#[cfg(feature = "chrono")]
+#[inline]
+pub fn wire2api_timestamp(ts: i64) -> Timestamp {
+    #[cfg(wasm)]
+    const PRECISION: i64 = 1_000;
+    #[cfg(not(wasm))]
+    const PRECISION: i64 = 1_000_000;
+
+    let s = ts / PRECISION;
+    let ns = (ts.rem_euclid(PRECISION) * (1_000_000_000 / PRECISION)) as u32;
+    Timestamp { s, ns }
+}
+
+/// a timestamp with microseconds precision on native and milliseconds precision on web
+#[cfg(feature = "chrono")]
+pub struct Timestamp {
+    /// seconds
+    pub s: i64,
+    /// nanoseconds
+    pub ns: u32,
+}
+
+#[cfg(test)]
+#[cfg(feature = "chrono")]
+mod tests {
+    #[test]
+    fn test_wire2api() {
+        #[cfg(not(wasm))]
+        {
+            // input in microseconds
+            let input: i64 = 3_496_567_123;
+            let super::Timestamp { s, ns } = super::wire2api_timestamp(input);
+            assert_eq!(s, 3_496);
+            assert_eq!(ns, 567_123_000);
+        }
+
+        #[cfg(wasm)]
+        {
+            // input in milliseconds
+            let input: i64 = 3_496_567;
+            let super::Timestamp { s, ns } = super::wire2api_timestamp(input);
+            assert_eq!(s, 3_496);
+            assert_eq!(ns, 567_000_000);
+        }
+    }
+}
+
