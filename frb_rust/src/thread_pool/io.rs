eac2764f602b456d974483031f086598389f89ed
fzyzcjy <ch271828n@outlook.com>
2023-12-02 18:28:18 +0800

    feat: try thread local

--- frb_rust/src/thread_pool/io.rs
+++ frb_rust/src/thread_pool/io.rs
@@ -1,1 +1,6 @@
 pub use threadpool::ThreadPool;
+
+/// A normal thread pool, except that, only a single thread will touch its methods like `execute`.
+/// For example, if thread pool's `execute` is only executed from Dart's synchronous call
+/// (thus the main thread), then this holds.
+pub(crate) struct SingleThreadAccessorThreadPool(threadpool::ThreadPool);
--- frb_rust/src/thread_pool/web.rs
+++ frb_rust/src/thread_pool/web.rs
@@ -1,1 +1,5 @@
+use thread_local::ThreadLocal;
+
 pub use crate::third_party::wasm_bindgen::worker_pool::WorkerPool as ThreadPool;
+
+pub(crate) struct SingleThreadAccessorThreadPool(ThreadLocal<ThreadPool>);
