4a43e079afa21196552cb44f1cfc2b9f61a3ce1a
fzyzcjy <ch271828n@outlook.com>
2023-12-02 14:07:15 +0800

    refactor: manual_impl

--- frb_rust/src/old/ffi/io.rs
+++ frb_rust/src/old/ffi/io.rs
@@ -11,36 +11,6 @@ use dart_sys::Dart_NewPersistentHandle_DL;
 use dart_sys::Dart_PersistentHandle;
 use libc::c_void;
 
-#[cfg(feature = "chrono")]
-#[inline]
-pub fn wire2api_timestamp(ts: i64) -> Timestamp {
-    let s = ts / 1_000_000;
-    let ns = (ts.rem_euclid(1_000_000) * 1_000) as u32;
-    Timestamp { s, ns }
-}
-
-/// a timestamp with microseconds precision
-#[cfg(feature = "chrono")]
-pub struct Timestamp {
-    /// seconds
-    pub s: i64,
-    /// nanoseconds
-    pub ns: u32,
-}
-
-#[cfg(test)]
-#[cfg(feature = "chrono")]
-mod tests {
-    #[test]
-    fn wire2api() {
-        // input in microseconds
-        let input: i64 = 3_496_567_123;
-        let super::Timestamp { s, ns } = super::wire2api_timestamp(input);
-        assert_eq!(s, 3_496);
-        assert_eq!(ns, 567_123_000);
-    }
-}
-
 /// # Safety
 ///
 /// This function should never be called manually.
