1c8db23f481d0fe52315bbfea348896bb47f2897
fzyzcjy <ch271828n@outlook.com>
2023-12-03 20:29:07 +0800

    refactor: generate_rust_arc_functions

--- frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/dart2rust/ty/rust_opaque.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/dart2rust/ty/rust_opaque.rs
@@ -10,7 +10,7 @@ use crate::codegen::generator::wire::rust::spec_generator::extern_func::{
     ExternFunc, ExternFuncParam,
 };
 use crate::codegen::generator::wire::rust::spec_generator::output_code::WireRustOutputCode;
-use crate::codegen::ir::ty::IrTypeTrait;
+use crate::codegen::ir::ty::{IrType, IrTypeTrait};
 use itertools::Itertools;
 use std::borrow::Cow;
 
@@ -27,33 +27,7 @@ impl<'a> WireRustGeneratorDart2RustTrait for RustOpaqueWireRustGenerator<'a> {
     }
 
     fn generate_related_funcs(&self) -> Acc<WireRustOutputCode> {
-        let generate_impl = |target| -> WireRustOutputCode {
-            ["increment", "decrement"].into_iter()
-                .map(|op|
-                     ExternFunc {
-                         func_name: format!("rust_arc_{op}_strong_count_{}", self.ir.safe_ident()),
-                         params: vec![ExternFuncParam {
-                             name: "ptr".to_owned(),
-                             rust_type: "*const std::ffi::c_void".to_owned(),
-                             dart_type: "dynamic".into(),
-                         }.clone()],
-                         return_type: None,
-                         body: format!(
-                             "unsafe {{ flutter_rust_bridge::for_generated::rust_arc_{op}_strong_count::<{}>(ptr); }}",
-                             self.ir.inner.rust_api_type()
-                         ),
-                         target,
-                     },
-                )
-                .collect_vec()
-                .into()
-        };
-
-        Acc {
-            io: generate_impl(Target::Io),
-            wasm: generate_impl(Target::Wasm),
-            ..Default::default()
-        }
+        generate_rust_arc_functions(self.ir.clone().into())
     }
 
     fn rust_wire_type(&self, target: Target) -> String {
@@ -67,4 +41,34 @@ impl<'a> WireRustGeneratorDart2RustTrait for RustOpaqueWireRustGenerator<'a> {
 
 fn generate_impl_wire2api_body() -> &'static str {
     r#"unsafe { flutter_rust_bridge::for_generated::wire2api_rust_opaque(self) }"#
+}
+
+pub(super) fn generate_rust_arc_functions(ir: IrType) -> Acc<WireRustOutputCode> {
+    let generate_impl = |target| -> WireRustOutputCode {
+        ["increment", "decrement"].into_iter()
+            .map(|op|
+                     ExternFunc {
+                         func_name: format!("rust_arc_{op}_strong_count_{}", ir.safe_ident()),
+                         params: vec![ExternFuncParam {
+                             name: "ptr".to_owned(),
+                             rust_type: "*const std::ffi::c_void".to_owned(),
+                             dart_type: "dynamic".into(),
+                         }.clone()],
+                         return_type: None,
+                         body: format!(
+                             "unsafe {{ flutter_rust_bridge::for_generated::rust_arc_{op}_strong_count::<{}>(ptr); }}",
+                             ir.inner.rust_api_type()
+                         ),
+                         target,
+                     },
+            )
+            .collect_vec()
+            .into()
+    };
+
+    Acc {
+        io: generate_impl(Target::Io),
+        wasm: generate_impl(Target::Wasm),
+        ..Default::default()
+    }
 }
