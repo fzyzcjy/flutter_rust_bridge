c4cca33eb7f97bdc399ddbac8b6bd8c91fb01aa2
fzyzcjy <ch271828n@outlook.com>
2023-12-03 19:58:10 +0800

    fix: compile

--- frb_codegen/src/library/codegen/parser/function_parser/mod.rs
+++ frb_codegen/src/library/codegen/parser/function_parser/mod.rs
@@ -74,7 +74,7 @@ impl<'a, 'b> FunctionParser<'a, 'b> {
             info = info.merge(self.parse_fn_arg(i, sig_input, &owner, &context)?)?;
         }
         info = info.merge(self.parse_fn_output(sig, &context)?)?;
-        info = transform_fn_info(info);
+        info = transform_fn_info(info, self.type_parser);
 
         let mode = compute_func_mode(attributes, &info);
 
