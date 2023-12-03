eca67a64050b667be85baec85ad061a95a17eeb8
fzyzcjy <ch271828n@outlook.com>
2023-12-04 07:34:18 +0800

    feat: more

--- frb_codegen/src/library/codegen/generator/api_dart/spec_generator/info.rs
+++ frb_codegen/src/library/codegen/generator/api_dart/spec_generator/info.rs
@@ -227,10 +227,9 @@ impl<'a> ApiDartGeneratorInfoTrait for UnencodableApiDartGenerator<'a> {
 
 fn rust_type_to_dart_type(rust: &str) -> String {
     lazy_static! {
-        static ref OPAQUE_FILTER: Regex = Regex::new(
-            r"(\bdyn|'static|\bDartSafe|\+ (Send|Sync|UnwindSafe|RefUnwindSafe|AssertUnwindSafe))\b"
-        )
-        .unwrap();
+        static ref OPAQUE_FILTER: Regex =
+            Regex::new(r"(\bdyn|'static|\bDartSafe|\bAssertUnwindSafe|\+ (Send|Sync|UnwindSafe|RefUnwindSafe))\b")
+                .unwrap();
     }
     let rust = rust.split("::").last().unwrap();
     OPAQUE_FILTER
