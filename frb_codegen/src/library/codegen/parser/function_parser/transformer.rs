ffa541c1a6e59ddd5b7c0c5fea216241b757bba6
fzyzcjy <ch271828n@outlook.com>
2023-12-03 19:57:31 +0800

    feat: visit subtree

--- frb_codegen/src/library/codegen/parser/function_parser/transformer.rs
+++ frb_codegen/src/library/codegen/parser/function_parser/transformer.rs
@@ -1,26 +1,44 @@
 use crate::codegen::ir::field::IrField;
+use crate::codegen::ir::pack::DistinctTypeGatherer;
 use crate::codegen::ir::ty::rust_auto_opaque::IrTypeRustAutoOpaque;
-use crate::codegen::ir::ty::IrType;
+use crate::codegen::ir::ty::{IrContext, IrType};
 use crate::codegen::parser::function_parser::FunctionPartialInfo;
 use itertools::Itertools;
 
-pub(super) fn transform_fn_info(info: FunctionPartialInfo) -> FunctionPartialInfo {
+pub(super) fn transform_fn_info(
+    info: FunctionPartialInfo,
+    ir_context: &impl IrContext,
+) -> FunctionPartialInfo {
     FunctionPartialInfo {
         inputs: (info.inputs.into_iter())
             .map(|x| IrField {
-                ty: transform_fn_arg_or_output_type(x.ty),
+                ty: transform_fn_arg_or_output_type(x.ty, ir_context),
                 ..x
             })
             .collect_vec(),
-        ok_output: info.ok_output.map(transform_fn_arg_or_output_type),
-        error_output: info.error_output.map(transform_fn_arg_or_output_type),
+        ok_output: (info.ok_output).map(|x| transform_fn_arg_or_output_type(x, ir_context)),
+        error_output: (info.error_output).map(|x| transform_fn_arg_or_output_type(x, ir_context)),
         mode: info.mode,
     }
 }
 
-fn transform_fn_arg_or_output_type(ty: IrType) -> IrType {
-    if children_type_has_unencodable && !children_type_has_rust_opaque {
-        return IrType::RustAutoOpaque(IrTypeRustAutoOpaque::new(TODO, ty));
+fn transform_fn_arg_or_output_type(ty: IrType, ir_context: &impl IrContext) -> IrType {
+    let subtree_types_except_rust_opaque = {
+        let mut gatherer = DistinctTypeGatherer::new();
+        ty.visit_types(
+            &mut |ty| {
+                gatherer.add(ty);
+
+                // skip subtrees inside RustOpaque
+                matches!(ty, IrType::RustOpaque(_))
+            },
+            ir_context,
+        );
+        gatherer.gather()
+    };
+
+    if (subtree_types_except_rust_opaque.iter()).any(|x| matches!(x, IrType::Unencodable(_))) {
+        return IrType::RustAutoOpaque(IrTypeRustAutoOpaque::new(todo!(), ty));
     }
     ty
 }
