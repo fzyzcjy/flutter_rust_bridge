0d71e851532c11551c40987772ac97c0c89a1a8b
fzyzcjy <ch271828n@outlook.com>
2023-12-03 20:26:13 +0800

    feat: generalized_rust_opaque_generate_impl_wire2api_body

--- frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/rust2dart/ty/rust_auto_opaque.rs
+++ frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/rust2dart/ty/rust_auto_opaque.rs
@@ -1,10 +1,11 @@
 use crate::codegen::generator::wire::dart::spec_generator::base::*;
+use crate::codegen::generator::wire::dart::spec_generator::rust2dart::ty::rust_opaque::generalized_rust_opaque_generate_impl_wire2api_body;
 use crate::codegen::generator::wire::dart::spec_generator::rust2dart::ty::WireDartGeneratorRust2DartTrait;
 use crate::library::codegen::generator::api_dart::spec_generator::base::ApiDartGenerator;
 use crate::library::codegen::generator::api_dart::spec_generator::info::ApiDartGeneratorInfoTrait;
 
 impl<'a> WireDartGeneratorRust2DartTrait for RustAutoOpaqueWireDartGenerator<'a> {
     fn generate_impl_wire2api_body(&self) -> String {
-        todo!()
+        generalized_rust_opaque_generate_impl_wire2api_body(self.ir.clone().into(), self.context)
     }
 }
--- frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/rust2dart/ty/rust_opaque.rs
+++ frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/rust2dart/ty/rust_opaque.rs
@@ -1,14 +1,22 @@
 use crate::codegen::generator::wire::dart::spec_generator::base::*;
 use crate::codegen::generator::wire::dart::spec_generator::rust2dart::ty::WireDartGeneratorRust2DartTrait;
+use crate::codegen::ir::ty::rust_opaque::IrTypeRustOpaque;
+use crate::codegen::ir::ty::IrType;
 use crate::library::codegen::generator::api_dart::spec_generator::base::ApiDartGenerator;
 use crate::library::codegen::generator::api_dart::spec_generator::info::ApiDartGeneratorInfoTrait;
 
 impl<'a> WireDartGeneratorRust2DartTrait for RustOpaqueWireDartGenerator<'a> {
     fn generate_impl_wire2api_body(&self) -> String {
-        format!(
-            "return {0}.fromWire(raw);",
-            ApiDartGenerator::new(self.ir.clone(), self.context.as_api_dart_context())
-                .dart_api_type()
-        )
+        generalized_rust_opaque_generate_impl_wire2api_body(self.ir.clone().into(), self.context)
     }
+}
+
+pub(super) fn generalized_rust_opaque_generate_impl_wire2api_body(
+    ir: IrType,
+    context: WireDartGeneratorContext,
+) -> String {
+    format!(
+        "return {}.fromWire(raw);",
+        ApiDartGenerator::new(ir, context.as_api_dart_context()).dart_api_type()
+    )
 }
