596e73eb15aca99bb16a9ac509b76b9fc13094bc
fzyzcjy <ch271828n@outlook.com>
2023-12-03 20:24:40 +0800

    feat: impl generate_extra_functions

--- frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/misc/ty/rust_auto_opaque.rs
+++ frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/misc/ty/rust_auto_opaque.rs
@@ -1,4 +1,11 @@
+use crate::codegen::generator::acc::Acc;
 use crate::codegen::generator::wire::dart::spec_generator::base::*;
+use crate::codegen::generator::wire::dart::spec_generator::misc::ty::rust_opaque::generate_rust_arc_functions;
 use crate::codegen::generator::wire::dart::spec_generator::misc::ty::WireDartGeneratorMiscTrait;
+use crate::codegen::generator::wire::dart::spec_generator::output_code::WireDartOutputCode;
 
-impl<'a> WireDartGeneratorMiscTrait for RustAutoOpaqueWireDartGenerator<'a> {}
+impl<'a> WireDartGeneratorMiscTrait for RustAutoOpaqueWireDartGenerator<'a> {
+    fn generate_extra_functions(&self) -> Option<Acc<WireDartOutputCode>> {
+        Some(generate_rust_arc_functions(self.ir.into(), self.context))
+    }
+}
