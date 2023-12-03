e4fc65954ca0dd923450cc48c0fccbab30c4d88f
fzyzcjy <ch271828n@outlook.com>
2023-12-03 20:27:51 +0800

    feat: generalized_rust_opaque_generate_imports

--- frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/misc/ty/rust_auto_opaque.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/misc/ty/rust_auto_opaque.rs
@@ -1,4 +1,9 @@
 use crate::codegen::generator::wire::rust::spec_generator::base::*;
+use crate::codegen::generator::wire::rust::spec_generator::misc::ty::rust_opaque::generalized_rust_opaque_generate_imports;
 use crate::codegen::generator::wire::rust::spec_generator::misc::ty::WireRustGeneratorMiscTrait;
 
-impl<'a> WireRustGeneratorMiscTrait for RustAutoOpaqueWireRustGenerator<'a> {}
+impl<'a> WireRustGeneratorMiscTrait for RustAutoOpaqueWireRustGenerator<'a> {
+    fn generate_imports(&self) -> Option<Vec<String>> {
+        generalized_rust_opaque_generate_imports(&self.ir.namespace)
+    }
+}
--- frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/misc/ty/rust_opaque.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/misc/ty/rust_opaque.rs
@@ -1,9 +1,16 @@
 use crate::codegen::generator::wire::rust::spec_generator::base::*;
 use crate::codegen::generator::wire::rust::spec_generator::misc::ty::WireRustGeneratorMiscTrait;
+use crate::codegen::ir::namespace::Namespace;
 
 impl<'a> WireRustGeneratorMiscTrait for RustOpaqueWireRustGenerator<'a> {
     fn generate_imports(&self) -> Option<Vec<String>> {
-        // To expose the `pub use`s inside that file
-        Some(vec![format!("use {}::*;", self.ir.namespace.joined_path)])
+        generalized_rust_opaque_generate_imports(&self.ir.namespace)
     }
+}
+
+pub(super) fn generalized_rust_opaque_generate_imports(
+    namespace: &Namespace,
+) -> Option<Vec<String>> {
+    // To expose the `pub use`s inside that file
+    Some(vec![format!("use {}::*;", namespace.joined_path)])
 }
