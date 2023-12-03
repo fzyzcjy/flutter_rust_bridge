cbc7e219e981e1583ce4043c661979c65102ec7f
fzyzcjy <ch271828n@outlook.com>
2023-12-03 19:37:34 +0800

    feat: IrTypeRustAutoOpaque

--- frb_codegen/src/library/codegen/ir/ty/rust_auto_opaque.rs
+++ frb_codegen/src/library/codegen/ir/ty/rust_auto_opaque.rs
@@ -4,22 +4,32 @@ use crate::codegen::ir::ty::{IrType, IrTypeTrait};
 
 crate::ir! {
 pub struct IrTypeRustAutoOpaque {
-    // TODO
+    pub namespace: Namespace,
+    pub inner: Box<IrType>,
 }
+}
+
+impl IrTypeRustAutoOpaque {
+    pub fn new(namespace: Namespace, inner: IrType) -> Self {
+        Self {
+            namespace,
+            inner: Box::new(inner),
+        }
+    }
 }
 
 impl IrTypeTrait for IrTypeRustAutoOpaque {
     fn visit_children_types<F: FnMut(&IrType) -> bool>(&self, _f: &mut F, _ir_pack: &IrPack) {}
 
     fn safe_ident(&self) -> String {
-        todo!()
+        format!("RustAutoOpaque_{}", self.inner.safe_ident())
     }
 
     fn rust_api_type(&self) -> String {
-        todo!()
+        self.inner.rust_api_type()
     }
 
     fn self_namespace(&self) -> Option<Namespace> {
-        todo!()
+        Some(self.namespace.clone())
     }
 }
