d720bd82889624876f504c5af9fc122bcb738b4b
fzyzcjy <ch271828n@outlook.com>
2023-12-03 21:54:21 +0800

    Revert "fix: compile"
    This reverts commit 344ab3bfa886a60a35d67baab58d39812380fed6.

--- frb_codegen/src/library/codegen/generator/api_dart/spec_generator/mod.rs
+++ frb_codegen/src/library/codegen/generator/api_dart/spec_generator/mod.rs
@@ -111,19 +111,17 @@ fn generate_item(
 
     let classes = namespaced_types
         .map(|classes| {
-            Ok::<_, anyhow::Error>(
-                classes
-                    .iter()
-                    .filter_map(|&ty| ApiDartGenerator::new(ty.clone(), context).generate_class())
-                    .map(|c| {
-                        c.sanity_check()?;
-                        Ok(c)
-                    })
-                    .collect::<Result<Vec<_>>>()?
-                    .into_iter()
-                    .unique_by(|c| (c.namespace.clone(), c.class_name.clone()))
-                    .collect_vec(),
-            )
+            Ok(classes
+                .iter()
+                .filter_map(|&ty| ApiDartGenerator::new(ty.clone(), context).generate_class())
+                .map(|c| {
+                    c.sanity_check()?;
+                    Ok(c)
+                })
+                .collect::<Result<Vec<_>>>()?
+                .into_iter()
+                .unique_by(|c| (c.namespace.clone(), c.class_name.clone()))
+                .collect_vec())
         })
         .unwrap_or(Ok(vec![]))?;
 
