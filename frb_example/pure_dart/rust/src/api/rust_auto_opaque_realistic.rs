231b7ef65ea7177b5d88551e9f39aa0bf33022a4
fzyzcjy <ch271828n@outlook.com>
2023-12-03 18:05:56 +0800

    feat: more exm

--- frb_example/pure_dart/rust/src/api/rust_auto_opaque_realistic.rs
+++ frb_example/pure_dart/rust/src/api/rust_auto_opaque_realistic.rs
@@ -1,6 +1,21 @@
-// TODO let's play with some FP? ;)
+use tempfile::NamedTempFile;
 
 /// Wraps the famous temporary file crate
 pub struct TempFileTwinNormal {
-    // TODO
+    // Use `NamedTempFile` instead of `tempfile::tempfile`, since the latter
+    // rely on OS to cleanup file, but we want to demonstrate Rust's `drop`
+    // logic in this demo
+    file: NamedTempFile,
+}
+
+impl TempFileTwinNormal {
+    pub fn new() -> anyhow::Result<Self> {
+        Ok(Self {
+            file: NamedTempFile::new()?,
+        })
+    }
+
+    pub fn path(&self) -> String {
+        self.file.path().to_str().unwrap().into_string()
+    }
 }
