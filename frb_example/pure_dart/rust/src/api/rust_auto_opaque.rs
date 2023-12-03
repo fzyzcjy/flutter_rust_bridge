f571e21124d93243577a8025edec88083305c9f3
fzyzcjy <ch271828n@outlook.com>
2023-12-03 17:01:04 +0800

    chore: sketch

--- frb_example/pure_dart/rust/src/api/rust_auto_opaque.rs
+++ frb_example/pure_dart/rust/src/api/rust_auto_opaque.rs
@@ -1,1 +1,10 @@
+// Do *NOT* make it Clone or serializable
+pub struct NonCloneNonSerializableTwinNormal;
 
+// TODO functions
+// TODO multi args
+// TODO methods
+// TODO static methods
+// TODO dyn trait
+// TODO mutability, borrow
+// TODO opaque *inside* other objects, especially ref/mutref ---- a bit hard, do it later
