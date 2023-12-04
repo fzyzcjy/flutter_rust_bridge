bb336a90f990bf9d04acbdd68f2272baf31f1cda
fzyzcjy <ch271828n@outlook.com>
2023-12-04 08:45:01 +0800

    feat: try change api

--- frb_rust/src/misc/rust_auto_opaque.rs
+++ frb_rust/src/misc/rust_auto_opaque.rs
@@ -1,19 +1,14 @@
 use crate::{DartSafe, RustOpaque};
 use anyhow::{Context, Result};
+use parking_lot::RwLock;
 
-pub trait RustAutoOpaqueWire2Api<T> {
-    fn rust_auto_opaque_wire2api(self) -> Result<T>;
-}
-
-impl<T: DartSafe> RustAutoOpaqueWire2Api<T> for RustOpaque<T> {
-    fn rust_auto_opaque_wire2api(self) -> Result<T> {
+impl<T: DartSafe> RustOpaque<RwLock<T>> {
+    pub fn rust_auto_opaque_wire2api_owned(self) -> Result<T> {
         self.into_inner()
             .context("Cannot convert RustOpaque to inner value. This is probably because you are having more than one references to it.")
     }
-}
 
-impl<'a, T: ?Sized + DartSafe> RustAutoOpaqueWire2Api<&'a T> for &'a RustOpaque<T> {
-    fn rust_auto_opaque_wire2api(self) -> Result<&'a T> {
+    pub fn rust_auto_opaque_wire2api_ref(&self) -> &Result<T> {
         Ok(&self)
     }
 }
