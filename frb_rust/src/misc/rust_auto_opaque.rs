baacd3d5d29823d4a4a3ab052eb9d8660ec206e1
fzyzcjy <ch271828n@outlook.com>
2023-12-04 08:27:34 +0800

    feat: impl RustAutoOpaqueWire2Api

--- frb_rust/src/misc/rust_auto_opaque.rs
+++ frb_rust/src/misc/rust_auto_opaque.rs
@@ -1,3 +1,13 @@
+use crate::RustOpaque;
+use anyhow::{Context, Result};
+
 pub trait RustAutoOpaqueWire2Api<T> {
-    fn rust_auto_opaque_wire2api(&self) -> T;
+    fn rust_auto_opaque_wire2api(self) -> Result<T>;
+}
+
+impl<T> RustAutoOpaqueWire2Api<T> for RustOpaque<T> {
+    fn rust_auto_opaque_wire2api(self) -> Result<T> {
+        self.into_inner()
+            .context("Cannot convert RustOpaque to inner value. This is probably because you are having more than one references to it.")
+    }
 }
