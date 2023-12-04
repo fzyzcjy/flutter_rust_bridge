51107d9d8e3bbd956191237fa6e3dc19b03a6a1b
fzyzcjy <ch271828n@outlook.com>
2023-12-04 18:15:15 +0800

    chore: changetemplate

--- frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/misc/mod.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/misc/mod.rs
@@ -137,19 +137,19 @@ fn generate_executor(ir_pack: &IrPack) -> String {
                 #[cfg(not(target_family = "wasm"))]
                 flutter_rust_bridge::for_generated::lazy_static! {{
                     pub static ref FLUTTER_RUST_BRIDGE_HANDLER:
-                    flutter_rust_bridge::DefaultHandler<flutter_rust_bridge::for_generated::ThreadPool>
+                    flutter_rust_bridge::DefaultHandler<flutter_rust_bridge::for_generated::SimpleThreadPool>
                     = flutter_rust_bridge::DefaultHandler::new_simple(Default::default());
                 }}
 
                 #[cfg(target_family = "wasm")]
                 thread_local! {{
-                    pub static THREAD_POOL: flutter_rust_bridge::for_generated::ThreadPool = Default::default();
+                    pub static THREAD_POOL: flutter_rust_bridge::for_generated::SimpleThreadPool = Default::default();
                 }}
 
                 #[cfg(target_family = "wasm")]
                 flutter_rust_bridge::for_generated::lazy_static! {{
                     pub static ref FLUTTER_RUST_BRIDGE_HANDLER:
-                    flutter_rust_bridge::DefaultHandler<&'static std::thread::LocalKey<flutter_rust_bridge::for_generated::ThreadPool>>
+                    flutter_rust_bridge::DefaultHandler<&'static std::thread::LocalKey<flutter_rust_bridge::for_generated::SimpleThreadPool>>
                     = flutter_rust_bridge::DefaultHandler::new_simple(&THREAD_POOL);
                 }}
             "#
