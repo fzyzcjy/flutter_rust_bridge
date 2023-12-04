24fc42566d4f2913efe4ec185f595f4a005987c4
fzyzcjy <ch271828n@outlook.com>
2023-12-04 08:49:08 +0800

    feat: owned case

--- frb_rust/src/misc/rust_auto_opaque.rs
+++ frb_rust/src/misc/rust_auto_opaque.rs
@@ -1,11 +1,12 @@
 use crate::{DartSafe, RustOpaque};
-use anyhow::{Context, Result};
+use anyhow::{anyhow, Context, Result};
 use std::sync::RwLock;
 
 impl<T: DartSafe> RustOpaque<RwLock<T>> {
     pub fn rust_auto_opaque_wire2api_owned(self) -> Result<T> {
         self.into_inner()
-            .context("Cannot convert RustOpaque to inner value. This is probably because you are having more than one references to it.")
+            .context("Cannot convert RustOpaque to inner value. This is probably because you are having more than one references to it.")?
+            .into_inner().map_err(|_| anyhow!("lock is poisoned"))
     }
 
     pub fn rust_auto_opaque_wire2api_ref(&self) -> Result<&T> {
