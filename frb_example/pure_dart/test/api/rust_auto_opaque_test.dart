67695ee4468095c0fad829bfbcd7b264a370d1ab
fzyzcjy <ch271828n@outlook.com>
2023-12-04 11:15:34 +0800

    feat: improve tests

--- frb_example/pure_dart/test/api/rust_auto_opaque_test.dart
+++ frb_example/pure_dart/test/api/rust_auto_opaque_test.dart
@@ -7,18 +7,49 @@ import '../test_utils.dart';
 Future<void> main({bool skipRustLibInit = false}) async {
   if (!skipRustLibInit) await RustLib.init();
 
-  test('simple functions', () async {
-    final obj = await rustAutoOpaqueReturnOwnTwinNormal();
+  group('simple functions', () {
+    test('return opaque', () async {
+      final obj = await rustAutoOpaqueReturnOwnTwinNormal(initial: 100);
+      expect(obj.isDisposed, false);
+      obj.dispose();
+    });
 
-    await futurizeVoidTwinNormal(rustAutoOpaqueArgBorrowTwinNormal(arg: obj));
-    expect(obj.isDisposed, false);
+    test('arg owned', () async {
+      final obj = await rustAutoOpaqueReturnOwnTwinNormal(initial: 100);
+      await futurizeVoidTwinNormal(
+          rustAutoOpaqueArgOwnTwinNormal(arg: obj, expect: 100));
+      expect(obj.isDisposed, true);
+    });
 
-    await futurizeVoidTwinNormal(
-        rustAutoOpaqueArgMutBorrowTwinNormal(arg: obj));
-    expect(obj.isDisposed, false);
+    test('arg ref', () async {
+      final obj = await rustAutoOpaqueReturnOwnTwinNormal(initial: 100);
+      await futurizeVoidTwinNormal(
+          rustAutoOpaqueArgBorrowTwinNormal(arg: obj, expect: 100));
+      expect(obj.isDisposed, false);
+    });
 
-    await futurizeVoidTwinNormal(rustAutoOpaqueArgOwnTwinNormal(arg: obj));
-    expect(obj.isDisposed, true);
+    group('arg ref mut', () {
+      test('can be called', () async {
+        final obj = await rustAutoOpaqueReturnOwnTwinNormal(initial: 100);
+        await futurizeVoidTwinNormal(rustAutoOpaqueArgMutBorrowTwinNormal(
+            arg: obj, expect: 100, adder: 1));
+        expect(obj.isDisposed, false);
+      });
+
+      test('does change the internal data', () async {
+        final obj = await rustAutoOpaqueReturnOwnTwinNormal(initial: 100);
+
+        await futurizeVoidTwinNormal(
+            rustAutoOpaqueArgBorrowTwinNormal(arg: obj, expect: 100));
+
+        await futurizeVoidTwinNormal(rustAutoOpaqueArgMutBorrowTwinNormal(
+            arg: obj, expect: 100, adder: 1));
+
+        // expect internal data to change
+        await futurizeVoidTwinNormal(
+            rustAutoOpaqueArgBorrowTwinNormal(arg: obj, expect: 100 + 1));
+      });
+    });
   });
 
   // TODO after `T` call, should auto dispose
