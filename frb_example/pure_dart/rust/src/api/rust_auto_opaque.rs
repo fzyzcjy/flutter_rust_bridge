c8f013b320305636998a19b0441b6b4b513a9aa5
fzyzcjy <ch271828n@outlook.com>
2023-12-04 11:04:50 +0800

    chore: add twin normal

--- frb_example/pure_dart/rust/src/api/rust_auto_opaque.rs
+++ frb_example/pure_dart/rust/src/api/rust_auto_opaque.rs
@@ -10,38 +10,41 @@ pub struct NonCloneSimpleTwinNormal {
 
 // ==================================== simple =======================================
 
-pub fn rust_auto_opaque_arg_own(arg: NonCloneSimpleTwinNormal) {
+pub fn rust_auto_opaque_arg_own_twin_normal(arg: NonCloneSimpleTwinNormal) {
     assert_eq!(arg.inner, 42);
 }
 
-pub fn rust_auto_opaque_arg_borrow(arg: &NonCloneSimpleTwinNormal) {
+pub fn rust_auto_opaque_arg_borrow_twin_normal(arg: &NonCloneSimpleTwinNormal) {
     assert_eq!(arg.inner, 42);
 }
 
-pub fn rust_auto_opaque_arg_mut_borrow(arg: &mut NonCloneSimpleTwinNormal) {
+pub fn rust_auto_opaque_arg_mut_borrow_twin_normal(arg: &mut NonCloneSimpleTwinNormal) {
     assert_eq!(arg.inner, 42);
     arg.inner += 1;
 }
 
-pub fn rust_auto_opaque_return_own() -> NonCloneSimpleTwinNormal {
+pub fn rust_auto_opaque_return_own_twin_normal() -> NonCloneSimpleTwinNormal {
     NonCloneSimpleTwinNormal { inner: 42 }
 }
 
 // ==================================== with other args =======================================
 
-pub fn rust_auto_opaque_arg_own_and_return_own(
+pub fn rust_auto_opaque_arg_own_and_return_own_twin_normal(
     arg: NonCloneSimpleTwinNormal,
 ) -> NonCloneSimpleTwinNormal {
     assert_eq!(arg.inner, 42);
     arg
 }
 
-pub fn rust_auto_opaque_two_args(a: NonCloneSimpleTwinNormal, b: NonCloneSimpleTwinNormal) {
+pub fn rust_auto_opaque_two_args_twin_normal(
+    a: NonCloneSimpleTwinNormal,
+    b: NonCloneSimpleTwinNormal,
+) {
     assert_eq!(a.inner, 42);
     assert_eq!(b.inner, 42);
 }
 
-pub fn rust_auto_opaque_normal_and_opaque_arg(a: NonCloneSimpleTwinNormal, b: String) {
+pub fn rust_auto_opaque_normal_and_opaque_arg_twin_normal(a: NonCloneSimpleTwinNormal, b: String) {
     assert_eq!(a.inner, 42);
     assert_eq!(b, "hello");
 }
@@ -58,21 +61,21 @@ impl MyTraitTwinNormal for String {
 }
 
 /// "+" inside the type signature
-pub fn rust_auto_opaque_plus_sign_arg(arg: Box<dyn MyTraitTwinNormal + Send + Sync>) {
+pub fn rust_auto_opaque_plus_sign_arg_twin_normal(arg: Box<dyn MyTraitTwinNormal + Send + Sync>) {
     assert_eq!(arg.f(), "hello");
 }
 
-pub fn rust_auto_opaque_plus_sign_return() -> Box<dyn MyTraitTwinNormal + Send + Sync> {
+pub fn rust_auto_opaque_plus_sign_return_twin_normal() -> Box<dyn MyTraitTwinNormal + Send + Sync> {
     Box::new("hello".to_owned())
 }
 
-pub fn rust_auto_opaque_callable_arg(
+pub fn rust_auto_opaque_callable_arg_twin_normal(
     arg: Box<dyn Fn(String) -> String + Send + Sync + UnwindSafe + RefUnwindSafe>,
 ) {
     assert_eq!(&arg("hello".into()), "hellohello");
 }
 
-pub fn rust_auto_opaque_callable_return(
+pub fn rust_auto_opaque_callable_return_twin_normal(
 ) -> Box<dyn Fn(String) -> String + Send + Sync + UnwindSafe + RefUnwindSafe> {
     Box::new(|x: String| x.repeat(2))
 }
@@ -107,51 +110,54 @@ impl HelloTraitTwinNormal for HelloTwoEnumTwinNormal {
     }
 }
 
-pub fn rust_auto_opaque_trait_object_arg_own(arg: Box<dyn HelloTraitTwinNormal>, expect: String) {
+pub fn rust_auto_opaque_trait_object_arg_own_twin_normal(
+    arg: Box<dyn HelloTraitTwinNormal>,
+    expect: String,
+) {
     assert_eq!(arg.func_hello(), expect);
 }
 
-pub fn rust_auto_opaque_trait_object_arg_borrow(
+pub fn rust_auto_opaque_trait_object_arg_borrow_twin_normal(
     arg: &Box<dyn HelloTraitTwinNormal>,
     expect: String,
 ) {
     assert_eq!(arg.func_hello(), expect);
 }
 
-pub fn rust_auto_opaque_trait_object_arg_mut_borrow(
+pub fn rust_auto_opaque_trait_object_arg_mut_borrow_twin_normal(
     arg: &mut Box<dyn HelloTraitTwinNormal>,
     expect: String,
 ) {
     assert_eq!(arg.func_hello(), expect);
 }
 
-pub fn rust_auto_opaque_trait_object_return_own_one() -> Box<dyn HelloTraitTwinNormal> {
+pub fn rust_auto_opaque_trait_object_return_own_one_twin_normal() -> Box<dyn HelloTraitTwinNormal> {
     Box::new(HelloOneStructTwinNormal {
         inner: "hello".into(),
     })
 }
 
-pub fn rust_auto_opaque_trait_object_return_own_two() -> Box<dyn HelloTraitTwinNormal> {
+pub fn rust_auto_opaque_trait_object_return_own_two_twin_normal() -> Box<dyn HelloTraitTwinNormal> {
     Box::new(HelloTwoEnumTwinNormal::B)
 }
 
 // ==================================== static method =======================================
 
 impl NonCloneSimpleTwinNormal {
-    pub fn static_method_arg_own(arg: NonCloneSimpleTwinNormal) {
+    pub fn static_method_arg_own_twin_normal(arg: NonCloneSimpleTwinNormal) {
         assert_eq!(arg.inner, 42);
     }
 
-    pub fn static_method_arg_borrow(arg: &NonCloneSimpleTwinNormal) {
+    pub fn static_method_arg_borrow_twin_normal(arg: &NonCloneSimpleTwinNormal) {
         assert_eq!(arg.inner, 42);
     }
 
-    pub fn static_method_arg_mut_borrow(arg: &mut NonCloneSimpleTwinNormal) {
+    pub fn static_method_arg_mut_borrow_twin_normal(arg: &mut NonCloneSimpleTwinNormal) {
         assert_eq!(arg.inner, 42);
         arg.inner += 1;
     }
 
-    pub fn static_method_return_own() -> NonCloneSimpleTwinNormal {
+    pub fn static_method_return_own_twin_normal() -> NonCloneSimpleTwinNormal {
         NonCloneSimpleTwinNormal { inner: 42 }
     }
 }
@@ -160,29 +166,29 @@ impl NonCloneSimpleTwinNormal {
 
 impl NonCloneSimpleTwinNormal {
     /// unnamed constructor
-    pub fn new() -> NonCloneSimpleTwinNormal {
+    pub fn new_twin_normal() -> NonCloneSimpleTwinNormal {
         Self { inner: 42 }
     }
 
     /// named constructor
-    pub fn new_custom_name() -> NonCloneSimpleTwinNormal {
+    pub fn new_custom_name_twin_normal() -> NonCloneSimpleTwinNormal {
         Self { inner: 42 }
     }
 
-    pub fn instance_method_arg_own(self) {
+    pub fn instance_method_arg_own_twin_normal(self) {
         assert_eq!(self.inner, 42);
     }
 
-    pub fn instance_method_arg_borrow(&self) {
+    pub fn instance_method_arg_borrow_twin_normal(&self) {
         assert_eq!(self.inner, 42);
     }
 
-    pub fn instance_method_arg_mut_borrow(&mut self) {
+    pub fn instance_method_arg_mut_borrow_twin_normal(&mut self) {
         assert_eq!(self.inner, 42);
         self.inner += 1;
     }
 
-    pub fn instance_method_return_own(&self) -> NonCloneSimpleTwinNormal {
+    pub fn instance_method_return_own_twin_normal(&self) -> NonCloneSimpleTwinNormal {
         Self { inner: 42 }
     }
 }
@@ -195,21 +201,21 @@ pub struct StructWithGoodAndOpaqueFieldTwinNormal {
     pub opaque: NonCloneSimpleTwinNormal,
 }
 
-pub fn rust_auto_opaque_struct_with_good_and_opaque_field_arg_own(
+pub fn rust_auto_opaque_struct_with_good_and_opaque_field_arg_own_twin_normal(
     arg: StructWithGoodAndOpaqueFieldTwinNormal,
 ) {
     assert_eq!(&arg.good, "hello");
     assert_eq!(arg.opaque.inner, 42);
 }
 
-pub fn rust_auto_opaque_struct_with_good_and_opaque_field_arg_borrow(
+pub fn rust_auto_opaque_struct_with_good_and_opaque_field_arg_borrow_twin_normal(
     arg: &StructWithGoodAndOpaqueFieldTwinNormal,
 ) {
     assert_eq!(&arg.good, "hello");
     assert_eq!(arg.opaque.inner, 42);
 }
 
-pub fn rust_auto_opaque_struct_with_good_and_opaque_field_arg_mut_borrow(
+pub fn rust_auto_opaque_struct_with_good_and_opaque_field_arg_mut_borrow_twin_normal(
     arg: &mut StructWithGoodAndOpaqueFieldTwinNormal,
 ) {
     assert_eq!(&arg.good, "hello");
@@ -218,7 +224,7 @@ pub fn rust_auto_opaque_struct_with_good_and_opaque_field_arg_mut_borrow(
     arg.opaque.inner += 1;
 }
 
-pub fn rust_auto_opaque_struct_with_good_and_opaque_field_return_own(
+pub fn rust_auto_opaque_struct_with_good_and_opaque_field_return_own_twin_normal(
 ) -> StructWithGoodAndOpaqueFieldTwinNormal {
     StructWithGoodAndOpaqueFieldTwinNormal {
         good: "hello".to_string(),
