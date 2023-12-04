5bc1bc46109136112d7e6635baf85bee27d0dd07
fzyzcjy <ch271828n@outlook.com>
2023-12-04 08:58:34 +0800

    feat: rust_auto_opaque_wire2api_ref

--- frb_rust/src/misc/rust_auto_opaque.rs
+++ frb_rust/src/misc/rust_auto_opaque.rs
@@ -1,6 +1,7 @@
 use crate::{DartSafe, RustOpaque};
 use anyhow::{anyhow, Context, Result};
-use std::sync::RwLock;
+use std::ops::Deref;
+use std::sync::{RwLock, RwLockReadGuard, RwLockWriteGuard};
 
 impl<T: DartSafe> RustOpaque<RwLock<T>> {
     pub fn rust_auto_opaque_wire2api_owned(self) -> Result<T> {
@@ -9,12 +10,12 @@ impl<T: DartSafe> RustOpaque<RwLock<T>> {
             .into_inner().map_err(|_| anyhow!("lock is poisoned"))
     }
 
-    pub fn rust_auto_opaque_wire2api_ref(&self) -> Result<&T> {
-        Ok(&self)
+    pub fn rust_auto_opaque_wire2api_ref(&self) -> Result<RwLockReadGuard<'_, T>> {
+        self.read().map_err(|_| anyhow!("lock is poisioned"))
     }
 
-    pub fn rust_auto_opaque_wire2api_ref_mut(&self) -> Result<&mut T> {
-        Ok(todo!())
+    pub fn rust_auto_opaque_wire2api_ref_mut(&self) -> Result<RwLockWriteGuard<'_, T>> {
+        self.write().map_err(|_| anyhow!("lock is poisioned"))
     }
 }
 
