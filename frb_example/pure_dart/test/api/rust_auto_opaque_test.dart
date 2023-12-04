4ec0db76252b04f2485c69939ae2c36138bd3034
fzyzcjy <ch271828n@outlook.com>
2023-12-04 11:03:35 +0800

    chore: comments

--- frb_example/pure_dart/test/api/rust_auto_opaque_test.dart
+++ frb_example/pure_dart/test/api/rust_auto_opaque_test.dart
@@ -3,5 +3,10 @@ import 'package:frb_example_pure_dart/src/rust/frb_generated.dart';
 Future<void> main({bool skipRustLibInit = false}) async {
   if (!skipRustLibInit) await RustLib.init();
 
-  throw Exception('TODO');
+  // TODO after `T` call, should auto dispose
+  // TODO after `&T`, `&mut T` call, should NOT auto dispose
+  // TODO can call multiple `&T` concurrently
+  // TODO can NOT call `&T` + `&mut T` concurrently
+  // TODO can NOT call multiple `&mut T` concurrently
+  // TODO all apis
 }
