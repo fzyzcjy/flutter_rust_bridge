9bd44cb6ee8f61a415f5e94816191f1f5a054263
fzyzcjy <ch271828n@outlook.com>
2023-12-04 17:13:15 +0800

    refactor: simplify, fix compile

--- frb_rust/src/handler/implementation/executor.rs
+++ frb_rust/src/handler/implementation/executor.rs
@@ -4,7 +4,7 @@ use crate::handler::error_handler::ErrorHandler;
 use crate::handler::executor::Executor;
 use crate::handler::handler::{FfiCallMode, TaskContext, TaskInfo, TaskRetFutTrait};
 use crate::misc::into_into_dart::IntoIntoDart;
-use crate::platform_types::MessagePort;
+use crate::platform_types::{DartAbi, MessagePort};
 use crate::rust2dart::action::Rust2DartAction;
 use crate::rust2dart::api2wire::Api2Wire;
 use crate::rust2dart::context::TaskRust2DartContext;
@@ -12,7 +12,6 @@ use crate::rust2dart::sender::Rust2DartSender;
 use crate::rust2dart::wire_sync_return_src::WireSyncReturnSrc;
 use crate::thread_pool::{BaseThreadPool, ThreadPool};
 use crate::{rust_async, transfer};
-use allo_isolate::ffi::DartCObject;
 use futures::FutureExt;
 use parking_lot::Mutex;
 use std::future::Future;
@@ -58,25 +57,23 @@ impl<EH: ErrorHandler + Sync, TP: BaseThreadPool> Executor for SimpleExecutor<EH
         let eh2 = self.error_handler;
 
         let TaskInfo { port, mode, .. } = task_info;
+        let port = port.unwrap();
 
-        self.thread_pool
-            .execute(transfer!(|port: Option<MessagePort>| {
-                let port2 = port.as_ref().cloned();
-                let thread_result = panic::catch_unwind(move || {
-                    let port2 = port2.expect("(worker) thread");
-                    #[allow(clippy::clone_on_copy)]
-                    let sender = Rust2DartSender::new(Channel::new(port2.clone()));
+        self.thread_pool.execute(transfer!(|port: MessagePort| {
+            let thread_result = panic::catch_unwind(|| {
+                #[allow(clippy::clone_on_copy)]
+                let sender = Rust2DartSender::new(Channel::new(port.clone()));
 
-                    let task_context = TaskContext::new(TaskRust2DartContext::new(sender.clone()));
-                    let ret = task(task_context).map(|e| e.into_into_dart().into_dart());
+                let task_context = TaskContext::new(TaskRust2DartContext::new(sender.clone()));
+                let ret = task(task_context).map(|e| e.into_into_dart().into_dart());
 
-                    ExecuteNormalOrAsyncUtils::handle_result(ret, mode, sender, eh2, port2);
-                });
+                ExecuteNormalOrAsyncUtils::handle_result(ret, mode, sender, eh2, port);
+            });
 
-                if let Err(error) = thread_result {
-                    eh.handle_error(port.expect("(worker) eh"), Error::Panic(error));
-                }
-            }));
+            if let Err(error) = thread_result {
+                eh.handle_error(port, Error::Panic(error));
+            }
+        }));
     }
 
     fn execute_sync<SyncTaskFn, TaskRetDirect, TaskRetData, Er>(
@@ -112,25 +109,24 @@ impl<EH: ErrorHandler + Sync, TP: BaseThreadPool> Executor for SimpleExecutor<EH
 
         rust_async::spawn(async move {
             let TaskInfo { port, mode, .. } = task_info;
-            let port2 = port.as_ref().cloned();
+            let port = port.unwrap();
 
             let async_result = async {
-                let port2 = port2.expect("(worker) thread");
                 #[allow(clippy::clone_on_copy)]
-                let sender = Rust2DartSender::new(Channel::new(port2.clone()));
+                let sender = Rust2DartSender::new(Channel::new(port.clone()));
 
                 let task_context = TaskContext::new(TaskRust2DartContext::new(sender.clone()));
                 let ret = task(task_context)
                     .await
                     .map(|e| e.into_into_dart().into_dart());
 
-                ExecuteNormalOrAsyncUtils::handle_result(ret, mode, sender, eh2, port2);
+                ExecuteNormalOrAsyncUtils::handle_result(ret, mode, sender, eh2, port);
             }
             .catch_unwind()
             .await;
 
             if let Err(error) = async_result {
-                eh.handle_error(port.expect("(worker) eh"), Error::Panic(error));
+                eh.handle_error(port, Error::Panic(error));
             }
         });
     }
@@ -140,7 +136,7 @@ struct ExecuteNormalOrAsyncUtils;
 
 impl ExecuteNormalOrAsyncUtils {
     fn handle_result<EH: ErrorHandler + Sync, Er: IntoDart + 'static>(
-        ret: Result<DartCObject, Er>,
+        ret: Result<DartAbi, Er>,
         mode: FfiCallMode,
         sender: Rust2DartSender,
         eh: EH,
