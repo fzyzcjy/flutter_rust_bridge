bf4f7bc8eab4f3b7f13dd0a40b4dda27f2a00291
fzyzcjy <ch271828n@outlook.com>
2023-12-04 11:18:43 +0800

    feat: more test

--- frb_example/pure_dart/test/api/rust_auto_opaque_test.dart
+++ frb_example/pure_dart/test/api/rust_auto_opaque_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_rust_bridge/src/droppable/droppable.dart';
 import 'package:frb_example_pure_dart/src/rust/api/rust_auto_opaque.dart';
 import 'package:frb_example_pure_dart/src/rust/frb_generated.dart';
 import 'package:test/test.dart';
@@ -14,18 +15,44 @@ Future<void> main({bool skipRustLibInit = false}) async {
       obj.dispose();
     });
 
-    test('arg owned', () async {
-      final obj = await rustAutoOpaqueReturnOwnTwinNormal(initial: 100);
-      await futurizeVoidTwinNormal(
-          rustAutoOpaqueArgOwnTwinNormal(arg: obj, expect: 100));
-      expect(obj.isDisposed, true);
+    group('arg owned', () {
+      test('can be called', () async {
+        final obj = await rustAutoOpaqueReturnOwnTwinNormal(initial: 100);
+        await futurizeVoidTwinNormal(
+            rustAutoOpaqueArgOwnTwinNormal(arg: obj, expect: 100));
+      });
+
+      test('after call, the object cannot be used again', () async {
+        final obj = await rustAutoOpaqueReturnOwnTwinNormal(initial: 100);
+        await futurizeVoidTwinNormal(
+            rustAutoOpaqueArgOwnTwinNormal(arg: obj, expect: 100));
+
+        expect(obj.isDisposed, true);
+
+        await expectLater(
+            () => rustAutoOpaqueArgBorrowTwinNormal(arg: obj, expect: 100),
+            throwsA(isA<DroppableDisposedException>()));
+      });
     });
 
-    test('arg ref', () async {
-      final obj = await rustAutoOpaqueReturnOwnTwinNormal(initial: 100);
-      await futurizeVoidTwinNormal(
-          rustAutoOpaqueArgBorrowTwinNormal(arg: obj, expect: 100));
-      expect(obj.isDisposed, false);
+    group('arg ref', () {
+      test('can be called', () async {
+        final obj = await rustAutoOpaqueReturnOwnTwinNormal(initial: 100);
+        await futurizeVoidTwinNormal(
+            rustAutoOpaqueArgBorrowTwinNormal(arg: obj, expect: 100));
+        expect(obj.isDisposed, false);
+      });
+
+      test('after call, the object can still be used again', () async {
+        final obj = await rustAutoOpaqueReturnOwnTwinNormal(initial: 100);
+        await futurizeVoidTwinNormal(
+            rustAutoOpaqueArgBorrowTwinNormal(arg: obj, expect: 100));
+
+        expect(obj.isDisposed, false);
+
+        await futurizeVoidTwinNormal(
+            rustAutoOpaqueArgBorrowTwinNormal(arg: obj, expect: 100));
+      });
     });
 
     group('arg ref mut', () {
@@ -34,6 +61,22 @@ Future<void> main({bool skipRustLibInit = false}) async {
         await futurizeVoidTwinNormal(rustAutoOpaqueArgMutBorrowTwinNormal(
             arg: obj, expect: 100, adder: 1));
         expect(obj.isDisposed, false);
+      });
+
+      test('after call, the object can still be used again', () async {
+        final obj = await rustAutoOpaqueReturnOwnTwinNormal(initial: 100);
+        await futurizeVoidTwinNormal(rustAutoOpaqueArgMutBorrowTwinNormal(
+            arg: obj, expect: 100, adder: 1));
+
+        expect(obj.isDisposed, false);
+
+        await futurizeVoidTwinNormal(rustAutoOpaqueArgMutBorrowTwinNormal(
+            arg: obj, expect: 101, adder: 10));
+
+        expect(obj.isDisposed, false);
+
+        await futurizeVoidTwinNormal(
+            rustAutoOpaqueArgBorrowTwinNormal(arg: obj, expect: 111));
       });
 
       test('does change the internal data', () async {
@@ -50,12 +93,25 @@ Future<void> main({bool skipRustLibInit = false}) async {
             rustAutoOpaqueArgBorrowTwinNormal(arg: obj, expect: 100 + 1));
       });
     });
+
+    group('concurrent calls', () {
+      test('cannot call multiple `T` concurrently', () async {
+        TODO;
+      });
+
+      test('can call multiple `&T` concurrently', () async {
+        TODO;
+      });
+
+      test('cannot call multiple `&mut T` concurrently', () async {
+        TODO;
+      });
+
+      test('cannot call one `&T` and one `&mut T` concurrently', () async {
+        TODO;
+      });
+    });
   });
 
-  // TODO after `T` call, should auto dispose
-  // TODO after `&T`, `&mut T` call, should NOT auto dispose
-  // TODO can call multiple `&T` concurrently
-  // TODO can NOT call `&T` + `&mut T` concurrently
-  // TODO can NOT call multiple `&mut T` concurrently
-  // TODO all apis
+  // TODO other apis
 }
