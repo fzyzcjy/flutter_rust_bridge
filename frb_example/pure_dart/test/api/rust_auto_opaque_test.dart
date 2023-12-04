46b32c43a399efd7ae6ae1e5e0622ae0fb6c4228
fzyzcjy <ch271828n@outlook.com>
2023-12-04 11:07:58 +0800

    feat: add a test

--- frb_example/pure_dart/test/api/rust_auto_opaque_test.dart
+++ frb_example/pure_dart/test/api/rust_auto_opaque_test.dart
@@ -1,7 +1,19 @@
+import 'package:frb_example_pure_dart/src/rust/api/rust_auto_opaque.dart';
 import 'package:frb_example_pure_dart/src/rust/frb_generated.dart';
+import 'package:test/test.dart';
+
+import '../test_utils.dart';
 
 Future<void> main({bool skipRustLibInit = false}) async {
   if (!skipRustLibInit) await RustLib.init();
+
+  test('simple functions', () async {
+    final obj = await rustAutoOpaqueReturnOwnTwinNormal();
+    await futurizeVoidTwinNormal(rustAutoOpaqueArgOwnTwinNormal(arg: obj));
+    await futurizeVoidTwinNormal(rustAutoOpaqueArgBorrowTwinNormal(arg: obj));
+    await futurizeVoidTwinNormal(
+        rustAutoOpaqueArgMutBorrowTwinNormal(arg: obj));
+  });
 
   // TODO after `T` call, should auto dispose
   // TODO after `&T`, `&mut T` call, should NOT auto dispose
