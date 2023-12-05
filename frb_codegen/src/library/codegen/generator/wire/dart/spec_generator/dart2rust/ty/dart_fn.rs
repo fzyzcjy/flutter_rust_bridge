1415c844289c44634655b1fe033d0e465dafd26f
fzyzcjy <ch271828n@outlook.com>
2023-12-05 19:19:04 +0800

    fix: cannot find func

--- frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/dart2rust/ty/dart_fn.rs
+++ frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/dart2rust/ty/dart_fn.rs
@@ -1,5 +1,5 @@
 use crate::codegen::generator::acc::Acc;
-use crate::codegen::generator::misc::target::Target;
+use crate::codegen::generator::misc::target::{Target, TargetOrCommon};
 use crate::codegen::generator::wire::dart::spec_generator::base::*;
 use crate::codegen::generator::wire::dart::spec_generator::dart2rust::ty::WireDartGeneratorDart2RustTrait;
 use crate::codegen::ir::ty::dart_opaque::IrTypeDartOpaque;
@@ -7,7 +7,12 @@ use crate::codegen::ir::ty::IrType;
 
 impl<'a> WireDartGeneratorDart2RustTrait for DartFnWireDartGenerator<'a> {
     fn api2wire_body(&self) -> Acc<Option<String>> {
-        Acc::new_common(Some("return api2wire_DartOpaque(raw);".to_owned()))
+        Acc::new(|target| match target {
+            TargetOrCommon::Io | TargetOrCommon::Wasm => {
+                Some("return api2wire_DartOpaque(raw);".to_owned())
+            }
+            TargetOrCommon::Common => None,
+        })
     }
 
     fn dart_wire_type(&self, target: Target) -> String {
