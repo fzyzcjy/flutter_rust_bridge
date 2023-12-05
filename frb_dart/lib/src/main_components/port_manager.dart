c90baf5ec4a44424f6d9830159bc5ab01ddc3241
fzyzcjy <ch271828n@outlook.com>
2023-12-05 17:14:05 +0800

    feat: DartFnInvokePortManager

--- frb_dart/lib/src/main_components/port_manager.dart
+++ frb_dart/lib/src/main_components/port_manager.dart
@@ -1,21 +1,28 @@
 import 'package:flutter_rust_bridge/src/generalized_frb_rust_binding/generalized_frb_rust_binding.dart';
+import 'package:flutter_rust_bridge/src/misc/dart_fn.dart';
 import 'package:flutter_rust_bridge/src/misc/dart_opaque.dart';
 import 'package:flutter_rust_bridge/src/platform_types/platform_types.dart';
 
 /// {@macro flutter_rust_bridge.only_for_generated_code}
 class PortManager {
   final DartOpaqueDropPortManager _dartOpaqueDropPortManager;
+  final DartFnInvokePortManager _dartFnInvokePortManager;
 
   /// {@macro flutter_rust_bridge.only_for_generated_code}
   PortManager(GeneralizedFrbRustBinding generalizedFrbRustBinding)
       : _dartOpaqueDropPortManager =
-            DartOpaqueDropPortManager(generalizedFrbRustBinding);
+            DartOpaqueDropPortManager(generalizedFrbRustBinding),
+        _dartFnInvokePortManager = DartFnInvokePortManager();
 
   /// {@macro flutter_rust_bridge.only_for_generated_code}
   NativePortType get dartOpaqueDropPort => _dartOpaqueDropPortManager.port;
 
   /// {@macro flutter_rust_bridge.only_for_generated_code}
+  NativePortType get dartFnInvokePort => _dartFnInvokePortManager.port;
+
+  /// {@macro flutter_rust_bridge.only_for_generated_code}
   void dispose() {
     _dartOpaqueDropPortManager.dispose();
+    _dartFnInvokePortManager.dispose();
   }
 }
---
+++ frb_dart/lib/src/misc/dart_fn.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_rust_bridge/src/generalized_frb_rust_binding/generalized_frb_rust_binding.dart';
+import 'package:flutter_rust_bridge/src/utils/base_lazy_port_manager.dart';
+
+/// {@macro flutter_rust_bridge.only_for_generated_code}
+class DartFnInvokePortManager extends BaseLazyPortManager {
+  @override
+  void onData(dynamic message) {
+    TODO;
+  }
+}
