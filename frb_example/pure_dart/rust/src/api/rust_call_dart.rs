bed0fc92ee826efe150cd9254d28e9c7ec0950d2
fzyzcjy <ch271828n@outlook.com>
2023-12-05 11:40:41 +0800

    feat: more func

--- frb_example/pure_dart/rust/src/api/rust_call_dart.rs
+++ frb_example/pure_dart/rust/src/api/rust_call_dart.rs
@@ -17,8 +17,40 @@ impl<F> std::ops::Deref for DartFn<F> {
     }
 }
 
+struct DemoStructForRustCallDart {
+    pub name: String,
+}
+
 pub async fn rust_call_dart_simple(callback: DartFn<fn() -> BoxFuture<()>>) {
     println!("rust_call_dart_simple before");
     callback().await;
     println!("rust_call_dart_simple after");
+}
+
+pub async fn rust_call_dart_one_arg(callback: DartFn<fn(String) -> BoxFuture<()>>) {
+    callback("a".to_owned()).await;
+}
+
+pub async fn rust_call_dart_two_args(
+    callback: DartFn<fn(String, DemoStructForRustCallDart) -> BoxFuture<()>>,
+) {
+    callback(
+        "a".to_owned(),
+        DemoStructForRustCallDart {
+            name: "b".to_owned(),
+        },
+    )
+    .await;
+}
+
+pub async fn rust_call_dart_return(callback: DartFn<fn() -> BoxFuture<String>>) {
+    let result = callback().await;
+    assert_eq!(&result, "a");
+}
+
+pub async fn rust_call_dart_loopback(
+    callback: DartFn<fn(DemoStructForRustCallDart) -> BoxFuture<DemoStructForRustCallDart>>,
+) {
+    let result = callback(DemoStructForRustCallDart { name: "a".into() }).await;
+    assert_eq!(&result.name, "a");
 }
