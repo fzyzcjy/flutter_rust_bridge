9e8e81a3b0159dee57c89352d6399b4d993267bd
fzyzcjy <ch271828n@outlook.com>
2023-12-05 20:16:34 +0800

    refactor: api2wireDartOpaque io

--- frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/dart2rust/ty/dart_opaque.rs
+++ frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/dart2rust/ty/dart_opaque.rs
@@ -1,22 +1,16 @@
 use crate::codegen::generator::acc::Acc;
-use crate::codegen::generator::misc::target::Target;
+use crate::codegen::generator::misc::target::{Target, TargetOrCommon};
 use crate::codegen::generator::wire::dart::spec_generator::base::*;
 use crate::codegen::generator::wire::dart::spec_generator::dart2rust::ty::WireDartGeneratorDart2RustTrait;
 
 impl<'a> WireDartGeneratorDart2RustTrait for DartOpaqueWireDartGenerator<'a> {
     fn api2wire_body(&self) -> Acc<Option<String>> {
-        Acc {
-            io: Some(
-                "
-                final ptr = wire.new_DartOpaque();
-                _api_fill_to_wire_DartOpaque(raw, ptr);
-                return ptr;
-                "
-                .to_owned(),
-            ),
-            wasm: Some("return api2wireDartOpaque(raw);".to_owned()),
-            ..Default::default()
-        }
+        Acc::new(|target| match target {
+            TargetOrCommon::Io | TargetOrCommon::Wasm => {
+                Some("return api2wireDartOpaque(raw);".to_owned())
+            }
+            TargetOrCommon::Common => None,
+        })
     }
 
     fn api_fill_to_wire_body(&self) -> Option<String> {
---
+++ frb_dart/lib/src/dart_opaque/_io.dart
@@ -0,0 +1,6 @@
+/// {@macro flutter_rust_bridge.only_for_generated_code}
+wire_DartOpaque api2wireDartOpaque(Object raw) {
+  final ptr = wire.new_DartOpaque();
+  _api_fill_to_wire_DartOpaque(raw, ptr);
+  return ptr;
+}
