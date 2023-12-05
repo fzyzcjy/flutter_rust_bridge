6d664479f5f90e43dd2b2c075d97f8812a3e6019
fzyzcjy <ch271828n@outlook.com>
2023-12-05 13:03:29 +0800

    feat: find name

--- frb_codegen/src/library/codegen/parser/type_parser/dart_fn.rs
+++ frb_codegen/src/library/codegen/parser/type_parser/dart_fn.rs
@@ -1,7 +1,7 @@
 use crate::codegen::ir::ty::primitive::IrTypePrimitive;
 use crate::codegen::ir::ty::IrType;
 use crate::codegen::ir::ty::IrType::Primitive;
-use crate::codegen::parser::type_parser::unencodable::SplayedSegment;
+use crate::codegen::parser::type_parser::unencodable::{ArgsRefs, SplayedSegment};
 use crate::codegen::parser::type_parser::TypeParserWithContext;
 
 impl<'a, 'b, 'c> TypeParserWithContext<'a, 'b, 'c> {
@@ -9,6 +9,15 @@ impl<'a, 'b, 'c> TypeParserWithContext<'a, 'b, 'c> {
         &mut self,
         last_segment: &SplayedSegment,
     ) -> anyhow::Result<Option<IrType>> {
+        Ok(Some(match last_segment {
+            ("DartFn", inner) => self.parse_dart_fn(inner),
+
+            _ => return Ok(None),
+        }))
+    }
+
+    fn parse_dart_fn(&mut self, inner: &Option<ArgsRefs>) -> IrType {
+        log::info!("hi {inner:?}");
         todo!()
     }
 }
