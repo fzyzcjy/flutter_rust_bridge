5e936ae31be3caa438ec27b2842575c91a505331
fzyzcjy <ch271828n@outlook.com>
2023-12-05 13:09:25 +0800

    chore: dummy test

--- frb_codegen/src/library/codegen/parser/type_parser/dart_fn.rs
+++ frb_codegen/src/library/codegen/parser/type_parser/dart_fn.rs
@@ -24,3 +24,13 @@ impl<'a, 'b, 'c> TypeParserWithContext<'a, 'b, 'c> {
         todo!("{}", raw)
     }
 }
+
+// Use this unit "test" to see how a type will be parsed into a tree
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn test_syn_parse_example() {
+//         let ans: syn::Type = syn::parse_str("fn(DartOpaque) -> BoxFuture<String>").unwrap();
+//         println!("{ans:#?}");
+//     }
+// }
