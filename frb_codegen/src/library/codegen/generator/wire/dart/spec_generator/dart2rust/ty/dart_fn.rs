eca00d0ee994099c2d9f65f7a87c0164d56a938e
fzyzcjy <ch271828n@outlook.com>
2023-12-05 13:56:24 +0800

    feat: dart side dart2rust

--- frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/dart2rust/ty/dart_fn.rs
+++ frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/dart2rust/ty/dart_fn.rs
@@ -2,17 +2,16 @@ use crate::codegen::generator::acc::Acc;
 use crate::codegen::generator::misc::target::Target;
 use crate::codegen::generator::wire::dart::spec_generator::base::*;
 use crate::codegen::generator::wire::dart::spec_generator::dart2rust::ty::WireDartGeneratorDart2RustTrait;
+use crate::codegen::ir::ty::dart_opaque::IrTypeDartOpaque;
+use crate::codegen::ir::ty::IrType;
 
 impl<'a> WireDartGeneratorDart2RustTrait for DartFnWireDartGenerator<'a> {
     fn api2wire_body(&self) -> Acc<Option<String>> {
-        Acc {
-            io: Some("todo_api2wire_body;".into()),
-            wasm: Some("todo_api2wire_body;".into()),
-            ..Default::default()
-        }
+        Acc::new_common(Some("return api2wire_DartOpaque(raw);".to_owned()))
     }
 
     fn dart_wire_type(&self, target: Target) -> String {
-        "todo_dart_wire_type".into()
+        WireDartGenerator::new(IrType::DartOpaque(IrTypeDartOpaque), self.context)
+            .dart_wire_type(target)
     }
 }
