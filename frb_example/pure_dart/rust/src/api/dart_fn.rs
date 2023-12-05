4d9f75bd9e2fd47c2fcc5f173baea591783095da
fzyzcjy <ch271828n@outlook.com>
2023-12-05 14:40:33 +0800

    chore: temp only one func

--- frb_example/pure_dart/rust/src/api/dart_fn.rs
+++ frb_example/pure_dart/rust/src/api/dart_fn.rs
@@ -13,45 +13,46 @@ pub async fn rust_call_dart_simple(callback: DartFn<fn() -> BoxFuture<'static, (
     println!("rust_call_dart_simple after");
 }
 
-pub async fn rust_call_dart_one_arg(callback: DartFn<fn(String) -> BoxFuture<'static, ()>>) {
-    callback("a".to_owned()).await;
-}
-
-pub async fn rust_call_dart_two_args(
-    callback: DartFn<fn(String, DemoStructForRustCallDart) -> BoxFuture<'static, ()>>,
-) {
-    callback(
-        "a".to_owned(),
-        DemoStructForRustCallDart {
-            name: "b".to_owned(),
-        },
-    )
-    .await;
-}
-
-pub async fn rust_call_dart_return(callback: DartFn<fn() -> BoxFuture<'static, String>>) {
-    let result = callback().await;
-    assert_eq!(&result, "a");
-}
-
-pub async fn rust_call_dart_loopback(
-    callback: DartFn<
-        fn(DemoStructForRustCallDart) -> BoxFuture<'static, DemoStructForRustCallDart>,
-    >,
-) {
-    let result = callback(DemoStructForRustCallDart { name: "a".into() }).await;
-    assert_eq!(&result.name, "a");
-}
-
-pub async fn rust_call_dart_with_dart_opaque_arg(
-    input: DartOpaque,
-    callback: DartFn<fn(DartOpaque) -> BoxFuture<'static, ()>>,
-) {
-    callback(input).await
-}
-
-pub async fn rust_call_dart_with_dart_opaque_result(
-    callback: DartFn<fn() -> BoxFuture<'static, DartOpaque>>,
-) -> DartOpaque {
-    callback().await
-}
+// TODO
+// pub async fn rust_call_dart_one_arg(callback: DartFn<fn(String) -> BoxFuture<'static, ()>>) {
+//     callback("a".to_owned()).await;
+// }
+//
+// pub async fn rust_call_dart_two_args(
+//     callback: DartFn<fn(String, DemoStructForRustCallDart) -> BoxFuture<'static, ()>>,
+// ) {
+//     callback(
+//         "a".to_owned(),
+//         DemoStructForRustCallDart {
+//             name: "b".to_owned(),
+//         },
+//     )
+//     .await;
+// }
+//
+// pub async fn rust_call_dart_return(callback: DartFn<fn() -> BoxFuture<'static, String>>) {
+//     let result = callback().await;
+//     assert_eq!(&result, "a");
+// }
+//
+// pub async fn rust_call_dart_loopback(
+//     callback: DartFn<
+//         fn(DemoStructForRustCallDart) -> BoxFuture<'static, DemoStructForRustCallDart>,
+//     >,
+// ) {
+//     let result = callback(DemoStructForRustCallDart { name: "a".into() }).await;
+//     assert_eq!(&result.name, "a");
+// }
+//
+// pub async fn rust_call_dart_with_dart_opaque_arg(
+//     input: DartOpaque,
+//     callback: DartFn<fn(DartOpaque) -> BoxFuture<'static, ()>>,
+// ) {
+//     callback(input).await
+// }
+//
+// pub async fn rust_call_dart_with_dart_opaque_result(
+//     callback: DartFn<fn() -> BoxFuture<'static, DartOpaque>>,
+// ) -> DartOpaque {
+//     callback().await
+// }
