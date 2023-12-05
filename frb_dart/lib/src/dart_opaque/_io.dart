f0a06958c505a4de86ec10af6ac1b106fe0d5730
fzyzcjy <ch271828n@outlook.com>
2023-12-05 20:42:38 +0800

    fix: dart compile

--- frb_dart/lib/src/dart_opaque/_io.dart
+++ frb_dart/lib/src/dart_opaque/_io.dart
@@ -1,7 +1,12 @@
+import 'dart:ffi' as ffi;
+
 import 'package:flutter_rust_bridge/src/generalized_frb_rust_binding/generalized_frb_rust_binding.dart';
+import 'package:flutter_rust_bridge/src/platform_types/platform_types.dart';
 
 /// {@macro flutter_rust_bridge.only_for_generated_code}
-int api2wireDartOpaque(
+PlatformPointer api2wireDartOpaque(
     Object raw, GeneralizedFrbRustBinding generalizedFrbRustBinding) {
-  return generalizedFrbRustBinding.newDartOpaque(raw);
+  final ptr = generalizedFrbRustBinding.newDartOpaque(raw);
+  // TODO let the binding directly give a pointer instead
+  return ffi.Pointer.fromAddress(ptr);
 }
