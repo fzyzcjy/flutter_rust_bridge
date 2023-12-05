089bd0d696c844350b5ed7e2ee5c3c85eafd4857
fzyzcjy <ch271828n@outlook.com>
2023-12-05 11:31:50 +0800

    feat: more

--- frb_example/pure_dart/rust/src/api/rust_call_dart.rs
+++ frb_example/pure_dart/rust/src/api/rust_call_dart.rs
@@ -1,10 +1,19 @@
 // FRB_INTERNAL_GENERATOR: {"forbiddenDuplicatorModes": ["sync", "rustAsync"]}
 
 // TODO
-struct DartCallable<F> {
-    closure: F,
+struct DartFn<F> {
+    inner: F,
 }
 
-pub async fn rust_call_dart_simple(callback: DartCallable<fn()>) {
-    todo!()
+// https://github.com/rust-lang/rust/issues/29625#issuecomment-1692602873
+impl<F> std::ops::Deref for DartFn<F> {
+    type Target = F;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+pub async fn rust_call_dart_simple(callback: DartFn<fn()>) {
+    callback()
 }
