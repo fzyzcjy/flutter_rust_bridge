dc6fbf6b98e83d473ea9bebc6c00ecb5780f2cf2
fzyzcjy <ch271828n@outlook.com>
2023-12-05 14:02:22 +0800

    fix: compile

--- frb_example/pure_dart/rust/src/api/dart_fn.rs
+++ frb_example/pure_dart/rust/src/api/dart_fn.rs
@@ -7,18 +7,18 @@ pub struct DemoStructForRustCallDart {
     pub name: String,
 }
 
-pub async fn rust_call_dart_simple(callback: DartFn<fn() -> BoxFuture<()>>) {
+pub async fn rust_call_dart_simple(callback: DartFn<fn() -> BoxFuture<'static, ()>>) {
     println!("rust_call_dart_simple before");
     callback().await;
     println!("rust_call_dart_simple after");
 }
 
-pub async fn rust_call_dart_one_arg(callback: DartFn<fn(String) -> BoxFuture<()>>) {
+pub async fn rust_call_dart_one_arg(callback: DartFn<fn(String) -> BoxFuture<'static, ()>>) {
     callback("a".to_owned()).await;
 }
 
 pub async fn rust_call_dart_two_args(
-    callback: DartFn<fn(String, DemoStructForRustCallDart) -> BoxFuture<()>>,
+    callback: DartFn<fn(String, DemoStructForRustCallDart) -> BoxFuture<'static, ()>>,
 ) {
     callback(
         "a".to_owned(),
@@ -29,13 +29,15 @@ pub async fn rust_call_dart_two_args(
     .await;
 }
 
-pub async fn rust_call_dart_return(callback: DartFn<fn() -> BoxFuture<String>>) {
+pub async fn rust_call_dart_return(callback: DartFn<fn() -> BoxFuture<'static, String>>) {
     let result = callback().await;
     assert_eq!(&result, "a");
 }
 
 pub async fn rust_call_dart_loopback(
-    callback: DartFn<fn(DemoStructForRustCallDart) -> BoxFuture<DemoStructForRustCallDart>>,
+    callback: DartFn<
+        fn(DemoStructForRustCallDart) -> BoxFuture<'static, DemoStructForRustCallDart>,
+    >,
 ) {
     let result = callback(DemoStructForRustCallDart { name: "a".into() }).await;
     assert_eq!(&result.name, "a");
@@ -43,13 +45,13 @@ pub async fn rust_call_dart_loopback(
 
 pub async fn rust_call_dart_with_dart_opaque_arg(
     input: DartOpaque,
-    callback: DartFn<fn(DartOpaque) -> BoxFuture<()>>,
+    callback: DartFn<fn(DartOpaque) -> BoxFuture<'static, ()>>,
 ) {
     callback(input).await
 }
 
 pub async fn rust_call_dart_with_dart_opaque_result(
-    callback: DartFn<fn() -> BoxFuture<DartOpaque>>,
+    callback: DartFn<fn() -> BoxFuture<'static, DartOpaque>>,
 ) -> DartOpaque {
     callback().await
 }
