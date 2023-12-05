b484449ecac5b4d62e726042cece80d265cb75db
fzyzcjy <ch271828n@outlook.com>
2023-12-05 14:00:27 +0800

    feat: generate_impl_wire2api_body (though seems not used)

--- frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/rust2dart/ty/dart_fn.rs
+++ frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/rust2dart/ty/dart_fn.rs
@@ -3,6 +3,6 @@ use crate::codegen::generator::wire::dart::spec_generator::rust2dart::ty::WireDa
 
 impl<'a> WireDartGeneratorRust2DartTrait for DartFnWireDartGenerator<'a> {
     fn generate_impl_wire2api_body(&self) -> String {
-        "todo_impl_wire2api_body;".into()
+        "return _wire2api_DartOpaque(raw);".into()
     }
 }
