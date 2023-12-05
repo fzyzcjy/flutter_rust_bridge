6b0f4fe99f57038b911e93675fadb504714716c9
fzyzcjy <ch271828n@outlook.com>
2023-12-05 20:23:28 +0800

    refactor: more rm list/struct wrapper logic

--- frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/dart2rust/ty/dart_opaque.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/dart2rust/ty/dart_opaque.rs
@@ -1,5 +1,5 @@
 use crate::codegen::generator::acc::Acc;
-use crate::codegen::generator::misc::target::Target;
+use crate::codegen::generator::misc::target::{Target, TargetOrCommon};
 use crate::codegen::generator::wire::rust::spec_generator::base::*;
 use crate::codegen::generator::wire::rust::spec_generator::dart2rust::impl_new_with_nullptr::generate_impl_new_with_nullptr_code_block;
 use crate::codegen::generator::wire::rust::spec_generator::dart2rust::misc::{
@@ -12,19 +12,12 @@ use crate::codegen::ir::ty::IrTypeTrait;
 
 impl<'a> WireRustGeneratorDart2RustTrait for DartOpaqueWireRustGenerator<'a> {
     fn generate_impl_wire2api_body(&self) -> Acc<Option<String>> {
-        Acc {
-            // TODO extract to function
-            io: Some(
-                "unsafe{ flutter_rust_bridge::DartOpaque::new(self.handle as _, self.port) }"
-                    .to_owned(),
-            ),
-            wasm: Some(
-                "let arr = self.dyn_into::<flutter_rust_bridge::for_generated::js_sys::Array>().unwrap();
-                unsafe{ flutter_rust_bridge::DartOpaque::new(arr.get(0), arr.get(1)) }"
-                    .to_owned(),
-            ),
-            ..Default::default()
-        }
+        Acc::new(|target| match target {
+            TargetOrCommon::Io | TargetOrCommon::Wasm => {
+                Some("unsafe { flutter_rust_bridge::DartOpaque::new(self as _) }".to_owned())
+            }
+            TargetOrCommon::Common => None,
+        })
     }
 
     fn rust_wire_type(&self, target: Target) -> String {
