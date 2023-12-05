b30f1121f23fbceec5bb29e49513116dd9d4dba5
fzyzcjy <ch271828n@outlook.com>
2023-12-05 13:05:20 +0800

    feat: parse more

--- frb_codegen/src/library/codegen/parser/type_parser/dart_fn.rs
+++ frb_codegen/src/library/codegen/parser/type_parser/dart_fn.rs
@@ -1,8 +1,10 @@
 use crate::codegen::ir::ty::primitive::IrTypePrimitive;
+use crate::codegen::ir::ty::unencodable::IrTypeUnencodable;
 use crate::codegen::ir::ty::IrType;
 use crate::codegen::ir::ty::IrType::Primitive;
 use crate::codegen::parser::type_parser::unencodable::{ArgsRefs, SplayedSegment};
 use crate::codegen::parser::type_parser::TypeParserWithContext;
+use ArgsRefs::Generic;
 
 impl<'a, 'b, 'c> TypeParserWithContext<'a, 'b, 'c> {
     pub(crate) fn parse_type_path_data_dart_fn(
@@ -10,14 +12,15 @@ impl<'a, 'b, 'c> TypeParserWithContext<'a, 'b, 'c> {
         last_segment: &SplayedSegment,
     ) -> anyhow::Result<Option<IrType>> {
         Ok(Some(match last_segment {
-            ("DartFn", inner) => self.parse_dart_fn(inner),
+            ("DartFn", Some(Generic(IrType::Unencodable(IrTypeUnencodable { string, .. })))) => {
+                self.parse_dart_fn(string)
+            }
 
             _ => return Ok(None),
         }))
     }
 
-    fn parse_dart_fn(&mut self, inner: &Option<ArgsRefs>) -> IrType {
-        log::info!("hi {inner:?}");
-        todo!()
+    fn parse_dart_fn(&mut self, raw: &str) -> IrType {
+        todo!("{}", raw)
     }
 }
