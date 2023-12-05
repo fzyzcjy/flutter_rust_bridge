2a806d75d6a9b39aae590dae6f90336b530a0a23
fzyzcjy <ch271828n@outlook.com>
2023-12-05 14:09:01 +0800

    fix: not generate unit

--- frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/dart2rust/ty/primitive.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/dart2rust/ty/primitive.rs
@@ -4,15 +4,20 @@ use crate::codegen::generator::wire::rust::spec_generator::base::*;
 use crate::codegen::generator::wire::rust::spec_generator::dart2rust::ty::WireRustGeneratorDart2RustTrait;
 use crate::codegen::ir::ty::primitive::IrTypePrimitive;
 use crate::codegen::ir::ty::IrTypeTrait;
+use IrTypePrimitive::Unit;
 
 impl<'a> WireRustGeneratorDart2RustTrait for PrimitiveWireRustGenerator<'a> {
     fn generate_impl_wire2api_body(&self) -> Acc<Option<String>> {
-        "self".into()
+        match self.ir {
+            Unit => Acc::new(|_| None),
+            _ => "self".into(),
+        }
     }
 
     fn generate_impl_wire2api_jsvalue_body(&self) -> Option<std::borrow::Cow<str>> {
         use IrTypePrimitive::*;
         Some(match &self.ir {
+            Unit => return None,
             Bool => "self.is_truthy()".into(),
             I64 | U64 => "::std::convert::TryInto::try_into(self.dyn_into::<flutter_rust_bridge::for_generated::js_sys::BigInt>().unwrap()).unwrap()".into(),
             _ => "self.unchecked_into_f64() as _".into(),
