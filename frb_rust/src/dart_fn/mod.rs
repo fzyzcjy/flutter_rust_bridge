a4cee49925e183d9534faaeab42ca0d134f42e28
fzyzcjy <ch271828n@outlook.com>
2023-12-05 16:25:13 +0800

    fix: compile

--- frb_rust/src/dart_fn/mod.rs
+++ frb_rust/src/dart_fn/mod.rs
@@ -1,6 +1,9 @@
 use crate::platform_types::DartAbi;
 use futures::future::BoxFuture;
+use std::future::Future;
 use std::ops::Deref;
+use std::panic::UnwindSafe;
+use std::pin::Pin;
 
 /// Roughly speaking, just BoxFuture + UnwindSafe.
 pub type DartFnFuture<T> = Pin<Box<dyn Future<Output = T> + Send + UnwindSafe + 'static>>;
