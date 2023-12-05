de4c6d62d4643b0aefd957a5521ccef28131d778
fzyzcjy <ch271828n@outlook.com>
2023-12-05 18:30:24 +0800

    fix: target

--- frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/misc/mod.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/misc/mod.rs
@@ -134,31 +134,29 @@ fn generate_static_checks(types: &[IrType], context: WireRustGeneratorContext) -
 }
 
 fn generate_boilerplate() -> Acc<Vec<WireRustOutputCode>> {
-    let inner: Vec<WireRustOutputCode> = vec![ExternFunc {
-        func_name: "frb_initialize_rust".into(),
-        params: vec![
-            ExternFuncParam {
-                name: "dart_opaque_drop_port".to_owned(),
-                rust_type: "flutter_rust_bridge::for_generated::MessagePort".to_owned(),
-                dart_type: "TODO_darttype".to_owned(),
-            },
-            ExternFuncParam {
-                name: "dart_fn_invoke_port".to_owned(),
-                rust_type: "flutter_rust_bridge::for_generated::MessagePort".to_owned(),
-                dart_type: "TODO_darttype".to_owned(),
-            },
-        ],
-        return_type: None,
-        body: format!("{HANDLER_NAME}.initialize(dart_opaque_drop_port, dart_fn_invoke_port)"),
-        target: Target::Io,
-    }
-    .into()];
-
-    Acc {
-        io: inner.clone(),
-        wasm: inner,
-        ..Default::default()
-    }
+    Acc::new(|target| match target {
+        TargetOrCommon::Io | TargetOrCommon::Wasm => vec![ExternFunc {
+            func_name: "frb_initialize_rust".into(),
+            params: vec![
+                ExternFuncParam {
+                    name: "dart_opaque_drop_port".to_owned(),
+                    rust_type: "flutter_rust_bridge::for_generated::MessagePort".to_owned(),
+                    dart_type: "TODO_darttype".to_owned(),
+                },
+                ExternFuncParam {
+                    name: "dart_fn_invoke_port".to_owned(),
+                    rust_type: "flutter_rust_bridge::for_generated::MessagePort".to_owned(),
+                    dart_type: "TODO_darttype".to_owned(),
+                },
+            ],
+            return_type: None,
+            body: format!("{HANDLER_NAME}.initialize(dart_opaque_drop_port, dart_fn_invoke_port)"),
+            target: target.try_into().unwrap(),
+        }
+        .into()]
+        .into(),
+        TargetOrCommon::Common => "".into(),
+    })
 }
 
 fn generate_executor(ir_pack: &IrPack) -> String {
