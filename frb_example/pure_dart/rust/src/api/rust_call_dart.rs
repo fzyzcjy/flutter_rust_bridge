1dcd353bf33f133345c03025d7075a3c9afa2047
fzyzcjy <ch271828n@outlook.com>
2023-12-05 11:25:58 +0800

    chore: callable

---
+++ frb_example/pure_dart/rust/src/api/rust_call_dart.rs
@@ -0,0 +1,10 @@
+// FRB_INTERNAL_GENERATOR: {"forbiddenDuplicatorModes": ["sync", "rustAsync"]}
+
+// TODO
+struct DartCallable<F> {
+    closure: F,
+}
+
+pub async fn rust_call_dart_simple(callback: DartCallable<fn()>) {
+    todo!()
+}
