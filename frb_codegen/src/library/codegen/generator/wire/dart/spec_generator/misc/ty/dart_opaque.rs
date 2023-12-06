57c0bae10a675678d9d1c37a0f57105bd7d9d9f6
fzyzcjy <ch271828n@outlook.com>
2023-12-06 17:37:17 +0800

    Revert "feat: gen extra func"
    This reverts commit 1a3d9855cfbb4718ef30da999b8909bc233a847f.

--- frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/misc/ty/dart_opaque.rs
+++ frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/misc/ty/dart_opaque.rs
@@ -1,13 +1,4 @@
-use crate::codegen::generator::acc::Acc;
 use crate::codegen::generator::wire::dart::spec_generator::base::*;
 use crate::codegen::generator::wire::dart::spec_generator::misc::ty::WireDartGeneratorMiscTrait;
-use crate::codegen::generator::wire::dart::spec_generator::output_code::WireDartOutputCode;
 
-impl<'a> WireDartGeneratorMiscTrait for DartOpaqueWireDartGenerator<'a> {
-    fn generate_extra_functions(&self) -> Option<Acc<WireDartOutputCode>> {
-        Some(Acc {
-            io: "".into(),
-            ..Default::default()
-        })
-    }
-}
+impl<'a> WireDartGeneratorMiscTrait for DartOpaqueWireDartGenerator<'a> {}
