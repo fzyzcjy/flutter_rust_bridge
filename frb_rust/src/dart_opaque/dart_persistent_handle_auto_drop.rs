676cefc252470637405936b36e8a497fc0f484d0
fzyzcjy <ch271828n@outlook.com>
2023-12-06 09:08:45 +0800

    feat: create_dart_handle seems safe, since we assume persistenthandle ptr is safe after it is constructed

--- frb_rust/src/dart_opaque/dart_persistent_handle_auto_drop.rs
+++ frb_rust/src/dart_opaque/dart_persistent_handle_auto_drop.rs
@@ -17,10 +17,12 @@ impl DartPersistentHandleAutoDrop {
 
     /// https://github.com/dart-lang/sdk/blob/af20a8ab0394408ee48483c5c06c75281e7ba52c/runtime/include/dart_api.h#L424C8-L424C8
     /// "Allocates a handle in the current scope from a persistent handle."
-    pub unsafe fn create_dart_handle(&self) -> Dart_Handle {
-        Dart_HandleFromPersistent_DL.expect("dart_api_dl has not been initialized")(
-            self.0.unwrap(),
-        );
+    pub fn create_dart_handle(&self) -> Dart_Handle {
+        unsafe {
+            Dart_HandleFromPersistent_DL.expect("dart_api_dl has not been initialized")(
+                self.0.unwrap(),
+            );
+        }
     }
 }
 
