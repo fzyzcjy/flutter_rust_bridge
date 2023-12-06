336354b32e629e42c6ef3eeb37e131f7afcff913
fzyzcjy <ch271828n@outlook.com>
2023-12-06 17:00:09 +0800

    Revert "refactor: on io, also send raw"
    This reverts commit 79a877e58cf8dc4b9bb3675053fc14617fe8c85a.

--- frb_dart/lib/src/dart_opaque/_io.dart
+++ frb_dart/lib/src/dart_opaque/_io.dart
@@ -1,4 +1,8 @@
+import 'package:flutter_rust_bridge/src/generalized_frb_rust_binding/generalized_frb_rust_binding.dart';
+import 'package:flutter_rust_bridge/src/platform_types/platform_types.dart';
+
 /// {@macro flutter_rust_bridge.only_for_generated_code}
-Object api2wireDartOpaque(Object raw) {
-  return raw;
+PlatformPointer api2wireDartOpaque(
+    Object raw, GeneralizedFrbRustBinding generalizedFrbRustBinding) {
+  return generalizedFrbRustBinding.dartNewPersistentHandle(raw);
 }
