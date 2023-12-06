79a877e58cf8dc4b9bb3675053fc14617fe8c85a
fzyzcjy <ch271828n@outlook.com>
2023-12-06 16:51:46 +0800

    refactor: on io, also send raw

--- frb_dart/lib/src/dart_opaque/_io.dart
+++ frb_dart/lib/src/dart_opaque/_io.dart
@@ -1,8 +1,4 @@
-import 'package:flutter_rust_bridge/src/generalized_frb_rust_binding/generalized_frb_rust_binding.dart';
-import 'package:flutter_rust_bridge/src/platform_types/platform_types.dart';
-
 /// {@macro flutter_rust_bridge.only_for_generated_code}
-PlatformPointer api2wireDartOpaque(
-    Object raw, GeneralizedFrbRustBinding generalizedFrbRustBinding) {
-  return generalizedFrbRustBinding.dartNewPersistentHandle(raw);
+Object api2wireDartOpaque(Object raw) {
+  return raw;
 }
