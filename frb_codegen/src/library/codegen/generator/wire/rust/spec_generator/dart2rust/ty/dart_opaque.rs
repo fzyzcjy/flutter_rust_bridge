6be894ab9657b2bc08c20bc51c3a56aacea8dda5
fzyzcjy <ch271828n@outlook.com>
2023-12-06 17:40:09 +0800

    feat: gen func

--- frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/dart2rust/ty/dart_opaque.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/dart2rust/ty/dart_opaque.rs
@@ -7,9 +7,12 @@ use crate::codegen::generator::wire::rust::spec_generator::dart2rust::misc::{
 };
 use crate::codegen::generator::wire::rust::spec_generator::dart2rust::ty::rust_opaque::dart_opaque_or_generalized_rust_opaque_rust_wire_type;
 use crate::codegen::generator::wire::rust::spec_generator::dart2rust::ty::WireRustGeneratorDart2RustTrait;
-use crate::codegen::generator::wire::rust::spec_generator::extern_func::ExternFunc;
+use crate::codegen::generator::wire::rust::spec_generator::extern_func::{
+    ExternFunc, ExternFuncParam,
+};
 use crate::codegen::generator::wire::rust::spec_generator::output_code::WireRustOutputCode;
 use crate::codegen::ir::ty::IrTypeTrait;
+use crate::misc::consts::HANDLER_NAME;
 
 impl<'a> WireRustGeneratorDart2RustTrait for DartOpaqueWireRustGenerator<'a> {
     fn generate_impl_wire2api_body(&self) -> Acc<Option<String>> {
@@ -19,6 +22,23 @@ impl<'a> WireRustGeneratorDart2RustTrait for DartOpaqueWireRustGenerator<'a> {
             }
             TargetOrCommon::Common => None,
         })
+    }
+
+    fn generate_related_funcs(&self) -> Acc<WireRustOutputCode> {
+        Acc {
+            io: vec![ExternFunc {
+                func_name: "dart_opaque_dart2rust_api2wire".into(),
+                params: vec![ExternFuncParam {
+                    name: "handle".to_owned(),
+                    rust_type: "Dart_Handle".to_owned(),
+                    dart_type: "NOT_USED".into(),
+                }.clone()],
+                return_type: None,
+                body: "unsafe { flutter_rust_bridge::for_generated::dart_opaque_dart2rust_api2wire(&*{HANDLER_NAME}, handle); }".into(),
+                target: Target::Io,
+            }].into(),
+            ..Default::default()
+        }
     }
 
     fn rust_wire_type(&self, target: Target) -> String {
