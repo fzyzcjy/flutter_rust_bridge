522515f8472e8dc0f9817de6b69fed34a84655be
fzyzcjy <ch271828n@outlook.com>
2023-12-06 09:47:47 +0800

    Revert "chore: more"
    This reverts commit 89149ac920b96c8a065706f07870fe5aa15bba68.

--- frb_example/pure_dart/test/with_vm_service_test.dart
+++ frb_example/pure_dart/test/with_vm_service_test.dart
@@ -2,14 +2,14 @@ import 'package:flutter_rust_bridge/flutter_rust_bridge_for_generated_io.dart';
 import 'package:frb_example_pure_dart/src/rust/frb_generated.dart';
 import 'package:test/test.dart';
 
+import 'test_utils.dart';
+
 Future<void> main() async {
   await RustLib.init();
 
   test('drop', () async {
     print('hi dorptest 1');
-    Uint8List? strongRef = Uint8List(1000000 * 300);
-    // Uint8List? strongRef = Uint8List(1000000 * 1);
-    // Uint8List? strongRef = Uint8List(10000);
+    Uint8List? strongRef = createLargeList(mb: 300);
     print('hi dorptest 2');
     final weakRef = WeakReference(strongRef);
     print('hi dorptest 3');
