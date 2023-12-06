1c46ee5aa1d339e64267c23ae0f31af0e794c017
fzyzcjy <ch271828n@outlook.com>
2023-12-06 09:47:47 +0800

    Revert "chore: temp expr"
    This reverts commit c3790a59f9d381cb38eaebed223e6faba728551b.

--- frb_example/pure_dart/test/with_vm_service_test.dart
+++ frb_example/pure_dart/test/with_vm_service_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_rust_bridge/flutter_rust_bridge_for_generated_io.dart';
+import 'package:frb_example_pure_dart/src/rust/api/dart_opaque.dart';
 import 'package:frb_example_pure_dart/src/rust/api/dart_opaque_sync.dart';
 import 'package:frb_example_pure_dart/src/rust/api/pseudo_manual/primitive_list_misc_twin_sync.dart';
 import 'package:frb_example_pure_dart/src/rust/frb_generated.dart';
@@ -52,40 +53,19 @@ Future<void> main() async {
   group('dart opaque type', () {
     group('GC', () {
       test('drop', () async {
-        print('hi dorptest 1');
         Uint8List? strongRef = createLargeList(mb: 300);
-        print('hi dorptest 2');
         final weakRef = WeakReference(strongRef);
-        print('hi dorptest 3');
-        // TODO
-        // TODO
-        // TODO
-        // await setStaticDartOpaqueTwinNormal(opaque: strongRef);
-        (RustLib.instance.api as RustLibApiImpl)
-            .generalizedFrbRustBinding
-            .dartNewPersistentHandle(strongRef);
-        print('hi dorptest 4');
+        await setStaticDartOpaqueTwinNormal(opaque: strongRef);
         strongRef = null;
 
-        print('hi dorptest 5');
         await vmService.gc();
-        print('hi dorptest 6');
         await Future<void>.delayed(const Duration(milliseconds: 10));
-        print('hi dorptest 7');
         expect(weakRef.target, isNotNull);
 
-        print('hi dorptest 8');
-        // TODO
-        // TODO
-        // TODO
-        // await dropStaticDartOpaqueTwinNormal();
-        print('hi dorptest 9');
+        await dropStaticDartOpaqueTwinNormal();
         await vmService.gc();
-        print('hi dorptest 10');
         await Future<void>.delayed(const Duration(milliseconds: 10));
-        print('hi dorptest 11');
         expect(weakRef.target, isNull);
-        print('hi dorptest 12');
       });
 
       test('unwrap', () async {
