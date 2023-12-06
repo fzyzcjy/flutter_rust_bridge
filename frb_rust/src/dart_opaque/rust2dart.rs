62a03c60761b36f91e15f3be5c8c4c6cfbdd1dd0
fzyzcjy <ch271828n@outlook.com>
2023-12-06 19:53:25 +0800

    feat: wasm bindgen

--- frb_rust/src/dart_opaque/rust2dart.rs
+++ frb_rust/src/dart_opaque/rust2dart.rs
@@ -1,9 +1,10 @@
-use super::DartOpaque;
+use super::{DartOpaque, GeneralizedDartHandle};
 use crate::for_generated::{box_from_leak_ptr, new_leak_box_ptr};
 use crate::generalized_isolate::{Channel, IntoDart};
 use crate::platform_types::{handle_to_message_port, DartAbi, SendableMessagePortHandle};
 use log::warn;
 use std::thread::ThreadId;
+use wasm_bindgen::prelude::*;
 
 impl From<DartOpaque> for DartAbi {
     fn from(data: DartOpaque) -> Self {
@@ -12,9 +13,9 @@ impl From<DartOpaque> for DartAbi {
 }
 
 // TODO old name: `get_dart_object`, rename all users
-#[cfg(not(wasm))]
+#[wasm_bindgen]
 #[no_mangle]
-pub unsafe extern "C" fn dart_opaque_rust2dart_wire2api(ptr: usize) -> dart_sys::Dart_Handle {
+pub unsafe extern "C" fn dart_opaque_rust2dart_wire2api(ptr: usize) -> GeneralizedDartHandle {
     let value: DartOpaque = *box_from_leak_ptr(ptr as _);
     value.create_dart_handle()
 }
