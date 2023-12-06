ae4da4416f15e75288eddd8cd35cd7ef0c963cdc
fzyzcjy <ch271828n@outlook.com>
2023-12-06 16:52:42 +0800

    refactor: mv

--- frb_dart/lib/flutter_rust_bridge_for_generated_io.dart
+++ frb_dart/lib/flutter_rust_bridge_for_generated_io.dart
@@ -4,7 +4,6 @@ library;
 export 'flutter_rust_bridge_for_generated_common.dart';
 export 'src/dart_c_object_into_dart/_io.dart'
     show debugOnExternalTypedDataFinalizer;
-export 'src/dart_opaque/_io.dart';
 export 'src/droppable/_io.dart' show CrossPlatformFinalizerArg;
 export 'src/manual_impl/_io.dart';
 export 'src/platform_types/_io.dart';
--- frb_dart/lib/flutter_rust_bridge_for_generated_web.dart
+++ frb_dart/lib/flutter_rust_bridge_for_generated_web.dart
@@ -4,7 +4,6 @@ library;
 export 'package:js/js.dart';
 
 export 'flutter_rust_bridge_for_generated_common.dart';
-export 'src/dart_opaque/_web.dart';
 export 'src/droppable/_web.dart' show CrossPlatformFinalizerArg;
 export 'src/manual_impl/_web.dart';
 export 'src/platform_types/_web.dart';
