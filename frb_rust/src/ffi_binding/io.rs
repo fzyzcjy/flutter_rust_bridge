2550d055b2825cc9cb26bd0116ab83fd3a339d5d
fzyzcjy <ch271828n@outlook.com>
2023-12-07 14:12:36 +0800

    chore: temp naive func

--- frb_rust/src/ffi_binding/io.rs
+++ frb_rust/src/ffi_binding/io.rs
@@ -24,3 +24,17 @@ pub unsafe extern "C" fn init_frb_dart_api_dl(data: *mut c_void) -> isize {
 pub unsafe extern "C" fn free_wire_sync_return(ptr: WireSyncReturn) {
     let _ = box_from_leak_ptr(ptr);
 }
+
+#[no_mangle]
+pub unsafe extern "C" fn naive_NewPersistentHandle(non_persistent_handle: Dart_Handle) -> usize {
+    let persistent_handle = Dart_NewPersistentHandle_DL.unwrap()(non_persistent_handle);
+    println!("hi naive_NewPersistentHandle non_persistent_handle={non_persistent_handle:?} persistent_handle={persistent_handle:?}");
+    persistent_handle as _
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn naive_HandleFromPersistent(persistent_handle: usize) -> usize {
+    let ans = Dart_HandleFromPersistent_DL.unwrap()(persistent_handle as _);
+    println!("hi naive_HandleFromPersistent persistent_handle={persistent_handle:?} ans={ans:?}");
+    ans as _
+}
