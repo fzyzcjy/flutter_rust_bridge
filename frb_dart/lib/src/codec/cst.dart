00e4c092418512d696f6c35e70e074fddbec2f6f
fzyzcjy <ch271828n@outlook.com>
2023-12-07 21:38:48 +0800

    chore: dummy impl

--- frb_dart/lib/src/codec/cst.dart
+++ frb_dart/lib/src/codec/cst.dart
@@ -1,4 +1,7 @@
 import 'package:flutter_rust_bridge/src/codec/base.dart';
 
 /// {@macro flutter_rust_bridge.only_for_generated_code}
-class CstCodec extends BaseCodec {}
+class CstCodec<S, E extends Object> extends BaseCodec<S, E> {
+  @override
+  S decode(dynamic raw) => throw UnimplementedError('unreachable');
+}
--- frb_dart/lib/src/codec/sse.dart
+++ frb_dart/lib/src/codec/sse.dart
@@ -1,4 +1,7 @@
 import 'package:flutter_rust_bridge/src/codec/base.dart';
 
 /// {@macro flutter_rust_bridge.only_for_generated_code}
-class SseCodec extends BaseCodec {}
+class SseCodec<S, E extends Object> extends BaseCodec<S, E> {
+  @override
+  S decode(dynamic raw) => throw UnimplementedError('TODO');
+}
