b9f3dd6489d1da46c6ed33a48fbb22751b6af1ad
fzyzcjy <ch271828n@outlook.com>
2023-12-07 16:17:22 +0800

    refactor: rm empty

--- frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/transfer/cst/encoder/ty/optional_list.rs
+++
@@ -1,9 +0,0 @@
-use crate::codegen::generator::acc::Acc;
-use crate::codegen::generator::misc::target::Target;
-use crate::codegen::generator::wire::dart::spec_generator::base::*;
-use crate::codegen::generator::wire::dart::spec_generator::transfer::cst::encoder::ty::general_list::general_or_optional_list_dart_wire_type;
-use crate::codegen::generator::wire::dart::spec_generator::transfer::cst::base::*;
-use crate::codegen::generator::wire::dart::spec_generator::transfer::cst::encoder::ty::WireDartTransferCstGeneratorEncoderTrait;
-use crate::codegen::ir::ty::IrTypeTrait;
-
-impl<'a> WireDartTransferCstGeneratorEncoderTrait for OptionalListWireDartTransferCstGenerator<'a> {}
