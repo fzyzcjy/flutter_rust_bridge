643c71d929c4d6fbdbebab126d21e6bb290f93a5
fzyzcjy <ch271828n@outlook.com>
2023-12-07 14:14:40 +0800

    chore: try to repro

--- frb_example/pure_dart/test/api/dart_opaque_sync_test.dart
+++ frb_example/pure_dart/test/api/dart_opaque_sync_test.dart
@@ -1,11 +1,18 @@
 // FRB_INTERNAL_GENERATOR: {"forbiddenDuplicatorModes": ["sync", "rustAsync"]}
 
-import 'package:frb_example_pure_dart/src/rust/api/dart_opaque_sync.dart';
 import 'package:frb_example_pure_dart/src/rust/frb_generated.dart';
 
 Future<void> main() async {
   await RustLib.init();
+
+  final binding = (RustLib.instance.api as RustLibApiImpl)
+      .generalizedFrbRustBinding
+      .binding;
   String f() => 'Test_String';
-  var syncBack = syncLoopbackTwinNormal(opaque: f);
-  print('$f $syncBack');
+  final persistentHandle = binding.naive_NewPersistentHandle(f);
+  binding.naive_HandleFromPersistent(persistentHandle);
+
+  // String f() => 'Test_String';
+  // var syncBack = syncLoopbackTwinNormal(opaque: f);
+  // print('$f $syncBack');
 }
--- frb_rust/src/ffi_binding/io.rs
+++ frb_rust/src/ffi_binding/io.rs
@@ -27,14 +27,18 @@ pub unsafe extern "C" fn free_wire_sync_return(ptr: WireSyncReturn) {
 
 #[no_mangle]
 pub unsafe extern "C" fn naive_NewPersistentHandle(non_persistent_handle: Dart_Handle) -> usize {
+    println!("hi naive_NewPersistentHandle START non_persistent_handle={non_persistent_handle:?}");
     let persistent_handle = Dart_NewPersistentHandle_DL.unwrap()(non_persistent_handle);
-    println!("hi naive_NewPersistentHandle non_persistent_handle={non_persistent_handle:?} persistent_handle={persistent_handle:?}");
+    println!("hi naive_NewPersistentHandle END non_persistent_handle={non_persistent_handle:?} persistent_handle={persistent_handle:?}");
     persistent_handle as _
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn naive_HandleFromPersistent(persistent_handle: usize) -> usize {
+    println!("hi naive_HandleFromPersistent START persistent_handle={persistent_handle:?}");
     let ans = Dart_HandleFromPersistent_DL.unwrap()(persistent_handle as _);
-    println!("hi naive_HandleFromPersistent persistent_handle={persistent_handle:?} ans={ans:?}");
+    println!(
+        "hi naive_HandleFromPersistent END persistent_handle={persistent_handle:?} ans={ans:?}"
+    );
     ans as _
 }
