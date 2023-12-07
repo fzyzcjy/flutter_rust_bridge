98b25b69b768e20df897ccd3faf1fcd9cd7f23f1
fzyzcjy <ch271828n@outlook.com>
2023-12-07 10:35:14 +0800

    refactor: mv mod

--- frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/transfer/dco/encoder/mod.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/transfer/dco/encoder/mod.rs
@@ -1,2 +1,30 @@
+use crate::codegen::generator::acc::Acc;
+use crate::codegen::generator::wire::rust::spec_generator::base::{
+    WireRustGenerator, WireRustGeneratorContext,
+};
+use crate::codegen::generator::wire::rust::spec_generator::output_code::WireRustOutputCode;
+use crate::codegen::generator::wire::rust::IrPackComputedCache;
+use crate::library::codegen::generator::wire::rust::spec_generator::rust2dart::ty::WireRustGeneratorRust2DartTrait;
+use serde::Serialize;
+
 mod misc;
-mod ty;
+pub(crate) mod ty;
+
+#[derive(Serialize)]
+pub(crate) struct WireRustOutputSpecRust2Dart {
+    pub impl_into_dart: Acc<Vec<WireRustOutputCode>>,
+}
+
+pub(crate) fn generate(
+    context: WireRustGeneratorContext,
+    cache: &IrPackComputedCache,
+) -> WireRustOutputSpecRust2Dart {
+    WireRustOutputSpecRust2Dart {
+        impl_into_dart: cache
+            .distinct_types
+            .iter()
+            .filter_map(|ty| WireRustGenerator::new(ty.clone(), context).generate_impl_into_dart())
+            .map(|x| Acc::new_common(x.into()))
+            .collect(),
+    }
+}
