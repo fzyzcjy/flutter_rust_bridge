ce7078d993a157cc14564f6a24ea8b3e04a58b03
fzyzcjy <ch271828n@outlook.com>
2023-12-07 18:02:55 +0800

    chore: empty impl

--- frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/transfer/cst/entrypoint.rs
+++ frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/transfer/cst/entrypoint.rs
@@ -1,5 +1,10 @@
 use crate::codegen::generator::wire::dart::spec_generator::transfer::base::WireDartTransferEntrypointTrait;
+use crate::codegen::ir::func::IrFunc;
 
 pub(crate) struct CstWireDartTransferEntrypoint {}
 
-impl WireDartTransferEntrypointTrait for CstWireDartTransferEntrypoint {}
+impl WireDartTransferEntrypointTrait for CstWireDartTransferEntrypoint {
+    fn generate_func_stmt_prepare_args(&self, func: &IrFunc) -> Vec<String> {
+        todo!()
+    }
+}
--- frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/transfer/dco/entrypoint.rs
+++ frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/transfer/dco/entrypoint.rs
@@ -1,5 +1,10 @@
 use crate::codegen::generator::wire::dart::spec_generator::transfer::base::WireDartTransferEntrypointTrait;
+use crate::codegen::ir::func::IrFunc;
 
 pub(crate) struct DcoWireDartTransferEntrypoint {}
 
-impl WireDartTransferEntrypointTrait for DcoWireDartTransferEntrypoint {}
+impl WireDartTransferEntrypointTrait for DcoWireDartTransferEntrypoint {
+    fn generate_func_stmt_prepare_args(&self, func: &IrFunc) -> Vec<String> {
+        unreachable!()
+    }
+}
