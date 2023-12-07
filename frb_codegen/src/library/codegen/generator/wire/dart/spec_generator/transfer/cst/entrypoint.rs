0fe5ae6628f255845c646ab9ee597de2b81ac1fd
fzyzcjy <ch271828n@outlook.com>
2023-12-07 18:03:16 +0800

    chore: mv code

--- frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/transfer/cst/entrypoint.rs
+++ frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/transfer/cst/entrypoint.rs
@@ -1,10 +1,21 @@
 use crate::codegen::generator::wire::dart::spec_generator::transfer::base::WireDartTransferEntrypointTrait;
 use crate::codegen::ir::func::IrFunc;
+use itertools::Itertools;
 
 pub(crate) struct CstWireDartTransferEntrypoint {}
 
 impl WireDartTransferEntrypointTrait for CstWireDartTransferEntrypoint {
     fn generate_func_stmt_prepare_args(&self, func: &IrFunc) -> Vec<String> {
-        todo!()
+        func.inputs
+            .iter()
+            .enumerate()
+            .map(|(index, input)| {
+                format!(
+                    "var arg{index} = cst_encode_{ty_ident}({name});",
+                    ty_ident = input.ty.safe_ident(),
+                    name = &input.name.dart_style()
+                )
+            })
+            .collect_vec()
     }
 }
