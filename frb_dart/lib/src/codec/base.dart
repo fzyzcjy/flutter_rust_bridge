f0aee6415467b9ce497e3e224710af96e4e13bd7
fzyzcjy <ch271828n@outlook.com>
2023-12-07 21:30:20 +0800

    chore: empty

---
+++ frb_dart/lib/src/codec/base.dart
@@ -0,0 +1,2 @@
+/// {@macro flutter_rust_bridge.only_for_generated_code}
+abstract class BaseCodec {}
---
+++ frb_dart/lib/src/codec/sse.dart
@@ -0,0 +1,4 @@
+import 'package:flutter_rust_bridge/src/codec/base.dart';
+
+/// {@macro flutter_rust_bridge.only_for_generated_code}
+class SseCodec extends BaseCodec {}
