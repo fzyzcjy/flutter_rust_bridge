68af3bcb49a67fdcbc86c62c4d30020e36c5892e
fzyzcjy <ch271828n@outlook.com>
2023-12-07 17:55:00 +0800

    feat: rough WireDartTransferGeneratorTrait

--- frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/transfer/base.rs
+++ frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/transfer/base.rs
@@ -1,1 +1,10 @@
+use enum_dispatch::enum_dispatch;
 
+#[enum_dispatch(WireDartTransferGeneratorTrait)]
+pub(crate) enum WireDartTransferGenerator {
+    Cst(WireDartTransferCstGenerator),
+    // TODO more
+}
+
+#[enum_dispatch]
+pub(crate) trait WireDartTransferGeneratorTrait {}
