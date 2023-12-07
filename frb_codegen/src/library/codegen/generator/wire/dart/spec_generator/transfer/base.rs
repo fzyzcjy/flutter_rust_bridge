9954bda1227b54aeb625ff4b07c8b4247112f175
fzyzcjy <ch271828n@outlook.com>
2023-12-07 18:12:04 +0800

    chore: rm import

--- frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/transfer/base.rs
+++ frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/transfer/base.rs
@@ -2,11 +2,9 @@ use crate::codegen::generator::misc::transfer::TransferMode;
 use crate::codegen::generator::wire::dart::spec_generator::transfer::cst::entrypoint::CstWireDartTransferEntrypoint;
 use crate::codegen::generator::wire::dart::spec_generator::transfer::dco::entrypoint::DcoWireDartTransferEntrypoint;
 use crate::codegen::generator::wire::dart::spec_generator::transfer::sse::entrypoint::SseWireDartTransferEntrypoint;
-use crate::codegen::generator::wire::misc::has_port_argument;
 use crate::codegen::ir::func::IrFunc;
 use crate::codegen_transfer_structs;
 use enum_dispatch::enum_dispatch;
-use std::{format, vec};
 
 codegen_transfer_structs!(WireDartTransferEntrypoint);
 
