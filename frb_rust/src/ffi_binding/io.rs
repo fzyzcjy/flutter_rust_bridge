7fc41ae7ccbf6ae57c3839467b77a0be74127d1d
fzyzcjy <ch271828n@outlook.com>
2023-12-08 16:15:09 +0800

    chore: mv

--- frb_rust/src/ffi_binding/io.rs
+++ frb_rust/src/ffi_binding/io.rs
@@ -24,13 +24,3 @@ pub unsafe extern "C" fn init_frb_dart_api_dl(data: *mut c_void) -> isize {
 pub unsafe extern "C" fn free_wire_sync_return(ptr: WireSyncReturn) {
     let _ = box_from_leak_ptr(ptr);
 }
-
-#[no_mangle]
-pub unsafe extern "C" fn rust_vec_u8_new(len: i32) -> *mut u8 {
-    new_leak_vec_ptr::<u8>(0, len)
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn rust_vec_u8_free(ptr: *mut u8, len: i32) {
-    vec_from_leak_ptr(ptr, len)
-}
--- frb_rust/src/ffi_binding/mod.rs
+++ frb_rust/src/ffi_binding/mod.rs
@@ -7,3 +7,13 @@ pub use io::*;
 mod web;
 #[cfg(wasm)]
 pub use web::*;
+
+#[no_mangle]
+pub unsafe extern "C" fn rust_vec_u8_new(len: i32) -> *mut u8 {
+    new_leak_vec_ptr::<u8>(0, len)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn rust_vec_u8_free(ptr: *mut u8, len: i32) {
+    vec_from_leak_ptr(ptr, len)
+}
