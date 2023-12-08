7ead660b73b2e88c50f25f0e77773f676cc00c35
fzyzcjy <ch271828n@outlook.com>
2023-12-08 09:42:34 +0800

    chore: empty

--- frb_codegen/src/library/codegen/generator/codec/sse/ty/enumeration.rs
+++ frb_codegen/src/library/codegen/generator/codec/sse/ty/enumeration.rs
@@ -1,3 +1,11 @@
 use crate::codegen::generator::codec::sse::ty::*;
 
-impl<'a> CodecSseTyTrait for EnumRefCodecSseTy<'a> {}
+impl<'a> CodecSseTyTrait for EnumRefCodecSseTy<'a> {
+    fn generate_encode(&self, lang: &impl Lang) -> String {
+        todo!()
+    }
+
+    fn generate_decode(&self, lang: &impl Lang) -> String {
+        todo!()
+    }
+}
