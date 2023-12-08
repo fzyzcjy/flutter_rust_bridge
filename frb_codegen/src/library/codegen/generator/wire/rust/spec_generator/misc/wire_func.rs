6f08f61a9ee5d80d00a8f9400b54b4fcb9458e32
fzyzcjy <ch271828n@outlook.com>
2023-12-08 08:22:29 +0800

    fix: morecompile

--- frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/misc/wire_func.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/misc/wire_func.rs
@@ -33,10 +33,10 @@ pub(crate) fn generate_wire_func(
     let dart2rust_codec = WireRustCodecEntrypoint::new(func.codec_mode_pack.dart2rust);
 
     let ir_pack = context.ir_pack;
-    let params = dart2rust_codec.generate_func_params(func, context);
+    let params = dart2rust_codec.0.generate_func_params(func, context);
     let inner_func_args = generate_inner_func_args(func, ir_pack, context);
     let wrap_info_obj = generate_wrap_info_obj(func);
-    let code_decode = dart2rust_codec.generate_func_call_decode(func, context);
+    let code_decode = dart2rust_codec.0.generate_func_call_decode(func, context);
     let code_inner_decode = generate_code_inner_decode(func);
     let code_call_inner_func_result = generate_code_call_inner_func_result(func, inner_func_args);
     let handler_func_name = generate_handler_func_name(func, ir_pack, context);
