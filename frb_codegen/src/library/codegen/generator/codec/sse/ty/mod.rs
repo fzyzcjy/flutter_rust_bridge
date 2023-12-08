9db54be4f274bd5fafcd3b7204810c84900c5afa
fzyzcjy <ch271828n@outlook.com>
2023-12-08 09:14:15 +0800

    feat: more empty

---
+++ frb_codegen/src/library/codegen/generator/codec/sse/ty/mod.rs
@@ -0,0 +1,35 @@
+mod boxed;
+mod dart_fn;
+mod dart_opaque;
+mod delegate;
+mod dynamic;
+mod enumeration;
+mod general_list;
+mod optional;
+mod ownership;
+mod primitive;
+mod primitive_list;
+mod record;
+mod rust_auto_opaque;
+mod rust_opaque;
+mod structure;
+mod unencodable;
+
+use crate::codegen::generator::codec::sse::*;
+use crate::codegen::generator::wire::rust::spec_generator::base::WireRustGeneratorImplTrait;
+use crate::codegen_generator_structs;
+use crate::library::codegen::ir::ty::IrTypeTrait;
+use enum_dispatch::enum_dispatch;
+
+codegen_generator_structs!(
+    #[enum_dispatch(CodecSseTyTrait)]
+    CodecSseTy
+);
+
+#[derive(Debug, Clone, Copy)]
+pub(crate) struct CodecSseTyContext<'a> {
+    pub(crate) ir_pack: &'a IrPack,
+}
+
+#[enum_dispatch]
+pub(crate) trait CodecSseTyTrait {}
---
+++ frb_codegen/src/library/codegen/generator/codec/sse/ty/primitive.rs
@@ -0,0 +1,3 @@
+use crate::codegen::generator::codec::sse::ty::*;
+
+impl<'a> CodecSseTyTrait for PrimitiveCodecSseTy<'a> {}
