ea8e31872b842a52ea46261c1da64ac413fb8304
fzyzcjy <ch271828n@outlook.com>
2023-12-08 13:18:02 +0800

    feat: more

--- frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/codec/sse/entrypoint.rs
+++ frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/codec/sse/entrypoint.rs
@@ -33,18 +33,14 @@ impl BaseCodecEntrypointTrait<WireDartGeneratorContext<'_>, WireDartCodecOutputS
 
 impl WireDartCodecEntrypointTrait<'_> for SseWireDartCodecEntrypoint {
     fn generate_dart2rust_func_stmt_prepare_args(&self, func: &IrFunc) -> Vec<String> {
-        todo!()
+        vec!["TODO_generate_dart2rust_func_stmt_prepare_args;".into()]
     }
 
     fn generate_dart2rust_func_wire_param_list(
         &self,
-        func: &IrFunc,
-        num_prepare_args: usize,
+        _func: &IrFunc,
+        _num_prepare_args: usize,
     ) -> Vec<String> {
-        todo!()
-    }
-
-    fn generate_rust2dart_codec_object(&self, func: &IrFunc) -> String {
-        todo!()
+        vec!["ptr_".into(), "len_".into()]
     }
 }
