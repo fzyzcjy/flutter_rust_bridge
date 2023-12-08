4cad1e3ee4c28da2a0f1d95db65a345e6b935e57
fzyzcjy <ch271828n@outlook.com>
2023-12-08 13:21:40 +0800

    chore: cp

--- frb_dart/lib/src/codec/sse.dart
+++ frb_dart/lib/src/codec/sse.dart
@@ -2,6 +2,20 @@ import 'package:flutter_rust_bridge/src/codec/base.dart';
 
 /// {@macro flutter_rust_bridge.only_for_generated_code}
 class SseCodec<S, E extends Object> extends BaseCodec<S, E> {
+  /// {@macro flutter_rust_bridge.only_for_generated_code}
+  final S Function(dynamic) parseSuccessData;
+
+  /// {@macro flutter_rust_bridge.only_for_generated_code}
+  final E Function(dynamic)? parseErrorData;
+
+  /// {@macro flutter_rust_bridge.only_for_generated_code}
+  const SseCodec({
+    required this.parseSuccessData,
+    required this.parseErrorData,
+  });
+
   @override
-  S decode(dynamic raw) => throw UnimplementedError('TODO');
+  S decode(dynamic raw) {
+    return TODO;
+  }
 }
