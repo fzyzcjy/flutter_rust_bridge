0ac917b4df0373e43e7566a9c765497cf17660a8
fzyzcjy <ch271828n@outlook.com>
2023-12-08 11:26:11 +0800

    feat: as_api_dart_context

--- frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/codec/sse/base.rs
+++ frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/codec/sse/base.rs
@@ -1,3 +1,6 @@
+use crate::codegen::generator::api_dart::internal_config::GeneratorApiDartInternalConfig;
+use crate::codegen::generator::api_dart::spec_generator::base::ApiDartGeneratorContext;
+use crate::codegen::generator::wire::dart::internal_config::GeneratorWireDartInternalConfig;
 use crate::codegen_generator_structs;
 use enum_dispatch::enum_dispatch;
 
@@ -11,4 +14,15 @@ codegen_generator_structs!(
 #[derive(Debug, Clone, Copy)]
 pub(crate) struct WireDartCodecSseGeneratorContext<'a> {
     pub(crate) ir_pack: &'a IrPack,
+    pub(crate) config: &'a GeneratorWireDartInternalConfig,
+    pub(crate) api_dart_config: &'a GeneratorApiDartInternalConfig,
+}
+
+impl WireDartCodecSseGeneratorContext<'_> {
+    pub(crate) fn as_api_dart_context(&self) -> ApiDartGeneratorContext {
+        ApiDartGeneratorContext {
+            ir_pack: self.ir_pack,
+            config: self.api_dart_config,
+        }
+    }
 }
