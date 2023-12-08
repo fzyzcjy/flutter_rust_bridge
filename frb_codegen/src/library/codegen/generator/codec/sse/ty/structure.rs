9981116d4bff9f1714b4b4c49c4b6a3593b123c1
fzyzcjy <ch271828n@outlook.com>
2023-12-08 10:57:48 +0800

    feat: GeneralizedStructGenerator

--- frb_codegen/src/library/codegen/generator/codec/sse/ty/structure.rs
+++ frb_codegen/src/library/codegen/generator/codec/sse/ty/structure.rs
@@ -1,8 +1,40 @@
 use crate::codegen::generator::codec::sse::ty::*;
+use crate::codegen::generator::misc::StructOrRecord;
+use crate::codegen::generator::misc::StructOrRecord::Struct;
 use itertools::Itertools;
 
 impl<'a> CodecSseTyTrait for StructRefCodecSseTy<'a> {
     fn generate_encode(&self, lang: &Lang) -> String {
+        self.new_generalized_generator().generate_encode(lang)
+    }
+
+    fn generate_decode(&self, lang: &Lang) -> String {
+        self.new_generalized_generator().generate_decode(lang)
+    }
+}
+
+impl<'a> StructRefCodecSseTy<'a> {
+    fn new_generalized_generator(&self) -> GeneralizedStructGenerator {
+        GeneralizedStructGenerator::new(self.ir.clone(), self.context, Struct)
+    }
+}
+
+pub(crate) struct GeneralizedStructGenerator<'a> {
+    ir: IrTypeStructRef,
+    context: CodecSseTyContext<'a>,
+    mode: StructOrRecord,
+}
+
+impl<'a> GeneralizedStructGenerator<'a> {
+    pub(crate) fn new(
+        ir: IrTypeStructRef,
+        context: CodecSseTyContext<'a>,
+        mode: StructOrRecord,
+    ) -> Self {
+        Self { ir, context, mode }
+    }
+
+    pub(super) fn generate_encode(&self, lang: &Lang) -> String {
         let st = self.ir.get(self.context.ir_pack);
         st.fields
             .iter()
@@ -15,7 +47,7 @@ impl<'a> CodecSseTyTrait for StructRefCodecSseTy<'a> {
             .join("")
     }
 
-    fn generate_decode(&self, lang: &Lang) -> String {
+    pub(super) fn generate_decode(&self, lang: &Lang) -> String {
         let st = self.ir.get(self.context.ir_pack);
         let decode_fields = st
             .fields
