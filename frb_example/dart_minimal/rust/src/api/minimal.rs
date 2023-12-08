248e65825f6166311ac9a84a4031b0a15c3e84a1
fzyzcjy <ch271828n@outlook.com>
2023-12-08 13:08:10 +0800

    chore: temp add func

--- frb_example/dart_minimal/rust/src/api/minimal.rs
+++ frb_example/dart_minimal/rust/src/api/minimal.rs
@@ -1,3 +1,9 @@
 pub fn minimal_adder(a: i32, b: i32) -> i32 {
     a + b
 }
+
+// TODO only temp add this for debug, should remove later (o/w this example is no longer "minimal"
+#[flutter_rust_bridge::frb(serialize)]
+pub fn hello(a: i32, b: i32) -> i32 {
+    a + b
+}
