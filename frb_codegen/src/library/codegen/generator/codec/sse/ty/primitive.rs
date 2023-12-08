7f8056ea6bc94e0bed160dd84ffdc7d43659e092
fzyzcjy <ch271828n@outlook.com>
2023-12-08 09:49:14 +0800

    feat: primitive (wrong)

--- frb_codegen/src/library/codegen/generator/codec/sse/ty/primitive.rs
+++ frb_codegen/src/library/codegen/generator/codec/sse/ty/primitive.rs
@@ -1,3 +1,11 @@
 use crate::codegen::generator::codec::sse::ty::*;
 
-impl<'a> CodecSseTyTrait for PrimitiveCodecSseTy<'a> {}
+impl<'a> CodecSseTyTrait for PrimitiveCodecSseTy<'a> {
+    fn generate_encode(&self, lang: &impl Lang) -> String {
+        format!("{};", lang.call_encode(&Primitive(self.ir.clone()), "src"))
+    }
+
+    fn generate_decode(&self, lang: &impl Lang) -> String {
+        format!("return {};", lang.call_decode(&Primitive(self.ir.clone())))
+    }
+}
