bfd4009f41d342aaf031ebbad12253a96f03c4b8
fzyzcjy <ch271828n@outlook.com>
2023-12-08 11:35:04 +0800

    chore: rm

--- frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/codec/sse/mod.rs
+++ frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/codec/sse/mod.rs
@@ -1,5 +1,3 @@
 pub(crate) mod base;
 mod body;
-pub(crate) mod decoder;
-pub(crate) mod encoder;
 pub(crate) mod entrypoint;
--- frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/codec/sse/mod.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/codec/sse/mod.rs
@@ -1,4 +1,2 @@
 pub(crate) mod base;
-pub(crate) mod decoder;
-pub(crate) mod encoder;
 pub(crate) mod entrypoint;
