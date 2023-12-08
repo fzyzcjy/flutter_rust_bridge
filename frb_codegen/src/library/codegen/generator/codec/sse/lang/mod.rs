266b830f75ea26f3b987c0e5a47b9b915d5bc4cf
fzyzcjy <ch271828n@outlook.com>
2023-12-08 09:28:42 +0800

    feat: boxed

--- frb_codegen/src/library/codegen/generator/codec/sse/lang/mod.rs
+++ frb_codegen/src/library/codegen/generator/codec/sse/lang/mod.rs
@@ -1,4 +1,10 @@
+use crate::codegen::ir::ty::IrType;
+
 pub(crate) mod dart;
 pub(crate) mod rust;
 
-pub(crate) trait Lang {}
+pub(crate) trait Lang {
+    fn call_encode(&self, ty: &IrType) -> String;
+
+    fn call_decode(&self, ty: &IrType) -> String;
+}
--- frb_codegen/src/library/codegen/generator/codec/sse/ty/boxed.rs
+++ frb_codegen/src/library/codegen/generator/codec/sse/ty/boxed.rs
@@ -2,10 +2,10 @@ use crate::codegen::generator::codec::sse::ty::*;
 
 impl<'a> CodecSseTyTrait for BoxedCodecSseTy<'a> {
     fn generate_encode(&self, lang: &impl Lang) -> String {
-        todo!()
+        lang.call_encode(&*self.ir.inner)
     }
 
     fn generate_decode(&self, lang: &impl Lang) -> String {
-        todo!()
+        lang.call_decode(&*self.ir.inner)
     }
 }
