c20f7fdb046844476163d69591dcd742be75170f
fzyzcjy <ch271828n@outlook.com>
2023-12-08 09:37:19 +0800

    feat: struct enc

--- frb_codegen/src/library/codegen/generator/codec/sse/ty/structure.rs
+++ frb_codegen/src/library/codegen/generator/codec/sse/ty/structure.rs
@@ -1,8 +1,18 @@
 use crate::codegen::generator::codec::sse::ty::*;
+use itertools::Itertools;
 
 impl<'a> CodecSseTyTrait for StructRefCodecSseTy<'a> {
     fn generate_encode(&self, lang: &impl Lang) -> String {
-        todo!()
+        let st = self.ir.get(self.context.ir_pack);
+        st.fields
+            .iter()
+            .map(|field| {
+                format!(
+                    "{};\n",
+                    lang.call_encode(&field.ty, &format!("src.{}", field.name))
+                )
+            })
+            .join("")
     }
 
     fn generate_decode(&self, lang: &impl Lang) -> String {
