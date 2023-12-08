3aa16e8c82c17b370ed92a18af14c49de0e50a22
fzyzcjy <ch271828n@outlook.com>
2023-12-08 10:22:05 +0800

    feat: string

--- frb_codegen/src/library/codegen/generator/codec/sse/ty/delegate.rs
+++ frb_codegen/src/library/codegen/generator/codec/sse/ty/delegate.rs
@@ -5,8 +5,8 @@ impl<'a> CodecSseTyTrait for DelegateCodecSseTy<'a> {
     fn generate_encode(&self, lang: &Lang) -> String {
         let gen = SimpleDelegateCodecCodegen::new(lang, self.ir.get_delegate());
         match self.ir {
-            IrTypeDelegate::Array(inner) => {}
-            IrTypeDelegate::String => gen.encode(),
+            IrTypeDelegate::Array(inner) => TODO,
+            IrTypeDelegate::String => gen.encode("utf8.encoder.convert(src)"),
             IrTypeDelegate::StringList => {}
             IrTypeDelegate::ZeroCopyBufferVecPrimitive(inner) => {}
             IrTypeDelegate::PrimitiveEnum(inner) => {}
@@ -34,14 +34,14 @@ impl<'a> SimpleDelegateCodecCodegen<'a> {
         Self { lang, inner_ty }
     }
 
-    pub(super) fn encode(&self, inner_name: &str) -> String {
-        format!("{};", self.lang.call_encode(&self.inner_ty, inner_name))
+    pub(super) fn encode(&self, inner_expr: &str) -> String {
+        format!("{};", self.lang.call_encode(&self.inner_ty, inner_expr))
     }
 
-    pub(super) fn decode(&self, wrapper: &str) -> String {
+    pub(super) fn decode(&self, wrapper_expr: &str) -> String {
         format!(
             "{var_decl} inner = {};
-            return {wrapper};",
+            return {wrapper_expr};",
             self.lang.call_decode(&self.inner_ty),
             var_decl = self.lang.var_decl()
         )
