707813a2514cb5e3cd6c0b5c0c9398204f249d41
fzyzcjy <ch271828n@outlook.com>
2023-12-08 10:26:31 +0800

    feat: more

--- frb_codegen/src/library/codegen/generator/codec/sse/ty/delegate.rs
+++ frb_codegen/src/library/codegen/generator/codec/sse/ty/delegate.rs
@@ -3,10 +3,9 @@ use crate::codegen::generator::codec::sse::ty::*;
 
 impl<'a> CodecSseTyTrait for DelegateCodecSseTy<'a> {
     fn generate_encode(&self, lang: &Lang) -> String {
-        let gen = SimpleDelegateCodecCodegen::new(lang, self.ir.get_delegate());
-        match self.ir {
+        let inner_expr = match &self.ir {
             IrTypeDelegate::Array(inner) => TODO,
-            IrTypeDelegate::String => gen.encode("utf8.encoder.convert(src)"),
+            IrTypeDelegate::String => "utf8.encoder.convert(src)",
             IrTypeDelegate::StringList => {}
             IrTypeDelegate::ZeroCopyBufferVecPrimitive(inner) => {}
             IrTypeDelegate::PrimitiveEnum(inner) => {}
@@ -16,34 +15,37 @@ impl<'a> CodecSseTyTrait for DelegateCodecSseTy<'a> {
             IrTypeDelegate::Uuids => {}
             IrTypeDelegate::Backtrace => {}
             IrTypeDelegate::Anyhow => {}
-        }
+        };
+        simple_delegate_encode(lang, &self.ir.get_delegate(), inner_expr)
     }
 
     fn generate_decode(&self, lang: &Lang) -> String {
-        todo!()
+        let wrapper_expr = match &self.ir {
+            IrTypeDelegate::Array(inner) => TODO,
+            IrTypeDelegate::String => "TODO",
+            IrTypeDelegate::StringList => {}
+            IrTypeDelegate::ZeroCopyBufferVecPrimitive(inner) => {}
+            IrTypeDelegate::PrimitiveEnum(inner) => {}
+            IrTypeDelegate::Time(inner) => {}
+            IrTypeDelegate::TimeList(inner) => {}
+            IrTypeDelegate::Uuid => {}
+            IrTypeDelegate::Uuids => {}
+            IrTypeDelegate::Backtrace => {}
+            IrTypeDelegate::Anyhow => {}
+        };
+        simple_delegate_decode(lang, &self.ir.get_delegate(), wrapper_expr)
     }
 }
 
-pub(super) struct SimpleDelegateCodecCodegen<'a> {
-    lang: &'a Lang,
-    inner_ty: IrType,
+pub(super) fn simple_delegate_encode(lang: &Lang, inner_ty: &IrType, inner_expr: &str) -> String {
+    format!("{};", lang.call_encode(inner_ty, inner_expr))
 }
 
-impl<'a> SimpleDelegateCodecCodegen<'a> {
-    pub fn new(lang: &'a Lang, inner_ty: IrType) -> Self {
-        Self { lang, inner_ty }
-    }
-
-    pub(super) fn encode(&self, inner_expr: &str) -> String {
-        format!("{};", self.lang.call_encode(&self.inner_ty, inner_expr))
-    }
-
-    pub(super) fn decode(&self, wrapper_expr: &str) -> String {
-        format!(
-            "{var_decl} inner = {};
-            return {wrapper_expr};",
-            self.lang.call_decode(&self.inner_ty),
-            var_decl = self.lang.var_decl()
-        )
-    }
+pub(super) fn simple_delegate_decode(lang: &Lang, inner_ty: &IrType, wrapper_expr: &str) -> String {
+    format!(
+        "{var_decl} inner = {};
+        return {wrapper_expr};",
+        lang.call_decode(&inner_ty),
+        var_decl = lang.var_decl()
+    )
 }
