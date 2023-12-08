5115c4f009e89fbdb776302bdfbca67f664d3fe1
fzyzcjy <ch271828n@outlook.com>
2023-12-08 09:35:53 +0800

    chore: empty

--- frb_codegen/src/library/codegen/generator/codec/sse/ty/structure.rs
+++ frb_codegen/src/library/codegen/generator/codec/sse/ty/structure.rs
@@ -1,3 +1,11 @@
 use crate::codegen::generator::codec::sse::ty::*;
 
-impl<'a> CodecSseTyTrait for StructRefCodecSseTy<'a> {}
+impl<'a> CodecSseTyTrait for StructRefCodecSseTy<'a> {
+    fn generate_encode(&self, lang: &impl Lang) -> String {
+        todo!()
+    }
+
+    fn generate_decode(&self, lang: &impl Lang) -> String {
+        todo!()
+    }
+}
