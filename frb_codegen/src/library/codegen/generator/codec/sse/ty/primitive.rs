4baec097b47207702889d6fe9ebe47ecb1cd7895
fzyzcjy <ch271828n@outlook.com>
2023-12-08 09:53:06 +0800

    chore: revert since we should inline here directly

--- frb_codegen/src/library/codegen/generator/codec/sse/ty/primitive.rs
+++ frb_codegen/src/library/codegen/generator/codec/sse/ty/primitive.rs
@@ -2,10 +2,10 @@ use crate::codegen::generator::codec::sse::ty::*;
 
 impl<'a> CodecSseTyTrait for PrimitiveCodecSseTy<'a> {
     fn generate_encode(&self, _lang: &impl Lang) -> String {
-        format!("serializer.serialize_{};", self.ir.safe_ident());
+        todo!()
     }
 
     fn generate_decode(&self, _lang: &impl Lang) -> String {
-        format!("return deserializer.deserialize_{};", self.ir.safe_ident());
+        todo!()
     }
 }
