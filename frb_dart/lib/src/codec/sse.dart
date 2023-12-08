a360bd91b08e7a6e29da0f1f596ae56aec1a0f1b
fzyzcjy <ch271828n@outlook.com>
2023-12-08 13:22:23 +0800

    fix: type

--- frb_dart/lib/src/codec/sse.dart
+++ frb_dart/lib/src/codec/sse.dart
@@ -3,10 +3,10 @@ import 'package:flutter_rust_bridge/src/codec/base.dart';
 /// {@macro flutter_rust_bridge.only_for_generated_code}
 class SseCodec<S, E extends Object> extends BaseCodec<S, E> {
   /// {@macro flutter_rust_bridge.only_for_generated_code}
-  final S Function(dynamic) parseSuccessData;
+  final S Function(Deserializer deserializer) parseSuccessData;
 
   /// {@macro flutter_rust_bridge.only_for_generated_code}
-  final E Function(dynamic)? parseErrorData;
+  final E Function(Deserializer deserializer)? parseErrorData;
 
   /// {@macro flutter_rust_bridge.only_for_generated_code}
   const SseCodec({
