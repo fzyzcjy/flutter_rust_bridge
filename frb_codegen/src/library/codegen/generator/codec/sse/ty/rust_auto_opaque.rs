3628f1d035356a95236f4bae3a3b71dc6a1c573a
fzyzcjy <ch271828n@outlook.com>
2023-12-08 11:09:58 +0800

    feat: rust opaque

--- frb_codegen/src/library/codegen/generator/codec/sse/ty/rust_auto_opaque.rs
+++ frb_codegen/src/library/codegen/generator/codec/sse/ty/rust_auto_opaque.rs
@@ -1,14 +1,20 @@
 use crate::codegen::generator::codec::sse::ty::delegate::{
     simple_delegate_decode, simple_delegate_encode,
 };
+use crate::codegen::generator::codec::sse::ty::rust_opaque::RUST_OPAQUE_WIRE_TYPE;
 use crate::codegen::generator::codec::sse::ty::*;
 
 impl<'a> CodecSseTyTrait for RustAutoOpaqueCodecSseTy<'a> {
     fn generate_encode(&self, lang: &Lang) -> String {
-        todo!()
+        let needs_move = self.ir.needs_move();
+        simple_delegate_encode(
+            lang,
+            &RUST_OPAQUE_WIRE_TYPE,
+            &format!("src.sseEncode(move: {needs_move})"),
+        )
     }
 
     fn generate_decode(&self, lang: &Lang) -> String {
-        todo!()
+        simple_delegate_decode(lang, &RUST_OPAQUE_WIRE_TYPE, "inner")
     }
 }
--- frb_codegen/src/library/codegen/generator/codec/sse/ty/rust_opaque.rs
+++ frb_codegen/src/library/codegen/generator/codec/sse/ty/rust_opaque.rs
@@ -1,3 +1,16 @@
+use crate::codegen::generator::codec::sse::ty::delegate::{
+    simple_delegate_decode, simple_delegate_encode,
+};
 use crate::codegen::generator::codec::sse::ty::*;
 
-impl<'a> CodecSseTyTrait for RustOpaqueCodecSseTy<'a> {}
+impl<'a> CodecSseTyTrait for RustOpaqueCodecSseTy<'a> {
+    fn generate_encode(&self, lang: &Lang) -> String {
+        simple_delegate_encode(lang, &RUST_OPAQUE_WIRE_TYPE, "src.sseEncode()")
+    }
+
+    fn generate_decode(&self, lang: &Lang) -> String {
+        simple_delegate_decode(lang, &RUST_OPAQUE_WIRE_TYPE, "inner")
+    }
+}
+
+pub(super) const RUST_OPAQUE_WIRE_TYPE: IrType = Primitive(IrTypePrimitive::Usize);
