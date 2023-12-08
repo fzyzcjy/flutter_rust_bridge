1e3807e06d1569500edbea6278e1983c61bec9ea
fzyzcjy <ch271828n@outlook.com>
2023-12-08 09:25:26 +0800

    chore: unreachable

--- frb_codegen/src/library/codegen/generator/codec/sse/ty/ownership.rs
+++ frb_codegen/src/library/codegen/generator/codec/sse/ty/ownership.rs
@@ -1,3 +1,11 @@
 use crate::codegen::generator::codec::sse::ty::*;
 
-impl<'a> CodecSseTyTrait for OwnershipCodecSseTy<'a> {}
+impl<'a> CodecSseTyTrait for OwnershipCodecSseTy<'a> {
+    fn generate_encode(&self) -> String {
+        unreachable!()
+    }
+
+    fn generate_decode(&self) -> String {
+        unreachable!()
+    }
+}
--- frb_codegen/src/library/codegen/generator/codec/sse/ty/unencodable.rs
+++ frb_codegen/src/library/codegen/generator/codec/sse/ty/unencodable.rs
@@ -1,3 +1,11 @@
 use crate::codegen::generator::codec::sse::ty::*;
 
-impl<'a> CodecSseTyTrait for UnencodableCodecSseTy<'a> {}
+impl<'a> CodecSseTyTrait for UnencodableCodecSseTy<'a> {
+    fn generate_encode(&self) -> String {
+        unreachable!()
+    }
+
+    fn generate_decode(&self) -> String {
+        unreachable!()
+    }
+}
