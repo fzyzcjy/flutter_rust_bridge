42f0ebc2f657e5e11fc5ef1e02aa49d69900c30f
fzyzcjy <ch271828n@outlook.com>
2023-12-08 11:10:52 +0800

    feat: rough dart opaque

--- frb_codegen/src/library/codegen/generator/codec/sse/ty/dart_opaque.rs
+++ frb_codegen/src/library/codegen/generator/codec/sse/ty/dart_opaque.rs
@@ -1,3 +1,20 @@
+use crate::codegen::generator::codec::sse::ty::delegate::{
+    simple_delegate_decode, simple_delegate_encode,
+};
 use crate::codegen::generator::codec::sse::ty::*;
 
-impl<'a> CodecSseTyTrait for DartOpaqueCodecSseTy<'a> {}
+impl<'a> CodecSseTyTrait for DartOpaqueCodecSseTy<'a> {
+    fn generate_encode(&self, lang: &Lang) -> String {
+        simple_delegate_encode(
+            lang,
+            &DART_OPAQUE_WIRE_TYPE,
+            "wire.dart_opaque_dart2rust_encode(src)",
+        )
+    }
+
+    fn generate_decode(&self, lang: &Lang) -> String {
+        simple_delegate_decode(lang, &DART_OPAQUE_WIRE_TYPE, "inner")
+    }
+}
+
+pub(super) const DART_OPAQUE_WIRE_TYPE: IrType = Primitive(IrTypePrimitive::Usize);
