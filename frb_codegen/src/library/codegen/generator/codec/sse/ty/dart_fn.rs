8943405b48ee254c3aad6f7fab5b040047993797
fzyzcjy <ch271828n@outlook.com>
2023-12-08 11:01:58 +0800

    feat: dart fn

--- frb_codegen/src/library/codegen/generator/codec/sse/ty/dart_fn.rs
+++ frb_codegen/src/library/codegen/generator/codec/sse/ty/dart_fn.rs
@@ -1,3 +1,14 @@
+use crate::codegen::generator::codec::sse::ty::delegate::{
+    simple_delegate_decode, simple_delegate_encode,
+};
 use crate::codegen::generator::codec::sse::ty::*;
 
-impl<'a> CodecSseTyTrait for DartFnCodecSseTy<'a> {}
+impl<'a> CodecSseTyTrait for DartFnCodecSseTy<'a> {
+    fn generate_encode(&self, lang: &Lang) -> String {
+        simple_delegate_encode(lang, &self.ir.get_delegate(), "src")
+    }
+
+    fn generate_decode(&self, lang: &Lang) -> String {
+        simple_delegate_decode(lang, &self.ir.get_delegate(), "inner")
+    }
+}
--- frb_codegen/src/library/codegen/generator/codec/sse/ty/rust_auto_opaque.rs
+++ frb_codegen/src/library/codegen/generator/codec/sse/ty/rust_auto_opaque.rs
@@ -1,3 +1,14 @@
+use crate::codegen::generator::codec::sse::ty::delegate::{
+    simple_delegate_decode, simple_delegate_encode,
+};
 use crate::codegen::generator::codec::sse::ty::*;
 
-impl<'a> CodecSseTyTrait for RustAutoOpaqueCodecSseTy<'a> {}
+impl<'a> CodecSseTyTrait for RustAutoOpaqueCodecSseTy<'a> {
+    fn generate_encode(&self, lang: &Lang) -> String {
+        todo!()
+    }
+
+    fn generate_decode(&self, lang: &Lang) -> String {
+        todo!()
+    }
+}
