43229ce880e44e2b5bacfd201238d48a3fd9701d
fzyzcjy <ch271828n@outlook.com>
2023-12-08 17:09:44 +0800

    feat: rust_vec_u8_resize

--- frb_rust/src/ffi_binding/mod.rs
+++ frb_rust/src/ffi_binding/mod.rs
@@ -1,4 +1,6 @@
-use crate::for_generated::{box_from_leak_ptr, new_leak_vec_ptr, vec_from_leak_ptr};
+use crate::for_generated::{
+    box_from_leak_ptr, into_leak_vec_ptr, new_leak_vec_ptr, vec_from_leak_ptr,
+};
 
 #[cfg(not(wasm))]
 mod io;
@@ -13,6 +15,19 @@ pub use web::*;
 #[no_mangle]
 pub unsafe extern "C" fn rust_vec_u8_new(len: i32) -> *mut u8 {
     new_leak_vec_ptr::<u8>(0, len)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn rust_vec_u8_resize(ptr: *mut u8, old_len: i32, new_len: i32) -> *mut u8 {
+    let mut vec = vec_from_leak_ptr(ptr, old_len);
+
+    if new_len > vec.len() {
+        vec.reserve_exact(new_len - vec.len());
+    }
+    vec.resize(new_len, 0);
+    debug_assert!(vec.len() == new_len);
+
+    into_leak_vec_ptr(vec)
 }
 
 #[no_mangle]
