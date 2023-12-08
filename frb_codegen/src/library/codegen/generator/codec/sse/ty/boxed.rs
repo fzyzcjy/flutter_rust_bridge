3a7edd8565bd5cf1703aca08c39097fcd77be5cb
fzyzcjy <ch271828n@outlook.com>
2023-12-08 09:25:06 +0800

    chore: empty Boxed

--- frb_codegen/src/library/codegen/generator/codec/sse/ty/boxed.rs
+++ frb_codegen/src/library/codegen/generator/codec/sse/ty/boxed.rs
@@ -1,3 +1,11 @@
 use crate::codegen::generator::codec::sse::ty::*;
 
-impl<'a> CodecSseTyTrait for BoxedCodecSseTy<'a> {}
+impl<'a> CodecSseTyTrait for BoxedCodecSseTy<'a> {
+    fn generate_encode(&self) -> String {
+        todo!()
+    }
+
+    fn generate_decode(&self) -> String {
+        todo!()
+    }
+}
