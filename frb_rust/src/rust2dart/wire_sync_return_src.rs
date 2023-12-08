ceeb52148f9383d1ac2f486e2e302133c92489ab
fzyzcjy <ch271828n@outlook.com>
2023-12-08 20:58:57 +0800

    chore: trait

--- frb_rust/src/rust2dart/wire_sync_return_src.rs
+++ frb_rust/src/rust2dart/wire_sync_return_src.rs
@@ -6,14 +6,20 @@ use crate::rust2dart::action::Rust2DartAction;
 /// An object that can be converted into `WireSyncReturn*`
 /// This object is safe (no worries about memory leak, etc), while `WireSyncReturn` is not.
 /// That is why we have this intermediate object - we can safely play with this one.
+pub trait WireSyncReturnSrc {
+    fn new(inner: DartAbi) -> Self;
+
+    fn into_raw(self) -> WireSyncReturnDco;
+}
+
 pub struct WireSyncReturnDcoSrc(DartAbi);
 
-impl WireSyncReturnDcoSrc {
-    pub fn new(inner: DartAbi) -> Self {
+impl WireSyncReturnSrc for WireSyncReturnDcoSrc {
+    fn new(inner: DartAbi) -> Self {
         Self(inner)
     }
 
-    pub fn into_raw(self) -> WireSyncReturnDco {
+    fn into_raw(self) -> WireSyncReturnDco {
         #[cfg(not(wasm))]
         return new_leak_box_ptr(self.0);
 
