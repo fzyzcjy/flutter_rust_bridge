a1f7c1add01007d41d978a814929e47d86eaf034
fzyzcjy <ch271828n@outlook.com>
2023-12-08 18:06:50 +0800

    Revert "refactor: generate_wire_func_call_decode"
    This reverts commit d510d2c5ccb11656258fbe73a20bec91fcbae55e.

--- frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/misc/ty/dart_fn.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/misc/ty/dart_fn.rs
@@ -1,35 +1,4 @@
-use crate::codegen::generator::codec::structs::CodecMode;
 use crate::codegen::generator::wire::rust::spec_generator::base::*;
 use crate::codegen::generator::wire::rust::spec_generator::misc::ty::WireRustGeneratorMiscTrait;
-use convert_case::{Case, Casing};
-use itertools::Itertools;
 
-impl<'a> WireRustGeneratorMiscTrait for DartFnWireRustGenerator<'a> {
-    fn generate_wire_func_call_decode(&self, name: &str, codec_mode: CodecMode) -> String {
-        let closure_args = (0..self.ir.inputs.len())
-            .map(|i| format!("arg{i}"))
-            .collect_vec();
-        let closure_args_str = closure_args.join(", ");
-        let closure_args_into_dart_str = closure_args
-            .iter()
-            .map(|x| format!("{x}.into_into_dart().into_dart()"))
-            .join(", ");
-
-        format!(
-            "
-            {{
-                use flutter_rust_bridge::IntoDart;
-                let dart_opaque: flutter_rust_bridge::DartOpaque = {name}.{codec_mode}_decode();
-
-                move |{closure_args_str}| {{    
-                    {HANDLER_NAME}.dart_fn_invoke(vec![
-                        dart_opaque.clone().into_into_dart().into_dart(),
-                        {closure_args_into_dart_str}
-                    ])
-                }}
-            }}
-            ",
-            codec_mode = codec_mode.to_string().to_case(Case::Snake)
-        )
-    }
-}
+impl<'a> WireRustGeneratorMiscTrait for DartFnWireRustGenerator<'a> {}
