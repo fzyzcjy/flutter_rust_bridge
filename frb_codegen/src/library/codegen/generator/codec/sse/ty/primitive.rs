efdfe6c0993ae06210791bd10f6494bdba61c73f
fzyzcjy <ch271828n@outlook.com>
2023-12-08 09:51:22 +0800

    feat: use safe ident

--- frb_codegen/src/library/codegen/generator/codec/sse/ty/primitive.rs
+++ frb_codegen/src/library/codegen/generator/codec/sse/ty/primitive.rs
@@ -1,11 +1,11 @@
 use crate::codegen::generator::codec::sse::ty::*;
 
 impl<'a> CodecSseTyTrait for PrimitiveCodecSseTy<'a> {
-    fn generate_encode(&self, lang: &impl Lang) -> String {
-        format!("serializer.serialize_{};", TODO);
+    fn generate_encode(&self, _lang: &impl Lang) -> String {
+        format!("serializer.serialize_{};", self.ir.safe_ident());
     }
 
-    fn generate_decode(&self, lang: &impl Lang) -> String {
-        format!("return deserializer.deserialize_{};", TODO);
+    fn generate_decode(&self, _lang: &impl Lang) -> String {
+        format!("return deserializer.deserialize_{};", self.ir.safe_ident());
     }
 }
