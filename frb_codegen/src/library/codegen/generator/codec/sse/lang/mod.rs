6379489cb756c321ad5d15fd6e5f1b4676297b4e
fzyzcjy <ch271828n@outlook.com>
2023-12-08 09:47:40 +0800

    feat: general list

--- frb_codegen/src/library/codegen/generator/codec/sse/lang/mod.rs
+++ frb_codegen/src/library/codegen/generator/codec/sse/lang/mod.rs
@@ -10,6 +10,8 @@ pub(crate) trait Lang {
 
     fn call_constructor(&self, class_name: &str, field_names: &[String]) -> String;
 
+    fn for_loop(&self, lhs: &str, rhs: &str, body: &str) -> String;
+
     fn null(&self) -> String;
 
     fn var_decl(&self) -> String;
--- frb_codegen/src/library/codegen/generator/codec/sse/ty/general_list.rs
+++ frb_codegen/src/library/codegen/generator/codec/sse/ty/general_list.rs
@@ -1,3 +1,27 @@
 use crate::codegen::generator::codec::sse::ty::*;
 
-impl<'a> CodecSseTyTrait for GeneralListCodecSseTy<'a> {}
+impl<'a> CodecSseTyTrait for GeneralListCodecSseTy<'a> {
+    fn generate_encode(&self, lang: &impl Lang) -> String {
+        lang.for_loop(
+            "item",
+            "src",
+            &format!("{};", lang.call_encode(&self.ir.inner, "item")),
+        )
+    }
+
+    fn generate_decode(&self, lang: &impl Lang) -> String {
+        format!(
+            "
+            {var_decl} ans;
+            {}
+            return ans;
+            ",
+            lang.for_loop(
+                "item",
+                "src",
+                &format!("ans.push({});", lang.call_decode(&self.ir.inner))
+            ),
+            var_decl = lang.var_decl(),
+        )
+    }
+}
