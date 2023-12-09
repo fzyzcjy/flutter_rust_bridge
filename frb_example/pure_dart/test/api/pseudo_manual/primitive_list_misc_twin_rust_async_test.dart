1a488a0648375250a27aaadef4623f5ae8fa7825
fzyzcjy <ch271828n@outlook.com>
2023-12-09 10:32:27 +0800

    chore: comment out tests

--- frb_example/pure_dart/test/api/pseudo_manual/primitive_list_misc_twin_rust_async_test.dart
+++ frb_example/pure_dart/test/api/pseudo_manual/primitive_list_misc_twin_rust_async_test.dart
@@ -28,18 +28,19 @@ Future<void> main({bool skipRustLibInit = false}) async {
     expect(resp.boolList, List.filled(n, true));
   });
 
-  test('dart call handleZeroCopyVecOfPrimitive', () async {
-    final n = 10000;
-    final resp = await handleZeroCopyVecOfPrimitiveTwinRustAsync(n: n);
-    expect(resp.uint8List, Uint8List.fromList(List.filled(n, 42)));
-    expect(resp.int8List, Int8List.fromList(List.filled(n, 42)));
-    expect(resp.uint16List, Uint16List.fromList(List.filled(n, 42)));
-    expect(resp.int16List, Int16List.fromList(List.filled(n, 42)));
-    expect(resp.uint32List, Uint32List.fromList(List.filled(n, 42)));
-    expect(resp.int32List, Int32List.fromList(List.filled(n, 42)));
-    expect(resp.float32List, Float32List.fromList(List.filled(n, 42)));
-    expect(resp.float64List, Float64List.fromList(List.filled(n, 42)));
-    expect(resp.uint64List, Uint64List.fromList(List.filled(n, 42)));
-    expect(resp.int64List, Int64List.fromList(List.filled(n, 42)));
-  });
+  // no longer needed, since auto enable zero copy
+  // test('dart call handleZeroCopyVecOfPrimitive', () async {
+  //   final n = 10000;
+  //   final resp = await handleZeroCopyVecOfPrimitiveTwinRustAsync(n: n);
+  //   expect(resp.uint8List, Uint8List.fromList(List.filled(n, 42)));
+  //   expect(resp.int8List, Int8List.fromList(List.filled(n, 42)));
+  //   expect(resp.uint16List, Uint16List.fromList(List.filled(n, 42)));
+  //   expect(resp.int16List, Int16List.fromList(List.filled(n, 42)));
+  //   expect(resp.uint32List, Uint32List.fromList(List.filled(n, 42)));
+  //   expect(resp.int32List, Int32List.fromList(List.filled(n, 42)));
+  //   expect(resp.float32List, Float32List.fromList(List.filled(n, 42)));
+  //   expect(resp.float64List, Float64List.fromList(List.filled(n, 42)));
+  //   expect(resp.uint64List, Uint64List.fromList(List.filled(n, 42)));
+  //   expect(resp.int64List, Int64List.fromList(List.filled(n, 42)));
+  // });
 }
