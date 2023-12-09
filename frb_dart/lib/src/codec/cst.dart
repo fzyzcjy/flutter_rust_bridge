ea88315af4cfa7bfdcfd2f809ac9ec5b5045df10
fzyzcjy <ch271828n@outlook.com>
2023-12-09 08:31:15 +0800

    chore: unreachable

--- frb_dart/lib/src/codec/cst.dart
+++ frb_dart/lib/src/codec/cst.dart
@@ -4,10 +4,13 @@ import 'package:flutter_rust_bridge/src/generalized_frb_rust_binding/_io.dart';
 /// {@macro flutter_rust_bridge.only_for_generated_code}
 class CstCodec<S, E extends Object> extends BaseCodec<S, E, dynamic> {
   @override
-  S decode(dynamic raw) => throw UnimplementedError('unreachable');
-
-  @override
   void freeWireSyncReturn(
           dynamic raw, GeneralizedFrbRustBinding generalizedFrbRustBinding) =>
       throw UnimplementedError('unreachable');
+
+  @override
+  S decodeObject(dynamic raw) => throw UnimplementedError('unreachable');
+
+  @override
+  S decodeWireSyncType(dynamic raw) => throw UnimplementedError('unreachable');
 }
