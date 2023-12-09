39261e4869767134d9d8601c872c55e03e559d23
fzyzcjy <ch271828n@outlook.com>
2023-12-09 14:38:38 +0800

    fix: dup gen

--- frb_codegen/src/library/codegen/generator/codec/sse/ty/boxed.rs
+++ frb_codegen/src/library/codegen/generator/codec/sse/ty/boxed.rs
@@ -3,10 +3,18 @@ use crate::library::codegen::generator::codec::sse::lang::LangTrait;
 
 impl<'a> CodecSseTyTrait for BoxedCodecSseTy<'a> {
     fn generate_encode(&self, lang: &Lang) -> Option<String> {
-        Some(format!("{};", lang.call_encode(&*self.ir.inner, "self")))
+        self.should_generate()
+            .then(|| format!("{};", lang.call_encode(&*self.ir.inner, "self")))
     }
 
     fn generate_decode(&self, lang: &Lang) -> Option<String> {
-        Some(format!("return {};", lang.call_decode(&*self.ir.inner)))
+        self.should_generate()
+            .then(|| format!("return {};", lang.call_decode(&*self.ir.inner)))
+    }
+}
+
+impl<'a> BoxedCodecSseTy<'a> {
+    fn should_generate(&self) -> bool {
+        self.ir.exist_in_real_api
     }
 }
