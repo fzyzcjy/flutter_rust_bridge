1d6fbc8a8eb57767ac5563417db08a7db024c2d9
fzyzcjy <ch271828n@outlook.com>
2023-12-09 16:31:14 +0800

    chore: extract

--- frb_codegen/src/library/codegen/generator/codec/sse/ty/general_list.rs
+++ frb_codegen/src/library/codegen/generator/codec/sse/ty/general_list.rs
@@ -3,16 +3,20 @@ use crate::library::codegen::generator::codec::sse::lang::LangTrait;
 
 impl<'a> CodecSseTyTrait for GeneralListCodecSseTy<'a> {
     fn generate_encode(&self, lang: &Lang) -> Option<String> {
-        Some(lang.for_loop(
-            "item",
-            "self",
-            &format!("{};", lang.call_encode(&self.ir.inner, "item")),
-        ))
+        Some(general_list_generate_encode(lang, &self.ir.inner))
     }
 
     fn generate_decode(&self, lang: &Lang) -> Option<String> {
         Some(general_list_generate_decode(lang, &self.ir.inner))
     }
+}
+
+pub(super) fn general_list_generate_encode(lang: &Lang, ir_inner: &IrType) -> String {
+    lang.for_loop(
+        "item",
+        "self",
+        &format!("{};", lang.call_encode(ir_inner, "item")),
+    )
 }
 
 pub(super) fn general_list_generate_decode(lang: &Lang, ir_inner: &IrType) -> String {
