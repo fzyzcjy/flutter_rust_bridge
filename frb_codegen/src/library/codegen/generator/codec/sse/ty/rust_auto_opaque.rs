ceebdb32bf60302878b979c0f52b196b389e230d
fzyzcjy <ch271828n@outlook.com>
2023-12-09 19:03:59 +0800

    feat: autoopaque do not gen rust

--- frb_codegen/src/library/codegen/generator/codec/sse/ty/rust_auto_opaque.rs
+++ frb_codegen/src/library/codegen/generator/codec/sse/ty/rust_auto_opaque.rs
@@ -9,17 +9,19 @@ use crate::codegen::generator::codec::sse::ty::*;
 impl<'a> CodecSseTyTrait for RustAutoOpaqueCodecSseTy<'a> {
     fn generate_encode(&self, lang: &Lang) -> Option<String> {
         let needs_move = self.ir.needs_move();
-        Some(generate_generalized_rust_opaque_encode(
-            lang,
-            &format!("{needs_move}"),
-        ))
+        self.should_generate(lang)
+            .then(|| generate_generalized_rust_opaque_encode(lang, &format!("{needs_move}")))
     }
 
     fn generate_decode(&self, lang: &Lang) -> Option<String> {
-        Some(generate_generalized_rust_opaque_decode(
-            lang,
-            self.ir.clone().into(),
-            self.context,
-        ))
+        self.should_generate(lang).then(|| {
+            generate_generalized_rust_opaque_decode(lang, self.ir.clone().into(), self.context)
+        })
+    }
+}
+
+impl<'a> RustAutoOpaqueCodecSseTy<'a> {
+    fn should_generate(&self, lang: &Lang) -> bool {
+        !matches!(lang, Lang::RustLang(_))
     }
 }
