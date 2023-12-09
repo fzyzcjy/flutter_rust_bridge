29536eb8fff08e0025f6b1495e724f783d52f7f0
fzyzcjy <ch271828n@outlook.com>
2023-12-09 17:18:38 +0800

    fix: box

--- frb_codegen/src/library/codegen/generator/codec/sse/ty/boxed.rs
+++ frb_codegen/src/library/codegen/generator/codec/sse/ty/boxed.rs
@@ -3,18 +3,19 @@ use crate::library::codegen::generator::codec::sse::lang::LangTrait;
 
 impl<'a> CodecSseTyTrait for BoxedCodecSseTy<'a> {
     fn generate_encode(&self, lang: &Lang) -> Option<String> {
-        self.should_generate()
+        self.should_generate(lang)
             .then(|| format!("{};", lang.call_encode(&*self.ir.inner, "self")))
     }
 
     fn generate_decode(&self, lang: &Lang) -> Option<String> {
-        self.should_generate()
+        self.should_generate(lang)
             .then(|| format!("return {};", lang.call_decode(&*self.ir.inner)))
     }
 }
 
 impl<'a> BoxedCodecSseTy<'a> {
-    fn should_generate(&self) -> bool {
-        self.ir.exist_in_real_api
+    fn should_generate(&self, lang: &Lang) -> bool {
+        // The fake Box is only needed for CST codec, thus here we mostly ignore it.
+        self.ir.exist_in_real_api || matches!(lang, Lang::DartLang(_))
     }
 }
