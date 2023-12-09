a15364f87ad6d834853b730eb4b8dfc0698c7355
fzyzcjy <ch271828n@outlook.com>
2023-12-09 17:32:19 +0800

    feat: more rustopaque

--- frb_codegen/src/library/codegen/generator/codec/sse/ty/rust_auto_opaque.rs
+++ frb_codegen/src/library/codegen/generator/codec/sse/ty/rust_auto_opaque.rs
@@ -1,18 +1,15 @@
 use crate::codegen::generator::codec::sse::ty::delegate::{
     simple_delegate_decode, simple_delegate_encode,
 };
+use crate::codegen::generator::codec::sse::ty::rust_opaque::generate_generalized_rust_opaque_encode;
 use crate::codegen::generator::codec::sse::ty::*;
 
 impl<'a> CodecSseTyTrait for RustAutoOpaqueCodecSseTy<'a> {
     fn generate_encode(&self, lang: &Lang) -> Option<String> {
         let needs_move = self.ir.needs_move();
-        Some(simple_delegate_encode(
+        Some(generate_generalized_rust_opaque_encode(
             lang,
-            &IrTypeRustOpaque::DELEGATE_TYPE,
-            &match lang {
-                Lang::DartLang(_) => format!("self.sseEncode(move: {needs_move})"),
-                Lang::RustLang(_) => "TODO".to_owned(),
-            },
+            &format!("{needs_move}"),
         ))
     }
 
--- frb_codegen/src/library/codegen/generator/codec/sse/ty/rust_opaque.rs
+++ frb_codegen/src/library/codegen/generator/codec/sse/ty/rust_opaque.rs
@@ -5,11 +5,7 @@ use crate::codegen::generator::codec::sse::ty::*;
 
 impl<'a> CodecSseTyTrait for RustOpaqueCodecSseTy<'a> {
     fn generate_encode(&self, lang: &Lang) -> Option<String> {
-        Some(simple_delegate_encode(
-            lang,
-            &self.ir.get_delegate(),
-            "self.sseEncode()",
-        ))
+        Some(generate_generalized_rust_opaque_encode(lang, "null"))
     }
 
     fn generate_decode(&self, lang: &Lang) -> Option<String> {
@@ -22,12 +18,34 @@ impl<'a> CodecSseTyTrait for RustOpaqueCodecSseTy<'a> {
                     {var_decl} externalSizeOnNative = {};
                     return {}.sseDecode(ptr, externalSizeOnNative);
                     ",
-                    lang.call_decode(&Primitive(IrTypePrimitive::Bool)),
-                    lang.call_decode(&*self.ir.inner),
+                    lang.call_decode(&self.ir.get_delegate()),
+                    lang.call_decode(&EXTERNAL_SIZE_TYPE),
                     lang.null(),
                 )
             }
             Lang::RustLang(_) => simple_delegate_decode(lang, &self.ir.get_delegate(), "inner"),
         })
+    }
+}
+
+const EXTERNAL_SIZE_TYPE: IrType = IrType::Primitive(IrTypePrimitive::I32);
+
+pub(super) fn generate_generalized_rust_opaque_encode(lang: &Lang, needs_move: &str) -> String {
+    match lang {
+        Lang::DartLang(_) => simple_delegate_encode(
+            lang,
+            &IrTypeRustOpaque::DELEGATE_TYPE,
+            &format!("self.sseEncode(move: {needs_move})"),
+        ),
+        Lang::RustLang(_) => {
+            format!(
+                "
+                    {};
+                    {};
+                    ",
+                lang.call_encode(&IrTypeRustOpaque::DELEGATE_TYPE, "TODO"),
+                lang.call_encode(&EXTERNAL_SIZE_TYPE, "TODO"),
+            )
+        }
     }
 }
