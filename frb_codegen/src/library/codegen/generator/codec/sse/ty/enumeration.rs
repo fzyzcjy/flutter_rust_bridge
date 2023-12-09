8029670594faa78a9ac9b612dd699fe1780a31bb
fzyzcjy <ch271828n@outlook.com>
2023-12-09 13:04:04 +0800

    chore: todo

--- frb_codegen/src/library/codegen/generator/codec/sse/ty/enumeration.rs
+++ frb_codegen/src/library/codegen/generator/codec/sse/ty/enumeration.rs
@@ -5,11 +5,17 @@ use itertools::Itertools;
 
 impl<'a> CodecSseTyTrait for EnumRefCodecSseTy<'a> {
     fn generate_encode(&self, lang: &Lang) -> String {
-        format!("return TODO;")
+        match lang {
+            Lang::DartLang(_) => format!("return TODO;"),
+            Lang::RustLang(_) => format!("return TODO;"),
+        }
     }
 
     fn generate_decode(&self, lang: &Lang) -> String {
-        format!("return TODO;")
+        match lang {
+            Lang::DartLang(_) => format!("return TODO;"),
+            Lang::RustLang(_) => format!("return TODO;"),
+        }
     }
 }
 
