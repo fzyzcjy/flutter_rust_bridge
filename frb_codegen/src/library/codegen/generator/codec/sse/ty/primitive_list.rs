9a57625c6143e30421164ccfbfba1e471124c098
fzyzcjy <ch271828n@outlook.com>
2023-12-09 12:41:39 +0800

    chore: copy

--- frb_codegen/src/library/codegen/generator/codec/sse/ty/primitive_list.rs
+++ frb_codegen/src/library/codegen/generator/codec/sse/ty/primitive_list.rs
@@ -2,10 +2,30 @@ use crate::codegen::generator::codec::sse::ty::*;
 
 impl<'a> CodecSseTyTrait for PrimitiveListCodecSseTy<'a> {
     fn generate_encode(&self, lang: &Lang) -> String {
-        format!("return TODO_depend_on_serializer;")
+        match lang {
+            Lang::DartLang(_) => format!(
+                "serializer.buffer.put{}(self);",
+                get_serializer_dart_postfix(&self.ir)
+            ),
+            Lang::RustLang(_) => format!(
+                "serializer.cursor.write_{}::<NativeEndian>(self).unwrap();",
+                self.ir.rust_api_type()
+            ),
+        }
     }
 
     fn generate_decode(&self, lang: &Lang) -> String {
-        format!("return TODO_depend_on_serializer;")
+        match lang {
+            Lang::DartLang(_) => format!(
+                "return deserializer.buffer.get{}();",
+                get_serializer_dart_postfix(&self.ir)
+            ),
+            Lang::RustLang(_) => {
+                format!(
+                    "deserializer.cursor.read_{}::<NativeEndian>().unwrap()",
+                    self.ir.rust_api_type()
+                )
+            }
+        }
     }
 }
