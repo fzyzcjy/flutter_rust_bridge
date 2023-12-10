0e6c5981795de638e452523285650bc6f01cfc90
fzyzcjy <ch271828n@outlook.com>
2023-12-10 09:46:54 +0800

    fix: dart dyn not supported in sse mode

--- frb_example/pure_dart/rust/src/api/dart_dynamic.rs
+++ frb_example/pure_dart/rust/src/api/dart_dynamic.rs
@@ -1,3 +1,5 @@
+// FRB_INTERNAL_GENERATOR: {"forbiddenDuplicatorModes": ["sse", "syncSse", "rustAsyncSse"]}
+
 use flutter_rust_bridge::{DartDynamic, IntoDart};
 
 pub fn return_dart_dynamic_twin_normal() -> DartDynamic {
