14c18ddde65baac350d6e2d44e9bf27e582a1b38
fzyzcjy <ch271828n@outlook.com>
2023-12-10 09:19:10 +0800

    chore: fmt

--- frb_example/pure_dart/test/api/pseudo_manual/rust_opaque_sync_twin_sse_test.dart
+++ frb_example/pure_dart/test/api/pseudo_manual/rust_opaque_sync_twin_sse_test.dart
@@ -1,4 +1,4 @@
-// NOTE: This file is mimicking how a human developer writes tests, 
+// NOTE: This file is mimicking how a human developer writes tests,
 // and is auto-generated from `rust_opaque_sync_test.dart` by frb_internal
 // Please do not modify manually, but modify the origin and re-run frb_internal generator
 
