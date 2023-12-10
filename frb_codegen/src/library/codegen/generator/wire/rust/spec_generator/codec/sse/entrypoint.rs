4daf579842452e3b2c00b8c54e7fef1b5f46002f
fzyzcjy <ch271828n@outlook.com>
2023-12-10 10:58:29 +0800

    fix: platformpointer

--- frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/codec/sse/entrypoint.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/codec/sse/entrypoint.rs
@@ -43,7 +43,7 @@ impl WireRustCodecEntrypointTrait<'_> for SseWireRustCodecEntrypoint {
                 ExternFuncParam {
                     name: "ptr_".to_owned(),
                     rust_type: "*mut u8".to_owned(),
-                    dart_type: "ffi.Pointer<ffi.Uint8>".to_owned(),
+                    dart_type: "PlatformPointer".to_owned(),
                 },
                 ExternFuncParam {
                     name: "rust_vec_len_".to_owned(),
