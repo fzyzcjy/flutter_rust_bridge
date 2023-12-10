100769682a0a9ec4ed03fbdb6b98c1bb58d7c1da
fzyzcjy <ch271828n@outlook.com>
2023-12-10 09:51:25 +0800

    Revert "chore: temp test"
    This reverts commit 83c87e28f7edc4c156aa87c3daf349b5f90e8410.

--- frb_example/pure_dart/test/api/simple_test.dart
+++ frb_example/pure_dart/test/api/simple_test.dart
@@ -1,23 +1,11 @@
-import 'dart:typed_data';
-
-import 'package:frb_example_pure_dart/src/rust/api/pseudo_manual/primitive_list_twin_sync_sse.dart';
+import 'package:frb_example_pure_dart/src/rust/api/simple.dart';
 import 'package:frb_example_pure_dart/src/rust/frb_generated.dart';
 import 'package:test/test.dart';
 
 Future<void> main({bool skipRustLibInit = false}) async {
   if (!skipRustLibInit) await RustLib.init();
 
-  // test('dart call simpleAdder', () async {
-  //   expect(await simpleAdderTwinNormal(a: 42, b: 100), 142);
-  // });
-
-  print('TODO temp');
-
-  test('hi', () async {
-    expect(
-        examplePrimitiveListTypeU8TwinSyncSse(arg: Uint8List.fromList([255])),
-        255);
-   
-    throw Exception('TODO');
+  test('dart call simpleAdder', () async {
+    expect(await simpleAdderTwinNormal(a: 42, b: 100), 142);
   });
 }
