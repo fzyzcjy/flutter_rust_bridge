5da26477483dae8fec8694c5c043277c18e39318
fzyzcjy <ch271828n@outlook.com>
2023-12-10 22:29:33 +0800

    Revert "chore: codegen"
    This reverts commit be4f4780ecc2b81337eb04a2ecb72543a6e97c41.

--- frb_example/dart_minimal/lib/src/rust/frb_generated.dart
+++ frb_example/dart_minimal/lib/src/rust/frb_generated.dart
@@ -159,6 +159,15 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
   }
 
   void _sse_decode_unit(SseDeserializer deserializer) {}
+  @protected
+  int cst_encode_i_32(int raw) {
+    return raw;
+  }
+
+  @protected
+  void cst_encode_unit(void raw) {
+    return raw;
+  }
 
   void _sse_encode_box_autoadd_the_enum(
       TheEnum self, SseSerializer serializer) {
@@ -178,16 +187,4 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
   }
 
   void _sse_encode_unit(void self, SseSerializer serializer) {}
-}
-
-extension ExtRustLibApiImpl on RustLibApiImplPlatform {
-  @protected
-  int cst_encode_i_32(int raw) {
-    return raw;
-  }
-
-  @protected
-  void cst_encode_unit(void raw) {
-    return raw;
-  }
 }
