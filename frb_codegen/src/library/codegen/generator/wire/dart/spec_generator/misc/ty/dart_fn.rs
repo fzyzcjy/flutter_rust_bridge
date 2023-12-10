ce74064e8bb8ebfeb387bb203e09cf101980d053
fzyzcjy <ch271828n@outlook.com>
2023-12-10 21:36:32 +0800

    feat: some boilerplate

--- frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/misc/ty/dart_fn.rs
+++ frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/misc/ty/dart_fn.rs
@@ -1,4 +1,17 @@
+use crate::codegen::generator::acc::Acc;
 use crate::codegen::generator::wire::dart::spec_generator::base::*;
 use crate::codegen::generator::wire::dart::spec_generator::misc::ty::WireDartGeneratorMiscTrait;
+use crate::codegen::generator::wire::dart::spec_generator::output_code::WireDartOutputCode;
 
-impl<'a> WireDartGeneratorMiscTrait for DartFnWireDartGenerator<'a> {}
+impl<'a> WireDartGeneratorMiscTrait for DartFnWireDartGenerator<'a> {
+    fn generate_extra_functions(&self) -> Option<Acc<WireDartOutputCode>> {
+        let api_impl_body = format!(
+            "
+            "
+        );
+        Some(Acc::new_common(WireDartOutputCode {
+            api_impl_body,
+            ..Default::default()
+        }))
+    }
+}
