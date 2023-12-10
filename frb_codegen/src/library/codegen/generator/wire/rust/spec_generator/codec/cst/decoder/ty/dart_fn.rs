40bf3038a7e750d73b6e94a272da477f1a4c1d6b
fzyzcjy <ch271828n@outlook.com>
2023-12-10 21:25:58 +0800

    refactor: rm

--- frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/codec/cst/decoder/ty/dart_fn.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/codec/cst/decoder/ty/dart_fn.rs
@@ -2,35 +2,10 @@ use crate::codegen::generator::misc::target::Target;
 use crate::codegen::generator::wire::rust::spec_generator::codec::cst::base::*;
 use crate::codegen::generator::wire::rust::spec_generator::codec::cst::decoder::ty::WireRustCodecCstGeneratorDecoderTrait;
 use crate::codegen::ir::ty::IrTypeTrait;
-use crate::misc::consts::HANDLER_NAME;
-use itertools::Itertools;
 
 impl<'a> WireRustCodecCstGeneratorDecoderTrait for DartFnWireRustCodecCstGenerator<'a> {
     fn generate_wire_func_call_decode(&self, name: &str) -> String {
-        let closure_args = (0..self.ir.inputs.len())
-            .map(|i| format!("arg{i}"))
-            .collect_vec();
-        let closure_args_str = closure_args.join(", ");
-        let closure_args_into_dart_str = closure_args
-            .iter()
-            .map(|x| format!("{x}.into_into_dart().into_dart()"))
-            .join(", ");
-
-        format!(
-            "
-            {{
-                use flutter_rust_bridge::IntoDart;
-                let dart_opaque: flutter_rust_bridge::DartOpaque = {name}.cst_decode();
-
-                move |{closure_args_str}| {{    
-                    {HANDLER_NAME}.dart_fn_invoke(vec![
-                        dart_opaque.clone().into_into_dart().into_dart(),
-                        {closure_args_into_dart_str}
-                    ])
-                }}
-            }}
-            "
-        )
+        format!("decode_{}({name}.cst_decode())", self.ir.safe_ident())
     }
 
     fn generate_wire_func_param_api_type(&self) -> Option<String> {
