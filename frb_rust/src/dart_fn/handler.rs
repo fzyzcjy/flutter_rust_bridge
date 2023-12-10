568daecdd6af76d0aa469b6b6515031268f10dbb
fzyzcjy <ch271828n@outlook.com>
2023-12-10 19:52:02 +0800

    fix: move

--- frb_rust/src/dart_fn/handler.rs
+++ frb_rust/src/dart_fn/handler.rs
@@ -38,7 +38,9 @@ impl DartFnHandler {
         let sender = Rust2DartSender::new(Channel::new(invoke_port));
         sender.send(dart_fn_and_args);
 
-        Box::pin(AssertUnwindSafe(receiver.then(|x| async { x.unwrap() })))
+        Box::pin(AssertUnwindSafe(
+            receiver.then(|x| async move { x.unwrap() }),
+        ))
     }
 
     pub(crate) fn handle_output(&self, call_id: i32) {
