94c9890e24390604c4eeef762c65a7da97deb8fa
fzyzcjy <ch271828n@outlook.com>
2023-12-10 13:50:33 +0800

    feat: more types

--- frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/misc/ty/dart_opaque.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/misc/ty/dart_opaque.rs
@@ -10,23 +10,29 @@ use crate::misc::consts::HANDLER_NAME;
 
 impl<'a> WireRustGeneratorMiscTrait for DartOpaqueWireRustGenerator<'a> {
     fn generate_related_funcs(&self) -> Acc<WireRustOutputCode> {
-        Acc::new(|target| {
-            match target {
-                TargetOrCommon::Io | TargetOrCommon::Wasm => {
-                    vec![ExternFunc {
+        Acc::new(|target| match target {
+            TargetOrCommon::Io | TargetOrCommon::Wasm => {
+                let target: Target = target.try_into().unwrap();
+                let func = ExternFunc {
                         func_name: "dart_opaque_dart2rust_encode".into(),
                         params: vec![ExternFuncParam {
                             name: "handle".to_owned(),
-                            rust_type: "flutter_rust_bridge::for_generated::dart_sys::Dart_Handle".to_owned(),
-                            dart_type: "NOT_USED".into(),
-                        }.clone()],
-                        return_type: Some("*const std::ffi::c_void".into()),
-                        body: format!("unsafe {{ flutter_rust_bridge::for_generated::dart_opaque_dart2rust_encode(&*{HANDLER_NAME}, handle) }}"),
+                            rust_type: match target {
+                                Target::Io => "flutter_rust_bridge::for_generated::dart_sys::Dart_Handle",
+                                Target::Wasm => "flutter_rust_bridge::for_generated::wasm_bindgen::JsValue",
+                            }.into(),
+                            dart_type: "int".into(),
+                        }],
+                        return_type: Some(match target {
+                            Target::Io => "*const std::ffi::c_void",
+                            Target::Wasm => "usize",
+                        }.into()),
+                        body: format!("unsafe {{ flutter_rust_bridge::for_generated::dart_opaque_dart2rust_encode(&*{HANDLER_NAME}, handle) }} as _"),
                         target: Target::Io,
-                    }].into()
-                }
-                TargetOrCommon::Common => Default::default()
+                    };
+                vec![func].into()
             }
+            TargetOrCommon::Common => Default::default(),
         })
     }
 }
