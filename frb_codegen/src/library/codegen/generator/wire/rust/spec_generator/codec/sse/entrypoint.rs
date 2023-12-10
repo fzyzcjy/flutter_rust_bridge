e010ed7f924fa434c7a37203d86689735a871174
fzyzcjy <ch271828n@outlook.com>
2023-12-10 08:07:48 +0800

    fix: `<>`

--- frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/codec/sse/entrypoint.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/codec/sse/entrypoint.rs
@@ -80,7 +80,7 @@ impl WireRustCodecEntrypointTrait<'_> for SseWireRustCodecEntrypoint {
             .map(|field| {
                 let name = field.name.rust_style();
                 let rust_api_type = field.ty.rust_api_type();
-                format!("let api_{name} = {rust_api_type}::sse_decode(&mut deserializer);")
+                format!("let api_{name} = <{rust_api_type}>::sse_decode(&mut deserializer);")
             })
             .join("\n");
         format!(
