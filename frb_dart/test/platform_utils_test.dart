7009fd15df033c6c0732ee67e783c12289610735
fzyzcjy <ch271828n@outlook.com>
2023-12-10 14:49:28 +0800

    feat: more test

--- frb_dart/test/platform_utils_test.dart
+++ frb_dart/test/platform_utils_test.dart
@@ -4,71 +4,49 @@ import 'package:flutter_rust_bridge/src/platform_utils/platform_utils.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('generalizedSetUint64', () {
-    for (final info in const [
-      _Info(
-        setValue: 0,
-        expectLittleEndian: [0, 0, 0, 0, 0, 0, 0, 0],
-        expectBigEndian: [0, 0, 0, 0, 0, 0, 0, 0],
-      ),
-      _Info(
-        setValue: 2,
-        expectLittleEndian: [2, 0, 0, 0, 0, 0, 0, 0],
-        expectBigEndian: [0, 0, 0, 0, 0, 0, 0, 2],
-      ),
-      _Info(
-        setValue: -2,
-        expectLittleEndian: [254, 255, 255, 255, 255, 255, 255, 255],
-        expectBigEndian: [255, 255, 255, 255, 255, 255, 255, 254],
-      ),
-      _Info(
-        setValue: 2023,
-        expectLittleEndian: [231, 7, 0, 0, 0, 0, 0, 0],
-        expectBigEndian: [0, 0, 0, 0, 0, 0, 7, 231],
-      ),
-      _Info(
-        setValue: 0x112233445566,
-        expectLittleEndian: [0x66, 0x55, 0x44, 0x33, 0x22, 0x11, 0, 0],
-        expectBigEndian: [0, 0, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66],
-      ),
-      // TODO
-    ]) {
-      test('$info', () => _body((b) => b.generalizedSetUint64, info));
-    }
-  });
-
-  group('generalizedSetInt64', () {
-    for (final info in const [
-      _Info(
-        setValue: 0,
-        expectLittleEndian: [0, 0, 0, 0, 0, 0, 0, 0],
-        expectBigEndian: [0, 0, 0, 0, 0, 0, 0, 0],
-      ),
-      _Info(
-        setValue: 2,
-        expectLittleEndian: [2, 0, 0, 0, 0, 0, 0, 0],
-        expectBigEndian: [0, 0, 0, 0, 0, 0, 0, 2],
-      ),
-      _Info(
-        setValue: -2,
-        expectLittleEndian: [254, 255, 255, 255, 255, 255, 255, 255],
-        expectBigEndian: [255, 255, 255, 255, 255, 255, 255, 254],
-      ),
-      _Info(
-        setValue: 2023,
-        expectLittleEndian: [231, 7, 0, 0, 0, 0, 0, 0],
-        expectBigEndian: [0, 0, 0, 0, 0, 0, 7, 231],
-      ),
-      _Info(
-        setValue: 0x112233445566,
-        expectLittleEndian: [0x66, 0x55, 0x44, 0x33, 0x22, 0x11, 0, 0],
-        expectBigEndian: [0, 0, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66],
-      ),
-      // TODO
-    ]) {
-      test('$info', () => _body((b) => b.generalizedSetInt64, info));
-    }
-  });
+  for (final (name, getFunction)
+      in <(String, _GeneralizedSetter Function(ByteData))>[
+    ('Uint64', (b) => b.generalizedSetUint64),
+    ('Int64', (b) => b.generalizedSetInt64),
+  ]) {
+    group(name, () {
+      for (final info in const [
+        _Info(
+          setValue: 0,
+          expectLittleEndian: [0, 0, 0, 0, 0, 0, 0, 0],
+          expectBigEndian: [0, 0, 0, 0, 0, 0, 0, 0],
+        ),
+        _Info(
+          setValue: 2,
+          expectLittleEndian: [2, 0, 0, 0, 0, 0, 0, 0],
+          expectBigEndian: [0, 0, 0, 0, 0, 0, 0, 2],
+        ),
+        _Info(
+          setValue: -2,
+          expectLittleEndian: [254, 255, 255, 255, 255, 255, 255, 255],
+          expectBigEndian: [255, 255, 255, 255, 255, 255, 255, 254],
+        ),
+        _Info(
+          setValue: 2023,
+          expectLittleEndian: [231, 7, 0, 0, 0, 0, 0, 0],
+          expectBigEndian: [0, 0, 0, 0, 0, 0, 7, 231],
+        ),
+        _Info(
+          setValue: 0x112233445566,
+          expectLittleEndian: [0x66, 0x55, 0x44, 0x33, 0x22, 0x11, 0, 0],
+          expectBigEndian: [0, 0, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66],
+        ),
+        _Info(
+          setValue: -0x112233445566,
+          expectLittleEndian: [154, 170, 187, 204, 221, 238, 255, 255],
+          expectBigEndian: [255, 255, 238, 221, 204, 187, 170, 154],
+        ),
+        // TODO
+      ]) {
+        test('$info', () => _body(getFunction, info));
+      }
+    });
+  }
 }
 
 typedef _GeneralizedSetter = void Function(
