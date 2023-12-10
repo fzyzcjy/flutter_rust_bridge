f2eea10e69dcc779a584db3f8e82058f2332d682
fzyzcjy <ch271828n@outlook.com>
2023-12-10 13:48:55 +0800

    feat: add to wasm

--- frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/misc/ty/dart_opaque.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/misc/ty/dart_opaque.rs
@@ -1,5 +1,5 @@
 use crate::codegen::generator::acc::Acc;
-use crate::codegen::generator::misc::target::Target;
+use crate::codegen::generator::misc::target::{Target, TargetOrCommon};
 use crate::codegen::generator::wire::rust::spec_generator::base::*;
 use crate::codegen::generator::wire::rust::spec_generator::extern_func::{
     ExternFunc, ExternFuncParam,
@@ -10,19 +10,23 @@ use crate::misc::consts::HANDLER_NAME;
 
 impl<'a> WireRustGeneratorMiscTrait for DartOpaqueWireRustGenerator<'a> {
     fn generate_related_funcs(&self) -> Acc<WireRustOutputCode> {
-        Acc {
-            io: vec![ExternFunc {
-                func_name: "dart_opaque_dart2rust_encode".into(),
-                params: vec![ExternFuncParam {
-                    name: "handle".to_owned(),
-                    rust_type: "flutter_rust_bridge::for_generated::dart_sys::Dart_Handle".to_owned(),
-                    dart_type: "NOT_USED".into(),
-                }.clone()],
-                return_type: Some("*const std::ffi::c_void".into()),
-                body: format!("unsafe {{ flutter_rust_bridge::for_generated::dart_opaque_dart2rust_encode(&*{HANDLER_NAME}, handle) }}"),
-                target: Target::Io,
-            }].into(),
-            ..Default::default()
-        }
+        Acc::new(|target| {
+            match target {
+                TargetOrCommon::Io | TargetOrCommon::Wasm => {
+                    vec![ExternFunc {
+                        func_name: "dart_opaque_dart2rust_encode".into(),
+                        params: vec![ExternFuncParam {
+                            name: "handle".to_owned(),
+                            rust_type: "flutter_rust_bridge::for_generated::dart_sys::Dart_Handle".to_owned(),
+                            dart_type: "NOT_USED".into(),
+                        }.clone()],
+                        return_type: Some("*const std::ffi::c_void".into()),
+                        body: format!("unsafe {{ flutter_rust_bridge::for_generated::dart_opaque_dart2rust_encode(&*{HANDLER_NAME}, handle) }}"),
+                        target: Target::Io,
+                    }].into()
+                }
+                TargetOrCommon::Common => Default::default()
+            }
+        })
     }
 }
