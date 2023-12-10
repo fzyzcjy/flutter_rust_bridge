0766492cace854b85e14771805f9c540969db7b0
fzyzcjy <ch271828n@outlook.com>
2023-12-10 21:38:38 +0800

    feat: call

--- frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/codec/cst/encoder/ty/dart_fn.rs
+++ frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/codec/cst/encoder/ty/dart_fn.rs
@@ -2,10 +2,14 @@ use crate::codegen::generator::acc::Acc;
 use crate::codegen::generator::misc::target::{Target, TargetOrCommon};
 use crate::codegen::generator::wire::dart::spec_generator::codec::cst::base::*;
 use crate::codegen::generator::wire::dart::spec_generator::codec::cst::encoder::ty::WireDartCodecCstGeneratorEncoderTrait;
+use crate::codegen::ir::ty::IrTypeTrait;
 
 impl<'a> WireDartCodecCstGeneratorEncoderTrait for DartFnWireDartCodecCstGenerator<'a> {
     fn encode_func_body(&self) -> Acc<Option<String>> {
-        Acc::new_common(Some("return cst_encode_DartOpaque(raw);".to_owned()))
+        Acc::new_common(Some(format!(
+            "return cst_encode_DartOpaque(encode_{}(raw));",
+            self.ir.safe_ident(),
+        )))
     }
 
     fn dart_wire_type(&self, target: Target) -> String {
