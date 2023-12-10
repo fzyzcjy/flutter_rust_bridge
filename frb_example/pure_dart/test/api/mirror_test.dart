97603e8e847eda47795f7aedd46cd1bd97935845
fzyzcjy <ch271828n@outlook.com>
2023-12-10 09:18:09 +0800

    fix: import

--- frb_example/pure_dart/test/api/mirror_test.dart
+++ frb_example/pure_dart/test/api/mirror_test.dart
@@ -1,4 +1,4 @@
-// FRB_INTERNAL_GENERATOR: {"removeCode": ["import 'package:frb_example_pure_dart/src/rust/api/pseudo_manual/mirror_twin_sync_twin_sync.dart';\n", "import 'package:frb_example_pure_dart/src/rust/api/pseudo_manual/mirror_twin_sync_twin_rust_async.dart';\n"], "addCode": "import 'package:frb_example_pure_dart/src/rust/api/pseudo_manual/mirror_twin_sync.dart';"}
+// FRB_INTERNAL_GENERATOR: {"removeCode":["import 'package:frb_example_pure_dart/src/rust/api/pseudo_manual/mirror_twin_sync_sse_twin_rust_async_sse.dart';\n","import 'package:frb_example_pure_dart/src/rust/api/pseudo_manual/mirror_twin_sync_sse_twin_rust_async.dart';\n","import 'package:frb_example_pure_dart/src/rust/api/pseudo_manual/mirror_twin_sync_sse_twin_sse.dart';\n","import 'package:frb_example_pure_dart/src/rust/api/pseudo_manual/mirror_twin_sync_sse_twin_sync_sse.dart';\n","import 'package:frb_example_pure_dart/src/rust/api/pseudo_manual/mirror_twin_sync_sse_twin_sync.dart';\n"],"addCode":"import 'package:frb_example_pure_dart/src/rust/api/pseudo_manual/mirror_twin_sync_sse.dart';"}
 
 // ignore_for_file: duplicate_import
 
