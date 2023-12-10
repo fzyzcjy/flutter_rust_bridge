7dd86898e20f280a70f6010c774f362c2907f3d7
fzyzcjy <ch271828n@outlook.com>
2023-12-10 15:27:40 +0800

    chore: group

--- frb_dart/test/typed_data_test.dart
+++ frb_dart/test/typed_data_test.dart
@@ -9,32 +9,34 @@ final u64maxb = BigInt.parse('0xFFFFFFFFFFFFFFFF');
 const isWeb = bool.fromEnvironment('dart.library.html');
 
 void main() {
-  test('Int64List', () {
-    final buf = Int64List(1);
-    buf[0] = i64maxb;
-    expect(buf[0], i64maxb, reason: 'max');
-    buf[0] += BigInt.one;
-    if (isWeb) {
-      expect(buf[0], i64minb, reason: 'max+1 (wrapped)');
-    } else {
-      expect(buf[0], i64maxb, reason: 'max+1 (clamped)');
-    }
-  });
+  group('big lists', () {
+    test('Int64List', () {
+      final buf = Int64List(1);
+      buf[0] = i64maxb;
+      expect(buf[0], i64maxb, reason: 'max');
+      buf[0] += BigInt.one;
+      if (isWeb) {
+        expect(buf[0], i64minb, reason: 'max+1 (wrapped)');
+      } else {
+        expect(buf[0], i64maxb, reason: 'max+1 (clamped)');
+      }
+    });
 
-  test('Uint64List', () {
-    final buf = Uint64List(2);
-    buf[0] = u64maxb;
-    buf[1] = i64maxb;
-    expect(buf[0], u64maxb, reason: 'max');
-    expect(buf[1], i64maxb, reason: 'i64max');
-    buf[0] += BigInt.one;
-    if (isWeb) {
-      expect(buf[0], BigInt.zero, reason: 'max+1 (wrapped)');
-    } else {
-      expect(buf[0], u64maxb, reason: 'max+1 (clamped)');
-    }
-    buf[1] += BigInt.one;
-    expect(buf[1], i64maxb + BigInt.one, reason: 'i64max+1');
+    test('Uint64List', () {
+      final buf = Uint64List(2);
+      buf[0] = u64maxb;
+      buf[1] = i64maxb;
+      expect(buf[0], u64maxb, reason: 'max');
+      expect(buf[1], i64maxb, reason: 'i64max');
+      buf[0] += BigInt.one;
+      if (isWeb) {
+        expect(buf[0], BigInt.zero, reason: 'max+1 (wrapped)');
+      } else {
+        expect(buf[0], u64maxb, reason: 'max+1 (clamped)');
+      }
+      buf[1] += BigInt.one;
+      expect(buf[1], i64maxb + BigInt.one, reason: 'i64max+1');
+    });
   });
 
   group('read/write ByteData', () {
