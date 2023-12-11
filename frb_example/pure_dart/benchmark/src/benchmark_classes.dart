1ab8454a193df6d56c36eb14bb914344ca5fc842
fzyzcjy <ch271828n@outlook.com>
2023-12-11 14:23:12 +0800

    feat: more bench

--- frb_example/pure_dart/benchmark/src/benchmark_classes.dart
+++ frb_example/pure_dart/benchmark/src/benchmark_classes.dart
@@ -41,6 +41,11 @@ List<MaybeAsyncBenchmarkBase> createBenchmarks(
       OutputBytesSyncBenchmark(len, emitter: emitter),
       OutputBytesAsyncRawBenchmark(len, emitter: emitter),
     ],
+
+    for (final depth in [0, 5, 10]) ...[
+      BinaryTreeInputSyncBenchmark(depth, emitter: emitter),
+      BinaryTreeOutputSyncBenchmark(depth, emitter: emitter),
+    ],
   ];
 }
 
@@ -196,4 +201,43 @@ class OutputBytesAsyncRawBenchmark extends EnhancedAsyncBenchmarkBase {
     // sanity check
     if (result.length != len + 4) throw Exception();
   }
+}
+
+const _kBinaryTreeNodeName = 'HelloWorld';
+
+class BinaryTreeInputSyncBenchmark extends EnhancedBenchmarkBase {
+  final BenchmarkBinaryTreeTwinSync tree;
+
+  BinaryTreeInputSyncBenchmark(int depth, {super.emitter})
+      : tree = _createTree(depth),
+        super('BinaryTreeInputSync_Depth$depth');
+
+  static BenchmarkBinaryTreeTwinSync _createTree(int depth) {
+    if (depth == 0) {
+      return BenchmarkBinaryTreeTwinSync(
+        name: _kBinaryTreeNodeName,
+        left: null,
+        right: null,
+      );
+    }
+    return BenchmarkBinaryTreeTwinSync(
+      name: _kBinaryTreeNodeName,
+      left: _createTree(depth - 1),
+      right: _createTree(depth - 1),
+    );
+  }
+
+  @override
+  void run() => benchmarkBinaryTreeInputTwinSync(tree: tree);
+}
+
+class BinaryTreeOutputSyncBenchmark extends EnhancedBenchmarkBase {
+  final int depth;
+
+  BinaryTreeOutputSyncBenchmark(this.depth, {super.emitter})
+      : super('BinaryTreeOutputSync_Depth$depth');
+
+  @override
+  void run() => benchmarkBinaryTreeOutputTwinSync(
+      depth: depth, name: _kBinaryTreeNodeName);
 }
