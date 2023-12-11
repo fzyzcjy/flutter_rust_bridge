dd00a7725bc5d7112f73ea464e813b3c2ede9347
fzyzcjy <ch271828n@outlook.com>
2023-12-11 14:14:30 +0800

    feat: bench cases

--- frb_example/pure_dart/rust/src/api/benchmark_api.rs
+++ frb_example/pure_dart/rust/src/api/benchmark_api.rs
@@ -1,5 +1,7 @@
 #![allow(unused_variables)]
 
+use std::hint::black_box;
+
 pub fn benchmark_void_twin_normal() {}
 
 pub fn benchmark_input_bytes_twin_normal(bytes: Vec<u8>) -> i32 {
@@ -8,4 +10,56 @@ pub fn benchmark_input_bytes_twin_normal(bytes: Vec<u8>) -> i32 {
 
 pub fn benchmark_output_bytes_twin_normal(size: i32) -> Vec<u8> {
     vec![0; size as usize]
+}
+
+pub struct BenchmarkBinaryTreeTwinNormal {
+    name: String,
+    left: Option<BenchmarkBinaryTreeTwinNormal>,
+    right: Option<BenchmarkBinaryTreeTwinNormal>,
+}
+
+pub fn benchmark_binary_tree_input_twin_normal(tree: BenchmarkBinaryTreeTwinNormal) {
+    black_box(tree);
+}
+
+pub fn benchmark_binary_tree_output_twin_normal(
+    depth: i32,
+    name: String,
+) -> BenchmarkBinaryTreeTwinNormal {
+    create_tree(depth, &name)
+}
+
+fn create_tree(depth: i32, name: &str) -> BenchmarkBinaryTreeTwinNormal {
+    if depth == 0 {
+        BenchmarkBinaryTreeTwinNormal {
+            name: name.to_owned(),
+            left: None,
+            right: None,
+        }
+    } else {
+        BenchmarkBinaryTreeTwinNormal {
+            name: name.to_owned(),
+            left: Some(create_tree(depth - 1)),
+            right: Some(create_tree(depth - 1)),
+        }
+    }
+}
+
+pub struct BenchmarkBlobTwinNormal {
+    first: Vec<u8>,
+    second: Vec<u8>,
+    third: Vec<u8>,
+}
+
+pub fn benchmark_blob_input_twin_normal(blob: BenchmarkBlobTwinNormal) {
+    black_box(blob);
+}
+
+pub fn benchmark_blob_output_twin_normal(size: i32) -> BenchmarkBlobTwinNormal {
+    let data = vec![0; size as _];
+    BenchmarkBlobTwinNormal {
+        first: data.clone(),
+        second: data.clone(),
+        third: data,
+    }
 }
