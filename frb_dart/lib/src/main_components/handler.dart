b519e9d75da97e69da05ba3d53cdf975eb6783ad
fzyzcjy <ch271828n@outlook.com>
2023-12-11 09:52:49 +0800

    fix: import

--- frb_dart/lib/src/main_components/handler.dart
+++ frb_dart/lib/src/main_components/handler.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'package:flutter_rust_bridge/src/codec/base.dart';
 import 'package:flutter_rust_bridge/src/dart_opaque/dart_opaque.dart';
 import 'package:flutter_rust_bridge/src/exceptions.dart';
-import 'package:flutter_rust_bridge/src/generalized_frb_rust_binding/_io.dart';
+import 'package:flutter_rust_bridge/src/generalized_frb_rust_binding/generalized_frb_rust_binding.dart';
 import 'package:flutter_rust_bridge/src/generalized_isolate/generalized_isolate.dart';
 import 'package:flutter_rust_bridge/src/task.dart';
 import 'package:flutter_rust_bridge/src/utils/port_generator.dart';
