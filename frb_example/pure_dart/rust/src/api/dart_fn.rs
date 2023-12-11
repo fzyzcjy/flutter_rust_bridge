bddb48154d348af1ef39f97d48b12a674559837d
fzyzcjy <ch271828n@outlook.com>
2023-12-11 10:26:51 +0800

    fix: twin

--- frb_example/pure_dart/rust/src/api/dart_fn.rs
+++ frb_example/pure_dart/rust/src/api/dart_fn.rs
@@ -5,52 +5,59 @@ use std::future::Future;
 use std::panic::UnwindSafe;
 use std::pin::Pin;
 
-pub struct DemoStructForRustCallDart {
+pub struct DemoStructForRustCallDartTwinNormal {
     pub name: String,
 }
 
-pub async fn rust_call_dart_simple(callback: impl Fn() -> DartFnFuture<()> + UnwindSafe) {
+pub async fn rust_call_dart_simple_twin_normal(
+    callback: impl Fn() -> DartFnFuture<()> + UnwindSafe,
+) {
     println!("rust_call_dart_simple before");
     callback().await;
     println!("rust_call_dart_simple after");
 }
 
-pub async fn rust_call_dart_one_arg(callback: impl Fn(String) -> DartFnFuture<()> + UnwindSafe) {
+pub async fn rust_call_dart_one_arg_twin_normal(
+    callback: impl Fn(String) -> DartFnFuture<()> + UnwindSafe,
+) {
     callback("a".to_owned()).await;
 }
 
-pub async fn rust_call_dart_two_args(
-    callback: impl Fn(String, DemoStructForRustCallDart) -> DartFnFuture<()> + UnwindSafe,
+pub async fn rust_call_dart_two_args_twin_normal(
+    callback: impl Fn(String, DemoStructForRustCallDartTwinNormal) -> DartFnFuture<()> + UnwindSafe,
 ) {
     callback(
         "a".to_owned(),
-        DemoStructForRustCallDart {
+        DemoStructForRustCallDartTwinNormal {
             name: "b".to_owned(),
         },
     )
     .await;
 }
 
-pub async fn rust_call_dart_return(callback: impl Fn() -> DartFnFuture<String> + UnwindSafe) {
+pub async fn rust_call_dart_return_twin_normal(
+    callback: impl Fn() -> DartFnFuture<String> + UnwindSafe,
+) {
     let result = callback().await;
     assert_eq!(&result, "a");
 }
 
-pub async fn rust_call_dart_loopback(
-    callback: impl Fn(DemoStructForRustCallDart) -> DartFnFuture<DemoStructForRustCallDart> + UnwindSafe,
+pub async fn rust_call_dart_loopback_twin_normal(
+    callback: impl Fn(DemoStructForRustCallDartTwinNormal) -> DartFnFuture<DemoStructForRustCallDartTwinNormal>
+        + UnwindSafe,
 ) {
-    let result = callback(DemoStructForRustCallDart { name: "a".into() }).await;
+    let result = callback(DemoStructForRustCallDartTwinNormal { name: "a".into() }).await;
     assert_eq!(&result.name, "a");
 }
 
-pub async fn rust_call_dart_with_dart_opaque_arg(
+pub async fn rust_call_dart_with_dart_opaque_arg_twin_normal(
     input: DartOpaque,
     callback: impl Fn(DartOpaque) -> DartFnFuture<()> + UnwindSafe,
 ) {
     callback(input).await
 }
 
-pub async fn rust_call_dart_with_dart_opaque_result(
+pub async fn rust_call_dart_with_dart_opaque_result_twin_normal(
     callback: impl Fn() -> DartFnFuture<DartOpaque> + UnwindSafe,
 ) -> DartOpaque {
     callback().await
@@ -60,7 +67,7 @@ pub async fn rust_call_dart_with_dart_opaque_result(
 // pub async fn TODO_dart_closure_be_async() {}
 
 // TODO make dart side stateful
-pub async fn rust_call_dart_multi_times(
+pub async fn rust_call_dart_multi_times_twin_normal(
     callback: impl Fn() -> DartFnFuture<()> + UnwindSafe,
     num_times: i32,
 ) {
