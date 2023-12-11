2f78c012dcd0a7f2e0f2efed15fd7a29da6087a3
fzyzcjy <ch271828n@outlook.com>
2023-12-11 10:16:01 +0800

    feat: update api

--- frb_example/pure_dart/rust/src/api/dart_fn.rs
+++ frb_example/pure_dart/rust/src/api/dart_fn.rs
@@ -1,6 +1,6 @@
 // FRB_INTERNAL_GENERATOR: {"forbiddenDuplicatorModes": ["sync", "rustAsync", "sse", "syncSse", "rustAsyncSse"]}
 
-use flutter_rust_bridge::DartFnFuture;
+use flutter_rust_bridge::{DartFnFuture, DartOpaque};
 use std::future::Future;
 use std::panic::UnwindSafe;
 use std::pin::Pin;
@@ -15,48 +15,45 @@ pub async fn rust_call_dart_simple(callback: impl Fn() -> DartFnFuture<()> + Unw
     println!("rust_call_dart_simple after");
 }
 
-// TODO
-// pub async fn rust_call_dart_one_arg(callback: DartFn<fn(String) -> BoxFuture<'static, ()>>) {
-//     callback("a".to_owned()).await;
-// }
-//
-// pub async fn rust_call_dart_two_args(
-//     callback: DartFn<fn(String, DemoStructForRustCallDart) -> BoxFuture<'static, ()>>,
-// ) {
-//     callback(
-//         "a".to_owned(),
-//         DemoStructForRustCallDart {
-//             name: "b".to_owned(),
-//         },
-//     )
-//     .await;
-// }
-//
-// pub async fn rust_call_dart_return(callback: DartFn<fn() -> BoxFuture<'static, String>>) {
-//     let result = callback().await;
-//     assert_eq!(&result, "a");
-// }
-//
-// pub async fn rust_call_dart_loopback(
-//     callback: DartFn<
-//         fn(DemoStructForRustCallDart) -> BoxFuture<'static, DemoStructForRustCallDart>,
-//     >,
-// ) {
-//     let result = callback(DemoStructForRustCallDart { name: "a".into() }).await;
-//     assert_eq!(&result.name, "a");
-// }
-//
-// pub async fn rust_call_dart_with_dart_opaque_arg(
-//     input: DartOpaque,
-//     callback: DartFn<fn(DartOpaque) -> BoxFuture<'static, ()>>,
-// ) {
-//     callback(input).await
-// }
-//
-// pub async fn rust_call_dart_with_dart_opaque_result(
-//     callback: DartFn<fn() -> BoxFuture<'static, DartOpaque>>,
-// ) -> DartOpaque {
-//     callback().await
-// }
-//
-// pub async fn TODO_dart_closure_be_async(){}
+pub async fn rust_call_dart_one_arg(callback: impl Fn(String) -> DartFnFuture<()> + UnwindSafe) {
+    callback("a".to_owned()).await;
+}
+
+pub async fn rust_call_dart_two_args(
+    callback: impl Fn(String, DemoStructForRustCallDart) -> DartFnFuture<()> + UnwindSafe,
+) {
+    callback(
+        "a".to_owned(),
+        DemoStructForRustCallDart {
+            name: "b".to_owned(),
+        },
+    )
+    .await;
+}
+
+pub async fn rust_call_dart_return(callback: impl Fn() -> DartFnFuture<String> + UnwindSafe) {
+    let result = callback().await;
+    assert_eq!(&result, "a");
+}
+
+pub async fn rust_call_dart_loopback(
+    callback: impl Fn(DemoStructForRustCallDart) -> DartFnFuture<DemoStructForRustCallDart> + UnwindSafe,
+) {
+    let result = callback(DemoStructForRustCallDart { name: "a".into() }).await;
+    assert_eq!(&result.name, "a");
+}
+
+pub async fn rust_call_dart_with_dart_opaque_arg(
+    input: DartOpaque,
+    callback: impl Fn(DartOpaque) -> DartFnFuture<()> + UnwindSafe,
+) {
+    callback(input).await
+}
+
+pub async fn rust_call_dart_with_dart_opaque_result(
+    callback: impl Fn() -> DartFnFuture<DartOpaque> + UnwindSafe,
+) -> DartOpaque {
+    callback().await
+}
+
+pub async fn TODO_dart_closure_be_async() {}
