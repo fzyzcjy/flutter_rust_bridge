b2f89a787ca4a5bebb9affab9ed52a0a0fc0668e
fzyzcjy <ch271828n@outlook.com>
2023-12-11 10:26:24 +0800

    chore: simple

--- frb_example/pure_dart/test/api/dart_fn_test.dart
+++ frb_example/pure_dart/test/api/dart_fn_test.dart
@@ -1,14 +1,15 @@
 // FRB_INTERNAL_GENERATOR: {"forbiddenDuplicatorModes": ["sync", "syncSse"]}
 
+import 'package:frb_example_pure_dart/src/rust/api/dart_fn.dart';
 import 'package:frb_example_pure_dart/src/rust/frb_generated.dart';
 import 'package:test/test.dart';
-
-import '../test_utils.dart';
 
 Future<void> main({bool skipRustLibInit = false}) async {
   if (!skipRustLibInit) await RustLib.init();
 
   test('simple', () async {
-    expect(await simpleAdderTwinNormal(a: 42, b: 100), 142);
+    var callbackCallCount = 0;
+    await rustCallDartSimple(callback: () => callbackCallCount++);
+    expect(callbackCallCount, 1);
   });
 }
