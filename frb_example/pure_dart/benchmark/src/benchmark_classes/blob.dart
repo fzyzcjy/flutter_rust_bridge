7e0c139ecc6ea85c239bfc3327031a1a2c1c828f
fzyzcjy <ch271828n@outlook.com>
2023-12-12 13:44:28 +0800

    feat: impl

--- frb_example/pure_dart/benchmark/src/benchmark_classes/blob.dart
+++ frb_example/pure_dart/benchmark/src/benchmark_classes/blob.dart
@@ -6,6 +6,7 @@ import 'package:frb_example_pure_dart/src/rust/api/pseudo_manual/benchmark_api_t
 import 'package:frb_example_pure_dart/src/rust/api/pseudo_manual/benchmark_api_twin_sync_sse.dart';
 
 import '../benchmark_utils.dart';
+import '../protobuf_for_benchmark/protobuf_for_benchmark.pb.dart';
 
 class BlobInputSyncBenchmark extends EnhancedBenchmarkBase {
   final BenchmarkBlobTwinSync blob;
@@ -32,15 +33,17 @@ class BlobOutputSyncBenchmark extends EnhancedBenchmarkBase {
   void run() => benchmarkBlobOutputTwinSync(size: len);
 }
 
+BenchmarkBlobTwinSyncSse _createBlob(int len) => BenchmarkBlobTwinSyncSse(
+      first: Uint8List(len),
+      second: Uint8List(len),
+      third: Uint8List(len),
+    );
+
 class BlobInputSyncSseBenchmark extends EnhancedBenchmarkBase {
   final BenchmarkBlobTwinSyncSse blob;
 
   BlobInputSyncSseBenchmark(int len, {super.emitter})
-      : blob = BenchmarkBlobTwinSyncSse(
-          first: Uint8List(len),
-          second: Uint8List(len),
-          third: Uint8List(len),
-        ),
+      : blob = _createBlob(len),
         super('BlobInputSyncSseBenchmark_Len$len');
 
   @override
@@ -55,4 +58,50 @@ class BlobOutputSyncSseBenchmark extends EnhancedBenchmarkBase {
 
   @override
   void run() => benchmarkBlobOutputTwinSyncSse(size: len);
+}
+
+class BlobInputSyncProtobufBenchmark extends EnhancedBenchmarkBase {
+  final BlobProtobuf blob;
+
+  BlobInputSyncProtobufBenchmark(int len, {super.emitter})
+      : blob = BlobProtobuf(
+          first: Uint8List(len),
+          second: Uint8List(len),
+          third: Uint8List(len),
+        ),
+        super('BlobInputSyncProtobufBenchmark_Len$len');
+
+  @override
+  void run() => benchmarkBlobInputProtobufTwinSync(blob: blob);
+}
+
+class BlobOutputSyncProtobufBenchmark extends EnhancedBenchmarkBase {
+  final int len;
+
+  BlobOutputSyncProtobufBenchmark(this.len, {super.emitter})
+      : super('BlobOutputSyncProtobuf_Len$len');
+
+  @override
+  void run() => benchmarkBlobOutputProtobufTwinSync(size: len);
+}
+
+class BlobInputSyncJsonBenchmark extends EnhancedBenchmarkBase {
+  final BenchmarkBlobTwinSyncSse blob;
+
+  BlobInputSyncJsonBenchmark(int len, {super.emitter})
+      : blob = _createBlob(len),
+        super('BlobInputSyncJsonBenchmark_Len$len');
+
+  @override
+  void run() => benchmarkBlobInputJsonTwinSync(blob: blob);
+}
+
+class BlobOutputSyncJsonBenchmark extends EnhancedBenchmarkBase {
+  final int len;
+
+  BlobOutputSyncJsonBenchmark(this.len, {super.emitter})
+      : super('BlobOutputSyncJson_Len$len');
+
+  @override
+  void run() => benchmarkBlobOutputJsonTwinSync(size: len);
 }
