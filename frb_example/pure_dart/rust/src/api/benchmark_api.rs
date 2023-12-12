b7f666e2909111a08f32c10cd02334398d4ab703
fzyzcjy <ch271828n@outlook.com>
2023-12-12 13:30:00 +0800

    fix: more

--- frb_example/pure_dart/rust/src/api/benchmark_api.rs
+++ frb_example/pure_dart/rust/src/api/benchmark_api.rs
@@ -1,3 +1,6 @@
+// Note: This file contains tons of duplicated code! Because it is only for benchmarking
+// and many are ad-hoc code.
+
 #![allow(unused_variables)]
 
 use crate::auxiliary::protobuf_for_benchmark::{BinaryTreeProtobuf, BlobProtobuf};
@@ -42,14 +45,39 @@ impl BenchmarkBinaryTreeTwinNormal {
     }
 }
 
-lazy_static! {
-    static ref BINARY_TREES: HashMap<i32, BenchmarkBinaryTreeTwinNormal> = {
-        let mut m = HashMap::new();
-        for depth in vec![0, 5, 10].into_iter() {
-            m.insert(depth, create_tree(depth, "HelloWorld"));
+impl BinaryTreeProtobuf {
+    fn new(depth: i32, name: &str) -> Self {
+        if depth == 0 {
+            Self {
+                name: name.to_owned(),
+                left: protobuf::SingularPtrField::none(),
+                right: protobuf::SingularPtrField::none(),
+                ..Default::default()
+            }
+        } else {
+            Self {
+                name: name.to_owned(),
+                left: protobuf::SingularPtrField::some(Self::new(depth - 1, name)),
+                right: protobuf::SingularPtrField::some(Self::new(depth - 1, name)),
+                ..Default::default()
+            }
         }
-        m
-    };
+    }
+}
+
+fn create_binary_tree_map<T>(creator: impl Fn(i32, &str) -> T) -> HashMap<i32, T> {
+    let mut m = HashMap::new();
+    for depth in vec![0, 5, 10].into_iter() {
+        m.insert(depth, creator(depth, "HelloWorld"));
+    }
+    m
+}
+
+lazy_static! {
+    static ref BINARY_TREES: HashMap<i32, BenchmarkBinaryTreeTwinNormal> =
+        create_binary_tree_map(BenchmarkBinaryTreeTwinNormal::new);
+    static ref BINARY_TREES_PROTOBUF: HashMap<i32, BinaryTreeProtobuf> =
+        create_binary_tree_map(BinaryTreeProtobuf::new);
 }
 
 pub fn benchmark_binary_tree_input_twin_normal(tree: BenchmarkBinaryTreeTwinNormal) {
@@ -66,7 +94,11 @@ pub fn benchmark_binary_tree_input_protobuf_twin_normal(raw: Vec<u8>) {
 }
 
 pub fn benchmark_binary_tree_output_protobuf_twin_normal(depth: i32) -> Vec<u8> {
-    BINARY_TREES.get(&depth).unwrap().write_to_bytes().unwrap()
+    BINARY_TREES_PROTOBUF
+        .get(&depth)
+        .unwrap()
+        .write_to_bytes()
+        .unwrap()
 }
 
 pub fn benchmark_binary_tree_input_json_twin_normal(raw: String) {
