19976e546f20c15b24dc26924fe78b8655de3ad8
fzyzcjy <ch271828n@outlook.com>
2023-12-13 09:41:52 +0800

    feat: with_sse_extra_types

--- frb_codegen/src/library/codegen/generator/codec/sse/misc.rs
+++ frb_codegen/src/library/codegen/generator/codec/sse/misc.rs
@@ -1,12 +1,19 @@
 use crate::codegen::ir::ty::primitive::IrTypePrimitive;
 use crate::codegen::ir::ty::IrType;
+use itertools::Itertools;
 
 pub(crate) fn with_sse_extra_types(raw: &[IrType]) -> Vec<IrType> {
     [
         raw.to_owned(),
         vec![
-            // SSE's
+            // Many SSE codec needs these types to encode length, existence, etc
+            // So we unconditionally generate code for these
             IrType::Primitive(IrTypePrimitive::I32),
+            IrType::Primitive(IrTypePrimitive::Bool),
         ],
     ]
+    .concat()
+    .into_iter()
+    .unique()
+    .collect_vec()
 }
