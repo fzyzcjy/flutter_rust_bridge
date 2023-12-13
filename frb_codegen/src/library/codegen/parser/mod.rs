977c989ac4402f433815718034e27f5ba0acca09
fzyzcjy <ch271828n@outlook.com>
2023-12-13 14:17:40 +0800

    feat: syn parse

--- frb_codegen/src/library/codegen/parser/mod.rs
+++ frb_codegen/src/library/codegen/parser/mod.rs
@@ -100,11 +100,19 @@ fn read_files(
     cached_rust_reader: &mut CachedRustReader,
     dumper: &Dumper,
 ) -> anyhow::Result<Vec<FileData>> {
-    rust_input_paths
+    let contents = rust_input_paths
         .iter()
         .map(|rust_input_path| {
             let content =
                 cached_rust_reader.read_rust_file(rust_input_path, rust_crate_dir, dumper)?;
+            (rust_input_path.to_owned(), content)
+        })
+        .collect_vec();
+
+    let _pb = simple_progress("Run syn".to_owned(), 0);
+    contents
+        .into_iter()
+        .map(|(rust_input_path, content)| {
             let ast = syn::parse_file(&content)?;
             Ok(FileData {
                 path: (*rust_input_path).clone(),
