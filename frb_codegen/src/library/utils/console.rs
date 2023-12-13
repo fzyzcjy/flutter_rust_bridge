761e6f06e220e79c8867dc53a5188b351737cda1
fzyzcjy <ch271828n@outlook.com>
2023-12-13 13:57:39 +0800

    feat: simple progress bar

--- frb_codegen/src/library/utils/console.rs
+++ frb_codegen/src/library/utils/console.rs
@@ -1,9 +1,27 @@
-use indicatif::{ProgressBar, ProgressStyle};
+use indicatif::{ProgressBar, ProgressState, ProgressStyle};
+use std::fmt::Write;
+use std::time::Duration;
 
-pub(crate) fn simple_progress() {
-    let style =
-        ProgressStyle::with_template("{spinner:.green} [{elapsed_precise}] {wide_msg}").unwrap();
+pub(crate) struct SimpleProgress {
+    pb: ProgressBar,
+}
+
+impl Drop for SimpleProgress {
+    fn drop(&mut self) {
+        self.pb.finish();
+    }
+}
+
+pub(crate) fn simple_progress(message: &str) -> SimpleProgress {
+    let style = ProgressStyle::with_template("{spinner} {my_elapsed:.dim} {wide_msg}")
+        .unwrap()
+        .with_key("my_elapsed", |state: &ProgressState, w: &mut dyn Write| {
+            write!(w, "[{:.1}s]", state.elapsed().as_secs_f64()).unwrap()
+        })
+        .tick_chars("⠁⠂⠄⡀⢀⠠⠐⠈ ");
     let pb = ProgressBar::new_spinner();
     pb.set_style(style);
-    pb.finish_with_message("downloaded");
+    pb.enable_steady_tick(Duration::from_millis(50));
+    pb.set_message(message);
+    SimpleProgress { pb }
 }
