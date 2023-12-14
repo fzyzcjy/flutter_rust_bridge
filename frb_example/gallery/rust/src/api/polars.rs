2cb63eb516c81f86b869987164a623bb6458b833
fzyzcjy <ch271828n@outlook.com>
2023-12-14 12:54:58 +0800

    chore: rm

--- frb_example/gallery/rust/src/api/polars.rs
+++ frb_example/gallery/rust/src/api/polars.rs
@@ -6,19 +6,6 @@ use polars_lazy::prelude::*;
 // This demo is a minimal version of https://github.com/Desdaemon/polars_dart
 // Refer to that repository for more details (though may have not migrated to V2 yet)
 
-// TODO rm
-pub fn hello_polars() -> String {
-    // demo from https://pola-rs.github.io/polars/
-    let df = polars_related::create_df_iris()
-        .lazy()
-        .filter(col("sepal_length").gt(lit(5)))
-        .group_by(vec![col("species")])
-        .agg([col("*").sum()])
-        .collect()
-        .unwrap();
-    format!("{}", df)
-}
-
 #[frb(opaque)]
 pub struct DataFrame(polars_core::prelude::DataFrame);
 
