765f5be7d4008394d156e95ffa4cf4fe284fb540
fzyzcjy <ch271828n@outlook.com>
2023-12-14 14:47:30 +0800

    fix: compile

--- frb_example/pure_dart/rust/src/api/async_spawn.rs
+++ frb_example/pure_dart/rust/src/api/async_spawn.rs
@@ -2,13 +2,14 @@
 
 pub async fn simple_use_async_spawn() {
     // ref demo in https://docs.rs/tokio/latest/tokio/task/fn.spawn.html
-    let handle = flutter_rust_bridge::spawn(async move || "hello".to_owned());
+    let handle = flutter_rust_bridge::spawn(async { "hello".to_owned() });
     let output = handle.await.unwrap();
     assert_eq!(output, "hello".to_owned());
 }
 
 pub async fn simple_use_async_spawn_blocking() {
-    let handle = flutter_rust_bridge::spawn_blocking(async move || "hello".to_owned());
+    // ref demo in https://docs.rs/tokio/latest/tokio/task/fn.spawn_blocking.html
+    let handle = flutter_rust_bridge::spawn_blocking(|| "hello".to_owned());
     let output = handle.await.unwrap();
     assert_eq!(output, "hello".to_owned());
 }
