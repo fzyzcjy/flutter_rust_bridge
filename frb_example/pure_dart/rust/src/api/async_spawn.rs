6fd001aeef5dc6bea9a8209db523085fa0b6e121
fzyzcjy <ch271828n@outlook.com>
2023-12-14 14:46:59 +0800

    feat: impl test

--- frb_example/pure_dart/rust/src/api/async_spawn.rs
+++ frb_example/pure_dart/rust/src/api/async_spawn.rs
@@ -1,9 +1,14 @@
 // FRB_INTERNAL_GENERATOR: {"forbiddenDuplicatorModes": ["sync", "rustAsync", "syncSse", "rustAsyncSse"]}
 
 pub async fn simple_use_async_spawn() {
-    todo!()
+    // ref demo in https://docs.rs/tokio/latest/tokio/task/fn.spawn.html
+    let handle = flutter_rust_bridge::spawn(async move || "hello".to_owned());
+    let output = handle.await.unwrap();
+    assert_eq!(output, "hello".to_owned());
 }
 
 pub async fn simple_use_async_spawn_blocking() {
-    todo!()
+    let handle = flutter_rust_bridge::spawn_blocking(async move || "hello".to_owned());
+    let output = handle.await.unwrap();
+    assert_eq!(output, "hello".to_owned());
 }
