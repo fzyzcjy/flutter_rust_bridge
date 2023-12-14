2997670f77f21b0090289a8e8f077da1e5765650
fzyzcjy <ch271828n@outlook.com>
2023-12-14 09:42:27 +0800

    chore: cp back flutter_rust_bridge v1 demo

--- frb_example/gallery/lib/src/examples/mandelbrot.dart
+++ frb_example/gallery/lib/src/examples/mandelbrot.dart
@@ -1,10 +1,43 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 
-class MandelbrotPageBody extends StatelessWidget {
+class MandelbrotPageBody extends StatefulWidget {
   const MandelbrotPageBody({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Text('hi');
+  State<MandelbrotPageBody> createState() => _MandelbrotPageBodyState();
+}
+
+class _MandelbrotPageBodyState extends State<MandelbrotPageBody> {
+  Uint8List? exampleImage;
+  String? exampleText;
+
+  @override
+  void initState() {
+    super.initState();
+    runPeriodically(_callExampleFfiOne);
+    _callExampleFfiTwo();
+  }
+
+  @override
+  Widget build(BuildContext context) => buildPageUi(
+        exampleImage,
+        exampleText,
+      );
+
+  Future<void> _callExampleFfiOne() async {
+    final receivedImage = await api.drawMandelbrot(
+        imageSize: const Size(width: 50, height: 50),
+        zoomPoint: examplePoint,
+        scale: generateScale(),
+        numThreads: 4);
+    if (mounted) setState(() => exampleImage = receivedImage);
+  }
+
+  Future<void> _callExampleFfiTwo() async {
+    final receivedText =
+        await api.passingComplexStructs(root: createExampleTree());
+    if (mounted) setState(() => exampleText = receivedText);
   }
 }
