f2fd90684fee30c595f3303c955aca6fd09f38e5
fzyzcjy <ch271828n@outlook.com>
2023-12-14 11:08:32 +0800

    faet: dart demo

--- frb_example/gallery/lib/src/examples/polars.dart
+++ frb_example/gallery/lib/src/examples/polars.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:frb_example_gallery/src/rust/api/polars.dart';
 
-class PolarsPageBody extends StatelessWidget {
+class PolarsPageBody extends StatefulWidget {
   const PolarsPageBody({super.key});
+
+  @override
+  State<PolarsPageBody> createState() => _PolarsPageBodyState();
+}
+
+class _PolarsPageBodyState extends State<PolarsPageBody> {
+  @override
+  void initState() {
+    super.initState();
+    () async {
+      final msg = await helloPolars();
+      print('helloPolars=$msg');
+    }();
+  }
 
   @override
   Widget build(BuildContext context) {
