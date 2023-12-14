a5e3b2b65fd17a8c980f0bf45fed3d9105e4f096
fzyzcjy <ch271828n@outlook.com>
2023-12-14 13:10:51 +0800

    feat: unwind safe

--- frb_example/gallery/rust/src/api/polars.rs
+++ frb_example/gallery/rust/src/api/polars.rs
@@ -1,74 +1,93 @@
 use crate::ignore_me::polars_related;
-use flutter_rust_bridge::frb;
+use flutter_rust_bridge::{frb, DartSafe};
 use polars_core::prelude::*;
 use polars_lazy::prelude::*;
+use std::panic::AssertUnwindSafe;
 
 // This demo is a minimal version of https://github.com/Desdaemon/polars_dart
 // Refer to that repository for more details (though may have not migrated to V2 yet)
 
 #[frb(opaque)]
-pub struct DataFrame(polars_core::prelude::DataFrame);
+pub struct DataFrame(AssertUnwindSafe<polars_core::prelude::DataFrame>);
 
 impl DataFrame {
+    fn new(inner: polars_core::prelude::DataFrame) -> DataFrame {
+        Self(AssertUnwindSafe(inner))
+    }
+
     pub fn lazy(self) -> LazyFrame {
-        LazyFrame(self.0.lazy())
+        LazyFrame::new(self.0 .0.lazy())
     }
 }
 
 #[frb(opaque)]
-pub struct LazyFrame(polars_lazy::prelude::LazyFrame);
+pub struct LazyFrame(AssertUnwindSafe<polars_lazy::prelude::LazyFrame>);
 
 impl LazyFrame {
+    fn new(inner: polars_lazy::prelude::LazyFrame) -> LazyFrame {
+        Self(AssertUnwindSafe(inner))
+    }
+
     pub fn filter(self, predicate: Expr) -> LazyFrame {
-        Self(self.0.filter(predicate.0))
+        Self::new(self.0 .0.filter(predicate.0 .0))
     }
 
     pub fn group_by(self, expr: Vec<Expr>) -> LazyGroupBy {
-        LazyGroupBy(
+        LazyGroupBy::new(
             self.0
-                .group_by(expr.into_iter().map(|x| x.0).collect::<Vec<_>>()),
+                 .0
+                .group_by(expr.into_iter().map(|x| x.0 .0).collect::<Vec<_>>()),
         )
     }
 
     pub fn collect(self) -> anyhow::Result<DataFrame> {
-        Ok(DataFrame(self.0.collect()?))
+        Ok(DataFrame::new(self.0 .0.collect()?))
     }
 }
 
 #[frb(opaque)]
-pub struct LazyGroupBy(polars_lazy::prelude::LazyGroupBy);
+pub struct LazyGroupBy(AssertUnwindSafe<polars_lazy::prelude::LazyGroupBy>);
 
 impl LazyGroupBy {
+    fn new(inner: polars_lazy::prelude::LazyGroupBy) -> LazyGroupBy {
+        Self(AssertUnwindSafe(inner))
+    }
+
     pub fn agg(self, expr: Vec<Expr>) -> LazyFrame {
-        LazyFrame(
+        LazyFrame::new(
             self.0
-                .agg(expr.into_iter().map(|x| x.0).collect::<Vec<_>>()),
+                 .0
+                .agg(expr.into_iter().map(|x| x.0 .0).collect::<Vec<_>>()),
         )
     }
 }
 
 pub fn read_sample_dataset() -> DataFrame {
-    DataFrame(polars_related::create_df_iris())
+    DataFrame::new(polars_related::create_df_iris())
 }
 
 // Instead of opaque, we can also use the translatable types mode
 #[frb(opaque)]
-pub struct Expr(polars_lazy::prelude::Expr);
+pub struct Expr(AssertUnwindSafe<polars_lazy::prelude::Expr>);
 
 impl Expr {
+    fn new(inner: polars_lazy::prelude::Expr) -> Expr {
+        Self(AssertUnwindSafe(inner))
+    }
+
     pub fn gt(self, other: Expr) -> Expr {
-        Expr(self.0.gt(other.0))
+        Expr::new(self.0 .0.gt(other.0))
     }
 
     pub fn sum(self) -> Expr {
-        Expr(self.0.sum())
+        Expr::new(self.0 .0.sum())
     }
 }
 
 pub fn col(name: String) -> Expr {
-    Expr(polars_lazy::prelude::col(&name))
+    Expr::new(polars_lazy::prelude::col(&name))
 }
 
 pub fn lit(t: i32) -> Expr {
-    Expr(polars_lazy::prelude::lit(t))
+    Expr::new(polars_lazy::prelude::lit(t))
 }
