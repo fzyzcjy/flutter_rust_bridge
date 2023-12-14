b4220b5c6cae9cafba7f764f79d5202e82fd85b9
fzyzcjy <ch271828n@outlook.com>
2023-12-14 09:11:23 +0800

    refactor: MainPageWidget

--- frb_example/gallery/lib/main.dart
+++ frb_example/gallery/lib/main.dart
@@ -1,25 +1,17 @@
-// ignore_for_file: use_key_in_widget_constructors
-
 import 'package:flutter/material.dart';
-import 'package:frb_example_gallery/src/rust/api/simple.dart';
+import 'package:frb_example_gallery/src/ignore_me/main_page.dart';
 import 'package:frb_example_gallery/src/rust/frb_generated.dart';
 
 Future<void> main() async {
   await RustLib.init();
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('flutter_rust_bridge Gallery')),
-        body: Center(
-          child: Text(
-              'Action: Call Rust `greet("Tom")`\nResult: `${greet(name: "Tom")}`'),
-        ),
-      ),
-    );
+    return const MainPageWidget();
   }
 }
---
+++ frb_example/gallery/lib/src/ignore_me/main_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MainPageWidget extends StatelessWidget {
+  const MainPageWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('flutter_rust_bridge Gallery')),
+        body: Center(
+          child: Text(
+              'Action: Call Rust `greet("Tom")`\nResult: `${greet(name: "Tom")}`'),
+        ),
+      ),
+    );
+  }
+}
