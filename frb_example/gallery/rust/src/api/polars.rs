b56c80f7741d52c502919e6c3fe5b199529426d2
fzyzcjy <ch271828n@outlook.com>
2023-12-14 11:33:24 +0800

    feat: dataset

--- frb_example/gallery/rust/src/api/polars.rs
+++ frb_example/gallery/rust/src/api/polars.rs
@@ -4,6 +4,9 @@ use polars_lazy::prelude::*;
 // This demo is a minimal version of https://github.com/Desdaemon/polars_dart
 // Refer to that repository for more details (though may have not migrated to V2 yet)
 
+// Thanks for the `iris` dataset
+static SAMPLE_DATASET: &'static str = include_str!("../ignore_me/iris.csv");
+
 pub fn hello_polars() -> String {
     let df: DataFrame = df!(
         "integer" => &[1, 2, 3, 4, 5],
