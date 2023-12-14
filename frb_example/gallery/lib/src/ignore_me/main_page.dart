bce51078eeef9cf0a9ee2e53ea076af4676dd559
fzyzcjy <ch271828n@outlook.com>
2023-12-14 09:12:14 +0800

    feat: bg white

--- frb_example/gallery/lib/src/ignore_me/main_page.dart
+++ frb_example/gallery/lib/src/ignore_me/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frb_example_gallery/src/rust/api/simple.dart';
 
 class MainPageWidget extends StatelessWidget {
   const MainPageWidget({super.key});
@@ -6,6 +7,11 @@ class MainPageWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        colorScheme: const ColorScheme.light(
+          background: Colors.white,
+        ),
+      ),
       home: Scaffold(
         appBar: AppBar(title: const Text('flutter_rust_bridge Gallery')),
         body: Center(
