1d6453aa15f295be772cef02be2664011ddc66b5
fzyzcjy <ch271828n@outlook.com>
2023-12-14 14:27:45 +0800

    chore: copy io side sign

--- frb_rust/src/rust_async/web.rs
+++ frb_rust/src/rust_async/web.rs
@@ -19,3 +19,19 @@ impl BaseAsyncRuntime for SimpleAsyncRuntime {
         spawn_local(future)
     }
 }
+
+pub fn spawn<F>(future: F) -> JoinHandle<F::Output>
+where
+    F: Future + Send + 'static,
+    F::Output: Send + 'static,
+{
+    todo!()
+}
+
+pub fn spawn_blocking<F, R>(f: F) -> JoinHandle<R>
+where
+    F: FnOnce() -> R + Send + 'static,
+    R: Send + 'static,
+{
+    todo!()
+}
