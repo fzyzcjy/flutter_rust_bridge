737ba583008afbc0138fc568a292bb6943137e43
fzyzcjy <ch271828n@outlook.com>
2023-12-16 20:30:36 +0800

    Revert "feat: empty AutoDropDartPersistentHandleBox"
    This reverts commit 2c266637e578914216b470d13609ad0f8664267b.

--- frb_rust/src/dart_opaque/boxes/auto_drop_dart_persistent_handle_box.rs
+++
@@ -1,5 +0,0 @@
-use crate::dart_opaque::auto_drop_dart_persistent_handle::AutoDropDartPersistentHandle;
-use crate::dart_opaque::boxes::dart_isolate_box::DartIsolateBox;
-
-#[derive(Debug)]
-pub(crate) struct AutoDropDartPersistentHandleBox(DartIsolateBox<AutoDropDartPersistentHandle>);
