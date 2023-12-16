01aaf63bfc65b1a686c926e90f00289f5eb59a6b
fzyzcjy <ch271828n@outlook.com>
2023-12-16 19:58:17 +0800

    chore: more dbg log

--- frb_rust/src/dart_opaque/rust2dart.rs
+++ frb_rust/src/dart_opaque/rust2dart.rs
@@ -31,14 +31,16 @@ pub unsafe extern "C" fn dart_opaque_rust2dart_decode(ptr: usize) -> Generalized
 unsafe fn dart_opaque_rust2dart_decode_inner(ptr: usize) -> GeneralizedDartHandle {
     let opaque = DartOpaque::from_raw(ptr as _);
     println!(
-        "hi dart_opaque_rust2dart_decode_inner BEFORE ptr={ptr} thread_id={:?} backtrace={:?}",
+        "hi dart_opaque_rust2dart_decode_inner BEFORE ptr={ptr} thread_id={:?} Dart_CurrentIsolate={:?} backtrace={:?}",
         std::thread::current().id(),
+        dart_sys::Dart_CurrentIsolate_DL.unwrap()(),
         std::backtrace::Backtrace::force_capture(),
     );
     let ans = opaque.create_dart_handle();
     println!(
-        "hi dart_opaque_rust2dart_decode_inner AFTER ptr={ptr} ans={ans:?} thread_id={:?} backtrace={:?}",
+        "hi dart_opaque_rust2dart_decode_inner AFTER ptr={ptr} ans={ans:?} thread_id={:?} Dart_CurrentIsolate={:?} backtrace={:?}",
         std::thread::current().id(),
+        dart_sys::Dart_CurrentIsolate_DL.unwrap()(),
         std::backtrace::Backtrace::force_capture(),
     );
     ans
