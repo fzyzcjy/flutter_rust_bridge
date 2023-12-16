ced392759618706700264b5a3dd65896130b9bca
fzyzcjy <ch271828n@outlook.com>
2023-12-16 16:33:13 +0800

    feat: impl ondata

--- frb_dart/lib/src/main_components/port_manager.dart
+++ frb_dart/lib/src/main_components/port_manager.dart
@@ -1,8 +1,5 @@
-import 'package:flutter_rust_bridge/src/dart_opaque/dart_opaque.dart';
 import 'package:flutter_rust_bridge/src/generalized_frb_rust_binding/generalized_frb_rust_binding.dart';
 import 'package:flutter_rust_bridge/src/main_components/handler.dart';
-import 'package:flutter_rust_bridge/src/misc/dart_fn.dart';
-import 'package:flutter_rust_bridge/src/platform_types/platform_types.dart';
 import 'package:flutter_rust_bridge/src/utils/base_lazy_port_manager.dart';
 
 /// {@macro flutter_rust_bridge.internal}
@@ -14,12 +11,23 @@ class DartHandlerPortManager extends BaseLazyPortManager {
   DartHandlerPortManager(this._generalizedFrbRustBinding, this._handler);
 
   @override
-  void onData(Object? message) {
-    TODO;
-
-    // TODO drop port
-    // _generalizedFrbRustBinding.dartOpaqueDropThreadBoxPersistentHandle(message)
-
-    // TODO invoke port
+  void onData(covariant List<dynamic> message) {
+    switch (message[0]) {
+      case _DartHandlerPortAction.dartOpaqueDrop:
+        assert(message.length == 2);
+        _generalizedFrbRustBinding
+            .dartOpaqueDropThreadBoxPersistentHandle(message[1]);
+      case _DartHandlerPortAction.dartFnInvoke:
+        _handler.dartFnInvoke(message.sublist(1), _generalizedFrbRustBinding);
+      default:
+        throw UnimplementedError('Unsupported message: $message');
+    }
   }
+}
+
+/// NOTE: Please keep in sync with the Rust side
+class _DartHandlerPortAction {
+  // Do not use enum, but use raw integers, to avoid extra overhead
+  static const dartOpaqueDrop = 0;
+  static const dartFnInvoke = 1;
 }
