a443c345735d4abe719a0868433e2e789b2800f9
fzyzcjy <ch271828n@outlook.com>
2023-12-16 09:39:31 +0800

    refactor: extract

--- frb_example/gallery/lib/src/examples/mandelbrot.dart
+++ frb_example/gallery/lib/src/examples/mandelbrot.dart
@@ -54,111 +54,17 @@ class _MandelbrotPageBodyState extends State<MandelbrotPageBody> {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          child: Align(
-            alignment: Alignment.centerRight,
-            child: Padding(
-              padding: const EdgeInsets.only(right: 64),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  _buildSlider(
-                    label: 'Num threads',
-                    value: numThreads.toDouble(),
-                    onChanged: (newValue) =>
-                        setState(() => numThreads = newValue.round()),
-                    min: 1,
-                    max: 4,
-                    divisions: 3,
-                  ),
-                  _buildSlider(
-                    label: 'Image size',
-                    value: size,
-                    onChanged: (newValue) => setState(() => size = newValue),
-                    min: 100,
-                    max: 1000,
-                  ),
-                  const SizedBox(height: 32),
-                  Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      TextButton(onPressed: start, child: const Text('Start')),
-                      const SizedBox(width: 32),
-                      TextButton(onPressed: stop, child: const Text('Stop')),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-        Expanded(
-          child: Align(
-            alignment: Alignment.centerLeft,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                if (computeTime != null)
-                  Text('Time: ${computeTime!.inMilliseconds}ms'),
-                const SizedBox(height: 8),
-                SizedBox.square(
-                  dimension: size,
-                  child: image != null
-                      ? AnimatedReplaceableImage(
-                          image: MemoryImage(image!),
-                        )
-                      : Container(
-                          color: Colors.grey.shade100,
-                          padding: const EdgeInsets.all(16),
-                          child: const Center(
-                            child: Text(
-                              'Use buttons on the left to start animation',
-                              style: TextStyle(color: Colors.grey),
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                        ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget _buildSlider({
-    required String label,
-    required double value,
-    required ValueChanged<double>? onChanged,
-    required double min,
-    required double max,
-    int? divisions,
-  }) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        const SizedBox(width: 32),
-        SizedBox(
-          width: 100,
-          child: Text(label),
-        ),
-        SizedBox(
-          width: 200,
-          child: Slider(
-            value: value,
-            onChanged: onChanged,
-            min: min,
-            max: max,
-            divisions: divisions,
-            label: value.round().toString(),
-            inactiveColor: Colors.blue.shade100,
-          ),
-        ),
-      ],
+    // Since we are not providing a demo to teach Flutter itself,
+    // how the detailed UI is constructed is unrelated
+    return MandelbrotPageUI(
+      numThreads: numThreads,
+      setNumThreads: (value) => setState(() => numThreads = value),
+      size: size,
+      setSize: (value) => setState(() => size = value),
+      image: image,
+      computeTime: computeTime,
+      start: start,
+      stop: stop,
     );
   }
 }
--- frb_example/gallery/lib/src/ignore_me/mandelbrot_related.dart
+++ frb_example/gallery/lib/src/ignore_me/mandelbrot_related.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:frb_example_gallery/src/rust/api/mandelbrot.dart';
 
@@ -85,5 +87,138 @@ class SimpleRunner {
 
   void dispose() {
     _running = false;
+  }
+}
+
+class MandelbrotPageUI extends StatelessWidget {
+  final int numThreads;
+  final void Function(int value) setNumThreads;
+  final double size;
+  final void Function(double value) setSize;
+  final Uint8List? image;
+  final Duration? computeTime;
+  final void Function() start;
+  final void Function() stop;
+
+  const MandelbrotPageUI({
+    super.key,
+    required this.numThreads,
+    required this.setNumThreads,
+    required this.size,
+    required this.setSize,
+    required this.image,
+    required this.computeTime,
+    required this.start,
+    required this.stop,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Align(
+            alignment: Alignment.centerRight,
+            child: Padding(
+              padding: const EdgeInsets.only(right: 64),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  _buildSlider(
+                    label: 'Num threads',
+                    value: numThreads.toDouble(),
+                    onChanged: (newValue) => setNumThreads(newValue.round()),
+                    min: 1,
+                    max: 4,
+                    divisions: 3,
+                  ),
+                  _buildSlider(
+                    label: 'Image size',
+                    value: size,
+                    onChanged: setSize,
+                    min: 100,
+                    max: 1000,
+                  ),
+                  const SizedBox(height: 32),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      TextButton(onPressed: start, child: const Text('Start')),
+                      const SizedBox(width: 32),
+                      TextButton(onPressed: stop, child: const Text('Stop')),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                if (computeTime != null)
+                  Text('Time: ${computeTime!.inMilliseconds}ms'),
+                const SizedBox(height: 8),
+                SizedBox.square(
+                  dimension: size,
+                  child: image != null
+                      ? AnimatedReplaceableImage(
+                          image: MemoryImage(image!),
+                        )
+                      : Container(
+                          color: Colors.grey.shade100,
+                          padding: const EdgeInsets.all(16),
+                          child: const Center(
+                            child: Text(
+                              'Use buttons on the left to start animation',
+                              style: TextStyle(color: Colors.grey),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+    return const Placeholder();
+  }
+
+  Widget _buildSlider({
+    required String label,
+    required double value,
+    required ValueChanged<double>? onChanged,
+    required double min,
+    required double max,
+    int? divisions,
+  }) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        const SizedBox(width: 32),
+        SizedBox(
+          width: 100,
+          child: Text(label),
+        ),
+        SizedBox(
+          width: 200,
+          child: Slider(
+            value: value,
+            onChanged: onChanged,
+            min: min,
+            max: max,
+            divisions: divisions,
+            label: value.round().toString(),
+            inactiveColor: Colors.blue.shade100,
+          ),
+        ),
+      ],
+    );
   }
 }
