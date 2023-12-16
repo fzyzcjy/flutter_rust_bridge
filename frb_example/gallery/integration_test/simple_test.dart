9f279e75bec53428a5af04f87de8a3fc5f25e6a4
fzyzcjy <ch271828n@outlook.com>
2023-12-16 10:12:22 +0800

    feat: simple test

--- frb_example/gallery/integration_test/simple_test.dart
+++ frb_example/gallery/integration_test/simple_test.dart
@@ -1,13 +1,22 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:frb_example_gallery/main.dart';
+import 'package:frb_example_gallery/src/examples/mandelbrot.dart';
+import 'package:frb_example_gallery/src/examples/polars.dart';
 import 'package:frb_example_gallery/src/rust/frb_generated.dart';
 import 'package:integration_test/integration_test.dart';
 
 Future<void> main() async {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   setUpAll(() async => await RustLib.init());
-  testWidgets('Can call rust function', (WidgetTester tester) async {
-    await tester.pumpWidget(MyApp());
-    expect(find.textContaining('Result: `Hello, Tom!`'), findsOneWidget);
-  });
+  for (final (pageName, pageFinder) in [
+    ('Mandelbrot', find.byType(MandelbrotPageBody)),
+    ('Polars', find.byType(PolarsPageBody)),
+  ]) {
+    testWidgets('$pageName page', (WidgetTester tester) async {
+      await tester.pumpWidget(const MyApp());
+      await tester.tap(find.text(pageName));
+      await tester.pumpAndSettle();
+      expect(pageFinder, findsOneWidget);
+    });
+  }
 }
