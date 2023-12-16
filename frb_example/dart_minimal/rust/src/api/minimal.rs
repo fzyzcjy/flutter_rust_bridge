589ce9f43a88ce6fd5a4d422814d225e8173eae7
fzyzcjy <ch271828n@outlook.com>
2023-12-16 17:16:39 +0800

    chore: rm temp code

--- frb_example/dart_minimal/rust/src/api/minimal.rs
+++ frb_example/dart_minimal/rust/src/api/minimal.rs
@@ -1,16 +1,5 @@
-use flutter_rust_bridge::DartFnFuture;
 pub use std::panic::{RefUnwindSafe, UnwindSafe};
 
 pub fn minimal_adder(a: i32, b: i32) -> i32 {
     a + b
-}
-
-// TODO temp
-pub async fn hi_1(callback: impl Fn() -> DartFnFuture<()> + UnwindSafe) {
-    println!("rust_call_dart_simple before");
-    callback().await;
-    println!("rust_call_dart_simple after");
-}
-pub fn hi_2(opaque: flutter_rust_bridge::DartOpaque) -> flutter_rust_bridge::DartOpaque {
-    opaque
 }
