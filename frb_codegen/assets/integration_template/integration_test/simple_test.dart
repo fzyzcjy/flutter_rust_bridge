c31c85c86a0c0669c6e647dc6cce0cd70f336b8a
fzyzcjy <ch271828n@outlook.com>
2023-12-17 07:58:44 +0800

    fix: binding

--- frb_codegen/assets/integration_template/integration_test/simple_test.dart
+++ frb_codegen/assets/integration_template/integration_test/simple_test.dart
@@ -1,10 +1,11 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:REPLACE_ME_PACKAGE_NAME/main.dart';
 import 'package:REPLACE_ME_PACKAGE_NAME/src/rust/frb_generated.dart';
 import 'package:integration_test/integration_test.dart';
 
 void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   setUpAll(() async => await RustLib.init());
   testWidgets('Can call rust function', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
--- frb_example/flutter_via_create/integration_test/simple_test.dart
+++ frb_example/flutter_via_create/integration_test/simple_test.dart
@@ -1,10 +1,11 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_via_create/main.dart';
 import 'package:flutter_via_create/src/rust/frb_generated.dart';
 import 'package:integration_test/integration_test.dart';
 
 void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   setUpAll(() async => await RustLib.init());
   testWidgets('Can call rust function', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
