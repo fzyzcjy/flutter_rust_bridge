d37dd01aad3caf286c7074cb2005cf0c08061da1
fzyzcjy <ch271828n@outlook.com>
2023-12-16 11:57:10 +0800

    chore: log

--- frb_codegen/src/library/codegen/ir/ty/structure.rs
+++ frb_codegen/src/library/codegen/ir/ty/structure.rs
@@ -27,7 +27,8 @@ pub struct IrStruct {
 
 impl IrTypeStructRef {
     pub fn get<'a>(&self, ir_context: &'a impl IrContext) -> &'a IrStruct {
-        &ir_context.struct_pool()[&self.ident]
+        &(ir_context.struct_pool().get(&self.ident))
+            .unwrap_or_else(|| panic!("no entry found for key={:?}", self.ident))
     }
 }
 
