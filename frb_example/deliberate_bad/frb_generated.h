d8390f7af70b89fff25f12d62e6c14d3ad102a1b
fzyzcjy <ch271828n@outlook.com>
2023-12-16 10:26:34 +0800

    chore: codegen

--- frb_example/deliberate_bad/frb_generated.h
+++ frb_example/deliberate_bad/frb_generated.h
@@ -1,8 +1,21 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
-typedef struct DartCObject *WireSyncReturn;
+// EXTRA BEGIN
+typedef struct DartCObject *WireSyncRust2DartDco;
+typedef struct WireSyncRust2DartSse {
+  uint8_t *ptr;
+  int32_t len;
+} WireSyncRust2DartSse;
+// EXTRA END
 typedef struct _Dart_Handle* Dart_Handle;
+
+void frb_initialize_rust(int64_t dart_opaque_drop_port, int64_t dart_fn_invoke_port);
+
+void dart_fn_deliver_output(int32_t call_id,
+                            uint8_t *ptr_,
+                            int32_t rust_vec_len_,
+                            int32_t data_len_);
 
 void wire_make_data_race(int64_t port_);
 
@@ -15,7 +28,9 @@ void wire_make_stack_buffer_overflow(int64_t port_);
 void wire_make_use_of_uninitialized_value(int64_t port_);
 static int64_t dummy_method_to_enforce_bundling(void) {
     int64_t dummy_var = 0;
+    dummy_var ^= ((int64_t) (void*) dart_fn_deliver_output);
     dummy_var ^= ((int64_t) (void*) drop_dart_object);
+    dummy_var ^= ((int64_t) (void*) frb_initialize_rust);
     dummy_var ^= ((int64_t) (void*) get_dart_object);
     dummy_var ^= ((int64_t) (void*) new_dart_opaque);
     dummy_var ^= ((int64_t) (void*) store_dart_post_cobject);
