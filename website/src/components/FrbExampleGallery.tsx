2d5dc4aae490b3a7c2f9d9e544fcc5899993d220
fzyzcjy <ch271828n@outlook.com>
2023-12-18 16:39:34 +0800

    Revert "feat: flutter load"
    This reverts commit e1c0fa175c6f479a27dbad7558e49ebd92c4fa60.

--- website/src/components/FrbExampleGallery.tsx
+++ website/src/components/FrbExampleGallery.tsx
@@ -2,24 +2,6 @@ import React from 'react';
 
 // noinspection JSUnusedGlobalSymbols
 export default class FrbExampleGallery extends React.Component {
-    componentDidMount() {
-        console.log('FrbExampleGallery componentDidMount');
-
-        window.addEventListener("load", function (ev) {
-            console.log('FrbExampleGallery call flutter-loadEntrypoint');
-            _flutter.loader.loadEntrypoint({
-                onEntrypointLoaded: async function (engineInitializer) {
-                    console.log('FrbExampleGallery call flutter-appRunner');
-                    let appRunner = await engineInitializer.initializeEngine({
-                        // Pass a reference to "div#flutter_host" into the Flutter engine.
-                        hostElement: document.querySelector("#flutter_host")
-                    });
-                    await appRunner.runApp();
-                }
-            });
-        });
-    }
-
     render() {
         return (
             <div id="flutter_host" style={{height: '500px', border: '1px solid #ccc'}}>Loading Flutter web app...</div>
