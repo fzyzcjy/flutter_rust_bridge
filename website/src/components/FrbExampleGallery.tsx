45bdbf7f9302b84af994b6016b49a58789811ede
fzyzcjy <ch271828n@outlook.com>
2023-12-18 16:42:58 +0800

    refactor: extract to further mimic ordering

--- website/src/components/FrbExampleGallery.tsx
+++ website/src/components/FrbExampleGallery.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 
 // noinspection JSUnusedGlobalSymbols
-export default class FrbExampleGallery extends React.Component {
+export default function FrbExampleGallery() {
+    return (
+        <>
+            <script src="enable-threads.js"></script>
+            <script src="flutter.js" defer></script>
+            <div id="flutter_host" style={{height: '500px', border: '1px solid #ccc'}}>Loading Flutter web app...
+            </div>
+            <FlutterInitializer/>
+        </>
+    )
+}
+
+class FlutterInitializer extends React.Component {
     componentDidMount() {
-        console.log('FrbExampleGallery componentDidMount');
+        console.log('FrbExampleGallery.FlutterInitializer componentDidMount');
 
         window.addEventListener("load", function (ev) {
-            console.log('FrbExampleGallery call flutter-loadEntrypoint');
+            console.log('FrbExampleGallery.FlutterInitializer call flutter-loadEntrypoint');
             _flutter.loader.loadEntrypoint({
                 onEntrypointLoaded: async function (engineInitializer) {
-                    console.log('FrbExampleGallery call flutter-appRunner');
+                    console.log('FrbExampleGallery.FlutterInitializer call flutter-appRunner');
                     let appRunner = await engineInitializer.initializeEngine({
                         // Pass a reference to "div#flutter_host" into the Flutter engine.
                         hostElement: document.querySelector("#flutter_host")
@@ -21,13 +33,7 @@ export default class FrbExampleGallery extends React.Component {
     }
 
     render() {
-        return (
-            <>
-                <script src="enable-threads.js"></script>
-                <script src="flutter.js" defer></script>
-                <div id="flutter_host" style={{height: '500px', border: '1px solid #ccc'}}>Loading Flutter web app...
-                </div>
-            </>
-        )
+        return <span></span>
     }
 }
+
