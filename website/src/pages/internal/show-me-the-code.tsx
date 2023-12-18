77d3ad1913edf24d1eb66d792af60f7aa0c58641
fzyzcjy <ch271828n@outlook.com>
2023-12-18 10:23:34 +0800

    feat: hack

--- website/src/pages/internal/show-me-the-code.tsx
+++ website/src/pages/internal/show-me-the-code.tsx
@@ -5,17 +5,23 @@ import ShowMeTheCode from "@site/src/components/ShowMeTheCode";
 /**
  * How to take screenshot:
  * 1. Follow https://davidaugustat.com/web/take-ultra-high-resolution-screenshots-in-chrome
- *    to create a super large device (e.g. 7900x6950)
+ *    to create a super large device (e.g. 3950x3475)
  * 2. Chrome's "capture screenshot"
  */
 export default class Page extends React.Component {
+    componentDidMount() {
+        setInterval(() => {
+            document.body.style.overflow = "hidden";
+        }, 1000);
+    }
+
     render() {
         return (
             <div className="my-hide-nav">
                 <Layout>
                     <div style={{
                         padding: '32px',
-                        scale: '8.0',
+                        scale: '4.0',
                         transformOrigin: 'top left',
                     }}>
                         <ShowMeTheCode/>
