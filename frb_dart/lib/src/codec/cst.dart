02c694fc523f980c0f37c9db81efd931a8a56cea
fzyzcjy <ch271828n@outlook.com>
2023-12-18 21:55:42 +0800

    chore: try coverage:ignore

--- frb_dart/lib/src/codec/cst.dart
+++ frb_dart/lib/src/codec/cst.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_rust_bridge/src/codec/base.dart';
 import 'package:flutter_rust_bridge/src/generalized_frb_rust_binding/generalized_frb_rust_binding.dart';
 
+// This is nothing but "unreachable", so by definition it will not be covered
+// coverage:ignore-start
 /// {@macro flutter_rust_bridge.only_for_generated_code}
 class CstCodec<S, E extends Object> extends BaseCodec<S, E, dynamic> {
   @override
@@ -14,3 +16,4 @@ class CstCodec<S, E extends Object> extends BaseCodec<S, E, dynamic> {
   @override
   S decodeWireSyncType(dynamic raw) => throw UnimplementedError('unreachable');
 }
+// coverage:ignore-end
