ded4bd6b46e21ffc0c18ebb0b74b29ee63b0d0d8
fzyzcjy <ch271828n@outlook.com>
2023-12-18 22:21:24 +0800

    refactor: migrate release

--- tools/frb_internal/lib/src/makefile_dart/release.dart
+++ tools/frb_internal/lib/src/makefile_dart/release.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: avoid_print
 
 import 'package:args/command_runner.dart';
+import 'package:flutter_rust_bridge_internal/src/makefile_dart/consts.dart';
 import 'package:flutter_rust_bridge_internal/src/utils/makefile_dart_infra.dart';
 
 List<Command<void>> createCommands() {
@@ -9,6 +10,47 @@ List<Command<void>> createCommands() {
   ];
 }
 
+class _VersionInfo {
+  final String oldVersion;
+  final String newVersion;
+
+  const _VersionInfo({required this.oldVersion, required this.newVersion});
+}
+
 Future<void> release() async {
+  final versionInfo = _computeVersionInfo();
+  await _releaseUpdateVersion(versionInfo);
+  await _releaseUpdateScoop();
+  await _releaseUpdateGit(versionInfo);
+  await _releaseUpdateGitHub(versionInfo);
+  await _releasePublishAll();
+}
+
+_VersionInfo _computeVersionInfo() {
+  return TODO;
+}
+
+Future<void> _releaseUpdateVersion(_VersionInfo versionInfo) async {
   TODO;
+}
+
+Future<void> _releaseUpdateScoop() async {
+  await exec(
+      'cd frb_codegen && ./contrib/scoop.json.sh > ./contrib/flutter_rust_bridge_codegen.json');
+}
+
+Future<void> _releaseUpdateGit(_VersionInfo versionInfo) async {
+  TODO;
+}
+
+Future<void> _releaseUpdateGitHub(_VersionInfo versionInfo) async {
+  TODO;
+}
+
+Future<void> _releasePublishAll() async {
+  await exec('cd frb_codegen && cargo publish');
+  await exec('cd frb_rust && cargo publish');
+  await exec('cd frb_macros && cargo publish');
+  await exec(
+      'cd frb_dart && flutter pub publish --force --server=https://pub.dartlang.org');
 }
