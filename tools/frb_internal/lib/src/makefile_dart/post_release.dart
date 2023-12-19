c579ede846cb67eaca81a82388ddaf177467e43a
fzyzcjy <ch271828n@outlook.com>
2023-12-19 10:28:09 +0800

    feat: install

--- tools/frb_internal/lib/src/makefile_dart/post_release.dart
+++ tools/frb_internal/lib/src/makefile_dart/post_release.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: avoid_print
 
 import 'package:args/command_runner.dart';
+import 'package:flutter_rust_bridge_internal/src/makefile_dart/consts.dart';
 import 'package:flutter_rust_bridge_internal/src/utils/makefile_dart_infra.dart';
 
 List<Command<void>> createCommands() {
@@ -10,5 +11,27 @@ List<Command<void>> createCommands() {
 }
 
 Future<void> postReleaseMimicQuickstart() async {
-  // TODO
+  await _stepInstall(config.installMode);
+}
+
+enum InstallMode {
+  cargoInstall,
+  cargoBinstall,
+  scoop,
+  homebrew,
+}
+
+Future<void> _stepInstall(InstallMode mode) async {
+  switch (mode) {
+    case InstallMode.cargoInstall:
+      await exec('cargo install flutter_rust_bridge_codegen');
+    case InstallMode.cargoBinstall:
+      await exec('cargo binstall flutter_rust_bridge_codegen');
+    case InstallMode.scoop:
+      await exec(
+          'scoop bucket add frb https://github.com/Desdaemon/scoop-repo');
+      await exec('scoop install flutter_rust_bridge_codegen');
+    case InstallMode.homebrew:
+      await exec('brew install desdaemon/repo/flutter_rust_bridge_codegen');
+  }
 }
