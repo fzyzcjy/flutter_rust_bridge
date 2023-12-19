e39c39b13bef2ea24f420291e007a17f0ddde087
fzyzcjy <ch271828n@outlook.com>
2023-12-19 13:38:04 +0800

    feat: trailingSlash=true

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
  
     // https://docusaurus.io/docs/api/docusaurus-config#trailingSlash
     // https://github.com/facebook/docusaurus/issues/3372
-    trailingSlash: false,
+    trailingSlash: true,
 
     // Even if you don't use internalization, you can use this field to set useful
     // metadata like html lang. For example, if your site is Chinese, you may want
