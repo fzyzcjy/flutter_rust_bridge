ad105ac747880d210cb50a23049a9b38974f58ea
fzyzcjy <ch271828n@outlook.com>
2023-12-20 09:24:48 +0800

    chore: ignore unused

--- website/src/theme/NotFound.js
+++ website/src/theme/NotFound.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Translate, {translate} from '@docusaurus/Translate';
 import {PageMetadata} from '@docusaurus/theme-common';
 import Layout from '@theme/Layout';
+
+// noinspection JSUnusedGlobalSymbols
 export default function NotFound() {
   return (
     <>
