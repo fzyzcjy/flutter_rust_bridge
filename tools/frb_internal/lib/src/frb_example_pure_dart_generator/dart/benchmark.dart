a16d0dd4b78db6873d7ce54feae4b4647e019172
fzyzcjy <ch271828n@outlook.com>
2023-12-22 14:25:31 +0800

    chore: empty

--- tools/frb_internal/lib/src/frb_example_pure_dart_generator/dart/benchmark.dart
+++ tools/frb_internal/lib/src/frb_example_pure_dart_generator/dart/benchmark.dart
@@ -1,3 +1,32 @@
 String generateBenchmark() {
-  return TODO;
+  return '''
+${_benchmarkVoidFunction()}
+${_benchmarkBytes()}
+${_benchmarkBinaryTree()}
+${_benchmarkBlob()}
+  ''';
+}
+
+String _benchmarkVoidFunction() {
+  return '''
+  $TODO
+  ''';
+}
+
+String _benchmarkBytes() {
+  return '''
+  $TODO
+  ''';
+}
+
+String _benchmarkBinaryTree() {
+  return '''
+  $TODO
+  ''';
+}
+
+String _benchmarkBlob() {
+  return '''
+  $TODO
+  ''';
 }
