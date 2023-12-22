4163ee2ca88e4cde4d80afb6844ff28445e514ec
fzyzcjy <ch271828n@outlook.com>
2023-12-22 21:18:27 +0800

    chore: codegen

--- tools/frb_internal/lib/src/makefile_dart/bench.g.dart
+++ tools/frb_internal/lib/src/makefile_dart/bench.g.dart
@@ -23,3 +23,40 @@ BenchConfig parseBenchConfig(List<String> args) {
   final result = _$parserForBenchConfig.parse(args);
   return _$parseBenchConfigResult(result);
 }
+
+T _$badNumberFormat<T extends num>(
+  String source,
+  String type,
+  String argName,
+) =>
+    throw FormatException(
+      'Cannot parse "$source" into `$type` for option "$argName".',
+    );
+
+BenchFlamegraphRunConfig _$parseBenchFlamegraphRunConfigResult(
+        ArgResults result) =>
+    BenchFlamegraphRunConfig(
+      filter: result['filter'] as String,
+      loopCount: int.tryParse(result['loop-count'] as String) ??
+          _$badNumberFormat(
+            result['loop-count'] as String,
+            'int',
+            'loop-count',
+          ),
+    );
+
+ArgParser _$populateBenchFlamegraphRunConfigParser(ArgParser parser) => parser
+  ..addOption(
+    'filter',
+  )
+  ..addOption(
+    'loop-count',
+  );
+
+final _$parserForBenchFlamegraphRunConfig =
+    _$populateBenchFlamegraphRunConfigParser(ArgParser());
+
+BenchFlamegraphRunConfig parseBenchFlamegraphRunConfig(List<String> args) {
+  final result = _$parserForBenchFlamegraphRunConfig.parse(args);
+  return _$parseBenchFlamegraphRunConfigResult(result);
+}
