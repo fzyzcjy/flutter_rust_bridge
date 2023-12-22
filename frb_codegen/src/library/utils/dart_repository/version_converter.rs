ea33232d095702187f8f5f3d338c345ba8c260c5
fzyzcjy <ch271828n@outlook.com>
2023-12-22 09:00:39 +0800

    feat: UT about version

--- frb_codegen/src/library/utils/dart_repository/version_converter.rs
+++ frb_codegen/src/library/utils/dart_repository/version_converter.rs
@@ -4,7 +4,7 @@ use anyhow::bail;
 use cargo_metadata::{Version, VersionReq};
 use std::convert::TryFrom;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct CargoDependencyVersion(String);
 
 impl From<&DartDependencyVersion> for CargoDependencyVersion {
@@ -74,5 +74,28 @@ impl TryFrom<&CargoDependencyVersion> for DartPackageVersion {
             let version = Version::parse(&s.0)?;
             Ok(DartPackageVersion::Exact(version))
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_dart_dependency_version_to_cargo_dependency_version() {
+        for (dart, cargo) in [("^1.2.3", "1.2.3"), ("^0.2.3", "0.2")] {
+            assert_eq!(
+                CargoDependencyVersion::from(&DartDependencyVersion(dart.to_owned())),
+                CargoDependencyVersion(cargo.to_owned())
+            );
+        }
+    }
+
+    #[test]
+    fn test_cargo_dependency_version_to_dart_package_version() {
+        assert_eq!(
+            DartPackageVersion::try_from(&CargoDependencyVersion(">=1.2.3".to_owned())).unwrap(),
+            DartPackageVersion::Range(VersionReq::parse(">=1.2.3").unwrap())
+        );
     }
 }
