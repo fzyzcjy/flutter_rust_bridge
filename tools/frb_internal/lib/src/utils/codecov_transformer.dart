0e85a623014a6b9a7a029620a1606b8e98ddc3e9
fzyzcjy <ch271828n@outlook.com>
2023-12-22 10:08:29 +0800

    feat: impl _transformFile

--- tools/frb_internal/lib/src/utils/codecov_transformer.dart
+++ tools/frb_internal/lib/src/utils/codecov_transformer.dart
@@ -1,3 +1,7 @@
+// ignore_for_file: avoid_print
+
+import 'dart:io';
+
 // format: https://docs.codecov.com/docs/codecov-custom-coverage-format
 Future<Map<String, dynamic>> transformCodecovReport(Map<String, dynamic> raw) async {
   return {
@@ -6,6 +10,34 @@ Future<Map<String, dynamic>> transformCodecovReport(Map<String, dynamic> raw) as
   };
 }
 
-Map<String, dynamic> _transformFile(String filename, Map<String, dynamic> data) {
-  return TODO;
+Map<String, dynamic> _transformFile(String filename, Map<String, dynamic> srcData) {
+  final ansData = {...srcData};
+  final fileLines = File(filename).readAsStringSync().split('\n');
+
+  var ignoring = false;
+  var removeCount = 0;
+  for (var i = 0; i < fileLines.length; ++i) {
+    final lineContent = fileLines[i];
+    final lineNumber = i + 1;
+
+    if (lineContent.contains('frb-coverage:ignore-start')) {
+      ignoring = true;
+    }
+    if (lineContent.contains('frb-coverage:ignore-end')) {
+      ignoring = false;
+    }
+
+    if (ignoring) {
+      final removed = ansData.remove(lineNumber.toString());
+      if (removed != null) {
+        removeCount++;
+      }
+    }
+  }
+
+  if (removeCount > 0) {
+    print('transformCodecovReport remove $removeCount lines from $filename');
+  }
+
+  return ansData;
 }
