e9f64cd7c8a0edc1369f1ef8ffc6674191254aa7
fzyzcjy <ch271828n@outlook.com>
2023-12-23 09:44:28 +0800

    chore: rename

--- frb_example/pure_dart/test/init_and_dispose_test.dart
+++ frb_example/pure_dart/test/init_and_dispose_test.dart
@@ -3,9 +3,11 @@ import 'package:test/test.dart';
 
 Future<void> main() async {
   await RustLib.init();
+
   tearDownAll(() {
     // This is the main thing to test in this file
     RustLib.dispose();
   });
+
   test('dummy (the main thing is not this test, but the dispose)', () {});
 }
