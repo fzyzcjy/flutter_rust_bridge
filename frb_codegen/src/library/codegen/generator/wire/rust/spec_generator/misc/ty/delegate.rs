1f7db8f084873c91d35969f95f92f15c2ac65057
fzyzcjy <ch271828n@outlook.com>
2023-12-23 21:09:24 +0800

    feat: more

--- frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/misc/ty/delegate.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/misc/ty/delegate.rs
@@ -3,7 +3,10 @@ use crate::codegen::generator::wire::rust::spec_generator::misc::ty::WireRustGen
 use crate::codegen::ir::ty::delegate::IrTypeDelegate;
 
 impl<'a> WireRustGeneratorMiscTrait for DelegateWireRustGenerator<'a> {
+    // the function signature is not covered while the whole body is covered - looks like a bug in coverage tool
+    // frb-coverage:ignore-start
     fn wrapper_struct_name(&self) -> Option<String> {
+        // frb-coverage:ignore-end
         if let IrTypeDelegate::PrimitiveEnum(enu) = &self.ir {
             WireRustGenerator::new(enu.ir.clone(), self.context).wrapper_struct_name()
         } else {
