4b037ef9a8e7be3f3a2d8a9608976fcac9b86ae0
fzyzcjy <ch271828n@outlook.com>
2023-12-23 09:07:16 +0800

    refactor: simplify code based on codecov hints

--- frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/codec/dco/encoder/ty/delegate.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/codec/dco/encoder/ty/delegate.rs
@@ -1,9 +1,10 @@
 use crate::codegen::generator::wire::rust::spec_generator::codec::dco::base::*;
 use crate::codegen::generator::wire::rust::spec_generator::codec::dco::encoder::misc::generate_impl_into_into_dart;
-use crate::codegen::generator::wire::rust::spec_generator::codec::dco::encoder::ty::enumeration::parse_wrapper_name_into_dart_name_and_self_path;
+use crate::codegen::generator::wire::rust::spec_generator::codec::dco::encoder::ty::enumeration::{
+    generate_enum_access_object_core, parse_wrapper_name_into_dart_name_and_self_path,
+};
 use crate::codegen::generator::wire::rust::spec_generator::codec::dco::encoder::ty::WireRustCodecDcoGeneratorEncoderTrait;
 use crate::codegen::ir::ty::delegate::{IrTypeDelegate, IrTypeDelegatePrimitiveEnum};
-use crate::codegen::ir::ty::IrType::EnumRef;
 use itertools::Itertools;
 
 impl<'a> WireRustCodecDcoGeneratorEncoderTrait for DelegateWireRustCodecDcoGenerator<'a> {
@@ -16,7 +17,7 @@ impl<'a> WireRustCodecDcoGeneratorEncoderTrait for DelegateWireRustCodecDcoGener
             let (name, self_path) =
                 parse_wrapper_name_into_dart_name_and_self_path(&src.name, &src.wrapper_name);
 
-            let self_ref = self.generate_access_object_core("self".to_owned());
+            let self_ref = generate_enum_access_object_core(ir, "self".to_owned(), self.context);
             let variants = src
                 .variants()
                 .iter()
@@ -39,17 +40,5 @@ impl<'a> WireRustCodecDcoGeneratorEncoderTrait for DelegateWireRustCodecDcoGener
             ));
         }
         None
-    }
-
-    // the function signature is not covered while the whole body is covered - looks like a bug in coverage tool
-    // frb-coverage:ignore-start
-    fn generate_access_object_core(&self, obj: String) -> String {
-        // frb-coverage:ignore-end
-        if let IrTypeDelegate::PrimitiveEnum(IrTypeDelegatePrimitiveEnum { ir, .. }) = &self.ir {
-            WireRustCodecDcoGenerator::new(EnumRef(ir.clone()), self.context)
-                .generate_access_object_core(obj)
-        } else {
-            obj
-        }
     }
 }
