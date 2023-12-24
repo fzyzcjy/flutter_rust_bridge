ef1748a7a439c5b7b9c8a831c7acfd562e39aece
Daniel Hahne <d@niel.re>
2023-12-24 21:18:46 +0100

    ci: Revert previous change to trigger ci without regenerating code.

--- frb_example/pure_dart/rust/src/api/rust_auto_opaque.rs
+++ frb_example/pure_dart/rust/src/api/rust_auto_opaque.rs
@@ -181,7 +181,7 @@ impl NonCloneSimpleTwinNormal {
         Self { inner: 42 }
     }
 
-    /// constructor that returns a Result
+    /// constructor with Result
     pub fn new_with_result_twin_normal() -> anyhow::Result<NonCloneSimpleTwinNormal> {
         Ok(Self { inner: 42 })
     }
