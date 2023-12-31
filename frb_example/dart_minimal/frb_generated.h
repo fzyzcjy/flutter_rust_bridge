3d0d40b4dc8f0dd8fc83080fd4d604e21fa888ad
fzyzcjy <ch271828n@outlook.com>
2023-12-31 19:33:24 +0800

    chore: codegen

--- frb_example/dart_minimal/frb_generated.h
+++ frb_example/dart_minimal/frb_generated.h
@@ -18,11 +18,11 @@ void frbgen_frb_example_dart_minimal_dart_fn_deliver_output(int32_t call_id,
 void frbgen_frb_example_dart_minimal_wire_minimal_adder(int64_t port_, int32_t a, int32_t b);
 static int64_t dummy_method_to_enforce_bundling(void) {
     int64_t dummy_var = 0;
-    dummy_var ^= ((int64_t) (void*) dart_fn_deliver_output);
     dummy_var ^= ((int64_t) (void*) drop_dart_object);
+    dummy_var ^= ((int64_t) (void*) frbgen_frb_example_dart_minimal_dart_fn_deliver_output);
+    dummy_var ^= ((int64_t) (void*) frbgen_frb_example_dart_minimal_wire_minimal_adder);
     dummy_var ^= ((int64_t) (void*) get_dart_object);
     dummy_var ^= ((int64_t) (void*) new_dart_opaque);
     dummy_var ^= ((int64_t) (void*) store_dart_post_cobject);
-    dummy_var ^= ((int64_t) (void*) wire_minimal_adder);
     return dummy_var;
 }
--- frb_example/deliberate_bad/frb_generated.h
+++ frb_example/deliberate_bad/frb_generated.h
@@ -26,15 +26,15 @@ void frbgen_frb_example_deliberate_bad_wire_make_stack_buffer_overflow(int64_t p
 void frbgen_frb_example_deliberate_bad_wire_make_use_of_uninitialized_value(int64_t port_);
 static int64_t dummy_method_to_enforce_bundling(void) {
     int64_t dummy_var = 0;
-    dummy_var ^= ((int64_t) (void*) dart_fn_deliver_output);
     dummy_var ^= ((int64_t) (void*) drop_dart_object);
+    dummy_var ^= ((int64_t) (void*) frbgen_frb_example_deliberate_bad_dart_fn_deliver_output);
+    dummy_var ^= ((int64_t) (void*) frbgen_frb_example_deliberate_bad_wire_make_data_race);
+    dummy_var ^= ((int64_t) (void*) frbgen_frb_example_deliberate_bad_wire_make_heap_use_after_free);
+    dummy_var ^= ((int64_t) (void*) frbgen_frb_example_deliberate_bad_wire_make_memory_leak);
+    dummy_var ^= ((int64_t) (void*) frbgen_frb_example_deliberate_bad_wire_make_stack_buffer_overflow);
+    dummy_var ^= ((int64_t) (void*) frbgen_frb_example_deliberate_bad_wire_make_use_of_uninitialized_value);
     dummy_var ^= ((int64_t) (void*) get_dart_object);
     dummy_var ^= ((int64_t) (void*) new_dart_opaque);
     dummy_var ^= ((int64_t) (void*) store_dart_post_cobject);
-    dummy_var ^= ((int64_t) (void*) wire_make_data_race);
-    dummy_var ^= ((int64_t) (void*) wire_make_heap_use_after_free);
-    dummy_var ^= ((int64_t) (void*) wire_make_memory_leak);
-    dummy_var ^= ((int64_t) (void*) wire_make_stack_buffer_overflow);
-    dummy_var ^= ((int64_t) (void*) wire_make_use_of_uninitialized_value);
     return dummy_var;
 }
