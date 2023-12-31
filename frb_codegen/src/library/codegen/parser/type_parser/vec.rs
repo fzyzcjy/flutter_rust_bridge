29076a32e43c9818f59a2cd0d1fa2624d22c90de
fzyzcjy <ch271828n@outlook.com>
2023-12-31 20:58:03 +0800

    chore: lint

--- frb_codegen/src/library/codegen/parser/type_parser/vec.rs
+++ frb_codegen/src/library/codegen/parser/type_parser/vec.rs
@@ -1,8 +1,6 @@
 use crate::codegen::ir::ty::general_list::{ir_list, IrTypeGeneralList};
-use crate::codegen::ir::ty::primitive::IrTypePrimitive;
-use crate::codegen::ir::ty::primitive_list::IrTypePrimitiveList;
 use crate::codegen::ir::ty::IrType;
-use crate::codegen::ir::ty::IrType::{GeneralList, Primitive, PrimitiveList};
+use crate::codegen::ir::ty::IrType::{GeneralList, Primitive};
 use crate::codegen::parser::type_parser::unencodable::ArgsRefs::Generic;
 use crate::codegen::parser::type_parser::unencodable::SplayedSegment;
 use crate::codegen::parser::type_parser::TypeParserWithContext;
