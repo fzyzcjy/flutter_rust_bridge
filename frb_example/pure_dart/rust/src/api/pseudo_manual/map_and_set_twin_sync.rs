4bf4bac371a63e1d7d423b4ca721281b67e58412
fzyzcjy <ch271828n@outlook.com>
2023-12-31 20:29:16 +0800

    chore: empty

---
+++ frb_example/pure_dart/rust/src/api/pseudo_manual/map_and_set_twin_sync.rs
@@ -0,0 +1,5 @@
+// NOTE: This file is mimicking how a human developer writes tests, 
+// and is auto-generated from `map_and_set.rs` by frb_internal
+// Please do not modify manually, but modify the origin and re-run frb_internal generator
+
+// TODO
