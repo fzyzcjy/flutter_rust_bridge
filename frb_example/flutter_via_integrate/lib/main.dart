6dff1ed7c2c4ccc37f6ef97959200a3680d21f2e
fzyzcjy <ch271828n@outlook.com>
2023-12-31 22:03:25 +0800

    chore: codegen

--- frb_example/flutter_via_integrate/lib/main.dart
+++ frb_example/flutter_via_integrate/lib/main.dart
@@ -1,29 +1,3 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_via_integrate/src/rust/api/simple.dart';
-import 'package:flutter_via_integrate/src/rust/frb_generated.dart';
-
-Future<void> main() async {
-  await RustLib.init();
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('flutter_rust_bridge quickstart')),
-        body: Center(
-          child: Text(
-              'Action: Call Rust `greet("Tom")`\nResult: `${greet(name: "Tom")}`'),
-        ),
-      ),
-    );
-  }
-}
-
 // import 'package:flutter/material.dart';
 //
 // void main() {
@@ -150,3 +124,29 @@ class MyApp extends StatelessWidget {
 //   }
 // }
 //
+
+import 'package:flutter/material.dart';
+import 'package:flutter_via_integrate/src/rust/api/simple.dart';
+import 'package:flutter_via_integrate/src/rust/frb_generated.dart';
+
+Future<void> main() async {
+  await RustLib.init();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('flutter_rust_bridge quickstart')),
+        body: Center(
+          child: Text(
+              'Action: Call Rust `greet("Tom")`\nResult: `${greet(name: "Tom")}`'),
+        ),
+      ),
+    );
+  }
+}
