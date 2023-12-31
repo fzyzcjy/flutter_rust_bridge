58424a117a03f685c10d1f5e7930f59bdb624cd7
fzyzcjy <ch271828n@outlook.com>
2023-12-31 20:35:40 +0800

    feat: some tests

--- frb_example/pure_dart/rust/src/api/map_and_set.rs
+++ frb_example/pure_dart/rust/src/api/map_and_set.rs
@@ -1,3 +1,49 @@
-pub fn func_map_TODO_twin_normal(arg: TODO) -> TODO {
+use crate::api::enumeration::{EnumSimpleTwinNormal, KitchenSinkTwinNormal};
+use crate::auxiliary::sample_types::MySize;
+use std::collections::{HashMap, HashSet};
+
+pub fn func_hash_map_i32_i32_twin_normal(arg: HashMap<i32, i32>) -> HashMap<i32, i32> {
+    arg
+}
+
+pub fn func_hash_set_i32_twin_normal(arg: HashSet<i32>) -> HashSet<i32> {
+    arg
+}
+
+pub fn func_hash_map_string_string_twin_normal(
+    arg: HashMap<String, String>,
+) -> HashMap<String, String> {
+    arg
+}
+
+pub fn func_hash_set_string_twin_normal(arg: HashSet<String>) -> HashSet<String> {
+    arg
+}
+
+pub fn func_hash_map_bytes_bytes_twin_normal(
+    arg: HashMap<Vec<u8>, Vec<u8>>,
+) -> HashMap<Vec<u8>, Vec<u8>> {
+    arg
+}
+
+pub fn func_hash_set_bytes_twin_normal(arg: HashSet<Vec<u8>>) -> HashSet<Vec<u8>> {
+    arg
+}
+
+pub fn func_hash_map_string_struct_twin_normal(
+    arg: HashMap<String, MySize>,
+) -> HashMap<String, MySize> {
+    arg
+}
+
+pub fn func_hash_map_string_simple_enum_twin_normal(
+    arg: HashMap<String, EnumSimpleTwinNormal>,
+) -> HashMap<String, EnumSimpleTwinNormal> {
+    arg
+}
+
+pub fn func_hash_map_string_complex_enum_twin_normal(
+    arg: HashMap<String, KitchenSinkTwinNormal>,
+) -> HashMap<String, KitchenSinkTwinNormal> {
     arg
 }
