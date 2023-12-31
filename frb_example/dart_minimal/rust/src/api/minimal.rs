a37566158c32b7ef532befdf24a93c745ebc996b
fzyzcjy <ch271828n@outlook.com>
2023-12-31 15:04:03 +0800

    Revert "fix: simple"
    This reverts commit 871afee0fbbdee3a136fbbc70a06f0db30d64bb9.

--- frb_example/dart_minimal/rust/src/api/minimal.rs
+++ frb_example/dart_minimal/rust/src/api/minimal.rs
@@ -2,7 +2,7 @@ pub use std::panic::{RefUnwindSafe, UnwindSafe};
 
 // TODO for temporary experiment
 pub struct MyStruct {
-    pub my_field: String,
+    my_field: String,
 }
 
 pub fn minimal_adder(a: i32, b: i32, x: MyStruct) -> i32 {
