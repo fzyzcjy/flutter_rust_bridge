372f25760b186ba5eab803fdb9db5a754c49cb6f
fzyzcjy <ch271828n@outlook.com>
2024-01-01 11:32:55 +0800

    refactor: minor

--- tools/frb_internal/lib/src/makefile_dart/build.dart
+++ tools/frb_internal/lib/src/makefile_dart/build.dart
@@ -38,17 +38,25 @@ class BuildFlutterConfig {
 // ref: https://docs.flutter.dev/deployment
 Future<void> buildFlutter(BuildFlutterConfig config) async {
   const package = 'frb_example/flutter_via_create';
+
   final outputDir = '${exec.pwd}target/build_flutter_output';
   Directory(outputDir).createSync(recursive: true);
+  void copyArtifacts(List<String> paths) {
+    for (final path in paths) {
+      copyPath(path, outputDir);
+    }
+  }
 
   switch (config.target) {
     case BuildTarget.windows:
-      TODO;
+      // https://docs.flutter.dev/deployment/windows
+      await exec('flutter build windows --verbose', relativePwd: package);
+      copyArtifacts(['${exec.pwd}TODO']);
 
     case BuildTarget.macos:
       // https://docs.flutter.dev/deployment/macos
       await exec('flutter build macos --verbose', relativePwd: package);
-      copyPathSync('${exec.pwd}Build/Products/Release', outputDir);
+      copyArtifacts(['${exec.pwd}Build/Products/Release']);
 
     case BuildTarget.linux:
       TODO;
@@ -56,17 +64,17 @@ Future<void> buildFlutter(BuildFlutterConfig config) async {
     case BuildTarget.androidAab:
       // https://docs.flutter.dev/deployment/android
       await exec('flutter build appbundle --verbose', relativePwd: package);
-      copyPathSync('${exec.pwd}build/app/outputs/bundle/release', outputDir);
+      copyArtifacts(['${exec.pwd}build/app/outputs/bundle/release']);
 
     case BuildTarget.androidApk:
       // https://docs.flutter.dev/deployment/android
       await exec('flutter build apk --verbose', relativePwd: package);
-      copyPathSync('${exec.pwd}build/app/outputs/apk/release', outputDir);
+      copyArtifacts(['${exec.pwd}build/app/outputs/apk/release']);
 
     case BuildTarget.ios:
       // https://docs.flutter.dev/deployment/ios
       await exec('flutter build ipa --verbose', relativePwd: package);
-      copyPathSync('${exec.pwd}build/ios/archive', outputDir);
-      copyPathSync('${exec.pwd}build/ios/ipa', outputDir);
+      copyArtifacts(
+          ['${exec.pwd}build/ios/archive', '${exec.pwd}build/ios/ipa']);
   }
 }
