bb0ec2d34b75d4de77c7a6eaf085cecc0ef4cbfd
fzyzcjy <ch271828n@outlook.com>
2024-01-01 09:30:47 +0800

    feat: some tests

--- frb_example/pure_dart/test/api/map_and_set_test.dart
+++ frb_example/pure_dart/test/api/map_and_set_test.dart
@@ -1,11 +1,33 @@
-import 'package:frb_example_pure_dart/src/rust/api/dart_fn.dart';
+import 'dart:typed_data';
+
+import 'package:frb_example_pure_dart/src/rust/api/map_and_set.dart';
 import 'package:frb_example_pure_dart/src/rust/frb_generated.dart';
-import 'package:test/test.dart';
+
+import '../test_utils.dart';
 
 Future<void> main({bool skipRustLibInit = false}) async {
   if (!skipRustLibInit) await RustLib.init();
 
-  test('TODO', () async {
-    fail('TODO');
-  });
+  addTestsIdentityFunctionCall(funcHashMapI32I32TwinNormal, <Map<int, int>>[
+    {},
+    {10: 20},
+    {10: 20, 30: 40},
+  ]);
+  addTestsIdentityFunctionCall(funcHashSetI32TwinNormal, <Set<int>>[
+    {},
+    {10},
+    {10, 20},
+  ]);
+
+  addTestsIdentityFunctionCall(
+      funcHashMapStringStringTwinNormal, <Map<String, String>>[
+    {},
+    {'a': 'b'},
+    {'a': 'b', 'c': 'd'},
+  ]);
+  addTestsIdentityFunctionCall(funcHashSetStringTwinNormal, <Set<String>>[
+    {},
+    {'a'},
+    {'a', 'b'},
+  ]);
 }
