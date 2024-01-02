ba5b1357bcab926b6800f441180dbbacf3334670
fzyzcjy <ch271828n@outlook.com>
2024-01-02 09:18:11 +0800

    chore: rename

--- frb_rust/src/misc/user_utils.rs
+++ frb_rust/src/misc/user_utils.rs
@@ -1,4 +1,6 @@
-pub fn setup_included_battery() {
+/// Setup defaults that is usually useful for a new project.
+/// Surely, you are free to customize anything.
+pub fn setup_defaults() {
     setup_backtrace();
     setup_log_to_console();
 }
