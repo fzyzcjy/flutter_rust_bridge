4a255e487f293c2843130fd607b56de0041c1cee
fzyzcjy <ch271828n@outlook.com>
2024-01-02 09:34:17 +0800

    feat: cfg

--- frb_rust/src/misc/mod.rs
+++ frb_rust/src/misc/mod.rs
@@ -3,6 +3,7 @@ pub(crate) mod into_into_dart;
 pub(crate) mod manual_impl;
 pub(crate) mod rust_arc;
 pub(crate) mod rust_auto_opaque;
+#[cfg(feature = "user-utils")]
 pub(crate) mod user_utils;
 #[cfg(target_family = "wasm")]
 pub(crate) mod web_utils;
