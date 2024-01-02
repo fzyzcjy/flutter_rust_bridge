f835a8275d0da43c3382a6cac95e23930217e331
fzyzcjy <ch271828n@outlook.com>
2024-01-02 21:56:09 +0800

    feat: panic_backtrace

---
+++ frb_rust/src/misc/panic_backtrace.rs
@@ -0,0 +1,20 @@
+use backtrace::Backtrace;
+use std::cell::RefCell;
+
+thread_local! {
+    static backtrace: RefCell<Option<Backtrace>> = RefCell::new(None);
+}
+
+pub(crate) fn setup() {
+    let old_hook = std::panic::take_hook();
+    std::panic::set_hook(Box::new(|arg| {
+        let trace = Backtrace::new();
+        backtrace.with(move |b| b.borrow_mut().replace(trace));
+
+        old_hook(arg);
+    }));
+}
+
+pub(crate) fn take_last() -> Option<Backtrace> {
+    backtrace.with(|b| b.borrow_mut().take())
+}
