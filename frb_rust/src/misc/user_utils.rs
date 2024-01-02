9568e2b45280060e821be28fe8ad41f97867710f
fzyzcjy <ch271828n@outlook.com>
2024-01-02 09:29:02 +0800

    feat: impl

--- frb_rust/src/misc/user_utils.rs
+++ frb_rust/src/misc/user_utils.rs
@@ -1,14 +1,32 @@
+use std::env;
+use std::env::VarError;
+
 /// Setup defaults that is usually useful for a new project.
 /// Surely, you are free to customize everything.
-pub fn setup_default_utils() {
-    setup_backtrace();
+pub fn setup_default_user_utils() {
+    // setup log before others, such that we can see logs in other setup functions
     setup_log_to_console();
+    setup_backtrace();
 }
 
 fn setup_backtrace() {
-    TODO;
+    if env::var("RUST_BACKTRACE").err() == Some(VarError::NotPresent) {
+        env::set_var("RUST_BACKTRACE", "1");
+    } else {
+        debug!("Skip setup RUST_BACKTRACE because there is already environment variable");
+    }
 }
 
 fn setup_log_to_console() {
-    TODO;
+    #[cfg(target_os = "android")]
+    let _ = android_logger::init_once(
+        android_logger::Config::default().with_max_level(LevelFilter::Trace),
+    );
+
+    #[cfg(target_os = "ios")]
+    let _ = oslog::OsLogger::new("frb_user")
+        .level_filter(LevelFilter::Trace)
+        .init();
+
+    // TODO add more platforms, e.g. web, ...
 }
