6d700a328bbc82eb396fe60fa341ba8e50f5659b
fzyzcjy <ch271828n@outlook.com>
2024-01-02 09:21:03 +0800

    chore: more name

--- frb_rust/src/misc/user_utils.rs
+++ frb_rust/src/misc/user_utils.rs
@@ -1,6 +1,6 @@
 /// Setup defaults that is usually useful for a new project.
-/// Surely, you are free to customize anything.
-pub fn setup_defaults() {
+/// Surely, you are free to customize everything.
+pub fn setup_default_utils() {
     setup_backtrace();
     setup_log_to_console();
 }
