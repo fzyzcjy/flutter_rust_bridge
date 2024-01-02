26150c5a052a5a8f32aa39c490e27a753bb20d1a
fzyzcjy <ch271828n@outlook.com>
2024-01-02 13:14:46 +0800

    feat: try RawReceivePort

--- frb_dart/lib/src/utils/base_lazy_port_manager.dart
+++ frb_dart/lib/src/utils/base_lazy_port_manager.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_rust_bridge/src/generalized_isolate/generalized_isolate.dart';
 import 'package:flutter_rust_bridge/src/platform_types/platform_types.dart';
-import 'package:flutter_rust_bridge/src/utils/port_generator.dart';
 import 'package:meta/meta.dart';
 
 /// {@macro flutter_rust_bridge.internal}
@@ -10,13 +9,20 @@ abstract class BaseLazyPortManager {
   late final _port = _initPort();
 
   /// {@macro flutter_rust_bridge.only_for_generated_code}
-  ReceivePort _initPort() {
-    print('hi BaseLazyPortManager._initPort start');
-    final port = broadcastPort(BaseLazyPortIdGenerator.create());
-    port.listen(onData);
-    print(
-        'hi BaseLazyPortManager._initPort end port=${port.sendPort.nativePort}');
+  RawReceivePort _initPort() {
+    print('hi HACK!!! use RawReceivePort');
+    var port = RawReceivePort();
+    port.handler = (response) {
+      print('hi BaseLazyPortManager port.handler called (and hacked!)');
+    };
     return port;
+
+    // print('hi BaseLazyPortManager._initPort start');
+    // final port = broadcastPort(BaseLazyPortIdGenerator.create());
+    // port.listen(onData);
+    // print(
+    //     'hi BaseLazyPortManager._initPort end port=${port.sendPort.nativePort}');
+    // return port;
   }
 
   /// {@macro flutter_rust_bridge.only_for_generated_code}
