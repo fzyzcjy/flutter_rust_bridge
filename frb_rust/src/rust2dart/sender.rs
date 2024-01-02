ebb4614dd3ed52820646557e108a0e816a4787c4
fzyzcjy <ch271828n@outlook.com>
2024-01-02 10:46:26 +0800

    feat: impl

--- frb_rust/src/rust2dart/sender.rs
+++ frb_rust/src/rust2dart/sender.rs
@@ -1,5 +1,6 @@
 use crate::generalized_isolate::Channel;
 use crate::generalized_isolate::IntoDart;
+use anyhow::anyhow;
 
 #[derive(Clone)]
 pub struct Rust2DartSender {
@@ -11,7 +12,11 @@ impl Rust2DartSender {
         Rust2DartSender { channel }
     }
 
-    pub fn send(&self, msg: impl IntoDart) -> bool {
-        self.channel.post(msg)
+    pub fn send(&self, msg: impl IntoDart) -> anyhow::Result<()> {
+        if self.channel.post(msg) {
+            Ok(())
+        } else {
+            Err(anyhow!("Fail to post message to Dart"))
+        }
     }
 }
