7f4db008aaab632c1d64494c25adf6ec1427f973
fzyzcjy <ch271828n@outlook.com>
2024-01-03 08:34:00 +0800

    fix: lint

--- frb_rust/src/misc/user_utils.rs
+++ frb_rust/src/misc/user_utils.rs
@@ -1,6 +1,3 @@
-use std::env;
-use std::env::VarError;
-
 /// Setup defaults that is usually useful for a new project.
 /// Surely, you are free to customize everything.
 pub fn setup_default_user_utils() {
@@ -11,8 +8,8 @@ pub fn setup_default_user_utils() {
 
 fn setup_backtrace() {
     #[cfg(not(wasm))]
-    if env::var("RUST_BACKTRACE").err() == Some(VarError::NotPresent) {
-        env::set_var("RUST_BACKTRACE", "1");
+    if std::env::var("RUST_BACKTRACE").err() == Some(std::env::VarError::NotPresent) {
+        std::env::set_var("RUST_BACKTRACE", "1");
     } else {
         log::debug!("Skip setup RUST_BACKTRACE because there is already environment variable");
     }
