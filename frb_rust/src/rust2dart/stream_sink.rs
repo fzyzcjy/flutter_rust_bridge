e8e0a6e371ffa434c300be6e3e2923a2a6905688
fzyzcjy <ch271828n@outlook.com>
2024-01-04 13:09:18 +0800

    feat: StreamSinkNonClone

--- frb_rust/src/rust2dart/stream_sink.rs
+++ frb_rust/src/rust2dart/stream_sink.rs
@@ -3,32 +3,48 @@ use crate::for_generated::DcoCodec;
 use crate::generalized_isolate::{channel_to_handle, handle_to_channel, SendableChannelHandle};
 use crate::rust2dart::sender::Rust2DartSender;
 use std::marker::PhantomData;
+use std::sync::Arc;
 
 /// A sink to send asynchronous data back to Dart.
 /// Represented as a Dart
 /// [`Stream`](https://api.dart.dev/stable/dart-async/Stream-class.html).
 #[derive(Clone)]
-pub struct StreamSinkBase<T, Rust2DartCodec: BaseCodec = DcoCodec> {
-    sendable_channel_handle: SendableChannelHandle,
-    _phantom_data: (PhantomData<T>, PhantomData<Rust2DartCodec>),
-}
+pub struct StreamSinkBase<T, Rust2DartCodec: BaseCodec = DcoCodec>(
+    Arc<StreamSinkNonClone<T, Rust2DartCodec>>,
+);
 
 impl<T, Rust2DartCodec: BaseCodec> StreamSinkBase<T, Rust2DartCodec> {
     /// Create a new sink from a port wrapper.
     pub fn new(sender: Rust2DartSender) -> Self {
+        Self(StreamSinkNonClone::new(sender))
+    }
+
+    /// Add data to the stream. Returns false when data could not be sent,
+    /// or the stream has been closed.
+    pub fn add(&self, value: Rust2DartCodec::Message) -> anyhow::Result<()> {
+        self.0.add(value)
+    }
+}
+
+// non-Clone, since we want to close the stream when dropping it
+struct StreamSinkNonClone<T, Rust2DartCodec: BaseCodec> {
+    sendable_channel_handle: SendableChannelHandle,
+    _phantom_data: (PhantomData<T>, PhantomData<Rust2DartCodec>),
+}
+
+impl<T, Rust2DartCodec: BaseCodec> crate::for_generated::StreamSinkNonClone<T, Rust2DartCodec> {
+    fn new(sender: Rust2DartSender) -> Self {
         Self {
             sendable_channel_handle: channel_to_handle(&sender.channel),
             _phantom_data: Default::default(),
         }
     }
 
-    fn sender(&self) -> Rust2DartSender {
-        Rust2DartSender::new(handle_to_channel(&self.sendable_channel_handle))
+    fn add(&self, value: Rust2DartCodec::Message) -> anyhow::Result<()> {
+        self.sender().send(value.into_dart_abi())
     }
 
-    /// Add data to the stream. Returns false when data could not be sent,
-    /// or the stream has been closed.
-    pub fn add(&self, value: Rust2DartCodec::Message) -> anyhow::Result<()> {
-        self.sender().send(value.into_dart_abi())
+    fn sender(&self) -> Rust2DartSender {
+        Rust2DartSender::new(handle_to_channel(&self.sendable_channel_handle))
     }
 }
