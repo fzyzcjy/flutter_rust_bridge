ab5ea257eefb7590d742cc8ebb8a550c69cf4c26
fzyzcjy <ch271828n@outlook.com>
2024-01-04 09:10:05 +0800

    feat: 64bit

--- frb_codegen/src/library/codegen/parser/function_parser/transformer.rs
+++ frb_codegen/src/library/codegen/parser/function_parser/transformer.rs
@@ -1,9 +1,11 @@
 use crate::codegen::ir::field::IrField;
+use crate::codegen::ir::ty::primitive::IrTypePrimitive;
 use crate::codegen::ir::ty::primitive_list::IrTypePrimitiveList;
 use crate::codegen::ir::ty::IrType;
 use crate::codegen::parser::function_parser::{FunctionParser, FunctionPartialInfo};
 use crate::codegen::parser::type_parser::TypeParserParsingContext;
 use itertools::Itertools;
+use IrTypePrimitive::{Isize, Usize, I64, U64};
 
 impl<'a, 'b> FunctionParser<'a, 'b> {
     pub(super) fn transform_fn_info(
@@ -39,11 +41,14 @@ impl<'a, 'b> FunctionParser<'a, 'b> {
 }
 
 fn transform_primitive_list_param(ty: IrType) -> IrType {
-    if let IrType::PrimitiveList(inner) = ty {
-        IrType::PrimitiveList(IrTypePrimitiveList {
-            strict_dart_type: false,
-            ..inner
-        })
+    if let IrType::PrimitiveList(inner) = &ty {
+        match inner.primitive {
+            U64 | I64 | Usize | Isize => ty,
+            _ => IrType::PrimitiveList(IrTypePrimitiveList {
+                strict_dart_type: false,
+                ..inner.clone()
+            }),
+        }
     } else {
         ty
     }
