69d5dfa6af0e3ecd0ad457803ab1c08ac489a2f3
fzyzcjy <ch271828n@outlook.com>
2024-01-04 09:21:31 +0800

    feat: type converter

--- frb_codegen/src/library/codegen/generator/codec/sse/ty/primitive_list.rs
+++ frb_codegen/src/library/codegen/generator/codec/sse/ty/primitive_list.rs
@@ -8,12 +8,23 @@ use crate::library::codegen::generator::codec::sse::lang::LangTrait;
 impl<'a> CodecSseTyTrait for PrimitiveListCodecSseTy<'a> {
     fn generate_encode(&self, lang: &Lang) -> Option<String> {
         Some(match lang {
-            Lang::DartLang(_) => format!(
-                "{};
-                serializer.buffer.put{}List(self);",
-                lang.call_encode(&LEN_TYPE, &format!("self.{}", list_len_method(lang))),
-                get_serializer_dart_postfix(&self.ir.primitive)
-            ),
+            Lang::DartLang(_) => {
+                let type_converter = if self.ir.strict_dart_type {
+                    "self".to_owned()
+                } else {
+                    format!(
+                        "self is {prim}List ? self : {prim}List.fromList(self)",
+                        prim = get_serializer_dart_postfix(&self.ir.primitive),
+                    )
+                };
+
+                format!(
+                    "{};
+                    serializer.buffer.put{}List({type_converter});",
+                    lang.call_encode(&LEN_TYPE, &format!("self.{}", list_len_method(lang))),
+                    get_serializer_dart_postfix(&self.ir.primitive)
+                )
+            }
             Lang::RustLang(_) => {
                 // TODO do not use naive loop
                 general_list_generate_encode(lang, &IrType::Primitive(self.ir.primitive.clone()))
