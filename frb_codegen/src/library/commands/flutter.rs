696e127969e04b18d0fc9e846dfaabbb02f2bec4
MateusHBR <mateusmorais.r@gmail.com>
2024-01-05 18:54:45 -0300

    fix import sort

--- frb_codegen/src/library/commands/flutter.rs
+++ frb_codegen/src/library/commands/flutter.rs
@@ -1,9 +1,8 @@
-use std::path::PathBuf;
-
 use crate::command_run;
 use crate::commands::command_runner::call_shell;
 use crate::library::commands::command_runner::check_exit_code;
 use log::info;
+use std::path::PathBuf;
 
 #[allow(clippy::vec_init_then_push)]
 pub fn flutter_create(name: &str) -> anyhow::Result<()> {
@@ -23,6 +22,7 @@ pub fn flutter_pub_add(items: &[String]) -> anyhow::Result<()> {
     )?)
 }
 
+#[allow(clippy::vec_init_then_push)]
 pub fn flutter_pub_get(path: &PathBuf) -> anyhow::Result<()> {
     info!("Execute `flutter pub get` inside {path:?}");
     check_exit_code(&command_run!(call_shell[None, None], "flutter", "pub", "get", path)?)
