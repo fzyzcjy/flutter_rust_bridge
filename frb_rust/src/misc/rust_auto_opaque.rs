8ad9f5995480457100a00d84edcfa86e0b2ffb09
fzyzcjy <ch271828n@outlook.com>
2024-01-06 17:29:37 +0800

    feat: async

--- frb_rust/src/misc/rust_auto_opaque.rs
+++ frb_rust/src/misc/rust_auto_opaque.rs
@@ -19,6 +19,18 @@ impl<T> RustOpaque<RwLock<T>> {
     pub fn rust_auto_opaque_decode_sync_ref_mut(&self) -> Result<RwLockWriteGuard<'_, T>> {
         Ok(self.blocking_write())
     }
+
+    pub async fn rust_auto_opaque_decode_async_owned(self) -> Result<T> {
+        self.rust_auto_opaque_decode_sync_owned()
+    }
+
+    pub async fn rust_auto_opaque_decode_async_ref(&self) -> Result<RwLockReadGuard<'_, T>> {
+        Ok(self.read())
+    }
+
+    pub async fn rust_auto_opaque_decode_async_ref_mut(&self) -> Result<RwLockWriteGuard<'_, T>> {
+        Ok(self.write())
+    }
 }
 
 pub fn rust_auto_opaque_encode<T>(value: T) -> RustOpaque<RwLock<T>> {
