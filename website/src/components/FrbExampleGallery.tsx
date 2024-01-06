b623ba6f61d8c6a0358a1bfb49074efdef0e6e75
fzyzcjy <ch271828n@outlook.com>
2024-01-06 12:44:25 +0800

    feat: loading

--- website/src/components/FrbExampleGallery.tsx
+++ website/src/components/FrbExampleGallery.tsx
@@ -4,16 +4,20 @@ import React from 'react';
 export default function FrbExampleGallery() {
     return (
         <>
-            <div id="flutter_host" style={{height: '500px', border: '1px solid #ccc'}}>Loading Flutter web app...
-            </div>
+            <div id="flutter_host" style={{height: '500px', border: '1px solid #ccc'}}></div>
             <FlutterInitializer/>
         </>
     )
 }
 
 class FlutterInitializer extends React.Component {
-    componentDidMount() {
+    state = {
+        loading: true,
+    };
+
+    componentDidMount = () => {
         console.log('FrbExampleGallery.FlutterInitializer componentDidMount');
+        const that = this;
 
         {
             console.log('FrbExampleGallery.FlutterInitializer add enable-threads.js script');
@@ -40,13 +44,14 @@ class FlutterInitializer extends React.Component {
                         hostElement: document.querySelector("#flutter_host")
                     });
                     await appRunner.runApp();
+                    that.setState({loading: false})
                 }
             });
         });
     }
 
     render() {
-        return <span></span>
+        return this.state.loading ? <span>Loading</span> : <></>
     }
 }
 
