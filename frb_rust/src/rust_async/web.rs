c8b12c111682f54f23c9598d9ffa806ce9fbb936
fzyzcjy <ch271828n@outlook.com>
2024-01-06 17:49:39 +0800

    feat: try std rwlock

--- frb_rust/src/rust_async/web.rs
+++ frb_rust/src/rust_async/web.rs
@@ -3,6 +3,7 @@ use crate::transfer;
 use futures::channel::oneshot;
 use std::future::Future;
 use std::pin::Pin;
+pub use std::sync::RwLock;
 use std::task::{Context, Poll};
 
 pub trait BaseAsyncRuntime {
