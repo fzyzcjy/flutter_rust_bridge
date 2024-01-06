545571d3d1e3de1197ddbafc5cf3634d7497d146
MateusHBR <mateusmorais.r@gmail.com>
2024-01-06 12:31:17 -0300

    execute command run sending the path to execute inside macro

--- frb_codegen/src/library/commands/flutter.rs
+++ frb_codegen/src/library/commands/flutter.rs
@@ -2,7 +2,7 @@ use crate::command_run;
 use crate::commands::command_runner::call_shell;
 use crate::library::commands::command_runner::check_exit_code;
 use log::info;
-use std::path::PathBuf;
+use std::path::Path;
 
 #[allow(clippy::vec_init_then_push)]
 pub fn flutter_create(name: &str) -> anyhow::Result<()> {
@@ -23,7 +23,7 @@ pub fn flutter_pub_add(items: &[String]) -> anyhow::Result<()> {
 }
 
 #[allow(clippy::vec_init_then_push)]
-pub fn flutter_pub_get(path: &PathBuf) -> anyhow::Result<()> {
+pub fn flutter_pub_get(path: &Path) -> anyhow::Result<()> {
     info!("Execute `flutter pub get` inside {path:?}");
-    check_exit_code(&command_run!(call_shell[None, None], "flutter", "pub", "get", path)?)
+    check_exit_code(&command_run!(call_shell[Some(path), None], "flutter", "pub", "get")?)
 }
