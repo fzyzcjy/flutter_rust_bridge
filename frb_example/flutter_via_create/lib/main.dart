dd7e1c482087ec9e664a6259aa4a223de82d5138
fzyzcjy <ch271828n@outlook.com>
2024-01-06 19:35:48 +0800

    feat: try error handler

--- frb_example/flutter_via_create/lib/main.dart
+++ frb_example/flutter_via_create/lib/main.dart
@@ -1,10 +1,27 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_via_create/src/rust/api/simple.dart';
 import 'package:flutter_via_create/src/rust/frb_generated.dart';
 
 Future<void> main() async {
-  await RustLib.init();
-  runApp(const MyApp());
+  // TODO temp
+  FlutterError.onError = (details) {
+    FlutterError.presentError(details);
+    print('FlutterError.onError $details');
+  };
+  PlatformDispatcher.instance.onError = (error, stack) {
+    print('PlatformDispatcher.instance.onError $error $stack');
+    return true;
+  };
+  await runZonedGuarded(
+    () async {
+      await RustLib.init();
+      runApp(const MyApp());
+    },
+    (error, stackTrace) => print('runZonedGuarded error $error $stackTrace'),
+  );
 }
 
 class MyApp extends StatelessWidget {
