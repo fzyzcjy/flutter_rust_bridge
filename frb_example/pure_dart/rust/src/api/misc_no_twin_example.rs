b9bd52b575e8af5039f5d890c13862c029e79c3a
fzyzcjy <ch271828n@outlook.com>
2024-01-06 16:45:23 +0800

    feat: try reproduce

---
+++ frb_example/pure_dart/rust/src/api/misc_no_twin_example.rs
@@ -0,0 +1,14 @@
+// FRB_INTERNAL_GENERATOR: {"forbiddenDuplicatorModes": ["sync", "rustAsync", "sse", "syncSse", "rustAsyncSse"]}
+
+use std::sync::Arc;
+
+// Reproduce https://github.com/fzyzcjy/flutter_rust_bridge/issues/1613
+pub struct StructWithArcField {
+    inner: Arc<String>,
+}
+
+impl StructWithArcField {
+    pub async fn func_async(&self) -> usize {
+        self.inner.len()
+    }
+}
