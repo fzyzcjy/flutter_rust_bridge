67651665809b802d640a5f6ab3ea1490068b9331
fzyzcjy <ch271828n@outlook.com>
2024-01-06 17:28:58 +0800

    refactor: rename

--- frb_rust/src/misc/rust_auto_opaque.rs
+++ frb_rust/src/misc/rust_auto_opaque.rs
@@ -5,18 +5,18 @@ use tokio::sync::{RwLock, RwLockReadGuard, RwLockWriteGuard};
 impl<T> RustOpaque<RwLock<T>> {
     // the function signature is not covered while the whole body is covered - looks like a bug in coverage tool
     // frb-coverage:ignore-start
-    pub fn rust_auto_opaque_decode_owned(self) -> Result<T> {
+    pub fn rust_auto_opaque_decode_sync_owned(self) -> Result<T> {
         // frb-coverage:ignore-end
         Ok(self.into_inner()
             .context("Cannot convert RustOpaque to inner value. This is probably because you are having more than one references to it.")?
             .into_inner())
     }
 
-    pub fn rust_auto_opaque_decode_ref(&self) -> Result<RwLockReadGuard<'_, T>> {
+    pub fn rust_auto_opaque_decode_sync_ref(&self) -> Result<RwLockReadGuard<'_, T>> {
         Ok(self.blocking_read())
     }
 
-    pub fn rust_auto_opaque_decode_ref_mut(&self) -> Result<RwLockWriteGuard<'_, T>> {
+    pub fn rust_auto_opaque_decode_sync_ref_mut(&self) -> Result<RwLockWriteGuard<'_, T>> {
         Ok(self.blocking_write())
     }
 }
