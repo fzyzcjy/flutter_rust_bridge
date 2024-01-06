2642ffc4ca2983c970639eae556fb76597991cc4
fzyzcjy <ch271828n@outlook.com>
2024-01-06 17:26:32 +0800

    fix: moer compile

--- frb_rust/src/misc/rust_auto_opaque.rs
+++ frb_rust/src/misc/rust_auto_opaque.rs
@@ -1,26 +1,23 @@
 use crate::RustOpaque;
 use anyhow::{anyhow, Context, Result};
-use std::sync::{RwLockReadGuard, RwLockWriteGuard};
-use tokio::sync::RwLock;
+use tokio::sync::{RwLock, RwLockReadGuard, RwLockWriteGuard};
 
 impl<T> RustOpaque<RwLock<T>> {
     // the function signature is not covered while the whole body is covered - looks like a bug in coverage tool
     // frb-coverage:ignore-start
     pub fn rust_auto_opaque_decode_owned(self) -> Result<T> {
         // frb-coverage:ignore-end
-        self.into_inner()
+        Ok(self.into_inner()
             .context("Cannot convert RustOpaque to inner value. This is probably because you are having more than one references to it.")?
-            .into_inner().map_err(|_| anyhow!("lock is poisoned"))
+            .into_inner())
     }
 
     pub fn rust_auto_opaque_decode_ref(&self) -> Result<RwLockReadGuard<'_, T>> {
-        self.blocking_read()
-            .map_err(|_| anyhow!("lock is poisioned"))
+        Ok(self.blocking_read())
     }
 
     pub fn rust_auto_opaque_decode_ref_mut(&self) -> Result<RwLockWriteGuard<'_, T>> {
-        self.blocking_write()
-            .map_err(|_| anyhow!("lock is poisioned"))
+        Ok(self.blocking_write())
     }
 }
 
