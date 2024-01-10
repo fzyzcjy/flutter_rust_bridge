735b002295955d8776022b4ab306f13eb62ce6d7
fzyzcjy <ch271828n@outlook.com>
2024-01-10 21:29:10 +0800

    chore: lint

--- frb_example/pure_dart/rust/src/api/misc_no_twin_example_a.rs
+++ frb_example/pure_dart/rust/src/api/misc_no_twin_example_a.rs
@@ -1,6 +1,5 @@
 // FRB_INTERNAL_GENERATOR: {"forbiddenDuplicatorModes": ["sync", "rustAsync", "sse", "syncSse", "rustAsyncSse"]}
 
-use crate::api::misc_no_twin_example_b::StructInMiscNoTwinExampleB;
 use flutter_rust_bridge::frb;
 
 // Reproduce #1630
