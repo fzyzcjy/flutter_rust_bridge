2edf1f2f69cb4b33c68534810a77c040a8e000b0
fzyzcjy <ch271828n@outlook.com>
2024-01-10 20:56:57 +0800

    chore: partially reproduce

--- frb_example/pure_dart/rust/src/api/misc_no_twin_example.rs
+++ frb_example/pure_dart/rust/src/api/misc_no_twin_example.rs
@@ -1,3 +1,13 @@
 // FRB_INTERNAL_GENERATOR: {"forbiddenDuplicatorModes": ["sync", "rustAsync", "sse", "syncSse", "rustAsyncSse"]}
 
-// TODO
+use flutter_rust_bridge::frb;
+
+// Reproduce #1630
+#[frb(opaque)]
+pub struct StructInMiscNoTwinExample;
+
+impl StructInMiscNoTwinExample {
+    pub async fn get_struct_in_other_files(&self) -> FeatureA {
+        TODO
+    }
+}
