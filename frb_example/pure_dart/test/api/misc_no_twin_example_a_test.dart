5e75e484d44d2b27399661cb4a98eaf732d9773c
fzyzcjy <ch271828n@outlook.com>
2024-01-10 21:03:14 +0800

    chore: comments

--- frb_example/pure_dart/test/api/misc_no_twin_example_a_test.dart
+++ frb_example/pure_dart/test/api/misc_no_twin_example_a_test.dart
@@ -5,5 +5,5 @@ import 'package:frb_example_pure_dart/src/rust/frb_generated.dart';
 Future<void> main({bool skipRustLibInit = false}) async {
   if (!skipRustLibInit) await RustLib.init();
 
-  // TODO
+  // nothing yet
 }
