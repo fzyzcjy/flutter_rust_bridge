f7b858f40a3d030d8318c504b59b95d808de934f
fzyzcjy <ch271828n@outlook.com>
2024-01-10 20:58:58 +0800

    feat: more repro

--- frb_example/pure_dart/rust/src/api/misc_no_twin_example_a.rs
+++ frb_example/pure_dart/rust/src/api/misc_no_twin_example_a.rs
@@ -4,10 +4,4 @@ use flutter_rust_bridge::frb;
 
 // Reproduce #1630
 #[frb(opaque)]
-pub struct StructInMiscNoTwinExample;
-
-impl StructInMiscNoTwinExample {
-    pub async fn get_struct_in_other_files(&self) -> FeatureA {
-        TODO
-    }
-}
+pub struct StructInMiscNoTwinExampleA;
