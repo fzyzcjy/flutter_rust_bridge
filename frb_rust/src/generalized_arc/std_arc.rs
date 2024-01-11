437c99d757b00122fd0ae08e94fca84b37083723
fzyzcjy <ch271828n@outlook.com>
2024-01-11 18:00:56 +0800

    fix: from

--- frb_rust/src/generalized_arc/std_arc.rs
+++ frb_rust/src/generalized_arc/std_arc.rs
@@ -1,4 +1,6 @@
 use crate::generalized_arc::base_arc::BaseArc;
+use crate::rust_opaque::codec::nom::NomRustOpaqueCodec;
+use crate::RustOpaque;
 use std::sync::Arc;
 
 pub(crate) struct StdArc<T: ?Sized>(Arc<T>);
@@ -14,5 +16,11 @@ impl<T> BaseArc<T> for StdArc<T> {
 
     fn into_inner(self) -> Option<T> {
         Arc::into_inner(self.0)
+    }
+}
+
+impl<T: ?Sized> From<Arc<T>> for StdArc<T> {
+    fn from(ptr: Arc<T>) -> Self {
+        Self(ptr)
     }
 }
