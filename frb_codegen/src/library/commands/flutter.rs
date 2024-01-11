1ca51d66873d948ad365af696ba92092943731c2
wxitcode <ifishcode@163.com>
2024-01-11 21:01:08 +0800

    chore: change to match

--- frb_codegen/src/library/commands/flutter.rs
+++ frb_codegen/src/library/commands/flutter.rs
@@ -6,17 +6,17 @@ use std::path::Path;
 
 #[allow(clippy::vec_init_then_push)]
 pub fn flutter_create(name: &str, org: &Option<String>) -> anyhow::Result<()> {
-    if org.is_none() {
-        info!("Execute `flutter create {name}`");
-        check_exit_code(
-            &command_run!(call_shell[None, None], "flutter", "create", name )?,
-        )
-    } else {
-        let org_v = org.as_deref().unwrap();
-        info!("Execute `flutter create  --org {org_v} {name}`");
-        check_exit_code(
-            &command_run!(call_shell[None, None], "flutter", "create", "--org", org_v, name )?,
-        )
+    match org {
+        Some(o) => {
+            info!("Execute `flutter create  --org {o} {name}`");
+            check_exit_code(
+                &command_run!(call_shell[None, None], "flutter", "create", "--org", o, name )?,
+            )
+        }
+        None => {
+            info!("Execute `flutter create {name}`");
+            check_exit_code(&command_run!(call_shell[None, None], "flutter", "create", name )?)
+        }
     }
 }
 
