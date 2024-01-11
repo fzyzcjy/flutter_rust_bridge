f16a71ee91c3e6a7a4ac308218db720f4e3772a4
fzyzcjy <ch271828n@outlook.com>
2024-01-11 21:48:54 +0800

    chore: try

--- frb_rust/src/rust_opaque/codec/mod.rs
+++ frb_rust/src/rust_opaque/codec/mod.rs
@@ -4,5 +4,6 @@ pub(crate) mod moi;
 pub(crate) mod nom;
 
 pub trait BaseRustOpaqueCodec: Clone + Copy + Send + Sync {
-    type Arc<T: ?Sized + 'static>: BaseArc<T>;
+    type ArcValue;
+    type Arc<T: ?Sized + Self::ArcValue>: BaseArc<T>;
 }
--- frb_rust/src/rust_opaque/codec/moi.rs
+++ frb_rust/src/rust_opaque/codec/moi.rs
@@ -1,9 +1,9 @@
 use super::BaseRustOpaqueCodec;
-use crate::generalized_arc::map_based_arc::MapBasedArc;
+use crate::generalized_arc::map_based_arc::{MapBasedArc, MapBasedArcValue};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct MoiRustOpaqueCodec;
 
 impl BaseRustOpaqueCodec for MoiRustOpaqueCodec {
-    type Arc<T: ?Sized + 'static> = MapBasedArc<T>;
+    type Arc<T: ?Sized + MapBasedArcValue> = MapBasedArc<T>;
 }
