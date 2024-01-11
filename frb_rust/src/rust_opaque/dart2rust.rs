00d9678e1f00f44b4d9e47f05d22bf451e5b0c16
fzyzcjy <ch271828n@outlook.com>
2024-01-11 21:29:19 +0800

    feat: rm some

--- frb_rust/src/rust_opaque/dart2rust.rs
+++ frb_rust/src/rust_opaque/dart2rust.rs
@@ -1,45 +1,21 @@
 use super::RustOpaque;
 use crate::generalized_arc::base_arc::BaseArc;
+use crate::rust_opaque::codec::moi::MoiRustOpaqueCodec;
+use crate::rust_opaque::codec::nom::NomRustOpaqueCodec;
 use crate::rust_opaque::codec::BaseRustOpaqueCodec;
 
 /// # Safety
 ///
-/// This function should never be called manually.
-/// Retrieving an opaque pointer from Dart is an implementation detail, so this
-/// function is not guaranteed to be API-stable.
-#[cfg(not(wasm))]
-pub unsafe fn cst_decode_rust_opaque<T, C: BaseRustOpaqueCodec>(ptr: usize) -> RustOpaque<T, C> {
-    decode_rust_opaque_inner(ptr as _)
-}
-
-/// # Safety
-///
 /// This should never be called manually.
-#[cfg(wasm)]
-pub unsafe fn cst_decode_rust_opaque<T, C: BaseRustOpaqueCodec>(
-    raw: wasm_bindgen::JsValue,
-) -> RustOpaque<T, C> {
-    #[cfg(target_pointer_width = "64")]
-    {
-        compile_error!("64-bit pointers are not supported.");
-    }
-
-    decode_rust_opaque_inner((raw.as_f64().unwrap() as usize) as _)
-}
-
-/// # Safety
-///
-/// This should never be called manually.
-pub unsafe fn sse_decode_rust_opaque<T, C: BaseRustOpaqueCodec>(ptr: usize) -> RustOpaque<T, C> {
-    decode_rust_opaque_inner(ptr)
-}
-
-/// # Safety
-///
-/// This should never be called manually.
-unsafe fn decode_rust_opaque_inner<T, C: BaseRustOpaqueCodec>(ptr: usize) -> RustOpaque<T, C> {
+unsafe fn decode_rust_opaque_nom<T>(ptr: usize) -> RustOpaque<T, NomRustOpaqueCodec> {
     assert!(ptr != 0);
     RustOpaque {
-        arc: C::Arc::from_raw(ptr),
+        arc: NomRustOpaqueCodec::Arc::from_raw(ptr),
+    }
+}
+
+fn decode_rust_opaque_moi<T>(ptr: usize) -> RustOpaque<T, MoiRustOpaqueCodec> {
+    RustOpaque {
+        arc: MoiRustOpaqueCodec::Arc::from_raw(ptr),
     }
 }
