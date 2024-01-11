c20d368522adb928365090378bca0097b393076d
fzyzcjy <ch271828n@outlook.com>
2024-01-11 21:01:20 +0800

    refactor: more

--- frb_rust/src/misc/rust_arc.rs
+++ frb_rust/src/misc/rust_arc.rs
@@ -1,19 +1,20 @@
 use crate::rust_opaque::codec::moi::MoiRustOpaqueCodec;
+use crate::rust_opaque::codec::nom::NomRustOpaqueCodec;
 use crate::rust_opaque::codec::BaseRustOpaqueCodec;
 use std::sync::Arc;
 
 /// # Safety
 ///
 /// This should never be called manually.
-pub unsafe fn rust_arc_increment_strong_count_nom<T>(ptr: *const std::ffi::c_void) {
-    Arc::<T>::increment_strong_count(ptr as _)
+pub unsafe fn rust_arc_increment_strong_count_nom<T>(raw: usize) {
+    <NomRustOpaqueCodec as BaseRustOpaqueCodec>::Arc::<T>::increment_strong_count(raw);
 }
 
 /// # Safety
 ///
 /// This should never be called manually.
-pub unsafe fn rust_arc_decrement_strong_count_nom<T>(ptr: *const std::ffi::c_void) {
-    Arc::<T>::decrement_strong_count(ptr as _)
+pub unsafe fn rust_arc_decrement_strong_count_nom<T>(raw: usize) {
+    <NomRustOpaqueCodec as BaseRustOpaqueCodec>::Arc::<T>::decrement_strong_count(raw);
 }
 
 // This is a separate function, because it is safe (not `unsafe`)
