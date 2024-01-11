811f5737dcd315d4413ea8b2297da004000a1211
fzyzcjy <ch271828n@outlook.com>
2024-01-11 18:02:04 +0800

    feat: more

--- frb_rust/src/generalized_arc/base_arc.rs
+++ frb_rust/src/generalized_arc/base_arc.rs
@@ -1,9 +1,14 @@
-pub(crate) trait BaseArc<T> {
-    fn new(value: T) -> Self;
+pub(crate) trait BaseArc<T: ?Sized> {
+    fn new(value: T) -> Self
+    where
+        T: Sized;
 
     fn try_unwrap(self) -> Result<T, Self>
     where
-        Self: Sized;
+        Self: Sized,
+        T: Sized;
 
-    fn into_inner(self) -> Option<T>;
+    fn into_inner(self) -> Option<T>
+    where
+        T: Sized;
 }
--- frb_rust/src/generalized_arc/std_arc.rs
+++ frb_rust/src/generalized_arc/std_arc.rs
@@ -5,16 +5,25 @@ use std::sync::Arc;
 
 pub(crate) struct StdArc<T: ?Sized>(Arc<T>);
 
-impl<T> BaseArc<T> for StdArc<T> {
-    fn new(value: T) -> Self {
+impl<T: ?Sized> BaseArc<T> for StdArc<T> {
+    fn new(value: T) -> Self
+    where
+        T: Sized,
+    {
         Self(Arc::new(value))
     }
 
-    fn try_unwrap(self) -> Result<T, Self> {
-        Arc::try_unwrap(self.0)
+    fn try_unwrap(self) -> Result<T, Self>
+    where
+        T: Sized,
+    {
+        Arc::try_unwrap(self.0).map_err(Self)
     }
 
-    fn into_inner(self) -> Option<T> {
+    fn into_inner(self) -> Option<T>
+    where
+        T: Sized,
+    {
         Arc::into_inner(self.0)
     }
 }
