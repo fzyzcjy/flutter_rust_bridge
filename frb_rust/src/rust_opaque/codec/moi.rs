35847cc7db8c6714d7e317e36dd21a754e842d45
fzyzcjy <ch271828n@outlook.com>
2024-01-11 18:29:28 +0800

    chore: lint

--- frb_rust/src/rust_opaque/codec/moi.rs
+++ frb_rust/src/rust_opaque/codec/moi.rs
@@ -1,11 +1,9 @@
 use super::BaseRustOpaqueCodec;
 use crate::generalized_arc::map_based_arc::MapBasedArc;
-use crate::generalized_arc::std_arc::StdArc;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct MoiRustOpaqueCodec;
 
 impl BaseRustOpaqueCodec for MoiRustOpaqueCodec {
-    // TODO
     type Arc<T: ?Sized> = MapBasedArc<T>;
 }
