7e0dab649079db5d8645940c4fe54d9947cb6f42
fzyzcjy <ch271828n@outlook.com>
2024-01-11 21:17:57 +0800

    feat: postfix

--- frb_rust/src/rust_opaque/dart2rust.rs
+++ frb_rust/src/rust_opaque/dart2rust.rs
@@ -3,7 +3,7 @@ use crate::generalized_arc::base_arc::BaseArc;
 use crate::rust_opaque::codec::BaseRustOpaqueCodec;
 
 macro_rules! impl_functions {
-    ($T:ty) => {
+    ($postfix:ident) => {
         paste::paste! {
             /// # Safety
             ///
@@ -11,17 +11,17 @@ macro_rules! impl_functions {
             /// Retrieving an opaque pointer from Dart is an implementation detail, so this
             /// function is not guaranteed to be API-stable.
             #[cfg(not(wasm))]
-            pub unsafe fn cst_decode_rust_opaque<T, C: BaseRustOpaqueCodec>(
+            pub unsafe fn [<cst_decode_rust_opaque_ $postfix>]<T, C: BaseRustOpaqueCodec>(
                 ptr: usize,
             ) -> RustOpaque<T, C> {
-                decode_rust_opaque_inner(ptr as _)
+                [<decode_rust_opaque_innerr_ $postfix>](ptr as _)
             }
 
             /// # Safety
             ///
             /// This should never be called manually.
             #[cfg(wasm)]
-            pub unsafe fn cst_decode_rust_opaque<T, C: BaseRustOpaqueCodec>(
+            pub unsafe fn [<cst_decode_rust_opaque_ $postfix>]<T, C: BaseRustOpaqueCodec>(
                 raw: wasm_bindgen::JsValue,
             ) -> RustOpaque<T, C> {
                 #[cfg(target_pointer_width = "64")]
@@ -29,22 +29,22 @@ macro_rules! impl_functions {
                     compile_error!("64-bit pointers are not supported.");
                 }
 
-                decode_rust_opaque_inner((raw.as_f64().unwrap() as usize) as _)
+                [<decode_rust_opaque_innerr_ $postfix>]((raw.as_f64().unwrap() as usize) as _)
             }
 
             /// # Safety
             ///
             /// This should never be called manually.
-            pub unsafe fn sse_decode_rust_opaque<T, C: BaseRustOpaqueCodec>(
+            pub unsafe fn [<sse_decode_rust_opaque_ $postfix>]<T, C: BaseRustOpaqueCodec>(
                 ptr: usize,
             ) -> RustOpaque<T, C> {
-                decode_rust_opaque_inner(ptr)
+                [<decode_rust_opaque_innerr_ $postfix>](ptr)
             }
 
             /// # Safety
             ///
             /// This should never be called manually.
-            unsafe fn decode_rust_opaque_inner<T, C: BaseRustOpaqueCodec>(
+            unsafe fn [<decode_rust_opaque_innerr_ $postfix>]<T, C: BaseRustOpaqueCodec>(
                 ptr: usize,
             ) -> RustOpaque<T, C> {
                 assert!(ptr != 0);
@@ -55,3 +55,6 @@ macro_rules! impl_functions {
         }
     };
 }
+
+impl_functions!(nom);
+impl_functions!(moi);
