e861b96afed3c81aa4f808b6dc237e07deb4f418
fzyzcjy <ch271828n@outlook.com>
2024-01-11 22:08:46 +0800

    feat: add MapBasedArcValue

--- frb_rust/src/rust_opaque/codec/moi.rs
+++ frb_rust/src/rust_opaque/codec/moi.rs
@@ -1,9 +1,9 @@
 use super::BaseRustOpaqueCodec;
-use crate::generalized_arc::map_based_arc::MapBasedArc;
+use crate::generalized_arc::map_based_arc::{MapBasedArc, MapBasedArcValue};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct MoiRustOpaqueCodec;
 
-impl<T: ?Sized + 'static> BaseRustOpaqueCodec<T> for MoiRustOpaqueCodec {
+impl<T: ?Sized + 'static + MapBasedArcValue> BaseRustOpaqueCodec<T> for MoiRustOpaqueCodec {
     type Arc = MapBasedArc<T>;
 }
