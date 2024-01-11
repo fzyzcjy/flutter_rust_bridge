b451514f422ca378f7e5069dc8a48ee09fc28531
fzyzcjy <ch271828n@outlook.com>
2024-01-11 17:52:00 +0800

    chore: empty

---
+++ frb_rust/src/generalized_arc/mod.rs
@@ -0,0 +1,3 @@
+pub(crate) mod base_arc;
+pub(crate) mod map_based_arc;
+pub(crate) mod std_arc;
--- frb_rust/src/lib.rs
+++ frb_rust/src/lib.rs
@@ -14,6 +14,7 @@ pub(crate) mod dart_opaque;
 pub(crate) mod ffi_binding;
 #[doc(hidden)]
 pub mod for_generated;
+pub(crate) mod generalized_arc;
 #[doc(hidden)] // only to be used as `for_generated::rust_async`
 pub mod rust_async;
 pub(crate) mod rust_opaque;
