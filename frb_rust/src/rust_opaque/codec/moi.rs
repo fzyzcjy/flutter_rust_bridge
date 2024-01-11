19f09c4cd102b2992a70fa50592732207dafa138
fzyzcjy <ch271828n@outlook.com>
2024-01-11 22:07:25 +0800

    fix: some

--- frb_rust/src/rust_opaque/codec/moi.rs
+++ frb_rust/src/rust_opaque/codec/moi.rs
@@ -1,8 +1,9 @@
 use super::BaseRustOpaqueCodec;
 use crate::generalized_arc::map_based_arc::MapBasedArc;
+use std::marker::PhantomData;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct MoiRustOpaqueCodec<T: ?Sized + 'static>;
+pub struct MoiRustOpaqueCodec<T: ?Sized + 'static>(PhantomData<T>);
 
 impl<T: ?Sized + 'static> BaseRustOpaqueCodec<T> for MoiRustOpaqueCodec<T> {
     type Arc = MapBasedArc<T>;
--- frb_rust/src/rust_opaque/codec/nom.rs
+++ frb_rust/src/rust_opaque/codec/nom.rs
@@ -1,8 +1,9 @@
 use super::BaseRustOpaqueCodec;
 use crate::generalized_arc::std_arc::StdArc;
+use std::marker::PhantomData;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct NomRustOpaqueCodec<T: ?Sized + 'static>;
+pub struct NomRustOpaqueCodec<T: ?Sized + 'static>(PhantomData<T>);
 
 impl<T: ?Sized + 'static> BaseRustOpaqueCodec<T> for NomRustOpaqueCodec<T> {
     type Arc = StdArc<T>;
