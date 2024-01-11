cb93c32cb32c1d6c137a4c4dd6749ea65b752e80
fzyzcjy <ch271828n@outlook.com>
2024-01-11 22:09:24 +0800

    fix: compile

--- frb_rust/src/rust_opaque/dart2rust.rs
+++ frb_rust/src/rust_opaque/dart2rust.rs
@@ -1,5 +1,6 @@
 use super::RustOpaque;
 use crate::generalized_arc::base_arc::BaseArc;
+use crate::generalized_arc::map_based_arc::MapBasedArcValue;
 use crate::rust_opaque::codec::moi::MoiRustOpaqueCodec;
 use crate::rust_opaque::codec::nom::NomRustOpaqueCodec;
 use crate::rust_opaque::codec::BaseRustOpaqueCodec;
@@ -10,13 +11,15 @@ use crate::rust_opaque::codec::BaseRustOpaqueCodec;
 pub unsafe fn decode_rust_opaque_nom<T>(ptr: usize) -> RustOpaque<T, NomRustOpaqueCodec> {
     assert!(ptr != 0);
     RustOpaque {
-        arc: <NomRustOpaqueCodec as BaseRustOpaqueCodec>::Arc::from_raw(ptr),
+        arc: <NomRustOpaqueCodec as BaseRustOpaqueCodec<T>>::Arc::from_raw(ptr),
     }
 }
 
 // This does not have `unsafe` keyword, thus is a separate function
-pub fn decode_rust_opaque_moi<T>(ptr: usize) -> RustOpaque<T, MoiRustOpaqueCodec> {
+pub fn decode_rust_opaque_moi<T: MapBasedArcValue>(
+    ptr: usize,
+) -> RustOpaque<T, MoiRustOpaqueCodec> {
     RustOpaque {
-        arc: <MoiRustOpaqueCodec as BaseRustOpaqueCodec>::Arc::from_raw(ptr),
+        arc: <MoiRustOpaqueCodec as BaseRustOpaqueCodec<T>>::Arc::from_raw(ptr),
     }
 }
