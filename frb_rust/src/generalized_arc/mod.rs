d454436693d6aaf400ebc4e695f25cd2d62c3074
fzyzcjy <ch271828n@outlook.com>
2024-01-11 20:23:54 +0800

    chore: empty test

--- frb_rust/src/generalized_arc/mod.rs
+++ frb_rust/src/generalized_arc/mod.rs
@@ -4,10 +4,24 @@ pub(crate) mod std_arc;
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use crate::generalized_arc::base_arc::BaseArc;
+    use crate::generalized_arc::map_based_arc::MapBasedArc;
+    use crate::generalized_arc::std_arc::StdArc;
+
+    // Do NOT make it `clone` (to test non-clone behavior)
+    struct DummyType(String);
 
     #[test]
-    fn test_TODO() {
-        todo!();
+    fn test_std_arc() {
+        body::<StdArc<DummyType>>();
+    }
+
+    #[test]
+    fn test_map_based_arc() {
+        body::<MapBasedArc<DummyType>>();
+    }
+
+    fn body<T: BaseArc<DummyType>>() {
+        todo!()
     }
 }
