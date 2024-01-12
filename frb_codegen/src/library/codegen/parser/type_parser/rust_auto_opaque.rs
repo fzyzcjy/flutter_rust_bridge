c80457c3ae68232e26a79d2fe285efba5c21f2fc
fzyzcjy <ch271828n@outlook.com>
2024-01-12 10:09:45 +0800

    chore: lint

--- frb_codegen/src/library/codegen/parser/type_parser/rust_auto_opaque.rs
+++ frb_codegen/src/library/codegen/parser/type_parser/rust_auto_opaque.rs
@@ -1,7 +1,7 @@
 use crate::codegen::ir::pack::DistinctTypeGatherer;
 use crate::codegen::ir::ty::ownership::IrTypeOwnershipMode;
 use crate::codegen::ir::ty::rust_auto_opaque::IrTypeRustAutoOpaque;
-use crate::codegen::ir::ty::rust_opaque::{IrTypeRustOpaque, RustOpaqueCodecMode};
+use crate::codegen::ir::ty::rust_opaque::IrTypeRustOpaque;
 use crate::codegen::ir::ty::unencodable::IrTypeUnencodable;
 use crate::codegen::ir::ty::IrType;
 use crate::codegen::parser::type_parser::rust_opaque::{
