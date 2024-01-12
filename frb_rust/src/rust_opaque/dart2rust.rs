83d3c2f6d510e141eb3b1f4396a8dd3962c152bf
fzyzcjy <ch271828n@outlook.com>
2024-01-12 15:51:30 +0800

    feat: more

--- frb_rust/src/rust_opaque/dart2rust.rs
+++ frb_rust/src/rust_opaque/dart2rust.rs
@@ -2,21 +2,19 @@
 #[macro_export]
 macro_rules! frb_generated_rust_opaque_dart2rust {
     () => {
-        use $crate::for_generated::{BaseRustOpaqueCodec, NomRustOpaqueCodec};
+        use $crate::for_generated::StdArc;
         use $crate::RustOpaque;
 
         /// # Safety
         ///
         /// This should never be called manually.
-        pub unsafe fn decode_rust_opaque_nom<T>(ptr: usize) -> RustOpaque<T, NomRustOpaqueCodec> {
+        pub unsafe fn decode_rust_opaque_nom<T>(ptr: usize) -> RustOpaque<T, StdArc<_>> {
             assert!(ptr != 0);
             RustOpaque::from_arc(<NomRustOpaqueCodec as BaseRustOpaqueCodec<T>>::Arc::from_raw(ptr))
         }
 
         // This does not have `unsafe` keyword, thus is a separate function
-        pub fn decode_rust_opaque_moi<T: MoiArcValue>(
-            ptr: usize,
-        ) -> RustOpaque<T, MoiRustOpaqueCodec> {
+        pub fn decode_rust_opaque_moi<T: MoiArcValue>(ptr: usize) -> RustOpaque<T, MoiArc<_>> {
             RustOpaque::from_arc(<MoiRustOpaqueCodec as BaseRustOpaqueCodec<T>>::Arc::from_raw(ptr))
         }
     };
