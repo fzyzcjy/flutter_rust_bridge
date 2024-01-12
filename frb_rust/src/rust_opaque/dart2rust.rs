9eaf03288d5c3298d692926a1d85410f8c4eb0a4
fzyzcjy <ch271828n@outlook.com>
2024-01-12 13:24:08 +0800

    fix: private

--- frb_rust/src/rust_opaque/dart2rust.rs
+++ frb_rust/src/rust_opaque/dart2rust.rs
@@ -10,18 +10,14 @@ macro_rules! frb_generated_rust_opaque_dart2rust {
         /// This should never be called manually.
         pub unsafe fn decode_rust_opaque_nom<T>(ptr: usize) -> RustOpaque<T, NomRustOpaqueCodec> {
             assert!(ptr != 0);
-            RustOpaque {
-                arc: <NomRustOpaqueCodec as BaseRustOpaqueCodec<T>>::Arc::from_raw(ptr),
-            }
+            RustOpaque::from_arc(<NomRustOpaqueCodec as BaseRustOpaqueCodec<T>>::Arc::from_raw(ptr))
         }
 
         // This does not have `unsafe` keyword, thus is a separate function
         pub fn decode_rust_opaque_moi<T: MapBasedArcValue>(
             ptr: usize,
         ) -> RustOpaque<T, MoiRustOpaqueCodec> {
-            RustOpaque {
-                arc: <MoiRustOpaqueCodec as BaseRustOpaqueCodec<T>>::Arc::from_raw(ptr),
-            }
+            RustOpaque::from_arc(<MoiRustOpaqueCodec as BaseRustOpaqueCodec<T>>::Arc::from_raw(ptr))
         }
     };
 }
