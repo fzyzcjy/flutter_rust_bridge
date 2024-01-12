c2b991018e7c4993a69b13ad5964b655a7092177
fzyzcjy <ch271828n@outlook.com>
2024-01-12 09:32:51 +0800

    feat: cst

--- frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/codec/cst/decoder/ty/rust_opaque.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/codec/cst/decoder/ty/rust_opaque.rs
@@ -3,27 +3,38 @@ use crate::codegen::generator::misc::target::Target;
 use crate::codegen::generator::wire::rust::spec_generator::codec::cst::base::*;
 use crate::codegen::generator::wire::rust::spec_generator::codec::cst::decoder::misc::JS_VALUE;
 use crate::codegen::generator::wire::rust::spec_generator::codec::cst::decoder::ty::WireRustCodecCstGeneratorDecoderTrait;
+use crate::codegen::ir::ty::rust_opaque::RustOpaqueCodecMode;
+use convert_case::{Case, Casing};
 use std::borrow::Cow;
 
 impl<'a> WireRustCodecCstGeneratorDecoderTrait for RustOpaqueWireRustCodecCstGenerator<'a> {
     fn generate_impl_decode_body(&self) -> Acc<Option<String>> {
         Acc {
-            io: Some(generalized_rust_opaque_generate_impl_decode_body().into()),
+            io: Some(format!(
+                "unsafe {{ flutter_rust_bridge::for_generated::decode_rust_opaque_{}(self as _) }}",
+                self.ir.codec.to_string().to_case(Case::Snake),
+            )),
             ..Default::default()
         }
     }
 
     fn generate_impl_decode_jsvalue_body(&self) -> Option<Cow<str>> {
-        Some(generalized_rust_opaque_generate_impl_decode_body().into())
+        Some(
+            format!(
+                r#"
+                #[cfg(target_pointer_width = "64")]
+                {{ compile_error!("64-bit pointers are not supported."); }}
+                unsafe {{ flutter_rust_bridge::for_generated::decode_rust_opaque_{}((self.as_f64().unwrap() as usize) as _) }}
+                "#,
+                self.ir.codec.to_string().to_case(Case::Snake),
+            )
+            .into(),
+        )
     }
 
     fn rust_wire_type(&self, target: Target) -> String {
         dart_opaque_or_generalized_rust_opaque_rust_wire_type(target)
     }
-}
-
-pub(super) fn generalized_rust_opaque_generate_impl_decode_body() -> &'static str {
-    r#"unsafe { flutter_rust_bridge::for_generated::cst_decode_rust_opaque(self) }"#
 }
 
 pub(super) fn dart_opaque_or_generalized_rust_opaque_rust_wire_type(target: Target) -> String {
