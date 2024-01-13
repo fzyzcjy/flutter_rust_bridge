30d29e8d0eca9ec7e98390aca23fac5abf1c38c8
fzyzcjy <ch271828n@outlook.com>
2024-01-13 14:48:19 +0800

    feat: postfix

--- tools/frb_internal/lib/src/frb_example_pure_dart_generator/rust/direct_sources.dart
+++ tools/frb_internal/lib/src/frb_example_pure_dart_generator/rust/direct_sources.dart
@@ -4,21 +4,23 @@ import 'package:flutter_rust_bridge_internal/src/frb_example_pure_dart_generator
 
 Map<String, String> generateRustDirectSources() {
   return {
-    'pseudo_manual/basic.rs': _generateBasicRelated((x) => x),
+    'pseudo_manual/basic.rs': _generateBasicRelated((x) => x, ''),
     'pseudo_manual/optional_basic.rs':
-        _generateBasicRelated((x) => 'Option<$x>'),
-    'pseudo_manual/basic_list.rs': _generateBasicRelated((x) => 'Vec<$x>'),
+        _generateBasicRelated((x) => 'Option<$x>', '_optional'),
+    'pseudo_manual/basic_list.rs':
+        _generateBasicRelated((x) => 'Vec<$x>', '_list'),
     'pseudo_manual/basic_map.rs':
-        _generateBasicRelated((x) => 'HashMap<i32, $x>'),
+        _generateBasicRelated((x) => 'HashMap<i32, $x>', '_map'),
     'pseudo_manual/benchmark_api.rs': generateBenchmark(),
   };
 }
 
-String _generateBasicRelated(String Function(String) rustTypeNameWrapper) {
+String _generateBasicRelated(
+    String Function(String) rustTypeNameWrapper, String postfix) {
   final builder = RustFileBuilder();
   for (final ty in kBasicTypes) {
-    builder.addIdentityFunction(
-        rustTypeNameWrapper(ty.rustTypeName), 'example_basic_type_${ty.name}');
+    builder.addIdentityFunction(rustTypeNameWrapper(ty.rustTypeName),
+        'example_basic${postfix}_type_${ty.name}');
   }
   return builder.toString();
 }
