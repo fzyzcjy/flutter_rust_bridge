d18fd14f51a72e410f2519c5260f8804f69f85d9
fzyzcjy <ch271828n@outlook.com>
2024-01-13 17:41:50 +0800

    fix: more

--- frb_rust/src/misc/logs.rs
+++ frb_rust/src/misc/logs.rs
@@ -1,6 +1,14 @@
 pub(crate) fn log_warn_or_println(message: &str) {
-    if log::log_enabled!(log::Level::Warn) {
+    #[cfg(feature = "log")]
+    let log_enabled = log::log_enabled!(log::Level::Warn);
+    #[cfg(not(feature = "log"))]
+    let log_enabled = false;
+
+    if log_enabled {
+        #[cfg(feature = "log")]
         log::warn!("{}", message);
+        #[cfg(not(feature = "log"))]
+        unreachable!();
     } else {
         println!("{}", message);
     }
