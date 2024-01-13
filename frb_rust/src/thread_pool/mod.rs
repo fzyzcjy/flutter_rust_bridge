24c0b16a5f292fcd8c3ca9c181502cb3408ecacf
fzyzcjy <ch271828n@outlook.com>
2024-01-13 17:04:45 +0800

    chore: stub

--- frb_rust/src/thread_pool/mod.rs
+++ frb_rust/src/thread_pool/mod.rs
@@ -1,9 +1,18 @@
+#[cfg(feature = "thread-pool")]
 #[cfg(wasm)]
 mod web;
+#[cfg(feature = "thread-pool")]
 #[cfg(wasm)]
 pub use web::*;
 
+#[cfg(feature = "thread-pool")]
 #[cfg(not(wasm))]
 mod io;
+#[cfg(feature = "thread-pool")]
 #[cfg(not(wasm))]
 pub use io::*;
+
+#[cfg(not(feature = "thread-pool"))]
+mod stub;
+#[cfg(not(feature = "thread-pool"))]
+pub use stub::*;
