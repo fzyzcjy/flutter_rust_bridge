54fa482d2ac86846a3957529e2e1a2b1cae75e68
fzyzcjy <ch271828n@outlook.com>
2024-01-14 18:08:34 +0800

    refactor: temp not consider Sync mode, do later

--- frb_dart/lib/src/codec/pde.dart
+++ frb_dart/lib/src/codec/pde.dart
@@ -2,7 +2,6 @@ import 'package:flutter_rust_bridge/src/codec/sse.dart';
 import 'package:flutter_rust_bridge/src/platform_types/platform_types.dart';
 
 /// {@macro flutter_rust_bridge.only_for_generated_code}
-WireSyncRust2DartSse? pdeCallFfi(
-    NativePortType? port, SseSerializer serializer) {
+void pdeCallFfi(NativePortType port, SseSerializer serializer) {
   return TODO;
 }
