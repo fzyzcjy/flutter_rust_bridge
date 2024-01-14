7d4a3d4df19400427ca21a8187475d51195167cf
fzyzcjy <ch271828n@outlook.com>
2024-01-14 17:34:36 +0800

    chore: empty

---
+++ frb_dart/lib/src/codec/pde.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_rust_bridge/src/codec/sse.dart';
+import 'package:flutter_rust_bridge/src/platform_types/platform_types.dart';
+
+void pdeTODO(NativePortType? port, SseSerializer serializer) {
+  TODO;
+}
