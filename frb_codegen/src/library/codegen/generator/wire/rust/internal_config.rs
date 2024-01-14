7ad2213e3a04c969dbb52e46b80e93cf4e9f9f7d
fzyzcjy <ch271828n@outlook.com>
2024-01-14 21:54:57 +0800

    feat: cfg

--- frb_codegen/src/library/codegen/generator/wire/rust/internal_config.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/internal_config.rs
@@ -1,4 +1,5 @@
 use crate::codegen::config::internal_config::RustInputPathPack;
+use crate::codegen::generator::codec::structs::CodecMode;
 use crate::codegen::generator::misc::target::TargetOrCommonMap;
 use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
@@ -11,4 +12,5 @@ pub(crate) struct GeneratorWireRustInternalConfig {
     pub rust_output_path: TargetOrCommonMap<PathBuf>,
     pub c_symbol_prefix: String,
     pub enable_extern_func_and_class: bool,
+    pub default_stream_sink_codec: CodecMode,
 }
