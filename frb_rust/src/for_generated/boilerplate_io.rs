0bb2daaffcbebb6085a0eed178014eee59652263
fzyzcjy <ch271828n@outlook.com>
2024-01-14 18:37:14 +0800

    refactor: more

--- frb_rust/src/for_generated/boilerplate_io.rs
+++ frb_rust/src/for_generated/boilerplate_io.rs
@@ -2,7 +2,14 @@
 #[macro_export]
 macro_rules! frb_generated_boilerplate_io {
     () => {
-        // For cst codec
+        $crate::frb_generated_io_cst_codec!();
+    };
+}
+
+#[doc(hidden)]
+#[macro_export]
+macro_rules! frb_generated_io_cst_codec {
+    () => {
         pub trait NewWithNullPtr {
             fn new_with_null_ptr() -> Self;
         }
--- frb_rust/src/for_generated/boilerplate_web.rs
+++ frb_rust/src/for_generated/boilerplate_web.rs
@@ -2,6 +2,14 @@
 #[macro_export]
 macro_rules! frb_generated_boilerplate_web {
     () => {
+        $crate::frb_generated_web_cst_codec!();
+    };
+}
+
+#[doc(hidden)]
+#[macro_export]
+macro_rules! frb_generated_web_cst_codec {
+    () => {
         impl<T> CstDecode<Option<T>> for flutter_rust_bridge::for_generated::wasm_bindgen::JsValue
         where
             JsValue: CstDecode<T>,
