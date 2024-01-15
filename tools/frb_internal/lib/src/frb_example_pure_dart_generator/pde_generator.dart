da365ef2fbf0fe45ae67161ae77ed8b68c37857f
fzyzcjy <ch271828n@outlook.com>
2024-01-15 10:53:54 +0800

    feat: prelude

--- tools/frb_internal/lib/src/frb_example_pure_dart_generator/pde_generator.dart
+++ tools/frb_internal/lib/src/frb_example_pure_dart_generator/pde_generator.dart
@@ -7,31 +7,35 @@ import 'package:path/path.dart';
 Future<void> generatePureDartPde({required Uri dirPureDart}) async {
   final dirPureDartPde = dirPureDart.resolve('../pure_dart_pde/');
 
-  copyRecursive(
-    Directory(dirPureDart.toFilePath()),
-    Directory(dirPureDartPde.toFilePath()),
-    filter: (entity) {
-      final relativePath =
-          relative(entity.path, from: dirPureDart.toFilePath());
-      return !const [
-        // gitignore them
-        '.dart_tool',
-        '.idea',
-        'benchmark',
-        'build',
-        'coverage',
-        'rust/target',
+  copyRecursive(Directory(dirPureDart.toFilePath()),
+      Directory(dirPureDartPde.toFilePath()), filter: (entity) {
+    final relativePath = relative(entity.path, from: dirPureDart.toFilePath());
+    return !const [
+      // gitignore them
+      '.dart_tool',
+      '.idea',
+      'benchmark',
+      'build',
+      'coverage',
+      'rust/target',
 
-        // will generate separately
-        'lib/src/rust',
-        'test/api/pseudo_manual',
-        'rust/src/api/pseudo_manual',
-        'rust/src/frb_generated.rs',
-        'rust/src/frb_generated.io.rs',
-        'rust/src/frb_generated.web.rs',
-      ].contains(relativePath);
-    },
-  );
+      // will generate separately
+      'lib/src/rust',
+      'test/api/pseudo_manual',
+      'rust/src/api/pseudo_manual',
+      'rust/src/frb_generated.rs',
+      'rust/src/frb_generated.io.rs',
+      'rust/src/frb_generated.web.rs',
+    ].contains(relativePath);
+  }, map: (file, text) {
+    final prelude = switch (extension(file.path)) {
+      '.rs' ||
+      '.dart' =>
+        '// AUTO-GENERATED FROM frb_example/pure_dart, DO NOT EDIT\n\n',
+      _ => '',
+    };
+    return prelude + text;
+  });
 }
 
 // copied and modified fromhttps://stackoverflow.com/questions/27204728
@@ -39,6 +43,7 @@ void copyRecursive(
   Directory src,
   Directory dst, {
   required bool Function(FileSystemEntity) filter,
+  required String Function(File, String) map,
 }) {
   if (!dst.existsSync()) {
     dst.createSync(recursive: true);
@@ -50,9 +55,9 @@ void copyRecursive(
     final newPath = join(dst.path, basename(entity.path));
     if (entity is File) {
       print('Copy $entity -> $newPath');
-      entity.copySync(newPath);
+      File(newPath).writeAsStringSync(map(entity, entity.readAsStringSync()));
     } else if (entity is Directory) {
-      copyRecursive(entity, Directory(newPath), filter: filter);
+      copyRecursive(entity, Directory(newPath), filter: filter, map: map);
     }
   }
 }
