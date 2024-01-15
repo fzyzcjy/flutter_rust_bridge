fc96e25c4fcc87e81494ecd1c272781e9ebeffe1
fzyzcjy <ch271828n@outlook.com>
2024-01-15 08:41:36 +0800

    chore: empty

--- frb_codegen/src/library/internal/frb_rust_source_code.rs
+++ frb_codegen/src/library/internal/frb_rust_source_code.rs
@@ -1,5 +1,15 @@
+use std::fs;
 use std::path::Path;
 
 pub(crate) fn generate_frb_rust_source_code(repo_base_dir: &Path) -> anyhow::Result<()> {
-    todo!()
+    let path_target = repo_base_dir
+        .join("frb_rust")
+        .join("src")
+        .join("internal_generated")
+        .join("mod.rs");
+
+    let text = TODO;
+
+    fs::write(path_target, text)?;
+    Ok(())
 }
