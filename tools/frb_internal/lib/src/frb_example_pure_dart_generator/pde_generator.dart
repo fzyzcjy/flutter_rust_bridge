56026b6395a97b8f1e05414dcb4cc30403b9830a
fzyzcjy <ch271828n@outlook.com>
2024-01-15 10:44:54 +0800

    feat: filter

--- tools/frb_internal/lib/src/frb_example_pure_dart_generator/pde_generator.dart
+++ tools/frb_internal/lib/src/frb_example_pure_dart_generator/pde_generator.dart
@@ -9,17 +9,23 @@ Future<void> generatePureDartPde({required Uri dirPureDart}) async {
 }
 
 // copied and modified fromhttps://stackoverflow.com/questions/27204728
-void copyRecursive(Directory src, Directory dst) {
+void copyRecursive(
+  Directory src,
+  Directory dst, {
+  required bool Function(FileSystemEntity) filter,
+}) {
   if (!dst.existsSync()) {
     dst.createSync(recursive: true);
   }
 
   for (final entity in src.listSync(recursive: false)) {
+    if (!filter(entity)) return;
+
     final newPath = join(dst.path, basename(entity.path));
     if (entity is File) {
       entity.copySync(newPath);
     } else if (entity is Directory) {
-      copyRecursive(entity, Directory(newPath));
+      copyRecursive(entity, Directory(newPath), filter: filter);
     }
   }
 }
