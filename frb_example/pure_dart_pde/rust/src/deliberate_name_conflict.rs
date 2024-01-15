6559f2c92a1f5dee091ef7a88e766b675e76b7ab
fzyzcjy <ch271828n@outlook.com>
2024-01-15 22:03:56 +0800

    chore: codegen

---
+++ frb_example/pure_dart_pde/rust/src/deliberate_name_conflict.rs
@@ -0,0 +1,9 @@
+// AUTO-GENERATED FROM frb_example/pure_dart, DO NOT EDIT
+
+// Please do not move or rename this file
+// This file paired with `src/api/deliberate_name_conflict.rs` are used to test module name conflict
+// Ensuring that `extract_module` in `frb_codegen\src\library\commands\cargo_expand.rs` can handle duplicated module names(though full path is not the same)
+
+pub struct StructInUpperLevel {
+    pub upper: usize,
+}
