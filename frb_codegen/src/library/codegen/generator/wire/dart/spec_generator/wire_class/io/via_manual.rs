e1b8d6abc2e78824a90ee6a1a313947e1deabd0e
fzyzcjy <ch271828n@outlook.com>
2024-01-15 10:05:21 +0800

    fix: lookup

--- frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/wire_class/io/via_manual.rs
+++ frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/wire_class/io/via_manual.rs
@@ -18,7 +18,14 @@ pub(crate) fn generate(
     let body = format!(
         "
         {wire_class_header}
-            {wire_class_name}(ffi.DynamicLibrary dynamicLibrary);
+            /// Holds the symbol lookup function.
+            final ffi.Pointer<T> Function<T extends ffi.NativeType>(String symbolName)
+                _lookup;
+  
+            /// The symbols are looked up in [dynamicLibrary].
+            {wire_class_name}(ffi.DynamicLibrary dynamicLibrary)
+                : _lookup = dynamicLibrary.lookup;
+
             {class_body}
         }}
         "
