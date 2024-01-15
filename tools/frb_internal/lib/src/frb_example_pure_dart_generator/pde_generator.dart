d185897a72892c6d7e89ee8af233f25e5e1a6b31
fzyzcjy <ch271828n@outlook.com>
2024-01-15 10:43:46 +0800

    chore: copy

--- tools/frb_internal/lib/src/frb_example_pure_dart_generator/pde_generator.dart
+++ tools/frb_internal/lib/src/frb_example_pure_dart_generator/pde_generator.dart
@@ -1,7 +1,25 @@
 import 'dart:io';
 
+import 'package:path/path.dart';
+
 Future<void> generatePureDartPde({required Uri dirPureDart}) async {
   final dirPureDartPde = Directory.current.uri.resolve('../pure_dart_pde/');
 
   TODO;
+}
+
+// copied and modified fromhttps://stackoverflow.com/questions/27204728
+void copyRecursive(Directory src, Directory dst) {
+  if (!dst.existsSync()) {
+    dst.createSync(recursive: true);
+  }
+
+  for (final entity in src.listSync(recursive: false)) {
+    final newPath = join(dst.path, basename(entity.path));
+    if (entity is File) {
+      entity.copySync(newPath);
+    } else if (entity is Directory) {
+      copyRecursive(entity, Directory(newPath));
+    }
+  }
 }
