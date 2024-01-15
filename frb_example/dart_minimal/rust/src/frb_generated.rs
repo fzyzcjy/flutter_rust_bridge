27538d9b893d8e1d6e843d128bb9a1a6899905dd
fzyzcjy <ch271828n@outlook.com>
2024-01-15 18:05:37 +0800

    Revert "chore: exp"
    This reverts commit 2631beda3f5388918410460dea54e88e53f5a7f5.

--- frb_example/dart_minimal/rust/src/frb_generated.rs
+++ frb_example/dart_minimal/rust/src/frb_generated.rs
@@ -28,12 +28,10 @@ flutter_rust_bridge::frb_generated_boilerplate!();
 
 const FLUTTER_RUST_BRIDGE_CODEGEN_VERSION: &str = "2.0.0-dev.19";
 const _: fn() = || {
-    // flutter_rust_bridge::for_generated::static_assertions::const_assert_eq!("what", "haha",);
-    #[allow(unknown_lints, eq_op)]
-    const _: [(); 0 - !{
-        const ASSERT: bool = ("what" == "haha");
-        ASSERT
-    } as usize] = [];
+    flutter_rust_bridge::for_generated::static_assertions::const_assert_eq!(
+        flutter_rust_bridge::for_generated::FLUTTER_RUST_BRIDGE_RUNTIME_VERSION,
+        FLUTTER_RUST_BRIDGE_CODEGEN_VERSION,
+    );
 };
 
 // Section: executor
