822e5e6a174050d50204339723c5866edd5d815a
fzyzcjy <ch271828n@outlook.com>
2024-01-18 10:05:16 +0800

    feat: use typedef

--- frb_rust/src/rust_auto_opaque/dart2rust.rs
+++ frb_rust/src/rust_auto_opaque/dart2rust.rs
@@ -1,11 +1,8 @@
 use crate::for_generated::BaseArc;
 use crate::rust_async::{RwLock, RwLockReadGuard, RwLockWriteGuard};
-use crate::rust_opaque::RustOpaqueBase;
+use crate::rust_auto_opaque::RustAutoOpaqueBase;
 
-use crate::for_generated::{BaseArc, RustOpaqueBase};
-use tokio::sync::{RwLock, RwLockReadGuard, RwLockWriteGuard};
-
-impl<T, A: BaseArc<RwLock<T>>> RustOpaqueBase<RwLock<T>, A> {
+impl<T, A: BaseArc<RwLock<T>>> RustAutoOpaqueBase<T, A> {
     // the function signature is not covered while the whole body is covered - looks like a bug in coverage tool
     // frb-coverage:ignore-start
     pub fn rust_auto_opaque_decode_owned(self) -> T {
--- frb_rust/src/rust_auto_opaque/utils.rs
+++ frb_rust/src/rust_auto_opaque/utils.rs
@@ -1,8 +1,8 @@
 use crate::for_generated::BaseArc;
 use crate::rust_async::{RwLock, RwLockReadGuard, RwLockWriteGuard};
-use crate::rust_opaque::RustOpaqueBase;
+use crate::rust_auto_opaque::RustAutoOpaqueBase;
 
-impl<T, A: BaseArc<RwLock<T>>> From<T> for RustOpaqueBase<RwLock<T>, A> {
+impl<T, A: BaseArc<RwLock<T>>> From<T> for RustAutoOpaqueBase<T, A> {
     fn from(value: T) -> Self {
         Self::new(RwLock::new(value))
     }
