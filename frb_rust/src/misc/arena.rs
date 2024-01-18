77b0bd94724d271c67bacbb8fd1b0cdb2234b466
fzyzcjy <ch271828n@outlook.com>
2024-01-18 18:39:50 +0800

    feat: try

--- frb_rust/src/misc/arena.rs
+++ frb_rust/src/misc/arena.rs
@@ -5,9 +5,10 @@ use std::any::Any;
 pub struct Arena(typed_arena::Arena<Box<dyn Any>>);
 
 impl Arena {
-    pub fn alloc<'a, T: 'static>(&'a self, value: T) -> &'a mut T {
-        let ans = self.0.alloc(Box::new(value));
-        ans.downcast_mut().unwrap()
+    pub fn alloc<'a, T: 'a>(&'a self, value: T) -> &'a mut T {
+        // let ans = self.0.alloc(Box::new(value));
+        // ans.downcast_mut().unwrap()
+        todo!()
     }
 }
 
