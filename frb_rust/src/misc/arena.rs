e119191a869f795ba561810b63fd574d8915a833
fzyzcjy <ch271828n@outlook.com>
2024-01-18 21:34:18 +0800

    Revert "fix: ty"
    This reverts commit 2f4dd2515499e373127f35f429cdec898de88b1f.

--- frb_rust/src/misc/arena.rs
+++ frb_rust/src/misc/arena.rs
@@ -16,8 +16,7 @@ impl<'a, T: 'a> ArenaBase<T> {
 #[macro_export]
 macro_rules! frb_generated_arena {
     () => {
-        #[derive(Default)]
-        struct Arena<'a>($crate::for_generated::ArenaBase<ArenaItem<'a>>);
+        type Arena<'a> = $crate::for_generated::ArenaBase<ArenaItem<'a>>;
     };
 }
 
