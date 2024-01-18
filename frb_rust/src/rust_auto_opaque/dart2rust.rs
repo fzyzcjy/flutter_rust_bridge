f503a1ed887cbc1384dc5ec982267e59f46069dc
fzyzcjy <ch271828n@outlook.com>
2024-01-18 10:07:45 +0800

    fix: err

--- frb_rust/src/rust_auto_opaque/dart2rust.rs
+++ frb_rust/src/rust_auto_opaque/dart2rust.rs
@@ -24,5 +24,5 @@ impl<T, A: BaseArc<RwLock<T>>> RustAutoOpaqueBase<T, A> {
 }
 
 pub fn rust_auto_opaque_encode<T, A: BaseArc<RwLock<T>>>(value: T) -> RustAutoOpaqueBase<T, A> {
-    RustOpaqueBase::new(RwLock::new(value))
+    RustAutoOpaqueBase::new(RwLock::new(value))
 }
