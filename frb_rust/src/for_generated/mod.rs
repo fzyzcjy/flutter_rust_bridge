5de41e8b9c7e26c7b1d9ef7656fb0f398b447716
fzyzcjy <ch271828n@outlook.com>
2024-01-18 10:04:34 +0800

    feat: mroe

--- frb_rust/src/for_generated/mod.rs
+++ frb_rust/src/for_generated/mod.rs
@@ -33,6 +33,7 @@ pub use crate::rust2dart::action::Rust2DartAction;
 pub use crate::rust2dart::stream_sink::StreamSinkBase;
 pub use crate::rust_async;
 pub use crate::rust_async::{BaseAsyncRuntime, SimpleAsyncRuntime};
+pub use crate::rust_auto_opaque::RustAutoOpaqueBase;
 pub use crate::rust_opaque::{dart2rust::decode_rust_opaque_nom, RustOpaqueBase};
 pub use crate::thread_pool::{BaseThreadPool, SimpleThreadPool};
 #[cfg(wasm)]
--- frb_rust/src/rust_auto_opaque/mod.rs
+++ frb_rust/src/rust_auto_opaque/mod.rs
@@ -15,12 +15,14 @@ pub type RustAutoOpaqueNom<T> = RustAutoOpaqueBase<T, StdArc<RwLock<T>>>;
 macro_rules! frb_generated_rust_auto_opaque_def {
     (default_rust_auto_opaque = $default_rust_auto_opaque:ident) => {
         /// Please refer to `RustAutoOpaque` for doc.
-        pub type RustAutoOpaqueMoi<T> =
-            $crate::for_generated::RustAutoOpaqueBase<T, MoiArc<RwLock<T>>>;
+        pub type RustAutoOpaqueMoi<T> = $crate::for_generated::RustAutoOpaqueBase<
+            T,
+            MoiArc<$crate::for_generated::rust_async::RwLock<T>>,
+        >;
 
         /// Usually this is unneeded, and just write down arbitrary types.
         /// However, when you need arbitrary types at places that are not supported yet,
         /// use `RustOpaqueOpaque<YourArbitraryType>`.
-        pub type RustAutoOpaque<T> = RustOpaque<$crate::for_generated::rust_async::RwLock<T>>;
+        pub type RustAutoOpaque<T> = $default_rust_auto_opaque<T>;
     };
 }
