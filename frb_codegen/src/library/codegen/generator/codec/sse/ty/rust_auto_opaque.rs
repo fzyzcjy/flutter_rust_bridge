31b6972f1cc055532e2b324b589c4a17fbd7b565
fzyzcjy <ch271828n@outlook.com>
2024-01-18 16:56:08 +0800

    Revert "refactor: inline"
    This reverts commit 783a0fa82496670473892264442641ed10e26c87.

--- frb_codegen/src/library/codegen/generator/codec/sse/ty/rust_auto_opaque.rs
+++ frb_codegen/src/library/codegen/generator/codec/sse/ty/rust_auto_opaque.rs
@@ -5,27 +5,25 @@ use crate::codegen::generator::codec::sse::ty::*;
 
 impl<'a> CodecSseTyTrait for RustAutoOpaqueCodecSseTy<'a> {
     fn generate_encode(&self, lang: &Lang) -> Option<String> {
-        match lang {
-            Lang::DartLang(_) => {
-                let needs_move = self.ir.needs_move();
-                Some(generate_generalized_rust_opaque_encode(
-                    lang,
-                    &format!("{needs_move}"),
-                ))
-            }
-            Lang::RustLang(_) => None,
-        }
+        let needs_move = self.ir.needs_move();
+        self.should_generate(lang)
+            .then(|| generate_generalized_rust_opaque_encode(lang, &format!("{needs_move}")))
     }
 
     fn generate_decode(&self, lang: &Lang) -> Option<String> {
-        match lang {
-            Lang::DartLang(_) => Some(generate_generalized_rust_opaque_decode(
+        self.should_generate(lang).then(|| {
+            generate_generalized_rust_opaque_decode(
                 lang,
                 self.ir.clone().into(),
                 self.ir.inner.codec,
                 self.context,
-            )),
-            Lang::RustLang(_) => None,
-        }
+            )
+        })
+    }
+}
+
+impl<'a> RustAutoOpaqueCodecSseTy<'a> {
+    fn should_generate(&self, lang: &Lang) -> bool {
+        !matches!(lang, Lang::RustLang(_))
     }
 }
