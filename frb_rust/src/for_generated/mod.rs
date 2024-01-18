df1bfe8e4fed0e16f44fa3ca36b2d0fbea0c3d46
fzyzcjy <ch271828n@outlook.com>
2024-01-18 17:48:40 +0800

    feat: simple arena

--- frb_rust/src/for_generated/mod.rs
+++ frb_rust/src/for_generated/mod.rs
@@ -21,6 +21,7 @@ pub use crate::generalized_arc::std_arc::StdArc; // TODO temp
 pub use crate::generalized_isolate::Channel;
 pub use crate::generalized_isolate::IntoDartExceptPrimitive;
 pub use crate::handler::handler::{FfiCallMode, TaskInfo};
+pub use crate::misc::arena::Arena;
 pub use crate::misc::manual_impl::*;
 pub use crate::misc::version::FLUTTER_RUST_BRIDGE_RUNTIME_VERSION;
 pub use crate::platform_types::{
