397873c0c322ba79d07f2284a884c509d5fae08c
fzyzcjy <ch271828n@outlook.com>
2024-01-18 10:04:01 +0800

    feat: typedef

--- frb_rust/src/rust_auto_opaque/mod.rs
+++ frb_rust/src/rust_auto_opaque/mod.rs
@@ -1,17 +1,22 @@
 use crate::for_generated::StdArc;
+use crate::rust_async::RwLock;
+use crate::rust_opaque::RustOpaqueBase;
 
 pub(crate) mod dart2rust;
 pub(crate) mod utils;
 
+pub type RustAutoOpaqueBase<T, A> = RustOpaqueBase<RwLock<T>, A>;
+
 /// Please refer to `RustAutoOpaque` for doc.
-pub type RustAutoOpaqueNom<T> = RustAutoOpaqueBase<T, StdArc<T>>;
+pub type RustAutoOpaqueNom<T> = RustAutoOpaqueBase<T, StdArc<RwLock<T>>>;
 
 #[doc(hidden)]
 #[macro_export]
 macro_rules! frb_generated_rust_auto_opaque_def {
     (default_rust_auto_opaque = $default_rust_auto_opaque:ident) => {
         /// Please refer to `RustAutoOpaque` for doc.
-        pub type RustAutoOpaqueMoi<T> = $crate::for_generated::RustAutoOpaqueBase<T, MoiArc<T>>;
+        pub type RustAutoOpaqueMoi<T> =
+            $crate::for_generated::RustAutoOpaqueBase<T, MoiArc<RwLock<T>>>;
 
         /// Usually this is unneeded, and just write down arbitrary types.
         /// However, when you need arbitrary types at places that are not supported yet,
