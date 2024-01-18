4203b531d305f70d1ed8ff192edc62ffbf7e8881
fzyzcjy <ch271828n@outlook.com>
2024-01-18 21:34:19 +0800

    Revert "feat: more alloc"
    This reverts commit 85af7c22d97dc1f083cc91be2683ac9328830095.

--- frb_example/dart_minimal/rust/src/frb_generated.rs
+++ frb_example/dart_minimal/rust/src/frb_generated.rs
@@ -52,10 +52,9 @@ impl<'a> SseDecode<'a> for &'a MyOpaqueType {
         arena: &'a flutter_rust_bridge::for_generated::Arena,
         deserializer: &mut flutter_rust_bridge::for_generated::SseDeserializer,
     ) -> Self {
-        let inner = <RustOpaqueMoi<
+        let mut inner = <RustOpaqueMoi<
             flutter_rust_bridge::for_generated::rust_async::RwLock<MyOpaqueType>,
         >>::sse_decode(arena, deserializer);
-        let inner = arena.alloc(inner);
         return arena.alloc(inner.rust_auto_opaque_decode_ref()); // TODO hack
     }
 }
