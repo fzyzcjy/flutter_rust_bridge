a6cc3a3ff53364db1ad0167d85454268a58dab5f
fzyzcjy <ch271828n@outlook.com>
2024-01-18 22:13:24 +0800

    feat: struct

--- frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/codec/dco/encoder/ty/rust_auto_opaque.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/codec/dco/encoder/ty/rust_auto_opaque.rs
@@ -10,10 +10,13 @@ use crate::codegen::ir::ty::IrTypeTrait;
 impl<'a> WireRustCodecDcoGeneratorEncoderTrait for RustAutoOpaqueWireRustCodecDcoGenerator<'a> {
     fn generate_impl_into_dart(&self) -> Option<String> {
         if self.ir.ownership_mode == OwnershipMode::Owned {
-            let name = self.ir.rust_api_type();
+            let rust_api_type = self.ir.rust_api_type();
+            let local_struct_type = format!("Local_{}", self.ir.safe_ident()); // Similar to "mirror"
             let body = format!("{}.into_dart()", generate_encode_rust_auto_opaque(&self.ir));
             Some(
-                generate_impl_into_dart(&name, &body) + &generate_impl_into_into_dart(&name, &None),
+                format!("struct {local_struct_type}({rust_api_type});\n")
+                    + &generate_impl_into_dart(&local_struct_type, &body)
+                    + &generate_impl_into_into_dart(&rust_api_type, &Some(local_struct_type)),
             )
         } else {
             None
