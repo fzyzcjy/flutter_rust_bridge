8e1a31f4ea12345d82a5804c68fc9f3366b5c093
fzyzcjy <ch271828n@outlook.com>
2024-01-18 17:50:08 +0800

    feat: simple test

--- frb_rust/src/misc/arena.rs
+++ frb_rust/src/misc/arena.rs
@@ -9,9 +9,30 @@ pub trait ArenaItem {}
 
 impl<T> ArenaItem for T {}
 
+impl Default for Arena {
+    fn default() -> Self {
+        Self { values: vec![] }
+    }
+}
+
 impl Arena {
     pub fn put<T: ArenaItem>(&mut self, value: T) -> &mut T {
         self.values.push(value);
         self.values.last_mut().unwrap()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::path::PathBuf;
+
+    #[test]
+    fn test_simple() {
+        let arena = Arena::default();
+        let apple = arena.put("Apple".to_owned());
+        let orange = arena.put(PathBuf::new());
+        assert_eq!(apple, "Apple");
+        drop(arena);
     }
 }
