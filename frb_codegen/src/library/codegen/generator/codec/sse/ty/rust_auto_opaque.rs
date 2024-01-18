ccd56fd6e3bf1f9d44472666a03625d631d1a74e
fzyzcjy <ch271828n@outlook.com>
2024-01-18 16:59:55 +0800

    feat: fill in ty

--- frb_codegen/src/library/codegen/generator/codec/sse/ty/rust_auto_opaque.rs
+++ frb_codegen/src/library/codegen/generator/codec/sse/ty/rust_auto_opaque.rs
@@ -28,7 +28,7 @@ impl<'a> CodecSseTyTrait for RustAutoOpaqueCodecSseTy<'a> {
             Lang::RustLang(_) => {
                 let arc = self.ir.inner.codec.arc_ty();
                 Some(generate_sse_encode_or_decode_impl(
-                    TODO,
+                    &self.ir.raw.string,
                     &format!("flutter_rust_bridge::for_generated::rust_auto_opaque_encode::<_, {arc}<_>>(self)"),
                     EncodeOrDecode::Encode,
                 ))
