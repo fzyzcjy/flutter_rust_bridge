19dc1b561bc984d0a4884719462f2ef08a179355
fzyzcjy <ch271828n@outlook.com>
2024-01-18 08:52:16 +0800

    feat: partial

--- frb_codegen/src/library/codegen/generator/codec/sse/ty/delegate.rs
+++ frb_codegen/src/library/codegen/generator/codec/sse/ty/delegate.rs
@@ -39,7 +39,14 @@ impl<'a> CodecSseTyTrait for DelegateCodecSseTy<'a> {
                     "{ let boxed: Box<[_]> = Box::new(self); boxed.into_vec() }".to_owned()
                 }
                 IrTypeDelegate::String => "self.into_bytes()".to_owned(),
-                IrTypeDelegate::PrimitiveEnum(_) => "TODO".to_owned(),
+                IrTypeDelegate::PrimitiveEnum(ir) => {
+                    let variants = TODO;
+                    lang.switch_expr(
+                        "self",
+                        &variants,
+                        Some(format!("{};", lang.throw_unimplemented(""))),
+                    )
+                }
                 IrTypeDelegate::Backtrace => r#"format!("{:?}", self)"#.to_owned(),
                 IrTypeDelegate::AnyhowException => r#"format!("{:?}", self)"#.to_owned(),
                 IrTypeDelegate::Map(_) => "self.into_iter().collect()".to_owned(),
