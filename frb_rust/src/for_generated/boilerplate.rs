fbc5ce3d0e36d1468ea46079c0a8f8465fe0b740
fzyzcjy <ch271828n@outlook.com>
2024-01-18 20:37:53 +0800

    fix: err

--- frb_rust/src/for_generated/boilerplate.rs
+++ frb_rust/src/for_generated/boilerplate.rs
@@ -46,13 +46,13 @@ macro_rules! frb_generated_sse_codec {
     () => {
         pub trait SseDecode<'a> {
             fn sse_decode(
-                arena: &'a $crate::for_generated::ArenaBase,
+                arena: &'a Arena,
                 deserializer: &mut $crate::for_generated::SseDeserializer,
             ) -> Self;
 
             // just syntax sugar
             fn sse_decode_single(
-                arena: &'a $crate::for_generated::ArenaBase,
+                arena: &'a Arena,
                 message: $crate::for_generated::Dart2RustMessageSse,
             ) -> Self
             where
