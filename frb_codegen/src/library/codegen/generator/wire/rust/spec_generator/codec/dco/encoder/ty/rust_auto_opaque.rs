c553094bd61a1269368c0c863f4d46fe95308c2d
fzyzcjy <ch271828n@outlook.com>
2024-01-19 08:52:40 +0800

    refactor: extract

--- frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/codec/dco/encoder/ty/rust_auto_opaque.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/codec/dco/encoder/ty/rust_auto_opaque.rs
@@ -4,14 +4,16 @@ use crate::codegen::generator::wire::rust::spec_generator::codec::dco::encoder::
     generate_impl_into_dart, generate_impl_into_into_dart,
 };
 use crate::codegen::generator::wire::rust::spec_generator::codec::dco::encoder::ty::WireRustCodecDcoGeneratorEncoderTrait;
-use crate::codegen::ir::ty::rust_auto_opaque::OwnershipMode;
+use crate::codegen::ir::pack::IrPack;
+use crate::codegen::ir::ty::primitive::IrTypePrimitive;
+use crate::codegen::ir::ty::rust_auto_opaque::{IrTypeRustAutoOpaque, OwnershipMode};
 use crate::codegen::ir::ty::IrTypeTrait;
 
 impl<'a> WireRustCodecDcoGeneratorEncoderTrait for RustAutoOpaqueWireRustCodecDcoGenerator<'a> {
     fn generate_impl_into_dart(&self) -> Option<String> {
         if self.ir.ownership_mode == OwnershipMode::Owned {
             let rust_api_type = self.ir.rust_api_type();
-            let local_struct_type = format!("Local_{}", self.ir.safe_ident()); // Similar to "mirror"
+            let local_struct_type = rust_auto_opaque_local_struct_type(&self.ir);
             let body = format!(
                 "{}.into_dart()",
                 generate_encode_rust_auto_opaque(&self.ir, "self.0")
@@ -25,4 +27,9 @@ impl<'a> WireRustCodecDcoGeneratorEncoderTrait for RustAutoOpaqueWireRustCodecDc
             None
         }
     }
+}
+
+// Similar to "mirror"
+fn rust_auto_opaque_local_struct_type(ir: &IrTypeRustAutoOpaque) -> String {
+    format!("Local_{}", ir.safe_ident())
 }
