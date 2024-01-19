248eaf1a698c157fc3fd5d89f4cef77440dc92c6
fzyzcjy <ch271828n@outlook.com>
2024-01-19 09:32:51 +0800

    feat: more tests

--- frb_example/pure_dart/test/api/rust_auto_opaque_test.dart
+++ frb_example/pure_dart/test/api/rust_auto_opaque_test.dart
@@ -236,7 +236,7 @@ Future<void> main({bool skipRustLibInit = false}) async {
     expect(await obj.instanceMethodGetterTwinNormal, 42);
   });
 
-  test('types with both encodable and opaque fields', () async {
+  test('structs with both encodable and opaque fields', () async {
     final obj =
         await rustAutoOpaqueStructWithGoodAndOpaqueFieldReturnOwnTwinNormal();
     expect(obj.good, 'hello');
@@ -244,6 +244,22 @@ Future<void> main({bool skipRustLibInit = false}) async {
         rustAutoOpaqueArgBorrowTwinNormal(arg: obj.opaque, expect: 42));
     await futurizeVoidTwinNormal(
         rustAutoOpaqueStructWithGoodAndOpaqueFieldArgOwnTwinNormal(arg: obj));
+  });
+
+  test('enums with both encodable and opaque', () async {
+    final good =
+        (await rustAutoOpaqueEnumWithGoodAndOpaqueReturnOwnGoodTwinNormal());
+    final opaque =
+        (await rustAutoOpaqueEnumWithGoodAndOpaqueReturnOwnOpaqueTwinNormal());
+
+    await futurizeVoidTwinNormal(
+        rustAutoOpaqueEnumWithGoodAndOpaqueArgOwnTwinNormal(arg: good));
+    await futurizeVoidTwinNormal(
+        rustAutoOpaqueEnumWithGoodAndOpaqueArgOwnTwinNormal(arg: opaque));
+
+    await futurizeVoidTwinNormal(
+        rustAutoOpaqueEnumWithGoodAndOpaqueArgOwnTwinNormal(
+            arg: EnumWithGoodAndOpaqueTwinNormal.good('good')));
   });
 
   test('vec of opaque', () async {
