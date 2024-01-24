a6bb16508481722c6fc635392562d543a24288f1
fzyzcjy <ch271828n@outlook.com>
2024-01-24 08:43:20 +0800

    feat: more

--- frb_example/dart_build_rs/rust/build.rs
+++ frb_example/dart_build_rs/rust/build.rs
@@ -2,9 +2,13 @@ use lib_flutter_rust_bridge_codegen::codegen;
 use lib_flutter_rust_bridge_codegen::codegen::Config;
 use lib_flutter_rust_bridge_codegen::utils::logs::configure_opinionated_logging;
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     // If you want to see logs
-    configure_opinionated_logging("./logs", true).unwrap();
+    configure_opinionated_logging("./logs", true)?;
+
     // Execute code generator with auto-detected config
-    codegen::generate(Config::from_files_auto().unwrap(), Default::default()).unwrap();
+    codegen::generate(
+        Config::from_config_file("../flutter_rust_bridge.yaml")?.unwrap(),
+        Default::default(),
+    )
 }
