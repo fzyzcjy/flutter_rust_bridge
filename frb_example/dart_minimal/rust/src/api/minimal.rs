16fd35a314e30a8fa45d6ee5c9a3b360f25f193b
fzyzcjy <ch271828n@outlook.com>
2024-01-24 09:05:26 +0800

    feat: temp example

--- frb_example/dart_minimal/rust/src/api/minimal.rs
+++ frb_example/dart_minimal/rust/src/api/minimal.rs
@@ -1,4 +1,6 @@
+use chrono::{DateTime, Utc};
 use flutter_rust_bridge::frb;
+use uuid::Uuid;
 
 #[frb(init)]
 pub fn init_app() {
@@ -7,4 +9,12 @@ pub fn init_app() {
 
 pub fn minimal_adder(a: i32, b: i32) -> i32 {
     a + b
+}
+
+pub fn gen_uuid() -> Uuid {
+    Uuid::new_v4()
+}
+
+pub fn get_now() -> DateTime<Utc> {
+    Utc::now()
 }
