074ec4023945fa4d899bb807c8331ae51cfa1fa4
fzyzcjy <ch271828n@outlook.com>
2024-01-24 20:22:15 +0800

    refactor: script

--- tools/frb_internal/lib/src/makefile_dart/test.dart
+++ tools/frb_internal/lib/src/makefile_dart/test.dart
@@ -266,19 +266,23 @@ Future<void> testUpgrade() async {
   }
 
   // This old-version can be bumped if needed
-  const kOldVersion = '2.0.0-dev.20';
+  const oldVersion = '2.0.0-dev.20';
+  final newVersion = computeVersionInfo().newVersion;
+  if (oldVersion != newVersion) {
+    throw Exception('This test requires oldVersion!=newVersion');
+  }
 
   await quickstartStepInstall(CodegenInstallMode.cargoInstall,
-      versionConstraint: kOldVersion);
+      versionConstraint: oldVersion);
 
   MimicQuickstartTester._prepareDir();
 
   await const MimicQuickstartTester(postRelease: true)._quickstartStepCreate();
-  checkVersion(expectVersion: kOldVersion);
+  checkVersion(expectVersion: oldVersion);
 
   await const MimicQuickstartTester(postRelease: false)
       ._quickstartStepGenerate();
-  checkVersion(expectVersion: computeVersionInfo().newVersion);
+  checkVersion(expectVersion: newVersion);
 }
 
 Future<void> testRust(TestRustConfig config) async {
