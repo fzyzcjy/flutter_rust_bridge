4f64ed84f5cf70470e301152a897cb0d41bbcb19
fzyzcjy <ch271828n@outlook.com>
2024-01-24 17:15:47 +0800

    feat: use pwd

--- frb_codegen/src/library/commands/format_dart.rs
+++ frb_codegen/src/library/commands/format_dart.rs
@@ -2,22 +2,25 @@ use crate::command_run;
 use crate::commands::command_runner::call_shell;
 use crate::library::commands::command_runner::check_exit_code;
 use crate::utils::path_utils::{normalize_windows_unc_path, path_to_string};
+use anyhow::Context;
 use log::debug;
+use pathdiff::diff_paths;
 use std::path::{Path, PathBuf};
 
 #[allow(clippy::vec_init_then_push)]
 pub fn format_dart(paths: &[PathBuf], base_path: &Path, line_length: u32) -> anyhow::Result<()> {
     let paths = (paths.iter())
-        .map(|p| {
-            Ok(normalize_windows_unc_path(&path_to_string(p)?)
-                .to_owned()
-                .into())
+        .map(|path| {
+            let path: PathBuf =
+                (normalize_windows_unc_path(&path_to_string(path)?).to_owned()).into();
+            let path = diff_paths(path, base_path).context("diff path")?;
+            Ok(path)
         })
         .collect()?;
-    debug!("execute format_dart path={path:?} line_length={line_length}");
+    debug!("execute format_dart paths={paths:?} line_length={line_length}");
 
     let res = command_run!(
-        call_shell[None, None],
+        call_shell[Some(base_path), None],
         "dart",
         "format",
         "--line-length",
