f1723f91d4bd8b4b30aa0fb9650deb3e0ac7e227
fzyzcjy <ch271828n@outlook.com>
2024-01-24 20:23:03 +0800

    fix: flip

--- tools/frb_internal/lib/src/makefile_dart/test.dart
+++ tools/frb_internal/lib/src/makefile_dart/test.dart
@@ -270,7 +270,7 @@ Future<void> testUpgrade() async {
   // This old-version can be bumped if needed
   const oldVersion = '2.0.0-dev.20';
   final newVersion = computeVersionInfo().newVersion;
-  if (oldVersion != newVersion) {
+  if (oldVersion == newVersion) {
     throw Exception('This test requires oldVersion!=newVersion');
   }
 
