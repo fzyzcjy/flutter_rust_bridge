1ccd8f34f08462c053c6b12dde985e295c715913
fzyzcjy <ch271828n@outlook.com>
2024-01-24 17:13:04 +0800

    feat: add arg

--- frb_codegen/src/library/commands/format_dart.rs
+++ frb_codegen/src/library/commands/format_dart.rs
@@ -3,10 +3,10 @@ use crate::commands::command_runner::call_shell;
 use crate::library::commands::command_runner::check_exit_code;
 use crate::utils::path_utils::{normalize_windows_unc_path, path_to_string};
 use log::debug;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 #[allow(clippy::vec_init_then_push)]
-pub fn format_dart(path: &[PathBuf], line_length: u32) -> anyhow::Result<()> {
+pub fn format_dart(path: &[PathBuf], base_path: &Path, line_length: u32) -> anyhow::Result<()> {
     let path = normalize_windows_unc_paths(path)?;
     debug!("execute format_dart path={path:?} line_length={line_length}");
 
