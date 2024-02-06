26741db91b88f7f87180f00df02b6df35db859fe
fzyzcjy <ch271828n@outlook.com>
2024-02-06 20:26:21 +0800

    chore: codegen

--- frb_example/pure_dart_pde/rust/src/api/async_spawn.rs
+++ frb_example/pure_dart_pde/rust/src/api/async_spawn.rs
@@ -18,3 +18,19 @@ pub async fn simple_use_async_spawn_blocking(arg: String) -> String {
     );
     handle.await.unwrap()
 }
+
+pub async fn simple_use_async_spawn_local(arg: String) -> String {
+    async fn core(arg: String) -> String {
+        let handle = flutter_rust_bridge::spawn_local(async move { arg.repeat(2) });
+        handle.await.unwrap()
+    }
+
+    // ref demo in https://docs.rs/tokio/latest/tokio/task/fn.spawn_local.html
+    #[cfg(not(wasm))]
+    return tokio::task::LocalSet::new()
+        .run_until(async move { core(arg) })
+        .await;
+
+    #[cfg(wasm)]
+    return core(arg).await;
+}
