6211a407a290c7b813e6987fbbdfc573bd807a84
fzyzcjy <ch271828n@outlook.com>
2024-02-07 13:42:19 +0800

    feat: more

--- frb_example/pure_dart/rust/src/api/async_spawn.rs
+++ frb_example/pure_dart/rust/src/api/async_spawn.rs
@@ -1,6 +1,6 @@
 // FRB_INTERNAL_GENERATOR: {"forbiddenDuplicatorModes": ["sync", "rustAsync", "sse", "sync sse", "rustAsync sse"]}
 
-use crate::frb_generated::FLUTTER_RUST_BRIDGE_HANDLER;
+use crate::frb_generated::{StreamSink, FLUTTER_RUST_BRIDGE_HANDLER};
 
 pub async fn simple_use_async_spawn(arg: String) -> String {
     // ref demo in https://docs.rs/tokio/latest/tokio/task/fn.spawn.html
@@ -17,18 +17,29 @@ pub async fn simple_use_async_spawn_blocking(arg: String) -> String {
     handle.await.unwrap()
 }
 
-pub async fn simple_use_async_spawn_local(arg: String) -> String {
-    async fn core(arg: String) -> String {
+// NOTE: Often the `spawn` and `spawn_blocking` is enough
+pub async fn simple_use_async_spawn_local(arg: String, sink: StreamSink<String>) {
+    let core = || async {
         let handle = flutter_rust_bridge::spawn_local(async move { arg.repeat(2) });
-        handle.await.unwrap()
+        sink.add(handle.await.unwrap())
+    };
+
+    // Usually you will not use like this. This is just a test to ensure the function works.
+    // ref tokio demo in https://docs.rs/tokio/latest/tokio/task/fn.spawn_local.html
+    // and how tokio `LocalSet` is used https://docs.rs/tokio/latest/tokio/task/struct.LocalSet.html#use-inside-tokiospawn
+    #[cfg(not(wasm))]
+    {
+        use tokio::runtime::Builder;
+        use tokio::task::LocalSet;
+
+        let rt = Builder::new_current_thread().enable_all().build().unwrap();
+        std::thread::spawn(move || {
+            let local = LocalSet::new();
+            local.spawn_local(async move { core().await });
+            rt.block_on(local);
+        });
     }
 
-    // ref demo in https://docs.rs/tokio/latest/tokio/task/fn.spawn_local.html
-    #[cfg(not(wasm))]
-    return tokio::task::LocalSet::new()
-        .run_until(async move { core(arg).await })
-        .await;
-
     #[cfg(wasm)]
-    return core(arg).await;
+    core().await;
 }
