dc90912cf617e638a67a7328db63957ccba742f0
fzyzcjy <ch271828n@outlook.com>
2024-02-25 20:02:50 +0800

    feat: ret

--- frb_codegen/src/library/codegen/parser/sanity_checker.rs
+++ frb_codegen/src/library/codegen/parser/sanity_checker.rs
@@ -1,6 +1,7 @@
 use crate::codegen::config::internal_config::RustInputPathPack;
-use crate::codegen::ir::namespace::Namespace;
+use crate::codegen::ir::namespace::{Namespace, NamespacedName};
 use crate::codegen::ir::pack::IrPack;
+use crate::codegen::ir::ty::delegate::IrTypeDelegate;
 use crate::codegen::ir::ty::IrType;
 use crate::codegen::parser::source_graph::modules::{Enum, Struct, StructOrEnumWrapper};
 use crate::codegen::parser::type_parser::path_data::extract_path_data;
@@ -9,28 +10,25 @@ use log::warn;
 use std::collections::{HashMap, HashSet};
 use std::path::PathBuf;
 use syn::Type;
-use crate::codegen::ir::ty::delegate::IrTypeDelegate;
 
-pub(super) fn sanity_check_unused_struct_enum(
+pub(super) fn sanity_check_get_unused_types(
     pack: &IrPack,
     src_structs: &HashMap<String, &Struct>,
     src_enums: &HashMap<String, &Enum>,
     rust_input_path_pack: &RustInputPathPack,
     rust_crate_dir: &PathBuf,
-) -> anyhow::Result<()> {
+) -> anyhow::Result<Vec<NamespacedName>> {
     let interest_input_paths = rust_input_path_pack
         .rust_input_paths
         .iter()
         .map(|p| Namespace::new_from_rust_crate_path(p, rust_crate_dir))
         .collect::<anyhow::Result<Vec<_>>>()?;
 
-    let all_types: HashSet<String> = [
+    let all_types = [
         extract_interest_src_types(src_structs, &interest_input_paths),
         extract_interest_src_types(src_enums, &interest_input_paths),
     ]
-    .concat()
-    .into_iter()
-    .collect();
+    .concat();
 
     let used_types = pack
         .distinct_types(None)
@@ -39,26 +37,26 @@ pub(super) fn sanity_check_unused_struct_enum(
         .flatten_ok()
         .collect::<anyhow::Result<HashSet<String>>>()?;
 
-    let unused_types = all_types.difference(&used_types).collect_vec();
+    let unused_types = (all_types.into_iter())
+        .filter(|ty| !used_types.contains(&ty.name))
+        .collect_vec();
 
-    if !unused_types.is_empty() {
-        warn!(
-            "Some structs/enums are exported as `pub`, but are never used in any `pub` functions, thus they are ignored: {:?}",
-            unused_types,
-        )
-    }
-
-    Ok(())
+    Ok(unused_types)
 }
 
 fn extract_interest_src_types<T: StructOrEnumWrapper<I>, I>(
     src_items: &HashMap<String, &T>,
     interest_input_paths: &[Namespace],
-) -> Vec<String> {
-    src_items
-        .iter()
-        .filter(|(_, v)| interest_input_paths.contains(&v.inner().namespace()))
-        .map(|(k, _)| k.to_owned())
+) -> Vec<NamespacedName> {
+    (src_items.iter())
+        .filter_map(|(k, v)| {
+            let namespace = v.inner().namespace();
+            if interest_input_paths.contains(&namespace) {
+                Some(NamespacedName::new(namespace, k))
+            } else {
+                None
+            }
+        })
         .collect_vec()
 }
 
