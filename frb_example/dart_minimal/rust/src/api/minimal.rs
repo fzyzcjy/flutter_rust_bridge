7994d5448b93124bc859d3507bc10152b55a4036
fzyzcjy <ch271828n@outlook.com>
2024-02-25 21:01:30 +0800

    feat: repro

--- frb_example/dart_minimal/rust/src/api/minimal.rs
+++ frb_example/dart_minimal/rust/src/api/minimal.rs
@@ -8,3 +8,20 @@ pub fn init_app() {
 pub fn minimal_adder(a: i32, b: i32) -> i32 {
     a + b
 }
+
+pub struct BMPimage {
+    pub width: u32,
+    pub height: u32,
+    pub bmp: Vec<u8>,
+}
+
+pub fn render_image(width: i64, height: i64) -> BMPimage {
+    let tt = std::time::Instant::now();
+    let res = BMPimage {
+        width: width as u32,
+        height: height as u32,
+        bmp: vec![0; (width * height * 4) as usize],
+    };
+    println!("IN RUST: {:?}", tt.elapsed());
+    res
+}
