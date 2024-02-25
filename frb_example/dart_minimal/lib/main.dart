e0a71a477fe2aa5f5ffc2ee003ad6ed7750507e3
fzyzcjy <ch271828n@outlook.com>
2024-02-25 21:03:00 +0800

    feat: repro dart

--- frb_example/dart_minimal/lib/main.dart
+++ frb_example/dart_minimal/lib/main.dart
@@ -5,4 +5,8 @@ import 'package:frb_example_dart_minimal/src/rust/frb_generated.dart';
 Future<void> main() async {
   await RustLib.init();
   print('Call Rust and get: 100+200 = ${await minimalAdder(a: 100, b: 200)}');
+
+  var tt = Stopwatch()..start();
+  await renderImage(width: 3000, height: 3000);
+  print(' => IN FLUTTER: ${tt.elapsedMilliseconds} ms');
 }
--- frb_example/dart_minimal/lib/src/rust/api/minimal.dart
+++ frb_example/dart_minimal/lib/src/rust/api/minimal.dart
@@ -3,8 +3,9 @@
 
 // ignore_for_file: invalid_use_of_internal_member, unused_import, unnecessary_import
 
-import '../frb_generated.dart';
 import 'package:flutter_rust_bridge/flutter_rust_bridge_for_generated.dart';
+
+import '../frb_generated.dart';
 
 Future<int> minimalAdder({required int a, required int b, dynamic hint}) =>
     RustLib.instance.api.minimalAdder(a: a, b: b, hint: hint);
