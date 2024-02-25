c20460d00b8f7933d0d19ec8fd72293ab572883c
fzyzcjy <ch271828n@outlook.com>
2024-02-25 21:03:33 +0800

    feat: more repeat

--- frb_example/dart_minimal/lib/main.dart
+++ frb_example/dart_minimal/lib/main.dart
@@ -6,7 +6,10 @@ Future<void> main() async {
   await RustLib.init();
   print('Call Rust and get: 100+200 = ${await minimalAdder(a: 100, b: 200)}');
 
-  var tt = Stopwatch()..start();
-  await renderImage(width: 3000, height: 3000);
-  print(' => IN FLUTTER: ${tt.elapsedMilliseconds} ms');
+  for (var i = 0; i < 10; ++i) {
+    final tt = Stopwatch()..start();
+    await renderImage(width: 3000, height: 3000);
+    tt.stop();
+    print(' => IN FLUTTER: ${tt.elapsedMilliseconds} ms');
+  }
 }
