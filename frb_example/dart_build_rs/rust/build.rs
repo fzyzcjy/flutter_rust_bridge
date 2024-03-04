a32a4a4b199deca81a3c6ccfd76894f2714552f5
fzyzcjy <5236035+fzyzcjy@users.noreply.github.com>
2024-03-04 11:08:17 +0800

    Update build.rs

--- frb_example/dart_build_rs/rust/build.rs
+++ frb_example/dart_build_rs/rust/build.rs
@@ -3,7 +3,8 @@ use lib_flutter_rust_bridge_codegen::codegen::Config;
 use lib_flutter_rust_bridge_codegen::utils::logs::configure_opinionated_logging;
 
 fn main() -> anyhow::Result<()> {
-    // Uncomment the below line, if you only want to generate bindings on api directory change.
+    // Uncomment the line below, if you only want to generate bindings on api directory change.
+    //
     // NOTE: This accelerates the build process, but you will need to manually trigger binding
     // generation whenever there are changes to definitions outside of the api directory that it
     // depends on.
