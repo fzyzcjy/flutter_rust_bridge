8e4a04edd4c576849fdc30c863fe14077960c2fb
Haled Odat <8566042+HalidOdat@users.noreply.github.com>
2024-03-04 02:14:41 +0100

    Only run build.rs if api directory changes

--- frb_example/dart_build_rs/rust/build.rs
+++ frb_example/dart_build_rs/rust/build.rs
@@ -3,6 +3,9 @@ use lib_flutter_rust_bridge_codegen::codegen::Config;
 use lib_flutter_rust_bridge_codegen::utils::logs::configure_opinionated_logging;
 
 fn main() -> anyhow::Result<()> {
+    // Only run binding generation, if the api directory changes.
+    println!("cargo:rerun-if-changed=src/api");
+
     // If you want to see logs
     // Alternatively, use `cargo build -vvv` (instead of `cargo build`) to see logs on screen
     configure_opinionated_logging("./logs/", true)?;
