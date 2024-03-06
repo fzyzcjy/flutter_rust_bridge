de26272209f49fb3ed0081ef8737f151983d2e92
fzyzcjy <ch271828n@outlook.com>
2024-03-06 17:33:47 +0800

    feat: more

--- frb_example/pure_dart/rust/src/api/constructor.rs
+++ frb_example/pure_dart/rust/src/api/constructor.rs
@@ -1,3 +1,5 @@
+// FRB_INTERNAL_GENERATOR: {"forbiddenDuplicatorModes": ["sync", "sync sse"]}
+
 use flutter_rust_bridge::frb;
 
 pub struct ConstructorTranslatableStructTwinNormal {
@@ -24,6 +26,39 @@ impl ConstructorOpaqueStructTwinNormal {
         }
     }
 
+    #[frb(sync)]
+    pub fn check(&self) {
+        assert_eq!(self.one, "hello");
+    }
+}
+
+pub struct ConstructorTranslatableSyncStructTwinNormal {
+    pub one: String,
+}
+
+impl ConstructorTranslatableSyncStructTwinNormal {
+    #[frb(sync)]
+    pub fn new() -> Self {
+        Self {
+            one: "hello".to_owned(),
+        }
+    }
+}
+
+#[frb(opaque)]
+pub struct ConstructorOpaqueSyncStructTwinNormal {
+    pub one: String,
+}
+
+impl ConstructorOpaqueSyncStructTwinNormal {
+    #[frb(sync)]
+    pub fn new() -> Self {
+        Self {
+            one: "hello".to_owned(),
+        }
+    }
+
+    #[frb(sync)]
     pub fn check(&self) {
         assert_eq!(self.one, "hello");
     }
