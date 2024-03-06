bc6c3565a4e6477bfb5624a6ff2536647887e413
fzyzcjy <ch271828n@outlook.com>
2024-03-06 13:21:21 +0800

    chore: rename

--- frb_macros/src/lib.rs
+++ frb_macros/src/lib.rs
@@ -19,20 +19,20 @@ pub fn frb(attribute: TokenStream, item: TokenStream) -> TokenStream {
 fn strip_frb_attr(item: TokenStream) -> TokenStream {
     item.into_iter()
         .scan(None, |pound, tok| {
-            use TokenTree as T;
+            use TokenTree as TT;
             match (&pound, &tok) {
-                (None, T::Punct(punct)) if punct.as_char() == '#' => {
+                (None, TT::Punct(punct)) if punct.as_char() == '#' => {
                     *pound = Some(tok);
                     Some(TokenStream::new())
                 }
-                (Some(_), T::Group(group)) if is_frb_bracket(group) => {
+                (Some(_), TT::Group(group)) if is_frb_bracket(group) => {
                     _ = pound.take();
                     Some(format_frb_attribute(format!("#[{}]", group.stream())))
                 }
-                (_, T::Group(group)) => Some(
+                (_, TT::Group(group)) => Some(
                     [
                         pound.take(),
-                        Some(T::Group(Group::new(
+                        Some(TT::Group(Group::new(
                             group.delimiter(),
                             strip_frb_attr(group.stream()),
                         ))),
