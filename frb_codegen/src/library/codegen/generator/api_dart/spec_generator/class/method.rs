db4166a0c21b61be19ef82715c5c5aab6c9e34d1
fzyzcjy <ch271828n@outlook.com>
2024-03-06 16:57:45 +0800

    feat: rename

--- frb_codegen/src/library/codegen/generator/api_dart/spec_generator/class/method.rs
+++ frb_codegen/src/library/codegen/generator/api_dart/spec_generator/class/method.rs
@@ -84,7 +84,7 @@ fn generate_signature(
         &ApiDartGenerator::new(func.output.clone(), context).dart_api_type(),
     );
     let method_name = if is_static_method && method_info.actual_method_name == "new" {
-        format!("new{}", generalized_class_name)
+        format!("newInstance")
     } else {
         method_info.actual_method_name.to_case(Case::Camel)
     };
