e86827653081ffe66aac8d071d6dec7b584a9aac
fzyzcjy <ch271828n@outlook.com>
2024-03-07 20:49:02 +0800

    Revert "refactor: simp"
    This reverts commit 2ae4307bc8bb246d4ef5af87ce985174a6223a89.

--- frb_macros/src/lib.rs
+++ frb_macros/src/lib.rs
@@ -11,8 +11,40 @@ use proc_macro::*;
 #[proc_macro_attribute]
 pub fn frb(attribute: TokenStream, item: TokenStream) -> TokenStream {
     let mut output = format_frb_attribute(format!("#[frb({attribute})]"));
+    let item = strip_frb_attr(item);
     output.extend(item);
     output
+}
+
+fn strip_frb_attr(item: TokenStream) -> TokenStream {
+    item.into_iter()
+        .scan(None, |pound, tok| {
+            use TokenTree as TT;
+            match (&pound, &tok) {
+                (None, TT::Punct(punct)) if punct.as_char() == '#' => {
+                    *pound = Some(tok);
+                    Some(TokenStream::new())
+                }
+                (Some(_), TT::Group(group)) if is_frb_bracket(group) => {
+                    _ = pound.take();
+                    Some(format_frb_attribute(format!("#[{}]", group.stream())))
+                }
+                (_, TT::Group(group)) => Some(
+                    [
+                        pound.take(),
+                        Some(TT::Group(Group::new(
+                            group.delimiter(),
+                            strip_frb_attr(group.stream()),
+                        ))),
+                    ]
+                    .into_iter()
+                    .flatten()
+                    .collect(),
+                ),
+                _ => Some(tok.into()),
+            }
+        })
+        .collect()
 }
 
 fn is_frb_bracket(group: &Group) -> bool {
