951d1515838a1cc350a4ae13ed4167f658cb5d8a
fzyzcjy <ch271828n@outlook.com>
2024-04-05 20:19:23 +0800

    repro

--- frb_example/pure_dart/rust/src/api/stream.rs
+++ frb_example/pure_dart/rust/src/api/stream.rs
@@ -1,8 +1,10 @@
 // FRB_INTERNAL_GENERATOR: {"forbiddenDuplicatorModes": ["sync", "sync sse"]}
 
+use std::sync::Mutex;
 use crate::frb_generated::StreamSink;
 use crate::frb_generated::FLUTTER_RUST_BRIDGE_HANDLER;
 use anyhow::anyhow;
+use lazy_static::lazy_static;
 use flutter_rust_bridge::for_generated::BaseThreadPool;
 use flutter_rust_bridge::transfer;
 
