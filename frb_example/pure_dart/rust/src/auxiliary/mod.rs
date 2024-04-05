c3b49713353a6ef3a8a1817999aae33a6d03d101
fzyzcjy <ch271828n@outlook.com>
2024-04-05 20:10:08 +0800

    more repro

--- frb_example/pure_dart/rust/src/auxiliary/mod.rs
+++ frb_example/pure_dart/rust/src/auxiliary/mod.rs
@@ -3,6 +3,3 @@ pub mod new_module_system;
 pub mod old_module_system;
 pub mod protobuf_for_benchmark;
 pub mod sample_types;
-
-#[cfg(target_os = "non_existent_os")]
-pub mod conditionally_compiled_module;
