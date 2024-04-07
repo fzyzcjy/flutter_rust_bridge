0c784a3bbd9e7b532fd1905fcd268f9104533fef
fzyzcjy <ch271828n@outlook.com>
2024-04-07 18:02:57 +0800

    feat: impl

--- frb_codegen/src/library/codegen/generator/api_dart/spec_generator/class/misc.rs
+++ frb_codegen/src/library/codegen/generator/api_dart/spec_generator/class/misc.rs
@@ -1,5 +1,9 @@
+use crate::codegen::ir::ty::IrType;
 use std::collections::HashMap;
 
-pub(crate) fn generate_class_extra_body(dart_code_of_type: &HashMap<String, String>) -> String {
-    TODO
+pub(crate) fn generate_class_extra_body(
+    ir_type: IrType,
+    dart_code_of_type: &HashMap<String, String>,
+) -> String {
+    dart_code_of_type.get(ir_type.safe_ident()).unwrap_or_default()
 }
