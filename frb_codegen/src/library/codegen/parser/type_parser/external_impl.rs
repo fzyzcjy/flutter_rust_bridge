9e27e20c54dbcf97ac3bf95cffba2c3db35f8b74
fzyzcjy <ch271828n@outlook.com>
2024-04-07 21:35:25 +0800

    chore: log

--- frb_codegen/src/library/codegen/parser/type_parser/external_impl.rs
+++ frb_codegen/src/library/codegen/parser/type_parser/external_impl.rs
@@ -2,8 +2,6 @@ use anyhow::Result;
 use syn::{parse_str, Path, Type, visit_mut, visit_mut::VisitMut};
 
 pub(crate) fn parse_type(mut ty: Type) -> Result<Type> {
-    println!("parse_type {ty:?}");
-
     struct Visitor;
     impl VisitMut for Visitor {
         fn visit_path_mut(&mut self, node: &mut Path) {
