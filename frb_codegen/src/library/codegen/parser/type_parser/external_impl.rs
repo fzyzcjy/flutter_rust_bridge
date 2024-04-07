fc8e1ed5674906986594512cd7687225983be02f
fzyzcjy <ch271828n@outlook.com>
2024-04-07 20:56:58 +0800

    fix: more compile

--- frb_codegen/src/library/codegen/parser/type_parser/external_impl.rs
+++ frb_codegen/src/library/codegen/parser/type_parser/external_impl.rs
@@ -1,7 +1,14 @@
-pub(crate) fn parse_external_impl_dummy_struct_name(raw_name: &str) -> Option<String> {
+use anyhow::Result;
+
+pub(crate) fn parse_external_impl_dummy_struct_name(raw_name: &str) -> Result<Option<String>> {
     const DUMMY_STRUCT_PREFIX: &str = "__external_impl__";
-    (raw_name.starts_with(DUMMY_STRUCT_PREFIX))
-        .then(|| hex::decode(&raw_name[DUMMY_STRUCT_PREFIX.len()..]))
+    Ok(if raw_name.starts_with(DUMMY_STRUCT_PREFIX) {
+        Some(String::from_utf8(hex::decode(
+            &raw_name[DUMMY_STRUCT_PREFIX.len()..],
+        )?)?)
+    } else {
+        None
+    })
 }
 
 #[cfg(test)]
@@ -11,13 +18,14 @@ mod tests {
     #[test]
     fn test_parse_external_impl_dummy_struct_name() {
         assert_eq!(
-            parse_external_impl_dummy_struct_name("One<Two,Three>"),
+            parse_external_impl_dummy_struct_name("One<Two,Three>").unwrap(),
             None,
         );
         assert_eq!(
             parse_external_impl_dummy_struct_name(
                 "__external_impl__4f6e65203c2054776f2c205468726565203e"
-            ),
+            )
+            .unwrap(),
             Some("One<Two,Three>".to_owned()),
         );
     }
