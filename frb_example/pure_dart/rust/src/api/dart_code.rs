5dc7f829ca7b06f81a525534c69fe970dc95662c
fzyzcjy <ch271828n@outlook.com>
2024-04-07 18:19:28 +0800

    feat: more test

--- frb_example/pure_dart/rust/src/api/dart_code.rs
+++ frb_example/pure_dart/rust/src/api/dart_code.rs
@@ -1,6 +1,9 @@
 use flutter_rust_bridge::frb;
 
-#[frb(dart_code = "
+#[frb(
+    non_hash,
+    non_eq,
+    dart_code = "
   @override
   bool operator ==(StructWithDartCodeTwinNormal other) => a == other.a;
 
@@ -8,7 +11,8 @@ use flutter_rust_bridge::frb;
   int get hashCode => a.hashCode;
 
   int dartCodeMethod() => a * 2;
-")]
+"
+)]
 pub struct TranslatableStructWithDartCodeTwinNormal {
     pub a: i32,
 }
@@ -21,3 +25,9 @@ impl TranslatableStructWithDartCodeTwinNormal {
 
 #[frb(opaque, dart_code = "static int dartCodeGetter => 123;")]
 pub struct OpaqueStructWithDartCodeTwinNormal;
+
+impl OpaqueStructWithDartCodeTwinNormal {
+    pub fn normal_method_twin_normal(&self) -> i32 {
+        self.a * 2
+    }
+}
