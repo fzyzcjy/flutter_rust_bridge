acacb4f48e126df2acd6cc9d33f945c01d55c2a7
fzyzcjy <ch271828n@outlook.com>
2024-04-08 09:28:30 +0800

    chore: import

--- frb_example/pure_dart/test/api/external_impl_test.dart
+++ frb_example/pure_dart/test/api/external_impl_test.dart
@@ -1,6 +1,6 @@
 // FRB_INTERNAL_GENERATOR: {"forbiddenDuplicatorModes": ["sync", "rustAsync", "sse", "sync sse", "rustAsync sse"]}
 
-import 'package:frb_example_pure_dart/src/rust/api/pseudo_manual/external_impl_twin_sync_sse.dart';
+import 'package:frb_example_pure_dart/src/rust/api/external_impl.dart';
 import 'package:frb_example_pure_dart/src/rust/frb_generated.dart';
 import 'package:test/test.dart';
 
