360ad1f4d4a05f8a07fb657a4fc3a795fbbfc04b
fzyzcjy <ch271828n@outlook.com>
2024-04-08 17:20:58 +0800

    feat: more serialize

--- frb_dart/lib/src/generalized_isolate/generalized_isolate.dart
+++ frb_dart/lib/src/generalized_isolate/generalized_isolate.dart
@@ -1,9 +1,4 @@
 /// Like Dart's `isolate`, but works for both native and Web.
 library;
 
-import 'package:flutter_rust_bridge/src/platform_types/platform_types.dart';
-
 export '_io.dart' if (dart.library.html) '_web.dart';
-
-/// {@macro flutter_rust_bridge.internal}
-String serializeNativePort(NativePortType port) => port.toString();
