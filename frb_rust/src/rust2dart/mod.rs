edb09b90dbc722260096a8ae32a3b1b5d12c8dfd
fzyzcjy <ch271828n@outlook.com>
2024-04-08 17:49:57 +0800

    refactor: more mv

--- frb_rust/src/rust2dart/mod.rs
+++ frb_rust/src/rust2dart/mod.rs
@@ -1,4 +1,3 @@
 pub(crate) mod action;
 pub(crate) mod context;
 pub(crate) mod sender;
-pub(crate) mod stream_sink;
--- frb_rust/src/stream/stream_sink.rs
+++ frb_rust/src/stream/stream_sink.rs
@@ -1,13 +1,45 @@
-use std::marker::PhantomData;
+use crate::codec::BaseCodec;
+use crate::codec::Rust2DartMessageTrait;
+use crate::generalized_isolate::{channel_to_handle, handle_to_channel, SendableChannelHandle};
 use crate::platform_types::MessagePort;
+use crate::rust2dart::sender::{Rust2DartSendError, Rust2DartSender};
+use std::marker::PhantomData;
+use std::sync::Arc;
 
-pub(crate) struct StreamSink<T> {
-    port: MessagePort,
-    _phantom: PhantomData<T>,
+/// A sink to send asynchronous data back to Dart.
+/// Represented as a Dart
+/// [`Stream`](https://api.dart.dev/stable/dart-async/Stream-class.html).
+#[derive(Clone)]
+pub struct StreamSinkBase<T, Rust2DartCodec: BaseCodec> {
+    sendable_channel_handle: SendableChannelHandle,
+    _closer: Arc<StreamSinkCloser<Rust2DartCodec>>,
+    _phantom_data: (PhantomData<T>, PhantomData<Rust2DartCodec>),
 }
 
-impl<T> StreamSink<T> {
+impl<T, Rust2DartCodec: BaseCodec> StreamSinkBase<T, Rust2DartCodec> {
     pub fn deserialize() -> Self {
         todo!()
     }
+
+    /// Create a new sink from a port wrapper.
+    pub(crate) fn new(
+        sender: Rust2DartSender,
+        closer: Arc<StreamSinkCloser<Rust2DartCodec>>,
+    ) -> Self {
+        Self {
+            sendable_channel_handle: channel_to_handle(&sender.channel),
+            _closer: closer,
+            _phantom_data: Default::default(),
+        }
+    }
+
+    /// Add data to the stream. Returns false when data could not be sent,
+    /// or the stream has been closed.
+    pub fn add(&self, value: Rust2DartCodec::Message) -> Result<(), Rust2DartSendError> {
+        sender(&self.sendable_channel_handle).send(value.into_dart_abi())
+    }
+}
+
+fn sender(sendable_channel_handle: &SendableChannelHandle) -> Rust2DartSender {
+    Rust2DartSender::new(handle_to_channel(sendable_channel_handle))
 }
