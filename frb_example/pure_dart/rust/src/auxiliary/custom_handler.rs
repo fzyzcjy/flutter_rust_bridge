61f7bef95e1f571ebfcb9a23a2b34a506e614579
fzyzcjy <ch271828n@outlook.com>
2024-04-08 10:35:20 +0800

    chore: empty

--- frb_example/pure_dart/rust/src/auxiliary/custom_handler.rs
+++ frb_example/pure_dart/rust/src/auxiliary/custom_handler.rs
@@ -1,9 +1,37 @@
+#![allow(unused_variables)]
+
+use std::future::Future;
 use flutter_rust_bridge::for_generated::*;
+use flutter_rust_bridge::*;
 
 pub type MyUnmodifiedHandler = SimpleHandler<
     SimpleExecutor<NoOpErrorListener, SimpleThreadPool, SimpleAsyncRuntime>,
     NoOpErrorListener,
 >;
+
+pub struct MyCustomHandler;
+
+impl Handler for MyCustomHandler {
+    fn wrap_normal<Rust2DartCodec, PrepareFn, TaskFn>(&self, task_info: TaskInfo, prepare: PrepareFn) where PrepareFn: FnOnce() -> TaskFn, TaskFn: FnOnce(flutter_rust_bridge::handler::handler::TaskContext<Rust2DartCodec>) -> Result<Rust2DartCodec::Message, Rust2DartCodec::Message> + Send + 'static, Rust2DartCodec: BaseCodec {
+        unimplemented!()
+    }
+
+    fn wrap_sync<Rust2DartCodec, SyncTaskFn>(&self, task_info: TaskInfo, sync_task: SyncTaskFn) -> <Rust2DartCodec::Message as flutter_rust_bridge::codec::Rust2DartMessageTrait>::WireSyncRust2DartType where SyncTaskFn: FnOnce() -> Result<Rust2DartCodec::Message, Rust2DartCodec::Message>, Rust2DartCodec: BaseCodec {
+        unimplemented!()
+    }
+
+    fn wrap_async<Rust2DartCodec, PrepareFn, TaskFn, TaskRetFut>(&self, task_info: TaskInfo, prepare: PrepareFn) where PrepareFn: FnOnce() -> TaskFn, TaskFn: FnOnce(flutter_rust_bridge::handler::handler::TaskContext<Rust2DartCodec>) -> TaskRetFut + Send + 'static, TaskRetFut: Future<Output=Result<Rust2DartCodec::Message, Rust2DartCodec::Message>> + flutter_rust_bridge::handler::handler::TaskRetFutTrait, Rust2DartCodec: BaseCodec {
+        unimplemented!()
+    }
+
+    fn dart_fn_invoke(&self, dart_fn: DartOpaque, args: Vec<DartAbi>) -> DartFnFuture<Dart2RustMessageSse> {
+        unimplemented!()
+    }
+
+    fn dart_fn_handle_output(&self, call_id: i32, message: Dart2RustMessageSse) {
+        unimplemented!()
+    }
+}
 
 #[cfg(test)]
 mod tests {
