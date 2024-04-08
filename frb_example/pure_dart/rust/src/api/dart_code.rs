8158724bc1a48065f2038408d761637a563851fd
fzyzcjy <ch271828n@outlook.com>
2024-04-08 10:58:46 +0800

    chore: fix test

--- frb_example/pure_dart/rust/src/api/dart_code.rs
+++ frb_example/pure_dart/rust/src/api/dart_code.rs
@@ -5,7 +5,7 @@ use flutter_rust_bridge::frb;
     non_eq,
     dart_code = "
   @override
-  bool operator ==(StructWithDartCodeTwinNormal other) => a == other.a;
+  bool operator ==(dynamic other) => a == other.a;
 
   @override
   int get hashCode => a.hashCode;
