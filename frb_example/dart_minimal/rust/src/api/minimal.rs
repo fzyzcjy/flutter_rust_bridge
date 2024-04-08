26a79028d7faf9f88dfd41e91cffa99453eba8fb
fzyzcjy <ch271828n@outlook.com>
2024-04-08 08:38:00 +0800

    chore: rm demo

--- frb_example/dart_minimal/rust/src/api/minimal.rs
+++ frb_example/dart_minimal/rust/src/api/minimal.rs
@@ -8,13 +8,3 @@ pub fn init_app() {
 pub fn minimal_adder(a: i32, b: i32) -> i32 {
     a + b
 }
-
-// Indeed in external crate
-#[frb(opaque)]
-pub struct HelloType;
-
-// TODO temporary
-#[frb(external)]
-impl HelloType {
-    pub fn simple_external_method(&self) -> String {}
-}
