41d79f5c626391baffa4175f516c9cc08521ed65
fzyzcjy <ch271828n@outlook.com>
2024-04-08 11:17:18 +0800

    fix: simple

--- frb_example/pure_dart/rust/src/api/dart_code.rs
+++ frb_example/pure_dart/rust/src/api/dart_code.rs
@@ -5,7 +5,8 @@ use flutter_rust_bridge::frb;
     non_eq,
     dart_code = "
   @override
-  bool operator ==(TranslatableStructWithDartCodeTwinNormal other) => a == other.a;
+  bool operator ==(Object other) =>
+    other is TranslatableStructWithDartCodeTwinNormal && a == other.a;
 
   @override
   int get hashCode => a.hashCode;
