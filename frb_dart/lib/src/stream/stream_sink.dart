4dab5fe9a5e7a8b91899a13d229fd2c950949d87
fzyzcjy <ch271828n@outlook.com>
2024-04-08 17:46:22 +0800

    feat: reimpl

--- frb_dart/lib/src/stream/stream_sink.dart
+++ frb_dart/lib/src/stream/stream_sink.dart
@@ -4,39 +4,42 @@ import 'package:flutter_rust_bridge/src/utils/port_generator.dart';
 
 /// The Rust `StreamSink<T>` on the Dart side.
 class RustStreamSink<T> {
-  final ReceivePort _receivePort;
-  final Stream<T> _stream;
-
-  /// Construct an instance
-  factory RustStreamSink({
-    required BaseCodec<T, dynamic, dynamic> codec,
-    String? debugName,
-  }) {
-    final portName = ExecuteStreamPortGenerator.create(debugName ?? 'unnamed');
-    final receivePort = broadcastPort(portName);
-
-    final Stream<T> stream = () async* {
-      try {
-        await for (final raw in receivePort) {
-          try {
-            yield codec.decodeObject(raw);
-          } on CloseStreamException {
-            break;
-          }
-        }
-      } finally {
-        receivePort.close();
-      }
-    }();
-
-    return RustStreamSink._(receivePort, stream);
-  }
-
-  RustStreamSink._(this._receivePort, this._stream);
+  _State<T>? _state;
 
   /// {@macro flutter_rust_bridge.only_for_generated_code}
-  String serialize() => serializeNativePort(_receivePort.sendPort.nativePort);
+  String setupAndSerialize({required BaseCodec<T, dynamic, dynamic> codec}) {
+    _state ??= _setup(codec);
+    return serializeNativePort(_state!.receivePort.sendPort.nativePort);
+  }
 
   /// The Dart stream for the Rust sink
-  Stream<T> get stream => _stream;
+  Stream<T> get stream => _state!.stream;
+}
+
+class _State<T> {
+  final ReceivePort receivePort;
+  final Stream<T> stream;
+
+  const _State(this.receivePort, this.stream);
+}
+
+_State<T> _setup<T>(BaseCodec<T, dynamic, dynamic> codec) {
+  final portName = ExecuteStreamPortGenerator.create('RustStreamSink');
+  final receivePort = broadcastPort(portName);
+
+  final Stream<T> stream = () async* {
+    try {
+      await for (final raw in receivePort) {
+        try {
+          yield codec.decodeObject(raw);
+        } on CloseStreamException {
+          break;
+        }
+      }
+    } finally {
+      receivePort.close();
+    }
+  }();
+
+  return _State(receivePort, stream);
 }
