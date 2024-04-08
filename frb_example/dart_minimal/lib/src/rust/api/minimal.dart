88d42beabbe687d2083e03718cdba08b44647f8e
fzyzcjy <ch271828n@outlook.com>
2024-04-08 18:30:48 +0800

    chore: codegen

--- frb_example/dart_minimal/lib/src/rust/api/minimal.dart
+++ frb_example/dart_minimal/lib/src/rust/api/minimal.dart
@@ -9,5 +9,8 @@ import 'package:flutter_rust_bridge/flutter_rust_bridge_for_generated.dart';
 Future<int> minimalAdder({required int a, required int b, dynamic hint}) =>
     RustLib.instance.api.minimalAdder(a: a, b: b, hint: hint);
 
-Future<void> helloStream({required RustStreamSink<int> sink, dynamic hint}) =>
-    RustLib.instance.api.helloStream(sink: sink, hint: hint);
+Future<void> helloStream({required RustStreamSink<int> sink, dynamic hint}) {
+  final sink = RustStreamSink();
+  await RustLib.instance.api.helloStream(sink: sink, hint: hint);
+  return sink.stream;
+}
