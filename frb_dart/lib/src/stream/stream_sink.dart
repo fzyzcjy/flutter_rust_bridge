2a019e0936f3ab9522010401eb5a4c7a9bb7a4b9
fzyzcjy <ch271828n@outlook.com>
2024-04-09 13:59:01 +0800

    Revert "chore: more"
    This reverts commit f52ef3626af0529fd135ce49df3970804974307d.

--- frb_dart/lib/src/stream/stream_sink.dart
+++ frb_dart/lib/src/stream/stream_sink.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter_rust_bridge/src/codec/base.dart';
 import 'package:flutter_rust_bridge/src/generalized_isolate/generalized_isolate.dart';
 import 'package:flutter_rust_bridge/src/utils/port_generator.dart';
@@ -29,21 +27,6 @@ _State<T> _setup<T>(BaseCodec<T, dynamic, dynamic> codec) {
   final portName = ExecuteStreamPortGenerator.create('RustStreamSink');
   final receivePort = broadcastPort(portName);
 
-  final outputStreamController = StreamController<T>();
-
-  late final StreamSubscription<dynamic> subscription;
-  subscription = receivePort.listen(
-    (message) {
-      TODO;
-    },
-    onError: (Object e, StackTrace s) {
-      TODO;
-    },
-    onDone: () {
-      TODO;
-    },
-  );
-
   final Stream<T> stream = () async* {
     try {
       print('hi RustStreamSink async* start');
@@ -62,5 +45,5 @@ _State<T> _setup<T>(BaseCodec<T, dynamic, dynamic> codec) {
     }
   }();
 
-  return _State(receivePort, outputStreamController.stream);
+  return _State(receivePort, stream);
 }
