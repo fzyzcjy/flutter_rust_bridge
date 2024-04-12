733344cb58fd7a9fa7c4eb58e8a1e39f85a09f5a
fzyzcjy <ch271828n@outlook.com>
2024-04-12 21:25:21 +0800

    feat: more

--- frb_codegen/src/library/codegen/generator/wire/rust/content_hasher.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/content_hasher.rs
@@ -1,5 +1,4 @@
 use crate::codegen::generator::acc::Acc;
-use itertools::Itertools;
 use sha1::Digest;
 
 pub(crate) const CONTENT_HASH_PLACEHOLDER: &str = "__PLACEHOLDER_FRB_CONTENT_HASH__";
@@ -13,9 +12,7 @@ pub(crate) fn text_inject_content_hash(
 }
 
 pub(crate) fn compute_content_hash(text: &Acc<Option<String>>) -> i32 {
-    let joined_text = (text.clone().into_vec().into_iter())
-        .filter_map(|x| x)
-        .join("\n");
-    let digest = sha1::Sha1::digest(joined_text.as_bytes());
+    let interest_text = text.common.as_deref().unwrap_or("");
+    let digest = sha1::Sha1::digest(interest_text.as_bytes());
     i32::from_le_bytes(digest[..4].try_into().unwrap())
 }
