0c239ae06e303dd825c96f8cbed247a12ac2ce28
fzyzcjy <ch271828n@outlook.com>
2024-04-12 16:30:52 +0800

    feat: change ty

--- frb_codegen/src/library/codegen/generator/api_dart/spec_generator/class/mod.rs
+++ frb_codegen/src/library/codegen/generator/api_dart/spec_generator/class/mod.rs
@@ -1,4 +1,4 @@
-use crate::codegen::ir::namespace::Namespace;
+use crate::codegen::ir::namespace::{Namespace, NamespacedName};
 use crate::utils::basic_code::DartBasicHeaderCode;
 use serde::Serialize;
 
@@ -10,7 +10,7 @@ pub(crate) mod ty;
 #[derive(Debug, Serialize, Default)]
 pub(crate) struct ApiDartGeneratedClass {
     pub(crate) header: DartBasicHeaderCode,
-    pub(crate) namespace: Namespace,
+    pub(crate) name: NamespacedName,
     pub(crate) code: String,
     pub(crate) needs_freezed: bool,
 }
