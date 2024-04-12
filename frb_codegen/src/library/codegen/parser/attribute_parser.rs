e6e332304c8f398c584ec1219ba928f9805be62d
fzyzcjy <ch271828n@outlook.com>
2024-04-12 17:12:44 +0800

    feat: attr

--- frb_codegen/src/library/codegen/parser/attribute_parser.rs
+++ frb_codegen/src/library/codegen/parser/attribute_parser.rs
@@ -62,6 +62,10 @@ impl FrbAttributes {
         self.any_eq(&FrbAttribute::Sync)
     }
 
+    pub(crate) fn dart_async(&self) -> bool {
+        self.any_eq(&FrbAttribute::DartAsync)
+    }
+
     pub(crate) fn getter(&self) -> bool {
         self.any_eq(&FrbAttribute::Getter)
     }
@@ -154,6 +158,7 @@ mod frb_keyword {
     syn::custom_keyword!(mirror);
     syn::custom_keyword!(non_final);
     syn::custom_keyword!(sync);
+    syn::custom_keyword!(dart_async);
     syn::custom_keyword!(getter);
     syn::custom_keyword!(init);
     syn::custom_keyword!(ignore);
@@ -187,6 +192,7 @@ enum FrbAttribute {
     Mirror(FrbAttributeMirror),
     NonFinal,
     Sync,
+    DartAsync,
     Getter,
     Init,
     Ignore,
@@ -212,6 +218,7 @@ impl Parse for FrbAttribute {
 
         let keyword_output = parse_keyword::<non_final, _>(input, &lookahead, non_final, NonFinal)
             .or_else(|| parse_keyword::<sync, _>(input, &lookahead, sync, Sync))
+            .or_else(|| parse_keyword::<dart_async, _>(input, &lookahead, dart_async, DartAsync))
             .or_else(|| parse_keyword::<getter, _>(input, &lookahead, getter, Getter))
             .or_else(|| parse_keyword::<init, _>(input, &lookahead, init, Init))
             .or_else(|| parse_keyword::<ignore, _>(input, &lookahead, ignore, Ignore))
@@ -544,6 +551,11 @@ mod tests {
     #[test]
     fn test_sync() {
         simple_keyword_tester("sync", FrbAttribute::Sync);
+    }
+
+    #[test]
+    fn test_dart_async() {
+        simple_keyword_tester("dart_async", FrbAttribute::DartAsync);
     }
 
     #[test]
