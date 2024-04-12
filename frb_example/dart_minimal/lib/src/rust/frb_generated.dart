1e5387c3b211ca36e7fc845de39ea1510f680eac
fzyzcjy <ch271828n@outlook.com>
2024-04-12 20:08:12 +0800

    Revert "chore: temp exp"
    This reverts commit bca33d72898dc7f3731f4029c6319ecb81176768.

--- frb_example/dart_minimal/lib/src/rust/frb_generated.dart
+++ frb_example/dart_minimal/lib/src/rust/frb_generated.dart
@@ -56,7 +56,7 @@ class RustLib extends BaseEntrypoint<RustLibApi, RustLibApiImpl, RustLibWire> {
   String get codegenVersion => '2.0.0-dev.31';
 
   @override
-  int get rustContentHash => 42;
+  int get rustContentHash => 20304542;
 
   static const kDefaultExternalLibraryLoaderConfig =
       ExternalLibraryLoaderConfig(
