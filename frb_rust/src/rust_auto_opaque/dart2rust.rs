58544750905a52f327d4bb8600e0517cba2b81c9
fzyzcjy <ch271828n@outlook.com>
2024-05-05 17:57:06 +0800

    chore: empty

--- frb_rust/src/rust_auto_opaque/dart2rust.rs
+++ frb_rust/src/rust_auto_opaque/dart2rust.rs
@@ -2,6 +2,7 @@ use crate::for_generated::BaseArc;
 use crate::rust_async::{RwLock, RwLockReadGuard, RwLockWriteGuard};
 use crate::rust_auto_opaque::order::RustAutoOpaqueOrder;
 use crate::rust_auto_opaque::{RustAutoOpaqueBase, RustAutoOpaqueInner};
+use std::iter::zip;
 
 impl<T, A: BaseArc<RustAutoOpaqueInner<T>>> RustAutoOpaqueBase<T, A> {
     // the function signature is not covered while the whole body is covered - looks like a bug in coverage tool
@@ -42,11 +43,33 @@ pub fn rust_auto_opaque_encode<T, A: BaseArc<RustAutoOpaqueInner<T>>>(
 }
 
 pub fn rust_auto_opaque_decode_compute_order(lock_orders: &[RustAutoOpaqueOrder]) -> Vec<usize> {
-    let mut sorted_index_and_lock_orders: Vec<_> = lock_orders.iter().enumerate().collect();
+    let mut sorted_index_and_lock_orders: Vec<_> =
+        lock_orders.iter().cloned().enumerate().collect();
     sorted_index_and_lock_orders.sort_unstable_by_key(|(_index, order)| order);
+
+    assert!(
+        check_no_mut_multi_borrow(&sorted_index_and_lock_orders),
+        "Cannot borrow an object mutably, and at the same time borrow again in another argument"
+    );
 
     sorted_index_and_lock_orders
         .into_iter()
         .map(|index, _order| index)
         .collect()
+}
+
+fn check_no_mut_multi_borrow(
+    sorted_index_and_lock_orders: &[(usize, RustAutoOpaqueOrder)],
+) -> bool {
+    TODO
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_check_no_mut_multi_borrow() {
+        TODO
+    }
 }
