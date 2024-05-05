9acc9a0eefae444f7098c0bfcc49a8d6cb85e81f
fzyzcjy <ch271828n@outlook.com>
2024-05-05 17:19:50 +0800

    feat: more

--- frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/misc/wire_func_rao.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/misc/wire_func_rao.rs
@@ -6,19 +6,52 @@ use itertools::Itertools;
 
 pub(crate) fn generate_code_inner_decode(func: &IrFunc) -> String {
     let interest_fields = filter_interest_fields(func);
-    TODO
+
+    let declarations = (interest_fields.iter())
+        .map(|(field, _ty)| {
+            format!(
+                "let mut api_{name}_decoded = None;\n",
+                name = get_variable_name(field)
+            )
+        })
+        .join();
+
+    let var_names = (interest_fields.iter())
+        .map(|(field, _ty)| format!("api_{name}", name = get_variable_name(field)))
+        .join(", ");
+
+    let match_arms = (interest_fields.iter().enumerate())
+        .map(|(index, (field, ty))| format!("{index} => {},\n", generate_decode_statement(field)))
+        .join();
+
+    let unwraps = (interest_fields.iter())
+        .map(|(field, _ty)| {
+            format!(
+                "let api_{name} = api_{name}_raw.unwrap();\n",
+                name = get_variable_name(field)
+            )
+        })
+        .join();
+
+    format!(
+        "
+        {declarations}
+        let decoder_orders_ = flutter_rust_bridge::for_generated::rust_auto_opaque_decode_order(&[{var_names}]);
+        for i in decoder_orders_ {{
+            match i {{
+                {match_arms}
+            }}
+        }}
+        {unwraps}
+        "
+    )
 }
 
 fn generate_decode_statement(field: &IrFuncInput) -> Option<String> {
     let mode = o.ownership_mode.to_string().to_case(Case::Snake);
-    let mutability = if o.ownership_mode == OwnershipMode::RefMut {
-        "mut "
-    } else {
-        ""
-    };
     format!(
-        "let {mutability}api_{name} = api_{name}.rust_auto_opaque_decode_{syncness}_{mode}(){maybe_await};\n",
-        name = field.inner.name.rust_style(),
+        "api_{name}_decoded = Some(api_{name}.rust_auto_opaque_decode_{syncness}_{mode}(){maybe_await})",
+        name = get_variable_name(field),
         syncness = if func.rust_async { "async" } else { "sync" },
         maybe_await = if func.rust_async { ".await" } else { "" },
     )
@@ -38,4 +71,8 @@ fn filter_interest_fields(func: &IrFunc) -> Vec<(&IrFuncInput, &IrTypeRustAutoOp
             }
         })
         .collect_vec()
+}
+
+fn get_variable_name(field: &IrFuncInput) -> String {
+    field.inner.name.rust_style()
 }
