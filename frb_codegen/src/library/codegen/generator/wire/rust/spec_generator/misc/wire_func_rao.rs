94c5b5966a815af83bc1e66509682cec588082a3
fzyzcjy <ch271828n@outlook.com>
2024-05-05 17:11:30 +0800

    refactor: func

--- frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/misc/wire_func_rao.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/misc/wire_func_rao.rs
@@ -1,31 +1,32 @@
-use crate::codegen::ir::func::OwnershipMode;
+use crate::codegen::ir::func::{IrFunc, IrFuncInput, OwnershipMode};
 use crate::codegen::ir::ty::IrType;
-use convert_case::Case;
+use convert_case::{Case, Casing};
 
-pub(crate) fn generate_code_inner_decode() -> String {
-    func.inputs
-        .iter()
-        .filter_map(|field| {
-            if let IrType::RustAutoOpaque(o) = &field.inner.ty {
-                if o.ownership_mode != OwnershipMode::Owned {
-                    let mode = o.ownership_mode.to_string().to_case(Case::Snake);
-                    let mutability = if o.ownership_mode == OwnershipMode::RefMut {
-                        "mut "
-                    } else {
-                        ""
-                    };
-                    Some(format!(
-                        "let {mutability}api_{name} = api_{name}.rust_auto_opaque_decode_{syncness}_{mode}(){maybe_await};\n",
-                        name = field.inner.name.rust_style(),
-                        syncness = if func.rust_async { "async" } else { "sync" },
-                        maybe_await = if func.rust_async { ".await" } else { "" },
-                    ))
-                } else {
-                    None
-                }
-            } else {
-                None
-            }
-        })
+pub(crate) fn generate_code_inner_decode(func: &IrFunc) -> String {
+    (func.inputs.iter())
+        .filter_map(|field| generate_decode_statement(field))
         .join("")
+}
+
+fn generate_decode_statement(field: &IrFuncInput) -> Option<String> {
+    if let IrType::RustAutoOpaque(o) = &field.inner.ty {
+        if o.ownership_mode != OwnershipMode::Owned {
+            let mode = o.ownership_mode.to_string().to_case(Case::Snake);
+            let mutability = if o.ownership_mode == OwnershipMode::RefMut {
+                "mut "
+            } else {
+                ""
+            };
+            Some(format!(
+                "let {mutability}api_{name} = api_{name}.rust_auto_opaque_decode_{syncness}_{mode}(){maybe_await};\n",
+                name = field.inner.name.rust_style(),
+                syncness = if func.rust_async { "async" } else { "sync" },
+                maybe_await = if func.rust_async { ".await" } else { "" },
+            ))
+        } else {
+            None
+        }
+    } else {
+        None
+    }
 }
