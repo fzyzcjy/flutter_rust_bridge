56200f7103ddca401f83c480c59fa5fc4968c1a7
fzyzcjy <ch271828n@outlook.com>
2024-05-15 17:57:15 +0800

    chore: early return

--- frb_codegen/src/library/commands/format_dart.rs
+++ frb_codegen/src/library/commands/format_dart.rs
@@ -9,6 +9,10 @@ use std::path::{Path, PathBuf};
 
 #[allow(clippy::vec_init_then_push)]
 pub fn format_dart(paths: &[PathBuf], base_path: &Path, line_length: u32) -> anyhow::Result<()> {
+    if paths.is_empty() {
+        return Ok(());
+    }
+
     let paths = prepare_paths(paths, base_path)?;
     debug!("execute format_dart paths={paths:?} line_length={line_length}");
 
