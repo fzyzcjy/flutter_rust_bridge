5e118d41328b225cb4dd8d27d4bed157d87352de
fzyzcjy <ch271828n@outlook.com>
2024-05-15 20:06:29 +0800

    fix: err

--- frb_codegen/src/library/codegen/generator/api_dart/spec_generator/function.rs
+++ frb_codegen/src/library/codegen/generator/api_dart/spec_generator/function.rs
@@ -58,7 +58,7 @@ pub(crate) fn generate(
 
     let func_expr = format!(
         "{func_return_type} {func_name}({func_params_str})",
-        func_name = func.name_dart_api(),
+        func_name = func.name_dart_wire(),
     );
 
     let func_comments = generate_dart_comments(&func.comments);
