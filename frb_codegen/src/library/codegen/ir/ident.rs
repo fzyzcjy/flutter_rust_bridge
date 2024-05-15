cd6e7e4142de5dcd21bb4009f8077863a38a72a6
fzyzcjy <ch271828n@outlook.com>
2024-05-15 18:47:41 +0800

    feat: mv

--- frb_codegen/src/library/codegen/ir/ident.rs
+++ frb_codegen/src/library/codegen/ir/ident.rs
@@ -1,4 +1,5 @@
 use crate::codegen::generator::codec::sse::lang::Lang;
+use crate::utils::cbindgen_keywords;
 use convert_case::{Case, Casing};
 crate::ir! {
 #[serde(transparent)]
@@ -51,86 +52,7 @@ fn convert_rust_to_c_style(raw: &str) -> String {
     }
 
     // match behavior of cbindgen
-    if CBINDGEN_RESERVED_KEYWORDS.contains(&ans) {
-        ans.push("_");
-    }
+    cbindgen_keywords::escape(&mut ans);
 
     ans
 }
-
-// copied from https://github.com/mozilla/cbindgen/blob/ca78140c01518a655355f84da1f3872939123b66/src/bindgen/reserved.rs#L8
-const CBINDGEN_RESERVED_KEYWORDS: &[&str] = &[
-    "alignas",
-    "alignof",
-    "auto",
-    "bool",
-    "break",
-    "case",
-    "catch",
-    "char",
-    "char16_t",
-    "char32_t",
-    "char8_t",
-    "class",
-    "const",
-    "const_cast",
-    "consteval",
-    "constexpr",
-    "continue",
-    "decltype",
-    "default",
-    "delete",
-    "do",
-    "double",
-    "dynamic_cast",
-    "else",
-    "enum",
-    "explicit",
-    "export",
-    "extern",
-    "false",
-    "float",
-    "for",
-    "friend",
-    "goto",
-    "if",
-    "inline",
-    "int",
-    "long",
-    "mutable",
-    "namespace",
-    "new",
-    "noexcept",
-    "nullptr",
-    "operator",
-    "private",
-    "protected",
-    "public",
-    "register",
-    "reinterpret_cast",
-    "return",
-    "short",
-    "signed",
-    "sizeof",
-    "static",
-    "static_assert",
-    "static_cast",
-    "struct",
-    "switch",
-    "template",
-    "this",
-    "thread_local",
-    "throw",
-    "true",
-    "try",
-    "typedef",
-    "typename",
-    "union",
-    "unsigned",
-    "using",
-    "virtual",
-    "void",
-    "volatile",
-    "wchar_t",
-    "while",
-];
