aa03d0b08af6759324dc7c0175e1be51031af746
fzyzcjy <ch271828n@outlook.com>
2024-05-19 15:33:48 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/sanity_checker.rs
+++ frb_codegen/src/library/codegen/parser/sanity_checker.rs
@@ -2,6 +2,7 @@ use crate::codegen::dumper::Dumper;
 use crate::codegen::parser::file_reader::read_files;
 use crate::codegen::parser::function_extractor::extract_generalized_functions_from_file;
 use crate::codegen::parser::reader::CachedRustReader;
+use itertools::Itertools;
 use log::warn;
 use std::path::{Path, PathBuf};
 
@@ -24,7 +25,7 @@ pub(crate) fn check_suppressed_input_path_no_content(
             warn!(
                 "Functions or methods in {:?} are ignored. Please do not put them in `mod.rs`. (Function names: {:?})",
                 file.path,
-                extracted_fns.iter().map(|f| f.sig().ident).collect_vec()
+                extracted_fns.iter().map(|f| f.generalized_item_fn.sig().ident).collect_vec()
             );
         }
     }
