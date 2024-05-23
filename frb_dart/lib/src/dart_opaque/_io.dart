8ed784d4fd2d9e864551ef0ffe2ddd66754f9df2
fzyzcjy <ch271828n@outlook.com>
2024-05-23 21:47:09 +0800

    Revert "feat: more"
    This reverts commit fe09d4b11f2085d3a91b2b2ca714b574b5992fd2.

--- frb_dart/lib/src/dart_opaque/_io.dart
+++ frb_dart/lib/src/dart_opaque/_io.dart
@@ -1,7 +1,2 @@
-import 'package:flutter_rust_bridge/src/generalized_frb_rust_binding/generalized_frb_rust_binding.dart';
-
 /// {@macro flutter_rust_bridge.only_for_generated_code}
-Object decodeDartOpaque(int raw,
-    GeneralizedFrbRustBinding generalizedFrbRustBinding) {
-  return generalizedFrbRustBinding.dartOpaqueRust2DartDecode(raw);
-}
+int decoderDartOpaqueInner(int raw) => raw;
--- frb_dart/lib/src/dart_opaque/_web.dart
+++ frb_dart/lib/src/dart_opaque/_web.dart
@@ -1,7 +1,2 @@
-import 'package:flutter_rust_bridge/src/generalized_frb_rust_binding/generalized_frb_rust_binding.dart';
-
 /// {@macro flutter_rust_bridge.only_for_generated_code}
-Object decodeDartOpaque(
-    BigInt raw, GeneralizedFrbRustBinding generalizedFrbRustBinding) {
-  return generalizedFrbRustBinding.dartOpaqueRust2DartDecode(raw.toInt());
-}
+int decoderDartOpaqueInner(BigInt raw) => raw.toInt();
