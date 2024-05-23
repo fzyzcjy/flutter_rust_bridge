f184c71c1457fdecd65cb9e3ef5d15d6e6215a56
fzyzcjy <ch271828n@outlook.com>
2024-05-23 13:53:36 +0800

    feat: assert

--- tools/frb_internal/lib/src/frb_example_pure_dart_generator/rust/direct_sources.dart
+++ tools/frb_internal/lib/src/frb_example_pure_dart_generator/rust/direct_sources.dart
@@ -4,7 +4,8 @@ import 'package:flutter_rust_bridge_internal/src/frb_example_pure_dart_generator
 
 Map<String, String> generateRustDirectSources() {
   return {
-    'pseudo_manual/basic.rs': _generateBasicRelated((x) => x, ''),
+    'pseudo_manual/basic.rs':
+        _generateBasicRelated((x) => x, '', withArgExpect: true),
     'pseudo_manual/basic_optional.rs':
         _generateBasicRelated((x) => 'Option<$x>', '_optional'),
     'pseudo_manual/basic_list.rs': _generateBasicRelated(
@@ -24,6 +25,7 @@ String _generateBasicRelated(
   String postfix, {
   String extraBody = '',
   bool Function(BasicTypeInfo)? enable,
+  bool withArgExpect = false,
 }) {
   final builder = RustFileBuilder();
   builder.body += extraBody;
@@ -52,8 +54,17 @@ pub use super::basic::*;
   }
   for (final ty in kBasicTypes) {
     if (enable?.call(ty) ?? true) {
-      builder.addIdentityFunction(rustTypeNameWrapper(ty.rustTypeName),
-          'example_basic${postfix}_type_${ty.name}');
+      final partialName = 'example_basic${postfix}_type_${ty.name}';
+      if (withArgExpect) {
+        builder.body +=
+            '''pub fn ${partialName}_twin_normal(arg: $ty, expect: String) -> $ty {
+          assert_eq!(arg, expect.parse().unwrap());
+          arg
+        }\n\n''';
+      } else {
+        builder.addIdentityFunction(
+            rustTypeNameWrapper(ty.rustTypeName), partialName);
+      }
     }
   }
   return builder.toString();
