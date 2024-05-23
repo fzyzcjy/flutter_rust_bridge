7e529ae7453bd69026919e0143930beebb9a8ca2
fzyzcjy <ch271828n@outlook.com>
2024-05-23 15:14:25 +0800

    chore: codegen

--- frb_example/pure_dart/test/api/pseudo_manual/type_alias_twin_rust_async_test.dart
+++ frb_example/pure_dart/test/api/pseudo_manual/type_alias_twin_rust_async_test.dart
@@ -11,17 +11,18 @@ Future<void> main({bool skipRustLibInit = false}) async {
   if (!skipRustLibInit) await RustLib.init();
 
   test('dart call handle_type_id', () async {
-    final id = await handleTypeAliasIdTwinRustAsync(input: 42);
+    final id = await handleTypeAliasIdTwinRustAsync(input: BigInt.from(42));
     expect(id.toInt(), 42);
   });
 
   test('dart call handle_type_nest_alias_id', () async {
-    final id = await handleTypeNestAliasIdTwinRustAsync(input: 42);
+    final id = await handleTypeNestAliasIdTwinRustAsync(input: BigInt.from(42));
     expect(id.toInt(), 42);
   });
 
   test('dart call handle_type_model', () async {
-    final testModel = await handleTypeAliasModelTwinRustAsync(input: 42);
+    final testModel =
+        await handleTypeAliasModelTwinRustAsync(input: BigInt.from(42));
     expect(testModel.id.toInt(), 42);
     expect(testModel.name, "TestModel");
     expect(testModel.aliasEnum, MyEnum.False);
