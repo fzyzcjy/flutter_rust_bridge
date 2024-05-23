b6c69f22e198bd67148871e70096cf118b2c7ff0
fzyzcjy <ch271828n@outlook.com>
2024-05-23 21:44:04 +0800

    fix: more

--- frb_dart/lib/src/dart_opaque/_io.dart
+++ frb_dart/lib/src/dart_opaque/_io.dart
@@ -1,1 +1,2 @@
-
+/// {@macro flutter_rust_bridge.only_for_generated_code}
+int decoderDartOpaqueInner(int raw) => raw;
--- frb_dart/lib/src/dart_opaque/_web.dart
+++ frb_dart/lib/src/dart_opaque/_web.dart
@@ -1,1 +1,2 @@
-
+/// {@macro flutter_rust_bridge.only_for_generated_code}
+int decoderDartOpaqueInner(BigInt raw) => raw.toInt();
