0e7da761598b165f62a48a905e3f1c07e034accc
fzyzcjy <ch271828n@outlook.com>
2024-05-24 22:11:55 +0800

    chore: more

--- frb_codegen/src/library/codegen/parser/auto_accessor_parser.rs
+++ frb_codegen/src/library/codegen/parser/auto_accessor_parser.rs
@@ -1,5 +1,10 @@
 use crate::codegen::ir::func::IrFunc;
 
 pub(crate) fn parse_auto_accessors() -> anyhow::Result<Vec<IrFunc>> {
-    todo!()
+    TODO
 }
+
+fn get_interest_structs() -> anyhow::Result<Vec<TODO>>{
+    TODO
+}
+
