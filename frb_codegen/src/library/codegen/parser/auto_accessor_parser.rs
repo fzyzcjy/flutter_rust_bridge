cc27ebb360bf87ba5ed1568ae37f6ee1ab5b4ee8
fzyzcjy <ch271828n@outlook.com>
2024-05-24 22:23:40 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/auto_accessor_parser.rs
+++ frb_codegen/src/library/codegen/parser/auto_accessor_parser.rs
@@ -2,15 +2,28 @@ use crate::codegen::config::internal_config::RustInputPathPack;
 use crate::codegen::ir::func::IrFunc;
 use crate::codegen::parser::misc::extract_src_types_in_paths;
 use crate::codegen::parser::source_graph::modules::Struct;
+use crate::codegen::parser::type_parser::TypeParser;
+use itertools::Itertools;
 use std::collections::HashMap;
 use std::path::{Path, PathBuf};
 
 pub(crate) fn parse_auto_accessors(
     src_structs: &HashMap<String, &Struct>,
+    type_parser: &mut TypeParser,
     rust_input_paths: &[PathBuf],
     rust_crate_dir: &Path,
 ) -> anyhow::Result<Vec<IrFunc>> {
     let src_structs_in_paths =
         extract_src_types_in_paths(src_structs, rust_input_paths, rust_crate_dir)?;
-    todo!()
+    src_structs_in_paths
+        .iter()
+        .flat_map(|struct_name| parse_auto_accessors_of_struct(&struct_name.name, type_parser))
+        .collect()
+}
+
+fn parse_auto_accessors_of_struct(
+    struct_name: &str,
+    type_parser: &mut TypeParser,
+) -> anyhow::Result<Vec<IrFunc>> {
+    TODO
 }
