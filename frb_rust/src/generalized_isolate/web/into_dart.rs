61a2bfbb05fbd367c25c50faf504468ea8b19935
fzyzcjy <ch271828n@outlook.com>
2024-05-25 07:58:27 +0800

    fix: err

--- frb_rust/src/generalized_isolate/web/into_dart.rs
+++ frb_rust/src/generalized_isolate/web/into_dart.rs
@@ -1,6 +1,7 @@
 use crate::for_generated::BaseArc;
 use crate::generalized_isolate::ZeroCopyBuffer;
 use crate::platform_types::DartAbi;
+#[cfg(feature = "rust-async")]
 use crate::rust_auto_opaque::{inner::RustAutoOpaqueInner, RustAutoOpaqueBase};
 use crate::rust_opaque::RustOpaqueBase;
 use js_sys::{Array, BigInt64Array, BigUint64Array, Int32Array};
@@ -15,6 +16,7 @@ pub trait IntoDart {
 pub trait IntoDartExceptPrimitive: IntoDart {}
 impl IntoDartExceptPrimitive for JsValue {}
 impl<T, A: BaseArc<T>> IntoDartExceptPrimitive for RustOpaqueBase<T, A> {}
+#[cfg(feature = "rust-async")]
 impl<T, A: BaseArc<RustAutoOpaqueInner<T>>> IntoDartExceptPrimitive for RustAutoOpaqueBase<T, A> {}
 #[cfg(feature = "dart-opaque")]
 impl IntoDartExceptPrimitive for crate::dart_opaque::DartOpaque {}
@@ -250,6 +252,7 @@ impl<T, A: BaseArc<T>> IntoDart for RustOpaqueBase<T, A> {
     }
 }
 
+#[cfg(feature = "rust-async")]
 impl<T, A: BaseArc<RustAutoOpaqueInner<T>>> IntoDart for RustAutoOpaqueBase<T, A> {
     #[inline]
     fn into_dart(self) -> DartAbi {
