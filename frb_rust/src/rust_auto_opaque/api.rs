a20b17bf9e2ef74d42c98999834962110eb11394
fzyzcjy <ch271828n@outlook.com>
2024-05-24 13:47:36 +0800

    chore: more

--- frb_rust/src/rust_auto_opaque/api.rs
+++ frb_rust/src/rust_auto_opaque/api.rs
@@ -5,7 +5,7 @@ use crate::rust_auto_opaque::RustAutoOpaqueBase;
 use crate::rust_opaque::RustOpaqueBase;
 use tokio::sync::RwLock;
 
-impl<T: 'static, A: BaseArc<RustAutoOpaqueInner<T>>> RustAutoOpaqueBase<T, A> {
+impl<T, A: BaseArc<RustAutoOpaqueInner<T>>> RustAutoOpaqueBase<T, A> {
     pub fn new(value: T) -> Self {
         Self(RustOpaqueBase::new(RustAutoOpaqueInner::new(RwLock::new(
             value,
