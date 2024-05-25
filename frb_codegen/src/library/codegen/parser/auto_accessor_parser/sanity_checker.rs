583eae6758cd27234bcfe9500a3afaabb2f0aa94
fzyzcjy <ch271828n@outlook.com>
2024-05-25 14:27:38 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/auto_accessor_parser/sanity_checker.rs
+++ frb_codegen/src/library/codegen/parser/auto_accessor_parser/sanity_checker.rs
@@ -1,11 +1,16 @@
 use crate::codegen::ir::field::IrField;
 use crate::codegen::ir::namespace::NamespacedName;
+use crate::codegen::ir::ty::delegate::IrTypeDelegate;
 use crate::codegen::ir::ty::IrType;
 use log::warn;
 
 pub(super) fn sanity_check_field(struct_name: &NamespacedName, field: &IrField) {
     match &field.ty {
-        TODO | TODO => {},
+        // TODO more
+        IrType::Primitive(_)
+        | IrType::Delegate(IrTypeDelegate::String)
+        | IrType::Delegate(IrTypeDelegate::Char)
+        | IrType::Delegate(IrTypeDelegate::BigPrimitive(_)) => {},
         _ => warn!(
             "It is suggested (but not required) to wrap field `{struct_name}.{field_name}` with `RustAutoOpaque<..>`. \
             Please refer to https://fzyzcjy.github.io/flutter_rust_bridge/guides/types/arbitrary/rust-auto-opaque/properties for more details.",
