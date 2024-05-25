6e381b3f9ccb429e46cdfda0b6c49a015c8be1a3
fzyzcjy <ch271828n@outlook.com>
2024-05-25 10:48:40 +0800

    fix: err

--- frb_example/pure_dart/test/api/misc_no_twin_example_a_test.dart
+++ frb_example/pure_dart/test/api/misc_no_twin_example_a_test.dart
@@ -2,6 +2,7 @@
 
 import 'package:frb_example_pure_dart/src/rust/api/misc_no_twin_example_a.dart';
 import 'package:frb_example_pure_dart/src/rust/frb_generated.dart';
+import 'package:test/test.dart';
 
 Future<void> main({bool skipRustLibInit = false}) async {
   if (!skipRustLibInit) await RustLib.init();
