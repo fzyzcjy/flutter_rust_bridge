b00ed277ab2873869a13ddacba7a303fd19314e8
fzyzcjy <ch271828n@outlook.com>
2024-05-25 14:25:07 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/auto_accessor_parser/sanity_checker.rs
+++ frb_codegen/src/library/codegen/parser/auto_accessor_parser/sanity_checker.rs
@@ -4,12 +4,13 @@ use crate::codegen::ir::ty::IrType;
 use log::warn;
 
 pub(super) fn sanity_check_field(struct_name: &NamespacedName, field: &IrField) {
-    if matches!(field.ty, IrType::RustAutoOpaqueImplicit(_)) {
-        warn!(
+    match &field.ty {
+        TODO | TODO => {},
+        _ => warn!(
             "It is suggested (but not required) to wrap field `{struct_name}.{field_name}` with `RustAutoOpaque<..>`. \
             Please refer to https://fzyzcjy.github.io/flutter_rust_bridge/guides/types/arbitrary/rust-auto-opaque/properties for more details.",
             struct_name = struct_name.rust_style(),
             field_name = field.name.rust_style(),
-        );
+        ),
     }
 }
