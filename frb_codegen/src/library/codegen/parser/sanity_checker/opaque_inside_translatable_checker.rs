d0526dbfa82432221f632ee3481d6617fa1f6d99
fzyzcjy <ch271828n@outlook.com>
2024-05-25 21:13:10 +0800

    chore: more

--- frb_codegen/src/library/codegen/parser/sanity_checker/opaque_inside_translatable_checker.rs
+++ frb_codegen/src/library/codegen/parser/sanity_checker/opaque_inside_translatable_checker.rs
@@ -1,3 +1,14 @@
-pub(crate) fn check_opaque_inside_translatable() {
-    todo!()
+use crate::codegen::ir::pack::IrPack;
+use crate::codegen::ir::ty::IrType;
+use itertools::Itertools;
+use std::collections::HashSet;
+
+pub(crate) fn check_opaque_inside_translatable(pack: &IrPack) {
+    (pack.distinct_types(None).into_iter())
+        .map(handle_type)
+        .collect()
+}
+
+fn handle_type(ty: IrType) {
+    TODO
 }
