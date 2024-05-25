0b26980be9a14173ba57670a819eaeb7b21edc10
fzyzcjy <ch271828n@outlook.com>
2024-05-25 14:42:12 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/auto_accessor_parser/sanity_checker.rs
+++ frb_codegen/src/library/codegen/parser/auto_accessor_parser/sanity_checker.rs
@@ -4,19 +4,33 @@ use crate::codegen::ir::ty::delegate::IrTypeDelegate;
 use crate::codegen::ir::ty::IrType;
 use log::warn;
 
-pub(super) fn check_field(struct_name: &NamespacedName, field: &IrField) {
-    TODO
+pub(super) fn check_field(
+    struct_name: &NamespacedName,
+    field: &IrField,
+) -> Option<SanityCheckHint> {
+    does_need_hint(&field.ty).then(|| SanityCheckHint {
+        name: format!("{}.{}", struct_name.rust_style(), field.name.rust_style()),
+    })
 }
 
 pub(super) fn report(hints: &[SanityCheckHint]) {
-    TODO
+    if hints.is_empty() {
+        return;
+    }
+
+    warn!(
+        "It is suggested (but not required) to wrap field `{struct_name}.{field_name}` with `RustAutoOpaque<..>`. \
+        Please refer to https://fzyzcjy.github.io/flutter_rust_bridge/guides/types/arbitrary/rust-auto-opaque/properties for more details.",
+        struct_name = struct_name.rust_style(),
+        field_name = field.name.rust_style(),
+    )
 }
 
 pub(super) struct SanityCheckHint {
     name: String,
 }
 
-fn does_need_hint(ty: &IrType) {
+fn does_need_hint(ty: &IrType) -> bool {
     match ty {
         // TODO more
         IrType::Primitive(_)
@@ -27,11 +41,4 @@ fn does_need_hint(ty: &IrType) {
     }
 }
 
-fn display_hint() {
-    warn!(
-        "It is suggested (but not required) to wrap field `{struct_name}.{field_name}` with `RustAutoOpaque<..>`. \
-        Please refer to https://fzyzcjy.github.io/flutter_rust_bridge/guides/types/arbitrary/rust-auto-opaque/properties for more details.",
-        struct_name = struct_name.rust_style(),
-        field_name = field.name.rust_style(),
-    )
-}
+fn display_hint() {}
