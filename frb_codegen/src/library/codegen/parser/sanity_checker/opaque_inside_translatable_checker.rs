6b064d62a7120249ae4c391b7686c0542bc51172
fzyzcjy <ch271828n@outlook.com>
2024-05-25 21:17:04 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/sanity_checker/opaque_inside_translatable_checker.rs
+++ frb_codegen/src/library/codegen/parser/sanity_checker/opaque_inside_translatable_checker.rs
@@ -1,14 +1,33 @@
 use crate::codegen::ir::pack::IrPack;
+use crate::codegen::ir::ty::enumeration::IrVariantKind;
+use crate::codegen::ir::ty::structure::IrStruct;
 use crate::codegen::ir::ty::IrType;
 use itertools::Itertools;
 use std::collections::HashSet;
 
 pub(crate) fn check_opaque_inside_translatable(pack: &IrPack) {
     (pack.distinct_types(None).into_iter())
-        .map(handle_type)
+        .map(|ty| handle_type(pack, ty))
         .collect()
 }
 
-fn handle_type(ty: IrType) {
-    TODO
+fn handle_type(pack: &IrPack, ty: IrType) -> Vec<TODO> {
+    match ty {
+        IrType::StructRef(ty) => {
+            let st = ty.get(pack);
+            handle_struct(st)
+        }
+        IrType::EnumRef(ty) => {
+            let en = ty.get(pack);
+            en.variants.iter().flat_map(|variant| match variant.kind {
+                IrVariantKind::Value => vec![],
+                IrVariantKind::Struct(st) => handle_struct(st),
+            })
+        }
+        _ => vec![],
+    }
+}
+
+fn handle_struct(st: &IrStruct) -> Vec<TODO> {
+    st.fields.iter().map(|field| handle_field(field)).collect()
 }
