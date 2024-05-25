d953e50e35b47dbcfa4e3aa316ddbeae5ac661c4
fzyzcjy <ch271828n@outlook.com>
2024-05-25 14:36:18 +0800

    refactor: split

--- frb_codegen/src/library/codegen/parser/auto_accessor_parser/sanity_checker.rs
+++ frb_codegen/src/library/codegen/parser/auto_accessor_parser/sanity_checker.rs
@@ -5,17 +5,25 @@ use crate::codegen::ir::ty::IrType;
 use log::warn;
 
 pub(super) fn sanity_check_field(struct_name: &NamespacedName, field: &IrField) {
-    match &field.ty {
+    TODO
+}
+
+fn does_need_hint(ty: &IrType) {
+    match ty {
         // TODO more
         IrType::Primitive(_)
         | IrType::Delegate(IrTypeDelegate::String)
         | IrType::Delegate(IrTypeDelegate::Char)
-        | IrType::Delegate(IrTypeDelegate::BigPrimitive(_)) => {},
-        _ => warn!(
-            "It is suggested (but not required) to wrap field `{struct_name}.{field_name}` with `RustAutoOpaque<..>`. \
-            Please refer to https://fzyzcjy.github.io/flutter_rust_bridge/guides/types/arbitrary/rust-auto-opaque/properties for more details.",
-            struct_name = struct_name.rust_style(),
-            field_name = field.name.rust_style(),
-        ),
+        | IrType::Delegate(IrTypeDelegate::BigPrimitive(_)) => false,
+        _ => true,
     }
+}
+
+fn display_hint() {
+    warn!(
+        "It is suggested (but not required) to wrap field `{struct_name}.{field_name}` with `RustAutoOpaque<..>`. \
+        Please refer to https://fzyzcjy.github.io/flutter_rust_bridge/guides/types/arbitrary/rust-auto-opaque/properties for more details.",
+        struct_name = struct_name.rust_style(),
+        field_name = field.name.rust_style(),
+    )
 }
