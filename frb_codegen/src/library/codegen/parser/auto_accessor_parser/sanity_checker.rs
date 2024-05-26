e0c28f506452239bd41e5ba46ebbcee2b40eaa4d
fzyzcjy <ch271828n@outlook.com>
2024-05-26 08:04:54 +0800

    chore: tests

--- frb_codegen/src/library/codegen/parser/auto_accessor_parser/sanity_checker.rs
+++ frb_codegen/src/library/codegen/parser/auto_accessor_parser/sanity_checker.rs
@@ -30,3 +30,48 @@ pub(super) fn report(hints: &[SanityCheckHint]) {
 pub(super) struct SanityCheckHint {
     name: String,
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::codegen::ir::namespace::Namespace;
+    use crate::codegen::ir::ty::boxed::IrTypeBoxed;
+    use crate::codegen::ir::ty::dart_opaque::IrTypeDartOpaque;
+    use crate::codegen::ir::ty::delegate::IrTypeDelegate;
+    use crate::codegen::ir::ty::dynamic::IrTypeDynamic;
+    use crate::codegen::ir::ty::optional::IrTypeOptional;
+    use crate::codegen::ir::ty::primitive::IrTypePrimitive;
+    use crate::codegen::ir::ty::rust_opaque::{
+        IrRustOpaqueInner, IrTypeRustOpaque, RustOpaqueCodecMode,
+    };
+    use crate::codegen::ir::ty::IrType;
+    use crate::library::codegen::ir::ty::IrTypeTrait;
+
+    #[test]
+    fn test_cloned_getter_semantics_reasonable() {
+        assert!(IrType::Boxed(IrTypeBoxed {
+            exist_in_real_api: true,
+            inner: Box::new(IrType::Primitive(IrTypePrimitive::F32))
+        })
+        .cloned_getter_semantics_reasonable());
+
+        assert!(IrType::RustOpaque(IrTypeRustOpaque {
+            namespace: Namespace::new_raw("".to_owned()),
+            inner: IrRustOpaqueInner("".to_owned()),
+            codec: RustOpaqueCodecMode::Nom,
+            brief_name: true,
+        })
+        .cloned_getter_semantics_reasonable());
+
+        assert!(IrType::Optional(IrTypeOptional {
+            inner: Box::new(IrType::Primitive(IrTypePrimitive::F32))
+        })
+        .cloned_getter_semantics_reasonable());
+
+        assert!(IrType::Dynamic(IrTypeDynamic).cloned_getter_semantics_reasonable());
+
+        assert!(IrType::DartOpaque(IrTypeDartOpaque).cloned_getter_semantics_reasonable());
+
+        assert!(IrType::Delegate(IrTypeDelegate::String).cloned_getter_semantics_reasonable());
+        assert!(IrType::Delegate(IrTypeDelegate::Char).cloned_getter_semantics_reasonable());
+    }
+}
