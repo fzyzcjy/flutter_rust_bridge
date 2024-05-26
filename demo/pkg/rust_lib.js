2a02fda357a380cc248ad9c7c8c897fe2871f8ed
fzyzcjy <fzyzcjy@users.noreply.github.com>
2024-05-26 05:21:23 +0000

    deploy: d46844b8855891385509d4a4726f8ac0ec8aabf5

--- demo/pkg/rust_lib.js
+++ demo/pkg/rust_lib.js
@@ -224,7 +224,7 @@ function makeMutClosure(arg0, arg1, dtor, f) {
     return real;
 }
 function __wbg_adapter_34(arg0, arg1, arg2) {
-    wasm._dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__ha65f94c2da84a260(arg0, arg1, addHeapObject(arg2));
+    wasm._dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h2ec4c9d3cb5d6245(arg0, arg1, addHeapObject(arg2));
 }
 
 function __wbg_adapter_39(arg0, arg1, arg2) {
@@ -480,10 +480,10 @@ function __wbg_get_imports() {
         const ret = getStringFromWasm0(arg0, arg1);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbg_postMessage_0ab256ae93dc505e = function() { return handleError(function (arg0, arg1) {
+    imports.wbg.__wbg_postMessage_a86d14632008fc58 = function() { return handleError(function (arg0, arg1) {
         getObject(arg0).postMessage(getObject(arg1));
     }, arguments) };
-    imports.wbg.__wbg_error_47cdedd8074067f8 = function(arg0, arg1) {
+    imports.wbg.__wbg_error_61b8775293cd091c = function(arg0, arg1) {
         console.error(getStringFromWasm0(arg0, arg1));
     };
     imports.wbg.__wbindgen_is_falsy = function(arg0) {
