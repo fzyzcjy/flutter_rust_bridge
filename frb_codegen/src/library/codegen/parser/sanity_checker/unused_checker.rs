c70f806a3058d63f4960365f2a59921a750d1ba8
fzyzcjy <ch271828n@outlook.com>
2024-05-28 13:21:27 +0800

    feat: empty

--- frb_codegen/src/library/codegen/parser/sanity_checker/unused_checker.rs
+++ frb_codegen/src/library/codegen/parser/sanity_checker/unused_checker.rs
@@ -71,6 +71,10 @@ fn get_potential_struct_or_enum_names_from_syn_type(ty: &Type) -> anyhow::Result
     })
 }
 
+pub(crate) fn get_skipped_functions() -> anyhow::Result<Vec<NamespacedName>> {
+    return TODO;
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
