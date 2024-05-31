79e2161275fd0438a4bb25bde53e707141bece87
fzyzcjy <ch271828n@outlook.com>
2024-05-31 21:20:40 +0800

    feat: cache

---
+++ frb_codegen/src/library/utils/simple_cache.rs
@@ -0,0 +1,17 @@
+use std::collections::hash_map::Entry::{Occupied, Vacant};
+use std::collections::HashMap;
+
+pub(crate) struct SimpleCache<K, V>(HashMap<K, V>);
+
+impl<K, V> SimpleCache<K, V> {
+    pub(crate) fn get_or_insert(
+        &mut self,
+        key: &K,
+        inserter: impl FnOnce() -> anyhow::Result<V>,
+    ) -> anyhow::Result<&mut V> {
+        Ok(match self.0.entry(key) {
+            Occupied(entry) => entry.into_mut(),
+            Vacant(entry) => entry.insert(inserter()?),
+        })
+    }
+}
