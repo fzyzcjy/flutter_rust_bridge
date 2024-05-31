ea85efe7f4367f5172b5c485072bf0e5c03eb825
fzyzcjy <ch271828n@outlook.com>
2024-05-31 20:42:01 +0800

    feat: more

--- frb_codegen/src/library/codegen/config/internal_config_parser/rust_path_migrator.rs
+++ frb_codegen/src/library/codegen/config/internal_config_parser/rust_path_migrator.rs
@@ -1,1 +1,14 @@
-pub(super) struct ConfigRustRootAndRustInput {}
+use crate::codegen::Config;
+use std::path::PathBuf;
+
+pub(super) struct ConfigRustRootAndRustInput {
+    rust_root: PathBuf,
+    rust_input: String,
+}
+
+pub(super) fn migrate_rust_input_config(config: &Config) -> ConfigRustRootAndRustInput {
+    ConfigRustRootAndRustInput {
+        rust_root: TODO,
+        rust_input: TODO,
+    }
+}
