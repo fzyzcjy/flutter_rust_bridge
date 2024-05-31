3fcf938642e9697204e5bc8d7b53a34e25a9f560
fzyzcjy <ch271828n@outlook.com>
2024-05-31 20:46:41 +0800

    feat: more

--- frb_codegen/src/library/codegen/config/internal_config_parser/rust_path_migrator.rs
+++ frb_codegen/src/library/codegen/config/internal_config_parser/rust_path_migrator.rs
@@ -6,9 +6,54 @@ pub(super) struct ConfigRustRootAndRustInput {
     rust_input: String,
 }
 
-pub(super) fn migrate_rust_input_config(config: &Config) -> ConfigRustRootAndRustInput {
+pub(super) fn migrate_rust_input_config(
+    raw_rust_root: &Option<String>,
+    raw_rust_input: &str,
+) -> anyhow::Result<ConfigRustRootAndRustInput> {
     ConfigRustRootAndRustInput {
         rust_root: TODO,
         rust_input: TODO,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_previous_config_auto_migrated() -> anyhow::Result<()> {
+        let actual = migrate_rust_input_config(None, "rust/src/api/**/*.rs")?;
+        assert_eq!(
+            actual,
+            ConfigRustRootAndRustInput {
+                rust_root: "rust/".into(),
+                rust_input: "crate::api".into()
+            }
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_previous_config_unsupported() -> anyhow::Result<()> {
+        assert!(migrate_rust_input_config(None, "native/src/hello/**/*.rs").is_err());
+    }
+
+    #[test]
+    fn test_current_config() -> anyhow::Result<()> {
+        assert_eq!(
+            migrate_rust_input_config(None, "crate::apple"),
+            ConfigRustRootAndRustInput {
+                rust_root: "rust/".into(),
+                rust_input: "crate::apple".into()
+            }
+        );
+
+        assert_eq!(
+            migrate_rust_input_config(Some("native/"), "crate::orange"),
+            ConfigRustRootAndRustInput {
+                rust_root: "native/".into(),
+                rust_input: "crate::orange".into()
+            }
+        );
     }
 }
