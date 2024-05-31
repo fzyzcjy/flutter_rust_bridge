eb60b816004e74950849e59c998527b1aff9c3cb
fzyzcjy <ch271828n@outlook.com>
2024-05-31 21:53:57 +0800

    refactor: rm dumper

--- frb_codegen/src/library/commands/cargo_expand.rs
+++ frb_codegen/src/library/commands/cargo_expand.rs
@@ -1,4 +1,3 @@
-use crate::codegen::dumper::Dumper;
 use crate::codegen::ConfigDumpContent;
 use crate::command_args;
 use crate::library::commands::command_runner::execute_command;
@@ -14,7 +13,7 @@ use std::collections::HashMap;
 use std::path::{Path, PathBuf};
 use std::{env, fs};
 
-pub(crate) fn run_cargo_expand(rust_crate_dir: &Path, dumper: &Dumper) -> Result<String> {
+pub(crate) fn run_cargo_expand(rust_crate_dir: &Path) -> Result<String> {
     let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap_or_default();
     debug!("run_cargo_expand manifest_dir={manifest_dir} rust_crate_dir={rust_crate_dir:?}");
 
@@ -34,7 +33,7 @@ pub(crate) fn run_cargo_expand(rust_crate_dir: &Path, dumper: &Dumper) -> Result
         // return Ok(fs::read_to_string(rust_file_path)?);
     }
 
-    run_cargo_expand_with_frb_aware(rust_crate_dir, dumper)
+    run_cargo_expand_with_frb_aware(rust_crate_dir)
 }
 
 // fn extract_module(raw_expanded: &str, module: Option<String>) -> Result<String> {
@@ -69,11 +68,10 @@ pub(crate) fn run_cargo_expand(rust_crate_dir: &Path, dumper: &Dumper) -> Result
 //     Ok(raw_expanded.to_owned())
 // }
 
-fn run_cargo_expand_with_frb_aware(rust_crate_dir: &Path, dumper: &Dumper) -> Result<String> {
+fn run_cargo_expand_with_frb_aware(rust_crate_dir: &Path) -> Result<String> {
     Ok(unwrap_frb_attrs_in_doc(&run_cargo_expand_raw(
         rust_crate_dir,
         "--cfg frb_expand",
-        dumper,
         true,
     )?)
     .into_owned())
@@ -94,7 +92,6 @@ fn unwrap_frb_attrs_in_doc(code: &str) -> Cow<str> {
 fn run_cargo_expand_raw(
     rust_crate_dir: &Path,
     extra_rustflags: &str,
-    dumper: &Dumper,
     allow_auto_install: bool,
 ) -> Result<String> {
     // let _pb = simple_progress("Run cargo-expand".to_owned(), 1);
@@ -117,7 +114,7 @@ fn run_cargo_expand_raw(
         if stderr.contains("no such command: `expand`") && allow_auto_install {
             info!("Cargo expand is not installed. Automatically install and re-run.");
             install_cargo_expand()?;
-            return run_cargo_expand_raw(rust_crate_dir, extra_rustflags, dumper, false);
+            return run_cargo_expand_raw(rust_crate_dir, extra_rustflags, false);
         }
         // This will stop the whole generator and tell the users, so we do not care about testing it
         // frb-coverage:ignore-start
@@ -126,7 +123,7 @@ fn run_cargo_expand_raw(
     }
 
     let ans = stdout.lines().skip(1).join("\n");
-    dumper.dump_str(ConfigDumpContent::Source, "cargo_expand.rs", &ans)?;
+    // dumper.dump_str(ConfigDumpContent::Source, "cargo_expand.rs", &ans)?;
     Ok(ans)
 }
 
