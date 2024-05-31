7dc2295b6840082b993ba40e4be2514462692546
fzyzcjy <ch271828n@outlook.com>
2024-05-31 18:57:40 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/misc.rs
+++ frb_codegen/src/library/codegen/parser/misc.rs
@@ -11,17 +11,12 @@ pub(crate) fn parse_has_executor(source_rust_content: &str) -> bool {
 
 pub(crate) fn extract_src_types_in_paths<T: StructOrEnumWrapper<I>, I>(
     src_items: &HashMap<String, &T>,
-    rust_input_paths: &[PathBuf],
-    rust_crate_dir: &Path,
+    rust_input_namespaces: &[Namespace],
 ) -> anyhow::Result<Vec<NamespacedName>> {
-    let interest_input_paths = (rust_input_paths.iter())
-        .map(|p| Namespace::new_from_rust_crate_path(p, rust_crate_dir))
-        .collect::<anyhow::Result<Vec<_>>>()?;
-
     Ok((src_items.iter())
         .filter_map(|(k, v)| {
             let namespace = v.inner().namespace();
-            if interest_input_paths.contains(&namespace) {
+            if rust_input_namespaces.contains(&namespace) {
                 Some(NamespacedName::new(namespace, k.to_owned()))
             } else {
                 None
