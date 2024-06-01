d50108a95ce3264ee0249a69a1e3b7e9af753d8c
fzyzcjy <ch271828n@outlook.com>
2024-06-01 18:00:20 +0800

    chore: cp back

--- frb_codegen/src/library/codegen/parser/hir/raw/mod.rs
+++ frb_codegen/src/library/codegen/parser/hir/raw/mod.rs
@@ -3,12 +3,15 @@ use crate::codegen::ir::hir::raw::HirRawPack;
 use crate::codegen::ir::mir::namespace::Namespace;
 use crate::codegen::parser::hir::internal_config::ParserHirInternalConfig;
 use crate::library::commands::cargo_expand::run_cargo_expand;
+use crate::library::commands::cargo_metadata::execute_cargo_metadata;
 use itertools::concat;
 
 pub(crate) fn parse(
     config: &ParserHirInternalConfig,
     dumper: &Dumper,
 ) -> anyhow::Result<HirRawPack> {
+    let metadata = execute_cargo_metadata(&config.rust_crate_dir.join("Cargo.toml"))?;
+
     let crates = concat([
         vec![Namespace::SELF_CRATE.to_owned()],
         config.third_party_crates.clone(),
