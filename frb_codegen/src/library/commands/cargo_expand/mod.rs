a61633f046822d43f92eaf1a73b7031e3830e5b7
fzyzcjy <ch271828n@outlook.com>
2024-06-01 15:51:47 +0800

    feat: more

--- frb_codegen/src/library/commands/cargo_expand/mod.rs
+++ frb_codegen/src/library/commands/cargo_expand/mod.rs
@@ -16,29 +16,20 @@ use std::env;
 use std::path::Path;
 
 pub(crate) fn run_cargo_expand(rust_crate_dir: &Path, dumper: &Dumper) -> Result<String> {
-    let ans = if can_execute_real(rust_crate_dir) {
+    let ans = if can_execute_real(rust_crate_dir)? {
         real::run(rust_crate_dir)?
     } else {
-        // We do not care about this warning message
-        // frb-coverage:ignore-start
-        warn!(
-            "Skip cargo-expand on {rust_crate_dir:?}, \
-             because cargo is already running and would block cargo-expand. \
-             This might cause errors if your api contains macros or complex mods."
-        );
-        // frb-coverage:ignore-end
-        todo!("Usage in build.rs for new mod-based system is not implemented yet. Feel free to create an issue if you need this!")
-        // return Ok(fs::read_to_string(rust_file_path)?);
+        pseudo::run(rust_crate_dir)?
     };
 
     dumper.dump_str(ConfigDumpContent::Source, "cargo_expand.rs", &ans)?;
     Ok(ans)
 }
 
-fn can_execute_real(rust_crate_dir: &Path) -> bool {
+fn can_execute_real(rust_crate_dir: &Path) -> anyhow::Result<bool> {
     let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap_or_default();
     debug!("run_cargo_expand manifest_dir={manifest_dir} rust_crate_dir={rust_crate_dir:?}");
-    manifest_dir.is_empty()
+    Ok(manifest_dir.is_empty()
         || normalize_windows_unc_path(&path_to_string(rust_crate_dir)?)
-            != normalize_windows_unc_path(&manifest_dir)
+            != normalize_windows_unc_path(&manifest_dir))
 }
