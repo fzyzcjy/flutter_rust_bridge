82c1db752a3e6c8b3c73e2b2d350075a05d3ebee
fzyzcjy <ch271828n@outlook.com>
2024-06-01 09:49:40 +0800

    refactor: mv

--- frb_codegen/src/library/codegen/hir_parser/hierarchical/mod.rs
+++ frb_codegen/src/library/codegen/hir_parser/hierarchical/mod.rs
@@ -1,6 +1,14 @@
-mod crates;
+use crate::codegen::hir::hierarchical::crates::HirCrate;
+use crate::codegen::hir::hierarchical::module::HirVisibility;
+use crate::codegen::hir_parser::hierarchical::module::parse_module;
+
 pub(crate) mod item_type;
 pub(crate) mod mirror_ident;
 pub(crate) mod module;
 pub(crate) mod struct_or_enum;
 pub(crate) mod visibility;
+
+pub(crate) fn parse(file: syn::File) -> anyhow::Result<HirCrate> {
+    let root_modulee = parse_module(&file.items, HirVisibility::Public)?;
+    Ok(HirCrate { root_module })
+}
