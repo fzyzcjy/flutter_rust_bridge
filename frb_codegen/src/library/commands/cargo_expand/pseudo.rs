46a216943fc30cc079429395523deb2875218fb5
fzyzcjy <ch271828n@outlook.com>
2024-06-01 15:54:28 +0800

    chore: simp

--- frb_codegen/src/library/commands/cargo_expand/pseudo.rs
+++ frb_codegen/src/library/commands/cargo_expand/pseudo.rs
@@ -2,14 +2,11 @@ use log::warn;
 use std::path::Path;
 
 pub(super) fn run(rust_crate_dir: &Path) -> anyhow::Result<String> {
-    // We do not care about this warning message
-    // frb-coverage:ignore-start
     warn!(
         "Skip cargo-expand on {rust_crate_dir:?}, \
-             because cargo is already running and would block cargo-expand. \
-             This might cause errors if your api contains macros or complex mods."
+         because cargo is already running and would block cargo-expand. \
+         This might cause errors if your api contains macros or complex mods."
     );
-    // frb-coverage:ignore-end
-    todo!("Usage in build.rs for new mod-based system is not implemented yet. Feel free to create an issue if you need this!")
-    // return Ok(fs::read_to_string(rust_file_path)?);
+
+    TODO
 }
