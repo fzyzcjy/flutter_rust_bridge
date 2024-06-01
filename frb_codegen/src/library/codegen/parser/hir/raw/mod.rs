4a4ac401259af8017fe3781e8773c6fe04082e63
fzyzcjy <ch271828n@outlook.com>
2024-06-01 17:13:17 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/hir/raw/mod.rs
+++ frb_codegen/src/library/codegen/parser/hir/raw/mod.rs
@@ -1,11 +1,21 @@
 use crate::codegen::dumper::Dumper;
 use crate::codegen::ir::hir::raw::HirRawPack;
+use crate::codegen::ir::mir::namespace::Namespace;
 use crate::codegen::parser::hir::internal_config::ParserHirInternalConfig;
 use crate::library::commands::cargo_expand::run_cargo_expand;
+use itertools::concat;
 
 pub(crate) fn parse(
     config: &ParserHirInternalConfig,
     dumper: &Dumper,
 ) -> anyhow::Result<HirRawPack> {
-    run_cargo_expand(&config.rust_crate_dir, dumper)
+    Ok(concat([
+        vec![Namespace::SELF_CRATE],
+        config.third_party_crates.clone(),
+    ])
+    .iter()
+    .map(|crate_name| run_cargo_expand(&config.rust_crate_dir, dumper, TODO))
+    .collect::<anyhow::Result<Vec<_>>>()
+    .into_iter()
+    .collect())
 }
