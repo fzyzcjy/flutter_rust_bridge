29997d1869b08a00397f4acd4812c2df314736c6
fzyzcjy <ch271828n@outlook.com>
2024-06-01 15:56:15 +0800

    feat: more

--- frb_codegen/src/library/commands/cargo_expand/pseudo.rs
+++ frb_codegen/src/library/commands/cargo_expand/pseudo.rs
@@ -8,5 +8,9 @@ pub(super) fn run(rust_crate_dir: &Path) -> anyhow::Result<String> {
          This might cause errors if your api contains macros or complex mods."
     );
 
+    parse_file(&rust_crate_dir.join("src/lib.rs"))
+}
+
+fn parse_file(path: &Path) -> anyhow::Result<String> {
     TODO
 }
