3fb24127c45cbad98a32701205f61a55d7f1fa0f
fzyzcjy <ch271828n@outlook.com>
2024-06-01 15:59:37 +0800

    chore: more

--- frb_codegen/src/library/commands/cargo_expand/pseudo.rs
+++ frb_codegen/src/library/commands/cargo_expand/pseudo.rs
@@ -2,7 +2,7 @@ use log::warn;
 use std::fs;
 use std::path::Path;
 
-pub(super) fn run(rust_crate_dir: &Path) -> anyhow::Result<String> {
+pub(super) fn run(rust_crate_dir: &Path) -> anyhow::Result<syn::File> {
     warn!(
         "Skip cargo-expand on {rust_crate_dir:?}, \
          because cargo is already running and would block cargo-expand. \
@@ -12,7 +12,7 @@ pub(super) fn run(rust_crate_dir: &Path) -> anyhow::Result<String> {
     parse_file(&rust_crate_dir.join("src/lib.rs"))
 }
 
-fn parse_file(path: &Path) -> anyhow::Result<String> {
+fn parse_file(path: &Path) -> anyhow::Result<syn::File> {
     let code = fs::read_to_string(&path)?;
     let ast = syn::parse_file(&code)?;
     TODO
