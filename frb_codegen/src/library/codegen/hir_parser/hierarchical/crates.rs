31d6a79fee255c4e7207021825da9fe28678801e
fzyzcjy <ch271828n@outlook.com>
2024-06-01 09:49:11 +0800

    feat: more

--- frb_codegen/src/library/codegen/hir_parser/hierarchical/crates.rs
+++ frb_codegen/src/library/codegen/hir_parser/hierarchical/crates.rs
@@ -6,12 +6,7 @@ use crate::codegen::hir_parser::hierarchical::module::parse_module;
 use crate::codegen::parser::reader::CachedRustReader;
 use std::path::Path;
 
-pub(crate) fn parse_crate(
-    rust_crate_dir: &Path,
-    cached_rust_reader: &mut CachedRustReader,
-    dumper: &Dumper,
-) -> anyhow::Result<HirCrate> {
-    let file = cached_rust_reader.read_rust_crate(rust_crate_dir, dumper)?;
+pub(crate) fn parse_crate(rust_crate_dir: &Path, file: syn::File) -> anyhow::Result<HirCrate> {
     let root_modulee = parse_module(&file.items, HirVisibility::Public)?;
     Ok(HirCrate { root_module })
 }
