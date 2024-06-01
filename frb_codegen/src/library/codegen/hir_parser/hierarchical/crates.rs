5158976b1692b000a91349d44ddd1c363fcc1b1c
fzyzcjy <ch271828n@outlook.com>
2024-06-01 09:36:32 +0800

    feat: more

--- frb_codegen/src/library/codegen/hir_parser/hierarchical/crates.rs
+++ frb_codegen/src/library/codegen/hir_parser/hierarchical/crates.rs
@@ -1,5 +1,13 @@
+use crate::codegen::dumper::Dumper;
 use crate::codegen::hir::hierarchical::crates::HirCrate;
+use crate::codegen::parser::reader::CachedRustReader;
+use std::path::Path;
 
-pub(crate) fn parse_crate() -> anyhow::Result<HirCrate> {
+pub(crate) fn parse_crate(
+    rust_crate_dir: &Path,
+    cached_rust_reader: &mut CachedRustReader,
+    dumper: &Dumper,
+) -> anyhow::Result<HirCrate> {
+    let crate_ast = cached_rust_reader.read_rust_crate(rust_crate_dir, dumper)?;
     todo!()
 }
