909a459936699f485dd72601214fd09ac5bff381
fzyzcjy <ch271828n@outlook.com>
2024-06-01 09:41:59 +0800

    feat: more

--- frb_codegen/src/library/codegen/hir_parser/hierarchical/crates.rs
+++ frb_codegen/src/library/codegen/hir_parser/hierarchical/crates.rs
@@ -1,5 +1,7 @@
 use crate::codegen::dumper::Dumper;
 use crate::codegen::hir::hierarchical::crates::HirCrate;
+use crate::codegen::hir::hierarchical::module::HirModule;
+use crate::codegen::hir_parser::hierarchical::items::parse_syn_items;
 use crate::codegen::hir_parser::hierarchical::module::parse_module;
 use crate::codegen::parser::reader::CachedRustReader;
 use std::path::Path;
@@ -9,7 +11,7 @@ pub(crate) fn parse_crate(
     cached_rust_reader: &mut CachedRustReader,
     dumper: &Dumper,
 ) -> anyhow::Result<HirCrate> {
-    let crate_ast = cached_rust_reader.read_rust_crate(rust_crate_dir, dumper)?;
-    let root_modulee = parse_module(TODO)?;
+    let file = cached_rust_reader.read_rust_crate(rust_crate_dir, dumper)?;
+    let root_modulee = parse_syn_items(&file.items)?;
     Ok(HirCrate { root_module })
 }
---
+++ frb_codegen/src/library/codegen/hir_parser/hierarchical/items.rs
@@ -0,0 +1,5 @@
+use crate::codegen::hir::hierarchical::module::HirModule;
+
+pub(crate) fn parse_syn_items(items: &[syn::Item]) -> anyhow::Result<HirModule> {
+    todo!()
+}
