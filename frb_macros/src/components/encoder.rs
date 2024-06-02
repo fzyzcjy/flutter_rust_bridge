dd1990ae172d5de6e6cdd205c3e8a9ad2ebf25f9
fzyzcjy <ch271828n@outlook.com>
2024-06-02 21:41:58 +0800

    feat: better delimeter

--- frb_macros/src/components/encoder.rs
+++ frb_macros/src/components/encoder.rs
@@ -2,7 +2,7 @@ use proc_macro::TokenStream;
 use quote::quote;
 
 pub(crate) fn create_frb_encoded_comment(data: String) -> TokenStream {
-    let encoded = format!(r#"frb_encoded={}"#, hex::encode(data));
+    let encoded = format!(r#"frb_encoded({})"#, hex::encode(data));
     quote! {
         #[cfg_attr(frb_expand, doc=#encoded)]
     }.into()
