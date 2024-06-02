81e9b9772f636d36c69f5e2f107b5ba2d5d79e57
fzyzcjy <ch271828n@outlook.com>
2024-06-02 16:34:47 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/hir/hierarchical/third_party_override_transformer.rs
+++ frb_codegen/src/library/codegen/parser/hir/hierarchical/third_party_override_transformer.rs
@@ -4,12 +4,13 @@ use crate::codegen::misc::THIRD_PARTY_DIR_NAME;
 use crate::utils::crate_name::CrateName;
 
 pub(super) fn transform(mut pack: HirPack) -> anyhow::Result<HirPack> {
-    let module_third_party_root = remove_module_third_party_root(&mut pack);
-    TODO;
+    if let Some(module_third_party_root) = remove_module_third_party_root(&mut pack) {
+        TODO;
+    }
     Ok(pack)
 }
 
-fn remove_module_third_party_root(pack: &mut HirPack) -> HirModule {
+fn remove_module_third_party_root(pack: &mut HirPack) -> Option<HirModule> {
     let hir_crate = pack.crates.get_mut(&CrateName::self_crate()).unwrap();
-    hir_crate.root_module.content.get_module_nested(&[THIRD_PARTY_DIR_NAME])
+    hir_crate.root_module.content.remove_module_by_name(THIRD_PARTY_DIR_NAME)
 }
