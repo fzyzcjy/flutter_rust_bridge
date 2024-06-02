7e3453bdd261f663bb9528b8f9e6b062c6740531
fzyzcjy <ch271828n@outlook.com>
2024-06-02 16:43:17 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/hir/hierarchical/third_party_override_transformer.rs
+++ frb_codegen/src/library/codegen/parser/hir/hierarchical/third_party_override_transformer.rs
@@ -5,12 +5,28 @@ use crate::utils::crate_name::CrateName;
 
 pub(super) fn transform(mut pack: HirPack) -> anyhow::Result<HirPack> {
     if let Some(module_third_party_root) = remove_module_third_party_root(&mut pack) {
-        TODO;
+        for module_third_party_crate in module_third_party_root.content.modules {
+            transform_crate(&mut pack, module_third_party_crate)?;
+        }
     }
     Ok(pack)
 }
 
 fn remove_module_third_party_root(pack: &mut HirPack) -> Option<HirModule> {
     let hir_crate = pack.crates.get_mut(&CrateName::self_crate()).unwrap();
-    hir_crate.root_module.content.remove_module_by_name(THIRD_PARTY_DIR_NAME)
+    hir_crate
+        .root_module
+        .content
+        .remove_module_by_name(THIRD_PARTY_DIR_NAME)
+}
+
+fn transform_crate(pack: &mut HirPack, src: HirModule) -> anyhow::Result<()> {
+    if let Some(target_crate) = pack.crates.get_mut() {
+        TODO;
+    } else {
+        log::warn!(
+            "Skip `{}` since there is no corresponding scanned third party crate.",
+            src.meta.namespace,
+        );
+    }
 }
