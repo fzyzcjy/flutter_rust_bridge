5402f4b1ea8361cd16c91e7ecd49590623c2796d
fzyzcjy <ch271828n@outlook.com>
2024-06-02 13:33:28 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/hir/hierarchical/pub_use.rs
+++ frb_codegen/src/library/codegen/parser/hir/hierarchical/pub_use.rs
@@ -1,4 +1,5 @@
 use crate::codegen::ir::hir::hierarchical::module::HirModule;
+use itertools::Itertools;
 
 pub(crate) fn transform_module_by_pub_use(
     raw: HirModule,
@@ -7,6 +8,20 @@ pub(crate) fn transform_module_by_pub_use(
     TODO
 }
 
-fn parse_pub_use(items: &[syn::Item]) -> Vec<TODO> {
-    TODO
+fn parse_pub_use_from_items(items: &[syn::Item]) -> Vec<String> {
+    items
+        .iter()
+        .filter_map(parse_pub_use_from_item)
+        .collect_vec()
+}
+
+fn parse_pub_use_from_item(item: &syn::Item) -> Option<String> {
+    if let syn::Item::Use(item_use) = item {
+        if matches!(item_use.vis, syn::Visibility::Public(_)) {
+            let tree = item_use.tree;
+            let tree_string = quote::quote!(#tree).to_string();
+            TODO
+        }
+    }
+    None
 }
