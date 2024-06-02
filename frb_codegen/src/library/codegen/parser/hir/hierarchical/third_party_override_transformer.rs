734be92e32dcd49ac13038dcfe90a5de4bb36de1
fzyzcjy <ch271828n@outlook.com>
2024-06-02 16:46:28 +0800

    fix: err

--- frb_codegen/src/library/codegen/parser/hir/hierarchical/third_party_override_transformer.rs
+++ frb_codegen/src/library/codegen/parser/hir/hierarchical/third_party_override_transformer.rs
@@ -7,8 +7,9 @@ use crate::utils::crate_name::CrateName;
 pub(super) fn transform(mut pack: HirPack) -> anyhow::Result<HirPack> {
     if let Some(module_third_party_root) = remove_module_third_party_root(&mut pack) {
         for module_third_party_crate in module_third_party_root.content.modules {
-            let crate_name = (module_third_party_crate.meta.namespace.path().last()).unwrap();
-            if let Some(target_crate) = pack.crates.get_mut(crate_name) {
+            let crate_name =
+                CrateName::new((module_third_party_crate.meta.namespace.path().last()).unwrap());
+            if let Some(target_crate) = pack.crates.get_mut(&crate_name) {
                 transform_crate(target_crate, module_third_party_crate)
             } else {
                 log::warn!(
