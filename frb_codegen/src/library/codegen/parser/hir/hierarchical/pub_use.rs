d9f300c97198b435d2545b2dbf28197a0585d292
fzyzcjy <ch271828n@outlook.com>
2024-06-02 14:04:14 +0800

    fix: err

--- frb_codegen/src/library/codegen/parser/hir/hierarchical/pub_use.rs
+++ frb_codegen/src/library/codegen/parser/hir/hierarchical/pub_use.rs
@@ -24,7 +24,7 @@ fn parse_pub_use_from_item(item: &syn::Item) -> Option<Namespace> {
     if let syn::Item::Use(item_use) = item {
         if matches!(item_use.vis, syn::Visibility::Public(_)) {
             let tree = &item_use.tree;
-            let tree_string = quote::quote!(#tree).to_string();
+            let tree_string = quote::quote!(#tree).to_string().replace(' ', "");
             if let Some(interest_use_part) = tree_string.strip_suffix("::*") {
                 return Some(Namespace::new_raw(interest_use_part.to_owned()));
             }
