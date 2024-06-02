46467a0b8c85a0c37f20bd96c4fc214a40f0fa5a
fzyzcjy <ch271828n@outlook.com>
2024-06-02 21:33:55 +0800

    feat: change

--- frb_macros/src/components/encoder.rs
+++ frb_macros/src/components/encoder.rs
@@ -2,7 +2,7 @@ use proc_macro::TokenStream;
 
 // TODO rename
 pub(crate) fn format_frb_attribute(item: String) -> TokenStream {
-    format!("#[cfg_attr(frb_expand, doc = r###\"frb_marker: {item}\"###)]",)
+    format!(r###"#[cfg_attr(frb_expand, doc="frb_marker={}"]"###)
         .parse()
         .unwrap()
 }
