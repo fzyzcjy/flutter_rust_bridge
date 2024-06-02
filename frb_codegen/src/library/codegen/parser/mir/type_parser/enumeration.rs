3bb400aa91666a0d6345bba26f57838db81a1e05
fzyzcjy <ch271828n@outlook.com>
2024-06-02 20:23:13 +0800

    chore: fmt

--- frb_codegen/src/library/codegen/parser/mir/type_parser/enumeration.rs
+++ frb_codegen/src/library/codegen/parser/mir/type_parser/enumeration.rs
@@ -51,7 +51,11 @@ impl<'a, 'b, 'c> TypeParserWithContext<'a, 'b, 'c> {
 
         let mode = compute_enum_mode(&raw_variants);
         let variants = maybe_field_wrap_box(raw_variants, mode);
-        let ignore = parse_type_should_ignore_simple(&attributes, src_enum.visibility, &name.namespace.crate_name());
+        let ignore = parse_type_should_ignore_simple(
+            &attributes,
+            src_enum.visibility,
+            &name.namespace.crate_name(),
+        );
 
         Ok(MirEnum {
             name,
