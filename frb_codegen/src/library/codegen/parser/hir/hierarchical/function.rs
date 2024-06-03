25c701f062f513f999f545341b0c503378ce9b50
fzyzcjy <ch271828n@outlook.com>
2024-06-03 18:03:25 +0800

    chore: more

--- frb_codegen/src/library/codegen/parser/hir/hierarchical/function.rs
+++ frb_codegen/src/library/codegen/parser/hir/hierarchical/function.rs
@@ -15,20 +15,24 @@ pub(crate) fn parse_syn_item_fn(item_fn: &ItemFn, namespace: &Namespace) -> HirF
 
 pub(crate) fn parse_syn_item_impl(item_impl: &ItemImpl, namespace: &Namespace) -> Vec<HirFunction> {
     (item_impl.items.iter())
-        .filter_map(|item| if_then_some!(let ImplItem::Fn(ref impl_item_fn) = item, impl_item_fn))
+        .filter_map(|item| parse_syn_impl_item(impl_item))
         .map(|impl_item_fn| parse_syn_impl_item_fn(impl_item_fn, namespace))
         .collect_vec()
 }
 
-pub(crate) fn parse_syn_impl_item_fn(
-    impl_item_fn: &ImplItemFn,
+pub(crate) fn parse_syn_impl_item(
+    impl_item: ImplItem,
     namespace: &Namespace,
-) -> HirFunction {
-    HirFunction {
-        namespace: namespace.clone(),
-        inner: HirFunctionInner::Method {
-            item_impl: item_impl.clone(),
-            impl_item_fn: impl_item_fn.clone(),
-        },
+) -> Option<HirFunction> {
+    if let ImplItem::Fn(impl_item_fn) = impl_item {
+        Some(HirFunction {
+            namespace: namespace.clone(),
+            inner: HirFunctionInner::Method {
+                item_impl: item_impl.clone(),
+                impl_item_fn: impl_item_fn.clone(),
+            },
+        })
+    } else {
+        None
     }
 }
