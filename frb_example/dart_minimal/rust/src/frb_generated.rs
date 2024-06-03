bac7988d48309f19b63a5ba668c6235412acd16b
fzyzcjy <ch271828n@outlook.com>
2024-06-03 21:08:48 +0800

    chore: codegen

--- frb_example/dart_minimal/rust/src/frb_generated.rs
+++ frb_example/dart_minimal/rust/src/frb_generated.rs
@@ -21,6 +21,7 @@
 
 // Section: imports
 
+use crate::api::minimal::SimpleTraitTwinNormal;
 use flutter_rust_bridge::for_generated::byteorder::{NativeEndian, ReadBytesExt, WriteBytesExt};
 use flutter_rust_bridge::for_generated::transform_result_dco;
 use flutter_rust_bridge::{Handler, IntoIntoDart};
