922130aa51080422366350c579fe369412c1cd57
fzyzcjy <ch271828n@outlook.com>
2024-06-03 18:50:02 +0800

    chore: rm demo

--- frb_example/dart_minimal/rust/src/api/minimal.rs
+++ frb_example/dart_minimal/rust/src/api/minimal.rs
@@ -8,22 +8,3 @@ pub fn init_app() {
 pub fn minimal_adder(a: i32, b: i32) -> i32 {
     a + b
 }
-
-
-pub struct StructWithTraitTwinNormal {
-    pub value: u32,
-}
-
-pub trait SimpleTraitTwinNormal {
-    fn simple_trait_fn_twin_normal() -> Self;
-
-    fn simple_trait_fn_with_default_impl_twin_normal() -> i32 {
-        42
-    }
-}
-
-impl SimpleTraitTwinNormal for StructWithTraitTwinNormal {
-    fn simple_trait_fn_twin_normal() -> Self {
-        StructWithTraitTwinNormal { value: 42 }
-    }
-}
