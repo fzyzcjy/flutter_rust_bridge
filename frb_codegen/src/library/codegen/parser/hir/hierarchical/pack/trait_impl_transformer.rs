c260f281ddd981b9062aab9e38a7e9da0ee26bff
fzyzcjy <ch271828n@outlook.com>
2024-06-03 17:49:22 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/hir/hierarchical/pack/trait_impl_transformer.rs
+++ frb_codegen/src/library/codegen/parser/hir/hierarchical/pack/trait_impl_transformer.rs
@@ -1,6 +1,13 @@
+use crate::codegen::ir::hir::hierarchical::module::HirModule;
 use crate::codegen::ir::hir::hierarchical::pack::HirPack;
 
 pub(super) fn transform(mut pack: HirPack) -> anyhow::Result<HirPack> {
-    TODO;
+    for hir_crate in pack.crates.iter_mut() {
+        transform_module(&mut hir_crate.root_module)?;
+    }
     Ok(pack)
+}
+
+fn transform_module(target: &mut HirModule) -> anyhow::Result<()> {
+    TODO;
 }
