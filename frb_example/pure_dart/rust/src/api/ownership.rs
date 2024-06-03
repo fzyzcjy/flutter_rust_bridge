121e852fdfdb827500b8fb6e95c8ade5c628198d
fzyzcjy <ch271828n@outlook.com>
2024-06-03 13:46:48 +0800

    feat: more

--- frb_example/pure_dart/rust/src/api/ownership.rs
+++ frb_example/pure_dart/rust/src/api/ownership.rs
@@ -10,6 +10,14 @@ pub fn borrow_i32_twin_normal(arg: &i32) -> i32 {
     *arg
 }
 
+pub fn borrow_slice_u8_twin_normal(arg: &[u8]) -> Vec<u8> {
+    arg.to_owned()
+}
+
+pub fn borrow_slice_string_twin_normal(arg: &[String]) -> Vec<String> {
+    arg.to_owned()
+}
+
 #[derive(Clone)]
 pub struct SimpleStructForBorrowTwinNormal {
     pub one: String,
