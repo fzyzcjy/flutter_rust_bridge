b1bb1215a01e0ada2fe5a5a157d136e5afa0f3db
fzyzcjy <ch271828n@outlook.com>
2024-06-03 09:07:17 +0800

    chore: rm demo

--- frb_example/dart_minimal/rust/src/api/minimal.rs
+++ frb_example/dart_minimal/rust/src/api/minimal.rs
@@ -8,12 +8,3 @@ pub fn init_app() {
 pub fn minimal_adder(a: i32, b: i32) -> i32 {
     a + b
 }
-
-// TODO temp
-pub struct MyExternalType {
-    this_is_fake_external: i32,
-}
-#[frb(external)]
-impl MyExternalType {
-    pub fn f(&self, a: i32) {}
-}
