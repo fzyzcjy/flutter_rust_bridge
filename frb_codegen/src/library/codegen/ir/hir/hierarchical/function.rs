ebf5c7c7f26edd05ee0d57c87b9e96182f5dbcd6
fzyzcjy <ch271828n@outlook.com>
2024-06-03 20:53:20 +0800

    Revert "feat: field"
    This reverts commit 71ceb9a919f454201ee21b8ecdcf678b617d5b0a.

--- frb_codegen/src/library/codegen/ir/hir/hierarchical/function.rs
+++ frb_codegen/src/library/codegen/ir/hir/hierarchical/function.rs
@@ -1,4 +1,4 @@
-use crate::utils::namespace::{Namespace, NamespacedName};
+use crate::utils::namespace::Namespace;
 use proc_macro2::Span;
 use serde::Serialize;
 use syn::spanned::Spanned;
@@ -38,20 +38,15 @@ impl HirFunction {
 #[derive(Debug, Clone)]
 pub(crate) enum HirFunctionOwner {
     Function,
-    Method {
-        item_impl: ItemImpl,
-    },
-    TraitMethod {
-        item_impl: ItemImpl,
-        trait_def_name: NamespacedName,
-    },
+    Method { item_impl: ItemImpl },
+    TraitMethod { item_impl: ItemImpl },
 }
 
 impl HirFunctionOwner {
     pub(crate) fn simple_name(&self) -> Option<String> {
         match self {
             Self::Function => None,
-            Self::Method { item_impl } | Self::TraitMethod { item_impl, .. } => {
+            Self::Method { item_impl } | Self::TraitMethod { item_impl } => {
                 Some(ty_to_string(&item_impl.self_ty))
             }
         }
