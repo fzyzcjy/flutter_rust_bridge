2a7578dc1b747a5b5768095c06063cfd9ab2e4dd
fzyzcjy <ch271828n@outlook.com>
2024-06-03 17:52:57 +0800

    chore: lint

--- frb_codegen/src/library/codegen/parser/hir/hierarchical/pack/trait_impl_transformer.rs
+++ frb_codegen/src/library/codegen/parser/hir/hierarchical/pack/trait_impl_transformer.rs
@@ -1,4 +1,3 @@
-use crate::codegen::ir::hir::hierarchical::module::HirModule;
 use crate::codegen::ir::hir::hierarchical::pack::HirPack;
 
 pub(super) fn transform(mut pack: HirPack) -> anyhow::Result<HirPack> {
