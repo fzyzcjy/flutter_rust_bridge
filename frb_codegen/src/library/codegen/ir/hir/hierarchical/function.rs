dc0acc144c50644fd2d040b2ed21bcef6a920bac
fzyzcjy <ch271828n@outlook.com>
2024-06-03 18:27:35 +0800

    feaT: more

--- frb_codegen/src/library/codegen/ir/hir/hierarchical/function.rs
+++ frb_codegen/src/library/codegen/ir/hir/hierarchical/function.rs
@@ -13,13 +13,6 @@ pub(crate) struct HirFunction {
     pub(crate) item_fn: GeneralItemFn,
 }
 
-#[derive(Debug, Clone)]
-pub(crate) enum GeneralItemFn {
-    ItemFn(ItemFn),
-    ImplItemFn(ImplItemFn),
-    TraitItemFn(TraitItemFn),
-}
-
 impl HirFunction {
     pub(crate) fn with_namespace(&self, namespace: Namespace) -> Self {
         Self {
@@ -27,72 +20,66 @@ impl HirFunction {
             ..self.clone()
         }
     }
-}
-
-impl HirFunctionInner {
-    pub(crate) fn sig(&self) -> &Signature {
-        match self {
-            HirFunctionInner::Function { item_fn } => &item_fn.sig,
-            HirFunctionInner::Method {
-                item_fn: impl_item_fn,
-                ..
-            } => &impl_item_fn.sig,
-        }
-    }
 
     pub(crate) fn simple_owner(&self) -> Option<String> {
-        Some(match &self {
-            HirFunctionInner::Method { item_impl, .. }
-            | HirFunctionInner::TraitMethod { item_impl, .. } => ty_to_string(&item_impl.self_ty),
-            _ => return None,
-        })
+        self.item_impl
+            .map(|item_impl| ty_to_string(&item_impl.self_ty))
+    }
+
+    pub(crate) fn owner_and_name(&self) -> SimpleOwnerAndName {
+        (self.simple_owner(), self.item_fn.name())
+    }
+}
+
+#[derive(Debug, Clone)]
+pub(crate) enum GeneralItemFn {
+    ItemFn(ItemFn),
+    ImplItemFn(ImplItemFn),
+    TraitItemFn(TraitItemFn),
+}
+
+impl GeneralItemFn {
+    pub(crate) fn sig(&self) -> &Signature {
+        match self {
+            Self::ItemFn(inner) => &inner.sig,
+            Self::ImplItemFn(inner) => &inner.sig,
+            Self::TraitItemFn(inner) => &inner.sig,
+        }
     }
 
     pub(crate) fn name(&self) -> String {
         self.sig().ident.to_string()
     }
 
-    pub(crate) fn owner_and_name(&self) -> SimpleOwnerAndName {
-        (self.simple_owner(), self.name())
-    }
-
     pub(crate) fn attrs(&self) -> &Vec<Attribute> {
         match self {
-            HirFunctionInner::Function { item_fn } => &item_fn.attrs,
-            HirFunctionInner::Method {
-                item_fn: impl_item_fn,
-                ..
-            } => &impl_item_fn.attrs,
+            Self::ItemFn(inner) => &inner.attrs,
+            Self::ImplItemFn(inner) => &inner.attrs,
+            Self::TraitItemFn(inner) => &inner.attrs,
         }
     }
 
     pub(crate) fn attrs_mut(&mut self) -> &mut Vec<Attribute> {
         match self {
-            HirFunctionInner::Function { item_fn } => &mut item_fn.attrs,
-            HirFunctionInner::Method {
-                item_fn: impl_item_fn,
-                ..
-            } => &mut impl_item_fn.attrs,
+            Self::ItemFn(inner) => &mut inner.attrs,
+            Self::ImplItemFn(inner) => &mut inner.attrs,
+            Self::TraitItemFn(inner) => &mut inner.attrs,
         }
     }
 
     pub(crate) fn span(&self) -> Span {
         match self {
-            HirFunctionInner::Function { item_fn } => item_fn.span(),
-            HirFunctionInner::Method {
-                item_fn: impl_item_fn,
-                ..
-            } => impl_item_fn.span(),
+            Self::ItemFn(inner) => &inner.span(),
+            Self::ImplItemFn(inner) => &inner.span(),
+            Self::TraitItemFn(inner) => &inner.span(),
         }
     }
 
     pub(crate) fn vis(&self) -> &Visibility {
         match self {
-            HirFunctionInner::Function { item_fn } => &item_fn.vis,
-            HirFunctionInner::Method {
-                item_fn: impl_item_fn,
-                ..
-            } => &impl_item_fn.vis,
+            Self::ItemFn(inner) => &inner.vis,
+            Self::ImplItemFn(inner) => &inner.vis,
+            Self::TraitItemFn(inner) => &inner.vis,
         }
     }
 }
