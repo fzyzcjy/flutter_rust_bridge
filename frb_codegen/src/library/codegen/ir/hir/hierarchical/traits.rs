0f8a81b9a0d2b2db0efca7e5a19ff70e32e13662
fzyzcjy <ch271828n@outlook.com>
2024-06-03 17:40:57 +0800

    chore: lint

--- frb_codegen/src/library/codegen/ir/hir/hierarchical/traits.rs
+++ frb_codegen/src/library/codegen/ir/hir/hierarchical/traits.rs
@@ -1,6 +1,5 @@
 use serde::Serialize;
 use syn::ItemTrait;
-use crate::codegen::ir::hir::hierarchical::function::HirFunctionInner;
 use crate::utils::namespace::Namespace;
 
 #[derive(Debug, Clone, Serialize)]
