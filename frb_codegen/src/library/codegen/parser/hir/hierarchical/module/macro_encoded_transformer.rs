74a7f40cc2d4e343035e1ed5208b2c368ebcc692
fzyzcjy <ch271828n@outlook.com>
2024-06-03 08:05:24 +0800

    chore: rm

--- frb_codegen/src/library/codegen/parser/hir/hierarchical/module/macro_encoded_transformer.rs
+++
@@ -1,9 +0,0 @@
-use crate::codegen::ir::hir::hierarchical::module::HirModule;
-
-pub(super) fn transform(items: &[syn::Item]) -> anyhow::Result<Vec<syn::Item>> {
-    let items = items.to_owned();
-    for item in items.iter_mut() {
-        TODO;
-    }
-    Ok(items)
-}
