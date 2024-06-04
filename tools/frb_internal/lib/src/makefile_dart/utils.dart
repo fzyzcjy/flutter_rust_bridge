8d5e7a762203d0be627efcadb1b48965125055ac
fzyzcjy <ch271828n@outlook.com>
2024-06-04 14:18:43 +0800

    feat: more

--- tools/frb_internal/lib/src/makefile_dart/utils.dart
+++ tools/frb_internal/lib/src/makefile_dart/utils.dart
@@ -1,3 +1,9 @@
+import 'consts.dart';
+
 Future<void> installSystemDependencies() async {
-  TODO;
+  if (TODO) {
+    await exec('sudo apt-get update');
+    // https://github.com/RustAudio/cpal/blob/0246442da2f401895d2c82bbf941e3ebf6e93a04/.github/workflows/cpal.yml#L48C34-L48C47
+    await exec('sudo apt-get install libasound2-dev');
+  }
 }
