d627122657b542957363640b6c761eacd336adc2
fzyzcjy <ch271828n@outlook.com>
2024-06-04 17:12:30 +0800

    feat: ignore more

--- frb_example/integrate_third_party/rust/src/third_party/web_audio_api/node/mod.rs
+++ frb_example/integrate_third_party/rust/src/third_party/web_audio_api/node/mod.rs
@@ -1,8 +1,5 @@
 use flutter_rust_bridge::frb;
-use web_audio_api::node::{
-    AnalyserNode, AudioBufferSourceNode, BiquadFilterNode, ConvolverNode, IIRFilterNode,
-    WaveShaperNode,
-};
+use web_audio_api::node::*;
 
 #[frb(external)]
 impl AnalyserNode {
@@ -17,34 +14,142 @@ impl AnalyserNode {
 
     #[frb(ignore)]
     pub fn get_byte_frequency_data() {}
-}
 
-#[frb(external)]
-impl BiquadFilterNode {
     #[frb(ignore)]
-    pub fn get_frequency_response() {}
-}
-
-#[frb(external)]
-impl IIRFilterNode {
-    #[frb(ignore)]
-    pub fn get_frequency_response() {}
-}
-
-#[frb(external)]
-impl WaveShaperNode {
-    #[frb(ignore)]
-    pub fn curve() {}
-}
-
-#[frb(external)]
-impl ConvolverNode {
-    #[frb(ignore)]
-    pub fn buffer() {}
+    pub fn set_onprocessorerror() {}
 }
 
 #[frb(external)]
 impl AudioBufferSourceNode {
     #[frb(ignore)]
     pub fn buffer() {}
+
+    #[frb(ignore)]
+    pub fn set_onprocessorerror() {}
+}
+
+#[frb(external)]
+impl AudioDestinationNode {
+    #[frb(ignore)]
+    pub fn set_onprocessorerror() {}
+}
+
+#[frb(external)]
+impl BiquadFilterNode {
+    #[frb(ignore)]
+    pub fn get_frequency_response() {}
+
+    #[frb(ignore)]
+    pub fn set_onprocessorerror() {}
+}
+
+#[frb(external)]
+impl ChannelMergerNode {
+    #[frb(ignore)]
+    pub fn set_onprocessorerror() {}
+}
+
+#[frb(external)]
+impl ChannelSplitterNode {
+    #[frb(ignore)]
+    pub fn set_onprocessorerror() {}
+}
+
+#[frb(external)]
+impl ConstantSourceNode {
+    #[frb(ignore)]
+    pub fn set_onprocessorerror() {}
+}
+
+#[frb(external)]
+impl ConvolverNode {
+    #[frb(ignore)]
+    pub fn buffer() {}
+
+    #[frb(ignore)]
+    pub fn set_onprocessorerror() {}
+}
+
+#[frb(external)]
+impl DelayNode {
+    #[frb(ignore)]
+    pub fn set_onprocessorerror() {}
+}
+
+#[frb(external)]
+impl DynamicsCompressorNode {
+    #[frb(ignore)]
+    pub fn set_onprocessorerror() {}
+}
+
+#[frb(external)]
+impl GainNode {
+    #[frb(ignore)]
+    pub fn set_onprocessorerror() {}
+}
+
+#[frb(external)]
+impl IIRFilterNode {
+    #[frb(ignore)]
+    pub fn get_frequency_response() {}
+
+    #[frb(ignore)]
+    pub fn set_onprocessorerror() {}
+}
+
+#[frb(external)]
+impl MediaElementAudioSourceNode {
+    #[frb(ignore)]
+    pub fn set_onprocessorerror() {}
+}
+
+#[frb(external)]
+impl MediaStreamAudioDestinationNode {
+    #[frb(ignore)]
+    pub fn set_onprocessorerror() {}
+}
+
+#[frb(external)]
+impl MediaStreamAudioSourceNode {
+    #[frb(ignore)]
+    pub fn set_onprocessorerror() {}
+}
+
+#[frb(external)]
+impl MediaStreamTrackAudioSourceNode {
+    #[frb(ignore)]
+    pub fn set_onprocessorerror() {}
+}
+
+#[frb(external)]
+impl OscillatorNode {
+    #[frb(ignore)]
+    pub fn set_onprocessorerror() {}
+}
+
+#[frb(external)]
+impl PannerNode {
+    #[frb(ignore)]
+    pub fn set_onprocessorerror() {}
+}
+
+#[frb(external)]
+impl ScriptProcessorNode {
+    #[frb(ignore)]
+    pub fn set_onprocessorerror() {}
+}
+
+#[frb(external)]
+impl StereoPannerNode {
+    #[frb(ignore)]
+    pub fn set_onprocessorerror() {}
+}
+
+#[frb(external)]
+impl WaveShaperNode {
+    #[frb(ignore)]
+    pub fn curve() {}
+
+    #[frb(ignore)]
+    pub fn set_onprocessorerror() {}
 }
