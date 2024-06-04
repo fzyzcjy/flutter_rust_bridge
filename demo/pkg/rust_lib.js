967cd05bbcaa654a44875fca35fe8a099874f676
fzyzcjy <fzyzcjy@users.noreply.github.com>
2024-06-04 12:06:47 +0000

    deploy: aded016023a9fabec8c109b5b8c95c27a8bf7de0

--- demo/pkg/rust_lib.js
+++ demo/pkg/rust_lib.js
@@ -224,7 +224,7 @@ function makeMutClosure(arg0, arg1, dtor, f) {
     return real;
 }
 function __wbg_adapter_34(arg0, arg1, arg2) {
-    wasm._dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h51c166532fec7112(arg0, arg1, addHeapObject(arg2));
+    wasm._dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h1bbea5d8bdc1e0d6(arg0, arg1, addHeapObject(arg2));
 }
 
 function __wbg_adapter_39(arg0, arg1, arg2) {
