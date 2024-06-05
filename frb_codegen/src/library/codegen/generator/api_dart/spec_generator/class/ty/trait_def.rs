cbc2658d06623d09bc0b65d10faa289731e359a3
fzyzcjy <ch271828n@outlook.com>
2024-06-05 12:57:41 +0800

    feat: more

--- frb_codegen/src/library/codegen/generator/api_dart/spec_generator/class/ty/trait_def.rs
+++ frb_codegen/src/library/codegen/generator/api_dart/spec_generator/class/ty/trait_def.rs
@@ -1,9 +1,15 @@
+use crate::codegen::generator::api_dart::spec_generator::class::method::{
+    generate_api_methods, GenerateApiMethodMode,
+};
 use crate::codegen::generator::api_dart::spec_generator::class::ty::ApiDartGeneratorClassTrait;
 use crate::codegen::generator::api_dart::spec_generator::class::ApiDartGeneratedClass;
 use crate::library::codegen::generator::api_dart::spec_generator::base::*;
+use crate::utils::namespace::NamespacedName;
 
 impl<'a> ApiDartGeneratorClassTrait for TraitDefApiDartGenerator<'a> {
     fn generate_class(&self) -> Option<ApiDartGeneratedClass> {
+        let methods = generate_api_methods(&self.mir.name, self.context, TODO).join("\n");
+
         Some(ApiDartGeneratedClass {
             namespace: self.mir.name.namespace.clone(),
             class_name: self.mir.name.name.clone(),
