deb1e72ae74a1046a695669aa04dcece6298b832
fzyzcjy <ch271828n@outlook.com>
2024-06-05 18:20:34 +0800

    feat: more

--- frb_codegen/src/library/utils/syn_utils.rs
+++ frb_codegen/src/library/utils/syn_utils.rs
@@ -1,8 +1,11 @@
+use quote::ToTokens;
+
 pub(crate) fn canonicalize_rust_type(raw: &str) -> anyhow::Result<String> {
     let ast: syn::Type = syn::parse_str(raw)?;
     Ok(quote::quote!(#ast).to_string())
 }
 
-pub(crate) fn ty_to_string(ty: &syn::Type) -> String {
+// TODO rename
+pub(crate) fn ty_to_string<T: ToTokens>(ty: &T) -> String {
     quote::quote!(#ty).to_string()
 }
