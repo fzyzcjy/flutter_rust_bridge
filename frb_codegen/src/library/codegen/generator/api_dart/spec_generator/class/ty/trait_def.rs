e87e23431d2039247facf64e698c11e8636346d6
fzyzcjy <ch271828n@outlook.com>
2024-06-05 12:56:33 +0800

    feat: more

--- frb_codegen/src/library/codegen/generator/api_dart/spec_generator/class/ty/trait_def.rs
+++ frb_codegen/src/library/codegen/generator/api_dart/spec_generator/class/ty/trait_def.rs
@@ -4,6 +4,18 @@ use crate::library::codegen::generator::api_dart::spec_generator::base::*;
 
 impl<'a> ApiDartGeneratorClassTrait for TraitDefApiDartGenerator<'a> {
     fn generate_class(&self) -> Option<ApiDartGeneratedClass> {
-        todo!()
+        Some(ApiDartGeneratedClass {
+            namespace: self.mir.name.namespace.clone(),
+            class_name: self.mir.name.name.clone(),
+            code: format!(
+                "
+                abstract class {dart_api_type} {{
+                    {methods}
+                }}
+                "
+            ),
+            needs_freezed: false,
+            header: Default::default(),
+        })
     }
 }
