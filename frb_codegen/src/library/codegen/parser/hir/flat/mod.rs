cb9d5a6a19b3b6b632a64a373a350f033db60b9e
fzyzcjy <ch271828n@outlook.com>
2024-06-05 21:09:18 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/hir/flat/mod.rs
+++ frb_codegen/src/library/codegen/parser/hir/flat/mod.rs
@@ -13,25 +13,27 @@ pub(crate) fn parse(
     hir_tree: HirTreePack,
     dumper: &Dumper,
 ) -> anyhow::Result<HirFlatPack> {
-    let dump = |name, pack| dumper.dump(Hir, &format!("hir_flat/{name}.json"), pack);
-
     let pack = parser::pack::parse_pack(config, hir_tree)?;
-    dump("1_parse_pack", &pack)?;
+    dump(dumper, "1_parse_pack", &pack)?;
 
     let pack = transformer::function_interest_module_transformer::transform(pack, config)?;
-    dump("2_function_interest_module_transformer", &pack)?;
+    dump(dumper, "2_function_interest_module_transformer", &pack)?;
 
     let pack = transformer::remove_not_defined_trait_transformer::transform(pack)?;
-    dump("3_remove_not_defined_trait_transformer", &pack)?;
+    dump(dumper, "3_remove_not_defined_trait_transformer", &pack)?;
 
     let pack = transformer::copy_trait_def_to_impl_transformer::transform(pack)?;
-    dump("4_copy_trait_def_to_impl_transformer", &pack)?;
+    dump(dumper, "4_copy_trait_def_to_impl_transformer", &pack)?;
 
     let pack = transformer::merge_duplicate_transformer::transform(pack)?;
-    dump("5_merge_duplicate_transformer", &pack)?;
+    dump(dumper, "5_merge_duplicate_transformer", &pack)?;
 
     let pack = transformer::resolve_type_alias_transformer::transform(pack)?;
-    dump("6_resolve_type_alias_transformer", &pack)?;
+    dump(dumper, "6_resolve_type_alias_transformer", &pack)?;
 
     Ok(pack)
+}
+
+fn dump(dumper: &Dumper, name: &str, pack: &HirFlatPack) -> anyhow::Result<()> {
+    dumper.dump(Hir, &format!("hir_flat/{name}.json"), pack)
 }
