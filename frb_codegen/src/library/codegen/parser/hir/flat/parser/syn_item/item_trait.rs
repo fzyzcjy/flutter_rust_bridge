dfa1c7810ccb24f247bb6368bf557421e1098e3f
fzyzcjy <ch271828n@outlook.com>
2024-06-05 16:45:21 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/hir/flat/parser/syn_item/item_trait.rs
+++ frb_codegen/src/library/codegen/parser/hir/flat/parser/syn_item/item_trait.rs
@@ -1,44 +1,41 @@
+use crate::codegen::ir::hir::flat::function::{HirFlatFunction, HirFlatFunctionOwner};
 use crate::codegen::ir::hir::flat::pack::HirFlatPack;
 use crate::codegen::ir::hir::flat::traits::HirFlatTrait;
-use crate::codegen::ir::hir::hierarchical::traits::HirTrait;
+use crate::codegen::ir::hir::misc::item_fn::GeneralizedItemFn;
 use crate::codegen::ir::hir::tree::module::HirTreeModuleMeta;
 use crate::if_then_some;
 use crate::utils::namespace::{Namespace, NamespacedName};
-use syn::ItemImpl;
 use syn::ItemTrait;
+use syn::{ItemImpl, TraitItem};
 
 pub(crate) fn parse_syn_item_trait(
     target: &mut HirFlatPack,
     item_trait: ItemTrait,
     meta: &HirTreeModuleMeta,
-) -> HirTrait {
-    target.traits.push(parse_trait(&item_trait, meta));
-    target.functions.extend(TODO);
-}
+) {
+    let trait_name = NamespacedName::new(namespace.clone(), item_trait.ident.to_string());
 
-fn parse_trait(item_trait: &ItemTrait, meta: &HirTreeModuleMeta) -> HirFlatTrait {
-    HirFlatTrait {
-        name: NamespacedName::new(namespace.clone(), item_trait.ident.to_string()),
-    }
+    target.traits.push(HirFlatTrait { name: trait_name });
+
+    target
+        .functions
+        .extend(parse_functions(item_trait, meta, &trait_name));
 }
 
 fn parse_trait_def_functions(
-    trait_def: &HirTrait,
-    item_impl: &ItemImpl,
-    namespace: &Namespace,
+    item_trait: ItemTrait,
+    meta: &HirTreeModuleMeta,
     trait_def_name: &NamespacedName,
 ) -> Vec<HirFlatFunction> {
-    (trait_def.item_trait.items.iter())
-        .filter_map(
-            |item| if_then_some!(let TraitItem::Fn(ref trait_item_fn) = item, trait_item_fn),
-        )
+    (item_trait.items.into_iter())
+        .filter_map(|item| if_then_some!(let TraitItem::Fn(trait_item_fn) = item, trait_item_fn))
         .map(|trait_item_fn| HirFlatFunction {
-            namespace: namespace.clone(),
+            namespace: meta.namespace.clone(),
             owner: HirFlatFunctionOwner::Method {
                 item_impl: item_impl.to_owned(),
                 trait_def_name: Some(trait_def_name.to_owned()),
             },
-            item_fn: GeneralizedItemFn::TraitItemFn(trait_item_fn.to_owned()),
+            item_fn: GeneralizedItemFn::TraitItemFn(trait_item_fn),
         })
         .collect_vec()
 }
