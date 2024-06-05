a5e415dbbccf05edc600b35c71202978cd8a1e5c
fzyzcjy <ch271828n@outlook.com>
2024-06-05 16:36:04 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/hir/flat/parser/syn_item/item_impl.rs
+++ frb_codegen/src/library/codegen/parser/hir/flat/parser/syn_item/item_impl.rs
@@ -5,7 +5,7 @@ use crate::codegen::ir::hir::misc::item_fn::GeneralizedItemFn;
 use crate::if_then_some;
 use crate::utils::namespace::{Namespace, NamespacedName};
 use itertools::Itertools;
-use syn::{ItemImpl, ItemStruct};
+use syn::{ImplItem, ItemImpl, ItemStruct};
 use crate::codegen::ir::hir::tree::module::HirTreeModuleMeta;
 
 pub(crate) fn parse_syn_item_impl(
@@ -26,15 +26,15 @@ fn parse_for_struct_or_enum(
     meta: &HirTreeModuleMeta,
     trait_def_name: Option<NamespacedName>,
 ) -> Vec<HirFlatFunction> {
-    (item_impl.items.iter())
+    (item_impl.items.into_iter())
         .filter_map(|item| if_then_some!(let ImplItem::Fn(ref impl_item_fn) = item, impl_item_fn))
         .map(|impl_item_fn| HirFlatFunction {
-            namespace: namespace.clone(),
+            namespace: meta.namespace.clone(),
             owner: HirFlatFunctionOwner::Method {
                 item_impl: item_impl.to_owned(),
                 trait_def_name: trait_def_name.clone(),
             },
-            item_fn: GeneralizedItemFn::ImplItemFn(impl_item_fn.clone()),
+            item_fn: GeneralizedItemFn::ImplItemFn(impl_item_fn),
         })
         .collect_vec()
 }
