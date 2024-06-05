d82c72826fa1c0d5860f97e1097a893179130572
fzyzcjy <ch271828n@outlook.com>
2024-06-05 16:59:28 +0800

    chore: more

--- frb_codegen/src/library/codegen/parser/hir/flat/transformer/merge_duplicate_transformer/mod.rs
+++ frb_codegen/src/library/codegen/parser/hir/flat/transformer/merge_duplicate_transformer/mod.rs
@@ -7,5 +7,12 @@ pub(crate) mod third_party_override_merger;
 pub(crate) mod trait_impl_merger;
 
 pub(crate) fn transform(mut pack: HirFlatPack) -> anyhow::Result<HirFlatPack> {
+    transform_component(&mut pack.functions);
+    transform_component(&mut pack.structs);
+    transform_component(&mut pack.enums);
     Ok(pack)
+}
+
+fn transform_component<T>(items: &mut Vec<T>) {
+    TODO
 }
