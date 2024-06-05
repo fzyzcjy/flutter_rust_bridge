43b89c3b8e932f5cf418403d8ebbd96c7aa182de
fzyzcjy <ch271828n@outlook.com>
2024-06-05 18:08:48 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/hir/flat/exporter.rs
+++ frb_codegen/src/library/codegen/parser/hir/flat/exporter.rs
@@ -2,10 +2,21 @@ use crate::codegen::ir::hir::flat::pack::HirFlatPack;
 use log::debug;
 use std::collections::HashMap;
 use std::fmt::Debug;
+use std::hash::Hash;
 
 impl HirFlatPack {}
 
-fn vec_to_map_with_warn<T, K, V>(items: &[T], key_value: impl Fn(&T) -> (K, V)) -> HashMap<K, V> {
-    items.iter().map(|item| key_value(item))
-        .collect()
+fn vec_to_map_with_warn<T, K: Eq + Hash, V>(
+    items: &[T],
+    extract_entry: impl Fn(&T) -> (K, V),
+) -> HashMap<K, V> {
+    let mut ans = HashMap::new();
+    for item in items {
+        let (key, value) = extract_entry(item);
+        if let Some(old_value) = ans.get(&key) {
+            debug!("Same key={key} has multiple values: {old_value:?} (thrown away) and {value:?} (used). This may or may not be a problem.");
+        }
+        ans.insert(key, value);
+    }
+    ans
 }
