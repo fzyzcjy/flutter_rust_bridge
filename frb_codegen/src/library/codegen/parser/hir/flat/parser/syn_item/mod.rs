2dd09239e19cc31376b349edbbb55ef121f22967
fzyzcjy <ch271828n@outlook.com>
2024-06-05 14:25:21 +0800

    chore: more

--- frb_codegen/src/library/codegen/parser/hir/flat/parser/syn_item/mod.rs
+++ frb_codegen/src/library/codegen/parser/hir/flat/parser/syn_item/mod.rs
@@ -4,6 +4,7 @@ pub(crate) mod item_struct_or_enum;
 pub(crate) mod item_trait;
 pub(crate) mod item_type;
 
+use crate::codegen::ir::hir::flat::pack::HirFlatPack;
 use crate::codegen::ir::hir::tree::module::HirTreeModuleMeta;
 use crate::codegen::parser::hir::flat::parser::syn_item::item_fn::{
     parse_syn_item_fn, parse_syn_item_impl,
@@ -19,25 +20,25 @@ use crate::utils::namespace::Namespace;
 pub(crate) fn parse_syn_item(
     item: syn::Item,
     meta: HirTreeModuleMeta,
-    pack: &mut HirModuleContent,
+    target: &mut HirFlatPack,
     config: &ParserHirInternalConfig,
 ) -> anyhow::Result<()> {
     match item {
         syn::Item::Struct(item_struct) => {
-            (pack.structs).extend(parse_syn_item_struct(item_struct)?);
+            (target.structs).extend(parse_syn_item_struct(item_struct)?);
         }
         syn::Item::Enum(item_enum) => {
-            (pack.enums).extend(parse_syn_item_enum(item_enum)?);
+            (target.enums).extend(parse_syn_item_enum(item_enum)?);
         }
         syn::Item::Type(item_type) => {
-            pack.type_alias.extend(parse_syn_item_type(item_type));
+            target.type_alias.extend(parse_syn_item_type(item_type));
         }
         syn::Item::Fn(item_fn) => {
-            pack.functions.push(parse_syn_item_fn(item_fn));
+            target.functions.push(parse_syn_item_fn(item_fn));
         }
         syn::Item::Impl(item_impl) => TODO(parse_syn_item_impl(item_impl)),
         syn::Item::Trait(item_trait) => {
-            (pack.traits).push(parse_syn_item_trait(item_trait));
+            (target.traits).push(parse_syn_item_trait(item_trait));
         }
         _ => {}
     }
