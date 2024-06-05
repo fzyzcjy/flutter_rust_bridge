67e6062f4938bdddb6a2513b6463b5bcfc33aa7e
fzyzcjy <ch271828n@outlook.com>
2024-06-05 17:47:08 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/hir/flat/transformer/visibility_transformer.rs
+++ frb_codegen/src/library/codegen/parser/hir/flat/transformer/visibility_transformer.rs
@@ -1,6 +1,14 @@
 use crate::codegen::ir::hir::flat::pack::HirFlatPack;
+use crate::codegen::ir::hir::tree::module::HirTreeModuleMeta;
+use crate::utils::crate_name::CrateName;
 
 pub(crate) fn transform(mut pack: HirFlatPack) -> anyhow::Result<HirFlatPack> {
     TODO;
     Ok(pack)
+}
+
+fn is_interest_mod(meta: &HirTreeModuleMeta) -> bool {
+    // If it is third party crate, then we only scan the `pub` mods,
+    // since for non-pub modes, it is impossible to use them even if we scanned them.
+    meta.namespace.path()[0] == CrateName::SELF_CRATE || meta.is_public()
 }
