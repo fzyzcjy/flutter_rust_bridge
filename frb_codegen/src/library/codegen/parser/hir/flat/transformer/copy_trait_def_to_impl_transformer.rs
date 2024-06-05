32f97db46a78ed576070cf52fa54ef01a246da12
fzyzcjy <ch271828n@outlook.com>
2024-06-05 17:34:09 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/hir/flat/transformer/copy_trait_def_to_impl_transformer.rs
+++ frb_codegen/src/library/codegen/parser/hir/flat/transformer/copy_trait_def_to_impl_transformer.rs
@@ -1,6 +1,10 @@
-use crate::codegen::ir::hir::flat::function::HirFlatFunction;
+use crate::codegen::ir::hir::flat::function::{HirFlatFunction, HirFlatFunctionOwner};
 use crate::codegen::ir::hir::flat::pack::HirFlatPack;
 use crate::codegen::ir::hir::flat::trait_impl::HirFlatTraitImpl;
+use crate::codegen::ir::hir::flat::traits::HirFlatTrait;
+use crate::if_then_some;
+use itertools::Itertools;
+use std::collections::HashMap;
 
 pub(crate) fn transform(mut pack: HirFlatPack) -> anyhow::Result<HirFlatPack> {
     for trait_impl in &pack.trait_impls {
@@ -10,5 +14,14 @@ pub(crate) fn transform(mut pack: HirFlatPack) -> anyhow::Result<HirFlatPack> {
 }
 
 fn compute_functions(trait_impl: &HirFlatTraitImpl, pack: &HirFlatPack) -> Vec<HirFlatFunction> {
-    TODO;
+    (pack.functions.iter())
+        .filter(|f| {
+            TODO;
+            TODO;
+        })
+        .map(|f| {
+            TODO;
+            TODO;
+        })
+        .collect_vec()
 }
