214e7ca9c6090e44da47158839a4c1635ead7f56
fzyzcjy <ch271828n@outlook.com>
2024-06-05 17:46:23 +0800

    feat: empty

--- frb_codegen/src/library/codegen/parser/hir/flat/mod.rs
+++ frb_codegen/src/library/codegen/parser/hir/flat/mod.rs
@@ -12,5 +12,6 @@ pub(crate) fn parse(
     let pack = transformer::copy_trait_def_to_impl_transformer::transform(pack)?;
     let pack = transformer::merge_duplicate_transformer::transform(pack)?;
     let pack = transformer::resolve_type_alias_transformer::transform(pack)?;
+    let pack = transformer::visibility_transformer::transform(pack)?;
     Ok(pack)
 }
