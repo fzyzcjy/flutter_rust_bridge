9c5b35c23bfd97f477a3c4341ee066f4f80ad621
fzyzcjy <ch271828n@outlook.com>
2024-06-05 13:29:55 +0800

    chore: import

--- frb_codegen/src/library/codegen/ir/hir/tree/crates.rs
+++ frb_codegen/src/library/codegen/ir/hir/tree/crates.rs
@@ -1,5 +1,6 @@
 // This file is named `crates` not `crate`, because the latter is a Rust keyword
 
+use crate::codegen::ir::hir::tree::module::HirTreeModule;
 use crate::utils::crate_name::CrateName;
 
 #[derive(Debug, Clone)]
