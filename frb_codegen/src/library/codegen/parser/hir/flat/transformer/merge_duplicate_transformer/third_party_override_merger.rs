0e97c8b35863fe25eab0d405782a8a3147158855
fzyzcjy <ch271828n@outlook.com>
2024-06-05 17:20:02 +0800

    chore: more

--- frb_codegen/src/library/codegen/parser/hir/flat/transformer/merge_duplicate_transformer/third_party_override_merger.rs
+++ frb_codegen/src/library/codegen/parser/hir/flat/transformer/merge_duplicate_transformer/third_party_override_merger.rs
@@ -15,13 +15,14 @@ use std::fmt::Debug;
 pub(crate) struct ThirdPartyOverrideMerger;
 
 impl BaseMerger for ThirdPartyOverrideMerger {
-    fn merge_functions(&self, items: Vec<HirFlatFunction>) -> Vec<HirFlatFunction> {
+    fn merge_functions(&self, a: HirFlatFunction, b: HirFlatFunction) -> Option<HirFlatFunction> {
         todo!()
     }
 
     fn merge_struct_or_enums<Item: SynItemStructOrEnum>(
         &self,
-        items: Vec<HirFlatStructOrEnum<Item>>,
+        a: HirFlatStructOrEnum<Item>,
+        b: HirFlatStructOrEnum<Item>,
     ) -> Vec<HirFlatStructOrEnum<Item>> {
         todo!()
     }
