06f47db6395df26fc0eaab81e70aad890c62c21b
fzyzcjy <ch271828n@outlook.com>
2024-06-05 17:29:33 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/hir/flat/transformer/copy_trait_def_to_impl_transformer.rs
+++ frb_codegen/src/library/codegen/parser/hir/flat/transformer/copy_trait_def_to_impl_transformer.rs
@@ -1,5 +1,14 @@
+use crate::codegen::ir::hir::flat::function::HirFlatFunction;
 use crate::codegen::ir::hir::flat::pack::HirFlatPack;
+use crate::codegen::ir::hir::flat::trait_impl::HirFlatTraitImpl;
 
 pub(crate) fn transform(mut pack: HirFlatPack) -> anyhow::Result<HirFlatPack> {
+    for trait_impl in &pack.trait_impls {
+        pack.functions.extend(compute_functions(trait_impl, &pack));
+    }
+    Ok(pack)
+}
+
+fn compute_functions(trait_impl: &HirFlatTraitImpl, pack: &HirFlatPack) -> Vec<HirFlatFunction> {
     TODO;
 }
