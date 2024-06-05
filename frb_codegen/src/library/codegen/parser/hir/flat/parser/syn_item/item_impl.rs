37143847fcbc1dd3370c9e058b00149da04193ce
fzyzcjy <ch271828n@outlook.com>
2024-06-05 14:24:29 +0800

    feat: more

---
+++ frb_codegen/src/library/codegen/parser/hir/flat/parser/syn_item/item_impl.rs
@@ -0,0 +1,11 @@
+use crate::codegen::ir::hir::hierarchical::struct_or_enum::HirStruct;
+use crate::utils::namespace::Namespace;
+use syn::{ItemImpl, ItemStruct};
+
+pub(crate) fn parse_syn_item_impl(item: &ItemImpl) -> anyhow::Result<TODO> {
+    if item_impl.trait_.is_some() {
+        (pack.trait_impls).push(parse_trait_impl(item_impl, namespace));
+    } else {
+        (pack.functions).extend(parse_syn_item_impl(item_impl, namespace, None));
+    }
+}
--- frb_codegen/src/library/codegen/parser/hir/flat/parser/syn_item/mod.rs
+++ frb_codegen/src/library/codegen/parser/hir/flat/parser/syn_item/mod.rs
@@ -1,9 +1,12 @@
 pub(crate) mod item_fn;
+mod item_impl;
 pub(crate) mod item_struct_or_enum;
 pub(crate) mod item_type;
 
 use crate::codegen::ir::hir::tree::module::HirTreeModuleMeta;
-use crate::codegen::parser::hir::flat::parser::syn_item::item_fn::parse_syn_item_fn;
+use crate::codegen::parser::hir::flat::parser::syn_item::item_fn::{
+    parse_syn_item_fn, parse_syn_item_impl,
+};
 use crate::codegen::parser::hir::flat::parser::syn_item::item_struct_or_enum::{
     parse_syn_item_enum, parse_syn_item_struct,
 };
@@ -19,26 +22,20 @@ pub(crate) fn parse_syn_item(
 ) -> anyhow::Result<()> {
     match item {
         syn::Item::Struct(item_struct) => {
-            (pack.structs).extend(parse_syn_item_struct(item_struct, namespace)?);
+            (pack.structs).extend(parse_syn_item_struct(item_struct)?);
         }
         syn::Item::Enum(item_enum) => {
-            (pack.enums).extend(parse_syn_item_enum(item_enum, namespace)?);
+            (pack.enums).extend(parse_syn_item_enum(item_enum)?);
         }
         syn::Item::Type(item_type) => {
             pack.type_alias.extend(parse_syn_item_type(item_type));
         }
         syn::Item::Fn(item_fn) => {
-            pack.functions.push(parse_syn_item_fn(item_fn, namespace));
+            pack.functions.push(parse_syn_item_fn(item_fn));
         }
-        syn::Item::Impl(item_impl) => {
-            if item_impl.trait_.is_some() {
-                (pack.trait_impls).push(parse_trait_impl(item_impl, namespace));
-            } else {
-                (pack.functions).extend(parse_syn_item_impl(item_impl, namespace, None));
-            }
-        }
+        syn::Item::Impl(item_impl) => TODO(parse_syn_item_impl(item_impl)),
         syn::Item::Trait(item_trait) => {
-            (pack.traits).push(parse_syn_item_trait(item_trait, namespace));
+            (pack.traits).push(parse_syn_item_trait(item_trait));
         }
         _ => {}
     }
