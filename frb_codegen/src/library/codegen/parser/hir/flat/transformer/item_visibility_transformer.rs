b6e3bfc9e9b364622d96307d381cd1bec13f3676
fzyzcjy <ch271828n@outlook.com>
2024-06-05 20:55:47 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/hir/flat/transformer/item_visibility_transformer.rs
+++ frb_codegen/src/library/codegen/parser/hir/flat/transformer/item_visibility_transformer.rs
@@ -1,11 +1,19 @@
 use crate::codegen::ir::hir::flat::pack::HirFlatPack;
+use crate::codegen::parser::hir::flat::transformer::merge_duplicate_transformer::base::BaseMerger;
 use crate::codegen::parser::hir::internal_config::ParserHirInternalConfig;
 use itertools::Itertools;
+use serde::Serialize;
+use std::fmt::Debug;
+use std::hash::Hash;
 
 pub(crate) fn transform(
     mut pack: HirFlatPack,
     config: &ParserHirInternalConfig,
 ) -> anyhow::Result<HirFlatPack> {
-    TODO;
+    transform_component(&mut pack.functions);
     Ok(pack)
+}
+
+fn transform_component<T>(items: &mut Vec<T>, filter: impl Fn(&T) -> bool) {
+    *items = items.drain(..).filter(|x| filter(x)).collect_vec();
 }
