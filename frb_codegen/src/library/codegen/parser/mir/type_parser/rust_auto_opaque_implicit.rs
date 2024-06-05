b805c16054bc6b63e22c602a9c1296b866290900
fzyzcjy <ch271828n@outlook.com>
2024-06-05 09:31:57 +0800

    feat: empty

--- frb_codegen/src/library/codegen/parser/mir/type_parser/rust_auto_opaque_implicit.rs
+++ frb_codegen/src/library/codegen/parser/mir/type_parser/rust_auto_opaque_implicit.rs
@@ -64,7 +64,7 @@ impl<'a, 'b, 'c> TypeParserWithContext<'a, 'b, 'c> {
                     "flutter_rust_bridge::for_generated::RustAutoOpaqueInner<{inner_str}>"
                 )),
                 codec: info.codec,
-                impl_traits: TODO,
+                impl_traits: self.parse_trait_impls()?,
                 brief_name: true,
             },
         ))
