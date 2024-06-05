a8430c6afa5738437084f245bb8c3e836ef8a233
fzyzcjy <ch271828n@outlook.com>
2024-06-05 18:54:13 +0800

    feat: try

--- frb_codegen/src/library/codegen/parser/hir/flat/transformer/merge_duplicate_transformer/base.rs
+++ frb_codegen/src/library/codegen/parser/hir/flat/transformer/merge_duplicate_transformer/base.rs
@@ -1,6 +1,8 @@
-use crate::codegen::ir::hir::misc::syn_item_struct_or_enum::SynItemStructOrEnum;
 use crate::codegen::ir::hir::flat::function::HirFlatFunction;
-use crate::codegen::ir::hir::flat::struct_or_enum::HirFlatStructOrEnum;
+use crate::codegen::ir::hir::flat::struct_or_enum::{
+    HirFlatEnum, HirFlatStruct, HirFlatStructOrEnum,
+};
+use crate::codegen::ir::hir::misc::syn_item_struct_or_enum::SynItemStructOrEnum;
 
 pub(crate) trait BaseMerger {
     fn merge_functions(
@@ -8,6 +10,18 @@ pub(crate) trait BaseMerger {
         base: &HirFlatFunction,
         overrider: &HirFlatFunction,
     ) -> Option<HirFlatFunction>;
+
+    fn merge_structs(
+        &self,
+        base: &HirFlatStruct,
+        overrider: &HirFlatStruct,
+    ) -> Option<HirFlatStruct> {
+        self.merge_struct_or_enums(base, overrider)
+    }
+
+    fn merge_enums(&self, base: &HirFlatEnum, overrider: &HirFlatEnum) -> Option<HirFlatEnum> {
+        self.merge_struct_or_enums(base, overrider)
+    }
 
     fn merge_struct_or_enums<Item: SynItemStructOrEnum>(
         &self,
