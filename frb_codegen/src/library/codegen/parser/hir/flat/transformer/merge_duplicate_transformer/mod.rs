3789fd952b2c5c533cf799306f9e7eac73fb84e5
fzyzcjy <ch271828n@outlook.com>
2024-06-05 16:58:04 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/hir/flat/transformer/merge_duplicate_transformer/mod.rs
+++ frb_codegen/src/library/codegen/parser/hir/flat/transformer/merge_duplicate_transformer/mod.rs
@@ -1,9 +1,40 @@
+use crate::codegen::ir::hir::flat::function::HirFlatFunction;
 use crate::codegen::ir::hir::flat::pack::HirFlatPack;
+use crate::codegen::ir::hir::flat::struct_or_enum::HirFlatStructOrEnum;
 
 pub(crate) mod third_party_override_merger;
 pub(crate) mod trait_impl_merger;
 
 pub(crate) fn transform(mut pack: HirFlatPack) -> anyhow::Result<HirFlatPack> {
-    TODO;
     Ok(pack)
+}
+
+trait BaseMerger {
+    fn merge_functions(&self, items: Vec<HirFlatFunction>) -> Vec<HirFlatFunction>;
+
+    fn merge_struct_or_enums<Item: SynItemStructOrEnum>(
+        &self,
+        items: Vec<HirFlatStructOrEnum<Item>>,
+    ) -> Vec<HirFlatStructOrEnum<Item>>;
+}
+
+struct CombinedMerger(pub Vec<dyn BaseMerger>);
+
+impl BaseMerger for CombinedMerger {
+    fn merge_functions(&self, mut items: Vec<HirFlatFunction>) -> Vec<HirFlatFunction> {
+        for merger in &self.0 {
+            items = merger.merge_functions(items);
+        }
+        items
+    }
+
+    fn merge_struct_or_enums<Item: SynItemStructOrEnum>(
+        &self,
+        items: Vec<HirFlatStructOrEnum<Item>>,
+    ) -> Vec<HirFlatStructOrEnum<Item>> {
+        for merger in &self.0 {
+            items = merger.merge_struct_or_enums(items);
+        }
+        items
+    }
 }
