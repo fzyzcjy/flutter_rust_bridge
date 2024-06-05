48f1af0ccc498892195bb5b5618d282df3a047b3
fzyzcjy <ch271828n@outlook.com>
2024-06-05 17:26:34 +0800

    chore: empty

--- frb_codegen/src/library/codegen/parser/hir/flat/transformer/merge_duplicate_transformer/trait_def_default_impl_merger.rs
+++ frb_codegen/src/library/codegen/parser/hir/flat/transformer/merge_duplicate_transformer/trait_def_default_impl_merger.rs
@@ -1,2 +1,24 @@
+use crate::codegen::ir::hir::flat::function::HirFlatFunction;
+use crate::codegen::ir::hir::flat::struct_or_enum::HirFlatStructOrEnum;
+use crate::codegen::parser::hir::flat::transformer::merge_duplicate_transformer::base::BaseMerger;
+
 /// Merge the "default implementation in trait definition" with the overriden implementation in trait impl.
 pub(crate) struct TraitDefDefaultImplMerger;
+
+impl BaseMerger for TraitDefDefaultImplMerger {
+    fn merge_functions(
+        &self,
+        base: &HirFlatFunction,
+        overrider: &HirFlatFunction,
+    ) -> Option<HirFlatFunction> {
+        todo!()
+    }
+
+    fn merge_struct_or_enums<Item: SynItemStructOrEnum>(
+        &self,
+        base: &HirFlatStructOrEnum<Item>,
+        overrider: &HirFlatStructOrEnum<Item>,
+    ) -> Option<HirFlatStructOrEnum<Item>> {
+        todo!()
+    }
+}
