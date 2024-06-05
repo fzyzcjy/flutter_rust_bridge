a2b1de73151d251f100e0a725076d83493bea505
fzyzcjy <ch271828n@outlook.com>
2024-06-05 17:11:13 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/hir/flat/transformer/merge_duplicate_transformer/third_party_override_merger.rs
+++ frb_codegen/src/library/codegen/parser/hir/flat/transformer/merge_duplicate_transformer/third_party_override_merger.rs
@@ -1,22 +1,29 @@
+use crate::codegen::ir::hir::flat::function::HirFlatFunction;
+use crate::codegen::ir::hir::flat::struct_or_enum::HirFlatStructOrEnum;
 use crate::codegen::ir::hir::hierarchical::function::HirFlatFunction;
 use crate::codegen::ir::hir::hierarchical::module::HirModule;
 use crate::codegen::ir::hir::hierarchical::pack::HirPack;
 use crate::codegen::ir::hir::hierarchical::struct_or_enum::HirFlatStructOrEnum;
 use crate::codegen::ir::hir::hierarchical::syn_item_struct_or_enum::SynItemStructOrEnum;
 use crate::codegen::misc::THIRD_PARTY_DIR_NAME;
+use crate::codegen::parser::hir::flat::transformer::merge_duplicate_transformer::base::BaseMerger;
 use crate::utils::crate_name::CrateName;
 use itertools::Itertools;
 use std::fmt::Debug;
 
 pub(crate) struct ThirdPartyOverrideMerger;
 
-pub(crate) fn transform(mut pack: HirPack) -> anyhow::Result<HirPack> {
-    if let Some(module_third_party_root) = remove_module_third_party_root(&mut pack) {
-        for src in module_third_party_root.content.modules {
-            transform_crate(&mut pack, src)?;
-        }
+impl BaseMerger for ThirdPartyOverrideMerger {
+    fn merge_functions(&self, items: Vec<HirFlatFunction>) -> Vec<HirFlatFunction> {
+        todo!()
     }
-    Ok(pack)
+
+    fn merge_struct_or_enums<Item: SynItemStructOrEnum>(
+        &self,
+        items: Vec<HirFlatStructOrEnum<Item>>,
+    ) -> Vec<HirFlatStructOrEnum<Item>> {
+        todo!()
+    }
 }
 
 fn remove_module_third_party_root(pack: &mut HirPack) -> Option<HirModule> {
@@ -25,28 +32,6 @@ fn remove_module_third_party_root(pack: &mut HirPack) -> Option<HirModule> {
         .root_module
         .content
         .remove_module_by_name(THIRD_PARTY_DIR_NAME)
-}
-
-fn transform_crate(pack: &mut HirPack, src: HirModule) -> anyhow::Result<()> {
-    let crate_name = CrateName::new((src.meta.namespace.path().last()).unwrap().to_string());
-    if let Some(target_crate) = pack.get_mut_crate(&crate_name) {
-        transform_module(&mut target_crate.root_module, src)?;
-    } else {
-        log::warn!(
-            "Skip `{}` since there is no corresponding scanned third party crate.",
-            src.meta.namespace,
-        );
-    }
-
-    Ok(())
-}
-
-fn transform_module(target: &mut HirModule, src: HirModule) -> anyhow::Result<()> {
-    transform_module_content_functions(&mut target.content.functions, src.content.functions)?;
-    transform_module_content_struct_or_enums(&mut target.content.structs, src.content.structs)?;
-    transform_module_content_struct_or_enums(&mut target.content.enums, src.content.enums)?;
-    transform_module_content_modules(target, src.content.modules)?;
-    Ok(())
 }
 
 fn transform_module_content_functions(
@@ -102,24 +87,6 @@ fn transform_module_content_general_vec<T: Debug, K: Eq + Debug>(
         let target_item = interest_target_items.into_iter().next().unwrap();
 
         write(target_item, src_item);
-    }
-    Ok(())
-}
-
-fn transform_module_content_modules(
-    target: &mut HirModule,
-    src_content_modules: Vec<HirModule>,
-) -> anyhow::Result<()> {
-    for src_child_module in src_content_modules {
-        let name = *src_child_module.meta.namespace.path().last().unwrap();
-        if let Some(target_child_module) = target.content.get_mut_module_by_name(name) {
-            transform_module(target_child_module, src_child_module)?;
-        } else {
-            log::warn!(
-                "Skip `{}` since there is no corresponding scanned third party crate target module.",
-                src_child_module.meta.namespace,
-            );
-        }
     }
     Ok(())
 }
