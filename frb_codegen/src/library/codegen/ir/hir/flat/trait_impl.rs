ed80296b35b3df01826bb382ef3ac9e7c8de0ebc
fzyzcjy <ch271828n@outlook.com>
2024-06-05 18:13:37 +0800

    chore: more

--- frb_codegen/src/library/codegen/ir/hir/flat/trait_impl.rs
+++ frb_codegen/src/library/codegen/ir/hir/flat/trait_impl.rs
@@ -1,6 +1,6 @@
 use crate::utils::namespace::NamespacedName;
 
-#[derive(Clone, Derivative, Serialize, Debug)]
+#[derive(Clone, serde::Serialize, Debug)]
 pub struct HirFlatTraitImpl {
     pub(crate) trait_name: String,
     pub(crate) impl_ty: syn::Type,
