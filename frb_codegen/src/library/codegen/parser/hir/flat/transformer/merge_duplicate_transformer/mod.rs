798e741c005cd651b8f36d0f72ad59b5b3705890
fzyzcjy <ch271828n@outlook.com>
2024-06-05 17:02:46 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/hir/flat/transformer/merge_duplicate_transformer/mod.rs
+++ frb_codegen/src/library/codegen/parser/hir/flat/transformer/merge_duplicate_transformer/mod.rs
@@ -1,7 +1,7 @@
 use crate::codegen::ir::hir::flat::function::HirFlatFunction;
 use crate::codegen::ir::hir::flat::pack::HirFlatPack;
 use crate::codegen::ir::hir::flat::struct_or_enum::HirFlatStructOrEnum;
-use crate::codegen::parser::hir::flat::transformer::merge_duplicate_transformer::base::CombinedMerger;
+use crate::codegen::parser::hir::flat::transformer::merge_duplicate_transformer::base::{BaseMerger, CombinedMerger};
 use crate::codegen::parser::hir::flat::transformer::merge_duplicate_transformer::third_party_override_merger::ThirdPartyOverrideMerger;
 use crate::codegen::parser::hir::flat::transformer::merge_duplicate_transformer::trait_def_default_impl_merger::TraitDefDefaultImplMerger;
 
@@ -12,13 +12,12 @@ pub(crate) mod trait_impl_merger;
 
 pub(crate) fn transform(mut pack: HirFlatPack) -> anyhow::Result<HirFlatPack> {
     let merger = CombinedMerger(vec![TraitDefDefaultImplMerger, ThirdPartyOverrideMerger]);
-
-    transform_component(&mut pack.functions);
-    transform_component(&mut pack.structs);
-    transform_component(&mut pack.enums);
+    transform_component(&mut pack.functions, |x| merger.merge_functions(x));
+    transform_component(&mut pack.structs, |x| merger.merge_struct_or_enums(x));
+    transform_component(&mut pack.enums, |x| merger.merge_struct_or_enums(x));
     Ok(pack)
 }
 
-fn transform_component<T>(items: &mut Vec<T>) {
+fn transform_component<T>(items: &mut Vec<T>, merge: Fn(Vec<T>) -> Vec<T>) {
     TODO
 }
