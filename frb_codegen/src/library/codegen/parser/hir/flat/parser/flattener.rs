75c29a7436fa236577c87c94c8a8f75271a1c6b5
fzyzcjy <ch271828n@outlook.com>
2024-06-05 14:15:19 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/hir/flat/parser/flattener.rs
+++ frb_codegen/src/library/codegen/parser/hir/flat/parser/flattener.rs
@@ -2,7 +2,22 @@ use crate::codegen::ir::hir::tree::module::{HirTreeModule, HirTreeModuleMeta};
 use crate::codegen::ir::hir::tree::pack::HirTreePack;
 
 pub(crate) fn flatten(pack: HirTreePack) -> anyhow::Result<Vec<SynItemWithMeta>> {
-    TODO;
+    let mut ans = vec![];
+    for hir_crate in pack.crates {
+        flatten_module(hir_crate.root_module, &mut ans);
+    }
+    Ok(ans)
+}
+
+fn flatten_module(module: HirTreeModule, target: &mut Vec<SynItemWithMeta>) {
+    target.extend(module.items.into_iter().map(|item| SynItemWithMeta {
+        meta: module.meta.clone(),
+        item,
+    }));
+
+    for child_module in module.modules {
+        flatten_module(child_module);
+    }
 }
 
 pub(crate) struct SynItemWithMeta {
