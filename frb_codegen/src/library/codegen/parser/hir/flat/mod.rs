26763d803e1a41caae33484971fdf361bdca0d06
fzyzcjy <ch271828n@outlook.com>
2024-06-05 20:32:53 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/hir/flat/mod.rs
+++ frb_codegen/src/library/codegen/parser/hir/flat/mod.rs
@@ -11,10 +11,10 @@ pub(crate) fn parse(
     hir_tree: HirTreePack,
 ) -> anyhow::Result<HirFlatPack> {
     let pack = parser::pack::parse_pack(config, hir_tree)?;
+    let pack = transformer::function_interest_module_transformer::transform(pack, config)?;
     let pack = transformer::remove_not_defined_trait_transformer::transform(pack)?;
     let pack = transformer::copy_trait_def_to_impl_transformer::transform(pack)?;
     let pack = transformer::merge_duplicate_transformer::transform(pack)?;
     let pack = transformer::resolve_type_alias_transformer::transform(pack)?;
-    let pack = transformer::function_interest_module_transformer::transform(pack, config)?;
     Ok(pack)
 }
