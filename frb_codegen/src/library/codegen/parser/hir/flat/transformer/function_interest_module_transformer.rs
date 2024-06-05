e68a8c35231e86bb6f3355945c76d72fd6966f2b
fzyzcjy <ch271828n@outlook.com>
2024-06-05 21:16:43 +0800

    fix: err

--- frb_codegen/src/library/codegen/parser/hir/flat/transformer/function_interest_module_transformer.rs
+++ frb_codegen/src/library/codegen/parser/hir/flat/transformer/function_interest_module_transformer.rs
@@ -1,13 +1,17 @@
 use crate::codegen::ir::hir::flat::pack::HirFlatPack;
 use crate::codegen::parser::hir::internal_config::ParserHirInternalConfig;
 use itertools::Itertools;
+use crate::codegen::misc::THIRD_PARTY_NAMESPACE;
 
 pub(crate) fn transform(
     mut pack: HirFlatPack,
     config: &ParserHirInternalConfig,
 ) -> anyhow::Result<HirFlatPack> {
     pack.functions = (pack.functions.drain(..))
-        .filter(|f| (config.rust_input_namespace_pack).is_interest(&f.namespace) || TODO)
+        .filter(|f| {
+            (config.rust_input_namespace_pack).is_interest(&f.namespace)
+                || THIRD_PARTY_NAMESPACE.is_prefix_of(&f.namespace)
+        })
         .collect_vec();
     Ok(pack)
 }
