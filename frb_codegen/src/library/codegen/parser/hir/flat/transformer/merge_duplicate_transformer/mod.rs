f2185755ac5025256c3289f12d4d0299ec96cdb2
fzyzcjy <ch271828n@outlook.com>
2024-06-05 16:29:43 +0800

    chore: mv

--- frb_codegen/src/library/codegen/parser/hir/flat/transformer/merge_duplicate_transformer/mod.rs
+++ frb_codegen/src/library/codegen/parser/hir/flat/transformer/merge_duplicate_transformer/mod.rs
@@ -1,2 +1,9 @@
+use crate::codegen::ir::hir::flat::pack::HirFlatPack;
+
 pub(crate) mod third_party_override_merger;
 pub(crate) mod trait_impl_merger;
+
+pub(crate) fn transform(mut pack: HirFlatPack) -> anyhow::Result<HirFlatPack> {
+    TODO;
+    Ok(pack)
+}
--- frb_codegen/src/library/codegen/parser/hir/flat/transformer/mod.rs
+++ frb_codegen/src/library/codegen/parser/hir/flat/transformer/mod.rs
@@ -1,8 +1,2 @@
-use crate::codegen::ir::hir::flat::pack::HirFlatPack;
-
 pub(crate) mod merge_duplicate_transformer;
 
-pub(crate) fn transform(mut pack: HirFlatPack) -> anyhow::Result<HirFlatPack> {
-    TODO;
-    Ok(pack)
-}
