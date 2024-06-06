e885db751a54291b21c9e70e3403f65aec452698
fzyzcjy <ch271828n@outlook.com>
2024-06-06 15:00:07 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/hir/naive_flat/transformer/module_visibility_filter_transformer.rs
+++ frb_codegen/src/library/codegen/parser/hir/naive_flat/transformer/module_visibility_filter_transformer.rs
@@ -1,17 +1,17 @@
-use crate::codegen::ir::hir::naive_flat::item::HirNaiveFlatItemMeta;
+use crate::codegen::ir::hir::naive_flat::item::{HirNaiveFlatItem, HirNaiveFlatItemMeta};
 use crate::codegen::ir::hir::naive_flat::pack::HirNaiveFlatPack;
 use crate::utils::crate_name::CrateName;
 use itertools::Itertools;
 
 pub(crate) fn transform(mut pack: HirNaiveFlatPack) -> anyhow::Result<HirNaiveFlatPack> {
     pack.items = (pack.items.drain(..))
-        .filter(|item| is_interest_mod(&item.meta))
+        .filter(|item| is_interest_mod(item))
         .collect_vec();
     Ok(pack)
 }
 
-fn is_interest_mod(meta: &HirNaiveFlatItemMeta) -> bool {
+fn is_interest_mod(item: &HirNaiveFlatItem) -> bool {
     // If it is third party crate, then we only scan the `pub` mods,
     // since for non-pub modes, it is impossible to use them even if we scanned them.
-    meta.namespace.path()[0] == CrateName::SELF_CRATE || meta.is_public
+    item.meta.namespace.path()[0] == CrateName::SELF_CRATE || item.meta.is_public
 }
