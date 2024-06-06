943acdd5b19223baa45dd0432ba1cc5edd06ce71
fzyzcjy <ch271828n@outlook.com>
2024-06-06 18:19:08 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/hir/flat/transformer/function_frb_override_transformer.rs
+++ frb_codegen/src/library/codegen/parser/hir/flat/transformer/function_frb_override_transformer.rs
@@ -3,15 +3,18 @@ use crate::codegen::ir::hir::flat::pack::HirFlatPack;
 
 pub(crate) fn transform(mut pack: HirFlatPack) -> anyhow::Result<HirFlatPack> {
     for function in pack.functions.iter_mut() {
-        transform_function(function);
+        transform_function(function)?;
     }
     Ok(pack)
 }
 
-fn transform_function(function: &mut HirFlatFunction) {
+fn transform_function(function: &mut HirFlatFunction) -> anyhow::Result<()> {
     if let Some(func_name_stripped) = function.item_fn.name().strip_prefix(FRB_OVERRIDE_PREFIX) {
-        TODO
+        let attr_str = format!(r###"#[frb(name = "{}")]"###, func_name_stripped);
+        let attr = syn::parse_str(&attr_str)?;
+        function.item_fn.attrs_mut().push(attr);
     }
+    Ok(())
 }
 
 const FRB_OVERRIDE_PREFIX: &str = "frb_override_";
