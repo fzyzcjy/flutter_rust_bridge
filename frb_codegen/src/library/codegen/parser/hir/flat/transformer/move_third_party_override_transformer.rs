f80a93505d8a51eb25552b770fc11e4e24aade94
fzyzcjy <ch271828n@outlook.com>
2024-06-06 14:01:15 +0800

    Revert "feat: more"
    This reverts commit e4e790c2ed5d451ebffcc3e523ccf865c1405202.

--- frb_codegen/src/library/codegen/parser/hir/flat/transformer/move_third_party_override_transformer.rs
+++ frb_codegen/src/library/codegen/parser/hir/flat/transformer/move_third_party_override_transformer.rs
@@ -1,16 +1,6 @@
 use crate::codegen::ir::hir::flat::pack::HirFlatPack;
-use itertools::Itertools;
 
 pub(crate) fn transform(mut pack: HirFlatPack) -> anyhow::Result<HirFlatPack> {
-    transform_component(&mut pack.functions);
+    TODO;
     Ok(pack)
-}
-
-fn transform_component<T>(items: &mut Vec<T>) {
-    *items = (items.drain(..))
-        .map(|item| {
-            TODO;
-            TODO;
-        })
-        .collect_vec();
 }
