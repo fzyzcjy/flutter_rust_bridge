9ff7d90715c3fe52d2597d0a42cb3b54f34d5a00
fzyzcjy <ch271828n@outlook.com>
2024-06-06 22:12:55 +0800

    refactor: extract

--- frb_codegen/src/library/codegen/parser/hir/flat/transformer/filter_transformer.rs
+++ frb_codegen/src/library/codegen/parser/hir/flat/transformer/filter_transformer.rs
@@ -1,21 +1,24 @@
 use crate::codegen::ir::hir::flat::pack::HirFlatPack;
 use crate::codegen::misc::SELF_CRATE_THIRD_PARTY_NAMESPACE;
 use crate::codegen::parser::hir::internal_config::ParserHirInternalConfig;
+use crate::utils::namespace::Namespace;
 use itertools::Itertools;
 
 pub(crate) fn transform(
     mut pack: HirFlatPack,
     config: &ParserHirInternalConfig,
 ) -> anyhow::Result<HirFlatPack> {
-    filter_function_interest_module(&mut pack, config);
+    filter_function(&mut pack, config);
     Ok(pack)
 }
 
-fn filter_function_interest_module(pack: &mut HirFlatPack, config: &ParserHirInternalConfig) {
+fn filter_function(pack: &mut HirFlatPack, config: &ParserHirInternalConfig) {
     pack.functions = (pack.functions.drain(..))
-        .filter(|f| {
-            (config.rust_input_namespace_pack).is_interest(&f.namespace)
-                || SELF_CRATE_THIRD_PARTY_NAMESPACE.is_prefix_of(&f.namespace)
-        })
+        .filter(|f| is_interest_module(&f.namespace, config))
         .collect_vec();
+}
+
+fn is_interest_module(namespace: &Namespace, config: &ParserHirInternalConfig) -> bool {
+    (config.rust_input_namespace_pack).is_interest(namespace)
+        || SELF_CRATE_THIRD_PARTY_NAMESPACE.is_prefix_of(namespace)
 }
