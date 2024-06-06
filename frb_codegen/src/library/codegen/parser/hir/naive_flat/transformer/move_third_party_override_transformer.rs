f23bcda871dd7d3eca6b1d2dd177cae0a022776d
fzyzcjy <ch271828n@outlook.com>
2024-06-06 14:23:33 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/hir/naive_flat/transformer/move_third_party_override_transformer.rs
+++ frb_codegen/src/library/codegen/parser/hir/naive_flat/transformer/move_third_party_override_transformer.rs
@@ -1,11 +1,15 @@
 use crate::codegen::ir::hir::naive_flat::pack::HirNaiveFlatPack;
+use crate::codegen::misc::SELF_CRATE_THIRD_PARTY_NAMESPACE;
 use itertools::Itertools;
 
 pub(crate) fn transform(mut pack: HirNaiveFlatPack) -> anyhow::Result<HirNaiveFlatPack> {
     pack.items = (pack.items.drain(..))
         .map(|item| {
-            TODO;
-            TODO
+            if SELF_CRATE_THIRD_PARTY_NAMESPACE.is_prefix_of(&item.meta.namespace) {
+                TODO
+            } else {
+                item
+            }
         })
         .collect_vec();
     Ok(pack)
