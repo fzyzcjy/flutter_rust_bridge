d1ed38378537d8b4b04e69c0c7b9c2e9f12938a3
fzyzcjy <ch271828n@outlook.com>
2024-06-06 10:03:05 +0800

    chore: codegen

--- frb_example/pure_dart_pde/lib/src/rust/api/pseudo_manual/attribute_twin_rust_async.dart
+++ frb_example/pure_dart_pde/lib/src/rust/api/pseudo_manual/attribute_twin_rust_async.dart
@@ -9,7 +9,7 @@ import 'package:freezed_annotation/freezed_annotation.dart' hide protected;
 import 'package:meta/meta.dart' as meta;
 part 'attribute_twin_rust_async.freezed.dart';
 
-// These types are ignored because they are not used by any `pub` functions: `IgnoredStructTwinRustAsync`
+// These types are ignored because they are not used by any `pub` functions: `IgnoredStructTwinRustAsync`, `StructWithOnlyIgnoredMethodTwinRustAsync`
 // These functions are ignored: `func_should_not_exist_in_dart_twin_rust_async`, `method_should_not_exist_in_dart_twin_rust_async`, `method_should_not_exist_in_dart_twin_rust_async`
 
 Future<void> handleCustomizedStructTwinRustAsync(
