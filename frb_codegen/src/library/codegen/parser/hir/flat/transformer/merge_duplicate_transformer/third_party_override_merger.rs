19566bd549eb679253e72de93c0c812fa8e91c73
fzyzcjy <ch271828n@outlook.com>
2024-06-06 13:57:25 +0800

    feat: impl

--- frb_codegen/src/library/codegen/parser/hir/flat/transformer/merge_duplicate_transformer/third_party_override_merger.rs
+++ frb_codegen/src/library/codegen/parser/hir/flat/transformer/merge_duplicate_transformer/third_party_override_merger.rs
@@ -1,4 +1,5 @@
 use crate::codegen::ir::hir::flat::function::HirFlatFunction;
+use crate::codegen::ir::hir::flat::source::HirFlatGenerationSource;
 use crate::codegen::ir::hir::flat::struct_or_enum::{HirFlatEnum, HirFlatStruct};
 use crate::codegen::ir::hir::misc::syn_item_struct_or_enum::SynItemStructOrEnum;
 use crate::codegen::misc::SELF_CRATE_THIRD_PARTY_NAMESPACE;
@@ -37,16 +38,12 @@ impl BaseMerger for ThirdPartyOverrideMerger {
 
 fn merge_core<T: Clone>(
     base: &T,
-    overrider_namespace: &Namespace,
+    overrider_source: &HirFlatGenerationSource,
     writer: impl Fn(&mut T),
 ) -> Option<T> {
-    is_module_third_party(overrider_namespace).then(|| {
+    (overrider_source == HirFlatGenerationSource::MoveFromCrateThirdPartyFolder).then(|| {
         let mut ans = base.to_owned();
         writer(&mut ans);
         ans
     })
-}
-
-fn is_module_third_party(namespace: &Namespace) -> bool {
-    SELF_CRATE_THIRD_PARTY_NAMESPACE.is_prefix_of(namespace)
 }
