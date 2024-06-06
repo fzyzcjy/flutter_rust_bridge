b8dcae7ac21d5e1a41da57c22870f14d81043c7a
fzyzcjy <ch271828n@outlook.com>
2024-06-06 14:11:56 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/hir/flat/parser/mod.rs
+++ frb_codegen/src/library/codegen/parser/hir/flat/parser/mod.rs
@@ -1,6 +1,4 @@
 pub(crate) mod existing_handler;
-pub(crate) mod flattener;
 pub(crate) mod mirror_ident;
-pub(crate) mod module_visibility_filter;
 pub(crate) mod pack;
 pub(crate) mod syn_item;
--- frb_codegen/src/library/codegen/parser/hir/flat/parser/pack.rs
+++ frb_codegen/src/library/codegen/parser/hir/flat/parser/pack.rs
@@ -1,21 +1,21 @@
 use crate::codegen::ir::hir::flat::pack::HirFlatPack;
-use crate::codegen::ir::hir::tree::pack::HirTreePack;
+use crate::codegen::ir::hir::naive_flat::pack::HirNaiveFlatPack;
 use crate::codegen::parser::hir::flat::parser::syn_item::parse_syn_item;
 use crate::codegen::parser::hir::internal_config::ParserHirInternalConfig;
 
 pub(crate) fn parse_pack(
     config: &ParserHirInternalConfig,
-    hir_tree: HirTreePack,
+    hir_naive_flat: HirNaiveFlatPack,
 ) -> anyhow::Result<HirFlatPack> {
-    let items = super::flattener::flatten(hir_tree)?;
-    let items = super::module_visibility_filter_transformer::filter_visible_modules(items);
-
     let mut pack = HirFlatPack {
-        existing_handler: super::existing_handler::parse_existing_handler(&items, config)?,
+        existing_handler: super::existing_handler::parse_existing_handler(
+            &hir_naive_flat.items,
+            config,
+        )?,
         ..HirFlatPack::default()
     };
 
-    for item in items {
+    for item in hir_naive_flat.items {
         parse_syn_item(item.item, &item.meta, &mut pack, config)?;
     }
 
