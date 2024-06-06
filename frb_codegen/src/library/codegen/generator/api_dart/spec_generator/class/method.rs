4a57419d0e21439d581ae0fd28a983a3c5dce908
fzyzcjy <ch271828n@outlook.com>
2024-06-06 21:02:14 +0800

    feat: change

--- frb_codegen/src/library/codegen/generator/api_dart/spec_generator/class/method.rs
+++ frb_codegen/src/library/codegen/generator/api_dart/spec_generator/class/method.rs
@@ -58,7 +58,7 @@ pub(crate) fn generate_api_methods(
     config: &GenerateApiMethodConfig,
     dart_class_name: &str,
 ) -> GeneratedApiMethods {
-    let methods = get_methods_of_enum_or_struct(generalized_class_name, &context.mir_pack.funcs_with_impl())
+    let methods = get_methods_of_enum_or_struct(generalized_class_name, &context.mir_pack.funcs_all)
         .iter()
         .filter_map(|func| generate_api_method(func, context, config, dart_class_name))
         .collect_vec();
