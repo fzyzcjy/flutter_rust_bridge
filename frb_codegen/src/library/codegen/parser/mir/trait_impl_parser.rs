3ab186a1cd63a82964ace143e5c1044a771cc21e
fzyzcjy <ch271828n@outlook.com>
2024-06-06 19:47:10 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/mir/trait_impl_parser.rs
+++ frb_codegen/src/library/codegen/parser/mir/trait_impl_parser.rs
@@ -1,14 +1,16 @@
+use crate::codegen::generator::codec::structs::CodecMode;
 use crate::codegen::ir::hir::flat::trait_impl::HirFlatTraitImpl;
 use crate::codegen::ir::mir::func::MirFuncOwnerInfo;
 use crate::codegen::ir::mir::trait_impl::MirTraitImpl;
+use crate::codegen::ir::mir::ty::rust_opaque::RustOpaqueCodecMode;
+use crate::codegen::ir::mir::ty::MirType;
 use crate::codegen::parser::mir::attribute_parser::FrbAttributes;
 use crate::codegen::parser::mir::type_parser::{TypeParser, TypeParserParsingContext};
+use crate::if_then_some;
 use crate::utils::crate_name::CrateName;
 use crate::utils::namespace::Namespace;
 use crate::utils::syn_utils::ty_to_string;
 use itertools::Itertools;
-use crate::codegen::generator::codec::structs::CodecMode;
-use crate::codegen::ir::mir::ty::rust_opaque::RustOpaqueCodecMode;
 
 pub(super) fn parse(
     hir_trait_impls: &[HirFlatTraitImpl],
@@ -24,12 +26,18 @@ pub(super) fn parse(
         owner: None,
     };
 
-    (hir_trait_impls.iter())
+    Ok((hir_trait_impls.iter())
         .map(|x| {
-            Ok(MirTraitImpl {
-                trait_ty: type_parser.parse_type(&syn::parse_str(&x.trait_name)?, &context)?,
-                impl_ty: type_parser.parse_type(&x.impl_ty, &context)?,
-            })
+            let trait_ty_raw = type_parser.parse_type(&syn::parse_str(&x.trait_name)?, &context)?;
+            let impl_ty = type_parser.parse_type(&x.impl_ty, &context)?;
+
+            Ok(
+                if_then_some!(let MirType::TraitDef(trait_ty) = trait_ty_raw, trait_ty)
+                    .map(|trait_ty| MirTraitImpl { trait_ty, impl_ty }),
+            )
         })
-        .collect()
+        .collect::<anyhow::Result<Vec<_>>>()?
+        .into_iter()
+        .filter_map(|x| x)
+        .collect_vec())
 }
