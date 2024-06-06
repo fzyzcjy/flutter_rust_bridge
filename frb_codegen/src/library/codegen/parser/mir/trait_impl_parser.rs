84d9756356e661649a27e9dd851fd6a45d4e7bdd
fzyzcjy <ch271828n@outlook.com>
2024-06-06 18:53:56 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/mir/trait_impl_parser.rs
+++ frb_codegen/src/library/codegen/parser/mir/trait_impl_parser.rs
@@ -1,6 +1,8 @@
 use crate::codegen::ir::hir::flat::trait_impl::HirFlatTraitImpl;
+use crate::codegen::ir::mir::func::MirFuncOwnerInfo;
 use crate::codegen::ir::mir::trait_impl::MirTraitImpl;
-use crate::codegen::parser::mir::type_parser::TypeParser;
+use crate::codegen::parser::mir::attribute_parser::FrbAttributes;
+use crate::codegen::parser::mir::type_parser::{TypeParser, TypeParserParsingContext};
 use crate::utils::syn_utils::ty_to_string;
 use itertools::Itertools;
 
@@ -8,11 +10,19 @@ pub(super) fn parse(
     hir_trait_impls: &[HirFlatTraitImpl],
     type_parser: &mut TypeParser,
 ) -> anyhow::Result<Vec<MirTraitImpl>> {
+    let context = TypeParserParsingContext {
+        initiated_namespace: TODO,
+        func_attributes: FrbAttributes::parse(&[])?,
+        default_stream_sink_codec: TODO,
+        default_rust_opaque_codec: TODO,
+        owner: TODO,
+    };
+
     (hir_trait_impls.iter())
         .map(|x| {
             Ok(MirTraitImpl {
-                trait_ty: type_parser.parse_type(syn::parse_str(&x.trait_name)?)?,
-                impl_ty: type_parser.parse_type(&x.impl_ty)?,
+                trait_ty: type_parser.parse_type(&syn::parse_str(&x.trait_name)?, &context)?,
+                impl_ty: type_parser.parse_type(&x.impl_ty, &context)?,
             })
         })
         .collect()
