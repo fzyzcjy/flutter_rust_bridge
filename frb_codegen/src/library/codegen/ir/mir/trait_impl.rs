8fc74cc6d3845d2177eb4da42853f9ceaea9fb84
fzyzcjy <ch271828n@outlook.com>
2024-06-06 19:40:46 +0800

    feat: more

--- frb_codegen/src/library/codegen/ir/mir/trait_impl.rs
+++ frb_codegen/src/library/codegen/ir/mir/trait_impl.rs
@@ -1,7 +1,8 @@
+use crate::codegen::ir::mir::ty::trait_def::MirTypeTraitDef;
 use crate::codegen::ir::mir::ty::MirType;
 
 #[derive(Clone, serde::Serialize, Debug)]
 pub struct MirTraitImpl {
-    pub(crate) trait_ty: MirType,
+    pub(crate) trait_ty: MirTypeTraitDef,
     pub(crate) impl_ty: MirType,
 }
