1d62c0e1cea7282541e1eb6d035381a3a849e861
fzyzcjy <ch271828n@outlook.com>
2024-06-06 21:35:43 +0800

    feat: try

--- frb_example/integrate_third_party/rust/src/third_party/web_audio_api/node/mod.rs
+++ frb_example/integrate_third_party/rust/src/third_party/web_audio_api/node/mod.rs
@@ -68,6 +68,7 @@ macro_rules! handle_audio_node_trait_impls {
     };
 }
 
+handle_audio_node_trait_impls!(AudioNode);
 handle_audio_node_trait_impls!(AnalyserNode);
 handle_audio_node_trait_impls!(AudioBufferSourceNode);
 handle_audio_node_trait_impls!(AudioDestinationNode);
