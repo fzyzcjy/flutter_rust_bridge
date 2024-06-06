62515fad7aa5139b1a6aa7429967586d6fe9347a
fzyzcjy <ch271828n@outlook.com>
2024-06-06 20:43:49 +0800

    feat: more

--- frb_codegen/src/library/codegen/ir/mir/func.rs
+++ frb_codegen/src/library/codegen/ir/mir/func.rs
@@ -193,6 +193,7 @@ pub(crate) fn compute_interest_name_of_owner_ty(owner_ty: &MirType) -> Option<Na
         MirType::RustAutoOpaqueImplicit(ty) => {
             NamespacedName::new(ty.self_namespace().unwrap(), ty.rust_api_type())
         }
+        MirType::TraitDef(ty) => ty.name.clone(),
         _ => return None,
     })
 }
