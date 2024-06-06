e11ee8bf38acc59282622d387235c3717e43492b
fzyzcjy <ch271828n@outlook.com>
2024-06-06 14:50:22 +0800

    chore: field

--- frb_codegen/src/library/codegen/ir/hir/naive_flat/item.rs
+++ frb_codegen/src/library/codegen/ir/hir/naive_flat/item.rs
@@ -1,6 +1,7 @@
 use crate::utils::namespace::Namespace;
 use derivative::Derivative;
 use serde::Serialize;
+use crate::codegen::ir::hir::flat::source::HirFlatGenerationSource;
 
 #[derive(Debug, Clone, serde::Serialize)]
 pub(crate) struct HirNaiveFlatItem {
@@ -12,5 +13,6 @@ pub(crate) struct HirNaiveFlatItem {
 #[derive(Clone, Debug, Derivative, Serialize)]
 pub(crate) struct HirNaiveFlatItemMeta {
     pub namespace: Namespace,
+    pub source: HirFlatGenerationSource,
     pub is_public: bool,
 }
