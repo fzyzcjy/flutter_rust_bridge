daed2ee2a9db0ffc504e694cd630e59725c79ed2
fzyzcjy <ch271828n@outlook.com>
2024-06-06 18:54:52 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/mir/trait_impl_parser.rs
+++ frb_codegen/src/library/codegen/parser/mir/trait_impl_parser.rs
@@ -3,19 +3,25 @@ use crate::codegen::ir::mir::func::MirFuncOwnerInfo;
 use crate::codegen::ir::mir::trait_impl::MirTraitImpl;
 use crate::codegen::parser::mir::attribute_parser::FrbAttributes;
 use crate::codegen::parser::mir::type_parser::{TypeParser, TypeParserParsingContext};
+use crate::utils::crate_name::CrateName;
+use crate::utils::namespace::Namespace;
 use crate::utils::syn_utils::ty_to_string;
 use itertools::Itertools;
+use crate::codegen::generator::codec::structs::CodecMode;
+use crate::codegen::ir::mir::ty::rust_opaque::RustOpaqueCodecMode;
 
 pub(super) fn parse(
     hir_trait_impls: &[HirFlatTraitImpl],
     type_parser: &mut TypeParser,
+    default_stream_sink_codec: CodecMode,
+    default_rust_opaque_codec: RustOpaqueCodecMode,
 ) -> anyhow::Result<Vec<MirTraitImpl>> {
     let context = TypeParserParsingContext {
-        initiated_namespace: TODO,
+        initiated_namespace: CrateName::self_crate().namespace(), // just a dummy value
         func_attributes: FrbAttributes::parse(&[])?,
-        default_stream_sink_codec: TODO,
-        default_rust_opaque_codec: TODO,
-        owner: TODO,
+        default_stream_sink_codec,
+        default_rust_opaque_codec,
+        owner: None,
     };
 
     (hir_trait_impls.iter())
