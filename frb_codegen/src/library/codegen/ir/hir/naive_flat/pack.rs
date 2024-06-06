728b477dbe6e749406bfb08dd91b1f11d5316906
fzyzcjy <ch271828n@outlook.com>
2024-06-06 14:09:30 +0800

    feat: more

--- frb_codegen/src/library/codegen/ir/hir/naive_flat/pack.rs
+++ frb_codegen/src/library/codegen/ir/hir/naive_flat/pack.rs
@@ -1,6 +1,6 @@
 use crate::codegen::ir::hir::misc::syn_item_with_meta::SynItemWithMeta;
 
-#[derive(Debug, Clone, Default, serde::Serialize)]
+#[derive(Debug, Clone, serde::Serialize)]
 pub(crate) struct HirNaiveFlatPack {
     pub items: Vec<SynItemWithMeta>,
 }
