272ec264d260ba36ed983e1200f8eecd9b64008c
fzyzcjy <ch271828n@outlook.com>
2024-06-07 09:42:39 +0800

    feat: parse

--- frb_codegen/src/library/codegen/parser/mir/type_parser/trait_object.rs
+++ frb_codegen/src/library/codegen/parser/mir/type_parser/trait_object.rs
@@ -1,12 +1,15 @@
 use crate::codegen::ir::mir::ty::MirType;
 use crate::codegen::parser::mir::type_parser::TypeParserWithContext;
 use syn::TypeTraitObject;
+use crate::utils::syn_utils::ty_to_string;
 
 impl<'a, 'b, 'c> TypeParserWithContext<'a, 'b, 'c> {
     pub(crate) fn parse_type_trait_object(
         &mut self,
         type_trait_object: &TypeTraitObject,
     ) -> anyhow::Result<MirType> {
-        TODO
+        if let [syn::TypeParamBound::Trait(trait_bound)] = &type_trait_object.bounds {
+        }
+        todo!()
     }
 }
