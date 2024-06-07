a1a40a4d1a1d66e1c7ff759f151eddd24d07efe0
fzyzcjy <ch271828n@outlook.com>
2024-06-07 08:50:13 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/hir/flat/parser/syn_item/item_trait.rs
+++ frb_codegen/src/library/codegen/parser/hir/flat/parser/syn_item/item_trait.rs
@@ -1,6 +1,7 @@
 use crate::codegen::ir::hir::flat::function::{HirFlatFunction, HirFlatFunctionOwner};
 use crate::codegen::ir::hir::flat::pack::HirFlatPack;
 use crate::codegen::ir::hir::flat::traits::HirFlatTrait;
+use crate::codegen::ir::hir::misc::generation_source::HirGenerationSource;
 use crate::codegen::ir::hir::misc::item_fn::GeneralizedItemFn;
 use crate::codegen::ir::hir::naive_flat::item::HirNaiveFlatItemMeta;
 use crate::if_then_some;
@@ -17,6 +18,7 @@ pub(crate) fn parse_syn_item_trait(
     let trait_name = NamespacedName::new(meta.namespace.clone(), item_trait.ident.to_string());
     target.traits.push(HirFlatTrait {
         name: trait_name.clone(),
+        source: HirGenerationSource::Normal,
     });
     (target.functions).extend(parse_functions(item_trait, meta, &trait_name));
 }
