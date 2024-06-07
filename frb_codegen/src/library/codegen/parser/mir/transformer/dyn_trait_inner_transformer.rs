d2e1c1b6a6f5e9b75fb059483729d64bf4e4271a
fzyzcjy <ch271828n@outlook.com>
2024-06-07 16:19:35 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/mir/transformer/dyn_trait_inner_transformer.rs
+++ frb_codegen/src/library/codegen/parser/mir/transformer/dyn_trait_inner_transformer.rs
@@ -1,11 +1,17 @@
+use crate::codegen::ir::mir::field::MirField;
+use crate::codegen::ir::mir::ident::MirIdent;
 use crate::codegen::ir::mir::pack::MirPack;
-use crate::codegen::ir::mir::ty::delegate::{MirTypeDelegate, MirTypeDelegateDynTrait};
+use crate::codegen::ir::mir::ty::delegate::{
+    MirTypeDelegate, MirTypeDelegateDynTrait, MirTypeDelegateRustAutoOpaqueExplicit,
+};
 use crate::codegen::ir::mir::ty::enumeration::{
     MirEnum, MirEnumIdent, MirEnumMode, MirEnumVariant, MirVariantKind,
 };
+use crate::codegen::ir::mir::ty::rust_opaque::MirTypeRustOpaque;
 use crate::codegen::ir::mir::ty::structure::MirStruct;
 use crate::codegen::ir::mir::ty::MirType;
 use crate::if_then_some;
+use crate::utils::namespace::NamespacedName;
 use itertools::Itertools;
 
 pub(crate) fn transform(mut pack: MirPack) -> anyhow::Result<MirPack> {
@@ -34,20 +40,20 @@ fn handle_ty_dyn_trait(
         .map(|item| item.impl_ty.clone())
         .collect_vec();
     let enum_name = ty_dyn_trait.inner_raw().ident;
-    let mir_enum = create_enum(&interest_impl_types, &enum_name);
+    let mir_enum = create_enum(&interest_impl_types, &enum_name.0);
 
     pack.enum_pool.insert(enum_name, mir_enum);
 
     Ok(())
 }
 
-fn create_enum(interest_impl_types: &[MirType], enum_name: &MirEnumIdent) -> MirEnum {
+fn create_enum(interest_impl_types: &[MirType], enum_name: &NamespacedName) -> MirEnum {
     let variants = (interest_impl_types.iter())
-        .map(|ty| create_enum_variant(ty))
+        .map(|ty| create_enum_variant(ty, enum_name))
         .collect_vec();
 
     MirEnum {
-        name: enum_name.0.clone(),
+        name: enum_name.clone(),
         wrapper_name: None,
         comments: vec![],
         variants,
@@ -56,15 +62,32 @@ fn create_enum(interest_impl_types: &[MirType], enum_name: &MirEnumIdent) -> Mir
     }
 }
 
-fn create_enum_variant(ty: &MirType) -> MirEnumVariant {
+fn create_enum_variant(interest_ty: &MirType, enum_name: &NamespacedName) -> MirEnumVariant {
+    let variant_name = TODO;
+
+    let field_ty = MirType::Delegate(MirTypeDelegate::RustAutoOpaqueExplicit(
+        MirTypeDelegateRustAutoOpaqueExplicit {
+            raw: TODO,
+            inner: TODO,
+        },
+    ));
+
     MirEnumVariant {
-        name: todo!(),
-        wrapper_name: todo!(),
+        name: MirIdent::new(variant_name.clone()),
+        wrapper_name: MirIdent::new(format!("{}_{}", enum_name.name, variant_name)),
         comments: vec![],
         kind: MirVariantKind::Struct(MirStruct {
-            name: todo!(),
+            name: TODO,
             wrapper_name: None,
-            fields: todo!(),
+            fields: vec![MirField {
+                ty: field_ty,
+                name: MirIdent::new("value".to_string()),
+                is_final: false,
+                is_rust_public: None,
+                comments: vec![],
+                default: None,
+                settings: Default::default(),
+            }],
             is_fields_named: false,
             dart_metadata: vec![],
             ignore: false,
