62d73a0d90794a022a9ad1ebc0af88fddf30f7e1
fzyzcjy <ch271828n@outlook.com>
2024-06-07 09:44:48 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/mir/type_parser/trait_object.rs
+++ frb_codegen/src/library/codegen/parser/mir/type_parser/trait_object.rs
@@ -1,15 +1,33 @@
 use crate::codegen::ir::mir::ty::MirType;
 use crate::codegen::parser::mir::type_parser::TypeParserWithContext;
-use syn::TypeTraitObject;
 use crate::utils::syn_utils::ty_to_string;
+use syn::TypeTraitObject;
 
 impl<'a, 'b, 'c> TypeParserWithContext<'a, 'b, 'c> {
     pub(crate) fn parse_type_trait_object(
         &mut self,
         type_trait_object: &TypeTraitObject,
     ) -> anyhow::Result<MirType> {
-        if let [syn::TypeParamBound::Trait(trait_bound)] = &type_trait_object.bounds {
+        if let Some(out) = self.parse_type_trait_object_inner(type_trait_object)? {
+            return Ok(out);
         }
+        
+        // fallback
+        self.parse_type_rust_auto_opaque_implicit(
+            None,
+            &syn::Type::TraitObject(type_trait_object.clone()),
+            None,
+            None,
+        )
+    }
+
+    fn parse_type_trait_object_inner(
+        &mut self,
+        type_trait_object: &TypeTraitObject,
+    ) -> anyhow::Result<Option<MirType>> {
+        let bounds = &type_trait_object.bounds;
+        if bounds.len() != 1 {}
+        if let [syn::TypeParamBound::Trait(trait_bound)] = &bounds {}
         todo!()
     }
 }
