9d1d3f1f47031a19a93dee824419798ad7342cce
fzyzcjy <ch271828n@outlook.com>
2024-06-07 18:32:26 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/hir/flat/transformer/generate_trait_impl_enum_transformer.rs
+++ frb_codegen/src/library/codegen/parser/hir/flat/transformer/generate_trait_impl_enum_transformer.rs
@@ -3,19 +3,30 @@ use crate::codegen::ir::hir::flat::traits::HirFlatTrait;
 use crate::codegen::ir::mir::trait_impl::MirTraitImpl;
 use crate::codegen::parser::mir::parser::attribute::FrbAttributes;
 use crate::codegen::parser::mir::parser::tentative_parse_trait_impls;
+use itertools::Itertools;
 
 pub(crate) fn transform(mut pack: HirFlatPack) -> anyhow::Result<HirFlatPack> {
     let trait_impls = tentative_parse_trait_impls(&pack);
 
-    for hir_trait in &pack.traits {
-        if FrbAttributes::parse(&hir_trait.attrs)?.generate_impl_enum() {
-            generate_trait_impl_enum(hir_trait, &trait_impls);
-        }
-    }
+    let extra_code = (pack.traits.iter())
+        .filter(|x| FrbAttributes::parse(&x.attrs).unwrap().generate_impl_enum())
+        .map(|x| generate_trait_impl_enum(x, &trait_impls))
+        .collect::<anyhow::Result<Vec<_>>>()?
+        .into_iter()
+        .join("");
+
+    inject_extra_code(&mut pack, &extra_code);
 
     Ok(pack)
 }
 
-fn generate_trait_impl_enum(hir_trait: &HirFlatTrait, trait_impls: &[MirTraitImpl]) {
+fn generate_trait_impl_enum(
+    hir_trait: &HirFlatTrait,
+    trait_impls: &[MirTraitImpl],
+) -> anyhow::Result<String> {
+    Ok(TODO)
+}
+
+fn inject_extra_code(pack: &mut HirFlatPack, extra_code: &str) {
     TODO;
 }
