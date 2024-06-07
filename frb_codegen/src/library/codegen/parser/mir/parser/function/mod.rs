ff6a972db76d6413dc18af0a194547f9ad1fcf6e
fzyzcjy <ch271828n@outlook.com>
2024-06-07 10:16:15 +0800

    refactor: mv

--- frb_codegen/src/library/codegen/parser/mir/parser/function/mod.rs
+++ frb_codegen/src/library/codegen/parser/mir/parser/function/mod.rs
@@ -1,1 +1,2 @@
 pub(crate) mod real;
+pub(crate) mod auto_accessor;
--- frb_codegen/src/library/codegen/parser/mir/parser/mod.rs
+++ frb_codegen/src/library/codegen/parser/mir/parser/mod.rs
@@ -1,7 +1,5 @@
 pub(crate) mod attribute;
-pub(crate) mod auto_accessor;
 pub(crate) mod function;
 pub(crate) mod misc;
 pub(crate) mod trait_impl;
 pub(crate) mod ty;
-mod function;
