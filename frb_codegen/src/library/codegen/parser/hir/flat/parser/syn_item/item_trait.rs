25ae3c80bc7781e0762b31f05744a32580883177
fzyzcjy <ch271828n@outlook.com>
2024-06-07 08:50:39 +0800

    fix: err

--- frb_codegen/src/library/codegen/parser/hir/flat/parser/syn_item/item_trait.rs
+++ frb_codegen/src/library/codegen/parser/hir/flat/parser/syn_item/item_trait.rs
@@ -18,7 +18,7 @@ pub(crate) fn parse_syn_item_trait(
     let trait_name = NamespacedName::new(meta.namespace.clone(), item_trait.ident.to_string());
     target.traits.push(HirFlatTrait {
         name: trait_name.clone(),
-        source: HirGenerationSource::Normal,
+        source: meta.source,
     });
     (target.functions).extend(parse_functions(item_trait, meta, &trait_name));
 }
