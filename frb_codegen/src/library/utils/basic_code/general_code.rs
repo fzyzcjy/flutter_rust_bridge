fe0872bbd8a4e334db3263fc458a50d31ca4a54e
fzyzcjy <ch271828n@outlook.com>
2024-06-08 09:40:27 +0800

    feat: more

--- frb_codegen/src/library/utils/basic_code/general_code.rs
+++ frb_codegen/src/library/utils/basic_code/general_code.rs
@@ -1,5 +1,7 @@
-use serde::Serialize;
+use crate::simple_code_trait_impl;
 use crate::utils::basic_code::dart_header_code::DartHeaderCode;
+use serde::Serialize;
+use std::ops::AddAssign;
 
 #[derive(Clone, Debug, Serialize)]
 pub(crate) enum GeneralCode {
@@ -17,3 +19,21 @@ pub(crate) struct GeneralDartCode {
 pub(crate) struct GeneralRustCode {
     pub body: String,
 }
+
+impl AddAssign for GeneralDartCode {
+    #[inline]
+    fn add_assign(&mut self, rhs: Self) {
+        self.header += rhs.header;
+        self.body += &rhs.body;
+    }
+}
+
+impl AddAssign for GeneralRustCode {
+    #[inline]
+    fn add_assign(&mut self, rhs: Self) {
+        self.body += &rhs.body;
+    }
+}
+
+simple_code_trait_impl!(GeneralDartCode);
+simple_code_trait_impl!(GeneralRustCode);
