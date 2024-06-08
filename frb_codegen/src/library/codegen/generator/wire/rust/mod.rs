3dab971abd4d3ddf3f24584192c026fc422169b1
fzyzcjy <ch271828n@outlook.com>
2024-06-08 09:48:40 +0800

    feat: more

--- frb_codegen/src/library/codegen/generator/wire/rust/mod.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/mod.rs
@@ -4,6 +4,7 @@ use crate::codegen::generator::misc::path_texts::PathTexts;
 use crate::codegen::generator::wire::rust::spec_generator::base::WireRustGeneratorContext;
 use crate::codegen::generator::wire::rust::spec_generator::extern_func::ExternFunc;
 use crate::codegen::ir::mir::pack::MirPackComputedCache;
+use crate::utils::basic_code::general_code::GeneralCode;
 
 pub(crate) mod internal_config;
 pub(crate) mod spec_generator;
@@ -32,7 +33,10 @@ pub(crate) fn generate(
     )?;
 
     Ok(GeneratorWireRustOutput {
-        output_texts: PathTexts::new_from_targets(&context.config.rust_output_path, &text.text),
+        output_texts: PathTexts::new_from_targets(
+            &context.config.rust_output_path,
+            &(text.text.clone()).map(|x, _| x.map(|x| GeneralCode::new_rust(x))),
+        ),
         extern_funcs: text.extern_funcs,
         content_hash: spec.misc.content_hash,
         extern_struct_names: spec.extern_struct_names,
