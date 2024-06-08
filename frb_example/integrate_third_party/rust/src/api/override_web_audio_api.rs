2e37ef9f6f6b37d09e6e334c5510cacf953b7b22
fzyzcjy <ch271828n@outlook.com>
2024-06-08 15:09:47 +0800

    chore: rm

--- frb_example/integrate_third_party/rust/src/api/override_web_audio_api.rs
+++ frb_example/integrate_third_party/rust/src/api/override_web_audio_api.rs
@@ -15,11 +15,6 @@ pub impl AudioContext {
         self.decode_audio_data_sync(input)
             .map_err(|e| anyhow::anyhow!("{:?}", e))
     }
-
-    // TODO only to test overriding an existing function, not to be really used (and should be reverted later)!
-    fn frb_override_output_latency(&self) -> String {
-        "hello this is dummy override".to_owned()
-    }
 }
 
 macro_rules! handle_audio_node_trait_impls_override {
