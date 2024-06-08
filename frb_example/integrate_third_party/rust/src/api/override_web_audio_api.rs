d9d726bed595242c1ab1fc33905041b5417402ce
fzyzcjy <ch271828n@outlook.com>
2024-06-08 14:29:08 +0800

    feat: more

--- frb_example/integrate_third_party/rust/src/api/override_web_audio_api.rs
+++ frb_example/integrate_third_party/rust/src/api/override_web_audio_api.rs
@@ -1,7 +1,8 @@
 use extend::ext;
 use flutter_rust_bridge::for_generated::anyhow;
 use web_audio_api::context::{AudioContext, BaseAudioContext};
-use web_audio_api::AudioBuffer;
+use web_audio_api::node::*;
+use web_audio_api::{AudioBuffer, AudioParam};
 
 #[ext]
 pub impl AudioContext {
@@ -19,3 +20,35 @@ pub impl AudioContext {
         "hello this is dummy override".to_owned()
     }
 }
+
+macro_rules! handle_audio_node_trait_impls_override {
+    ($name:ident) => {
+        #[ext]
+        impl $name {
+            // TODO
+        }
+    };
+}
+
+handle_audio_node_trait_impls_override!(AudioParam);
+handle_audio_node_trait_impls_override!(AnalyserNode);
+handle_audio_node_trait_impls_override!(AudioBufferSourceNode);
+handle_audio_node_trait_impls_override!(AudioDestinationNode);
+handle_audio_node_trait_impls_override!(BiquadFilterNode);
+handle_audio_node_trait_impls_override!(ChannelMergerNode);
+handle_audio_node_trait_impls_override!(ChannelSplitterNode);
+handle_audio_node_trait_impls_override!(ConstantSourceNode);
+handle_audio_node_trait_impls_override!(ConvolverNode);
+handle_audio_node_trait_impls_override!(DelayNode);
+handle_audio_node_trait_impls_override!(DynamicsCompressorNode);
+handle_audio_node_trait_impls_override!(GainNode);
+handle_audio_node_trait_impls_override!(IIRFilterNode);
+handle_audio_node_trait_impls_override!(MediaElementAudioSourceNode);
+handle_audio_node_trait_impls_override!(MediaStreamAudioDestinationNode);
+handle_audio_node_trait_impls_override!(MediaStreamAudioSourceNode);
+handle_audio_node_trait_impls_override!(MediaStreamTrackAudioSourceNode);
+handle_audio_node_trait_impls_override!(OscillatorNode);
+handle_audio_node_trait_impls_override!(PannerNode);
+handle_audio_node_trait_impls_override!(ScriptProcessorNode);
+handle_audio_node_trait_impls_override!(StereoPannerNode);
+handle_audio_node_trait_impls_override!(WaveShaperNode);
