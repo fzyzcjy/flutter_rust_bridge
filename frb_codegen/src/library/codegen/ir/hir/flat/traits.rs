8f0eae59fe087d6ad6de0fb021ca7c89afb0e18d
fzyzcjy <ch271828n@outlook.com>
2024-06-08 15:20:53 +0800

    chore: lint

--- frb_codegen/src/library/codegen/ir/hir/flat/traits.rs
+++ frb_codegen/src/library/codegen/ir/hir/flat/traits.rs
@@ -1,6 +1,6 @@
-use crate::codegen::ir::hir::misc::serializers::serialize_vec_syn;
 use crate::codegen::ir::hir::flat::component::HirFlatComponent;
 use crate::codegen::ir::hir::misc::generation_source::HirGenerationSource;
+use crate::codegen::ir::hir::misc::serializers::serialize_vec_syn;
 use crate::utils::namespace::NamespacedName;
 
 #[derive(Clone, serde::Serialize, Debug)]
