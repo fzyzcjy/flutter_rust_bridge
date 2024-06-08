6af02aa96c7285a181fa6d7cb7e8b20d25a0419d
fzyzcjy <ch271828n@outlook.com>
2024-06-08 20:32:09 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/early_generator/proxy_enum.rs
+++ frb_codegen/src/library/codegen/parser/early_generator/proxy_enum.rs
@@ -1,11 +1,13 @@
+use crate::codegen::generator::api_dart::spec_generator::base::ApiDartGenerator;
+use crate::codegen::generator::api_dart::spec_generator::class::proxy_variant;
 use crate::codegen::ir::hir::flat::pack::HirFlatPack;
 use crate::codegen::ir::mir::pack::MirPack;
 use crate::codegen::ir::mir::ty::delegate::{MirTypeDelegate, MirTypeDelegateProxyVariant};
 use crate::codegen::ir::mir::ty::MirType;
 use crate::if_then_some;
+use crate::library::codegen::generator::api_dart::spec_generator::info::ApiDartGeneratorInfoTrait;
 use crate::library::codegen::ir::mir::ty::MirTypeTrait;
 use itertools::Itertools;
-use crate::codegen::generator::api_dart::spec_generator::class::proxy_variant;
 
 pub(crate) fn generate(pack: &HirFlatPack, tentative_mir_pack: &MirPack) -> anyhow::Result<String> {
     let distinct_types = tentative_mir_pack.distinct_types(None);
@@ -16,19 +18,25 @@ pub(crate) fn generate(pack: &HirFlatPack, tentative_mir_pack: &MirPack) -> anyh
     let proxy_variants_of_enum =
         (proxy_variants.into_iter()).into_group_map_by(|ty| ty.proxy_enum.safe_ident());
 
-    (proxy_variants_of_enum.into_iter())
+    Ok((proxy_variants_of_enum.into_iter())
         .map(|(_, proxy_variants)| generate_proxy_enum(&proxy_variants))
-        .join("")
+        .join(""))
 }
 
 fn generate_proxy_enum(proxy_variants: &[MirTypeDelegateProxyVariant]) -> String {
-    let proxy_enum = *proxy_variants[0].proxy_enum.clone();
+    let proxy_enum_ty = *proxy_variants[0].proxy_enum.clone();
+    let proxy_enum_dart_api_type =
+        ApiDartGenerator::new(proxy_enum_ty.clone(), context).dart_api_type();
 
-    let enum_name = format!("{}ProxyEnum");
+    let enum_name = format!("{proxy_enum_dart_api_type}ProxyEnum");
 
-    format!("
-    enum {TODO} {{
+    let variants = ""; // TODO
+
+    format!(
+        "
+    enum {enum_name} {{
         {variants}
     }}
-    ")
+    "
+    )
 }
