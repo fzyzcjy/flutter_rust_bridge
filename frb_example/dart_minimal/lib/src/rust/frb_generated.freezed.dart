2960a65d742fe5d4ebc04b7a0e5fdf2e43f767ee
fzyzcjy <ch271828n@outlook.com>
2024-06-08 10:04:10 +0800

    chore: codegen

---
+++ frb_example/dart_minimal/lib/src/rust/frb_generated.freezed.dart
@@ -0,0 +1,231 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'frb_generated.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+mixin _$SimpleTraitTwinNormalImpl {
+  int get field0 => throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function(int field0) hello,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function(int field0)? hello,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function(int field0)? hello,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(SimpleTraitTwinNormalImpl_Hello value) hello,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(SimpleTraitTwinNormalImpl_Hello value)? hello,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(SimpleTraitTwinNormalImpl_Hello value)? hello,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $SimpleTraitTwinNormalImplCopyWith<SimpleTraitTwinNormalImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $SimpleTraitTwinNormalImplCopyWith<$Res> {
+  factory $SimpleTraitTwinNormalImplCopyWith(SimpleTraitTwinNormalImpl value,
+          $Res Function(SimpleTraitTwinNormalImpl) then) =
+      _$SimpleTraitTwinNormalImplCopyWithImpl<$Res, SimpleTraitTwinNormalImpl>;
+  @useResult
+  $Res call({int field0});
+}
+
+/// @nodoc
+class _$SimpleTraitTwinNormalImplCopyWithImpl<$Res,
+        $Val extends SimpleTraitTwinNormalImpl>
+    implements $SimpleTraitTwinNormalImplCopyWith<$Res> {
+  _$SimpleTraitTwinNormalImplCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? field0 = null,
+  }) {
+    return _then(_value.copyWith(
+      field0: null == field0
+          ? _value.field0
+          : field0 // ignore: cast_nullable_to_non_nullable
+              as int,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$SimpleTraitTwinNormalImpl_HelloImplCopyWith<$Res>
+    implements $SimpleTraitTwinNormalImplCopyWith<$Res> {
+  factory _$$SimpleTraitTwinNormalImpl_HelloImplCopyWith(
+          _$SimpleTraitTwinNormalImpl_HelloImpl value,
+          $Res Function(_$SimpleTraitTwinNormalImpl_HelloImpl) then) =
+      __$$SimpleTraitTwinNormalImpl_HelloImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({int field0});
+}
+
+/// @nodoc
+class __$$SimpleTraitTwinNormalImpl_HelloImplCopyWithImpl<$Res>
+    extends _$SimpleTraitTwinNormalImplCopyWithImpl<$Res,
+        _$SimpleTraitTwinNormalImpl_HelloImpl>
+    implements _$$SimpleTraitTwinNormalImpl_HelloImplCopyWith<$Res> {
+  __$$SimpleTraitTwinNormalImpl_HelloImplCopyWithImpl(
+      _$SimpleTraitTwinNormalImpl_HelloImpl _value,
+      $Res Function(_$SimpleTraitTwinNormalImpl_HelloImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? field0 = null,
+  }) {
+    return _then(_$SimpleTraitTwinNormalImpl_HelloImpl(
+      null == field0
+          ? _value.field0
+          : field0 // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$SimpleTraitTwinNormalImpl_HelloImpl
+    extends SimpleTraitTwinNormalImpl_Hello {
+  const _$SimpleTraitTwinNormalImpl_HelloImpl(this.field0) : super._();
+
+  @override
+  final int field0;
+
+  @override
+  String toString() {
+    return 'SimpleTraitTwinNormalImpl.hello(field0: $field0)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$SimpleTraitTwinNormalImpl_HelloImpl &&
+            (identical(other.field0, field0) || other.field0 == field0));
+  }
+
+  @override
+  int get hashCode => Object.hash(runtimeType, field0);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$SimpleTraitTwinNormalImpl_HelloImplCopyWith<
+          _$SimpleTraitTwinNormalImpl_HelloImpl>
+      get copyWith => __$$SimpleTraitTwinNormalImpl_HelloImplCopyWithImpl<
+          _$SimpleTraitTwinNormalImpl_HelloImpl>(this, _$identity);
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function(int field0) hello,
+  }) {
+    return hello(field0);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function(int field0)? hello,
+  }) {
+    return hello?.call(field0);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function(int field0)? hello,
+    required TResult orElse(),
+  }) {
+    if (hello != null) {
+      return hello(field0);
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(SimpleTraitTwinNormalImpl_Hello value) hello,
+  }) {
+    return hello(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(SimpleTraitTwinNormalImpl_Hello value)? hello,
+  }) {
+    return hello?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(SimpleTraitTwinNormalImpl_Hello value)? hello,
+    required TResult orElse(),
+  }) {
+    if (hello != null) {
+      return hello(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class SimpleTraitTwinNormalImpl_Hello
+    extends SimpleTraitTwinNormalImpl {
+  const factory SimpleTraitTwinNormalImpl_Hello(final int field0) =
+      _$SimpleTraitTwinNormalImpl_HelloImpl;
+  const SimpleTraitTwinNormalImpl_Hello._() : super._();
+
+  @override
+  int get field0;
+  @override
+  @JsonKey(ignore: true)
+  _$$SimpleTraitTwinNormalImpl_HelloImplCopyWith<
+          _$SimpleTraitTwinNormalImpl_HelloImpl>
+      get copyWith => throw _privateConstructorUsedError;
+}
