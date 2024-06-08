8e13f6281e7b7b07b761a3bbdaf0914701e96578
fzyzcjy <ch271828n@outlook.com>
2024-06-08 14:05:44 +0800

    feat: demo

--- frb_example/dart_minimal/rust/src/api/minimal.rs
+++ frb_example/dart_minimal/rust/src/api/minimal.rs
@@ -1,4 +1,3 @@
-use crate::frb_generated::SimpleTraitForDynTwinNormalImpl;
 use flutter_rust_bridge::frb;
 
 #[frb(init)]
@@ -8,38 +7,4 @@ pub fn init_app() {
 
 pub fn minimal_adder(a: i32, b: i32) -> i32 {
     a + b
-}
-
-// TODO temp demo
-
-#[frb(generate_impl_enum)]
-pub trait SimpleTraitForDynTwinNormal {
-    fn simple_method_twin_normal(&self) -> i32;
-}
-
-#[frb(opaque)]
-pub struct StructOneWithTraitForDynTwinNormal {
-    pub one: i32,
-}
-
-impl SimpleTraitForDynTwinNormal for StructOneWithTraitForDynTwinNormal {
-    fn simple_method_twin_normal(&self) -> i32 {
-        self.one
-    }
-}
-
-#[frb(opaque)]
-pub struct StructTwoWithTraitForDynTwinNormal {
-    pub two: i32,
-}
-
-impl SimpleTraitForDynTwinNormal for StructTwoWithTraitForDynTwinNormal {
-    fn simple_method_twin_normal(&self) -> i32 {
-        self.two * 2
-    }
-}
-
-pub fn func_arg_trait_impl_twin_normal(arg: SimpleTraitForDynTwinNormalImpl) -> i32 {
-    let arg = arg.blocking_read();
-    arg.simple_method_twin_normal()
 }
--- frb_example/pure_dart/rust/src/api/impl_trait.rs
+++ frb_example/pure_dart/rust/src/api/impl_trait.rs
@@ -1,3 +1,4 @@
+use crate::frb_generated::SimpleTraitForDynTwinNormalImpl;
 use flutter_rust_bridge::frb;
 
 pub trait SimpleTraitTwinNormal {
@@ -38,4 +39,36 @@ impl SimpleTraitTwinNormal for StructTwoWithTraitTwinNormal {
     fn simple_trait_fn_receiver_borrow_twin_normal(&self) -> i32 {
         self.two * 2
     }
+}
+
+#[frb(generate_impl_enum)]
+pub trait SimpleTraitForDynTwinNormal {
+    fn simple_method_twin_normal(&self) -> i32;
+}
+
+#[frb(opaque)]
+pub struct StructOneWithTraitForDynTwinNormal {
+    pub one: i32,
+}
+
+impl SimpleTraitForDynTwinNormal for StructOneWithTraitForDynTwinNormal {
+    fn simple_method_twin_normal(&self) -> i32 {
+        self.one
+    }
+}
+
+#[frb(opaque)]
+pub struct StructTwoWithTraitForDynTwinNormal {
+    pub two: i32,
+}
+
+impl SimpleTraitForDynTwinNormal for StructTwoWithTraitForDynTwinNormal {
+    fn simple_method_twin_normal(&self) -> i32 {
+        self.two * 2
+    }
+}
+
+pub fn func_arg_trait_impl_twin_normal(arg: SimpleTraitForDynTwinNormalImpl) -> i32 {
+    let arg = arg.blocking_read();
+    arg.simple_method_twin_normal()
 }
