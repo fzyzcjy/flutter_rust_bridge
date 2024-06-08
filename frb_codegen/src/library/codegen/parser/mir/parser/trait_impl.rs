3ef02f65e3b613a9a28f7d33fd6a89c494c4361d
fzyzcjy <ch271828n@outlook.com>
2024-06-08 14:59:19 +0800

    feat: should_ignore

--- frb_codegen/src/library/codegen/parser/mir/parser/trait_impl.rs
+++ frb_codegen/src/library/codegen/parser/mir/parser/trait_impl.rs
@@ -26,13 +26,14 @@ pub(crate) fn parse(
         .map(|x| {
             let trait_ty = parse_type_trait(&x.trait_name, type_parser);
             let impl_ty = type_parser.parse_type(&x.impl_ty, &context).ok();
-            Ok(
-                if let (Some(trait_ty), Some(impl_ty)) = (trait_ty, impl_ty) {
-                    Some(MirTraitImpl { trait_ty, impl_ty })
-                } else {
-                    None
-                },
-            )
+
+            if let (Some(trait_ty), Some(impl_ty)) = (trait_ty, impl_ty) {
+                if !impl_ty.should_ignore(context) {
+                    return Ok(Some(MirTraitImpl { trait_ty, impl_ty }));
+                }
+            }
+
+            None
         })
         .collect::<anyhow::Result<Vec<_>>>()?
         .into_iter()
