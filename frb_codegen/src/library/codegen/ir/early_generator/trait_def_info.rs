4c8c4b0850589eb5c50a5868aecd21c1d8a35d0c
fzyzcjy <ch271828n@outlook.com>
2024-06-09 15:45:25 +0800

    feat: more

--- frb_codegen/src/library/codegen/ir/early_generator/trait_def_info.rs
+++ frb_codegen/src/library/codegen/ir/early_generator/trait_def_info.rs
@@ -1,10 +1,10 @@
 use crate::codegen::ir::mir::ty::delegate::MirTypeDelegateProxyVariant;
 use crate::codegen::ir::mir::ty::MirType;
-use crate::utils::namespace::Namespace;
+use crate::utils::namespace::{Namespace, NamespacedName};
 
 #[derive(Debug, Clone, serde::Serialize, PartialEq, Eq, Hash)]
 pub(crate) struct IrEarlyGeneratorTraitDefInfo {
-    pub original_ty: MirType,
+    pub trait_def_name: NamespacedName,
     pub delegate_namespace: Namespace,
     pub variants: Vec<TODO>,
 }
--- frb_codegen/src/library/codegen/parser/mir/parser/ty/trait_object.rs
+++ frb_codegen/src/library/codegen/parser/mir/parser/ty/trait_object.rs
@@ -31,7 +31,9 @@ impl<'a, 'b, 'c> TypeParserWithContext<'a, 'b, 'c> {
         if let Some(trait_name_path) = extract_trait_name_path(type_trait_object) {
             let trait_name = ty_to_string(&trait_name_path.segments.last().unwrap());
             if let Some(trait_ty) = parse_type_trait(&trait_name, self.inner) {
-                if let Some(trait_def_info) = (self.inner.trait_def_infos.iter()).find(TODO) {
+                if let Some(trait_def_info) = (self.inner.trait_def_infos.iter())
+                    .find(|info| info.trait_def_name == trait_ty.name)
+                {
                     return Ok(Some(MirType::Delegate(MirTypeDelegate::DynTrait(
                         MirTypeDelegateDynTrait {
                             trait_def_name: trait_ty.name,
