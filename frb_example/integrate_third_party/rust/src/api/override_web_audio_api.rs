800c2c4f988d92ca656d9e60e9b21cc684e4426e
fzyzcjy <ch271828n@outlook.com>
2024-06-09 14:13:21 +0800

    chorE: more

--- frb_example/integrate_third_party/rust/src/api/override_web_audio_api.rs
+++ frb_example/integrate_third_party/rust/src/api/override_web_audio_api.rs
@@ -17,37 +17,40 @@ pub impl AudioContext {
     }
 }
 
-// macro_rules! handle_audio_node_trait_impls_override {
-//     ($name:ident) => {
-//         #[ext]
-//         pub impl $name {
-//             fn frb_override_connect(&self, dest: AudioNodeImplementor) {
-//                 let dest = dest.blocking_read();
-//                 self.connect(&*dest);
-//             }
-//         }
-//     };
-// }
-//
-// handle_audio_node_trait_impls_override!(AudioParam);
-// handle_audio_node_trait_impls_override!(AnalyserNode);
-// handle_audio_node_trait_impls_override!(AudioBufferSourceNode);
-// handle_audio_node_trait_impls_override!(AudioDestinationNode);
-// handle_audio_node_trait_impls_override!(BiquadFilterNode);
-// handle_audio_node_trait_impls_override!(ChannelMergerNode);
-// handle_audio_node_trait_impls_override!(ChannelSplitterNode);
-// handle_audio_node_trait_impls_override!(ConstantSourceNode);
-// handle_audio_node_trait_impls_override!(ConvolverNode);
-// handle_audio_node_trait_impls_override!(DelayNode);
-// handle_audio_node_trait_impls_override!(DynamicsCompressorNode);
-// handle_audio_node_trait_impls_override!(GainNode);
-// handle_audio_node_trait_impls_override!(IIRFilterNode);
-// handle_audio_node_trait_impls_override!(MediaElementAudioSourceNode);
-// handle_audio_node_trait_impls_override!(MediaStreamAudioDestinationNode);
-// handle_audio_node_trait_impls_override!(MediaStreamAudioSourceNode);
-// handle_audio_node_trait_impls_override!(MediaStreamTrackAudioSourceNode);
-// handle_audio_node_trait_impls_override!(OscillatorNode);
-// handle_audio_node_trait_impls_override!(PannerNode);
-// handle_audio_node_trait_impls_override!(ScriptProcessorNode);
-// handle_audio_node_trait_impls_override!(StereoPannerNode);
-// handle_audio_node_trait_impls_override!(WaveShaperNode);
+macro_rules! handle_audio_node_trait_impls_override {
+    ($name:ident) => {
+        #[ext]
+        pub impl $name {
+            // fn frb_override_connect(&self, dest: AudioNodeImplementor) {
+            //     let dest = dest.blocking_read();
+            //     self.connect(&*dest);
+            // }
+            fn frb_override_connect(&self) {
+                todo!()
+            }
+        }
+    };
+}
+
+handle_audio_node_trait_impls_override!(AudioParam);
+handle_audio_node_trait_impls_override!(AnalyserNode);
+handle_audio_node_trait_impls_override!(AudioBufferSourceNode);
+handle_audio_node_trait_impls_override!(AudioDestinationNode);
+handle_audio_node_trait_impls_override!(BiquadFilterNode);
+handle_audio_node_trait_impls_override!(ChannelMergerNode);
+handle_audio_node_trait_impls_override!(ChannelSplitterNode);
+handle_audio_node_trait_impls_override!(ConstantSourceNode);
+handle_audio_node_trait_impls_override!(ConvolverNode);
+handle_audio_node_trait_impls_override!(DelayNode);
+handle_audio_node_trait_impls_override!(DynamicsCompressorNode);
+handle_audio_node_trait_impls_override!(GainNode);
+handle_audio_node_trait_impls_override!(IIRFilterNode);
+handle_audio_node_trait_impls_override!(MediaElementAudioSourceNode);
+handle_audio_node_trait_impls_override!(MediaStreamAudioDestinationNode);
+handle_audio_node_trait_impls_override!(MediaStreamAudioSourceNode);
+handle_audio_node_trait_impls_override!(MediaStreamTrackAudioSourceNode);
+handle_audio_node_trait_impls_override!(OscillatorNode);
+handle_audio_node_trait_impls_override!(PannerNode);
+handle_audio_node_trait_impls_override!(ScriptProcessorNode);
+handle_audio_node_trait_impls_override!(StereoPannerNode);
+handle_audio_node_trait_impls_override!(WaveShaperNode);
