9493fd5dd9a2866c6049b63e603c0a8161ff27f9
fzyzcjy <ch271828n@outlook.com>
2024-06-09 10:00:50 +0800

    feat: more

--- frb_rust/src/lockable/order_info.rs
+++ frb_rust/src/lockable/order_info.rs
@@ -1,7 +1,18 @@
+use crate::lockable::base::Lockable;
 use crate::lockable::order::LockableOrder;
 
 pub struct LockableOrderInfo {
-    pub index: usize,
-    pub mutable: bool,
-    pub object_order: LockableOrder,
+    pub(crate) object_order: LockableOrder,
+    pub(crate) index: usize,
+    pub(crate) mutable: bool,
+}
+
+impl LockableOrderInfo {
+    pub fn new(object: &impl Lockable, index: usize, mutable: bool) -> Self {
+        Self {
+            object_order: object.lockable_order(),
+            index,
+            mutable,
+        }
+    }
 }
