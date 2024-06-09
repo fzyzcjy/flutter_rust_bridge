6cbfa056f9c1e12fca40d55d309c22934adf5192
fzyzcjy <ch271828n@outlook.com>
2024-06-09 08:55:22 +0800

    feat: try

--- frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/misc/ty/delegate.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/misc/ty/delegate.rs
@@ -1,6 +1,7 @@
 use crate::codegen::generator::wire::rust::spec_generator::base::*;
 use crate::codegen::generator::wire::rust::spec_generator::misc::ty::WireRustGeneratorMiscTrait;
 use crate::codegen::ir::mir::ty::delegate::MirTypeDelegate;
+use crate::library::codegen::ir::mir::ty::MirTypeTrait;
 
 impl<'a> WireRustGeneratorMiscTrait for DelegateWireRustGenerator<'a> {
     // the function signature is not covered while the whole body is covered - looks like a bug in coverage tool
@@ -9,6 +10,14 @@ impl<'a> WireRustGeneratorMiscTrait for DelegateWireRustGenerator<'a> {
         // frb-coverage:ignore-end
         if let MirTypeDelegate::PrimitiveEnum(enu) = &self.mir {
             WireRustGenerator::new(enu.mir.clone(), self.context).wrapper_struct_name()
+        } else {
+            None
+        }
+    }
+
+    fn generate_wire_func_call_decode_type(&self) -> Option<String> {
+        if let MirTypeDelegate::ProxyEnum(mir) = &self.mir {
+            Some(mir.get_delegate().rust_api_type())
         } else {
             None
         }
