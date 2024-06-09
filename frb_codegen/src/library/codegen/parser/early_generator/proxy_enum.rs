4c5a9b020eddc6c7e4944f2b0a74cbbf3b1360e4
fzyzcjy <ch271828n@outlook.com>
2024-06-09 10:57:25 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/early_generator/proxy_enum.rs
+++ frb_codegen/src/library/codegen/parser/early_generator/proxy_enum.rs
@@ -74,13 +74,60 @@ fn generate_proxy_enum(proxy_variants: &[&MirTypeDelegateProxyVariant]) -> Strin
         })
         .join("");
 
+    let impl_lockable = generate_impl_lockable();
+
     format!(
         "
         enum {enum_name} {{
             {variants}
         }}
 
+        {impl_lockable}
+
         pub fn {FUNC_PREFIX_FRB_INTERNAL_NO_IMPL}_dummy_function_{enum_name}(a: {enum_name}) {{ }}
+        "
+    )
+}
+
+fn generate_impl_lockable() -> String {
+    format!(
+        "
+        impl<T: Send + Sync, A: BaseArc<RustAutoOpaqueInner<T>>> Lockable
+            for RustOpaqueBase<RustAutoOpaqueInner<T>, A>
+        {{
+            type RwLockReadGuard<'a> = crate::rust_async::RwLockReadGuard<'a, T> where A: 'a;
+            type RwLockWriteGuard<'a> = crate::rust_async::RwLockWriteGuard<'a, T>where A: 'a;
+
+            fn lockable_order(&self) -> LockableOrder {{
+                self.order
+            }}
+
+            fn lockable_decode_sync_ref(&self) -> Self::RwLockReadGuard<'_> {{
+                self.data.blocking_read()
+            }}
+
+            fn lockable_decode_sync_ref_mut(&self) -> Self::RwLockWriteGuard<'_> {{
+                self.data.blocking_write()
+            }}
+
+            fn lockable_decode_async_ref<'a>(
+                &'a self,
+            ) -> Pin<Box<dyn Future<Output = Self::RwLockReadGuard<'_>> + Send + 'a>>
+            where
+                Self: Sync + 'a,
+            {{
+                Box::pin(async move {{ self.data.read().await }})
+            }}
+
+            fn lockable_decode_async_ref_mut<'a>(
+                &'a self,
+            ) -> Pin<Box<dyn Future<Output = Self::RwLockWriteGuard<'_>> + Send + 'a>>
+            where
+                Self: Sync + 'a,
+            {{
+                Box::pin(async move {{ self.data.write().await }})
+            }}
+        }
         "
     )
 }
