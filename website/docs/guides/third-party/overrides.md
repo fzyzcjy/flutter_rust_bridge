7b8e3ff3bc90b35dbe6c12d80421b8ab21a88aaa
fzyzcjy <ch271828n@outlook.com>
2024-06-09 18:09:13 +0800

    chore: empty

---
+++ website/docs/guides/third-party/overrides.md
@@ -0,0 +1,3 @@
+# Overrides
+
+TODO
--- website/sidebars.js
+++ website/sidebars.js
@@ -228,6 +228,7 @@ const sidebars = {
                     items: [
                         'guides/third-party/overview',
                         'guides/third-party/scanning',
+                        'guides/third-party/overrides',
                     ],
                 },
                 {
