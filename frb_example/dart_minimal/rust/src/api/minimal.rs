9c85840e3ca1f9cb34f9d621253f1f6b1cc8941f
fzyzcjy <ch271828n@outlook.com>
2024-06-10 19:59:27 +0800

    chore: demo

--- frb_example/dart_minimal/rust/src/api/minimal.rs
+++ frb_example/dart_minimal/rust/src/api/minimal.rs
@@ -8,3 +8,31 @@ pub fn init_app() {
 pub fn minimal_adder(a: i32, b: i32) -> i32 {
     a + b
 }
+
+#[frb(
+    non_hash,
+    non_eq,
+    dart_code = "
+  import 'dart:math';
+
+  int dummyMethodThatNeedsDartImport() => Random().nextInt(10);
+
+  @override
+  bool operator ==(Object other) =>
+    other is TranslatableStructWithDartCodeTwinNormal && a == other.a;
+
+  @override
+  int get hashCode => a.hashCode;
+
+  int dartCodeMethod() => a * 2;
+"
+)]
+pub struct TranslatableStructWithDartCodeTwinNormal {
+    pub a: i32,
+}
+
+impl TranslatableStructWithDartCodeTwinNormal {
+    pub fn normal_method_twin_normal(&self) -> i32 {
+        self.a * 2
+    }
+}
