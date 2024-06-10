60c6b573a99e552b29eb61a2b5f5455b9b8e3b46
fzyzcjy <ch271828n@outlook.com>
2024-06-10 08:26:26 +0800

    chore: mv

--- frb_example/dart_minimal/rust/src/api/minimal.rs
+++ frb_example/dart_minimal/rust/src/api/minimal.rs
@@ -1,7 +1,4 @@
-use crate::frb_generated::{StreamSink, FLUTTER_RUST_BRIDGE_HANDLER};
-use anyhow::anyhow;
-use flutter_rust_bridge::for_generated::BaseThreadPool;
-use flutter_rust_bridge::{frb, transfer};
+use flutter_rust_bridge::frb;
 
 #[frb(init)]
 pub fn init_app() {
@@ -10,12 +7,4 @@ pub fn init_app() {
 
 pub fn minimal_adder(a: i32, b: i32) -> i32 {
     a + b
-}
-
-pub fn func_stream_add_value_and_error_twin_normal(sink: StreamSink<i32>) {
-    (FLUTTER_RUST_BRIDGE_HANDLER.thread_pool()).execute(transfer!(|| {
-        sink.add(100).unwrap();
-        sink.add(200).unwrap();
-        sink.add_error(anyhow!("deliberate error")).unwrap();
-    }));
 }
--- frb_example/dart_minimal/test/minimal_test.dart
+++ frb_example/dart_minimal/test/minimal_test.dart
@@ -16,20 +16,4 @@ Future<void> main() async {
     print('Action: Call rust (after)');
   });
   print('Action: Configure tests (end)');
-
-  test('func_stream_add_value_and_error_twin_normal', () async {
-    final stream = await funcStreamAddValueAndErrorTwinNormal();
-    final events = <String>[];
-    final onDone = Completer<void>();
-    stream.listen(
-      (e) => events.add('data $e'),
-      onError: (e, s) {
-        print('onError $e $s');
-        events.add('error $e');
-      },
-      onDone: () => onDone.complete(),
-    );
-    await onDone.future;
-    expect(events, ['data 100', 'data 200', contains('deliberate error')]);
-  });
 }
