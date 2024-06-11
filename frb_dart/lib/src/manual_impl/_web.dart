cfd164875dd9d355f081be470906ffb1fdbb8f32
fzyzcjy <ch271828n@outlook.com>
2024-06-11 09:27:06 +0800

    feat: decode

--- frb_dart/lib/src/manual_impl/_web.dart
+++ frb_dart/lib/src/manual_impl/_web.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_rust_bridge/src/generalized_typed_data/_web.dart';
 import 'package:flutter_rust_bridge/src/platform_types/_web.dart';
 import 'package:flutter_rust_bridge/src/platform_utils/_web.dart';
 
@@ -10,3 +11,14 @@ BigInt dcoDecodeI64(dynamic raw) => jsBigIntToDartBigInt(raw);
 
 /// {@macro flutter_rust_bridge.only_for_generated_code}
 BigInt dcoDecodeU64(dynamic raw) => jsBigIntToDartBigInt(raw);
+
+/// {@macro flutter_rust_bridge.only_for_generated_code}
+Int64List dcoDecodeInt64List(List<dynamic> raw) =>
+    Int64List.raw(_toListBigInt(raw));
+
+/// {@macro flutter_rust_bridge.only_for_generated_code}
+Uint64List dcoDecodeUint64List(List<dynamic> raw) =>
+    Uint64List.raw(_toListBigInt(raw));
+
+List<BigInt> _toListBigInt(List<dynamic> raw) =>
+    raw.map((x) => jsBigIntToDartBigInt(x!)).toList();
