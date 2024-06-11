996dc65442d507c978a64042fec81b5e88fa239a
fzyzcjy <ch271828n@outlook.com>
2024-06-11 18:24:11 +0800

    chore: more demo

--- frb_example/dart_minimal/rust/src/api/minimal.rs
+++ frb_example/dart_minimal/rust/src/api/minimal.rs
@@ -11,5 +11,24 @@ pub fn minimal_adder(a: i32, b: i32) -> i32 {
 
 pub async fn my_async_rust_function() {
     flutter_rust_bridge::console_error!("my_async_rust_function start");
+
+    // https://docs.rs/tokio/latest/tokio/sync/mpsc/struct.Sender.html
+    let (tx, mut rx) = tokio::sync::mpsc::channel(1);
+
+    let handle = flutter_rust_bridge::spawn(async move {
+        for i in 0..10 {
+            if let Err(_) = tx.send(i).await {
+                flutter_rust_bridge::console_error!("receiver dropped");
+                return;
+            }
+        }
+    });
+
+    while let Some(i) = rx.recv().await {
+        flutter_rust_bridge::console_error!("got = {}", i);
+    }
+
+    handle.await.unwrap();
+
     flutter_rust_bridge::console_error!("my_async_rust_function end");
 }
