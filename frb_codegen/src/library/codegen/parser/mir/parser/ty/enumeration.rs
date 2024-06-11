8fed07c7d266cc9e8f5d8c7911212c3e0b62e746
fzyzcjy <ch271828n@outlook.com>
2024-06-12 07:54:01 +0800

    chore: more

--- frb_codegen/src/library/codegen/parser/mir/parser/ty/enumeration.rs
+++ frb_codegen/src/library/codegen/parser/mir/parser/ty/enumeration.rs
@@ -144,8 +144,8 @@ pub(crate) fn compute_enum_variant_kind_struct_name(
 
 struct EnumOrStructParserEnum<'a, 'b, 'c, 'd>(&'d mut TypeParserWithContext<'a, 'b, 'c>);
 
-impl EnumOrStructParser<MirEnumIdent, MirEnum, ItemEnum>
-    for EnumOrStructParserEnum<'_, '_, '_, '_>
+impl<'a, 'b, 'c, 'd> EnumOrStructParser<MirEnumIdent, MirEnum, ItemEnum>
+    for EnumOrStructParserEnum<'a, 'b, 'c, 'd>
 {
     fn parse_inner_impl(
         &mut self,
@@ -193,9 +193,9 @@ impl EnumOrStructParser<MirEnumIdent, MirEnum, ItemEnum>
             .any(|ty| structure_compute_default_opaque(ty, &obj.name.namespace.crate_name()))
     }
 
-    fn inner_mut<'a>(&'a mut self) -> &'a mut TypeParserWithContext<'_, '_, '_>
+    fn inner_mut<'z: 'a + 'b + 'c + 'd>(&'z mut self) -> &'z mut TypeParserWithContext<'_, '_, '_>
     where
-        Self: 'a,
+        Self: 'z,
     {
         &mut self.0
     }
