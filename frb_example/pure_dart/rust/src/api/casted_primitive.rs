313f520493a6dcf017ee7e8d531d59a0ebaec76a
fzyzcjy <ch271828n@outlook.com>
2024-06-12 07:41:57 +0800

    feat: more test

--- frb_example/pure_dart/rust/src/api/casted_primitive.rs
+++ frb_example/pure_dart/rust/src/api/casted_primitive.rs
@@ -25,3 +25,23 @@ pub fn casted_primitive_isize_twin_normal(arg: isize) -> isize {
 pub fn casted_primitive_usize_twin_normal(arg: usize) -> usize {
     arg
 }
+
+#[frb(serialize)]
+#[frb(type_64bit_int)]
+pub fn casted_primitive_multi_arg_twin_normal(a: i32, b: i64, c: usize, d: i128) {
+    let _ = (a, b, c);
+}
+
+#[frb(type_64bit_int)]
+pub struct StructWithCastedPrimitiveTwinNormal {
+    pub field_i64: i64,
+    pub field_u64: u64,
+    pub field_i32: i32,
+    pub field_vec_u8: Vec<u8>,
+}
+
+pub fn function_for_struct_with_casted_primitive_twin_normal(
+    arg: StructWithCastedPrimitiveTwinNormal,
+) -> StructWithCastedPrimitiveTwinNormal {
+    arg
+}
