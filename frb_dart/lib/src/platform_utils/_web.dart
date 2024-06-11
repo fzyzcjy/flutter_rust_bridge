8196ae7c2acf50d169e39bfe918ffe98a3e94ea4
fzyzcjy <ch271828n@outlook.com>
2024-06-11 09:57:47 +0800

    feat: more

--- frb_dart/lib/src/platform_utils/_web.dart
+++ frb_dart/lib/src/platform_utils/_web.dart
@@ -6,19 +6,15 @@ int castInt(Object? value) {
   throw UnimplementedError('castInt($value) runtimeType=${value.runtimeType}');
 }
 
-// @JS('Function')
-// class _Function {
-//   external dynamic call();
-//
-//   external factory _Function(String script);
-// }
-//
-// /// {@macro flutter_rust_bridge.internal}
-// dynamic jsEval(String script) => _Function(script)();
+@JS('Function')
+extension type _Function._(JSObject _) implements JSObject {
+  external factory _Function(String script);
 
-dynamic jsEval(String script) {
-  throw UnimplementedError('jsEval(script=$script)');
+  external dynamic call();
 }
+
+/// {@macro flutter_rust_bridge.internal}
+dynamic jsEval(String script) => _Function(script)();
 
 /// Whether the web platform has been isolated by COOP and COEP headers,
 /// and is capable of sharing buffers between workers.
@@ -37,5 +33,5 @@ Object castNativeBigInt(Object? value) {
 /// {@macro flutter_rust_bridge.only_for_generated_code}
 BigInt jsBigIntToDartBigInt(Object bigInt) {
   throw UnimplementedError('todo!');
-  // return BigInt.parse(callMethod(bigInt, 'toString', const []));
+// return BigInt.parse(callMethod(bigInt, 'toString', const []));
 }
