ec19500558a675ee9ea914ecdb85c43aef314940
fzyzcjy <ch271828n@outlook.com>
2024-06-11 20:22:08 +0800

    feat: more

--- frb_rust/src/codec/dco.rs
+++ frb_rust/src/codec/dco.rs
@@ -24,7 +24,10 @@ impl BaseCodec for DcoCodec {
 
 impl DcoCodec {
     pub fn encode<T: IntoDart + Send>(result_code: Rust2DartAction, data: T) -> Rust2DartMessageDco {
-        Rust2DartMessageDco(vec![result_code.into_dart(), data.into_dart()].into_dart())
+        // TODO temp debug
+        let data2: Box<dyn IntoDart + Send> = Box::new(data);
+        log::warn!("temp debug!!!");
+        Rust2DartMessageDco(vec![result_code.into_dart(), data2.into_dart()].into_dart())
     }
 }
 
