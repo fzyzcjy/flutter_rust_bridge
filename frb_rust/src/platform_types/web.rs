2a7d22879f87d333c8102f21f0dda8291aa5e6fd
fzyzcjy <ch271828n@outlook.com>
2024-06-11 11:50:09 +0800

    refactor: more

--- frb_rust/src/platform_types/web.rs
+++ frb_rust/src/platform_types/web.rs
@@ -5,27 +5,12 @@ use web_sys::BroadcastChannel;
 pub type WireSyncRust2DartDco = wasm_bindgen::JsValue;
 pub type WireSyncRust2DartSse = wasm_bindgen::JsValue;
 
-pub type MessagePort = crate::generalized_isolate::PortLike;
+pub type DartNativeSendPort = String;
 
 pub type DartAbi = wasm_bindgen::JsValue;
 
-#[derive(Clone, Debug)]
-pub struct SendableMessagePortHandle(String);
-
-pub fn message_port_to_handle(port: &MessagePort) -> SendableMessagePortHandle {
-    SendableMessagePortHandle(
-        port.dyn_ref::<BroadcastChannel>()
-            .map(|channel| channel.name())
-            .expect("Not a BroadcastChannel"),
-    )
-}
-
-pub fn handle_to_message_port(handle: &SendableMessagePortHandle) -> MessagePort {
-    PortLike::broadcast(&handle.0)
-}
+pub type PlatformGeneralizedUint8ListPtr = wasm_bindgen::JsValue;
 
 pub fn deserialize_dart_native_send_port(raw: String) -> SendableMessagePortHandle {
-    SendableMessagePortHandle(raw)
+    raw
 }
-
-pub type PlatformGeneralizedUint8ListPtr = wasm_bindgen::JsValue;
