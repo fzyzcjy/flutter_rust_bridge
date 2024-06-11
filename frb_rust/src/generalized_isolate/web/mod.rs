bdaf2a34c0ae3e06a95d229bfd55b87f697bbb11
fzyzcjy <ch271828n@outlook.com>
2024-06-11 12:05:11 +0800

    feat: more

--- frb_rust/src/generalized_isolate/web/mod.rs
+++ frb_rust/src/generalized_isolate/web/mod.rs
@@ -1,5 +1,6 @@
 mod into_dart;
 
+use crate::for_generated::DartNativeSendPort;
 pub use into_dart::*;
 
 #[derive(Debug)]
@@ -18,11 +19,21 @@ pub type SendableDartSendPort = String;
 pub struct DartSendPort(web_sys::BroadcastChannel);
 
 impl DartSendPort {
+    pub fn new(native: DartNativeSendPort) -> Self {
+        Self(web_sys::BroadcastChannel::new(&native).unwrap())
+    }
+
     pub fn to_sendable(&self) -> SendableDartSendPort {
         self.0.name()
     }
 
     pub fn from_sendable(port: SendableDartSendPort) -> DartSendPort {
-        Self(web_sys::BroadcastChannel::new(port).unwrap())
+        Self(web_sys::BroadcastChannel::new(&port).unwrap())
+    }
+
+    pub fn post(&self, msg: impl IntoDart) -> bool {
+        (self.0.post_message(&msg.into_dart()))
+            .map_err(|err| crate::console_error!("error when DartSendPort.post: {:?}", err))
+            .is_ok()
     }
 }
