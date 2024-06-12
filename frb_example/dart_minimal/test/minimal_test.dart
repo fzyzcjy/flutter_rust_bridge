104c603718649b59ee02f7190c4670537954ec52
fzyzcjy <ch271828n@outlook.com>
2024-06-12 20:42:34 +0800

    fix: err

--- frb_example/dart_minimal/test/minimal_test.dart
+++ frb_example/dart_minimal/test/minimal_test.dart
@@ -15,7 +15,7 @@ Future<void> main() async {
   print('Dart before call my_rust_function');
   final messageChannel = html.MessageChannel();
   messageChannel.port1.addEventListener(
-      'message', (data) => 'messageChannel.port1 see event $data');
+      'message', (data) => print('messageChannel.port1 see event $data'));
   my_rust_function(messageChannel.port2);
   print('Dart after call my_rust_function');
 
