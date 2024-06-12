cdcd26d7af279b33af0dab98b725fe88eabe315e
fzyzcjy <ch271828n@outlook.com>
2024-06-12 20:14:46 +0800

    faet: demo

--- frb_example/dart_minimal/rust/src/api/minimal.rs
+++ frb_example/dart_minimal/rust/src/api/minimal.rs
@@ -1,4 +1,6 @@
 use flutter_rust_bridge::frb;
+use wasm_bindgen;
+use wasm_bindgen::prelude::*;
 
 // #[frb(init)]
 // pub fn init_app() {
@@ -13,3 +15,8 @@ pub fn f(a: Vec<u64>) -> Vec<u64> {
     a
 }
 
+#[wasm_bindgen]
+pub fn my_rust_function() {
+    flutter_rust_bridge::console_error!("my_rust_function start");
+    flutter_rust_bridge::console_error!("my_rust_function end");
+}
--- frb_example/dart_minimal/test/minimal_test.dart
+++ frb_example/dart_minimal/test/minimal_test.dart
@@ -1,25 +1,30 @@
 import 'dart:async';
 
-import 'package:flutter_rust_bridge/flutter_rust_bridge.dart';
-import 'package:frb_example_dart_minimal/src/rust/api/minimal.dart';
 import 'package:frb_example_dart_minimal/src/rust/frb_generated.dart';
-import 'package:test/test.dart';
+import 'package:js/js.dart';
+
+@JS("wasm_bindgen.my_rust_function")
+external void my_rust_function();
 
 Future<void> main() async {
   print('Action: Init rust (before)');
   await RustLib.init();
   print('Action: Init rust (after)');
 
-  print('Action: Configure tests (before)');
-  test('dart call minimalAdder', () async {
-    print('Action: Call rust (before)');
-    expect(await minimalAdder(a: 100, b: 200), 300);
-    print('Action: Call rust (after)');
-  });
-  print('Action: Configure tests (end)');
+  print('Dart before call my_rust_function');
+  my_rust_function();
+  print('Dart after call my_rust_function');
 
-  test('temp', () async {
-    final input = Uint64List(1)..[0] = BigInt.parse('123456789012345678');
-    expect(await f(a: input), input);
-  });
+  // print('Action: Configure tests (before)');
+  // test('dart call minimalAdder', () async {
+  //   print('Action: Call rust (before)');
+  //   expect(await minimalAdder(a: 100, b: 200), 300);
+  //   print('Action: Call rust (after)');
+  // });
+  // print('Action: Configure tests (end)');
+  //
+  // test('temp', () async {
+  //   final input = Uint64List(1)..[0] = BigInt.parse('123456789012345678');
+  //   expect(await f(a: input), input);
+  // });
 }
