0b3a0899cf3da73312d2e59398b3840d5be969f8
fzyzcjy <ch271828n@outlook.com>
2024-06-12 20:19:05 +0800

    feat: more demo

--- frb_example/dart_minimal/rust/src/api/minimal.rs
+++ frb_example/dart_minimal/rust/src/api/minimal.rs
@@ -16,7 +16,9 @@ pub fn f(a: Vec<u64>) -> Vec<u64> {
 }
 
 #[wasm_bindgen]
-pub fn my_rust_function() {
-    flutter_rust_bridge::console_error!("my_rust_function start");
+pub fn my_rust_function(message_port: web_sys::MessagePort) {
+    flutter_rust_bridge::console_error!("my_rust_function start message_port={:?}", message_port);
+    let msg: JsValue = "hello".into();
+    message_port.post_message(&msg).unwrap();
     flutter_rust_bridge::console_error!("my_rust_function end");
 }
--- frb_example/dart_minimal/test/minimal_test.dart
+++ frb_example/dart_minimal/test/minimal_test.dart
@@ -1,10 +1,11 @@
 import 'dart:async';
+import 'dart:html' as html;
 
 import 'package:frb_example_dart_minimal/src/rust/frb_generated.dart';
 import 'package:js/js.dart';
 
 @JS("wasm_bindgen.my_rust_function")
-external void my_rust_function();
+external void my_rust_function(dynamic message_port);
 
 Future<void> main() async {
   print('Action: Init rust (before)');
@@ -12,7 +13,10 @@ Future<void> main() async {
   print('Action: Init rust (after)');
 
   print('Dart before call my_rust_function');
-  my_rust_function();
+  final messageChannel = html.MessageChannel();
+  messageChannel.port1.addEventListener(
+      'message', (data) => 'messageChannel.port1 see event $data');
+  my_rust_function(messageChannel.port2);
   print('Dart after call my_rust_function');
 
   // print('Action: Configure tests (before)');
