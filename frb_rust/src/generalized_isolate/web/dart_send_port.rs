075436ad7da1bd803c38921368210149dacf791d
fzyzcjy <ch271828n@outlook.com>
2024-06-12 15:49:50 +0800

    feat: more

--- frb_rust/src/generalized_isolate/web/dart_send_port.rs
+++ frb_rust/src/generalized_isolate/web/dart_send_port.rs
@@ -1,6 +1,7 @@
 use super::into_dart::IntoDart;
 use crate::for_generated::DartNativeSendPort;
 use crate::generalized_isolate::web::box_into_dart::BoxIntoDart;
+use crate::platform_types::DartAbi;
 
 #[derive(Debug, Clone)]
 pub struct DartSendPort(i64);
@@ -15,14 +16,31 @@ impl DartSendPort {
         F: (FnOnce() -> T) + Send,
         T: IntoDart,
     {
-        // to test whether "send to another thread" can compile
-        std::thread::spawn(move || {
-            let msg = msg_creator();
-            let dart_abi = msg.into_dart();
-            // let msg_boxed: Box<dyn BoxIntoDart + Send> = Box::new(msg);
-            // let dart_abi = msg_boxed.box_into_dart();
-            let _ = dart_abi;
-        });
+        mimic_send_to_another_thread(Box::new(msg_creator));
+        // // to test whether "send to another thread" can compile
+        // std::thread::spawn(move || {
+        //     let msg = msg_creator();
+        //     let dart_abi = msg.into_dart();
+        //     // let msg_boxed: Box<dyn BoxIntoDart + Send> = Box::new(msg);
+        //     // let dart_abi = msg_boxed.box_into_dart();
+        //     let _ = dart_abi;
+        // });
         todo!()
     }
+}
+
+fn mimic_send_to_another_thread(msg_creator: Box<dyn (FnOnce() -> Box<dyn BoxIntoDart>) + Send>) {
+    let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel();
+
+    // fake receiver
+    tokio::task::spawn(move || async {
+        while let Some(msg_creator) = rx.recv().await {
+            let msg: Box<dyn BoxIntoDart> = msg_creator();
+            let dart_abi: DartAbi = msg.box_into_dart();
+            let _ = dart_abi;
+        }
+    });
+
+    // fake sender
+    tx.send(msg_creator).unwrap();
 }
