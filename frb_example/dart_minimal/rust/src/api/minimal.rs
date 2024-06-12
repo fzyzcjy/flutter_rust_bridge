5a88a16fac423f44e4540381403300d13143d6d9
fzyzcjy <ch271828n@outlook.com>
2024-06-12 20:40:06 +0800

    feat: more

--- frb_example/dart_minimal/rust/src/api/minimal.rs
+++ frb_example/dart_minimal/rust/src/api/minimal.rs
@@ -16,7 +16,10 @@ pub fn f(a: Vec<u64>) -> Vec<u64> {
 }
 
 #[wasm_bindgen]
-pub fn my_rust_function(message_port: web_sys::MessagePort) {
+pub fn my_rust_function(
+    // message_port: web_sys::MessagePort,
+    message_port: flutter_rust_bridge::for_generated::MessagePort,
+) {
     flutter_rust_bridge::console_error!("my_rust_function start message_port={:?}", message_port);
     let msg: JsValue = "hello".into();
     message_port.post_message(&msg).unwrap();
