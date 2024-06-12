e9d2c6675995986b521233ea0ce6ec12c5a7ef31
fzyzcjy <ch271828n@outlook.com>
2024-06-12 15:20:56 +0800

    refactor: rm sendable port

--- frb_rust/src/generalized_isolate/io.rs
+++ frb_rust/src/generalized_isolate/io.rs
@@ -2,22 +2,12 @@ use crate::platform_types::DartNativeSendPort;
 pub use allo_isolate::ZeroCopyBuffer;
 pub use allo_isolate::{IntoDart, IntoDartExceptPrimitive};
 
-pub type SendableDartSendPort = DartSendPort;
-
 #[derive(Debug, Clone)]
 pub struct DartSendPort(allo_isolate::Isolate);
 
 impl DartSendPort {
     pub fn new(native: DartNativeSendPort) -> Self {
         Self(allo_isolate::Isolate::new(native))
-    }
-
-    pub fn to_sendable(&self) -> SendableDartSendPort {
-        self.to_owned()
-    }
-
-    pub fn from_sendable(port: &SendableDartSendPort) -> DartSendPort {
-        port.to_owned()
     }
 
     pub fn post(&self, msg: impl IntoDart) -> bool {
--- frb_rust/src/generalized_isolate/web/mod.rs
+++ frb_rust/src/generalized_isolate/web/mod.rs
@@ -6,22 +6,12 @@ use crate::for_generated::DartNativeSendPort;
 pub use into_dart::*;
 pub use misc::*;
 
-pub type SendableDartSendPort = String;
-
 #[derive(Debug, Clone)]
 pub struct DartSendPort(web_sys::BroadcastChannel);
 
 impl DartSendPort {
     pub fn new(native: DartNativeSendPort) -> Self {
         Self(web_sys::BroadcastChannel::new(&native).unwrap())
-    }
-
-    pub fn to_sendable(&self) -> SendableDartSendPort {
-        self.0.name()
-    }
-
-    pub fn from_sendable(port: &SendableDartSendPort) -> DartSendPort {
-        Self(web_sys::BroadcastChannel::new(port).unwrap())
     }
 
     pub fn post(&self, msg: impl IntoDart) -> bool {
