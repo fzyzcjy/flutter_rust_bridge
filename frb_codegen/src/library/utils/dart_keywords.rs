4a3eadd01a3c82af2e3b9cee2bd5cefccf93e709
fzyzcjy <ch271828n@outlook.com>
2024-06-12 21:26:33 +0800

    Revert "Auxiliary commit to revert individual files from 8d59e87148367e9ff8b82bb74191c755e5932b18"
    This reverts commit f12e1e0156b6b03ee14b7fd9a1aecfdb9acb5de2, reversing
    changes made to 89ff866e043587eab55b944a619863c1af1a66b7.

--- frb_codegen/src/library/utils/dart_keywords.rs
+++ frb_codegen/src/library/utils/dart_keywords.rs
@@ -1,26 +1,9 @@
-use anyhow::bail;
-use convert_case::{Case, Casing};
-
-/// If the given string is a Dart keyword, then
-/// convert it to PascalCase to avoid issues.
-/// If the string is not a keyword, then the original
-/// is returned.
-pub(crate) fn make_string_keyword_safe(input: String) -> String {
-    if check_for_keywords(&[input.clone()]).is_err() {
-        input.to_case(Case::Pascal)
+pub(crate) fn escape(input: String) -> String {
+    if DART_KEYWORDS.contains(&input.as_str()) {
+        format!("{input}_")
     } else {
         input
     }
-}
-
-// the function signature is not covered while the whole body is covered - looks like a bug in coverage tool
-// frb-coverage:ignore-start
-fn check_for_keywords(v: &[String]) -> anyhow::Result<()> {
-    // frb-coverage:ignore-end
-    if let Some(s) = v.iter().find(|s| DART_KEYWORDS.contains(&s.as_str())) {
-        bail!("Api name cannot be a dart keyword: {}", s);
-    };
-    Ok(())
 }
 
 // https://dart.dev/guides/language/language-tour#keywords
