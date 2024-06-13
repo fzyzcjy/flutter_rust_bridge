9d13fbddb9cd252ea5d8cbd57d37dce86b6ff2d7
fzyzcjy <ch271828n@outlook.com>
2024-06-13 09:58:41 +0800

    Revert "feat: more"
    This reverts commit fcca04ef0d337d4c90f628ac0d2294da5295fa42.

--- frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/wire_class/web.rs
+++ frb_codegen/src/library/codegen/generator/wire/dart/spec_generator/wire_class/web.rs
@@ -73,8 +73,7 @@ fn generate_method(func: &ExternFunc) -> MethodInfo {
     let func_name = &func.partial_func_name;
 
     let return_type = (func.return_type.as_ref())
-        // .map(|x| reconstruct_dart_wire_type_from_raw_repr(x))
-        .map(|x| x.dart_type.clone())
+        .map(|x| reconstruct_dart_wire_type_from_raw_repr(x))
         .unwrap_or_else(|| "void".to_owned());
 
     let input_params = (func.params.iter())
@@ -91,26 +90,26 @@ fn generate_method(func: &ExternFunc) -> MethodInfo {
     }
 }
 
-// /// Since there exists no toolchain that can generate Dart bindings
-// /// for JS code, we have to supply our own stubs. The external function
-// /// generator however converts an [MirType] (sometimes a raw string)
-// /// into its string representation, so these heuristics parse those representations
-// /// into their appropriate Dart wire types. Note that only a subset of types
-// /// is supported, outside of which `dynamic` is returned.
-// ///
-// /// In practice however this is optional as unlike Rust, Dart values are
-// /// aware of their own types (via the `runtimeType` property) and can
-// /// safely assume the `dynamic` or `Object` type instead.
-// fn reconstruct_dart_wire_type_from_raw_repr(ty: &str) -> String {
-//     let ty = ty.trim();
-//     let real_ty = if is_rust_pointer(ty) {
-//         "int"
-//     } else {
-//         "dynamic"
-//     };
-//     format!("{real_ty} /* {ty} */")
-// }
-//
-// fn is_rust_pointer(ty: &str) -> bool {
-//     ty.starts_with("*mut") || ty.starts_with("*const")
-// }
+/// Since there exists no toolchain that can generate Dart bindings
+/// for JS code, we have to supply our own stubs. The external function
+/// generator however converts an [MirType] (sometimes a raw string)
+/// into its string representation, so these heuristics parse those representations
+/// into their appropriate Dart wire types. Note that only a subset of types
+/// is supported, outside of which `dynamic` is returned.
+///
+/// In practice however this is optional as unlike Rust, Dart values are
+/// aware of their own types (via the `runtimeType` property) and can
+/// safely assume the `dynamic` or `Object` type instead.
+fn reconstruct_dart_wire_type_from_raw_repr(ty: &str) -> String {
+    let ty = ty.trim();
+    let real_ty = if is_rust_pointer(ty) {
+        "int"
+    } else {
+        "dynamic"
+    };
+    format!("{real_ty} /* {ty} */")
+}
+
+fn is_rust_pointer(ty: &str) -> bool {
+    ty.starts_with("*mut") || ty.starts_with("*const")
+}
