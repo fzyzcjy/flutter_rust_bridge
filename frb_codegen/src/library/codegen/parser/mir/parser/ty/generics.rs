d9fc46d7c887556dc2e74bf82aa484924d87ac99
fzyzcjy <ch271828n@outlook.com>
2024-06-15 13:46:46 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/mir/parser/ty/generics.rs
+++ frb_codegen/src/library/codegen/parser/mir/parser/ty/generics.rs
@@ -1,9 +1,11 @@
 use crate::if_then_some;
+use anyhow::{Context, Result};
 
-pub(crate) fn parse_generic_lifetimes(generics: &syn::Generics) -> Option<Vec<syn::LifetimeParam>> {
+pub(crate) fn parse_generic_lifetimes(generics: &syn::Generics) -> Result<Vec<syn::LifetimeParam>> {
     (generics.params.iter())
-        .map(
-            |param| if_then_some!(let syn::GenericParam::Lifetime(inner) = param, inner.to_owned()),
-        )
+        .map(|param| {
+            if_then_some!(let syn::GenericParam::Lifetime(inner) = param, inner.to_owned())
+                .context("only support generic lifetime")
+        })
         .collect()
 }
