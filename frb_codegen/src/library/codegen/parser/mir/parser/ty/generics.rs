52dd1790861541d6764ea28496d28d461859075e
fzyzcjy <ch271828n@outlook.com>
2024-06-15 13:49:25 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/mir/parser/ty/generics.rs
+++ frb_codegen/src/library/codegen/parser/mir/parser/ty/generics.rs
@@ -1,11 +1,26 @@
 use crate::if_then_some;
-use anyhow::{Context, Result};
+use itertools::Itertools;
 
-pub(crate) fn parse_generic_lifetimes(generics: &syn::Generics) -> Result<Vec<syn::LifetimeParam>> {
-    (generics.params.iter())
-        .map(|param| {
-            if_then_some!(let syn::GenericParam::Lifetime(inner) = param, inner.to_owned())
-                .context("only support generic lifetime")
-        })
-        .collect()
+pub(crate) fn parse_generics_info(generics: &syn::Generics) -> GenericsInfo {
+    if generics.params.len() == 0 {
+        return GenericsInfo::Empty;
+    }
+
+    let lifetime_params = (generics.params.iter())
+        .filter_map(
+            |param| if_then_some!(let syn::GenericParam::Lifetime(inner) = param, inner.to_owned()),
+        )
+        .collect_vec();
+
+    if lifetime_params.len() == generics.params.len() {
+        GenericsInfo::LifetimeOnly(lifetime_params)
+    } else {
+        GenericsInfo::Unsupported
+    }
+}
+
+pub(crate) enum GenericsInfo {
+    Empty,
+    LifetimeOnly(Vec<syn::LifetimeParam>),
+    Unsupported,
 }
