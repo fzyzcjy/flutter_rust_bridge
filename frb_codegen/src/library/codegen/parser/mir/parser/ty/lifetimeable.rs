0b13659150488997ccc02e9fd4b27cc5e80b214a
fzyzcjy <ch271828n@outlook.com>
2024-06-15 16:32:49 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/mir/parser/ty/lifetimeable.rs
+++ frb_codegen/src/library/codegen/parser/mir/parser/ty/lifetimeable.rs
@@ -5,8 +5,13 @@ use crate::codegen::parser::mir::parser::ty::TypeParserWithContext;
 impl<'a, 'b, 'c> TypeParserWithContext<'a, 'b, 'c> {
     pub(crate) fn parse_maybe_lifetimeable(
         &mut self,
-        raw: MirTypeRustAutoOpaqueImplicit,
+        original: MirTypeRustAutoOpaqueImplicit,
     ) -> anyhow::Result<MirType> {
+        Ok(self.parse_maybe_lifetimeable_raw(&original.raw.string)?
+            .unwrap_or(MirType::RustAutoOpaqueImplicit(original)))
+    }
+
+    fn parse_maybe_lifetimeable_raw(&self, original: &str) -> anyhow::Result<Option<MirType>> {
         todo!()
     }
 }
