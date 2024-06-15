65e68278d136065845655151b7df59153b190e73
fzyzcjy <ch271828n@outlook.com>
2024-06-15 15:24:33 +0800

    feat: more

--- frb_example/temp_demo/src/demo_mine.rs
+++ frb_example/temp_demo/src/demo_mine.rs
@@ -4,14 +4,18 @@ use std::sync::Arc;
 use tokio::sync::{RwLock, RwLockReadGuard};
 
 struct WithOwnerGuard {
-    // NOTE order of fields!
+    // NOTE order of fields MUST be correct, which affects drop order
     value: RwLockReadGuard<'static, One>,
     owners: Vec<Box<dyn Any>>,
 }
 
 fn new(
     owner: Arc<RwLock<One>>,
-    guard_builder: impl for<'this> FnOnce(&'this RwLock<One>) -> RwLockReadGuard<'this, One>,
+    builder: impl for<'this> FnOnce(&'this RwLock<One>) -> RwLockReadGuard<'this, One>,
 ) -> WithOwnerGuard {
-    TODO
+    let owner_cloned = owner.clone();
+    WithOwnerGuard {
+        value: builder(&owner),
+        owners: vec![Box::new(owner_cloned)],
+    }
 }
