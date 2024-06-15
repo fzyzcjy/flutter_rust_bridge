5aaccbc074122caccf40144878b8b564805d5c31
fzyzcjy <ch271828n@outlook.com>
2024-06-15 17:13:06 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/mir/parser/lifetime_replacer.rs
+++ frb_codegen/src/library/codegen/parser/mir/parser/lifetime_replacer.rs
@@ -1,3 +1,13 @@
+use crate::codegen::parser::mir::parser::lifetime_extractor::LIFETIME_STATIC;
+
 pub(crate) fn replace_lifetime_to_static(ty: &str, lifetimes: &[String]) -> String {
-    TODO
+    let mut ans = ty.to_owned();
+    for lifetime in &lifetimes {
+        ans = replace_lifetime(&ans, lifetime, LIFETIME_STATIC);
+    }
+    ans
+}
+
+fn replace_lifetime(ty: &str, lifetime_src: &str, lifetime_dst: &str) -> String {
+
 }
