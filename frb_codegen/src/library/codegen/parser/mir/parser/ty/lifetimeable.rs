216aae63be339ac9a44eb354633e19dc2b0d85c3
fzyzcjy <ch271828n@outlook.com>
2024-06-15 21:54:51 +0800

    feat: back

--- frb_codegen/src/library/codegen/parser/mir/parser/ty/lifetimeable.rs
+++ frb_codegen/src/library/codegen/parser/mir/parser/ty/lifetimeable.rs
@@ -7,11 +7,13 @@ use crate::codegen::parser::mir::parser::lifetime_extractor::LifetimeExtractor;
 use crate::codegen::parser::mir::parser::lifetime_replacer::replace_lifetimes_to_static;
 use crate::codegen::parser::mir::parser::ty::TypeParserWithContext;
 use syn::Type;
+use crate::utils::namespace::Namespace;
 
 impl<'a, 'b, 'c> TypeParserWithContext<'a, 'b, 'c> {
     pub(crate) fn parse_maybe_lifetimeable(
         &mut self,
         original: MirTypeRustAutoOpaqueImplicit,
+        namespace: Option<Namespace>,
     ) -> anyhow::Result<MirType> {
         let ty_str = &original.raw.string;
         let ty: Type = syn::parse_str(ty_str)?;
@@ -27,7 +29,8 @@ impl<'a, 'b, 'c> TypeParserWithContext<'a, 'b, 'c> {
                 MirTypeDelegateLifetimeable {
                     api_type: Box::new(MirType::RustAutoOpaqueImplicit(original)),
                     delegate: Box::new(self.parse_type_rust_auto_opaque_implicit(
-                        Some(self.context.rust_output_path_namespace.clone()),
+                        namespace,
+                        // Some(self.context.rust_output_path_namespace.clone()),
                         &syn::parse_str(&delegate_ty_str)?,
                         None,
                         None,
