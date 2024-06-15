dd8c5b4e2b82489c790c4f96103fdc881f6669b6
fzyzcjy <ch271828n@outlook.com>
2024-06-15 17:09:05 +0800

    chore: arg

--- frb_codegen/src/library/codegen/parser/mir/parser/ty/lifetimeable.rs
+++ frb_codegen/src/library/codegen/parser/mir/parser/ty/lifetimeable.rs
@@ -1,14 +1,16 @@
 use crate::codegen::ir::mir::ty::delegate::{MirTypeDelegate, MirTypeDelegateLifetimeable};
-use crate::codegen::ir::mir::ty::rust_auto_opaque_implicit::MirTypeRustAutoOpaqueImplicit;
+use crate::codegen::ir::mir::ty::rust_auto_opaque_implicit::{MirTypeRustAutoOpaqueImplicit, MirTypeRustAutoOpaqueImplicitReason};
 use crate::codegen::ir::mir::ty::MirType;
 use crate::codegen::parser::mir::parser::lifetime_extractor::LifetimeExtractor;
 use crate::codegen::parser::mir::parser::ty::TypeParserWithContext;
 use syn::Type;
+use crate::utils::namespace::Namespace;
 
 impl<'a, 'b, 'c> TypeParserWithContext<'a, 'b, 'c> {
     pub(crate) fn parse_maybe_lifetimeable(
         &mut self,
         original: MirTypeRustAutoOpaqueImplicit,
+        namespace: Option<Namespace>,
     ) -> anyhow::Result<MirType> {
         let ty: Type = syn::parse_str(&original.raw.string)?;
         let lifetimes = LifetimeExtractor::extract(&ty);
