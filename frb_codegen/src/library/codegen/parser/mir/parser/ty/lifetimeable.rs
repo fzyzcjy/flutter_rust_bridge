585266f3bdf9cd1ea9e327122dfe986d103230a2
fzyzcjy <ch271828n@outlook.com>
2024-06-15 16:44:13 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/mir/parser/ty/lifetimeable.rs
+++ frb_codegen/src/library/codegen/parser/mir/parser/ty/lifetimeable.rs
@@ -1,25 +1,26 @@
+use crate::codegen::ir::mir::ty::delegate::{MirTypeDelegate, MirTypeDelegateLifetimeable};
 use crate::codegen::ir::mir::ty::rust_auto_opaque_implicit::MirTypeRustAutoOpaqueImplicit;
 use crate::codegen::ir::mir::ty::MirType;
 use crate::codegen::parser::mir::parser::lifetime_extractor::LifetimeExtractor;
 use crate::codegen::parser::mir::parser::ty::TypeParserWithContext;
-use crate::if_then_some;
-use itertools::Itertools;
-use syn::fold::fold_lifetime;
-use syn::{GenericArgument, Type};
+use syn::Type;
 
 impl<'a, 'b, 'c> TypeParserWithContext<'a, 'b, 'c> {
     pub(crate) fn parse_maybe_lifetimeable(
         &mut self,
         original: MirTypeRustAutoOpaqueImplicit,
     ) -> anyhow::Result<MirType> {
-        Ok(self
-            .parse_maybe_lifetimeable_raw(&original.raw.string)?
-            .unwrap_or(MirType::RustAutoOpaqueImplicit(original)))
-    }
-
-    fn parse_maybe_lifetimeable_raw(&self, original: &str) -> anyhow::Result<Option<MirType>> {
-        let ty: Type = syn::parse_str(original)?;
+        let ty: Type = syn::parse_str(&original.raw.string)?;
         let lifetimes = LifetimeExtractor::extract(&ty);
-        Ok(TODO)
+
+        if !lifetimes.is_empty() {
+            return Ok(MirType::Delegate(MirTypeDelegate::Lifetimeable(
+                MirTypeDelegateLifetimeable {
+                    inner: Box::new(MirType::RustAutoOpaqueImplicit(original)),
+                },
+            )));
+        }
+
+        Ok(MirType::RustAutoOpaqueImplicit(original))
     }
 }
