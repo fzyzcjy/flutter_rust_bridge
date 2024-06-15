6da0a1fedddf9249d66982a1b9a7fc5000809749
fzyzcjy <ch271828n@outlook.com>
2024-06-15 14:40:12 +0800

    chore: mv

---
+++ frb_example/temp_demo/src/demo_ouroboros.rs
@@ -0,0 +1,11 @@
+use ouroboros::self_referencing;
+
+#[self_referencing]
+pub struct LeafNodeIter {
+    node: String,
+    #[borrows(node)]
+    #[covariant]
+    iter: std::slice::Iter<'this, String>,
+}
+
+pub fn main() {}
