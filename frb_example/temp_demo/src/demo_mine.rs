bce9d246f154fb1373cf5d60bc4e4e7f08692c5d
fzyzcjy <ch271828n@outlook.com>
2024-06-15 15:22:56 +0800

    feat: more

--- frb_example/temp_demo/src/demo_mine.rs
+++ frb_example/temp_demo/src/demo_mine.rs
@@ -3,12 +3,15 @@ use std::any::Any;
 use std::sync::Arc;
 use tokio::sync::{RwLock, RwLockReadGuard};
 
-struct WithOwner<T> {
+struct WithOwnerGuard {
     // NOTE order of fields!
-    value: T,
+    value: RwLockReadGuard<'static, One>,
     owners: Vec<Box<dyn Any>>,
 }
 
-fn new<O, T>(owner: O, guard_builder: impl for<'this> FnOnce(&'this O) -> T<'this>) {
+fn new(
+    owner: One,
+    guard_builder: impl for<'this> FnOnce(&'this One) -> RwLockReadGuard<'this, One>,
+) -> WithOwnerGuard {
     TODO
 }
