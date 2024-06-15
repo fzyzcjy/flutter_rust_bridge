0fc12b9510b2c1ea63fc6a60740bbcbffc2f582d
fzyzcjy <ch271828n@outlook.com>
2024-06-15 15:39:51 +0800

    feat: more

--- frb_example/temp_demo/src/demo_yoke.rs
+++ frb_example/temp_demo/src/demo_yoke.rs
@@ -2,11 +2,15 @@ use std::borrow::Cow;
 use std::rc::Rc;
 use yoke::Yoke;
 
-fn load_object(filename: &str) -> Yoke<Cow<'static, str>, Rc<[u8]>> {
+fn load_object(filename: &str) -> Yoke<Cow<'static, [u8]>, Rc<[u8]>> {
     let rc: Rc<[u8]> = load_from_cache(filename);
-    Yoke::<Cow<'static, str>, Rc<[u8]>>::attach_to_cart(rc, |data: &[u8]| {
+    Yoke::<Cow<'static, [u8]>, Rc<[u8]>>::attach_to_cart(rc, |data: &[u8]| {
         Cow::Borrowed(&data[1..5])
     })
+}
+
+fn load_from_cache(_filename: &str) -> Rc<[u8]> {
+    Rc::new(vec![1u8, 2, 3, 4, 5].into())
 }
 
 pub fn main() {
