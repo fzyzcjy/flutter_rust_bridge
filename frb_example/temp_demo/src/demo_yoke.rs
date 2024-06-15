604719f9d61cd33c5dc36838ff4e59aed33c1e15
fzyzcjy <ch271828n@outlook.com>
2024-06-15 15:46:37 +0800

    feat: more

--- frb_example/temp_demo/src/demo_yoke.rs
+++ frb_example/temp_demo/src/demo_yoke.rs
@@ -5,16 +5,16 @@ use std::sync::Arc;
 use tokio::sync::{RwLock, RwLockReadGuard};
 use yoke::{Yoke, Yokeable};
 
-#[derive(Yokeable)]
+#[derive(Yokeable, Debug)]
 struct RwLockReadGuardOne<'a>(RwLockReadGuard<'a, One>);
 
-fn load_object(filename: &str) -> Yoke<RwLockReadGuardOne<'static>, Arc<RwLock<One>>> {
+fn load_object() -> Yoke<RwLockReadGuardOne<'static>, Arc<RwLock<One>>> {
     let one: Arc<RwLock<One>> = Arc::new(RwLock::new(One("hi_one".to_owned())));
     Yoke::attach_to_cart(one, |one: &RwLock<One>| RwLockReadGuardOne(one.blocking_read()))
 }
 
 pub fn main() {
-    let yoke = load_object("filename.bincode");
+    let yoke = load_object();
     println!("yoke={yoke:?}");
     // TODO
     // assert_eq!(&**yoke.get(), [2u8, 3]);
