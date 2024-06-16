50f8730752df092bf0558ab695335571b8716584
fzyzcjy <ch271828n@outlook.com>
2024-06-16 15:32:02 +0800

    chore: more

--- frb_codegen/src/library/codegen/parser/mir/parser/ty/lifetimeable.rs
+++ frb_codegen/src/library/codegen/parser/mir/parser/ty/lifetimeable.rs
@@ -15,29 +15,29 @@ impl<'a, 'b, 'c> TypeParserWithContext<'a, 'b, 'c> {
     ) -> anyhow::Result<MirType> {
         let ty_str = &original.raw.string.with_original_lifetime();
         let ty: Type = syn::parse_str(ty_str)?;
+
         let lifetimes = LifetimeExtractor::extract_skipping_static(&ty);
-
-        if !lifetimes.is_empty() {
-            let delegate_ty_str = format!(
-                "flutter_rust_bridge::for_generated::Lifetimeable<{}>",
-                replace_lifetimes_to_static(ty_str, &lifetimes)
-            );
-
-            let inner_dart_api_type = original.inner.sanitized_type();
-
-            return Ok(MirType::Delegate(MirTypeDelegate::Lifetimeable(
-                MirTypeDelegateLifetimeable {
-                    api_type: Box::new(MirType::RustAutoOpaqueImplicit(original)),
-                    delegate: self.parse_rust_auto_opaque_explicit_typed(
-                        &syn::parse_str(&delegate_ty_str)?,
-                        namespace,
-                        None,
-                        Some(inner_dart_api_type),
-                    )?,
-                },
-            )));
+        if lifetimes.is_empty() {
+            return Ok(MirType::RustAutoOpaqueImplicit(original));
         }
 
-        Ok(MirType::RustAutoOpaqueImplicit(original))
+        let delegate_ty_str = format!(
+            "flutter_rust_bridge::for_generated::Lifetimeable<{}>",
+            replace_lifetimes_to_static(ty_str, &lifetimes)
+        );
+
+        let inner_dart_api_type = original.inner.sanitized_type();
+
+        return Ok(MirType::Delegate(MirTypeDelegate::Lifetimeable(
+            MirTypeDelegateLifetimeable {
+                api_type: Box::new(MirType::RustAutoOpaqueImplicit(original)),
+                delegate: self.parse_rust_auto_opaque_explicit_typed(
+                    &syn::parse_str(&delegate_ty_str)?,
+                    namespace,
+                    None,
+                    Some(inner_dart_api_type),
+                )?,
+            },
+        )));
     }
 }
