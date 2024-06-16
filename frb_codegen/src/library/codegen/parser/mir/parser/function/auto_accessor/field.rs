41eb50fc9238c4cc3f744b709977eea8788717d9
fzyzcjy <ch271828n@outlook.com>
2024-06-16 09:42:18 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/mir/parser/function/auto_accessor/field.rs
+++ frb_codegen/src/library/codegen/parser/mir/parser/function/auto_accessor/field.rs
@@ -36,7 +36,7 @@ pub(super) fn parse_auto_accessor_of_field(
 
     let owner = MirFuncOwnerInfoMethod {
         owner_ty: ty_direct_parse.to_owned(),
-        owner_ty_raw: TODO,
+        owner_ty_raw: struct_name.name.to_owned(),
         actual_method_name: rust_method_name,
         actual_method_dart_name: Some(field.name.rust_style().to_owned()),
         mode: MirFuncOwnerInfoMethodMode::Instance,
