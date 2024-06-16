ec9cad078510f7cccac0d80ea047ff1d45be30c1
fzyzcjy <ch271828n@outlook.com>
2024-06-16 13:52:55 +0800

    chore: rename

--- frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/misc/function/lifetime.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/misc/function/lifetime.rs
@@ -16,7 +16,7 @@ pub(super) fn generate_code_inner_decode(func: &MirFunc, inner: &str) -> String 
 
 pub(super) fn generate_illegal_static_reference(var_name: &str) -> String {
     format!(
-        "let {var_name}_illegal_static_reference = unsafe {{
+        "let {var_name}_illegal_static_ref = unsafe {{
             flutter_rust_bridge::for_generated::ouroboros_change_lifetime(&{var_name})
         }};"
     )
