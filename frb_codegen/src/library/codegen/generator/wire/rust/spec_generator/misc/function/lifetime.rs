a1cdfed04db980540b00b8bc670a8a1b58e87f59
fzyzcjy <ch271828n@outlook.com>
2024-06-16 13:52:33 +0800

    feat: more

--- frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/misc/function/lifetime.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/misc/function/lifetime.rs
@@ -1,9 +1,17 @@
-use crate::codegen::ir::mir::func::MirFunc;
+use crate::codegen::ir::mir::func::{MirFunc, MirFuncInput};
+use itertools::Itertools;
 
 pub(super) fn generate_code_inner_decode(func: &MirFunc, inner: &str) -> String {
-    format!(
-        "{inner}"
-    )
+    let object_create_static_ref = (func.inputs.iter())
+        .map(|field| {
+            generate_illegal_static_reference(&format!(
+                "api_{name}",
+                name = get_variable_name(field)
+            ))
+        })
+        .join("");
+
+    format!("{object_create_static_ref}{inner}")
 }
 
 pub(super) fn generate_illegal_static_reference(var_name: &str) -> String {
@@ -12,4 +20,8 @@ pub(super) fn generate_illegal_static_reference(var_name: &str) -> String {
             flutter_rust_bridge::for_generated::ouroboros_change_lifetime(&{var_name})
         }};"
     )
+}
+
+fn get_variable_name(field: &MirFuncInput) -> String {
+    field.inner.name.rust_style()
 }
