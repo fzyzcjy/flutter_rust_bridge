aeb98c091622f016512def31b8f5ecb895b83d93
fzyzcjy <ch271828n@outlook.com>
2024-06-16 11:28:03 +0800

    chore: more

--- frb_codegen/src/library/codegen/parser/mir/parser/function/real/lifetime.rs
+++ frb_codegen/src/library/codegen/parser/mir/parser/function/real/lifetime.rs
@@ -1,6 +1,8 @@
 use syn::Signature;
 
 pub(crate) fn parse_function_lifetime(sig: &Signature) -> ParseFunctionLifetimeOutput {
+    let inputs_lifetimes: Vec<Vec<TODO>> = TODO;
+    let output_lifetimes: Vec<TODO> = TODO;
     todo!()
 }
 
