26bcffdfc85db73cb2628530c118d33e27b42b08
fzyzcjy <ch271828n@outlook.com>
2024-06-16 13:44:32 +0800

    chore: func

--- frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/misc/function/lockable.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/misc/function/lockable.rs
@@ -121,3 +121,11 @@ pub(crate) fn generate_inner_func_arg_ownership(field: &MirFuncInput) -> String 
             .to_owned(),
     }
 }
+
+fn generate_illegal_static_reference(var_name: &str) -> String {
+    format!(
+        "let {var_name}_illegal_static_reference = unsafe {{
+            flutter_rust_bridge::for_generated::ouroboros_change_lifetime(&{var_name})
+        }};"
+    )
+}
