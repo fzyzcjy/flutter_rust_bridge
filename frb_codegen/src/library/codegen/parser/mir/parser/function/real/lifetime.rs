2fed02b228226fc885f80225b5adeeb69a44e2de
fzyzcjy <ch271828n@outlook.com>
2024-06-16 11:31:37 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/mir/parser/function/real/lifetime.rs
+++ frb_codegen/src/library/codegen/parser/mir/parser/function/real/lifetime.rs
@@ -1,9 +1,24 @@
 use crate::codegen::parser::mir::parser::lifetime_extractor::Lifetime;
+use anyhow::ensure;
+use itertools::{concat, Itertools};
+use std::collections::HashSet;
 use syn::Signature;
 
-pub(crate) fn parse_function_lifetime(sig: &Signature) -> ParseFunctionLifetimeOutput {
-    let inputs_lifetimes: Vec<Vec<Lifetime>> = etod;
+pub(crate) fn parse_function_lifetime(
+    sig: &Signature,
+) -> anyhow::Result<ParseFunctionLifetimeOutput> {
+    let inputs_lifetimes: Vec<Vec<Lifetime>> = TODO;
     let output_lifetimes: Vec<Lifetime> = TODO;
+
+    let all_lifetimes = (inputs_lifetimes.iter().flatten())
+        .chain(output_lifetimes.iter())
+        .collect::<HashSet<_>>();
+    ensure!(
+        all_lifetimes.len() <= 1,
+        "Only support <=1 lifetime specifiers yet, but see {:?}",
+        all_lifetimes
+    );
+
     todo!()
 }
 
