e1317eb264bc07617100a15cb42169f2d9aa1e39
fzyzcjy <ch271828n@outlook.com>
2024-06-16 08:31:44 +0800

    feat: deref

--- frb_rust/src/lifetimeable/mod.rs
+++ frb_rust/src/lifetimeable/mod.rs
@@ -1,6 +1,8 @@
 pub(crate) mod lifetime_changer;
 
+use crate::for_generated::{BaseArc, RustOpaqueBase};
 use std::any::Any;
+use std::ops;
 
 pub struct Lifetimeable<T> {
     // NOTE: The borrowed value must be *before* the dependency values to have correct *drop order*
@@ -14,5 +16,19 @@ impl<T> Lifetimeable<T> {
             value,
             dependencies,
         }
+    }
+}
+
+impl<T> ops::Deref for Lifetimeable<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.value
+    }
+}
+
+impl<T> ops::DerefMut for Lifetimeable<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.value
     }
 }
