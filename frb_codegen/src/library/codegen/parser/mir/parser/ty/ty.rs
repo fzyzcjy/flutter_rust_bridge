84fbf478a22a9325358e87458ea6477060d1bed2
fzyzcjy <ch271828n@outlook.com>
2024-06-17 10:53:59 +0800

    chore: temp logs

--- frb_codegen/src/library/codegen/parser/mir/parser/ty/ty.rs
+++ frb_codegen/src/library/codegen/parser/mir/parser/ty/ty.rs
@@ -3,11 +3,14 @@ use crate::codegen::parser::mir::parser::ty::misc::convert_ident_str;
 use crate::codegen::parser::mir::parser::ty::{TypeParserParsingContext, TypeParserWithContext};
 use anyhow::Context;
 use syn::Type;
+use crate::utils::syn_utils::ty_to_string;
 
 impl<'a, 'b, 'c> TypeParserWithContext<'a, 'b, 'c> {
     pub(crate) fn parse_type(&mut self, ty: &Type) -> anyhow::Result<MirType> {
         let resolve_ty = self.resolve_alias(ty);
-        self.parse_type_inner(&resolve_ty)
+        let ans = self.parse_type_inner(&resolve_ty)?;
+        log::warn!("TypeParserWithContext.parse_type ty={} ans={ans:?}", ty_to_string(ty));
+        Ok(ans)
     }
 
     pub(crate) fn parse_type_with_context(
