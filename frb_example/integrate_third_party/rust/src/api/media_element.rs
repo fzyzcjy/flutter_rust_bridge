0e413123808bb319f99dbf2b9f678cf9c538201a
fzyzcjy <ch271828n@outlook.com>
2024-06-17 13:09:15 +0800

    feat: call

--- frb_example/integrate_third_party/rust/src/api/media_element.rs
+++ frb_example/integrate_third_party/rust/src/api/media_element.rs
@@ -1,8 +1,26 @@
+use delegate_attr::delegate;
 use flutter_rust_bridge::frb;
 
 #[frb(opaque)]
 pub struct MediaElement(web_audio_api::MediaElement);
 
+#[delegate(self.0)]
 impl MediaElement {
+    pub fn current_time(&self) -> f64 {}
 
+    pub fn set_current_time(&self, value: f64) {}
+
+    pub fn loop_(&self) -> bool {}
+
+    pub fn set_loop(&self, value: bool) {}
+
+    pub fn play(&self) {}
+
+    pub fn pause(&self) {}
+
+    pub fn paused(&self) -> bool {}
+
+    pub fn playback_rate(&self) -> f64 {}
+
+    pub fn set_playback_rate(&self, value: f64) {}
 }
