e3838cd3b2030e1979a0fbaa091fac7c3d1b86a0
fzyzcjy <ch271828n@outlook.com>
2024-06-17 22:37:31 +0800

    chore: codegen

--- frb_example/integrate_third_party/lib/src/rust/third_party/web_audio_api/media_recorder.dart
+++ frb_example/integrate_third_party/lib/src/rust/third_party/web_audio_api/media_recorder.dart
@@ -5,6 +5,7 @@
 
 import '../../frb_generated.dart';
 import '../web_audio_api.dart';
+import 'media_streams.dart';
 import 'node.dart';
 import 'package:flutter_rust_bridge/flutter_rust_bridge_for_generated.dart';
 
