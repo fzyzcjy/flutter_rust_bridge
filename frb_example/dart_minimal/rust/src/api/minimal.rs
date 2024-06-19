3bd1bda36dd899678bfa3837a48681017d770f0f
fzyzcjy <ch271828n@outlook.com>
2024-06-19 10:15:47 +0800

    chroe: repro

--- frb_example/dart_minimal/rust/src/api/minimal.rs
+++ frb_example/dart_minimal/rust/src/api/minimal.rs
@@ -1,3 +1,5 @@
+use bigdecimal::BigDecimal;
+use num_rational::Ratio;
 use flutter_rust_bridge::frb;
 
 #[frb(init)]
@@ -7,4 +9,24 @@ pub fn init_app() {
 
 pub fn minimal_adder(a: i32, b: i32) -> i32 {
     a + b
+}
+
+#[derive(Clone, Debug)]
+pub enum SpecificMetadata {
+    Image {
+        width: u32,
+        height: u32,
+        pixel_aspect: Ratio<u32>,
+    },
+    Video {
+        width: u32,
+        height: u32,
+        pixel_aspect: Ratio<u32>,
+        number_of_frames: usize,
+        frames_per_second: BigDecimal,
+    },
+}
+
+pub fn my_func(a: SpecificMetadata) -> SpecificMetadata {
+    a
 }
