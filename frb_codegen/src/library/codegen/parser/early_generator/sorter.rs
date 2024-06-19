d4c5aa1fc04e31c2693877dc0c363960b19be02c
fzyzcjy <ch271828n@outlook.com>
2024-06-19 17:23:54 +0800

    feat: sort more

--- frb_codegen/src/library/codegen/parser/early_generator/sorter.rs
+++ frb_codegen/src/library/codegen/parser/early_generator/sorter.rs
@@ -3,4 +3,7 @@ use crate::codegen::parser::hir::flat::transformer::sort_transformer::sort_hir_f
 
 pub(crate) fn generate(pack: &mut IrEarlyGeneratorPack) {
     sort_hir_flat_pack(&mut pack.hir_flat_pack);
+
+    (pack.proxied_types).sort_by_cached_key(|x| x.original_ty.safe_ident());
+    (pack.trait_def_infos).sort_by_cached_key(|x| x.trait_def_name.clone());
 }
