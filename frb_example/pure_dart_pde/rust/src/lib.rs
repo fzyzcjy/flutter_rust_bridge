c6c2a4bdc791548ab9aa8898f4963e1846684364
fzyzcjy <ch271828n@outlook.com>
2024-06-20 12:26:29 +0800

    chore: codegen

--- frb_example/pure_dart_pde/rust/src/lib.rs
+++ frb_example/pure_dart_pde/rust/src/lib.rs
@@ -1,8 +1,8 @@
 // AUTO-GENERATED FROM frb_example/pure_dart, DO NOT EDIT
 
 pub mod api;
-mod auxiliary;
-mod deliberate_name_conflict;
+pub mod auxiliary;
+pub mod deliberate_name_conflict;
 mod frb_generated; /* AUTO INJECTED BY flutter_rust_bridge. This line may not be accurate, and you can change it according to your needs. */
 
 pub fn function_at_lib_rs() {}
