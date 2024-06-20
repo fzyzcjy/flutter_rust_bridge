f95c934f6438636f6b3b10a6c0a6e05d5c96cfc2
fzyzcjy <ch271828n@outlook.com>
2024-06-20 12:25:25 +0800

    feat: more

--- frb_example/pure_dart/rust/src/lib.rs
+++ frb_example/pure_dart/rust/src/lib.rs
@@ -1,6 +1,6 @@
 pub mod api;
-mod auxiliary;
-mod deliberate_name_conflict;
+pub mod auxiliary;
+pub mod deliberate_name_conflict;
 mod frb_generated; /* AUTO INJECTED BY flutter_rust_bridge. This line may not be accurate, and you can change it according to your needs. */
 
 pub fn function_at_lib_rs() {}
