54f1b060c018e1dc9491c23e10e492a8ec236b1d
fzyzcjy <ch271828n@outlook.com>
2024-06-20 09:32:15 +0800

    feat: demo

--- frb_example/dart_minimal/rust/src/api/minimal.rs
+++ frb_example/dart_minimal/rust/src/api/minimal.rs
@@ -8,3 +8,18 @@ pub fn init_app() {
 pub fn minimal_adder(a: i32, b: i32) -> i32 {
     a + b
 }
+
+use flutter_rust_bridge::for_generated::anyhow;
+
+#[frb(opaque)]
+pub struct MyStructWithTryFromTwinNormal(String);
+
+// #2103
+impl TryFrom<String> for MyStructWithTryFromTwinNormal {
+    type Error = anyhow::Error;
+
+    #[frb(sync)]
+    fn try_from(value: String) -> anyhow::Result<Self> {
+        Ok(Self(value))
+    }
+}
