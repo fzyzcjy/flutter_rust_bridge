c6340f47d36e8bc6569dd4dcc2b8590822143e73
fzyzcjy <ch271828n@outlook.com>
2024-06-20 12:36:02 +0800

    chore: lint

--- frb_example/pure_dart/rust/src/api/deliberately_ignored_module.rs
+++ frb_example/pure_dart/rust/src/api/deliberately_ignored_module.rs
@@ -1,3 +1,4 @@
 // FRB_INTERNAL_GENERATOR: {"forbiddenDuplicatorModes": ["sync", "rustAsync", "sse", "sync sse", "rustAsync sse"]}
 
+#[allow(dead_code)]
 pub fn this_function_should_be_ignored_because_of_mod_level_ignore() {}
--- frb_example/pure_dart/rust/src/auxiliary/benchmark_raw.rs
+++ frb_example/pure_dart/rust/src/auxiliary/benchmark_raw.rs
@@ -1,4 +1,5 @@
 #![allow(non_camel_case_types)]
+#![allow(clippy::missing_safety_doc)]
 
 use crate::api::pseudo_manual::benchmark_api::benchmark_input_bytes_twin_normal;
 use flutter_rust_bridge::for_generated::{new_leak_vec_ptr, vec_from_leak_ptr};
