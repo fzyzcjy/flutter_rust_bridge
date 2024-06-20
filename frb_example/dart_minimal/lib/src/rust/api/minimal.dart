7f33859d7f999429b189b4443320ef46083762c2
fzyzcjy <ch271828n@outlook.com>
2024-06-20 09:33:16 +0800

    chore: demo

--- frb_example/dart_minimal/lib/src/rust/api/minimal.dart
+++ frb_example/dart_minimal/lib/src/rust/api/minimal.dart
@@ -6,5 +6,7 @@
 import '../frb_generated.dart';
 import 'package:flutter_rust_bridge/flutter_rust_bridge_for_generated.dart';
 
+// These types are ignored because they are not used by any `pub` functions: `MyStructWithTryFromTwinNormal`
+
 Future<int> minimalAdder({required int a, required int b}) =>
     RustLib.instance.api.crateApiMinimalMinimalAdder(a: a, b: b);
