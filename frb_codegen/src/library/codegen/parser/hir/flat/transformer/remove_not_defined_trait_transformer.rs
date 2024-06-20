556b6d49897bbe650d85c2ae6cb0502cc09ab56b
fzyzcjy <ch271828n@outlook.com>
2024-06-20 09:46:43 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/hir/flat/transformer/remove_not_defined_trait_transformer.rs
+++ frb_codegen/src/library/codegen/parser/hir/flat/transformer/remove_not_defined_trait_transformer.rs
@@ -1,5 +1,6 @@
 use crate::codegen::ir::hir::flat::function::HirFlatFunctionOwner;
 use crate::codegen::ir::hir::flat::pack::HirFlatPack;
+use crate::codegen::ir::misc::skip::IrValueOrSkip;
 use itertools::Itertools;
 use std::collections::HashSet;
 
@@ -7,18 +8,23 @@ pub(crate) fn transform(mut pack: HirFlatPack) -> anyhow::Result<HirFlatPack> {
     let good_trait_names: HashSet<String> =
         pack.traits.iter().map(|t| t.name.name.clone()).collect();
 
-    pack.functions = (pack.functions.drain(..))
-        .filter(|f| {
-            if let HirFlatFunctionOwner::StructOrEnum {
-                trait_def_name: Some(trait_def_name),
-                ..
-            } = &f.owner
-            {
-                good_trait_names.contains(trait_def_name)
-            } else {
-                true
-            }
-        })
-        .collect_vec();
+    let (funcs, skips) = IrValueOrSkip::split(
+        (pack.functions.drain(..))
+            .filter(|f| {
+                if let HirFlatFunctionOwner::StructOrEnum {
+                    trait_def_name: Some(trait_def_name),
+                    ..
+                } = &f.owner
+                {
+                    good_trait_names.contains(trait_def_name)
+                } else {
+                    true
+                }
+            })
+            .collect_vec(),
+    );
+    pack.functions = funcs;
+    pack.skips += skips;
+
     Ok(pack)
 }
