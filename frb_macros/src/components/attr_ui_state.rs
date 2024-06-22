1537b498e7284bd9cbd7d4da0ce1a179a56ae0ad
fzyzcjy <ch271828n@outlook.com>
2024-06-22 22:20:24 +0800

    chore: lint

--- frb_macros/src/components/attr_ui_state.rs
+++ frb_macros/src/components/attr_ui_state.rs
@@ -5,7 +5,7 @@ use syn::{parse_macro_input, ItemStruct};
 // This is surely executed - otherwise how can one use any `#[frb]` macro
 // but coverage tool does not think so, possibly because it is done in build time
 // frb-coverage:ignore-start
-pub(crate) fn handle( item: proc_macro::TokenStream) -> proc_macro::TokenStream {
+pub(crate) fn handle(item: proc_macro::TokenStream) -> proc_macro::TokenStream {
     // ref: https://users.rust-lang.org/t/solved-derive-and-proc-macro-add-field-to-an-existing-struct/52307
     let mut ast = parse_macro_input!(item as ItemStruct);
     if let syn::Fields::Named(ref mut fields) = ast.fields {
@@ -26,6 +26,7 @@ pub(crate) fn handle( item: proc_macro::TokenStream) -> proc_macro::TokenStream 
                 self.base_state = base_state;
             }
         }
-    }).into()
+    })
+    .into()
 }
 // frb-coverage:ignore-end
