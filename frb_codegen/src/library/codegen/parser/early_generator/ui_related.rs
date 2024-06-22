31ab60f921d9dd04d8d8c67c34584c463c43e0e1
fzyzcjy <ch271828n@outlook.com>
2024-06-22 10:27:57 +0800

    feat: should

--- frb_codegen/src/library/codegen/parser/early_generator/ui_related.rs
+++ frb_codegen/src/library/codegen/parser/early_generator/ui_related.rs
@@ -1,7 +1,22 @@
 use crate::codegen::ir::early_generator::pack::IrEarlyGeneratorPack;
+use crate::codegen::parser::mir::parser::attribute::FrbAttributes;
 
 pub(crate) fn generate(pack: &mut IrEarlyGeneratorPack) -> anyhow::Result<()> {
+    if !should_enable_ui(pack)? {
+        return Ok(());
+    }
+    
     TODO;
 
     Ok(())
+}
+
+fn should_enable_ui(pack: &mut IrEarlyGeneratorPack) -> anyhow::Result<bool> {
+    for ty in &pack.hir_flat_pack.structs {
+        let attr = FrbAttributes::parse(&ty.src.attrs)?;
+        if attr.ui_state() {
+            return Ok(true);
+        }
+    }
+    Ok(false)
 }
