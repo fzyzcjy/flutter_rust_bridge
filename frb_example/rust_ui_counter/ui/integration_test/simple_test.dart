62221d7bca5bfd218244321086035ba8ae5d5709
fzyzcjy <ch271828n@outlook.com>
2024-06-22 15:08:59 +0800

    chore: more

--- frb_example/rust_ui_counter/ui/integration_test/simple_test.dart
+++ frb_example/rust_ui_counter/ui/integration_test/simple_test.dart
@@ -1,13 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:frb_example_rust_ui_counter/main.dart';
 import 'package:frb_example_rust_ui_counter/src/rust/frb_generated.dart';
 import 'package:integration_test/integration_test.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   setUpAll(() async => await RustLib.init());
-  testWidgets('Can call rust function', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
-    expect(find.textContaining('Result: `Hello, Tom!`'), findsOneWidget);
+  testWidgets('todo', (WidgetTester tester) async {
+    // TODO
   });
 }
