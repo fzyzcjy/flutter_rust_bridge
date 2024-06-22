ee4d43adf94d5ed10b26af6e42fd6909d4b20fca
fzyzcjy <ch271828n@outlook.com>
2024-06-22 10:09:27 +0800

    more

--- frb_macros/src/components/attr_ui_state.rs
+++ frb_macros/src/components/attr_ui_state.rs
@@ -1,26 +1,29 @@
-use proc_macro::quote;
 use proc_macro2::TokenStream;
-use quote::format_ident;
+use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
+use syn::parse::Parser;
 
 // This is surely executed - otherwise how can one use any `#[frb]` macro
 // but coverage tool does not think so, possibly because it is done in build time
 // frb-coverage:ignore-start
 pub(crate) fn handle(attribute: TokenStream, item: proc_macro::TokenStream) -> TokenStream {
-    let input = parse_macro_input!(item as DeriveInput);
+    let mut ast: DeriveInput = parse_macro_input!(item as DeriveInput);
 
-    let name = input.ident.clone();
-    let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
-
-    let field_name = format_ident!("my_new_field");
-    let field_ty = syn::parse_str("i32").unwrap();
-
-    quote! {
-        #(#input.attrs)*
-        pub struct #name #ty_generics #where_clause {
-            #(#input.fields),*
-            pub #field_name: #field_ty,
+    // ref: https://users.rust-lang.org/t/solved-derive-and-proc-macro-add-field-to-an-existing-struct/52307
+    match &mut ast.data {
+        syn::Data::Struct(ref mut data_struct) => {
+            match &mut data_struct.fields {
+                syn::Fields::Named(fields) => {
+                    fields.named.push(
+                        syn::Field::parse_named.parse2(quote! { pub a: String }).unwrap(),
+                    );
+                }
+                _ => {}
+            }
         }
+        _ => panic!("`add_field` has to be used with structs "),
     }
+
+    quote! { #ast }
 }
 // frb-coverage:ignore-end
