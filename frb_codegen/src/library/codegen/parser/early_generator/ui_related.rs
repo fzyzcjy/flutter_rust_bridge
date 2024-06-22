89e89899cc3315d8273aa63f6717de11990da29f
fzyzcjy <ch271828n@outlook.com>
2024-06-22 11:27:22 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/early_generator/ui_related.rs
+++ frb_codegen/src/library/codegen/parser/early_generator/ui_related.rs
@@ -18,10 +18,12 @@ pub(crate) fn generate(
         &mut pack.hir_flat_pack,
         output_namespace,
         &[InjectExtraCodeBlock {
-            code: generate_boilerplate(),
+            code: generate_rust_boilerplate(),
             should_parse: true,
         }],
     )?;
+
+    pack.hir_flat_pack.extra_dart_output_code += generate_dart_boilerplate();
 
     Ok(())
 }
@@ -36,7 +38,7 @@ fn should_enable_ui(pack: &mut IrEarlyGeneratorPack) -> anyhow::Result<bool> {
     Ok(false)
 }
 
-fn generate_boilerplate() -> String {
+fn generate_rust_boilerplate() -> String {
     r###"
 #[flutter_rust_bridge::frb(opaque)]
 #[flutter_rust_bridge::frb(dart_code = r#"
@@ -66,6 +68,121 @@ impl BaseRustState {
     pub(crate) fn on_mutation(&self) {
         self.notify_ui.as_ref().unwrap().add(()).unwrap()
     }
+}
+    "###
+    .to_owned()
+}
+
+fn generate_dart_boilerplate() -> String {
+    r###"
+import 'package:flutter/material.dart';
+
+Future<void> runRustApp({
+  required Widget Function(RustState state) body,
+  required RustState Function() state,
+}) async {
+  await RustLib.init();
+  runApp(_MyApp(body: body, state: state()));
+}
+
+// improve typing later
+class _MyApp extends StatefulWidget {
+  final Widget Function(RustState state) body;
+  final RustState state;
+
+  const _MyApp({
+    required this.body,
+    required this.state,
+  });
+
+  @override
+  State<_MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<_MyApp> {
+  late final BaseRustState baseState;
+
+  @override
+  void initState() {
+    super.initState();
+    baseState = BaseRustState(onMutate: () {
+      if (mounted) setState(() {});
+    });
+    widget.state.setBaseState(baseState: baseState);
+  }
+
+  @override
+  void dispose() {
+    baseState.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: Scaffold(body: widget.body(widget.state)),
+    );
+  }
+}
+
+/// Adapted from https://github.com/mobxjs/mobx.dart/issues/750
+// TODO: Move to support library instead of inlining here
+class SyncTextField extends StatefulWidget {
+  final String text;
+
+  // forward
+  final ValueChanged<String>? onChanged;
+  final InputDecoration? decoration;
+  final ValueChanged<String>? onSubmitted;
+
+  const SyncTextField({
+    super.key,
+    required this.text,
+    this.onChanged,
+    this.decoration,
+    this.onSubmitted,
+  });
+
+  @override
+  State<SyncTextField> createState() => _SyncTextFieldState();
+}
+
+class _SyncTextFieldState extends State<SyncTextField> {
+  late final TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+    _controller.text = widget.text;
+  }
+
+  @override
+  void didUpdateWidget(covariant SyncTextField oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.text != widget.text) _controller.text = widget.text;
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: _controller,
+      // forward
+      onChanged: widget.onChanged,
+      decoration: widget.decoration,
+      onSubmitted: widget.onSubmitted,
+    );
+  }
 }
     "###
     .to_owned()
