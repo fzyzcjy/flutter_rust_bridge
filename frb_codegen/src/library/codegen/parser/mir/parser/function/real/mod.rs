d2bce11295ffe136a7c87bd1eeac1543035dab7a
fzyzcjy <ch271828n@outlook.com>
2024-06-22 09:40:07 +0800

    feat: more

--- frb_codegen/src/library/codegen/parser/mir/parser/function/real/mod.rs
+++ frb_codegen/src/library/codegen/parser/mir/parser/function/real/mod.rs
@@ -84,6 +84,7 @@ impl<'a, 'b> FunctionParser<'a, 'b> {
         default_rust_opaque_codec: RustOpaqueCodecMode,
         enable_lifetime: bool,
         type_64bit_int: bool,
+        default_dart_async: bool,
         parse_mode: ParseMode,
         stop_on_error: bool,
     ) -> anyhow::Result<MirFuncOrSkip> {
@@ -95,6 +96,7 @@ impl<'a, 'b> FunctionParser<'a, 'b> {
             default_rust_opaque_codec,
             enable_lifetime,
             type_64bit_int,
+            default_dart_async,
             parse_mode,
         ) {
             Ok(output) => Ok(output),
@@ -129,6 +131,7 @@ impl<'a, 'b> FunctionParser<'a, 'b> {
         default_rust_opaque_codec: RustOpaqueCodecMode,
         enable_lifetime: bool,
         type_64bit_int: bool,
+        default_dart_async: bool,
         parse_mode: ParseMode,
     ) -> anyhow::Result<MirFuncOrSkip> {
         debug!("parse_function function name: {:?}", func.item_fn.name());
@@ -219,7 +222,7 @@ impl<'a, 'b> FunctionParser<'a, 'b> {
         info = self.transform_fn_info(info);
 
         let codec_mode_pack = compute_codec_mode_pack(&attributes, force_codec_mode_pack);
-        let dart_async = compute_dart_async(&attributes);
+        let dart_async = compute_dart_async(&attributes, default_dart_async);
         let mode = compute_func_mode(dart_async, &info);
         let stream_dart_await = attributes.stream_dart_await() && dart_async;
         let namespace_refined = refine_namespace(&owner).unwrap_or(func.namespace.clone());
@@ -262,8 +265,12 @@ impl<'a, 'b> FunctionParser<'a, 'b> {
     }
 }
 
-fn compute_dart_async(attributes: &FrbAttributes) -> bool {
-    !attributes.sync()
+fn compute_dart_async(attributes: &FrbAttributes, default_dart_async: bool) -> bool {
+    if attributes.sync() {
+        false
+    } else {
+        default_dart_async
+    }
 }
 
 fn should_forbid_type_self_for_inputs(owner: &MirFuncOwnerInfo) -> bool {
