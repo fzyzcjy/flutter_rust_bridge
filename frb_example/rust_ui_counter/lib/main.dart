d964866080f321202a5ab3f98c5dd6b69264bf56
fzyzcjy <ch271828n@outlook.com>
2024-06-22 14:53:58 +0800

    chore: empty

---
+++ frb_example/rust_ui_counter/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:frb_example_rust_ui_counter/src/rust/api/simple.dart';
+import 'package:frb_example_rust_ui_counter/src/rust/frb_generated.dart';
+
+Future<void> main() async {
+  await RustLib.init();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('flutter_rust_bridge quickstart')),
+        body: Center(
+          child: Text(
+              'Action: Call Rust `greet("Tom")`\nResult: `${greet(name: "Tom")}`'),
+        ),
+      ),
+    );
+  }
+}
