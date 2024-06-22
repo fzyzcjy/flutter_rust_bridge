b7c2f5310851d144189c901f030a01c4c0e3a037
fzyzcjy <ch271828n@outlook.com>
2024-06-22 15:26:21 +0800

    chore: fmt

--- frb_example/rust_ui_todo_list/ui/lib/main.dart
+++ frb_example/rust_ui_todo_list/ui/lib/main.dart
@@ -1,15 +1,15 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'package:demo_rust_ui/src/rust/api/app.dart';
-import 'package:demo_rust_ui/src/rust/frb_generated.dart';
 import 'package:flutter/material.dart';
 import 'package:styled_widget/styled_widget.dart';
 
 void main() => runRustApp(body: body, state: RustState.new);
 
-Widget body(RustState state) => [
+Widget body(RustState state) {
+  return [
     SyncTextField(
-      decoration: InputDecoration(hintText: 'Input text and enter to add a todo'),
+      decoration:
+          InputDecoration(hintText: 'Input text and enter to add a todo'),
       text: state.inputText,
       onChanged: (text) => state.inputText = text,
       onSubmitted: (_) => state.add(),
@@ -18,16 +18,23 @@ Widget body(RustState state) => [
       for (final item in state.filteredItems()) todoItem(state, item)
     ]).expanded(),
     [
-      for (final filter in )
+      for (final filter in Filter.values)
         TextButton(
           onPressed: () => state.filter = filter,
-          child: Text(filter.name).textColor(state.filter == filter ? Colors.blue : Colors.black87),
+          child: Text(filter.name)
+              .textColor(state.filter == filter ? Colors.blue : Colors.black87),
         ),
     ].toRow(),
   ].toColumn().padding(all: 16);
+}
 
-Widget todoItem(RustState state, Item item) => [
-    Checkbox(value: item.completed, onChanged: (_) => state.toggle(id: item.id)),
+Widget todoItem(RustState state, Item item) {
+  return [
+    Checkbox(
+        value: item.completed, onChanged: (_) => state.toggle(id: item.id)),
     Text(item.content).expanded(),
-    IconButton(icon: Icon(Icons.close, color: Colors.grey), onPressed: () => state.remove(id: item.id)),
+    IconButton(
+        icon: Icon(Icons.close, color: Colors.grey),
+        onPressed: () => state.remove(id: item.id)),
   ].toRow();
+}
