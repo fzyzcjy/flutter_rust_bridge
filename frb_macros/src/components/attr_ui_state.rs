1bc0364cf58cb47648eed544b9e63e64a540c692
fzyzcjy <ch271828n@outlook.com>
2024-06-22 10:01:12 +0800

    more

--- frb_macros/src/components/attr_ui_state.rs
+++ frb_macros/src/components/attr_ui_state.rs
@@ -1,5 +1,12 @@
+use proc_macro::quote;
 use proc_macro2::TokenStream;
 
+// This is surely executed - otherwise how can one use any `#[frb]` macro
+// but coverage tool does not think so, possibly because it is done in build time
+// frb-coverage:ignore-start
 pub(crate) fn handle(attribute: TokenStream, item: TokenStream) -> TokenStream {
-    TODO
+    quote! {
+        #converted_item
+    }
 }
+// frb-coverage:ignore-end
