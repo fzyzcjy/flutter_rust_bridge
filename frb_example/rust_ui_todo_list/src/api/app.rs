41280e03e44a29bcc298c838370e17dc31528a31
fzyzcjy <ch271828n@outlook.com>
2024-06-22 15:21:11 +0800

    chore: cp

---
+++ frb_example/rust_ui_todo_list/src/api/app.rs
@@ -0,0 +1,20 @@
+use flutter_rust_bridge::frb;
+
+#[frb(ui_state)]
+pub struct RustState {
+    pub count: i32,
+}
+
+impl RustState {
+    pub fn new() -> Self {
+        Self {
+            count: 100,
+            base_state: Default::default(),
+        }
+    }
+
+    #[frb(ui_mutation)]
+    pub fn increment(&mut self) {
+        self.count += 1;
+    }
+}
---
+++ frb_example/rust_ui_todo_list/src/api/mod.rs
@@ -0,0 +1,7 @@
+pub mod app;
+
+#[flutter_rust_bridge::frb(init)]
+pub fn init_app() {
+    // Default utilities - feel free to customize
+    flutter_rust_bridge::setup_default_user_utils();
+}
