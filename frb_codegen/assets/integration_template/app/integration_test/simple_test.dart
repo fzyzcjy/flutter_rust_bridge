cee548cc2e5b8c81302a82ff22b66ae909daf372
mcmah309 <dillonmcmahon79@gmail.com>
2024-06-23 17:27:16 +0000

    feat: Add integration test to template generation

--- frb_codegen/assets/integration_template/app/integration_test/simple_test.dart
+++ frb_codegen/assets/integration_template/app/integration_test/simple_test.dart
@@ -5,7 +5,7 @@ import 'package:integration_test/integration_test.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-  setUpAll(() async => await REPLACE_ME_DART_ENTRYPOINT_CLASS_NAME.init());
+  setUpAll(() async => await RustLib.init());
   testWidgets('Can call rust function', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
     expect(find.textContaining('Result: `Hello, Tom!`'), findsOneWidget);
