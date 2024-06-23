c8c88599671656740f8e7c1b64cfbd4e125a7017
mcmah309 <dillonmcmahon79@gmail.com>
2024-06-23 20:32:03 +0000

    chore: submodule move

--- frb_codegen/assets/integration_template/shared/rust_builder/cargokit/build_tool/bin/build_tool.dart
+++
@@ -1,5 +0,0 @@
-import 'package:build_tool/build_tool.dart' as build_tool;
-
-void main(List<String> arguments) {
-  build_tool.runMain(arguments);
-}
--- frb_codegen/assets/integration_template/shared/rust_builder/cargokit/build_tool/lib/build_tool.dart
+++
@@ -1,5 +0,0 @@
-import 'src/build_tool.dart' as build_tool;
-
-Future<void> runMain(List<String> args) async {
-  return build_tool.runMain(args);
-}
