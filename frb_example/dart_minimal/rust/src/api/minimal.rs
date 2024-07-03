9bd73958b97e91c6a58ce370167afbcfd1315706
fzyzcjy <ch271828n@outlook.com>
2024-07-03 09:29:27 +0800

    pub

--- frb_example/dart_minimal/rust/src/api/minimal.rs
+++ frb_example/dart_minimal/rust/src/api/minimal.rs
@@ -13,6 +13,6 @@ pub trait MyTraitToBeUsedAsBoxDyn {
     fn my_method_twin_normal(&self);
 }
 
-fn function_with_box_dyn_trait(arg: Box<dyn MyTraitToBeUsedAsBoxDyn>) {
+pub fn function_with_box_dyn_trait(arg: Box<dyn MyTraitToBeUsedAsBoxDyn>) {
     let _ = arg;
 }
