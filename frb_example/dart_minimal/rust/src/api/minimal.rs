30d674c9124d5b41838588915399eb45cb31245d
fzyzcjy <ch271828n@outlook.com>
2024-07-03 09:34:37 +0800

    rm

--- frb_example/dart_minimal/rust/src/api/minimal.rs
+++ frb_example/dart_minimal/rust/src/api/minimal.rs
@@ -8,18 +8,3 @@ pub fn init_app() {
 pub fn minimal_adder(a: i32, b: i32) -> i32 {
     a + b
 }
-
-// https://github.com/fzyzcjy/flutter_rust_bridge/issues/2170
-pub trait Issue2170Trait {
-    fn method(&self);
-}
-
-pub struct Issue2170Struct {
-    pub field: Box<dyn Issue2170Trait>,
-}
-
-impl std::fmt::Debug for Issue2170Struct {
-    fn fmt(&self, _f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        unimplemented!()
-    }
-}
