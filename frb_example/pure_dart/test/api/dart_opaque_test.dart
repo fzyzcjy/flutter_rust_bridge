1ea47e7c90325643a41d6c3ec4e13377a3a111a1
fzyzcjy <ch271828n@outlook.com>
2024-07-06 16:49:22 +0800

    chore: test

--- frb_example/pure_dart/test/api/dart_opaque_test.dart
+++ frb_example/pure_dart/test/api/dart_opaque_test.dart
@@ -21,7 +21,7 @@ Future<void> main({bool skipRustLibInit = false}) async {
     expect(back1(), 'Test_String');
     var back2 = await loopBackTwinNormal(opaque: back1) as String Function();
     expect(back2(), 'Test_String');
-    expect(identical(back2, f), isTrue);
+    if (!kIsWeb) expect(identical(back2, f), isTrue);
   });
 
   test('drop', () async {
