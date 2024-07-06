5f632bd2459a950a663cd58769ffbc0f95c5f822
fzyzcjy <ch271828n@outlook.com>
2024-07-06 15:09:37 +0800

    chore: mv

--- frb_dart/lib/src/dart_opaque/_common.dart
+++ frb_dart/lib/src/dart_opaque/_common.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_rust_bridge/src/generalized_frb_rust_binding/generalized_frb_rust_binding.dart';
-import 'package:flutter_rust_bridge/src/platform_types/platform_types.dart';
 
 /// {@macro flutter_rust_bridge.only_for_generated_code}
 Object decodeDartOpaque(
@@ -8,11 +7,4 @@ Object decodeDartOpaque(
     raw = raw.toInt();
   }
   return generalizedFrbRustBinding.dartOpaqueRust2DartDecode(raw);
-}
-
-/// {@macro flutter_rust_bridge.only_for_generated_code}
-PlatformPointer encodeDartOpaque(Object raw, NativePortType dartHandlerPort,
-    GeneralizedFrbRustBinding generalizedFrbRustBinding) {
-  return generalizedFrbRustBinding.dartOpaqueDart2RustEncode(
-      raw, dartHandlerPort);
 }
--- frb_dart/lib/src/dart_opaque/dart_opaque.dart
+++ frb_dart/lib/src/dart_opaque/dart_opaque.dart
@@ -1,2 +1,12 @@
+import 'package:flutter_rust_bridge/src/generalized_frb_rust_binding/generalized_frb_rust_binding.dart';
+import 'package:flutter_rust_bridge/src/platform_types/platform_types.dart';
+
 export '_common.dart';
 export '_io.dart' if (dart.library.js_interop) '_web.dart';
+
+/// {@macro flutter_rust_bridge.only_for_generated_code}
+PlatformPointer encodeDartOpaque(Object raw, NativePortType dartHandlerPort,
+    GeneralizedFrbRustBinding generalizedFrbRustBinding) {
+  return generalizedFrbRustBinding.dartOpaqueDart2RustEncode(
+      raw, dartHandlerPort);
+}
