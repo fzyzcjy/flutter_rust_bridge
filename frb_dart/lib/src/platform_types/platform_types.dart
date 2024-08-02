33e3132600825e60c8ffe3c11b7b7f759cdc506e
Larpoux <larpoux@gmail.com>
2024-08-02 10:13:11 +0200

    Ready for a Pull Request

--- frb_dart/lib/src/platform_types/platform_types.dart
+++ frb_dart/lib/src/platform_types/platform_types.dart
@@ -1,5 +1,3 @@
-import 'package:flutter_rust_bridge/flutter_rust_bridge_for_generated.dart';
-
 export '_io.dart' if (dart.library.js_interop) '_web.dart';
 
 /// {@macro flutter_rust_bridge.internal}
