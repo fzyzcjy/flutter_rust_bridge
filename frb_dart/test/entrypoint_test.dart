34ee4124868c773bc7a28a022fa3d4d7d92d3b47
Tien Do Nam <dev.tien.donam@gmail.com>
2024-08-16 15:14:06 +0200

    test: add unit test for 2248

---
+++ frb_dart/test/entrypoint_test.dart
@@ -0,0 +1,36 @@
+import 'package:flutter_rust_bridge/flutter_rust_bridge_for_generated_common.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('Should be ready when initMock is called', () async {
+    final entrypoint = _FakeBaseEntrypoint();
+
+    // ignore: invalid_use_of_protected_member
+    entrypoint.initMockImpl(api: _FakeApi());
+
+    expect(entrypoint.initialized, true);
+    expect(entrypoint.api, isA<_FakeApi>());
+  });
+}
+
+class _FakeBaseEntrypoint extends BaseEntrypoint {
+  @override
+  get apiImplConstructor => throw UnimplementedError();
+
+  @override
+  String get codegenVersion => throw UnimplementedError();
+
+  @override
+  get defaultExternalLibraryLoaderConfig => throw UnimplementedError();
+
+  @override
+  Future<void> executeRustInitializers() => throw UnimplementedError();
+
+  @override
+  int get rustContentHash => throw UnimplementedError();
+
+  @override
+  get wireConstructor => throw UnimplementedError();
+}
+
+class _FakeApi implements BaseApi {}
