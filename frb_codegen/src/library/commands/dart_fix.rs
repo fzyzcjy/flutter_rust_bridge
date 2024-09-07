3c09d11f6697c244d654239be477729aec604594
Alex Li <github@alexv525.com>
2024-09-07 12:38:12 +0800

    ⚡️ Fix rust format

--- frb_codegen/src/library/commands/dart_fix.rs
+++ frb_codegen/src/library/commands/dart_fix.rs
@@ -10,11 +10,7 @@ use pathdiff::diff_paths;
 use std::path::{Path, PathBuf};
 
 #[allow(clippy::vec_init_then_push)]
-pub fn dart_fix(
-    paths: &[PathBuf],
-    base_path: &Path,
-    extra_extensions: &[&str],
-) -> Result<()> {
+pub fn dart_fix(paths: &[PathBuf], base_path: &Path, extra_extensions: &[&str]) -> Result<()> {
     if paths.is_empty() {
         return Ok(());
     }
@@ -41,7 +37,8 @@ pub(super) fn prepare_paths(
     let base_path_str = path_to_string(base_path)?;
     let normalized_base_path = normalize_windows_unc_path(&base_path_str);
 
-    Ok(paths.iter()
+    Ok(paths
+        .iter()
         .map(|path| {
             let mut path: PathBuf =
                 normalize_windows_unc_path(&path_to_string(path)?).to_owned().into();
@@ -55,7 +52,8 @@ pub(super) fn prepare_paths(
         .into_iter()
         .flat_map(|path| {
             vec![path.clone()].into_iter().chain(
-                extra_extensions.iter()
+                extra_extensions
+                    .iter()
                     .map(move |ext| with_extension(path.clone(), ext))
                     .filter(|path| path.exists()),
             )
