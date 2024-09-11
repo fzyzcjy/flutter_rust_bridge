c648a82905150b823813b2b820dededd2ed6d42a
fzyzcjy <ch271828n@outlook.com>
2024-09-11 20:22:57 +0800

    fix

--- frb_codegen/src/library/commands/dart_fix.rs
+++ frb_codegen/src/library/commands/dart_fix.rs
@@ -3,6 +3,7 @@ use crate::commands::command_runner::call_shell;
 use crate::library::commands::command_runner::check_exit_code;
 use crate::library::commands::dart_format::prepare_paths;
 use anyhow::Result;
+use itertools::Itertools;
 use log::debug;
 use std::path::{Path, PathBuf};
 
@@ -15,12 +16,14 @@ pub fn dart_fix(paths: &[PathBuf], base_path: &Path, extra_extensions: &[&str]) 
     let paths = prepare_paths(paths, base_path, extra_extensions)?;
     debug!("execute dart_fix paths={paths:?}");
 
+    let arg_code = format!("--code={}", paths.iter().map(|x| x.display()).join(","));
+
     let res = command_run!(
         call_shell[Some(base_path), None],
         "dart",
         "fix",
         "--apply",
-        *paths
+        arg_code
     )?;
     check_exit_code(&res)?;
     Ok(())
