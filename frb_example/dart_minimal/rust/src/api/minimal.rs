db2d287d5541b16d679664492354e4580de0a20e
fzyzcjy <ch271828n@outlook.com>
2024-09-12 10:10:52 +0800

    repro

--- frb_example/dart_minimal/rust/src/api/minimal.rs
+++ frb_example/dart_minimal/rust/src/api/minimal.rs
@@ -1,10 +1,29 @@
+use std::net::Ipv4Addr;
+
 use flutter_rust_bridge::frb;
 
-#[frb(init)]
-pub fn init_app() {
-    flutter_rust_bridge::setup_default_user_utils();
+#[flutter_rust_bridge::frb(sync)] // Synchronous mode for simplicity of the demo
+pub fn greet(name: Device) -> String {
+    format!("Hello, {}!", name.ip.to_string())
 }
 
-pub fn minimal_adder(a: i32, b: i32) -> i32 {
-    a + b
+#[frb(rust2dart(dart_type = "InternetAddress", dart_code = "InternetAddress({})"))]
+pub fn encode_ipv4_type(raw: Ipv4Addr) -> String {
+    return raw.to_string();
+}
+
+#[frb(dart2rust(dart_type = "InternetAddress", dart_code = "{}.address"))]
+pub fn decode_ipv4_type(raw: String) -> Ipv4Addr {
+    raw.parse().unwrap()
+}
+
+#[derive(Debug)]
+pub struct Device {
+    pub ip: Ipv4Addr,
+}
+
+#[flutter_rust_bridge::frb(init)]
+pub fn init_app() {
+    // Default utilities - feel free to customize
+    flutter_rust_bridge::setup_default_user_utils();
 }
