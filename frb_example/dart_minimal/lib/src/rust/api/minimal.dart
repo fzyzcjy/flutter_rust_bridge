7faa6ec711d9790c9520813797f92cd71951da15
fzyzcjy <ch271828n@outlook.com>
2024-09-12 09:22:43 +0800

    gen

--- frb_example/dart_minimal/lib/src/rust/api/minimal.dart
+++ frb_example/dart_minimal/lib/src/rust/api/minimal.dart
@@ -4,63 +4,7 @@
 // ignore_for_file: invalid_use_of_internal_member, unused_import, unnecessary_import
 
 import '../frb_generated.dart';
-import '../lib.dart';
 import 'package:flutter_rust_bridge/flutter_rust_bridge_for_generated.dart';
-
-// These function are ignored because they are on traits that is not defined in current crate (put an empty `#[frb]` on it to unignore): `clone`, `fmt`, `fmt`
 
 Future<int> minimalAdder({required int a, required int b}) =>
     RustLib.instance.api.crateApiMinimalMinimalAdder(a: a, b: b);
-
-// Rust type: RustOpaqueMoi<flutter_rust_bridge::for_generated::RustAutoOpaqueInner<NonCloneDataRaw>>
-abstract class NonCloneDataRaw implements RustOpaqueInterface {}
-
-class StructWithRustAutoOpaqueFieldWithManyDerive {
-  final String content;
-
-  const StructWithRustAutoOpaqueFieldWithManyDerive({
-    required this.content,
-  });
-
-  static Future<StructWithRustAutoOpaqueFieldWithManyDerive> default_() =>
-      RustLib.instance.api
-          .crateApiMinimalStructWithRustAutoOpaqueFieldWithManyDeriveDefault();
-
-  Future<void> f() => RustLib.instance.api
-          .crateApiMinimalStructWithRustAutoOpaqueFieldWithManyDeriveF(
-        that: this,
-      );
-
-  @override
-  int get hashCode => content.hashCode;
-
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is StructWithRustAutoOpaqueFieldWithManyDerive &&
-          runtimeType == other.runtimeType &&
-          content == other.content;
-}
-
-class StructWithRustAutoOpaqueWithNonCloneData {
-  final NonCloneDataRaw content;
-
-  const StructWithRustAutoOpaqueWithNonCloneData({
-    required this.content,
-  });
-
-  Future<void> f() => RustLib.instance.api
-          .crateApiMinimalStructWithRustAutoOpaqueWithNonCloneDataF(
-        that: this,
-      );
-
-  @override
-  int get hashCode => content.hashCode;
-
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is StructWithRustAutoOpaqueWithNonCloneData &&
-          runtimeType == other.runtimeType &&
-          content == other.content;
-}
--- frb_example/dart_minimal/lib/src/rust/frb_generated.io.dart
+++ frb_example/dart_minimal/lib/src/rust/frb_generated.io.dart
@@ -8,7 +8,6 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:ffi' as ffi;
 import 'frb_generated.dart';
-import 'lib.dart';
 import 'package:flutter_rust_bridge/flutter_rust_bridge_for_generated_io.dart';
 
 abstract class RustLibApiImplPlatform extends BaseApiImpl<RustLibWire> {
@@ -19,160 +18,26 @@ abstract class RustLibApiImplPlatform extends BaseApiImpl<RustLibWire> {
     required super.portManager,
   });
 
-  CrossPlatformFinalizerArg
-      get rust_arc_decrement_strong_count_NonCloneDataRawPtr => wire
-          ._rust_arc_decrement_strong_count_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerNonCloneDataRawPtr;
-
-  CrossPlatformFinalizerArg get rust_arc_decrement_strong_count_StringPtr => wire
-      ._rust_arc_decrement_strong_count_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerStringPtr;
-
-  @protected
-  NonCloneDataRaw
-      dco_decode_AutoExplicit_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerNonCloneDataRaw(
-          dynamic raw);
-
-  @protected
-  String
-      dco_decode_AutoExplicit_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerString(
-          dynamic raw);
-
-  @protected
-  NonCloneDataRaw
-      dco_decode_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerNonCloneDataRaw(
-          dynamic raw);
-
-  @protected
-  String
-      dco_decode_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerString(
-          dynamic raw);
-
-  @protected
-  StructWithRustAutoOpaqueFieldWithManyDerive
-      dco_decode_box_autoadd_struct_with_rust_auto_opaque_field_with_many_derive(
-          dynamic raw);
-
-  @protected
-  StructWithRustAutoOpaqueWithNonCloneData
-      dco_decode_box_autoadd_struct_with_rust_auto_opaque_with_non_clone_data(
-          dynamic raw);
-
   @protected
   int dco_decode_i_32(dynamic raw);
-
-  @protected
-  StructWithRustAutoOpaqueFieldWithManyDerive
-      dco_decode_struct_with_rust_auto_opaque_field_with_many_derive(
-          dynamic raw);
-
-  @protected
-  StructWithRustAutoOpaqueWithNonCloneData
-      dco_decode_struct_with_rust_auto_opaque_with_non_clone_data(dynamic raw);
 
   @protected
   void dco_decode_unit(dynamic raw);
 
   @protected
-  BigInt dco_decode_usize(dynamic raw);
-
-  @protected
-  NonCloneDataRaw
-      sse_decode_AutoExplicit_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerNonCloneDataRaw(
-          SseDeserializer deserializer);
-
-  @protected
-  String
-      sse_decode_AutoExplicit_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerString(
-          SseDeserializer deserializer);
-
-  @protected
-  NonCloneDataRaw
-      sse_decode_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerNonCloneDataRaw(
-          SseDeserializer deserializer);
-
-  @protected
-  String
-      sse_decode_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerString(
-          SseDeserializer deserializer);
-
-  @protected
-  StructWithRustAutoOpaqueFieldWithManyDerive
-      sse_decode_box_autoadd_struct_with_rust_auto_opaque_field_with_many_derive(
-          SseDeserializer deserializer);
-
-  @protected
-  StructWithRustAutoOpaqueWithNonCloneData
-      sse_decode_box_autoadd_struct_with_rust_auto_opaque_with_non_clone_data(
-          SseDeserializer deserializer);
-
-  @protected
   int sse_decode_i_32(SseDeserializer deserializer);
-
-  @protected
-  StructWithRustAutoOpaqueFieldWithManyDerive
-      sse_decode_struct_with_rust_auto_opaque_field_with_many_derive(
-          SseDeserializer deserializer);
-
-  @protected
-  StructWithRustAutoOpaqueWithNonCloneData
-      sse_decode_struct_with_rust_auto_opaque_with_non_clone_data(
-          SseDeserializer deserializer);
 
   @protected
   void sse_decode_unit(SseDeserializer deserializer);
 
   @protected
-  BigInt sse_decode_usize(SseDeserializer deserializer);
-
-  @protected
   bool sse_decode_bool(SseDeserializer deserializer);
-
-  @protected
-  void
-      sse_encode_AutoExplicit_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerNonCloneDataRaw(
-          NonCloneDataRaw self, SseSerializer serializer);
-
-  @protected
-  void
-      sse_encode_AutoExplicit_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerString(
-          String self, SseSerializer serializer);
-
-  @protected
-  void
-      sse_encode_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerNonCloneDataRaw(
-          NonCloneDataRaw self, SseSerializer serializer);
-
-  @protected
-  void
-      sse_encode_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerString(
-          String self, SseSerializer serializer);
-
-  @protected
-  void
-      sse_encode_box_autoadd_struct_with_rust_auto_opaque_field_with_many_derive(
-          StructWithRustAutoOpaqueFieldWithManyDerive self,
-          SseSerializer serializer);
-
-  @protected
-  void sse_encode_box_autoadd_struct_with_rust_auto_opaque_with_non_clone_data(
-      StructWithRustAutoOpaqueWithNonCloneData self, SseSerializer serializer);
 
   @protected
   void sse_encode_i_32(int self, SseSerializer serializer);
 
   @protected
-  void sse_encode_struct_with_rust_auto_opaque_field_with_many_derive(
-      StructWithRustAutoOpaqueFieldWithManyDerive self,
-      SseSerializer serializer);
-
-  @protected
-  void sse_encode_struct_with_rust_auto_opaque_with_non_clone_data(
-      StructWithRustAutoOpaqueWithNonCloneData self, SseSerializer serializer);
-
-  @protected
   void sse_encode_unit(void self, SseSerializer serializer);
-
-  @protected
-  void sse_encode_usize(BigInt self, SseSerializer serializer);
 
   @protected
   void sse_encode_bool(bool self, SseSerializer serializer);
@@ -191,68 +56,4 @@ class RustLibWire implements BaseWire {
   /// The symbols are looked up in [dynamicLibrary].
   RustLibWire(ffi.DynamicLibrary dynamicLibrary)
       : _lookup = dynamicLibrary.lookup;
-
-  void
-      rust_arc_increment_strong_count_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerNonCloneDataRaw(
-    ffi.Pointer<ffi.Void> ptr,
-  ) {
-    return _rust_arc_increment_strong_count_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerNonCloneDataRaw(
-      ptr,
-    );
-  }
-
-  late final _rust_arc_increment_strong_count_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerNonCloneDataRawPtr =
-      _lookup<ffi.NativeFunction<ffi.Void Function(ffi.Pointer<ffi.Void>)>>(
-          'frbgen_frb_example_dart_minimal_rust_arc_increment_strong_count_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerNonCloneDataRaw');
-  late final _rust_arc_increment_strong_count_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerNonCloneDataRaw =
-      _rust_arc_increment_strong_count_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerNonCloneDataRawPtr
-          .asFunction<void Function(ffi.Pointer<ffi.Void>)>();
-
-  void
-      rust_arc_decrement_strong_count_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerNonCloneDataRaw(
-    ffi.Pointer<ffi.Void> ptr,
-  ) {
-    return _rust_arc_decrement_strong_count_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerNonCloneDataRaw(
-      ptr,
-    );
-  }
-
-  late final _rust_arc_decrement_strong_count_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerNonCloneDataRawPtr =
-      _lookup<ffi.NativeFunction<ffi.Void Function(ffi.Pointer<ffi.Void>)>>(
-          'frbgen_frb_example_dart_minimal_rust_arc_decrement_strong_count_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerNonCloneDataRaw');
-  late final _rust_arc_decrement_strong_count_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerNonCloneDataRaw =
-      _rust_arc_decrement_strong_count_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerNonCloneDataRawPtr
-          .asFunction<void Function(ffi.Pointer<ffi.Void>)>();
-
-  void
-      rust_arc_increment_strong_count_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerString(
-    ffi.Pointer<ffi.Void> ptr,
-  ) {
-    return _rust_arc_increment_strong_count_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerString(
-      ptr,
-    );
-  }
-
-  late final _rust_arc_increment_strong_count_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerStringPtr =
-      _lookup<ffi.NativeFunction<ffi.Void Function(ffi.Pointer<ffi.Void>)>>(
-          'frbgen_frb_example_dart_minimal_rust_arc_increment_strong_count_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerString');
-  late final _rust_arc_increment_strong_count_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerString =
-      _rust_arc_increment_strong_count_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerStringPtr
-          .asFunction<void Function(ffi.Pointer<ffi.Void>)>();
-
-  void
-      rust_arc_decrement_strong_count_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerString(
-    ffi.Pointer<ffi.Void> ptr,
-  ) {
-    return _rust_arc_decrement_strong_count_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerString(
-      ptr,
-    );
-  }
-
-  late final _rust_arc_decrement_strong_count_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerStringPtr =
-      _lookup<ffi.NativeFunction<ffi.Void Function(ffi.Pointer<ffi.Void>)>>(
-          'frbgen_frb_example_dart_minimal_rust_arc_decrement_strong_count_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerString');
-  late final _rust_arc_decrement_strong_count_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerString =
-      _rust_arc_decrement_strong_count_RustOpaque_flutter_rust_bridgefor_generatedRustAutoOpaqueInnerStringPtr
-          .asFunction<void Function(ffi.Pointer<ffi.Void>)>();
 }
