e25d7cbb2ae54a903dfd0079097e63a84548fdd4
fzyzcjy <ch271828n@outlook.com>
2024-09-12 11:16:53 +0800

    more

--- frb_example/dart_minimal/lib/main.dart
+++ frb_example/dart_minimal/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:frb_example_dart_minimal/src/rust/api/minimal.dart';
 import 'package:frb_example_dart_minimal/src/rust/frb_generated.dart';
 
 // If you are developing a binary program, you may want to put it in `bin/something.dart`
--- frb_example/dart_minimal/test/minimal_test.dart
+++ frb_example/dart_minimal/test/minimal_test.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
-import 'dart:io';
 
+import 'package:frb_example_dart_minimal/src/rust/api/minimal.dart';
 import 'package:frb_example_dart_minimal/src/rust/frb_generated.dart';
 import 'package:test/test.dart';
 
@@ -9,11 +9,11 @@ Future<void> main() async {
   await RustLib.init();
   print('Action: Init rust (after)');
 
-  // print('Action: Configure tests (before)');
-  // test('dart call minimalAdder', () async {
-  //   print('Action: Call rust (before)');
-  //   expect(await minimalAdder(a: 100, b: 200), 300);
-  //   print('Action: Call rust (after)');
-  // });
-  // print('Action: Configure tests (end)');
+  print('Action: Configure tests (before)');
+  test('dart call minimalAdder', () async {
+    print('Action: Call rust (before)');
+    expect(await minimalAdder(a: 100, b: 200), 300);
+    print('Action: Call rust (after)');
+  });
+  print('Action: Configure tests (end)');
 }
