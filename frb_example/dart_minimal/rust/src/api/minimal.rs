775b1434e050c73e555fc30cac1774815d4930e9
fzyzcjy <ch271828n@outlook.com>
2024-09-12 09:22:25 +0800

    mv

--- frb_example/dart_minimal/rust/src/api/minimal.rs
+++ frb_example/dart_minimal/rust/src/api/minimal.rs
@@ -8,30 +8,3 @@ pub fn init_app() {
 pub fn minimal_adder(a: i32, b: i32) -> i32 {
     a + b
 }
-
-// TODO move
-
-#[derive(Debug, Clone, Default)]
-pub struct StructWithRustAutoOpaqueFieldWithManyDerive {
-    pub content: crate::frb_generated::RustAutoOpaque<String>,
-}
-
-impl StructWithRustAutoOpaqueFieldWithManyDerive {
-    pub fn f(&self) {}
-}
-
-#[derive(Clone)]
-pub struct StructWithRustAutoOpaqueWithNonCloneData {
-    pub content: crate::frb_generated::RustAutoOpaque<NonCloneDataRaw>,
-}
-
-impl StructWithRustAutoOpaqueWithNonCloneData {
-    pub fn f(&self) {}
-}
-
-// TODO use existing
-#[derive(Debug)]
-pub struct NonCloneDataRaw {
-    content: String,
-}
-
