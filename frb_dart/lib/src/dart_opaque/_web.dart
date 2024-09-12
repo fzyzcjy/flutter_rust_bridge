d9d505effb65870d32113f1dad423586e9cb0478
fzyzcjy <ch271828n@outlook.com>
2024-09-12 13:22:26 +0800

    experiment

--- frb_dart/lib/src/dart_opaque/_web.dart
+++ frb_dart/lib/src/dart_opaque/_web.dart
@@ -1,5 +1,3 @@
-import 'dart:js_util';
-
 import 'package:flutter_rust_bridge/src/dart_opaque/_common.dart';
 import 'package:flutter_rust_bridge/src/generalized_frb_rust_binding/generalized_frb_rust_binding.dart';
 import 'package:flutter_rust_bridge/src/platform_types/platform_types.dart';
@@ -13,7 +11,16 @@ PlatformPointer encodeDartOpaque(Object raw, NativePortType dartHandlerPort,
 Object _prepareDartOpaque(Object raw) {
   // #2183
   if (raw is Function) {
-    return allowInterop(raw);
+    return AllowInteropFunctionWrapper(raw);
   }
   return raw;
+}
+
+/// {@macro flutter_rust_bridge.internal}
+class AllowInteropFunctionWrapper {
+  /// {@macro flutter_rust_bridge.internal}
+  final Function inner;
+
+  /// {@macro flutter_rust_bridge.internal}
+  const AllowInteropFunctionWrapper(this.inner);
 }
