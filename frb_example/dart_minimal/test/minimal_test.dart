f822998468e9912ba56d46e672ab002429118253
fzyzcjy <ch271828n@outlook.com>
2024-09-12 11:09:24 +0800

    tester

--- frb_example/dart_minimal/test/minimal_test.dart
+++ frb_example/dart_minimal/test/minimal_test.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
+import 'dart:io';
 
+import 'package:frb_example_dart_minimal/src/rust/api/minimal.dart';
 import 'package:frb_example_dart_minimal/src/rust/frb_generated.dart';
 import 'package:test/test.dart';
 
@@ -8,11 +10,20 @@ Future<void> main() async {
   await RustLib.init();
   print('Action: Init rust (after)');
 
-  print('Action: Configure tests (before)');
-  test('dart call minimalAdder', () async {
-    print('Action: Call rust (before)');
-    expect(await minimalAdder(a: 100, b: 200), 300);
-    print('Action: Call rust (after)');
+  // print('Action: Configure tests (before)');
+  // test('dart call minimalAdder', () async {
+  //   print('Action: Call rust (before)');
+  //   expect(await minimalAdder(a: 100, b: 200), 300);
+  //   print('Action: Call rust (after)');
+  // });
+  // print('Action: Configure tests (end)');
+
+  final addr = InternetAddress.tryParse('192.168.0.1')!;
+  test('funcUsingIpv4Addr', () async {
+    expect(await funcUsingIpv4Addr(arg: addr), addr);
   });
-  print('Action: Configure tests (end)');
+  test('funcUsingNonOpaqueStructContainingIpv4Addr', () async {
+    final arg = NonOpaqueStructContainingIpv4Addr(inner: addr);
+    expect(await funcUsingNonOpaqueStructContainingIpv4Addr(arg: arg), arg);
+  });
 }
