762ebc33ae4f6236bd272f0a151aaeaa8efa1efe
patmuk <patrick@mukherjee.de>
2024-09-17 21:23:06 +0200

    improves the examples and sets the log level so we actually see the rust logs

--- frb_example/dart_logging/lib/main.dart
+++ frb_example/dart_logging/lib/main.dart
@@ -13,19 +13,24 @@ Future<void> main() async {
   await RustLib.init();
   init_logger();
 
-  LOGGER.info('The default log level is info!');
+  LOGGER.info('\n\nThe default log level is info!');
   // change the log level like this:
   // note that this is done on the package class `Logger`, not the instance `LOGGER`
   Logger.root.level = Level.WARNING;
   LOGGER.info("this is not logged");
   LOGGER.warning("this is logged");
 
-  // set you ow function to output logs
+  // set you own function to output logs
   LOGGER.setLogFunction((LogRecord record) => {
         print(
             "My own logging function - used for rust logs as well! ${record.message}")
       });
   LOGGER.shout("Logged with custom log funtion from above!");
+  // set it back to info - can be done multiple times
+  Logger.root.level = Level.INFO;
+  // the log function can be set mutliple times as well
+  LOGGER.setLogFunction((LogRecord record) =>
+      print("${record.level}! ${record.time} - ${record.message}"));
   LOGGER.warning(
       'from Dart: Call Rust and get: 100+200 = ${await minimalAdder(a: 100, b: 200)}');
 }
