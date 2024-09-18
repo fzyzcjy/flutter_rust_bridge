46debda75aba1ad7ab58a6e2029dc1c83d3d2fbc
patmuk <patrick@mukherjee.de>
2024-09-18 12:12:31 +0200

    implements loggign test

--- frb_example/dart_logging/test/dart_valgrind_test_entrypoint.dart
+++ frb_example/dart_logging/test/dart_valgrind_test_entrypoint.dart
@@ -1,4 +1,4 @@
-import 'minimal_test.dart' as minimal_test;
+import 'logging_test.dart' as minimal_test;
 
 Future<void> main() async {
   await minimal_test.main();
--- frb_example/dart_logging/test/logging_test.dart
+++ frb_example/dart_logging/test/logging_test.dart
@@ -1,7 +1,12 @@
 import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
 
+import 'package:frb_example_dart_logging/Logger.dart';
+import 'package:frb_example_dart_logging/main.dart';
 import 'package:frb_example_dart_logging/src/rust/api/logging_example.dart';
 import 'package:frb_example_dart_logging/src/rust/frb_generated.dart';
+import 'package:logging/logging.dart';
 import 'package:test/test.dart';
 
 Future<void> main() async {
@@ -10,10 +15,24 @@ Future<void> main() async {
   print('Action: Init rust (after)');
 
   print('Action: Configure tests (before)');
+
+  print('Action: Setup logging tests');
+
+  var logOutput = "no output received";
+
+  init_logger();
+  LOGGER.setLogFunction(
+      (LogRecord record) => logOutput = "${record.level}! - ${record.message}");
+
   test('dart call minimalAdder', () async {
     print('Action: Call rust (before)');
     expect(await minimalAdder(a: 100, b: 200), 300);
     print('Action: Call rust (after)');
+  });
+  test('log message from dart to stdout', () async {
+    final msg = "test log from dart";
+    LOGGER.info(msg);
+    expect(logOutput, "INFO! - ${msg}");
   });
   print('Action: Configure tests (end)');
 }
