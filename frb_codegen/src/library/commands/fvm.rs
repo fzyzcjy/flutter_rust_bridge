22d085f058f31b6aa4b0f9ef359ffa1ce7093d63
fzyzcjy <ch271828n@outlook.com>
2024-11-05 10:00:15 +0800

    impl

--- frb_codegen/src/library/commands/fvm.rs
+++ frb_codegen/src/library/commands/fvm.rs
@@ -1,13 +1,24 @@
+use crate::command_run;
+use crate::library::commands::command_runner::call_shell;
 use std::path::Path;
 
 pub(crate) fn command_arg_maybe_fvm(pwd: &Path) -> Option<String> {
-    if detect_fvm(pwd) {
+    if should_use_fvm(pwd) {
         Some("fvm".to_owned())
     } else {
         None
     }
 }
 
-fn detect_fvm(pwd: &Path) -> bool {
+fn should_use_fvm(pwd: &Path) -> bool {
+    has_fvmrc(pwd) && has_fvm_installation()
+}
+
+fn has_fvmrc(pwd: &Path) -> bool {
     pwd.join(".fvmrc").exists()
+}
+
+fn has_fvm_installation() -> bool {
+    command_run!(call_shell[None, None], "fvm", "--version")
+        .map_or(false, |res| res.status.success())
 }
