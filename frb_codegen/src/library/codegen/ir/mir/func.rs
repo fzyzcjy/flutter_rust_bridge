02cde3c30f303d5eacd05739814f36b70c6a9235
fzyzcjy <ch271828n@outlook.com>
2024-11-05 10:47:28 +0800

    Revert "call"
    This reverts commit fc383680d6f36e63ef540e51f69568e2b42cb730.

--- frb_codegen/src/library/codegen/ir/mir/func.rs
+++ frb_codegen/src/library/codegen/ir/mir/func.rs
@@ -1,7 +1,6 @@
 use crate::codegen::generator::codec::structs::CodecModePack;
 use crate::codegen::ir::mir::comment::MirComment;
 use crate::codegen::ir::mir::field::MirField;
-use crate::codegen::ir::mir::ident::convert_rust_to_dart_style;
 use crate::codegen::ir::mir::ty::delegate::{
     MirTypeDelegate, MirTypeDelegatePrimitiveEnum, MirTypeDelegateProxyVariant,
 };
@@ -180,7 +179,7 @@ impl MirFunc {
     // }
 
     pub(crate) fn name_dart_api(&self) -> String {
-        (self.dart_name.clone()).unwrap_or_else(|| convert_rust_to_dart_style(&self.name.name))
+        (self.dart_name.clone()).unwrap_or_else(|| self.name.name.to_owned().to_case(Case::Camel))
     }
 
     pub(crate) fn name_dart_wire(&self) -> String {
