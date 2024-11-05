a44a3a510d7bc318cc63ea3ea862ae5da93a7248
fzyzcjy <ch271828n@outlook.com>
2024-11-05 10:32:23 +0800

    gen

--- frb_example/dart_minimal/lib/src/rust/api/minimal.dart
+++ frb_example/dart_minimal/lib/src/rust/api/minimal.dart
@@ -11,7 +11,7 @@ import 'package:flutter_rust_bridge/flutter_rust_bridge_for_generated.dart';
 
             Future<int> minimalAdder({required int a , required int b }) => RustLib.instance.api.crateApiMinimalMinimalAdder(a: a, b: b);
 
-Future<void> r#for({required String type }) => RustLib.instance.api.crateApiMinimalR#for(type = type);
+Future<void> for({required String type }) => RustLib.instance.api.crateApiMinimalFor(type: type);
 
             class StructWithRawNameField  {
                 final String type;
