19122288830f1fd37be47eab3ba359cea8bf4f37
fzyzcjy <ch271828n@outlook.com>
2024-11-07 18:08:17 +0800

    Revert "repro"
    This reverts commit efc78eff5ca5b8d2d8af2159f390be78a6bf52c6.

--- frb_example/dart_minimal/rust/src/api/minimal.rs
+++ frb_example/dart_minimal/rust/src/api/minimal.rs
@@ -1,11 +1,10 @@
 use flutter_rust_bridge::frb;
-pub use xml_parser::*;
 
 #[frb(init)]
 pub fn init_app() {
     flutter_rust_bridge::setup_default_user_utils();
 }
 
-pub fn load_xml_file(file_path: &str) -> Option<XmlReadout> {
-    panic!()
+pub fn minimal_adder(a: i32, b: i32) -> i32 {
+    a + b
 }
--- frb_example/dart_minimal/rust/src/xml_parser/src/lib.rs
+++
@@ -1,3 +0,0 @@
-pub struct FirstEntry {
-    pub item: Option<Box<FirstEntry>>,
-}
