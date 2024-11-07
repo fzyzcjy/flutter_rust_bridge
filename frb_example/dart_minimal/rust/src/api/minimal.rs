efc78eff5ca5b8d2d8af2159f390be78a6bf52c6
fzyzcjy <ch271828n@outlook.com>
2024-11-07 17:40:13 +0800

    repro

--- frb_example/dart_minimal/rust/src/api/minimal.rs
+++ frb_example/dart_minimal/rust/src/api/minimal.rs
@@ -1,10 +1,11 @@
 use flutter_rust_bridge::frb;
+pub use xml_parser::*;
 
 #[frb(init)]
 pub fn init_app() {
     flutter_rust_bridge::setup_default_user_utils();
 }
 
-pub fn minimal_adder(a: i32, b: i32) -> i32 {
-    a + b
+pub fn load_xml_file(file_path: &str) -> Option<XmlReadout> {
+    panic!()
 }
---
+++ frb_example/dart_minimal/rust/src/xml_parser/src/lib.rs
@@ -0,0 +1,3 @@
+pub struct FirstEntry {
+    pub item: Option<Box<FirstEntry>>,
+}
