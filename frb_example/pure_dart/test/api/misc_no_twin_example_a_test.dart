74f2a2cec54528a06db363bb26a865740d700fad
fzyzcjy <ch271828n@outlook.com>
2025-02-09 19:13:14 +0800

    more

--- frb_example/pure_dart/test/api/misc_no_twin_example_a_test.dart
+++ frb_example/pure_dart/test/api/misc_no_twin_example_a_test.dart
@@ -49,9 +49,9 @@ Future<void> main({bool skipRustLibInit = false}) async {
   });
 
   test('const int', () async {
-    expect(constIntTwinNormal(), 42);
+    expect(constIntTwinNormal, 42);
   });
   test('const array', () async {
-    expect(constArrayTwinNormal().inner, [1.5, 3.0, 6.0]);
+    expect(constArrayTwinNormal.inner, [1.5, 3.0, 6.0]);
   });
 }
