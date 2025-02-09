d243ea34b65b5de33c77508b4fe51545368b3e00
fzyzcjy <ch271828n@outlook.com>
2025-02-09 18:55:58 +0800

    more

--- frb_codegen/src/library/codegen/ir/hir/flat/constant.rs
+++ frb_codegen/src/library/codegen/ir/hir/flat/constant.rs
@@ -1,3 +1,5 @@
+use crate::codegen::ir::hir::flat::component::HirFlatComponent;
+use crate::codegen::ir::hir::flat::type_alias::HirFlatTypeAlias;
 use crate::codegen::ir::hir::misc::serializers::serialize_syn;
 use crate::utils::namespace::Namespace;
 use serde::Serialize;
@@ -7,4 +9,10 @@ pub(crate) struct HirFlatConstant {
     pub(crate) namespace: Namespace,
     #[serde(serialize_with = "serialize_syn")]
     pub(crate) item_const: syn::ItemConst,
+}
+
+impl HirFlatComponent<String> for HirFlatConstant {
+    fn sort_key(&self) -> String {
+        self.item_const.ident.to_string()
+    }
 }
