855196421b339a5e98402729a944ec7c7463f816
fzyzcjy <ch271828n@outlook.com>
2025-02-09 14:46:07 +0800

    more

--- tools/frb_internal/lib/src/misc/dart_sanitizer_tester.dart
+++ tools/frb_internal/lib/src/misc/dart_sanitizer_tester.dart
@@ -17,6 +17,11 @@ Future<void> run(TestDartSanitizerConfig config) async {
 
   await runPubGet(config.package, kDartModeOfPackage[config.package]!);
 
+  // Otherwise it seems the sanitized dart binary does not compile native assets
+  await exec(
+      'dart --enable-experiment=native-assets test test/empty_entrypoint.dart',
+      relativePwd: config.package);
+
   if (config.package == 'frb_example/deliberate_bad') {
     await _runPackageDeliberateBad(config);
   } else {
