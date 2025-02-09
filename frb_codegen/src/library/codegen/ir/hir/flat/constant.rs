97c43830123c22c0b10c0472c0b5955c79eaf314
fzyzcjy <ch271828n@outlook.com>
2025-02-09 18:55:36 +0800

    more

--- frb_codegen/src/library/codegen/ir/hir/flat/constant.rs
+++ frb_codegen/src/library/codegen/ir/hir/flat/constant.rs
@@ -1,8 +1,10 @@
+use crate::codegen::ir::hir::misc::serializers::serialize_syn;
 use crate::utils::namespace::Namespace;
 use serde::Serialize;
 
 #[derive(Debug, Clone, Serialize)]
 pub(crate) struct HirFlatConstant {
     pub(crate) namespace: Namespace,
+    #[serde(serialize_with = "serialize_syn")]
     pub(crate) item_const: syn::ItemConst,
 }
