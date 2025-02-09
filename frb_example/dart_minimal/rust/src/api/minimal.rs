0ee281c3c36533cd2485fe6ac991a35f4924d27c
fzyzcjy <ch271828n@outlook.com>
2025-02-09 21:02:44 +0800

    more

--- frb_example/dart_minimal/rust/src/api/minimal.rs
+++ frb_example/dart_minimal/rust/src/api/minimal.rs
@@ -8,3 +8,9 @@ pub fn init_app() {
 pub fn minimal_adder(a: i32, b: i32) -> i32 {
     a + b
 }
+
+#[frb(json_serializable)]
+pub enum MyEnumWithJsonSerializable {
+    Apple(String),
+    Orange { a: i32 },
+}
