48a83992b98c7fc8fe8134c6761fcb20a3c8ab92
fzyzcjy <ch271828n@outlook.com>
2025-02-10 08:58:14 +0800

    more

--- frb_example/dart_minimal/test/minimal_test.dart
+++ frb_example/dart_minimal/test/minimal_test.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:convert';
 
 import 'package:frb_example_dart_minimal/src/rust/api/minimal.dart';
 import 'package:frb_example_dart_minimal/src/rust/frb_generated.dart';
