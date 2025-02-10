4f2d9170dcb53f7af625577a3116e9cdd2bec0c4
fzyzcjy <ch271828n@outlook.com>
2025-02-10 09:12:27 +0800

    more

--- frb_codegen/src/library/codegen/parser/mir/parser/function/dummy_for_unignore/mod.rs
+++ frb_codegen/src/library/codegen/parser/mir/parser/function/dummy_for_unignore/mod.rs
@@ -1,8 +1,12 @@
-use crate::codegen::ir::hir::flat::struct_or_enum::{HirFlatEnum, HirFlatStruct};
+use crate::codegen::ir::hir::flat::struct_or_enum::{
+    HirFlatEnum, HirFlatStruct, HirFlatStructOrEnum,
+};
+use crate::codegen::ir::hir::misc::syn_item_struct_or_enum::SynItemStructOrEnum;
 use crate::codegen::ir::misc::skip::MirFuncOrSkip;
 use crate::codegen::parser::mir::internal_config::ParserMirInternalConfig;
 use crate::codegen::parser::mir::parser::ty::TypeParser;
 use crate::codegen::parser::mir::ParseMode;
+use itertools::concat;
 use std::collections::HashMap;
 
 pub(crate) fn parse(
@@ -11,6 +15,15 @@ pub(crate) fn parse(
     src_enums: &HashMap<String, &HirFlatEnum>,
     type_parser: &mut TypeParser,
     parse_mode: ParseMode,
+) -> anyhow::Result<Vec<MirFuncOrSkip>> {
+    Ok(concat([
+        parse_structs_or_enums(src_structs)?,
+        parse_structs_or_enums(src_enums)?,
+    ]))
+}
+
+fn parse_structs_or_enums<Item: SynItemStructOrEnum>(
+    items: &[HirFlatStructOrEnum<Item>],
 ) -> anyhow::Result<Vec<MirFuncOrSkip>> {
     TODO
 }
