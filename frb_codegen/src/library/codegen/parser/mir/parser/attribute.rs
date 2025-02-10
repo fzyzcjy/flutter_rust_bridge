49df1952c76b37ed0baa71c371d25886c154fafe
fzyzcjy <ch271828n@outlook.com>
2025-02-10 09:05:56 +0800

    more

--- frb_codegen/src/library/codegen/parser/mir/parser/attribute.rs
+++ frb_codegen/src/library/codegen/parser/mir/parser/attribute.rs
@@ -98,6 +98,10 @@ impl FrbAttributes {
         self.any_eq(&FrbAttribute::Ignore)
     }
 
+    pub(crate) fn unignore(&self) -> bool {
+        self.any_eq(&FrbAttribute::Unignore)
+    }
+
     pub(crate) fn opaque(&self) -> Option<bool> {
         if self.any_eq(&FrbAttribute::Opaque) {
             Some(true)
@@ -274,6 +278,7 @@ mod frb_keyword {
     syn::custom_keyword!(setter);
     syn::custom_keyword!(init);
     syn::custom_keyword!(ignore);
+    syn::custom_keyword!(unignore);
     syn::custom_keyword!(opaque);
     syn::custom_keyword!(non_opaque);
     syn::custom_keyword!(non_hash);
@@ -322,6 +327,7 @@ enum FrbAttribute {
     External,
     Getter,
     Ignore,
+    Unignore,
     Init,
     Mirror(FrbAttributeMirror),
     Name(FrbAttributeName),
@@ -375,6 +381,7 @@ impl Parse for FrbAttribute {
             .or_else(|| parse_keyword::<setter, _>(input, &lookahead, setter, Setter))
             .or_else(|| parse_keyword::<init, _>(input, &lookahead, init, Init))
             .or_else(|| parse_keyword::<ignore, _>(input, &lookahead, ignore, Ignore))
+            .or_else(|| parse_keyword::<unignore, _>(input, &lookahead, unignore, Unignore))
             .or_else(|| parse_keyword::<opaque, _>(input, &lookahead, opaque, Opaque))
             .or_else(|| parse_keyword::<non_opaque, _>(input, &lookahead, non_opaque, NonOpaque))
             .or_else(|| parse_keyword::<non_hash, _>(input, &lookahead, non_hash, NonHash))
@@ -821,6 +828,11 @@ mod tests {
     #[test]
     fn test_ignore() {
         simple_keyword_tester("ignore", FrbAttribute::Ignore);
+    }
+
+    #[test]
+    fn test_unignore() {
+        simple_keyword_tester("unignore", FrbAttribute::Unignore);
     }
 
     #[test]
