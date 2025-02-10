fe5bf753482dc765eebc862faccca3dfd85d3060
svr2kos2 <svr2kos2@hotmail.com>
2025-02-10 17:00:14 +0800

    refactoring and add comment

--- frb_dart/lib/src/wasm_module/_web.dart
+++ frb_dart/lib/src/wasm_module/_web.dart
@@ -11,16 +11,17 @@ import 'dart:html' as html;
 /// {@macro flutter_rust_bridge.internal}
 Future<void> initializeWasmModule({required String root}) async {
   _ensureCrossOriginIsolated();
-  final versionQuery = await _getVersionQuery();
+  final packageInfo = await _getPackageInfo();
+  final versionQuery = _assembleVersionQuery(packageInfo);
   
-  final script = web.HTMLScriptElement()..src = '$root.js?$versionQuery';
+  final script = web.HTMLScriptElement()..src = '$root.js$versionQuery';
   web.document.head!.append(script);
 
   await script.onLoad.first;
 
   jsEval('window.wasm_bindgen = wasm_bindgen');
 
-  await _jsWasmBindgen('${root}_bg.wasm?$versionQuery').toDart;
+  await _jsWasmBindgen('${root}_bg.wasm$versionQuery').toDart;
 }
 
 @JS('wasm_bindgen')
@@ -43,23 +44,36 @@ void _ensureCrossOriginIsolated() {
   }
 }
 
-Future<String> _getVersionQuery() async {
-  String baseUrl = assetManager.getAssetUrl('').replaceAll('${assetManager.assetsDir}/', '');
-  return await _request(baseUrl) ??
-          await _request(web.window.document.baseURI) ??
-          '';
-}
+/// Get the version number of the web app.
+/// 
+/// These functions follows the approach used in `package_info_plus` to fetch version information.
+/// Since frb needs to be pure dart, so we cannot directly depend on `package_info_plus`.
 
-Future<String?> _request(String baseUrl) async {
+String _assembleVersionQuery(Map<String, dynamic>? packageInfo) =>
+    packageInfo != null && 
+    packageInfo['version'] != null &&
+    packageInfo['build_number'] != null ?
+    '?version=${packageInfo['version']}.${packageInfo['build_number']}' :
+    '';
+
+
+Future<Map<String, dynamic>?> _getPackageInfo() async {
+  String baseUrl = assetManager.getAssetUrl('').replaceAll('${assetManager.assetsDir}/', '');
+  final resJson = 
+    await _request(baseUrl) ??
+    await _request(web.window.document.baseURI);
+  if (resJson == null) return null;
   try {
-    final req = await html.HttpRequest.request('$baseUrl/version.json?t=${DateTime.now().millisecondsSinceEpoch}');
-    if (req.status == 200) {
-      final response = req.responseText;
-      final versionMap = jsonDecode(response!);
-      return 'version=${versionMap['version']!}.${versionMap['build_number']!}';
-    }
-    return null;
+    return jsonDecode(resJson);
   } catch (e) {
     return null;
   }
+}
+
+Future<String?> _request(String baseUrl) async {
+    final req = await html.HttpRequest.request('$baseUrl/version.json?t=${DateTime.now().millisecondsSinceEpoch}');
+    if (req.status == 200) {
+      return req.responseText;
+    }
+    return null;
 }
