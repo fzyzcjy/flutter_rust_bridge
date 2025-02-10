0cec0bffc29b1da35009b08ab601fe369d8d2a87
fzyzcjy <ch271828n@outlook.com>
2025-02-10 09:10:47 +0800

    more

--- frb_codegen/src/library/codegen/parser/mir/parser/function/dummy_for_unignore/mod.rs
+++ frb_codegen/src/library/codegen/parser/mir/parser/function/dummy_for_unignore/mod.rs
@@ -1,20 +1,9 @@
-use crate::codegen::ir::hir::flat::constant::HirFlatConstant;
 use crate::codegen::ir::hir::flat::struct_or_enum::{HirFlatEnum, HirFlatStruct};
-use crate::codegen::ir::mir::func::{
-    MirFunc, MirFuncAccessorMode, MirFuncArgMode, MirFuncImplMode, MirFuncMode, MirFuncOutput,
-    MirFuncOwnerInfo,
-};
-use crate::codegen::ir::mir::ident::MirIdent;
-use crate::codegen::ir::misc::skip::{IrSkip, IrSkipReason, IrValueOrSkip, MirFuncOrSkip};
+use crate::codegen::ir::misc::skip::MirFuncOrSkip;
 use crate::codegen::parser::mir::internal_config::ParserMirInternalConfig;
-use crate::codegen::parser::mir::parser::attribute::FrbAttributes;
-use crate::codegen::parser::mir::parser::function::auto_accessor::create_simplified_parsing_context;
-use crate::codegen::parser::mir::parser::function::real::compute_codec_mode_pack;
 use crate::codegen::parser::mir::parser::ty::TypeParser;
 use crate::codegen::parser::mir::ParseMode;
-use crate::utils::namespace::NamespacedName;
 use std::collections::HashMap;
-use syn::spanned::Spanned;
 
 pub(crate) fn parse(
     config: &ParserMirInternalConfig,
