4d3e5c7745c0d8112117d6d4b24e633893e1b298
fzyzcjy <ch271828n@outlook.com>
2025-02-10 08:30:14 +0800

    deps

--- frb_example/rust_ui_counter/ui/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frb_example/rust_ui_counter/ui/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import package_info_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
 }
