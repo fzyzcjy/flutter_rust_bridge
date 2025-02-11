231659a6564e379800c6083934cb8bd3482e85dd
fzyzcjy <ch271828n@outlook.com>
2025-02-11 11:45:44 +0800

    demo

--- frb_example/dart_minimal/rust/src/api/minimal.rs
+++ frb_example/dart_minimal/rust/src/api/minimal.rs
@@ -8,3 +8,9 @@ pub fn init_app() {
 pub fn minimal_adder(a: i32, b: i32) -> i32 {
     a + b
 }
+
+// These consts should be ignored
+pub(crate) const CONST_PUB_CRATE_SHOULD_IGNORE: i32 = 42;
+const CONST_PRIVATE_SHOULD_IGNORE: i32 = 42;
+#[frb(ignore)]
+pub const CONST_WITH_EXPLICIT_IGNORE_SHOULD_IGNORE: i32 = 42;
