ee32a7c09268b0e6ea11b6adb361df1423b7c59e
fzyzcjy <ch271828n@outlook.com>
2025-03-08 17:17:32 +0800

    more

--- frb_utils/lib/src/simple_build_utils.dart
+++ frb_utils/lib/src/simple_build_utils.dart
@@ -10,49 +10,48 @@ import 'package:native_assets_cli/native_assets_cli.dart';
 /// is still experimental.
 // ref: https://github.com/dart-lang/native/blob/main/pkgs/native_assets_cli/example/native_add_library/build.dart
 void simpleBuild(List<String> args, {List<String> features = const []}) async {
-  final buildConfig = await BuildConfig.fromArgs(args);
-  final buildOutput = BuildOutput();
+  await build(args, (input, output) async {
+    final rustCrateDir = input.packageRoot.resolve('rust');
 
-  final rustCrateDir = buildConfig.packageRoot.resolve('rust');
+    final cargoNightly =
+        Platform.environment['FRB_SIMPLE_BUILD_CARGO_NIGHTLY'] == '1';
+    final cargoExtraArgs =
+        Platform.environment['FRB_SIMPLE_BUILD_CARGO_EXTRA_ARGS']?.split(' ') ??
+            const <String>[];
+    final skip = Platform.environment['FRB_SIMPLE_BUILD_SKIP'] == '1';
+    final rustflags = Platform.environment['RUSTFLAGS'];
 
-  final cargoNightly =
-      Platform.environment['FRB_SIMPLE_BUILD_CARGO_NIGHTLY'] == '1';
-  final cargoExtraArgs =
-      Platform.environment['FRB_SIMPLE_BUILD_CARGO_EXTRA_ARGS']?.split(' ') ??
-          const <String>[];
-  final skip = Platform.environment['FRB_SIMPLE_BUILD_SKIP'] == '1';
-  final rustflags = Platform.environment['RUSTFLAGS'];
+    if (skip) {
+      print(
+          'frb_utils::simpleBuild SKIP BUILD since environment variable requires this');
+    } else {
+      final featureArgs = features.expand((x) => ['--features', x]).toList();
+      await runCommand(
+        'cargo',
+        [
+          if (cargoNightly) '+nightly',
+          'build',
+          '--release',
+          ...cargoExtraArgs,
+          ...featureArgs,
+        ],
+        pwd: rustCrateDir.toFilePath(),
+        printCommandInStderr: true,
+        env: {
+          // Though runCommand auto pass environment variable to commands,
+          // we do this to explicitly show this important flag
+          if (rustflags != null) 'RUSTFLAGS': rustflags,
+        },
+      );
+    }
 
-  if (skip) {
-    print(
-        'frb_utils::simpleBuild SKIP BUILD since environment variable requires this');
-  } else {
-    final featureArgs = features.expand((x) => ['--features', x]).toList();
-    await runCommand(
-      'cargo',
-      [
-        if (cargoNightly) '+nightly',
-        'build',
-        '--release',
-        ...cargoExtraArgs,
-        ...featureArgs,
-      ],
-      pwd: rustCrateDir.toFilePath(),
-      printCommandInStderr: true,
-      env: {
-        // Though runCommand auto pass environment variable to commands,
-        // we do this to explicitly show this important flag
-        if (rustflags != null) 'RUSTFLAGS': rustflags,
-      },
-    );
-  }
+    final dependencies = {
+      rustCrateDir,
+      input.packageRoot.resolve('build.rs'),
+    };
+    print('dependencies: $dependencies');
+    output.dependencies.dependencies.addAll(dependencies);
 
-  final dependencies = {
-    rustCrateDir,
-    buildConfig.packageRoot.resolve('build.rs'),
-  };
-  print('dependencies: $dependencies');
-  buildOutput.dependencies.dependencies.addAll(dependencies);
-
-  await buildOutput.writeToFile(outDir: buildConfig.outDir);
+    await output.writeToFile(outDir: input.outDir);
+  });
 }
