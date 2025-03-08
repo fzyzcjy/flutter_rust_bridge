1fd56ff8fb66a4633089b86bce60b4e16888f711
fzyzcjy <ch271828n@outlook.com>
2025-03-08 13:58:31 +0800

    revert

--- frb_example/flutter_package/example/android/app/src/main/kotlin/com/example/flutter_package_example/MainActivity.kt
+++ frb_example/flutter_package/example/android/app/src/main/kotlin/com/example/flutter_package_example/MainActivity.kt
@@ -2,4 +2,4 @@ package com.example.flutter_package_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity()
--- frb_example/flutter_via_create/linux/flutter/generated_plugins.cmake
+++ frb_example/flutter_via_create/linux/flutter/generated_plugins.cmake
@@ -6,6 +6,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
+  rust_lib_flutter_via_create
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
