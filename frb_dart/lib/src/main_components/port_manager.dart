2cb8ace073a74e0b3debea782770388629cebaab
fzyzcjy <ch271828n@outlook.com>
2025-03-08 14:51:48 +0800

    more

--- frb_dart/lib/src/main_components/port_manager.dart
+++ frb_dart/lib/src/main_components/port_manager.dart
@@ -1,3 +1,4 @@
+import 'dart:js_util';
 import 'package:flutter_rust_bridge/src/generalized_frb_rust_binding/generalized_frb_rust_binding.dart';
 import 'package:flutter_rust_bridge/src/main_components/handler.dart';
 import 'package:flutter_rust_bridge/src/platform_types/platform_types.dart';
@@ -37,7 +38,7 @@ class DartHandlerPortManager extends BaseLazyPortManager {
   void onData(Object? message) {
     if (message is! List<dynamic>) {
       throw UnimplementedError(
-          'Unsupported message type=${message?.runtimeType} message=$message');
+          'Unsupported message type=${message?.runtimeType} message=$message dartify=${dartify(message)}');
     }
 
     switch (message[0]) {
