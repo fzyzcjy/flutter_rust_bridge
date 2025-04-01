a5e27126d33d12285e0d716b29f644536ea0a167
alexlapa <lapa.alex@ex.ua>
2025-04-01 13:15:56 +0300

    fix for multiple isolates

--- frb_rust/src/ffi_binding/io.rs
+++ frb_rust/src/ffi_binding/io.rs
@@ -1,12 +1,14 @@
 use crate::codec::dco::Rust2DartMessageDco;
 use crate::codec::sse::Rust2DartMessageSse;
 use crate::codec::Rust2DartMessageTrait;
+use crate::misc::atomic::AtomicU64;
 use crate::platform_types::{WireSyncRust2DartDco, WireSyncRust2DartSse};
 use allo_isolate::{
     ffi::{DartCObject, DartPort},
     store_dart_post_cobject,
 };
 use std::ffi::c_void;
+use std::sync::atomic::Ordering;
 
 /// # Safety
 ///
@@ -40,6 +42,14 @@ pub unsafe extern "C" fn frb_free_wire_sync_rust2dart_sse(value: WireSyncRust2Da
 /// This function should never be called manually.
 #[no_mangle]
 pub unsafe extern "C" fn frb_get_shutdown_callback() -> unsafe extern "C" fn(*mut c_void) {
+    /// Counter for how many active shutdown callbacks there are.
+    ///
+    /// It is incremented when `frb_shutdown_callback` is returned and
+    /// decremented when it is called.
+    static ISOLATES_NUM: AtomicU64 = AtomicU64::new(0);
+
+    ISOLATES_NUM.add(1, Ordering::SeqCst);
+
     /// Called by Dart's `NativeFinalizer` on isolate group shutdown.
     unsafe extern "C" fn frb_shutdown_callback(_: *mut c_void) {
         unsafe extern "C" fn devnull(_: DartPort, _: *mut DartCObject) -> bool {
@@ -50,13 +60,17 @@ pub unsafe extern "C" fn frb_get_shutdown_callback() -> unsafe extern "C" fn(*mu
             true
         }
 
-        // So `Dart_PostCObject` won't do anything from now on. We need this
-        // cause once shutdown have started `Dart_Cleanup` might be called any
-        // moment from now and `Dart_PostCObject` can only be used before
-        // `Dart_Cleanup` has been called
-        // For more information refer to:
-        // https://github.com/dart-lang/native/issues/2079
-        store_dart_post_cobject(devnull);
+        let running = ISOLATES_NUM.fetch_sub(1, Ordering::SeqCst);
+
+        if running == 1 {
+            // So `Dart_PostCObject` won't do anything from now on. We need this
+            // cause once shutdown have started `Dart_Cleanup` might be called any
+            // moment from now and `Dart_PostCObject` can only be used before
+            // `Dart_Cleanup` has been called
+            // For more information refer to:
+            // https://github.com/dart-lang/native/issues/2079
+            store_dart_post_cobject(devnull);
+        };
     }
 
     frb_shutdown_callback
