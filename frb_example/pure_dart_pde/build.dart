3682d0c9eb966928ea5374df7eaa43f7f7f6adbf
Aran Donohue <aran@arandonohue.com>
2025-04-07 20:38:12 -0700

    fix: pure_dart[_pde] uses dart-opaque

--- frb_example/pure_dart_pde/build.dart
+++ frb_example/pure_dart_pde/build.dart
@@ -4,4 +4,4 @@ import 'package:flutter_rust_bridge_utils/flutter_rust_bridge_utils.dart';
 
 void main(List<String> args) async => simpleBuild(args,
     // This is solely used for testing
-    features: ['internal_feature_for_testing']);
+    features: ['dart-opaque', 'internal_feature_for_testing']);
