ad2c7aa1b42825a8e63153add605134ff0d20947
fzyzcjy <ch271828n@outlook.com>
2025-05-16 22:00:53 +0800

    temp

--- frb_example/dart_minimal/rust/src/api/minimal.rs
+++ frb_example/dart_minimal/rust/src/api/minimal.rs
@@ -1,4 +1,5 @@
-use flutter_rust_bridge::frb;
+use flutter_rust_bridge::{frb, DartFnFuture};
+use std::sync::Arc;
 
 #[frb(init)]
 pub fn init_app() {
@@ -7,4 +8,11 @@ pub fn init_app() {
 
 pub fn minimal_adder(a: i32, b: i32) -> i32 {
     a + b
+}
+
+pub async fn subscribe_to_values(dart_callback: impl Fn(String) -> DartFnFuture<String>) {
+    let dart_callback = Arc::new(dart_callback);
+    tokio::task::spawn(async move {
+        dart_callback("Hello from Rust!".to_owned()).await;
+    });
 }
