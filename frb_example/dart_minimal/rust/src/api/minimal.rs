aa035642aff0026509b69a407fdb72d44e86fec0
fzyzcjy <ch271828n@outlook.com>
2025-05-16 20:32:56 +0800

    more

--- frb_example/dart_minimal/rust/src/api/minimal.rs
+++ frb_example/dart_minimal/rust/src/api/minimal.rs
@@ -8,3 +8,12 @@ pub fn init_app() {
 pub fn minimal_adder(a: i32, b: i32) -> i32 {
     a + b
 }
+
+pub struct StructWithRawFieldName {
+    pub r#type: EnumForStructWithRawFieldName,
+}
+
+pub enum EnumForStructWithRawFieldName {
+    One,
+    Two,
+}
