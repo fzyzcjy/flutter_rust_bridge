e9ad40c344df32adeeb63add59e40bcb3ca6332e
patmuk <patrick@mukherjee.de>
2025-05-16 11:36:27 +0200

    calms rust linter by commenting out potentially unneeded function definition

--- frb_rust/src/misc/user_utils.rs
+++ frb_rust/src/misc/user_utils.rs
@@ -17,12 +17,14 @@ fn setup_backtrace() {
 
     PanicBacktrace::setup();
 }
-#[cfg(feature = "log")]
-fn setup_log_to_console() {
-    // TODO test logging (how?)
-    // TODO test and remove if not needed, i.e. printline writes into the console as well
-    #[cfg(target_family = "wasm")]
-    let _ = crate::misc::web_utils::WebConsoleLogger::init();
 
-    // TODO add more platforms
-}
+// TODO is this needed any longer? The default logging setup logs to the console
+// #[cfg(feature = "log")]
+// fn setup_log_to_console() {
+//     // TODO test logging (how?)
+//     // TODO test and remove if not needed, i.e. printline writes into the console as well
+//     #[cfg(target_family = "wasm")]
+//     let _ = crate::misc::web_utils::WebConsoleLogger::init();
+
+//     // TODO add more platforms
+// }
