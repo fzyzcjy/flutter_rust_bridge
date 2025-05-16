d860151d4412a0e666946cbd7a8eb2d417b697fb
fzyzcjy <ch271828n@outlook.com>
2025-05-16 22:03:43 +0800

    temp

--- frb_example/dart_minimal/rust/src/api/minimal.rs
+++ frb_example/dart_minimal/rust/src/api/minimal.rs
@@ -1,4 +1,6 @@
 use flutter_rust_bridge::{frb, DartFnFuture};
+use std::future::Future;
+use std::pin::Pin;
 use std::sync::Arc;
 
 #[frb(init)]
@@ -10,7 +12,12 @@ pub fn minimal_adder(a: i32, b: i32) -> i32 {
     a + b
 }
 
-pub async fn subscribe_to_values(dart_callback: impl Fn(String) -> DartFnFuture<String>) {
+pub async fn func_with_dart_callback_across_thread(
+    dart_callback: impl Fn(String) -> Pin<Box<dyn Future<Output = String> + Send + 'static>>
+        + Send
+        + Sync
+        + 'static,
+) {
     let dart_callback = Arc::new(dart_callback);
     tokio::task::spawn(async move {
         dart_callback("Hello from Rust!".to_owned()).await;
