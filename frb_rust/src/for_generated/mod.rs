40aa20ecf0cd8985aa51d6cf5ca9487a8452e1bc
patmuk <patrick@mukherjee.de>
2025-05-21 10:56:48 +0200

    adding chrono back as needed by enable_frb_logging()

--- frb_rust/src/for_generated/mod.rs
+++ frb_rust/src/for_generated/mod.rs
@@ -70,6 +70,8 @@ pub use anyhow;
 pub use byteorder;
 #[cfg(target_family = "wasm")]
 pub use cast::slice_from_byte_buffer;
+#[cfg(feature = "log")]
+pub use chrono;
 #[cfg(feature = "rust-async")]
 pub use futures;
 #[cfg(target_family = "wasm")]
