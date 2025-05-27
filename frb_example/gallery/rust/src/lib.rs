ad2d471c29e0b60a5ed35d927276f2a78c834f9d
patmuk <patrick@mukherjee.de>
2025-05-27 23:54:52 +0200

    re-adds wrongly removed dependency

--- frb_example/gallery/rust/src/lib.rs
+++ frb_example/gallery/rust/src/lib.rs
@@ -4,3 +4,4 @@ mod frb_generated;
 pub use crate::frb_generated::StreamSink as __FrbStreamSinkForLogging;
 // END of AUTO INJECTED code
 pub mod api;
+pub mod ignore_me; /* AUTO INJECTED BY flutter_rust_bridge. This line may not be accurate, and you can change it according to your needs. */
