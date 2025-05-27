c8df33dafbbf34c0a356bd830028a7d61c50e59a
patmuk <patrick@mukherjee.de>
2025-05-27 18:49:02 +0200

    removes not working inner attributes

--- frb_example/rust_ui_todo_list/src/lib.rs
+++ frb_example/rust_ui_todo_list/src/lib.rs
@@ -3,6 +3,5 @@ mod frb_generated;
 // this export is needed for logging
 pub use crate::frb_generated::StreamSink as __FrbStreamSinkForLogging;
 // END of AUTO INJECTED code
-#![allow(clippy::new_without_default)]
 
 pub mod app;
