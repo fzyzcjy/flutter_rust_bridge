16a9d81c6bf019788b27043227f1e3f1632a9b7b
patmuk <patrick@mukherjee.de>
2025-05-27 16:55:04 +0200

    updates compiled files

--- frb_example/pure_dart_pde/rust/src/lib.rs
+++ frb_example/pure_dart_pde/rust/src/lib.rs
@@ -1,8 +1,12 @@
 // AUTO-GENERATED FROM frb_example/pure_dart, DO NOT EDIT
 
+// AUTO INJECTED BY flutter_rust_bridge. The following lines may not be accurate; change them according to your needs.
+mod frb_generated;
+// this export is needed for logging
+pub use crate::frb_generated::StreamSink as __FrbStreamSinkForLogging;
+// END of AUTO INJECTED code
 pub mod api;
 mod auxiliary;
 mod deliberate_name_conflict;
-mod frb_generated; /* AUTO INJECTED BY flutter_rust_bridge. This line may not be accurate, and you can change it according to your needs. */
 
 pub fn function_at_lib_rs() {}
