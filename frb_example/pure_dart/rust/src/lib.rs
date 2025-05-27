9b203ed1f0aa2f88bffaabcb76b5c281341040cf
patmuk <patrick@mukherjee.de>
2025-05-27 19:06:03 +0200

    fixes lib.rs

--- frb_example/pure_dart/rust/src/lib.rs
+++ frb_example/pure_dart/rust/src/lib.rs
@@ -6,6 +6,5 @@ pub use crate::frb_generated::StreamSink as __FrbStreamSinkForLogging;
 pub mod api;
 mod auxiliary;
 mod deliberate_name_conflict;
-mod frb_generated; /* AUTO INJECTED BY flutter_rust_bridge. This line may not be accurate, and you can change it according to your needs. */
 
 pub fn function_at_lib_rs() {}
