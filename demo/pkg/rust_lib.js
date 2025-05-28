6b1d60a97e5e12971f162c896a7004738c6c41e2
fzyzcjy <fzyzcjy@users.noreply.github.com>
2025-05-28 10:28:30 +0000

    deploy: b11a66c97298a69f9f1c3821052336ba4cb72779

--- demo/pkg/rust_lib.js
+++ demo/pkg/rust_lib.js
@@ -307,15 +307,15 @@ let wasm_bindgen;
     };
 
     function __wbg_adapter_36(arg0, arg1, arg2) {
-        wasm.closure155_externref_shim(arg0, arg1, arg2);
+        wasm.closure153_externref_shim(arg0, arg1, arg2);
     }
 
     function __wbg_adapter_39(arg0, arg1, arg2) {
-        wasm.closure170_externref_shim(arg0, arg1, arg2);
+        wasm.closure168_externref_shim(arg0, arg1, arg2);
     }
 
     function __wbg_adapter_125(arg0, arg1, arg2, arg3) {
-        wasm.closure218_externref_shim(arg0, arg1, arg2, arg3);
+        wasm.closure216_externref_shim(arg0, arg1, arg2, arg3);
     }
 
     const WorkerPoolFinalization = (typeof FinalizationRegistry === 'undefined')
@@ -676,16 +676,16 @@ let wasm_bindgen;
             const ret = false;
             return ret;
         };
-        imports.wbg.__wbindgen_closure_wrapper399 = function(arg0, arg1, arg2) {
-            const ret = makeMutClosure(arg0, arg1, 156, __wbg_adapter_36);
+        imports.wbg.__wbindgen_closure_wrapper396 = function(arg0, arg1, arg2) {
+            const ret = makeMutClosure(arg0, arg1, 154, __wbg_adapter_36);
+            return ret;
+        };
+        imports.wbg.__wbindgen_closure_wrapper425 = function(arg0, arg1, arg2) {
+            const ret = makeMutClosure(arg0, arg1, 169, __wbg_adapter_39);
             return ret;
         };
         imports.wbg.__wbindgen_closure_wrapper427 = function(arg0, arg1, arg2) {
-            const ret = makeMutClosure(arg0, arg1, 171, __wbg_adapter_39);
-            return ret;
-        };
-        imports.wbg.__wbindgen_closure_wrapper429 = function(arg0, arg1, arg2) {
-            const ret = makeMutClosure(arg0, arg1, 171, __wbg_adapter_39);
+            const ret = makeMutClosure(arg0, arg1, 169, __wbg_adapter_39);
             return ret;
         };
         imports.wbg.__wbindgen_debug_string = function(arg0, arg1) {
