7f60bcdc9f6c356d626bb9ddc8dc64ceb90e55d3
patmuk <patrick@mukherjee.de>
2025-05-29 14:37:30 +0200

    fixes lib.rs new comment

--- frb_example/gallery/rust/src/lib.rs
+++ frb_example/gallery/rust/src/lib.rs
@@ -1,4 +1,5 @@
-// AUTO INJECTED BY flutter_rust_bridge. The following lines may not be accurate; change them according to your needs.
+// AUTO INJECTED BY flutter_rust_bridge.
+// The following lines may not be accurate; change them according to your needs.
 mod frb_generated;
 // this export is needed for logging
 pub use crate::frb_generated::StreamSink as __FrbStreamSinkForLogging;
