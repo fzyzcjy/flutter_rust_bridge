075ba472c618d497ec897c28f8d9f22239d5f44c
patmuk <patrick@mukherjee.de>
2025-05-29 21:03:28 +0200

    removes space again

--- frb_example/pure_dart_pde/rust/src/lib.rs
+++ frb_example/pure_dart_pde/rust/src/lib.rs
@@ -1,5 +1,4 @@
 // AUTO-GENERATED FROM frb_example/pure_dart, DO NOT EDIT
-
 // AUTO INJECTED BY flutter_rust_bridge.
 // The following lines may not be accurate; change them according to your needs.
 mod frb_generated;
