55d06ac02da91779b6b6e2957ab975744e90a82d
patmuk <patrick@mukherjee.de>
2025-07-03 11:45:58 +0200

    adapts generates lib.rs files

--- frb_example/rust_ui_todo_list/src/lib.rs
+++ frb_example/rust_ui_todo_list/src/lib.rs
@@ -3,8 +3,6 @@
 // AUTO INJECTED BY flutter_rust_bridge.
 // The following lines may not be accurate; change them according to your needs.
 mod frb_generated;
-// this export is needed for logging
-pub use crate::frb_generated::StreamSink as __FrbStreamSinkForLogging;
 // END of AUTO INJECTED code
 
 pub mod app;
