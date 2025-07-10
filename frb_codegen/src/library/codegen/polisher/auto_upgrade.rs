7eca81fd7c2c5f8664c2e50986c84a910caccf18
turtletongue <platonsterh@gmail.com>
2025-07-10 19:34:37 +0500

    Check for target dependencies when auto upgrading in polisher

--- frb_codegen/src/library/codegen/polisher/auto_upgrade.rs
+++ frb_codegen/src/library/codegen/polisher/auto_upgrade.rs
@@ -2,8 +2,9 @@ use crate::codegen::misc::GeneratorProgressBarPack;
 use crate::integration::integrator::pub_add_dependency_frb;
 use crate::library::commands::cargo::cargo_add;
 use crate::utils::dart_repository::dart_repo::{DartDependencyMode, DartRepository};
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use cargo_metadata::VersionReq;
+use cargo_toml::{Dependency, Manifest};
 use std::path::Path;
 use std::str::FromStr;
 
@@ -14,15 +15,19 @@ pub(super) fn execute(
     enable_auto_upgrade: bool,
 ) -> Result<()> {
     let _pb = progress_bar_pack.polish_upgrade.start();
-    DartUpgrader::execute(dart_root, enable_auto_upgrade)?;
-    RustUpgrader::execute(rust_crate_dir, enable_auto_upgrade)
+
+    let dart_upgrader = DartUpgrader::build(dart_root)?;
+    dart_upgrader.execute(enable_auto_upgrade)?;
+
+    let rust_upgrader = RustUpgrader::build(rust_crate_dir)?;
+    rust_upgrader.execute(enable_auto_upgrade)
 }
 
 trait Upgrader {
-    fn execute(base_dir: &Path, enable_auto_upgrade: bool) -> Result<()> {
-        if !Self::check(base_dir)? {
+    fn execute(&self, enable_auto_upgrade: bool) -> Result<()> {
+        if !self.check()? {
             if enable_auto_upgrade {
-                Self::upgrade(base_dir)?;
+                self.upgrade()?;
             } else {
                 log::warn!("Auto upgrader find wrong Dart/Rust flutter_rust_bridge dependency version, please enable `auto_upgrade_dependencies` flag or upgrade manually.");
             }
@@ -30,17 +35,29 @@ trait Upgrader {
         Ok(())
     }
 
-    fn check(base_dir: &Path) -> Result<bool>;
+    fn check(&self) -> Result<bool>;
 
-    fn upgrade(base_dir: &Path) -> Result<()>;
+    fn upgrade(&self) -> Result<()>;
 }
 
-struct DartUpgrader;
+struct DartUpgrader<'a> {
+    repository: DartRepository,
+    base_dir: &'a Path,
+}
 
-impl Upgrader for DartUpgrader {
-    fn check(base_dir: &Path) -> Result<bool> {
-        let repo = DartRepository::from_path(base_dir)?;
-        Ok(repo
+impl<'a> DartUpgrader<'a> {
+    fn build(base_dir: &'a Path) -> Result<Self> {
+        Ok(Self {
+            repository: DartRepository::from_path(base_dir)?,
+            base_dir,
+        })
+    }
+}
+
+impl Upgrader for DartUpgrader<'_> {
+    fn check(&self) -> Result<bool> {
+        Ok(self
+            .repository
             .has_specified_and_installed(
                 "flutter_rust_bridge",
                 DartDependencyMode::Main,
@@ -49,26 +66,69 @@ impl Upgrader for DartUpgrader {
             .is_ok())
     }
 
-    fn upgrade(base_dir: &Path) -> Result<()> {
+    fn upgrade(&self) -> Result<()> {
         log::info!("Auto upgrade Dart dependency");
-        pub_add_dependency_frb(false, Some(base_dir))
+        pub_add_dependency_frb(false, Some(self.base_dir))
     }
 }
 
-struct RustUpgrader;
+struct RustUpgrader<'a> {
+    dependency: Dependency,
+    target_name: Option<String>,
+    base_dir: &'a Path,
+}
 
-impl Upgrader for RustUpgrader {
-    fn check(base_dir: &Path) -> Result<bool> {
-        let manifest = cargo_toml::Manifest::from_path(base_dir.join("Cargo.toml"))?;
-        let dep = &manifest.dependencies["flutter_rust_bridge"];
-        Ok(dep.req() == concat!("=", env!("CARGO_PKG_VERSION")))
+impl<'a> RustUpgrader<'a> {
+    fn build(base_dir: &'a Path) -> Result<Self> {
+        let manifest = Manifest::from_path(base_dir.join("Cargo.toml"))?;
+
+        let (target_name, dependency) = manifest
+            .dependencies
+            .get("flutter_rust_bridge")
+            .map(|dep| (None, dep.clone()))
+            .into_iter()
+            .chain(
+                manifest
+                    .target
+                    .iter()
+                    .map(|(name, target)| {
+                        target
+                            .dependencies
+                            .get("flutter_rust_bridge")
+                            .map(|dep| (Some(name.to_owned()), dep.clone()))
+                    })
+                    .flatten(),
+            )
+            .next()
+            .ok_or_else(|| anyhow!("flutter_rust_bridge not found in Cargo.toml dependencies"))?;
+
+        Ok(Self {
+            dependency,
+            target_name,
+            base_dir,
+        })
+    }
+}
+
+impl Upgrader for RustUpgrader<'_> {
+    fn check(&self) -> Result<bool> {
+        Ok(self.dependency.req() == concat!("=", env!("CARGO_PKG_VERSION")))
     }
 
-    fn upgrade(base_dir: &Path) -> Result<()> {
+    fn upgrade(&self) -> Result<()> {
         log::info!("Auto upgrade Rust dependency");
-        cargo_add(
-            &[concat!("flutter_rust_bridge@=", env!("CARGO_PKG_VERSION"))],
-            base_dir,
-        )
+
+        let mut args = vec![concat!("flutter_rust_bridge@=", env!("CARGO_PKG_VERSION"))];
+
+        let target = self
+            .target_name
+            .as_ref()
+            .map(|name| format!("--target={name}"));
+
+        if let Some(target) = &target {
+            args.push(target);
+        }
+
+        cargo_add(&args, self.base_dir)
     }
 }
