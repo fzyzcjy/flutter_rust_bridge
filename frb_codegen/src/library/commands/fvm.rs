cac3cedd969a16477112fa154abb0a9fbbe60190
Stefan Huber <stefan.huber.niedling@outlook.com>
2026-02-05 19:09:36 +0100

    Ensure fvm is activated

--- frb_codegen/src/library/commands/fvm.rs
+++ frb_codegen/src/library/commands/fvm.rs
@@ -37,8 +37,11 @@ fn has_fvmrc(pwd: &Path) -> bool {
 
 #[allow(clippy::vec_init_then_push)]
 fn has_fvm_installation() -> bool {
-    command_run!(call_shell[None, Some(ExecuteCommandOptions { log_when_error: Some(false), ..Default::default() })], "fvm", "--version")
-        .map_or(false, |res| res.status.success())
+    let fvm_activation_succeeded = command_run!(call_shell[None, Some(ExecuteCommandOptions { log_when_error: Some(false), ..Default::default() })], "dart", "pub", "global", "activate", "fvm")
+        .map_or(false, |res| res.status.success());
+    let fvm_availability_succeeded = command_run!(call_shell[None, Some(ExecuteCommandOptions { log_when_error: Some(false), ..Default::default() })], "fvm", "--version")
+        .map_or(false, |res| res.status.success());
+    fvm_activation_succeeded && fvm_availability_succeeded
 }
 
 #[allow(clippy::vec_init_then_push)]
