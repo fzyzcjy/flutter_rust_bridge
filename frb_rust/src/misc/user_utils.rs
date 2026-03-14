953654a0664d75c3abd8d4af0db88f821319efb1
futpib <futpib@gmail.com>
2026-03-14 20:55:04 +0000

    Add setup_default_user_utils_with_log_level for configurable log level
    setup_default_user_utils() hardcodes LevelFilter::Trace on all platforms,
    flooding logcat/console with verbose output from dependencies in release
    builds. Add a new setup_default_user_utils_with_log_level() function that
    accepts a user-specified log level filter, while keeping the existing
    function unchanged for backwards compatibility.

--- frb_rust/src/misc/user_utils.rs
+++ frb_rust/src/misc/user_utils.rs
@@ -5,7 +5,22 @@ use crate::misc::panic_backtrace::PanicBacktrace;
 pub fn setup_default_user_utils() {
     // setup log before others, such that we can see logs in other setup functions
     #[cfg(feature = "log")]
-    setup_log_to_console();
+    setup_log_to_console(log::LevelFilter::Trace);
+    setup_backtrace();
+}
+
+/// Like [`setup_default_user_utils`], but allows configuring the log level filter.
+///
+/// # Example
+///
+/// ```rust,no_run
+/// flutter_rust_bridge::setup_default_user_utils_with_log_level(
+///     if cfg!(debug_assertions) { log::LevelFilter::Trace } else { log::LevelFilter::Warn }
+/// );
+/// ```
+#[cfg(feature = "log")]
+pub fn setup_default_user_utils_with_log_level(level: log::LevelFilter) {
+    setup_log_to_console(level);
     setup_backtrace();
 }
 
@@ -22,19 +37,19 @@ fn setup_backtrace() {
 }
 
 #[cfg(feature = "log")]
-fn setup_log_to_console() {
+fn setup_log_to_console(#[allow(unused)] level: log::LevelFilter) {
     #[cfg(target_os = "android")]
     let _ = android_logger::init_once(
-        android_logger::Config::default().with_max_level(log::LevelFilter::Trace),
+        android_logger::Config::default().with_max_level(level),
     );
 
     #[cfg(any(target_os = "ios", target_os = "macos"))]
     let _ = oslog::OsLogger::new("frb_user")
-        .level_filter(log::LevelFilter::Trace)
+        .level_filter(level)
         .init();
 
     #[cfg(target_family = "wasm")]
-    let _ = crate::misc::web_utils::WebConsoleLogger::init();
+    let _ = crate::misc::web_utils::WebConsoleLogger::init(level);
 
     // TODO add more platforms
 }
