935330a88909c114662fcc07d3810994c860579b
fzyzcjy <ch271828n@outlook.com>
2026-03-21 15:56:58 +0800

    Format generated pure_dart packages explicitly

--- tools/frb_internal/lib/src/utils/execute_process.dart
+++ tools/frb_internal/lib/src/utils/execute_process.dart
@@ -1,10 +1,21 @@
+import 'dart:io';
+
 // ignore: implementation_imports
 import 'package:flutter_rust_bridge/src/cli/run_command.dart';
 
 const kPinnedRustfmtNightly = 'nightly-2025-02-01';
 
-Future<void> executeDartFormat({required String pwd}) async =>
-    runCommand('dart', ['format', '.'], pwd: pwd);
+Future<void> executeDartFormat({required String pwd}) async {
+  final entries = [
+    'build.dart',
+    'bin',
+    'benchmark',
+    'lib',
+    'test',
+  ].where((entry) => File('$pwd/$entry').existsSync() || Directory('$pwd/$entry').existsSync()).toList();
+  if (entries.isEmpty) return;
+  await runCommand('dart', ['format', ...entries], pwd: pwd);
+}
 
 Future<void> executeRustFormat({required String pwd}) async =>
     runCommand('cargo', ['+$kPinnedRustfmtNightly', 'fmt'], pwd: pwd);
