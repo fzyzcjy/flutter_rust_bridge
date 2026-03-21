496167a35d7563f54ed37bd4cc23d35ac37c1441
fzyzcjy <ch271828n@outlook.com>
2026-03-21 14:49:11 +0800

    Format frb_utils Dart sources

--- frb_utils/lib/src/dart_web_test_utils/run_test.dart
+++ frb_utils/lib/src/dart_web_test_utils/run_test.dart
@@ -23,8 +23,9 @@ Future<void> executeTestWeb(TestWebConfig config) async {
   final webRoot = '$dartRoot/web';
   print('executeTestWeb: Pick dartRoot=$dartRoot');
 
-  List<String> cargoArgs =
-      config.rustFeatures.expand((x) => ['--features', x]).toList();
+  List<String> cargoArgs = config.rustFeatures
+      .expand((x) => ['--features', x])
+      .toList();
 
   print('executeTestWeb: compile');
   await executeBuildWeb(
@@ -91,14 +92,14 @@ Handler _createWebSocketHandler({
 const _kTestEntrypointHttpName = 'test_entrypoint.html';
 
 Handler _createIndexFileHandler() => (request) {
-      if (request.url.path == _kTestEntrypointHttpName) {
-        return Response.ok(
-          kTestEntrypointHtmlContent,
-          headers: {HttpHeaders.contentTypeHeader: 'text/html'},
-        );
-      }
-      return Response.notFound(null);
-    };
+  if (request.url.path == _kTestEntrypointHttpName) {
+    return Response.ok(
+      kTestEntrypointHtmlContent,
+      headers: {HttpHeaders.contentTypeHeader: 'text/html'},
+    );
+  }
+  return Response.notFound(null);
+};
 
 Future<Browser> _launchBrowser({
   required String baseAddr,
