e8137ba4e849596e4f62c3edd101ffec969384b6
fzyzcjy <ch271828n@outlook.com>
2026-03-21 16:25:31 +0800

    Apply dart format to execute_process helper

--- tools/frb_internal/lib/src/utils/execute_process.dart
+++ tools/frb_internal/lib/src/utils/execute_process.dart
@@ -6,13 +6,7 @@ import 'package:flutter_rust_bridge/src/cli/run_command.dart';
 const kPinnedRustfmtNightly = 'nightly-2025-02-01';
 
 Future<void> executeDartFormat({required String pwd}) async {
-  final entries = [
-    'build.dart',
-    'bin',
-    'benchmark',
-    'lib',
-    'test',
-  ]
+  final entries = ['build.dart', 'bin', 'benchmark', 'lib', 'test']
       .where(
         (entry) =>
             File('$pwd/$entry').existsSync() ||
