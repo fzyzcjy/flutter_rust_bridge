543541039d08d555a2d74042b754cf3af295e038
Chibeiyu <chibeiyu9907@gmail.com>
2026-03-26 20:45:27 +0800

    plugin support ohos

--- frb_codegen/src/library/commands/fvm.rs
+++ frb_codegen/src/library/commands/fvm.rs
@@ -6,10 +6,8 @@ pub(crate) fn command_arg_maybe_fvm(pwd: Option<&Path>) -> Option<String> {
     should_use_fvm(pwd).then(|| "fvm".to_owned())
 }
 
-fn should_use_fvm(_pwd: Option<&Path>) -> bool {
-    // if pwd.is_some() && !has_fvmrc(pwd.unwrap()) {
-    //     false
-    // } else {
+fn should_use_fvm(pwd: Option<&Path>) -> bool {
+    log::info!("pwd is {:?}",pwd);
     let has_fvm_installation_output = has_fvm_installation();
     if has_fvm_installation_output {
         fvm_install_flutter_version();
@@ -18,22 +16,35 @@ fn should_use_fvm(_pwd: Option<&Path>) -> bool {
         log::info!("Has .fvmrc but no fvm binary installation, thus skip using fvm.");
     }
     has_fvm_installation_output
-    // }
 }
-
-fn has_fvmrc(pwd: &Path) -> bool {
-    let mut directory = pwd;
-    loop {
-        if directory.join(".fvmrc").exists() {
-            return true;
-        }
-        if let Some(parent) = directory.parent() {
-            directory = parent;
-        } else {
-            return false;
-        }
-    }
-}
+// fn should_use_fvm(_pwd: Option<&Path>) -> bool {
+//     // if pwd.is_some() && !has_fvmrc(pwd.unwrap()) {
+//     //     false
+//     // } else {
+//     let has_fvm_installation_output = has_fvm_installation();
+//     if has_fvm_installation_output {
+//         fvm_install_flutter_version();
+//     }
+//     if !has_fvm_installation_output {
+//         log::info!("Has .fvmrc but no fvm binary installation, thus skip using fvm.");
+//     }
+//     has_fvm_installation_output
+//     // }
+// }
+// 
+// fn has_fvmrc(pwd: &Path) -> bool {
+//     let mut directory = pwd;
+//     loop {
+//         if directory.join(".fvmrc").exists() {
+//             return true;
+//         }
+//         if let Some(parent) = directory.parent() {
+//             directory = parent;
+//         } else {
+//             return false;
+//         }
+//     }
+// }
 
 #[allow(clippy::vec_init_then_push)]
 fn has_fvm_installation() -> bool {
