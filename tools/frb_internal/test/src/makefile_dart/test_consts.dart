e718114caeabcf89a5ce07eea4621f54786d1a98
fzyzcjy <ch271828n@outlook.com>
2026-04-04 19:32:32 +0800

    style: format frb_internal test consts

--- tools/frb_internal/test/src/makefile_dart/test_consts.dart
+++ tools/frb_internal/test/src/makefile_dart/test_consts.dart
@@ -3,7 +3,10 @@ import 'package:test/test.dart';
 
 void main() {
   test('kPrecommitGeneratePackages excludes deliberate_bad examples', () {
-    expect(kPrecommitGeneratePackages, isNot(contains('frb_example/deliberate_bad')));
+    expect(
+      kPrecommitGeneratePackages,
+      isNot(contains('frb_example/deliberate_bad')),
+    );
     expect(kPrecommitGeneratePackages, contains('frb_example/dart_minimal'));
     expect(kPrecommitGeneratePackages, contains('frb_example/flutter_package'));
   });
