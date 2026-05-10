99c4b4dfb7e00f35487fda2c46682641145a89aa
fzyzcjy <ch271828n@outlook.com>
2026-05-10 13:54:16 +0800

    Format generated valgrind entrypoints

--- frb_example/pure_dart_pde/test/dart_valgrind_test_entrypoint.dart
+++ frb_example/pure_dart_pde/test/dart_valgrind_test_entrypoint.dart
@@ -287,7 +287,8 @@ Future<void> callFileEntrypoints() async {
   await misc_type_twin_sync_test.main(skipRustLibInit: true);
   await newtype_pattern_twin_rust_async_test.main(skipRustLibInit: true);
   await newtype_pattern_twin_sync_test.main(skipRustLibInit: true);
-  await optional_primitive_misc_twin_rust_async_test.main(skipRustLibInit: true);
+  await optional_primitive_misc_twin_rust_async_test.main(
+      skipRustLibInit: true);
   await optional_primitive_misc_twin_sync_test.main(skipRustLibInit: true);
   await optional_twin_rust_async_test.main(skipRustLibInit: true);
   await optional_twin_sync_test.main(skipRustLibInit: true);
