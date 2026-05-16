4886a17ce04188ef575519693991629a66c10f8b
fzyzcjy <ch271828n@outlook.com>
2026-05-16 17:38:42 +0800

    Simplify console printing cfgs

--- frb_rust/src/misc/printing.rs
+++ frb_rust/src/misc/printing.rs
@@ -4,43 +4,8 @@
 /// failure paths where calling `log::*` could recursively re-enter the same
 /// failing logger.
 #[doc(hidden)]
-pub fn print_to_console(message: &str) {
-    #[cfg(target_os = "android")]
-    {
-        print_to_console_android(message);
-        return;
-    }
-
-    #[cfg(any(target_os = "ios", target_os = "macos"))]
-    {
-        print_to_console_apple(message);
-        return;
-    }
-
-    #[cfg(target_family = "wasm")]
-    {
-        crate::for_generated::web_utils::js_console_error(message);
-        return;
-    }
-
-    #[cfg(not(any(
-        target_os = "android",
-        target_os = "ios",
-        target_os = "macos",
-        target_family = "wasm"
-    )))]
-    {
-        eprintln!("{message}");
-    }
-}
-
-#[cfg(target_os = "android")]
-fn print_to_console_android(message: &str) {
-    print_to_console_android_impl(message);
-}
-
 #[cfg(all(target_os = "android", feature = "user-utils", feature = "log"))]
-fn print_to_console_android_impl(message: &str) {
+pub fn print_to_console(message: &str) {
     let record = log::Record::builder()
         .args(format_args!("{message}"))
         .level(log::Level::Error)
@@ -51,23 +16,24 @@ fn print_to_console_android_impl(message: &str) {
     android_logger::log(&record);
 }
 
-#[cfg(all(
-    target_os = "android",
-    not(all(feature = "user-utils", feature = "log"))
-))]
-fn print_to_console_android_impl(message: &str) {
-    eprintln!("{message}");
-}
-
+#[doc(hidden)]
 #[cfg(all(any(target_os = "ios", target_os = "macos"), feature = "user-utils"))]
-fn print_to_console_apple(message: &str) {
+pub fn print_to_console(message: &str) {
     oslog::OsLog::new("flutter_rust_bridge", "console").error(message);
 }
 
+#[doc(hidden)]
+#[cfg(target_family = "wasm")]
+pub fn print_to_console(message: &str) {
+    crate::for_generated::web_utils::js_console_error(message);
+}
+
+#[doc(hidden)]
 #[cfg(all(
-    any(target_os = "ios", target_os = "macos"),
-    not(feature = "user-utils")
+    not(target_family = "wasm"),
+    not(all(target_os = "android", feature = "user-utils", feature = "log")),
+    not(all(any(target_os = "ios", target_os = "macos"), feature = "user-utils"))
 ))]
-fn print_to_console_apple(message: &str) {
+pub fn print_to_console(message: &str) {
     eprintln!("{message}");
 }
