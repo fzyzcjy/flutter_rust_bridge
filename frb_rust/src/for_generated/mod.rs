b11308137d77b07301b2fda7a80c60276d1530ec
fzyzcjy <ch271828n@outlook.com>
2026-05-16 17:03:33 +0800

    Use platform logging fallback for bridge errors

--- frb_rust/src/for_generated/mod.rs
+++ frb_rust/src/for_generated/mod.rs
@@ -38,6 +38,8 @@ pub use crate::lockable::{
     base::Lockable, order::LockableOrder, order_computer::lockable_compute_decode_order,
     order_info::LockableOrderInfo,
 };
+#[cfg(feature = "log")]
+pub use crate::misc::frb_logging::frb_logging_fallback_print;
 #[allow(unused)]
 pub use crate::misc::manual_impl::*;
 pub use crate::misc::version::FLUTTER_RUST_BRIDGE_RUNTIME_VERSION;
