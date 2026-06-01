63b42cf52039bfebf6d050d85599796eb3087a25
fzyzcjy <fzyzcjy@users.noreply.github.com>
2026-06-01 14:45:59 +0000

    deploy: 9dd8e14332fe09d8ac1b5a8727efa6dff75b1f0d

--- demo/pkg/rust_lib.js
+++ demo/pkg/rust_lib.js
@@ -682,15 +682,15 @@ let wasm_bindgen;
             const ret = false;
             return ret;
         };
-        imports.wbg.__wbindgen_closure_wrapper390 = function(arg0, arg1, arg2) {
+        imports.wbg.__wbindgen_closure_wrapper392 = function(arg0, arg1, arg2) {
             const ret = makeMutClosure(arg0, arg1, 152, __wbg_adapter_36);
             return ret;
         };
-        imports.wbg.__wbindgen_closure_wrapper411 = function(arg0, arg1, arg2) {
+        imports.wbg.__wbindgen_closure_wrapper413 = function(arg0, arg1, arg2) {
             const ret = makeMutClosure(arg0, arg1, 166, __wbg_adapter_39);
             return ret;
         };
-        imports.wbg.__wbindgen_closure_wrapper421 = function(arg0, arg1, arg2) {
+        imports.wbg.__wbindgen_closure_wrapper423 = function(arg0, arg1, arg2) {
             const ret = makeMutClosure(arg0, arg1, 166, __wbg_adapter_39);
             return ret;
         };
