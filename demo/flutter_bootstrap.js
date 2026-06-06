f456dddac193fccd9f680ade1d4f7053bccfd53d
fzyzcjy <fzyzcjy@users.noreply.github.com>
2026-06-06 04:23:43 +0000

    deploy: 97a90de7c6649f7a83de331080b815f9ef3fa282

--- demo/flutter_bootstrap.js
+++ demo/flutter_bootstrap.js
@@ -33,6 +33,6 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7bf","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
+_flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a362","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
 _flutter.loader.load();
