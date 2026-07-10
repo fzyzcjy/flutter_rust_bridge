76c16150ea5bfd3de4ecddf17d2a1981603c979c
fzyzcjy <ch271828n@outlook.com>
2026-07-11 01:49:38 +0800

    Support valgrind namespaced type tests

--- frb_example/pure_dart_pde/test/api/namespaced_type_test.dart
+++ frb_example/pure_dart_pde/test/api/namespaced_type_test.dart
@@ -4,8 +4,8 @@ import 'package:frb_example_pure_dart_pde/src/rust/api/namespaced_type.dart';
 import 'package:frb_example_pure_dart_pde/src/rust/frb_generated.dart';
 import 'package:test/test.dart';
 
-Future<void> main() async {
-  await RustLib.init();
+Future<void> main({bool skipRustLibInit = false}) async {
+  if (!skipRustLibInit) await RustLib.init();
 
   test('returns a model from its namespaced module', () async {
     final models = await duplicateNamedModelsTwinNormal();
