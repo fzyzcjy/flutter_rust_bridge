71373157134e9b783746cc4cbfa413171b925c08
iWisp360 <iwisp360@tutamail.com>
2026-07-27 03:58:45 -0400

    add test to assert the library is linked

--- frb_example/flutter_via_integrate/integration_test/simple_test.dart
+++ frb_example/flutter_via_integrate/integration_test/simple_test.dart
@@ -1,13 +1,43 @@
+import 'dart:io';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_via_integrate/main.dart';
 import 'package:flutter_via_integrate/src/rust/frb_generated.dart';
 import 'package:integration_test/integration_test.dart';
 
-void main() {
+Future<void> main() async {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
   setUpAll(() async => await RustLib.init());
   testWidgets('Can call rust function', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
     expect(find.textContaining('Result: `Hello, Tom!`'), findsOneWidget);
   });
+
+  await verifyLibraryLinked();
+}
+
+Future<void> verifyLibraryLinked() async {
+  if (!Platform.isLinux) return;
+
+  final binaryPath = Platform.resolvedExecutable;
+  final processResult = await Process.run("ldd", [binaryPath]);
+
+  if (processResult.exitCode != 0) {
+    throw Exception("While trying to run ldd: ${processResult.stderr}");
+  }
+
+  final output = processResult.stdout.toString();
+
+  if (!output.contains('librust_lib')) {
+    throw Exception(
+      'The library from flutter_rust_bridge is not linked (by default with a `librust_lib` prefix)\nLDD Output:\n$output',
+    );
+  }
+
+  if (output.toLowerCase().contains('not found')) {
+    throw Exception(
+      'Unresolved dynamic dependencies found\nLDD Output:\n$output',
+    );
+  }
 }
