431d18a7b05178c22fac65d59ece7638d379ef57
fzyzcjy <ch271828n@outlook.com>
2026-08-09 12:35:52 +0800

    Limit direct boxed decoding to primitive identities

--- frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/codec/cst/decoder/ty/boxed.rs
+++ frb_codegen/src/library/codegen/generator/wire/rust/spec_generator/codec/cst/decoder/ty/boxed.rs
@@ -19,7 +19,11 @@ impl WireRustCodecCstGeneratorDecoderTrait for BoxedWireRustCodecCstGenerator<'_
         let exist_in_real_api = self.mir.exist_in_real_api;
         Acc::new(|target| {
             match (target, self.mir.inner.as_ref()) {
-                (Io, mir) if mir.is_primitive() => Some(format!(
+                (
+                    Io,
+                    MirType::Primitive(_)
+                    | MirType::Delegate(MirTypeDelegate::CastedPrimitive(_)),
+                ) => Some(format!(
                     "unsafe {{ {extra} flutter_rust_bridge::for_generated::box_from_leak_ptr(self) }}",
                     extra = if exist_in_real_api { "" } else { "*" }
                 )),
