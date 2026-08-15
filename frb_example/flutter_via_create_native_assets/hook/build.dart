93447edfecffd118010d42bed4dca4e2b0b27c1c
Tom <ch271828n@outlook.com>
2026-08-15 21:21:46 +0800

    Stabilize generated Dart formatting

--- frb_example/flutter_via_create_native_assets/hook/build.dart
+++ frb_example/flutter_via_create_native_assets/hook/build.dart
@@ -1,8 +1,9 @@
 import 'package:flutter_rust_bridge_hooks/flutter_rust_bridge_hooks.dart';
 
 void main(List<String> args) async {
+  const cratePath = 'rust';
   await build(args, (input, output) async {
-    await const FlutterRustBridgeNativeAssetsBuilder(cratePath: 'rust')
+    await const FlutterRustBridgeNativeAssetsBuilder(cratePath: cratePath)
         .run(input: input, output: output);
   });
 }
