a00bc5c53716c24d4b6e6fa5315b3ae828e18347
Tom <ch271828n@outlook.com>
2026-08-15 16:49:17 +0800

    Pin Pana before symlink regression

--- tools/frb_internal/lib/src/makefile_dart/lint.dart
+++ tools/frb_internal/lib/src/makefile_dart/lint.dart
@@ -182,7 +182,7 @@ Future<void> lintDartAnalyze(LintConfig config) async {
 }
 
 Future<void> lintDartPana(LintConfig config) async {
-  await exec('flutter pub global activate pana');
+  await exec('flutter pub global activate pana 0.23.17');
   for (final package in kDartPublishedPackages) {
     final exitCodeThreshold = kDartPanaExitCodeThresholdByPackage[package]!;
     await exec(
