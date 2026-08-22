ae1e528d207515d4beaeee9752203d4bc9aed30f
Tom Chen <ch271828n@outlook.com>
2026-08-22 16:30:42 +0800

    Restore stable Flutter plugin generated outputs
    Keep the import groups produced by repeated plugin integration runs under Flutter 3.47.

--- frb_example/flutter_package_native_assets/lib/src/rust/frb_generated.io.dart
+++ frb_example/flutter_package_native_assets/lib/src/rust/frb_generated.io.dart
@@ -4,10 +4,13 @@
 // ignore_for_file: unused_import, unused_element, unnecessary_import, duplicate_ignore, invalid_use_of_internal_member, annotate_overrides, non_constant_identifier_names, curly_braces_in_flow_control_structures, prefer_const_literals_to_create_immutables, unused_field
 
 import 'api/simple.dart';
+
 import 'dart:async';
 import 'dart:convert';
 import 'dart:ffi' as ffi;
+
 import 'frb_generated.dart';
+
 import 'package:flutter_rust_bridge/flutter_rust_bridge_for_generated_io.dart';
 
 abstract class RustLibApiImplPlatform extends BaseApiImpl<RustLibWire> {
