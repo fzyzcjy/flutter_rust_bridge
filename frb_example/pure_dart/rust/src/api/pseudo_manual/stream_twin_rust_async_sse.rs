b4d0b244af5dd969287c7ee8052a491f98230602
Tom <ch271828n@outlook.com>
2026-08-23 07:40:43 +0800

    Regenerate Web StreamSink examples

--- frb_example/pure_dart/rust/src/api/pseudo_manual/stream_twin_rust_async_sse.rs
+++ frb_example/pure_dart/rust/src/api/pseudo_manual/stream_twin_rust_async_sse.rs
@@ -60,8 +60,7 @@ pub async fn handle_stream_sink_at_1_twin_rust_async_sse(
     max: u32,
     sink: StreamSink<LogTwinRustAsyncSse, flutter_rust_bridge::SseCodec>,
 ) {
-    (FLUTTER_RUST_BRIDGE_HANDLER.thread_pool())
-        .execute(transfer!(|| { handle_stream_inner(key, max, sink) }));
+    dispatch_handle_stream(key, max, sink);
 }
 
 #[flutter_rust_bridge::frb(serialize)]
@@ -70,8 +69,7 @@ pub async fn handle_stream_sink_at_2_twin_rust_async_sse(
     sink: StreamSink<LogTwinRustAsyncSse, flutter_rust_bridge::SseCodec>,
     max: u32,
 ) {
-    (FLUTTER_RUST_BRIDGE_HANDLER.thread_pool())
-        .execute(transfer!(|| { handle_stream_inner(key, max, sink) }));
+    dispatch_handle_stream(key, max, sink);
 }
 
 #[flutter_rust_bridge::frb(serialize)]
@@ -80,6 +78,18 @@ pub async fn handle_stream_sink_at_3_twin_rust_async_sse(
     key: u32,
     max: u32,
 ) {
+    dispatch_handle_stream(key, max, sink);
+}
+
+fn dispatch_handle_stream(
+    key: u32,
+    max: u32,
+    sink: StreamSink<LogTwinRustAsyncSse, flutter_rust_bridge::SseCodec>,
+) {
+    #[cfg(target_family = "wasm")]
+    handle_stream_inner(key, max, sink);
+
+    #[cfg(not(target_family = "wasm"))]
     (FLUTTER_RUST_BRIDGE_HANDLER.thread_pool())
         .execute(transfer!(|| { handle_stream_inner(key, max, sink) }));
 }
