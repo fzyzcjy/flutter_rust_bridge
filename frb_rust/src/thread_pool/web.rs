f93d2cde5c8b9ee770f075b5c4af11de8ac530b5
Tom Chen <ch271828n@outlook.com>
2026-08-22 15:35:38 +0800

    Revert "Fall back when web worker pool is unavailable"
    This reverts commit 774b008962d22e18915d2d99474439fb21bd1d1f.

--- frb_rust/src/thread_pool/web.rs
+++ frb_rust/src/thread_pool/web.rs
@@ -1,9 +1,7 @@
-use crate::third_party::wasm_bindgen::worker_pool::WorkerPool;
+pub use crate::third_party::wasm_bindgen::worker_pool::WorkerPool as SimpleThreadPool;
 use crate::web_transfer::transfer_closure::TransferClosure;
 use std::thread::LocalKey;
 use wasm_bindgen::JsValue;
-
-pub struct SimpleThreadPool(Option<WorkerPool>);
 
 pub trait BaseThreadPool {
     fn execute(&self, closure: TransferClosure<JsValue>);
@@ -12,29 +10,5 @@ pub trait BaseThreadPool {
 impl BaseThreadPool for &'static LocalKey<SimpleThreadPool> {
     fn execute(&self, closure: TransferClosure<JsValue>) {
         self.with(|inner| inner.execute(closure)).unwrap()
-    }
-}
-
-impl SimpleThreadPool {
-    fn execute(&self, closure: TransferClosure<JsValue>) -> Result<(), JsValue> {
-        match &self.0 {
-            Some(inner) => inner.execute(closure),
-            None => {
-                closure.run_inline();
-                Ok(())
-            }
-        }
-    }
-}
-
-impl Default for SimpleThreadPool {
-    fn default() -> Self {
-        match WorkerPool::new(None, None, None, None) {
-            Ok(inner) => Self(Some(inner)),
-            Err(err) => {
-                crate::console_error!("Failed to initialize web worker pool: {:?}", err);
-                Self(None)
-            }
-        }
     }
 }
