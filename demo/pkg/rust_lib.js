73ff96a59148fb28e9fdfd4f65367cca074977ec
fzyzcjy <fzyzcjy@users.noreply.github.com>
2026-08-30 02:38:23 +0000

    deploy: e9712d67008b857acacd2285248360cf332da764

--- demo/pkg/rust_lib.js
+++ demo/pkg/rust_lib.js
@@ -312,7 +312,7 @@ let wasm_bindgen;
     }
 
     function __wbg_adapter_39(arg0, arg1) {
-        wasm._dyn_core_37c17653ddd24b40___ops__function__FnMut_____Output______as_wasm_bindgen_f216dbd2940be4eb___closure__WasmClosure___describe__invoke______(arg0, arg1);
+        wasm._dyn_core_2dd864f4fe5fa53___ops__function__FnMut_____Output______as_wasm_bindgen_e61f3978eca96c45___closure__WasmClosure___describe__invoke______(arg0, arg1);
     }
 
     function __wbg_adapter_42(arg0, arg1, arg2) {
