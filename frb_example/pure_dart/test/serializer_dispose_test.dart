f4c5f919fff5c0e01b388536ea4fa87c3b813657
Tom <ch271828n@outlook.com>
2026-09-05 20:04:12 +0800

    Import the native wire constructor in serializer regression

--- frb_example/pure_dart/test/serializer_dispose_test.dart
+++ frb_example/pure_dart/test/serializer_dispose_test.dart
@@ -11,6 +11,7 @@ import 'package:flutter_rust_bridge/src/main_components/handler.dart';
 import 'package:flutter_rust_bridge/src/main_components/port_manager.dart';
 import 'package:frb_example_pure_dart/src/rust/api/lifetimeable.dart';
 import 'package:frb_example_pure_dart/src/rust/frb_generated.dart';
+import 'package:frb_example_pure_dart/src/rust/frb_generated.io.dart';
 import 'package:test/test.dart';
 
 Future<void> main({bool skipRustLibInit = false}) async {
