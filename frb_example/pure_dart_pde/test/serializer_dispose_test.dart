299b4f4bfc0001678e91a82b21e7022569becf79
Tom <ch271828n@outlook.com>
2026-09-05 20:04:47 +0800

    Regenerate PDE native wire import

--- frb_example/pure_dart_pde/test/serializer_dispose_test.dart
+++ frb_example/pure_dart_pde/test/serializer_dispose_test.dart
@@ -13,6 +13,7 @@ import 'package:flutter_rust_bridge/src/main_components/handler.dart';
 import 'package:flutter_rust_bridge/src/main_components/port_manager.dart';
 import 'package:frb_example_pure_dart_pde/src/rust/api/lifetimeable.dart';
 import 'package:frb_example_pure_dart_pde/src/rust/frb_generated.dart';
+import 'package:frb_example_pure_dart_pde/src/rust/frb_generated.io.dart';
 import 'package:test/test.dart';
 
 Future<void> main({bool skipRustLibInit = false}) async {
