f32494a19a32a08e5d43f36dd7b970377b5bbc80
Tom <ch271828n@outlook.com>
2026-09-06 15:19:34 +0800

    Use the existing internal annotation export

--- frb_dart/lib/src/generalized_isolate/_web.dart
+++ frb_dart/lib/src/generalized_isolate/_web.dart
@@ -6,7 +6,6 @@ import 'dart:js_interop_unsafe';
 import 'dart:math';
 
 import 'package:flutter_rust_bridge/flutter_rust_bridge_for_generated_web.dart';
-import 'package:meta/meta.dart';
 import 'package:web/web.dart' as web;
 
 dynamic _extractData(JSAny? data) => data.dartify();
