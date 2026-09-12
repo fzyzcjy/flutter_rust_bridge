ae309cec8c4477b655768694edb780a78183fcae
Tom <ch271828n@outlook.com>
2026-09-12 12:18:26 +0800

    Keep the build.rs example adder as a regular function

--- frb_example/dart_build_rs/rust/src/api/minimal.rs
+++ frb_example/dart_build_rs/rust/src/api/minimal.rs
@@ -5,12 +5,6 @@ pub fn init_app() {
     flutter_rust_bridge::setup_default_user_utils();
 }
 
-macro_rules! define_adder {
-    () => {
-        pub fn minimal_adder(a: i32, b: i32) -> i32 {
-            a + b
-        }
-    };
+pub fn minimal_adder(a: i32, b: i32) -> i32 {
+    a + b
 }
-
-define_adder!();
