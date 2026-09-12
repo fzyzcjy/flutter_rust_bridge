40c990d4f12cd8ff81e1c8ac99a253eaebe2f856
Tom <ch271828n@outlook.com>
2026-09-13 07:41:10 +0800

    Copy logger initialization rationale from pure_dart

--- frb_example/dart_build_rs/rust/src/api/minimal.rs
+++ frb_example/dart_build_rs/rust/src/api/minimal.rs
@@ -4,6 +4,8 @@ flutter_rust_bridge::enable_frb_rust_to_dart_logging!();
 
 #[frb(init)]
 pub fn init_app() {
+    // Keep stacktraces, but do not install a default logger, since that would
+    // prevent users from enabling the FRB Rust-to-Dart logging bridge.
     flutter_rust_bridge::setup_backtrace();
 }
 
